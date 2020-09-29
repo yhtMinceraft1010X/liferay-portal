0fc93ba5595c0e07b96257e121b0bb218fffd724
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-09-29 12:56:01 +0100

    COMMERCE-4979 Define the field values of a CProduct

---
+++ modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/info/item/provider/CProductInfoItemFieldValuesProvider.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.content.web.internal.info.item.provider;
+
+import com.liferay.commerce.product.content.web.internal.info.CProductInfoItemFields;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CProduct;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.item.InfoItemFieldValues;
+import com.liferay.info.item.InfoItemReference;
+import com.liferay.info.item.field.reader.InfoItemFieldReaderFieldSetProvider;
+import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = Constants.SERVICE_RANKING + ":Integer=10",
+	service = InfoItemFieldValuesProvider.class
+)
+public class CProductInfoItemFieldValuesProvider
+	implements InfoItemFieldValuesProvider<CProduct> {
+
+	@Override
+	public InfoItemFieldValues getInfoItemFieldValues(CProduct cProduct) {
+		return InfoItemFieldValues.builder(
+		).infoFieldValues(
+			_getCProductInfoFieldValues(cProduct)
+		).infoFieldValues(
+			_infoItemFieldReaderFieldSetProvider.getInfoFieldValues(
+				CProduct.class.getName(), cProduct)
+		).infoItemReference(
+			new InfoItemReference(
+				CProduct.class.getName(), cProduct.getCProductId())
+		).build();
+	}
+
+	private List<InfoFieldValue<Object>> _getCProductInfoFieldValues(
+		CProduct cProduct) {
+
+		List<InfoFieldValue<Object>> cProductInfoFieldValues =
+			new ArrayList<>();
+
+		CPDefinition cpDefinition =
+			_cpDefinitionLocalService.fetchCPDefinitionByCProductId(
+				cProduct.getCProductId());
+
+		cProductInfoFieldValues.add(
+			new InfoFieldValue<>(
+				CProductInfoItemFields.titleInfoField,
+				InfoLocalizedValue.<String>builder(
+				).defaultLocale(
+					LocaleUtil.fromLanguageId(
+						cpDefinition.getDefaultLanguageId())
+				).values(
+					cpDefinition.getNameMap()
+				).build()));
+		cProductInfoFieldValues.add(
+			new InfoFieldValue<>(
+				CProductInfoItemFields.descriptionInfoField,
+				InfoLocalizedValue.<String>builder(
+				).defaultLocale(
+					LocaleUtil.fromLanguageId(
+						cpDefinition.getDefaultLanguageId())
+				).values(
+					cpDefinition.getDescriptionMap()
+				).build()));
+
+		return cProductInfoFieldValues;
+	}
+
+	@Reference
+	private CPDefinitionLocalService _cpDefinitionLocalService;
+
+	@Reference
+	private InfoItemFieldReaderFieldSetProvider
+		_infoItemFieldReaderFieldSetProvider;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
