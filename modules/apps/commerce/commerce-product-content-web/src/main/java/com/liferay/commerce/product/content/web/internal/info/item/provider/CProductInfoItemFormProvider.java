f1468ac8c46b4d2d8752d308af8cff6e65a7b703
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-09-29 12:55:43 +0100

    COMMERCE-4979 Define the fields of a CProduct

---
+++ modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/info/item/provider/CProductInfoItemFormProvider.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.content.web.internal.info.item.provider;
+
+import com.liferay.commerce.product.content.web.internal.info.CProductInfoItemFields;
+import com.liferay.commerce.product.model.CProduct;
+import com.liferay.info.field.InfoFieldSet;
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.item.field.reader.InfoItemFieldReaderFieldSetProvider;
+import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+
+import java.util.Locale;
+import java.util.Set;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = Constants.SERVICE_RANKING + ":Integer=10",
+	service = InfoItemFormProvider.class
+)
+public class CProductInfoItemFormProvider
+	implements InfoItemFormProvider<CProduct> {
+
+	@Override
+	public InfoForm getInfoForm() {
+		Set<Locale> availableLocales = LanguageUtil.getAvailableLocales();
+
+		InfoLocalizedValue.Builder<String> infoLocalizedValueBuilder =
+			InfoLocalizedValue.builder();
+
+		for (Locale locale : availableLocales) {
+			infoLocalizedValueBuilder.value(
+				locale,
+				ResourceActionsUtil.getModelResource(
+					locale, CProduct.class.getName()));
+		}
+
+		return InfoForm.builder(
+		).infoFieldSetEntry(
+			_getBasicInformationInfoFieldSet()
+		).labelInfoLocalizedValue(
+			infoLocalizedValueBuilder.build()
+		).name(
+			CProduct.class.getName()
+		).build();
+	}
+
+	private InfoFieldSet _getBasicInformationInfoFieldSet() {
+		return InfoFieldSet.builder(
+		).infoFieldSetEntry(
+			CProductInfoItemFields.titleInfoField
+		).infoFieldSetEntry(
+			CProductInfoItemFields.descriptionInfoField
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(
+				"com.liferay.commerce.lang", "basic-information")
+		).name(
+			"basic-information"
+		).build();
+	}
+
+	@Reference
+	private InfoItemFieldReaderFieldSetProvider
+		_infoItemFieldReaderFieldSetProvider;
+
+}
