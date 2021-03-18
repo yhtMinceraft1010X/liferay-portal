920b9161aa4976aafce9d0b0de0045a064d74ee4
Laszlo Pap <laszlo.pap@liferay.com>
2021-03-18 17:48:22 +0100

    LPS-129039 Update related Sites' languages when changing Instance Settings languages

--- modules/apps/portal-settings/portal-settings-web/src/main/java/com/liferay/portal/settings/web/internal/portlet/action/EditCompanyMVCActionCommand.java
+++ modules/apps/portal-settings/portal-settings-web/src/main/java/com/liferay/portal/settings/web/internal/portlet/action/EditCompanyMVCActionCommand.java
@@ -43,6 +43,7 @@ import com.liferay.portal.kernel.model.Address;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.EmailAddress;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Phone;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.Website;
@@ -314,7 +315,31 @@ public class EditCompanyMVCActionCommand extends BaseFormMVCActionCommand {
 			industry, type, size, languageId, timeZoneId, addresses,
 			emailAddresses, phones, websites, unicodeProperties);
 
+		_updateCompanySitesLocale(companyId, unicodeProperties);
+
 		_portal.resetCDNHosts();
+	}
+
+	private void _updateCompanySitesLocale(
+		long companyId, UnicodeProperties unicodeProperties) {
+
+		List<Group> groups = _groupLocalService.getActiveGroups(
+			companyId, true);
+
+		String newCompanyLanguageIds = unicodeProperties.getProperty(
+			PropsKeys.LOCALES);
+
+		for (Group group : groups) {
+			if (group.getType() == GroupConstants.TYPE_SITE_OPEN) {
+				UnicodeProperties groupTypeSettingsUnicodeProperties =
+					group.getTypeSettingsProperties();
+
+				groupTypeSettingsUnicodeProperties.setProperty(
+					PropsKeys.LOCALES, newCompanyLanguageIds);
+
+				_groupLocalService.updateGroup(group);
+			}
+		}
 	}
 
 	private void _validateAvailableLanguages(ActionRequest actionRequest)
