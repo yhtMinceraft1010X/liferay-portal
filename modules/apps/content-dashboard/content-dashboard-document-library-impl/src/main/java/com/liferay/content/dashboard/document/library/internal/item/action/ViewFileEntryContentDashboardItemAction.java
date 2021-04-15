4e83a6fab91a5932e47a5d4571e5d83c29b00fc7
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-04-15 08:19:58 +0200

    LPS-119966 Implementing actions for documents

---
+++ modules/apps/content-dashboard/content-dashboard-document-library-impl/src/main/java/com/liferay/content/dashboard/document/library/internal/item/action/ViewFileEntryContentDashboardItemAction.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.document.library.internal.item.action;
+
+import com.liferay.asset.display.page.portlet.AssetDisplayPageFriendlyURLProvider;
+import com.liferay.content.dashboard.item.action.ContentDashboardItemAction;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Locale;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alejandro Tardín
+ */
+public class ViewFileEntryContentDashboardItemAction
+	implements ContentDashboardItemAction {
+
+	public ViewFileEntryContentDashboardItemAction(
+		AssetDisplayPageFriendlyURLProvider assetDisplayPageFriendlyURLProvider,
+		Http http, HttpServletRequest httpServletRequest, FileEntry fileEntry,
+		Language language) {
+
+		_assetDisplayPageFriendlyURLProvider =
+			assetDisplayPageFriendlyURLProvider;
+		_http = http;
+		_httpServletRequest = httpServletRequest;
+		_fileEntry = fileEntry;
+		_language = language;
+	}
+
+	@Override
+	public String getIcon() {
+		return "view";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "view");
+	}
+
+	@Override
+	public String getName() {
+		return "view";
+	}
+
+	@Override
+	public Type getType() {
+		return Type.VIEW;
+	}
+
+	@Override
+	public String getURL() {
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)_httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return _getViewURL(themeDisplay.getLocale(), themeDisplay);
+	}
+
+	@Override
+	public String getURL(Locale locale) {
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)_httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return _getViewURL(locale, themeDisplay);
+	}
+
+	private String _getViewURL(Locale locale, ThemeDisplay themeDisplay) {
+		try {
+			ThemeDisplay clonedThemeDisplay =
+				(ThemeDisplay)themeDisplay.clone();
+
+			clonedThemeDisplay.setScopeGroupId(_fileEntry.getGroupId());
+
+			return Optional.ofNullable(
+				_assetDisplayPageFriendlyURLProvider.getFriendlyURL(
+					FileEntry.class.getName(), _fileEntry.getFileEntryId(),
+					locale, clonedThemeDisplay)
+			).map(
+				url -> {
+					String backURL = ParamUtil.getString(
+						_httpServletRequest, "backURL");
+
+					if (Validator.isNotNull(backURL)) {
+						return _http.setParameter(url, "p_l_back_url", backURL);
+					}
+
+					return _http.setParameter(
+						url, "p_l_back_url", themeDisplay.getURLCurrent());
+				}
+			).orElse(
+				StringPool.BLANK
+			);
+		}
+		catch (CloneNotSupportedException | PortalException exception) {
+			_log.error(exception, exception);
+
+			return StringPool.BLANK;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ViewFileEntryContentDashboardItemAction.class);
+
+	private final AssetDisplayPageFriendlyURLProvider
+		_assetDisplayPageFriendlyURLProvider;
+	private final FileEntry _fileEntry;
+	private final Http _http;
+	private final HttpServletRequest _httpServletRequest;
+	private final Language _language;
+
+}
---
+++ modules/apps/content-dashboard/content-dashboard-document-library-impl/src/main/java/com/liferay/content/dashboard/document/library/internal/item/action/provider/ViewFileEntryContentDashboardItemActionProvider.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.document.library.internal.item.action.provider;
+
+import com.liferay.asset.display.page.portlet.AssetDisplayPageFriendlyURLProvider;
+import com.liferay.content.dashboard.document.library.internal.item.action.ViewFileEntryContentDashboardItemAction;
+import com.liferay.content.dashboard.item.action.ContentDashboardItemAction;
+import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemActionProvider;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(service = ContentDashboardItemActionProvider.class)
+public class ViewFileEntryContentDashboardItemActionProvider
+	implements ContentDashboardItemActionProvider<FileEntry> {
+
+	@Override
+	public ContentDashboardItemAction getContentDashboardItemAction(
+		FileEntry fileEntry, HttpServletRequest httpServletRequest) {
+
+		if (!isShow(fileEntry, httpServletRequest)) {
+			return null;
+		}
+
+		return _getContentDashboardItemAction(httpServletRequest, fileEntry);
+	}
+
+	@Override
+	public String getKey() {
+		return "view";
+	}
+
+	@Override
+	public ContentDashboardItemAction.Type getType() {
+		return ContentDashboardItemAction.Type.VIEW;
+	}
+
+	@Override
+	public boolean isShow(
+		FileEntry fileEntry, HttpServletRequest httpServletRequest) {
+
+		if (ListUtil.isEmpty(
+				fileEntry.getFileVersions(WorkflowConstants.STATUS_APPROVED))) {
+
+			return false;
+		}
+
+		ContentDashboardItemAction contentDashboardItemAction =
+			_getContentDashboardItemAction(httpServletRequest, fileEntry);
+
+		if (Validator.isNull(contentDashboardItemAction.getURL())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private ContentDashboardItemAction _getContentDashboardItemAction(
+		HttpServletRequest httpServletRequest, FileEntry fileEntry) {
+
+		return new ViewFileEntryContentDashboardItemAction(
+			_assetDisplayPageFriendlyURLProvider, _http, httpServletRequest,
+			fileEntry, _language);
+	}
+
+	@Reference
+	private AssetDisplayPageFriendlyURLProvider
+		_assetDisplayPageFriendlyURLProvider;
+
+	@Reference
+	private Http _http;
+
+	@Reference
+	private Language _language;
+
+}
