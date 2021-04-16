6c68d608c26cbb0acfe88b645971c5aa14217a20
Samuel Tran <samuel.tran@liferay.com>
2021-04-15 19:15:56 -0700

    LPS-129709 Create group for each publication

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/model/impl/CTCollectionImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/model/impl/CTCollectionImpl.java
@@ -15,8 +15,13 @@
 package com.liferay.change.tracking.model.impl;
 
 import com.liferay.change.tracking.constants.CTConstants;
+import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 
 /**
@@ -24,6 +29,25 @@ import com.liferay.portal.kernel.service.UserLocalServiceUtil;
  * @author Daniel Kocsis
  */
 public class CTCollectionImpl extends CTCollectionBaseImpl {
+
+	@Override
+	public Group getGroup() {
+		return GroupLocalServiceUtil.fetchGroup(
+			getCompanyId(),
+			ClassNameLocalServiceUtil.getClassNameId(CTCollection.class),
+			getCtCollectionId());
+	}
+
+	@Override
+	public long getGroupId() {
+		Group group = getGroup();
+
+		if (group == null) {
+			return GroupConstants.DEFAULT_LIVE_GROUP_ID;
+		}
+
+		return group.getGroupId();
+	}
 
 	@Override
 	public String getUserName() {
