2a932741e2077287a27ee9b4ad44abf0df1b79ca
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-05-06 11:51:20 -0300

    LPS-131813 Delete app-builder and app-builder-workflow modules

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-rest-impl/src/main/resources/com/liferay/app/builder/workflow/rest/internal/resource/v1_0/helper/dependencies/approve-script.groovy
+++
@@ -1,5 +0,0 @@
-import com.liferay.portal.kernel.workflow.WorkflowStatusManagerUtil;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
-
-WorkflowStatusManagerUtil.updateStatus(
-	WorkflowConstants.getLabelStatus("approved"), workflowContext);
