9cf1fb84dbdcf96fe2207dee4f5ed6781a971010
Alessio Rendina <asilcomando@gmail.com>
2021-05-12 18:08:53 +0200

    COMMERCE-6405 Added integration tests

--- modules/apps/commerce/commerce-media-test/src/testIntegration/java/com/liferay/commerce/media/test/CommerceMediaResolverTest.java
+++ modules/apps/commerce/commerce-media-test/src/testIntegration/java/com/liferay/commerce/media/test/CommerceMediaResolverTest.java
@@ -18,7 +18,22 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.commerce.account.constants.CommerceAccountConstants;
 import com.liferay.commerce.account.model.CommerceAccount;
 import com.liferay.commerce.account.test.util.CommerceAccountTestUtil;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
 import com.liferay.commerce.media.CommerceMediaResolver;
+import com.liferay.commerce.media.constants.CommerceMediaConstants;
+import com.liferay.commerce.product.constants.CPAttachmentFileEntryConstants;
+import com.liferay.commerce.product.model.CPAttachmentFileEntry;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CommerceCatalog;
+import com.liferay.commerce.product.service.CPAttachmentFileEntryLocalService;
+import com.liferay.commerce.product.service.CommerceCatalogLocalService;
+import com.liferay.commerce.product.test.util.CPTestUtil;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.test.util.DLTestUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
@@ -31,10 +46,18 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -64,6 +87,9 @@ public class CommerceMediaResolverTest {
 		_group = GroupTestUtil.addGroup(
 			_company.getCompanyId(), _user.getUserId(), 0);
 
+		_commerceCurrency = CommerceCurrencyTestUtil.addCommerceCurrency(
+			_company.getCompanyId());
+
 		_serviceContext = ServiceContextTestUtil.getServiceContext(
 			_company.getCompanyId(), _group.getGroupId(), _user.getUserId());
 
@@ -79,20 +105,190 @@ public class CommerceMediaResolverTest {
 	}
 
 	@Test
-	public void testSendBytes() throws Exception {
+	public void testGetCdnUrl() throws Exception {
+		frutillaRule.scenario(
+			"Test commerce media resolver url"
+		).given(
+			"A commerce product attachment file entry"
+		).when(
+			"I invoke getUrl method"
+		).then(
+			"I expect the URL to be formatted as cdnUrl"
+		);
+
+		CommerceCatalog commerceCatalog =
+			_commerceCatalogLocalService.addCommerceCatalog(
+				null, RandomTestUtil.randomString(),
+				_commerceCurrency.getCode(), LocaleUtil.US.getDisplayLanguage(),
+				_serviceContext);
+
+		CPDefinition cpDefinition = CPTestUtil.addCPDefinition(
+			commerceCatalog.getGroupId());
+
+		long now = System.currentTimeMillis();
+
+		Date displayDate = new Date(now - Time.HOUR);
+		Date expirationDate = new Date(now + Time.DAY);
+
+		Calendar displayCal = CalendarFactoryUtil.getCalendar(
+			_user.getTimeZone());
+
+		displayCal.setTime(displayDate);
+
+		int displayDateMonth = displayCal.get(Calendar.MONTH);
+		int displayDateDay = displayCal.get(Calendar.DATE);
+		int displayDateYear = displayCal.get(Calendar.YEAR);
+		int displayDateHour = displayCal.get(Calendar.HOUR);
+		int displayDateMinute = displayCal.get(Calendar.MINUTE);
+
+		if (displayCal.get(Calendar.AM_PM) == Calendar.PM) {
+			displayDateHour += 12;
+		}
+
+		Calendar expirationCal = CalendarFactoryUtil.getCalendar(
+			_user.getTimeZone());
+
+		expirationCal.setTime(expirationDate);
+
+		int expirationDateMonth = expirationCal.get(Calendar.MONTH);
+		int expirationDateDay = expirationCal.get(Calendar.DATE);
+		int expirationDateYear = expirationCal.get(Calendar.YEAR);
+		int expirationDateHour = expirationCal.get(Calendar.HOUR);
+		int expirationDateMinute = expirationCal.get(Calendar.MINUTE);
+
+		if (expirationCal.get(Calendar.AM_PM) == Calendar.PM) {
+			expirationDateHour += 12;
+		}
+
+		String cdnUrl = "http://www.liferay.com/";
+
+		CPAttachmentFileEntry cpAttachmentFileEntry =
+			_cpAttachmentFileEntryLocalService.addCPAttachmentFileEntry(
+				null, _user.getUserId(), _group.getGroupId(),
+				PortalUtil.getClassNameId(CPDefinition.class.getName()),
+				cpDefinition.getCPDefinitionId(), 0, cdnUrl, true,
+				displayDateMonth, displayDateDay, displayDateYear,
+				displayDateHour, displayDateMinute, expirationDateMonth,
+				expirationDateDay, expirationDateYear, expirationDateHour,
+				expirationDateMinute, true,
+				RandomTestUtil.randomLocaleStringMap(), null, 0D,
+				CPAttachmentFileEntryConstants.TYPE_IMAGE, _serviceContext);
+
+		String url = _commerceMediaResolver.getUrl(
+			_commerceAccount.getCommerceAccountId(),
+			cpAttachmentFileEntry.getCPAttachmentFileEntryId());
+
+		Assert.assertEquals(cdnUrl, url);
+	}
+
+	@Test
+	public void testGetUrl() throws Exception {
+		frutillaRule.scenario(
+			"Test commerce media resolver url"
+		).given(
+			"A commerce product attachment file entry"
+		).when(
+			"I invoke getUrl method"
+		).then(
+			"I expect the URL to be formatted correctly"
+		);
+
+		CommerceCatalog commerceCatalog =
+			_commerceCatalogLocalService.addCommerceCatalog(
+				null, RandomTestUtil.randomString(),
+				_commerceCurrency.getCode(), LocaleUtil.US.getDisplayLanguage(),
+				_serviceContext);
+
+		CPDefinition cpDefinition = CPTestUtil.addCPDefinition(
+			commerceCatalog.getGroupId());
+
+		long now = System.currentTimeMillis();
+
+		Date displayDate = new Date(now - Time.HOUR);
+		Date expirationDate = new Date(now + Time.DAY);
+
+		Calendar displayCal = CalendarFactoryUtil.getCalendar(
+			_user.getTimeZone());
+
+		displayCal.setTime(displayDate);
+
+		int displayDateMonth = displayCal.get(Calendar.MONTH);
+		int displayDateDay = displayCal.get(Calendar.DATE);
+		int displayDateYear = displayCal.get(Calendar.YEAR);
+		int displayDateHour = displayCal.get(Calendar.HOUR);
+		int displayDateMinute = displayCal.get(Calendar.MINUTE);
+
+		if (displayCal.get(Calendar.AM_PM) == Calendar.PM) {
+			displayDateHour += 12;
+		}
+
+		Calendar expirationCal = CalendarFactoryUtil.getCalendar(
+			_user.getTimeZone());
+
+		expirationCal.setTime(expirationDate);
+
+		int expirationDateMonth = expirationCal.get(Calendar.MONTH);
+		int expirationDateDay = expirationCal.get(Calendar.DATE);
+		int expirationDateYear = expirationCal.get(Calendar.YEAR);
+		int expirationDateHour = expirationCal.get(Calendar.HOUR);
+		int expirationDateMinute = expirationCal.get(Calendar.MINUTE);
+
+		if (expirationCal.get(Calendar.AM_PM) == Calendar.PM) {
+			expirationDateHour += 12;
+		}
+
+		DLFolder dlFolder = DLTestUtil.addDLFolder(
+			commerceCatalog.getGroupId());
+
+		DLFileEntry dlFileEntry = DLTestUtil.addDLFileEntry(
+			dlFolder.getFolderId());
+
+		CPAttachmentFileEntry cpAttachmentFileEntry =
+			_cpAttachmentFileEntryLocalService.addCPAttachmentFileEntry(
+				null, _user.getUserId(), _group.getGroupId(),
+				PortalUtil.getClassNameId(CPDefinition.class.getName()),
+				cpDefinition.getCPDefinitionId(), dlFileEntry.getFileEntryId(),
+				null, false, displayDateMonth, displayDateDay, displayDateYear,
+				displayDateHour, displayDateMinute, expirationDateMonth,
+				expirationDateDay, expirationDateYear, expirationDateHour,
+				expirationDateMinute, true,
+				RandomTestUtil.randomLocaleStringMap(), null, 0D,
+				CPAttachmentFileEntryConstants.TYPE_IMAGE, _serviceContext);
+
+		String url = _commerceMediaResolver.getUrl(
+			_commerceAccount.getCommerceAccountId(),
+			cpAttachmentFileEntry.getCPAttachmentFileEntryId());
+
+		StringBundler sb = new StringBundler(7);
+
+		sb.append(PortalUtil.getPathModule());
+		sb.append(StringPool.SLASH);
+		sb.append(CommerceMediaConstants.SERVLET_PATH);
+		sb.append("/accounts/");
+		sb.append(_commerceAccount.getCommerceAccountId());
+		sb.append("/images/");
+		sb.append(cpAttachmentFileEntry.getCPAttachmentFileEntryId());
+
+		Assert.assertEquals(sb.toString(), url);
 	}
 
 	@DeleteAfterTestRun
 	private CommerceAccount _commerceAccount;
 
 	@Inject
+	private CommerceCatalogLocalService _commerceCatalogLocalService;
+
+	private CommerceCurrency _commerceCurrency;
+
+	@Inject
 	private CommerceMediaResolver _commerceMediaResolver;
 
 	@DeleteAfterTestRun
-	private CommerceOrder _commerceOrder;
-
-	@DeleteAfterTestRun
 	private Company _company;
+
+	@Inject
+	private CPAttachmentFileEntryLocalService
+		_cpAttachmentFileEntryLocalService;
 
 	private Group _group;
 	private ServiceContext _serviceContext;
