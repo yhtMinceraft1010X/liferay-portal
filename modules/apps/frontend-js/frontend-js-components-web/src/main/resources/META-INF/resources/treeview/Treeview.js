085f75584ce08545c3c36baff93d87835ead47ed
Georgel Pop <georgel.pop@liferay.com>
2021-05-14 09:14:28 +0200

    LPS-130851 Prevent focusing on first node on mouse click, because when using keyboard and state is not initialized with focusedNodeId it is focusing on the first node when using TAB key, so you can start navigating the node list items, this on mouse is not needed, a new event onMouseDown added to NodeList that prevents onFocus to happen when using mouse click to select an item

--- modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/treeview/Treeview.js
+++ modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/treeview/Treeview.js
@@ -240,8 +240,9 @@ function reducer(state, action) {
 
 	switch (action.type) {
 		case 'ACTIVATE': {
-			const focusedNodeId =
-				state.focusedNodeId || (nodes[0] && nodes[0].id);
+			const focusedNodeId = action.mouseNavigation
+				? state.focusedNodeId
+				: state.focusedNodeId || (nodes[0] && nodes[0].id);
 
 			return {
 				...state,
@@ -778,6 +779,23 @@ function Treeview({
 		}
 	};
 
+	const handleMouseDown = (event) => {
+		cancelTimer();
+
+		setHasFocus((hadFocus) => {
+			if (!hadFocus) {
+				dispatch({
+					mouseNavigation: true,
+					type: 'ACTIVATE',
+				});
+			}
+
+			return true;
+		});
+
+		event.preventDefault();
+	};
+
 	const handleFocus = () => {
 		cancelTimer();
 
@@ -816,6 +834,7 @@ function Treeview({
 				nodes={filteredNodes || nodes}
 				onBlur={handleBlur}
 				onFocus={handleFocus}
+				onMouseDown={handleMouseDown}
 				role="tree"
 				tabIndex={0}
 			/>
