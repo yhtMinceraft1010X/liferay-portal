b16042bab5f8b8f2adb3e8f46fdbdf67aeed7187
Jorge González <jorge.gonzalez@liferay.com>
2021-05-19 18:31:28 +0200

    LPS-131605 Fix a malformed condition that avoids rendering field error

--- modules/apps/translation/translation-web/src/main/resources/META-INF/resources/js/translate/components/TranslateFieldSetEntries.js
+++ modules/apps/translation/translation-web/src/main/resources/META-INF/resources/js/translate/components/TranslateFieldSetEntries.js
@@ -69,32 +69,28 @@ const TranslateAutoTranslateRow = ({
 	);
 };
 
-const TranslateFieldFeedback = ({message = '', status = ''}) => {
-	return (
-		status === FETCH_STATUS.ERROR ||
-		(status === FETCH_STATUS.SUCCESS && (
-			<div
-				className={classNames({
-					'has-error': status === FETCH_STATUS.ERROR,
-					'has-success': status === FETCH_STATUS.SUCCESS,
-				})}
-			>
-				<div className="form-feedback-item">
-					<span className="form-feedback-indicator mr-1">
-						<ClayIcon
-							symbol={
-								status === FETCH_STATUS.SUCCESS
-									? 'check-circle-full'
-									: 'exclamation-full'
-							}
-						/>
-					</span>
-					{message}
-				</div>
+const TranslateFieldFeedback = ({message = '', status = ''}) =>
+	(status === FETCH_STATUS.ERROR || status === FETCH_STATUS.SUCCESS) && (
+		<div
+			className={classNames({
+				'has-error': status === FETCH_STATUS.ERROR,
+				'has-success': status === FETCH_STATUS.SUCCESS,
+			})}
+		>
+			<div className="form-feedback-item">
+				<span className="form-feedback-indicator mr-1">
+					<ClayIcon
+						symbol={
+							status === FETCH_STATUS.SUCCESS
+								? 'check-circle-full'
+								: 'exclamation-full'
+						}
+					/>
+				</span>
+				{message}
 			</div>
-		))
+		</div>
 	);
-};
 
 const TranslateFieldEditor = ({
 	editorConfiguration,
