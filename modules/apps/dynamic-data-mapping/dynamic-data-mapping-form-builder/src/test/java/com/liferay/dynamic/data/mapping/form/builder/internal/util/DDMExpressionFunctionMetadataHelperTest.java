8a2fb39286cf64ae78806ee2d62e3ed1547db010
Carolina <carolina.barbosa@liferay.com>
2021-05-20 11:35:56 -0300

    LPS-132175 Test whether ddmExpressionFunctionsMetadata is being correctly populated with default functions

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/test/java/com/liferay/dynamic/data/mapping/form/builder/internal/util/DDMExpressionFunctionMetadataHelperTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/test/java/com/liferay/dynamic/data/mapping/form/builder/internal/util/DDMExpressionFunctionMetadataHelperTest.java
@@ -17,26 +17,45 @@ package com.liferay.dynamic.data.mapping.form.builder.internal.util;
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunctionTracker;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.mockito.Matchers;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.api.support.membermodification.MemberMatcher;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Carolina Barbosa
  */
+@PrepareForTest(ResourceBundleUtil.class)
 @RunWith(PowerMockRunner.class)
 public class DDMExpressionFunctionMetadataHelperTest {
+
+	@Before
+	public void setUp() throws Exception {
+		setUpLanguageUtil();
+		setUpPortal();
+		setUpResourceBundleUtil();
+	}
 
 	@Test
 	public void testPopulateCustomDDMExpressionFunctionsMetadata()
@@ -82,6 +101,41 @@ public class DDMExpressionFunctionMetadataHelperTest {
 			ddmExpressionFunctionMetadatas.get(0));
 	}
 
+	@Test
+	public void testPopulateDDMExpressionFunctionsMetadata() throws Exception {
+		Map<String, List<DDMExpressionFunctionMetadata>>
+			ddmExpressionFunctionMetadatasMap = new HashMap<>();
+
+		_ddmExpressionFunctionMetadataHelper.
+			populateDDMExpressionFunctionsMetadata(
+				ddmExpressionFunctionMetadatasMap, _resourceBundle);
+
+		Assert.assertEquals(
+			ddmExpressionFunctionMetadatasMap.toString(), 3,
+			ddmExpressionFunctionMetadatasMap.size());
+
+		List<DDMExpressionFunctionMetadata> ddmExpressionFunctionMetadatas =
+			ddmExpressionFunctionMetadatasMap.get("number");
+
+		Assert.assertEquals(
+			ddmExpressionFunctionMetadatas.toString(), 8,
+			ddmExpressionFunctionMetadatas.size());
+
+		ddmExpressionFunctionMetadatas = ddmExpressionFunctionMetadatasMap.get(
+			"text");
+
+		Assert.assertEquals(
+			ddmExpressionFunctionMetadatas.toString(), 6,
+			ddmExpressionFunctionMetadatas.size());
+
+		ddmExpressionFunctionMetadatas = ddmExpressionFunctionMetadatasMap.get(
+			"user");
+
+		Assert.assertEquals(
+			ddmExpressionFunctionMetadatas.toString(), 1,
+			ddmExpressionFunctionMetadatas.size());
+	}
+
 	protected void mockDDMExpressionFunctionTracker() throws Exception {
 		DDMExpressionFunctionTracker ddmExpressionFunctionTracker =
 			Mockito.mock(DDMExpressionFunctionTracker.class);
@@ -106,9 +160,46 @@ public class DDMExpressionFunctionMetadataHelperTest {
 		);
 	}
 
+	protected void setUpLanguageUtil() {
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(PowerMockito.mock(Language.class));
+	}
+
+	protected void setUpPortal() throws Exception {
+		Portal portal = PowerMockito.mock(Portal.class);
+
+		MemberMatcher.field(
+			DDMExpressionFunctionMetadataHelper.class, "_portal"
+		).set(
+			_ddmExpressionFunctionMetadataHelper, portal
+		);
+
+		PowerMockito.when(
+			portal.getResourceBundle(Matchers.any(Locale.class))
+		).thenReturn(
+			_resourceBundle
+		);
+	}
+
+	protected void setUpResourceBundleUtil() {
+		PowerMockito.mockStatic(ResourceBundleUtil.class);
+
+		PowerMockito.when(
+			ResourceBundleUtil.getBundle(
+				Matchers.anyString(), Matchers.any(Locale.class),
+				Matchers.any(ClassLoader.class))
+		).thenReturn(
+			ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE
+		);
+	}
+
 	private final DDMExpressionFunctionMetadataHelper
 		_ddmExpressionFunctionMetadataHelper =
 			new DDMExpressionFunctionMetadataHelper();
+
+	@Mock
+	private ResourceBundle _resourceBundle;
 
 	private static class BinaryFunction
 		implements DDMExpressionFunction.Function2<Object, Object, Boolean> {
