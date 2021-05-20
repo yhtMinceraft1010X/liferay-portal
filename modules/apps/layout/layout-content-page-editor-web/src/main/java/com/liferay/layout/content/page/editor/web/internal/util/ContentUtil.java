9ddda30226f7c3006ee944d8e0f9c4040562a6f7
Victor Galan <victor.galan@liferay.com>
2021-05-20 18:26:01 +0200

    LPS-132573 Make sure fragment belong to the experience

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/ContentUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/ContentUtil.java
@@ -55,6 +55,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -403,6 +404,15 @@ public class ContentUtil {
 				if (fragmentEntryLink == null) {
 					LayoutClassedModelUsageLocalServiceUtil.
 						deleteLayoutClassedModelUsage(layoutClassedModelUsage);
+
+					continue;
+				}
+
+				if (!Objects.equals(
+						fragmentEntryLink.getSegmentsExperienceId(),
+						ParamUtil.getLong(
+							httpServletRequest, "segmentExperienceId",
+							SegmentsExperienceConstants.ID_DEFAULT))) {
 
 					continue;
 				}
