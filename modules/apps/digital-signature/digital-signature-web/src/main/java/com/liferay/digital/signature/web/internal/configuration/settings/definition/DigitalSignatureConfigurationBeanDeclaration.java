2999f9e4aba15a24740c2464f6b1facd830826af
Jose Abelenda <jose.abelenda@liferay.com>
2021-05-20 15:03:12 -0300

    LPS-131206 SF

--- modules/apps/digital-signature/digital-signature-web/src/main/java/com/liferay/digital/signature/web/internal/configuration/settings/definition/DigitalSignatureConfigurationBeanDeclaration.java
+++ modules/apps/digital-signature/digital-signature-web/src/main/java/com/liferay/digital/signature/web/internal/configuration/settings/definition/DigitalSignatureConfigurationBeanDeclaration.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.digital.signature.web.internal.configuration.settings.definition;
 
 import com.liferay.digital.signature.configuration.DigitalSignatureConfiguration;
