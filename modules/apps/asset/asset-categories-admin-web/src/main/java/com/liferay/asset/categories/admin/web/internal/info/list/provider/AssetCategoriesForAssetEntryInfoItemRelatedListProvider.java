4d0878f78ef427de380d45887d366088461edffc
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-05-20 10:58:44 +0200

    LPS-132081 Better naming

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/list/provider/AssetCategoriesForAssetEntryInfoItemRelatedListProvider.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/list/provider/AssetCategoriesForAssetEntryInfoItemRelatedListProvider.java
@@ -43,7 +43,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Jürgen Kappler
  */
 @Component(immediate = true, service = InfoItemRelatedListProvider.class)
-public class RelatedAssetCategoriesInfoItemRelatedListProvider
+public class AssetCategoriesForAssetEntryInfoItemRelatedListProvider
 	implements InfoItemRelatedListProvider<AssetEntry, AssetCategory> {
 
 	@Override
