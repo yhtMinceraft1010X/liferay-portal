7e86cc2a13e5b0c90e1e9edbbaaec70bad50c13e
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2021-05-21 13:15:15 +0100

    LPS-123218 OrderedServiceTrackerMap is no longer exported, so need to move this registry impl

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/field/expression/handler/registry/SamlSpIdpConnectionFieldExpressionHandlerRegistryImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/field/expression/handler/registry/SamlSpIdpConnectionFieldExpressionHandlerRegistryImpl.java
@@ -12,12 +12,12 @@
  *
  */
 
-package com.liferay.saml.web.internal.opensaml.integration.field.expression.handler;
+package com.liferay.saml.opensaml.integration.internal.field.expression.handler.registry;
 
-import com.liferay.saml.opensaml.integration.internal.OrderedServiceTrackerMap;
-import com.liferay.saml.opensaml.integration.internal.OrderedServiceTrackerMapFactory;
 import com.liferay.saml.opensaml.integration.field.expression.handler.SamlSpIdpConnectionFieldExpressionHandler;
 import com.liferay.saml.opensaml.integration.field.expression.handler.registry.SamlSpIdpConnectionFieldExpressionHandlerRegistry;
+import com.liferay.saml.opensaml.integration.internal.service.tracker.collections.OrderedServiceTrackerMap;
+import com.liferay.saml.opensaml.integration.internal.service.tracker.collections.OrderedServiceTrackerMapFactory;
 
 import java.util.List;
 import java.util.Map;
