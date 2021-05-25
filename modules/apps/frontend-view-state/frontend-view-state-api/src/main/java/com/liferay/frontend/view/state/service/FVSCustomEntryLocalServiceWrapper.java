d959a3255d24bdca2d03d959005af99e892947c8
Brian Chan <brian.chan@liferay.com>
2021-05-24 19:05:16 -0700

    LPS-132448 regen

--- modules/apps/frontend-view-state/frontend-view-state-api/src/main/java/com/liferay/frontend/view/state/service/FVSCustomEntryLocalServiceWrapper.java
+++ modules/apps/frontend-view-state/frontend-view-state-api/src/main/java/com/liferay/frontend/view/state/service/FVSCustomEntryLocalServiceWrapper.java
@@ -133,6 +133,13 @@ public class FVSCustomEntryLocalServiceWrapper
 	}
 
 	@Override
+	public int dslQueryCount(
+		com.liferay.petra.sql.dsl.query.DSLQuery dslQuery) {
+
+		return _fvsCustomEntryLocalService.dslQueryCount(dslQuery);
+	}
+
+	@Override
 	public com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery() {
 		return _fvsCustomEntryLocalService.dynamicQuery();
 	}
