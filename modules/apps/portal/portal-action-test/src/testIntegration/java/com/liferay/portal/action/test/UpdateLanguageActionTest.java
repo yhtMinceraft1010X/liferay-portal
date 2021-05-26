8416dfe1ab464f4e064b57642c423546c6ebe9ba
Attila Bakay <attila.bakay@liferay.com>
2021-05-26 13:22:44 +0200

    LPS-132765 Add integration test

--- modules/apps/portal/portal-action-test/src/testIntegration/java/com/liferay/portal/action/test/UpdateLanguageActionTest.java
+++ modules/apps/portal/portal-action-test/src/testIntegration/java/com/liferay/portal/action/test/UpdateLanguageActionTest.java
@@ -19,31 +19,41 @@ import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.action.UpdateLanguageAction;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.VirtualLayoutConstants;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PropsValues;
 
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -61,6 +71,39 @@ public class UpdateLanguageActionTest {
 	@Rule
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_availableLocales = _language.getAvailableLocales();
+		_defaultLocale = LocaleUtil.getDefault();
+		_localesEnabled = PropsValues.LOCALES_ENABLED;
+
+		_language.init();
+
+		CompanyTestUtil.resetCompanyLocales(
+			_portal.getDefaultCompanyId(),
+			Arrays.asList(
+				LocaleUtil.GERMANY, LocaleUtil.FRANCE, LocaleUtil.UK,
+				LocaleUtil.US),
+			LocaleUtil.US);
+
+		PropsValues.LOCALES_ENABLED = new String[] {
+			_language.getLanguageId(LocaleUtil.GERMANY),
+			_language.getLanguageId(LocaleUtil.FRANCE),
+			_language.getLanguageId(LocaleUtil.UK),
+			_language.getLanguageId(LocaleUtil.US)
+		};
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		_language.init();
+
+		CompanyTestUtil.resetCompanyLocales(
+			_portal.getDefaultCompanyId(), _availableLocales, _defaultLocale);
+
+		PropsValues.LOCALES_ENABLED = _localesEnabled;
+	}
 
 	@Before
 	public void setUp() throws Exception {
@@ -91,6 +134,8 @@ public class UpdateLanguageActionTest {
 			).put(
 				_sourceLocale, "assetsource"
 			).put(
+				_sourceUKLocale, "assetuksource"
+			).put(
 				_targetLocale, "assettarget"
 			).build(),
 			null,
@@ -112,6 +157,52 @@ public class UpdateLanguageActionTest {
 
 		_testGetRedirectWithFriendlyURL(false);
 		_testGetRedirectWithFriendlyURL(true);
+	}
+
+	@Test
+	public void testGetRedirectWithFriendlyURLWithVirtualHost()
+		throws Exception {
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		LayoutSet layoutSet = _layout.getLayoutSet();
+
+		layoutSet.setVirtualHostname(_VIRTUALHOST);
+
+		themeDisplay.setI18nLanguageId(_sourceUKLocale.getLanguage());
+		themeDisplay.setI18nPath("/" + _sourceUKLocale.getLanguage());
+		themeDisplay.setLocale(_sourceUKLocale);
+		themeDisplay.setLayout(_layout);
+		themeDisplay.setLayoutSet(_group.getPublicLayoutSet());
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setPortalDomain(_VIRTUALHOST);
+		themeDisplay.setPortalURL(Http.HTTP_WITH_SLASH + _VIRTUALHOST);
+
+		String targetURL = StringBundler.concat(
+			Http.HTTP_WITH_SLASH, _VIRTUALHOST,
+			_getFriendlyURLSeparatorPart(_targetLocale), "?queryString");
+
+		String sourceURL = StringBundler.concat(
+			StringPool.SLASH, _sourceUKLocale.toLanguageTag(),
+			_getFriendlyURLSeparatorPart(_sourceUKLocale), "?queryString");
+
+		UpdateLanguageAction updateLanguageAction = new UpdateLanguageAction();
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setServerName(_VIRTUALHOST);
+
+		HttpSession httpSession = mockHttpServletRequest.getSession();
+
+		httpSession.setAttribute(WebKeys.LOCALE, _targetLocale);
+
+		mockHttpServletRequest.setParameter("redirect", sourceURL);
+
+		String redirect = updateLanguageAction.getRedirect(
+			mockHttpServletRequest, themeDisplay, _targetLocale);
+
+		Assert.assertEquals(targetURL, redirect);
 	}
 
 	private void _assertRedirect(
@@ -159,6 +250,7 @@ public class UpdateLanguageActionTest {
 		if (i18n) {
 			themeDisplay.setI18nLanguageId(_sourceLocale.getLanguage());
 			themeDisplay.setI18nPath("/" + _sourceLocale.getLanguage());
+			themeDisplay.setLocale(_sourceLocale);
 		}
 
 		Layout controlPanelLayout = LayoutLocalServiceUtil.getLayout(
@@ -215,6 +307,7 @@ public class UpdateLanguageActionTest {
 		if (i18n) {
 			themeDisplay.setI18nLanguageId(_sourceLocale.getLanguage());
 			themeDisplay.setI18nPath("/" + _sourceLocale.getLanguage());
+			themeDisplay.setLocale(_sourceLocale);
 		}
 
 		themeDisplay.setLayout(_layout);
@@ -264,6 +357,7 @@ public class UpdateLanguageActionTest {
 		if (i18n) {
 			themeDisplay.setI18nLanguageId(_sourceLocale.getLanguage());
 			themeDisplay.setI18nPath("/" + _sourceLocale.getLanguage());
+			themeDisplay.setLocale(_sourceLocale);
 		}
 
 		themeDisplay.setLayout(_layout);
@@ -293,11 +387,24 @@ public class UpdateLanguageActionTest {
 	private static final String _PORTLET_FRIENDLY_URL_PART_ASSET_PUBLISHER =
 		"/-/asset_publisher/instanceID/content/";
 
-	private final Locale _defaultLocale = LocaleUtil.US;
+	private static final String _VIRTUALHOST = "test.com";
+
+	private static Set<Locale> _availableLocales;
+	private static Locale _defaultLocale;
+
+	@Inject
+	private static Language _language;
+
+	private static String[] _localesEnabled;
+
+	@Inject
+	private static Portal _portal;
+
 	private Group _group;
 	private JournalArticle _journalArticle;
 	private Layout _layout;
 	private final Locale _sourceLocale = LocaleUtil.FRANCE;
+	private final Locale _sourceUKLocale = LocaleUtil.UK;
 	private final Locale _targetLocale = LocaleUtil.GERMANY;
 
 }
