226c3227101285be39c1743f46b1f411d0eff0a1
Hugo Huijser <hugo.huijser@liferay.com>
2021-05-26 11:06:27 -0600

    LPS-105380 Use getInt

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTCollectionServiceTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTCollectionServiceTest.java
@@ -178,7 +178,7 @@ public class CTCollectionServiceTest {
 
 			Assert.assertTrue(resultSet.next());
 
-			Assert.assertEquals(0, resultSet.getLong(1));
+			Assert.assertEquals(0, resultSet.getInt(1));
 		}
 	}
 
