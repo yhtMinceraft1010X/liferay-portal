4328ff7cff2668aeeac316f3d19337dc10b4d4a3
Matija Petanjek <matija.petanjek@liferay.com>
2021-05-28 17:56:13 +0200

    COMMERCE-6529 batch-engine-web - Import/Export Center, add new category to Control Panel

---
+++ modules/apps/batch-engine/batch-engine-web/src/main/java/com/liferay/batch/engine/web/internal/application/list/BatchEnginePanelApp.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.batch.engine.web.internal.application.list;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.batch.engine.constants.BatchEnginePortletKeys;
+import com.liferay.batch.engine.web.internal.application.list.constants.ImportExportCenterPanelCategoryKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Matija Petanjek
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.app.order:Integer=1000",
+		"panel.category.key=" + ImportExportCenterPanelCategoryKeys.APPLICATIONS_MENU_APPLICATIONS_IMPORT_EXPORT_CENTER
+	},
+	service = PanelApp.class
+)
+public class BatchEnginePanelApp extends BasePanelApp {
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "batch-jobs");
+	}
+
+	@Override
+	public String getPortletId() {
+		return BatchEnginePortletKeys.BATCH;
+	}
+
+	@Override
+	@Reference(
+		target = "(javax.portlet.name=" + BatchEnginePortletKeys.BATCH + ")",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
+
+}
