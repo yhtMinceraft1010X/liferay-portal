55043113639d2ed4ab3974473fa88c0b552f203f
Marcela Cunha <marcela.cunha@liferay.com>
2021-05-28 09:21:15 -0300

    LPS-132788 Add test

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/search/location/SearchLocationDDMFormFieldTypeSettingsTest.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.search.location;
+
+import com.liferay.dynamic.data.mapping.form.field.type.BaseDDMFormFieldTypeSettingsTestCase;
+import com.liferay.dynamic.data.mapping.form.field.type.internal.searchLocation.SearchLocationDDMFormFieldTypeSettings;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.DDMFormRule;
+import com.liferay.dynamic.data.mapping.util.DDMFormFactory;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Matchers;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Marcela Cunha
+ */
+@PrepareForTest({PortalClassLoaderUtil.class, ResourceBundleUtil.class})
+@RunWith(PowerMockRunner.class)
+public class SearchLocationDDMFormFieldTypeSettingsTest
+	extends BaseDDMFormFieldTypeSettingsTestCase {
+
+	@Before
+	@Override
+	public void setUp() {
+		setUpLanguageUtil();
+		setUpPortalUtil();
+		setUpResourceBundleUtil();
+	}
+
+	@Test
+	public void testCreateSearchLocationDDMFormFieldTypeSettingsDDMForm() {
+		DDMForm ddmForm = DDMFormFactory.create(
+			SearchLocationDDMFormFieldTypeSettings.class);
+
+		Map<String, DDMFormField> ddmFormFieldsMap =
+			ddmForm.getDDMFormFieldsMap(false);
+
+		DDMFormField layoutDDMFormField = ddmFormFieldsMap.get("layout");
+
+		Assert.assertNotNull(layoutDDMFormField);
+		Assert.assertNotNull(layoutDDMFormField.getLabel());
+		Assert.assertEquals("select", layoutDDMFormField.getType());
+		Assert.assertNotNull(layoutDDMFormField.getPredefinedValue());
+
+		DDMFormField placeholderDDMFormField = ddmFormFieldsMap.get(
+			"placeholder");
+
+		Assert.assertNotNull(placeholderDDMFormField);
+		Assert.assertEquals("string", placeholderDDMFormField.getDataType());
+		Assert.assertEquals("text", placeholderDDMFormField.getType());
+
+		DDMFormField visibleFieldsDDMFormField = ddmFormFieldsMap.get(
+			"visibleFields");
+
+		Assert.assertNotNull(visibleFieldsDDMFormField);
+		Assert.assertNotNull(visibleFieldsDDMFormField.getLabel());
+		Assert.assertEquals("select", visibleFieldsDDMFormField.getType());
+		Assert.assertNotNull(visibleFieldsDDMFormField.getPredefinedValue());
+
+		List<DDMFormRule> ddmFormRules = ddmForm.getDDMFormRules();
+
+		Assert.assertEquals(ddmFormRules.toString(), 1, ddmFormRules.size());
+
+		DDMFormRule ddmFormRule0 = ddmFormRules.get(0);
+
+		Assert.assertEquals("TRUE", ddmFormRule0.getCondition());
+
+		List<String> actions = ddmFormRule0.getActions();
+
+		Assert.assertEquals(actions.toString(), 3, actions.size());
+		Assert.assertEquals("setVisible('dataType', false)", actions.get(0));
+
+		StringBundler sb = new StringBundler(4);
+
+		sb.append("setVisible('layout', contains(getValue('visibleFields'), ");
+		sb.append("\"city\") OR contains(getValue('visibleFields'), \"state\"");
+		sb.append(") OR contains(getValue('visibleFields'), \"postal-code\"");
+		sb.append(") OR contains(getValue('visibleFields'), \"country\"))");
+
+		Assert.assertEquals(sb.toString(), actions.get(1));
+
+		Assert.assertEquals(
+			"setVisible('requiredErrorMessage', false)", actions.get(2));
+	}
+
+	@Override
+	protected void setUpLanguageUtil() {
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(PowerMockito.mock(Language.class));
+	}
+
+	protected void setUpPortalUtil() {
+		PortalUtil portalUtil = new PortalUtil();
+
+		Portal portal = mock(Portal.class);
+
+		ResourceBundle resourceBundle = mock(ResourceBundle.class);
+
+		when(
+			portal.getResourceBundle(Matchers.any(Locale.class))
+		).thenReturn(
+			resourceBundle
+		);
+
+		portalUtil.setPortal(portal);
+	}
+
+	@Override
+	protected void setUpResourceBundleUtil() {
+		PowerMockito.mockStatic(ResourceBundleUtil.class);
+
+		PowerMockito.when(
+			ResourceBundleUtil.getBundle(
+				Matchers.anyString(), Matchers.any(Locale.class),
+				Matchers.any(ClassLoader.class))
+		).thenReturn(
+			ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE
+		);
+	}
+
+}
