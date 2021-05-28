aaab65c4332d32f467e51042067debf38e1cdabd
Yang Cao <yang.cao@liferay.com>
2021-05-28 11:58:22 +0800

    LPS-132601 Override method to publish layout by plid

---
+++ modules/apps/layout/layout-service/src/main/java/com/liferay/layout/service/impl/ContentPageLayoutServiceWrapper.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.service.impl;
+
+import com.liferay.layout.util.LayoutCopyHelper;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.security.auth.GuestOrUserUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.LayoutService;
+import com.liferay.portal.kernel.service.LayoutServiceWrapper;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.service.permission.LayoutPermissionUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Yang Cao
+ */
+@Component(immediate = true, service = ServiceWrapper.class)
+public class ContentPageLayoutServiceWrapper extends LayoutServiceWrapper {
+
+	public ContentPageLayoutServiceWrapper() {
+		super(null);
+	}
+
+	public ContentPageLayoutServiceWrapper(LayoutService layoutService) {
+		super(layoutService);
+	}
+
+	@Override
+	public Layout publishLayout(long plid) throws Exception {
+		Layout layout = _layoutLocalService.getLayout(plid);
+
+		if (Objects.equals(layout.getType(), LayoutConstants.TYPE_CONTENT)) {
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			Layout draftLayout = _layoutLocalService.fetchLayout(
+				_portal.getClassNameId(Layout.class), layout.getPlid());
+
+			LayoutPermissionUtil.check(
+				GuestOrUserUtil.getPermissionChecker(), layout,
+				ActionKeys.UPDATE);
+
+			LayoutPermissionUtil.check(
+				GuestOrUserUtil.getPermissionChecker(), draftLayout,
+				ActionKeys.UPDATE);
+
+			layout = _layoutCopyHelper.copyLayout(draftLayout, layout);
+
+			_layoutLocalService.updateStatus(
+				layout.getUserId(), layout.getPlid(),
+				WorkflowConstants.STATUS_APPROVED, serviceContext);
+			_layoutLocalService.updateStatus(
+				layout.getUserId(), draftLayout.getPlid(),
+				WorkflowConstants.STATUS_APPROVED, serviceContext);
+		}
+		else {
+			return null;
+		}
+
+		return layout;
+	}
+
+	@Reference
+	private LayoutCopyHelper _layoutCopyHelper;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
