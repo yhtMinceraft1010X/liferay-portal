cb57ecee0cdb48cdb5d722127d9b0db7542dddd1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-06-01 15:42:05 +0100

    LPS-134203 Define the asset entry list info filter

---
+++ modules/apps/asset/asset-list-api/src/main/java/com/liferay/asset/list/info/filter/AssetEntryListInfoFilter.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.info.filter;
+
+import com.liferay.info.filter.InfoFilter;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class AssetEntryListInfoFilter implements InfoFilter {
+
+	public static final String KEY =
+		"com.liferay.asset.list.info.filter.AssetEntryListInfoFilter";
+
+	public AssetEntryListInfoFilter() {
+	}
+
+	public long[][] getAssetCategoryIds() {
+		return _assetCategoryIds;
+	}
+
+	public void setAssetCategoryIds(long[][] assetCategoryIds) {
+		_assetCategoryIds = assetCategoryIds;
+	}
+
+	private long[][] _assetCategoryIds;
+
+}
