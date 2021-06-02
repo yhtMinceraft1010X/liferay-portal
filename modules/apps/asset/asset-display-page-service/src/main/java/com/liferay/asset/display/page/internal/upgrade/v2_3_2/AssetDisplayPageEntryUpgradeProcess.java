9ccb22e99dad5b6526b83dbb33d26c95e8c97f8e
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-06-02 13:32:23 +0200

    LPS-132588 There was a change were we now use FileEntry instead of DLFileEntry for Display Pages, but some users might have upgraded before the change, so remove the duplicates, otherwise the upgrade process will fail

--- modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/internal/upgrade/v2_3_2/AssetDisplayPageEntryUpgradeProcess.java
+++ modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/internal/upgrade/v2_3_2/AssetDisplayPageEntryUpgradeProcess.java
@@ -15,11 +15,14 @@
 package com.liferay.asset.display.page.internal.upgrade.v2_3_2;
 
 import com.liferay.document.library.kernel.model.DLFileEntryConstants;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.PortalUtil;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * @author Jürgen Kappler
@@ -28,7 +31,49 @@ public class AssetDisplayPageEntryUpgradeProcess extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
+		_deleteDuplicateDLAssetDisplayPages();
 		_upgradeDLAssetDisplayPageTypes();
+	}
+
+	private void _deleteDuplicateDLAssetDisplayPages() throws Exception {
+		long dlFileEntryClassNameId = PortalUtil.getClassNameId(
+			DLFileEntryConstants.getClassName());
+		long fileEntryClassNameId = PortalUtil.getClassNameId(
+			FileEntry.class.getName());
+
+		StringBundler sb1 = new StringBundler(11);
+
+		sb1.append("select assetDisplayPageEntry1.assetDisplayPageEntryId ");
+		sb1.append("from AssetDisplayPageEntry assetDisplayPageEntry1 inner ");
+		sb1.append("join AssetDisplayPageEntry assetDisplayPageEntry2 on ");
+		sb1.append("assetDisplayPageEntry1.groupId = ");
+		sb1.append("assetDisplayPageEntry2.groupId and ");
+		sb1.append("assetDisplayPageEntry2.classNameId = ");
+		sb1.append(fileEntryClassNameId);
+		sb1.append(" and assetDisplayPageEntry1.classPK = ");
+		sb1.append("assetDisplayPageEntry2.classPK where ");
+		sb1.append("assetDisplayPageEntry1.classNameId = ");
+		sb1.append(dlFileEntryClassNameId);
+
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
+				sb1.toString());
+			PreparedStatement preparedStatement2 =
+				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
+					connection,
+					"delete from AssetDisplayPageEntry where " +
+						"assetDisplayPageEntryId = ?")) {
+
+			try (ResultSet resultSet = preparedStatement1.executeQuery()) {
+				while (resultSet.next()) {
+					preparedStatement2.setLong(
+						1, resultSet.getLong("assetDisplayPageEntryId"));
+
+					preparedStatement2.addBatch();
+				}
+
+				preparedStatement2.executeBatch();
+			}
+		}
 	}
 
 	private void _upgradeDLAssetDisplayPageTypes() throws Exception {
