677f83f9830a0a4defe2227232f7e087b18c004a
Lawrence Lee <lawrence.lee@liferay.com>
2021-06-02 15:41:31 -0500

    LPS-133529 Missing war-mvc-portlet template

---
+++ modules/sdk/project-templates/project-templates-war-mvc-portlet/src/main/java/com/liferay/project/templates/war/mvc/portlet/internal/WarMVCPortletProjectTemplateCustomizer.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.project.templates.war.mvc.portlet.internal;
+
+import aQute.bnd.version.Version;
+
+import com.liferay.project.templates.extensions.ProjectTemplateCustomizer;
+import com.liferay.project.templates.extensions.ProjectTemplatesArgs;
+import com.liferay.project.templates.extensions.util.FileUtil;
+
+import java.io.File;
+
+import java.nio.file.Path;
+
+import org.apache.maven.archetype.ArchetypeGenerationRequest;
+import org.apache.maven.archetype.ArchetypeGenerationResult;
+
+/**
+ * @author Lawrence Lee
+ */
+public class WarMVCPortletProjectTemplateCustomizer
+	implements ProjectTemplateCustomizer {
+
+	@Override
+	public void onAfterGenerateProject(
+			ProjectTemplatesArgs projectTemplatesArgs, File destinationDir,
+			ArchetypeGenerationResult archetypeGenerationResult)
+		throws Exception {
+
+		Path destinationDirPath = destinationDir.toPath();
+
+		Path projectPath = destinationDirPath.resolve(
+			projectTemplatesArgs.getName());
+
+		File buildDir = projectPath.toFile();
+
+		File webINFDir = new File(buildDir, "src/main/webapp/WEB-INF");
+
+		Version version = Version.parseVersion(
+			projectTemplatesArgs.getLiferayVersion());
+
+		int minorVersion = version.getMinor();
+
+		String minorVersionString = String.valueOf(minorVersion);
+
+		File liferayPortletXML = new File(webINFDir, "liferay-display.xml");
+
+		File liferayDisplayXML = new File(webINFDir, "liferay-portlet.xml");
+
+		FileUtil.replaceString(
+			liferayDisplayXML, "7.0", "7." + minorVersionString);
+		FileUtil.replaceString(
+			liferayDisplayXML, "7_0", "7_" + minorVersionString);
+
+		FileUtil.replaceString(
+			liferayPortletXML, "7.0", "7." + minorVersionString);
+		FileUtil.replaceString(
+			liferayPortletXML, "7_0", "7_" + minorVersionString);
+	}
+
+	@Override
+	public void onBeforeGenerateProject(
+			ProjectTemplatesArgs projectTemplatesArgs,
+			ArchetypeGenerationRequest archetypeGenerationRequest)
+		throws Exception {
+	}
+
+}
--- modules/sdk/project-templates/project-templates-war-mvc-portlet/src/main/resources/META-INF/archetype-post-generate.groovy
+++ modules/sdk/project-templates/project-templates-war-mvc-portlet/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -21,3 +21,24 @@ Path projectPath = Paths.get(request.outputDirectory, request.artifactId)
 Path buildGradlePath = projectPath.resolve("build.gradle")
 
 Files.deleteIfExists buildGradlePath
+
+def buildDir = projectPath.toFile()
+def webINFDir = new File(buildDir, "src/main/webapp/WEB-INF")
+
+String liferayVersion = request.properties.get("liferayVersion")
+
+char minorVersion = liferayVersion.charAt(2)
+
+File liferayDisplayXML = new File(
+	webINFDir, "liferay-display.xml");
+
+File liferayPortletXML = new File(
+	webINFDir, "liferay-portlet.xml");
+
+def newLiferayDisplayContent = liferayDisplayXML.text.replace("7.0", "7." + minorVersion).replace("7_0", "7_" + minorVersion)
+
+liferayDisplayXML.text = newLiferayDisplayContent
+
+def newLiferayPortletContent = liferayPortletXML.text.replace("7.0", "7." + minorVersion).replace("7_0", "7_" + minorVersion)
+
+liferayPortletXML.text = newLiferayPortletContent
