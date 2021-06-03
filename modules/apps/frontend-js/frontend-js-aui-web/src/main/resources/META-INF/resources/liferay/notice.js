3d78c8dd298d2eb9e47559d9a2a60dbfbdad0e60
Marcos <marcos.castro@liferay.com>
2021-06-03 13:55:29 +0200

    LPS-132927 - Update styles and remove popup-alert-close class

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/notice.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/notice.js
@@ -138,7 +138,7 @@ AUI.add(
 
 				if (instance._useCloseButton) {
 					var html =
-						'<button class="btn btn-secondary submit popup-alert-close">' +
+						'<button class="btn btn-secondary c-ml-1 submit">' +
 						instance._closeText +
 						'</button>';
 
