339cdbdf11d59d3b0f67b51dea74f9698e422889
Patrick Yeo <patrick.yeo@liferay.com>
2021-06-08 15:29:02 -0700

    LPS-132024 Adds frontend-css-cadmin-web. This module introduces our admin css framework for isolating component styles.

---
+++ modules/apps/frontend-css/frontend-css-cadmin-web/src/main/java/com/liferay/frontend/css/cadmin/web/internal/servlet/taglib/CadminTopHeadDynamicInclude.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.css.cadmin.web.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.url.builder.AbsolutePortalURLBuilder;
+import com.liferay.portal.url.builder.AbsolutePortalURLBuilderFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Patrick Yeo
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class CadminTopHeadDynamicInclude extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		if (!isSignedIn()) {
+			return;
+		}
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		AbsolutePortalURLBuilder absolutePortalURLBuilder =
+			_absolutePortalURLBuilderFactory.getAbsolutePortalURLBuilder(
+				httpServletRequest);
+
+		printWriter.print("<link data-senna-track=\"temporary\" href=\"");
+
+		printWriter.print(
+			absolutePortalURLBuilder.forModule(
+				_bundleContext.getBundle(), _FILE_NAME
+			).build());
+
+		printWriter.println(
+			"\" id=\"liferayCadminCSS\" rel=\"stylesheet\"");
+		printWriter.println(" type=\"text/css\" />");
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register("/html/common/themes/top_head.jsp#pre");
+	}
+
+	@Activate
+	@Modified
+	protected void activate(
+			BundleContext bundleContext, ComponentContext componentContext,
+			Map<String, Object> properties)
+		throws Exception {
+
+		_bundleContext = bundleContext;
+	}
+
+	protected boolean isSignedIn() {
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if ((permissionChecker == null) || !permissionChecker.isSignedIn()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private static final String _FILE_NAME = "main.css";
+
+	@Reference
+	private AbsolutePortalURLBuilderFactory _absolutePortalURLBuilderFactory;
+
+	private BundleContext _bundleContext;
+
+}
