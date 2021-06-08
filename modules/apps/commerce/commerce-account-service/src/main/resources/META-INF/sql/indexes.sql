8e62a2dce5934c2664c7c39bfd8e9eec10d62ac0
pei-jung <pei-jung.lan@liferay.com>
2021-06-08 13:29:12 -0700

    LPS-131760 Remove SQL statements

--- modules/apps/commerce/commerce-account-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/commerce/commerce-account-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,10 +1,3 @@
 create unique index IX_1F92C081 on CAccountGroupCAccountRel (commerceAccountGroupId, commerceAccountId);
 create index IX_9808F83D on CAccountGroupCAccountRel (commerceAccountId);
 create index IX_41A0DD5 on CAccountGroupCAccountRel (companyId, externalReferenceCode[$COLUMN_LENGTH:75$]);
-
-create index IX_721C700D on CommerceAccount (companyId, externalReferenceCode[$COLUMN_LENGTH:75$]);
-create index IX_FE5BDB63 on CommerceAccount (userId, type_);
-
-create index IX_16599BBC on CommerceAccountOrganizationRel (organizationId);
-
-create index IX_EC8DEFA6 on CommerceAccountUserRel (commerceAccountUserId);
