9b8d4eae0c0b681daef1046935aaf6634cb36316
Alberto Chaparro <alberto.chaparro@liferay.com>
2021-06-08 19:29:27 +0200

    LPS-133825 When DB Partition is enabled we must return the current schema when executing getCatalog

--- portal-impl/src/com/liferay/portal/db/partition/DBPartitionUtil.java
+++ portal-impl/src/com/liferay/portal/db/partition/DBPartitionUtil.java
@@ -293,6 +293,11 @@ public class DBPartitionUtil {
 			}
 
 			@Override
+			public String getCatalog() throws SQLException {
+				return _getSchemaName(CompanyThreadLocal.getCompanyId());
+			}
+
+			@Override
 			public PreparedStatement prepareStatement(String sql)
 				throws SQLException {
 
