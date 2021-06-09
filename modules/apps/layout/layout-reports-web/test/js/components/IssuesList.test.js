e49314b8a996f22b4c02eb2cee0c250dea0bffd0
Sandro Chinea <sandrochinea@gmail.com>
2021-06-09 14:11:01 +0100

    LPS-133070 Improve IssuesList tests

--- modules/apps/layout/layout-reports-web/test/js/components/IssuesList.test.js
+++ modules/apps/layout/layout-reports-web/test/js/components/IssuesList.test.js
@@ -125,24 +125,31 @@ const mockPageURLs = [
 
 const defaultLanguageId = 'en-US';
 
+const renderIssuesList = ({layoutReportsIssues, loading = false}) => {
+	return render(
+		<StoreContextProvider
+			value={{
+				data: {
+					defaultLanguageId,
+					imagesPath: 'imagesPath',
+					layoutReportsIssues,
+					pageURLs: mockPageURLs,
+				},
+				loading,
+			}}
+		>
+			<IssuesList />
+		</StoreContextProvider>
+	);
+};
+
 describe('IssuesList', () => {
 	afterEach(cleanup);
 
 	it('renders accessibility and seo sections with issues count', () => {
-		const {getByText} = render(
-			<StoreContextProvider
-				value={{
-					data: {
-						defaultLanguageId,
-						imagesPath: 'imagesPath',
-						layoutReportsIssues: mockLayoutReportsIssues,
-						pageURLs: mockPageURLs,
-					},
-				}}
-			>
-				<IssuesList />
-			</StoreContextProvider>
-		);
+		const {getByText} = renderIssuesList({
+			layoutReportsIssues: mockLayoutReportsIssues,
+		});
 
 		expect(getByText('Accessibility')).toBeInTheDocument();
 		expect(getByText('17')).toBeInTheDocument();
@@ -151,20 +158,9 @@ describe('IssuesList', () => {
 	});
 
 	it('renders no seo or accesibility issues message', () => {
-		const {getByText} = render(
-			<StoreContextProvider
-				value={{
-					data: {
-						defaultLanguageId,
-						imagesPath: 'imagesPath',
-						layoutReportsIssues: mockLayoutReportsIssuesNoSEO,
-						pageURLs: mockPageURLs,
-					},
-				}}
-			>
-				<IssuesList />
-			</StoreContextProvider>
-		);
+		const {getByText} = renderIssuesList({
+			layoutReportsIssues: mockLayoutReportsIssuesNoSEO,
+		});
 
 		expect(getByText('SEO')).toBeInTheDocument();
 		expect(getByText('0')).toBeInTheDocument();
@@ -174,20 +170,9 @@ describe('IssuesList', () => {
 	});
 
 	it('renders no issues message (no seo and accessibility issues)', () => {
-		const {getAllByText, getByText} = render(
-			<StoreContextProvider
-				value={{
-					data: {
-						defaultLanguageId,
-						imagesPath: 'imagesPath',
-						layoutReportsIssues: mockLayoutReportsIssuesNoAccessibilityNoSEO,
-						pageURLs: mockPageURLs,
-					},
-				}}
-			>
-				<IssuesList />
-			</StoreContextProvider>
-		);
+		const {getAllByText, getByText} = renderIssuesList({
+			layoutReportsIssues: mockLayoutReportsIssuesNoAccessibilityNoSEO,
+		});
 
 		expect(getByText('Accessibility')).toBeInTheDocument();
 		expect(getByText('SEO')).toBeInTheDocument();
@@ -196,20 +181,9 @@ describe('IssuesList', () => {
 	});
 
 	it('render list of accesibility issues with correct number of failing elements', () => {
-		const {getAllByText, getByText} = render(
-			<StoreContextProvider
-				value={{
-					data: {
-						defaultLanguageId,
-						imagesPath: 'imagesPath',
-						layoutReportsIssues: mockLayoutReportsIssues,
-						pageURLs: mockPageURLs,
-					},
-				}}
-			>
-				<IssuesList />
-			</StoreContextProvider>
-		);
+		const {getAllByText, getByText} = renderIssuesList({
+			layoutReportsIssues: mockLayoutReportsIssues,
+		});
 
 		expect(getByText('Missing Input ALT Attributes')).toBeInTheDocument();
 		expect(getByText('Missing Video Caption')).toBeInTheDocument();
@@ -218,39 +192,17 @@ describe('IssuesList', () => {
 	});
 
 	it('show +100 if a section has more than 100 issues', () => {
-		const {getByText} = render(
-			<StoreContextProvider
-				value={{
-					data: {
-						defaultLanguageId,
-						imagesPath: 'imagesPath',
-						layoutReportsIssues: mockLayoutReportsIssuesOver100SEO,
-						pageURLs: mockPageURLs,
-					},
-				}}
-			>
-				<IssuesList />
-			</StoreContextProvider>
-		);
+		const {getByText} = renderIssuesList({
+			layoutReportsIssues: mockLayoutReportsIssuesOver100SEO,
+		});
 
 		expect(getByText('+100')).toBeInTheDocument();
 	});
 
 	it('renders SEO section open and show checks with failing elements', () => {
-		const {getAllByText, getByText, queryByText} = render(
-			<StoreContextProvider
-				value={{
-					data: {
-						defaultLanguageId,
-						imagesPath: 'imagesPath',
-						layoutReportsIssues: mockLayoutReportsIssuesSEODetails,
-						pageURLs: mockPageURLs,
-					},
-				}}
-			>
-				<IssuesList />
-			</StoreContextProvider>
-		);
+		const {getAllByText, getByText, queryByText} = renderIssuesList({
+			layoutReportsIssues: mockLayoutReportsIssuesSEODetails,
+		});
 
 		expect(getByText('Missing Meta Description')).toBeInTheDocument();
 		expect(getAllByText('1').length).toBe(2);
@@ -259,21 +211,10 @@ describe('IssuesList', () => {
 	});
 
 	it('renders loading progress bar, title, URL and language when issues are loading', () => {
-		const {getByText} = render(
-			<StoreContextProvider
-				value={{
-					data: {
-						defaultLanguageId,
-						imagesPath: 'imagesPath',
-						layoutReportsIssues: mockLayoutReportsIssuesSEODetails,
-						pageURLs: mockPageURLs,
-					},
-					loading: true,
-				}}
-			>
-				<IssuesList />
-			</StoreContextProvider>
-		);
+		const {getByText} = renderIssuesList({
+			layoutReportsIssues: mockLayoutReportsIssuesSEODetails,
+			loading: true,
+		});
 
 		expect(
 			getByText('connecting-with-google-pagespeed')
@@ -285,21 +226,10 @@ describe('IssuesList', () => {
 	});
 
 	it('renders language selector button disabled when issues are loading', () => {
-		const {getByText} = render(
-			<StoreContextProvider
-				value={{
-					data: {
-						defaultLanguageId,
-						imagesPath: 'imagesPath',
-						layoutReportsIssues: mockLayoutReportsIssuesSEODetails,
-						pageURLs: mockPageURLs,
-					},
-					loading: true,
-				}}
-			>
-				<IssuesList />
-			</StoreContextProvider>
-		);
+		const {getByText} = renderIssuesList({
+			layoutReportsIssues: mockLayoutReportsIssuesSEODetails,
+			loading: true,
+		});
 
 		const button = getByText('en-US').parentElement;
 
