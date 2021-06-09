208ea4234e58efc3b6745f26f031c2e078f7fbc5
Cheryl Tang <cheryl.tang@liferay.com>
2021-06-09 12:15:37 -0700

    LPS-133956 Create DLContentTableReferenceDefinitionTest

---
+++ modules/apps/document-library/document-library-content-test/src/testIntegration/java/com/liferay/document/library/content/change/tracking/test/DLContentTableReferenceDefinitionTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.content.change.tracking.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.test.util.BaseTableReferenceDefinitionTestCase;
+import com.liferay.document.library.content.service.DLContentLocalService;
+import com.liferay.document.library.kernel.store.Store;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.test.constants.TestDataConstants;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Cheryl Tang
+ */
+@RunWith(Arquillian.class)
+public class DLContentTableReferenceDefinitionTest
+	extends BaseTableReferenceDefinitionTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Override
+	protected CTModel<?> addCTModel() {
+		String path = DLContentTableReferenceDefinitionTest.class.getName();
+
+		return _dlContentLocalService.addContent(
+			group.getCompanyId(), RandomTestUtil.nextLong(), path,
+			Store.VERSION_DEFAULT,
+			new ByteArrayInputStream(TestDataConstants.TEST_BYTE_ARRAY));
+	}
+
+	@Inject
+	private static DLContentLocalService _dlContentLocalService;
+
+}
