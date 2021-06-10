47107b2beeb73228685b7e45d54d026c04a39b93
arthurchan35 <arthurchan35@gmail.com>
2021-06-10 14:59:25 +0800

    LPS-133646 Initial support for log context

--- modules/apps/portal-security-audit/portal-security-audit-wiring/src/main/java/com/liferay/portal/security/audit/wiring/internal/servlet/filter/AuditFilter.java
+++ modules/apps/portal-security-audit/portal-security-audit-wiring/src/main/java/com/liferay/portal/security/audit/wiring/internal/servlet/filter/AuditFilter.java
@@ -14,24 +14,43 @@
 
 package com.liferay.portal.security.audit.wiring.internal.servlet.filter;
 
+import com.liferay.petra.lang.CentralizedThreadLocal;
 import com.liferay.portal.kernel.audit.AuditRequestThreadLocal;
 import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogContext;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.servlet.BaseFilter;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.servlet.TryFilter;
+import com.liferay.portal.kernel.util.DigesterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Michael C. Han
  * @author Brian Wing Shun Chan
+ * @author Arthur Chan
  */
 @Component(
 	enabled = false, immediate = true,
@@ -55,7 +74,11 @@ public class AuditFilter extends BaseFilter implements TryFilter {
 
 		auditRequestThreadLocal.setClientHost(
 			httpServletRequest.getRemoteHost());
-		auditRequestThreadLocal.setClientIP(getRemoteAddr(httpServletRequest));
+
+		String remoteAddr = getRemoteAddr(httpServletRequest);
+
+		auditRequestThreadLocal.setClientIP(remoteAddr);
+
 		auditRequestThreadLocal.setQueryString(
 			httpServletRequest.getQueryString());
 
@@ -77,7 +100,29 @@ public class AuditFilter extends BaseFilter implements TryFilter {
 			httpServletRequest.getServerPort());
 		auditRequestThreadLocal.setSessionID(session.getId());
 
+		String xRequestIdHeader = "placeHolder";
+
+		_auditFilterLogContext.setContext(
+			remoteAddr, _portal.getCompanyId(httpServletRequest),
+			session.getId(), httpServletRequest.getServerName(), userId,
+			xRequestIdHeader);
+
 		return null;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_auditFilterLogContext = new AuditFilterLogContext();
+
+		_serviceRegistration = bundleContext.registerService(
+			LogContext.class, _auditFilterLogContext, new HashMapDictionary());
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_serviceRegistration != null) {
+			_serviceRegistration.unregister();
+		}
 	}
 
 	@Override
@@ -96,6 +141,91 @@ public class AuditFilter extends BaseFilter implements TryFilter {
 		return httpServletRequest.getRemoteAddr();
 	}
 
+	private static final String _MESSAGE_DIGEST_ALGORITHM = "SHA-256";
+
 	private static final Log _log = LogFactoryUtil.getLog(AuditFilter.class);
+
+	private AuditFilterLogContext _auditFilterLogContext;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	private ServiceRegistration<LogContext> _serviceRegistration;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+	private class AuditFilterLogContext implements LogContext {
+
+		public AuditFilterLogContext() {
+			_contextThreadLocal = new CentralizedThreadLocal<>(
+				AuditFilterLogContext.class + ".context",
+				HashMap<String, String>::new);
+		}
+
+		@Override
+		public Map<String, String> getContext() {
+			return _contextThreadLocal.get();
+		}
+
+		@Override
+		public String getName() {
+			return AuditFilterLogContext.class.getSimpleName();
+		}
+
+		public void setContext(
+			String clientIP, long companyId, String sessionId,
+			String serverName, Long userId, String xRequestIdHeader) {
+
+			_contextThreadLocal.set(
+				HashMapBuilder.put(
+					"clientIP", clientIP
+				).put(
+					"companyId", String.valueOf(companyId)
+				).put(
+					"emailAddress",
+					() -> {
+						if (userId != null) {
+							User user = _userLocalService.fetchUser(userId);
+
+							if (user != null) {
+								return user.getEmailAddress();
+							}
+						}
+
+						return "";
+					}
+				).put(
+					"serverName", serverName
+				).put(
+					"sessionId",
+					DigesterUtil.digest(_MESSAGE_DIGEST_ALGORITHM, sessionId)
+				).put(
+					"userId", (userId != null) ? String.valueOf(userId) : ""
+				).put(
+					"virtualHostName",
+					() -> {
+						if (companyId >= 0) {
+							Company company = _companyLocalService.fetchCompany(
+								companyId);
+
+							if (company != null) {
+								return company.getVirtualHostname();
+							}
+						}
+
+						return "";
+					}
+				).put(
+					"xRequestIdHeader", xRequestIdHeader
+				).build());
+		}
+
+		private final ThreadLocal<Map<String, String>> _contextThreadLocal;
+
+	}
 
 }
