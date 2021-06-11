2151fe4faddffd8482d72a0a6747bf15637f1cf8
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-06-11 13:34:53 +0200

    LPS-128385 Replicating the same settings for sites using a configuration class

---
+++ modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/configuration/LayoutReportsGooglePageSpeedGroupConfiguration.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.reports.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedAttributeDefinition;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Alejandro Tardín
+ */
+@ExtendedObjectClassDefinition(
+	category = "pages", scope = ExtendedObjectClassDefinition.Scope.GROUP
+)
+@Meta.OCD(
+	description = "layout-reports-google-pagespeed-configuration-description",
+	id = "com.liferay.layout.reports.web.internal.configuration.LayoutReportsGooglePageSpeedGroupConfiguration",
+	localization = "content/Language",
+	name = "layout-reports-google-pagespeed-group-configuration-name"
+)
+public interface LayoutReportsGooglePageSpeedGroupConfiguration {
+
+	@Meta.AD(deflt = "true", name = "enable-google-pagespeed", required = false)
+	public boolean enabled();
+
+	@ExtendedAttributeDefinition(
+		descriptionArguments = "https://developers.google.com/speed/docs/insights/v5/get-started"
+	)
+	@Meta.AD(
+		description = "api-key-description", name = "api-key", required = false
+	)
+	public String apiKey();
+
+	@Meta.AD(
+		deflt = "MOBILE", description = "strategy-description",
+		name = "strategy", optionLabels = {"mobile", "desktop"},
+		optionValues = {"MOBILE", "DESKTOP"}, required = false
+	)
+	public String strategy();
+
+}
--- modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/init.jsp
+++ modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/init.jsp
@@ -14,13 +14,8 @@
  */
 --%>
 
-<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
-
-<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
-taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+<%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
 
 <liferay-theme:defineObjects />
-
-<%@ page import="com.liferay.layout.reports.web.internal.display.context.LayoutReportsGooglePageSpeedDisplayContext" %>
 
 <%@ include file="/init-ext.jsp" %>
