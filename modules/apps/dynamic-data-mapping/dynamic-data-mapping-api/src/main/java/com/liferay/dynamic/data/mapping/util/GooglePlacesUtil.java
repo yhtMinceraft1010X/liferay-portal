3184d0845cb02d9fac172b7cd96b14fb17077e5e
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2021-06-15 14:01:02 -0300

    LPS-132797 Constant and util to get the preferences

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/GooglePlacesUtil.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.util;
+
+import com.liferay.dynamic.data.mapping.constants.GooglePlacesWebKeys;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+
+import javax.portlet.PortletPreferences;
+
+/**
+ * @author Rodrigo Paulino
+ */
+public class GooglePlacesUtil {
+
+	public static String getGooglePlacesAPIKey(long companyId) {
+		PortletPreferences companyPortletPreferences =
+			PrefsPropsUtil.getPreferences(companyId);
+
+		return companyPortletPreferences.getValue(
+			GooglePlacesWebKeys.GOOGLE_PLACES_API_KEY, null);
+	}
+
+	public static String getGooglePlacesAPIKey(
+		long companyId, long groupId, GroupLocalService groupLocalService) {
+
+		String defaultGooglePlacesAPIKey = getGooglePlacesAPIKey(companyId);
+
+		Group group = groupLocalService.fetchGroup(groupId);
+
+		if (group == null) {
+			return defaultGooglePlacesAPIKey;
+		}
+
+		if (group.isStagingGroup()) {
+			group = group.getLiveGroup();
+		}
+
+		return GetterUtil.getString(
+			group.getTypeSettingsProperty(
+				GooglePlacesWebKeys.GOOGLE_PLACES_API_KEY),
+			defaultGooglePlacesAPIKey);
+	}
+
+}
