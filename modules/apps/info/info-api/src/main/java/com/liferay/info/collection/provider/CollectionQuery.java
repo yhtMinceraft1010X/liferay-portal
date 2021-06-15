682a92f16517d7e0dc34148d730d2d4a4dbc9f4f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-06-15 14:06:25 +0100

    LPS-130446 Adds new class to represent a Collection Query

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/collection/provider/CollectionQuery.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.collection.provider;
+
+import com.liferay.info.filter.InfoFilter;
+import com.liferay.info.pagination.Pagination;
+import com.liferay.info.sort.Sort;
+
+import java.util.Optional;
+
+/**
+ * @author Jorge Ferrer
+ * @author Eudaldo Alonso
+ */
+public class CollectionQuery {
+
+	public Optional<InfoFilter> getInfoFiltersOptional() {
+		return Optional.ofNullable(_infoFilter);
+	}
+
+	public Pagination getPagination() {
+		if (_pagination == null) {
+			return Pagination.of(20, 0);
+		}
+
+		return _pagination;
+	}
+
+	public Optional<Sort> getSortOptional() {
+		return Optional.ofNullable(_sort);
+	}
+
+	public void setInfoFilter(InfoFilter infoFilter) {
+		_infoFilter = infoFilter;
+	}
+
+	public void setPagination(Pagination pagination) {
+		_pagination = pagination;
+	}
+
+	public void setSort(Sort sort) {
+		_sort = sort;
+	}
+
+	private InfoFilter _infoFilter;
+	private Pagination _pagination;
+	private Sort _sort;
+
+}
