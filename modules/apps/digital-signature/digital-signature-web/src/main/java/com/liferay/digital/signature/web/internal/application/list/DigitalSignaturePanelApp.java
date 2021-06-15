7761c0ed62655226eb4b80bc108d2a99ded37a1b
Jose Abelenda <jose.abelenda@liferay.com>
2021-06-15 11:45:59 -0300

    LPS-134208 Control visibility on Content and Data sidebar menu

--- modules/apps/digital-signature/digital-signature-web/src/main/java/com/liferay/digital/signature/web/internal/application/list/DigitalSignaturePanelApp.java
+++ modules/apps/digital-signature/digital-signature-web/src/main/java/com/liferay/digital/signature/web/internal/application/list/DigitalSignaturePanelApp.java
@@ -17,8 +17,13 @@ package com.liferay.digital.signature.web.internal.application.list;
 import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.digital.signature.configuration.DigitalSignatureConfiguration;
+import com.liferay.digital.signature.configuration.DigitalSignatureConfigurationUtil;
 import com.liferay.digital.signature.constants.DigitalSignaturePortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -39,6 +44,21 @@ public class DigitalSignaturePanelApp extends BasePanelApp {
 	@Override
 	public String getPortletId() {
 		return DigitalSignaturePortletKeys.DIGITAL_SIGNATURE;
+	}
+
+	@Override
+	public boolean isShow(PermissionChecker permissionChecker, Group group)
+		throws PortalException {
+
+		DigitalSignatureConfiguration digitalSignatureConfiguration =
+			DigitalSignatureConfigurationUtil.getDigitalSignatureConfiguration(
+				permissionChecker.getCompanyId(), group.getGroupId());
+
+		if (!digitalSignatureConfiguration.enabled()) {
+			return false;
+		}
+
+		return super.isShow(permissionChecker, group);
 	}
 
 	@Override
