d97420d079335ddd2c07a58f7e8cac859ff630be
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-06-15 18:47:33 -0300

    LPS-133888 Create Data Provider to load Objects

---
+++ modules/apps/object/object-dynamic-data-mapping-storage/src/main/java/com/liferay/object/dynamic/data/mapping/storage/internal/ObjectsDataProvider.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.dynamic.data.mapping.storage.internal;
+
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderException;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderRequest;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderResponse;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.KeyValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+@Component(
+	immediate = true, property = "ddm.data.provider.instance.id=objects",
+	service = DDMDataProvider.class
+)
+public class ObjectsDataProvider implements DDMDataProvider {
+
+	@Override
+	public DDMDataProviderResponse getData(
+			DDMDataProviderRequest ddmDataProviderRequest)
+		throws DDMDataProviderException {
+
+		List<KeyValuePair> keyValuePairs = new ArrayList<>();
+
+		try {
+			List<ObjectDefinition> objectDefinitions =
+				_objectDefinitionLocalService.getObjectDefinitions(
+					0,
+					_objectDefinitionLocalService.getObjectDefinitionsCount(
+						ddmDataProviderRequest.getCompanyId()));
+
+			for (ObjectDefinition objectDefinition : objectDefinitions) {
+				keyValuePairs.add(
+					new KeyValuePair(
+						String.valueOf(
+							objectDefinition.getObjectDefinitionId()),
+						objectDefinition.getName()));
+			}
+
+			DDMDataProviderResponse.Builder builder =
+				DDMDataProviderResponse.Builder.newBuilder();
+
+			builder.withOutput("Default-Output", keyValuePairs);
+
+			return builder.build();
+		}
+		catch (PortalException portalException) {
+			throw new DDMDataProviderException(portalException);
+		}
+	}
+
+	@Override
+	public Class<?> getSettings() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+}
