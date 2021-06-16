19619e7e8e764269889c010b832721126c50d71f
Lawrence Lee <lawrence.lee@liferay.com>
2021-06-16 14:16:07 -0500

    LPS-134302 Use regex to capture versions

---
+++ modules/sdk/project-templates/project-templates-extensions/src/main/java/com/liferay/project/templates/extensions/util/VersionUtil.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.project.templates.extensions.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Gregory Amerson
+ * @author Lawrence Lee
+ */
+public class VersionUtil {
+
+	public static int getMajorVersion(String targetPlatformVersion) {
+		Matcher matcher = _liferayVersionPattern.matcher(targetPlatformVersion);
+
+		return Integer.parseInt(matcher.group(1));
+	}
+
+	public static int getMicroVersion(String targetPlatformVersion) {
+		Matcher matcher = _liferayVersionPattern.matcher(targetPlatformVersion);
+
+		return Integer.parseInt(matcher.group(3));
+	}
+
+	public static int getMinorVersion(String targetPlatformVersion) {
+		Matcher matcher = _liferayVersionPattern.matcher(targetPlatformVersion);
+
+		return Integer.parseInt(matcher.group(2));
+	}
+
+	public static boolean isLiferayVersion(String targetPlatformVersion) {
+		Matcher matcher = _liferayVersionPattern.matcher(targetPlatformVersion);
+
+		return matcher.matches();
+	}
+
+	private static final Pattern _liferayVersionPattern = Pattern.compile(
+		"^([0-9])\\.([0-9])\\.(\\d+)(-\\d+|(\\.((e|f)p)?[0-9]+(-[0-9]+)?))?$");
+
+}
