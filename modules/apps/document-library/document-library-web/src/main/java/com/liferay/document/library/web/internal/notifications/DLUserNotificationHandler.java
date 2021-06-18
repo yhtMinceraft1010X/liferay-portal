9af9f1d7f2d494ef188a153954cc53b7a909b578
Alicia Garcia <alicia.garcia@liferay.com>
2021-06-18 12:19:38 +0200

    LPS-133634 change message on the review notification

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/notifications/DLUserNotificationHandler.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/notifications/DLUserNotificationHandler.java
@@ -14,9 +14,16 @@
 
 package com.liferay.document.library.web.internal.notifications;
 
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.document.library.constants.DLPortletKeys;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.notifications.BaseModelUserNotificationHandler;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
 import com.liferay.portal.kernel.notifications.UserNotificationHandler;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -32,6 +39,42 @@ public class DLUserNotificationHandler
 
 	public DLUserNotificationHandler() {
 		setPortletId(DLPortletKeys.DOCUMENT_LIBRARY);
+	}
+
+	@Override
+	protected String getTitle(
+		JSONObject jsonObject, AssetRenderer<?> assetRenderer,
+		ServiceContext serviceContext) {
+
+		String message = StringPool.BLANK;
+
+		AssetRendererFactory<?> assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				assetRenderer.getClassName());
+
+		String typeName = assetRendererFactory.getTypeName(
+			serviceContext.getLocale());
+
+		int notificationType = jsonObject.getInt("notificationType");
+
+		if (notificationType ==
+				UserNotificationDefinition.NOTIFICATION_TYPE_ADD_ENTRY) {
+
+			message = "x-added-a-new-x";
+		}
+		else if (notificationType ==
+					UserNotificationDefinition.NOTIFICATION_TYPE_REVIEW_ENTRY) {
+
+			message = "x-review-a-x";
+		}
+		else if (notificationType ==
+					UserNotificationDefinition.NOTIFICATION_TYPE_UPDATE_ENTRY) {
+
+			message = "x-updated-a-x";
+		}
+
+		return getFormattedMessage(
+			jsonObject, serviceContext, message, typeName);
 	}
 
 }
