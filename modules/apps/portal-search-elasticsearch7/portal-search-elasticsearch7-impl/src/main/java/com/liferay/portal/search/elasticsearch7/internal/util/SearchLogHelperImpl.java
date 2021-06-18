1fd10e435df72de28654553f19993a94f1340e8b
Joshua Cords <joshua.cords@liferay.com>
2021-06-18 15:10:26 -0700

    LPS-131966 portal-search-elasticsearch7-impl: remove log4j and deprecate setRESTClientLoggerLevel
    https://issues.liferay.com/browse/LPS-131966

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/util/SearchLogHelperImpl.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/util/SearchLogHelperImpl.java
@@ -20,14 +20,8 @@ import com.liferay.portal.search.elasticsearch7.configuration.RESTClientLoggerLe
 
 import java.io.IOException;
 
-import org.apache.commons.logging.LogFactory;
-import org.apache.commons.logging.impl.Log4JLogger;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.common.io.stream.OutputStreamStreamOutput;
 
 /**
@@ -69,17 +63,6 @@ public class SearchLogHelperImpl implements SearchLogHelper {
 	@Override
 	public void setRESTClientLoggerLevel(
 		RESTClientLoggerLevel restClientLoggerLevel) {
-
-		org.apache.commons.logging.Log log = LogFactory.getLog(
-			RestClient.class);
-
-		if (log instanceof Log4JLogger) {
-			Log4JLogger log4JLogger = (Log4JLogger)log;
-
-			Logger logger = log4JLogger.getLogger();
-
-			logger.setLevel(Level.toLevel(restClientLoggerLevel.name()));
-		}
 	}
 
 }
