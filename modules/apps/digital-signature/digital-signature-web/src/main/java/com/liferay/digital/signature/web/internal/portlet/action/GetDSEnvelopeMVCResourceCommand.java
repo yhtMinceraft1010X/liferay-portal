96fe8072f8eba59dd7c9c1f6dbb74e6feb94a067
Keven Leone <keven.leone@liferay.com>
2021-06-21 09:54:32 -0300

    LPS-134540 Add Image Processor and PDF checking to create better Preview experience

--- modules/apps/digital-signature/digital-signature-web/src/main/java/com/liferay/digital/signature/web/internal/portlet/action/GetDSEnvelopeMVCResourceCommand.java
+++ modules/apps/digital-signature/digital-signature-web/src/main/java/com/liferay/digital/signature/web/internal/portlet/action/GetDSEnvelopeMVCResourceCommand.java
@@ -16,9 +16,11 @@ package com.liferay.digital.signature.web.internal.portlet.action;
 
 import com.liferay.digital.signature.constants.DigitalSignaturePortletKeys;
 import com.liferay.digital.signature.manager.DSEnvelopeManager;
-import com.liferay.digital.signature.model.DSDocument;
 import com.liferay.digital.signature.model.DSEnvelope;
+import com.liferay.document.library.kernel.model.DLProcessorConstants;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.document.library.kernel.util.DLProcessor;
+import com.liferay.document.library.kernel.util.ImageProcessor;
 import com.liferay.document.library.kernel.util.PDFProcessorUtil;
 import com.liferay.document.library.util.DLURLHelperUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -36,12 +38,14 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.Objects;
+import java.util.Set;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Keven Leone
@@ -76,15 +80,24 @@ public class GetDSEnvelopeMVCResourceCommand extends BaseMVCResourceCommand {
 				"fileEntries",
 				JSONUtil.toJSONArray(
 					dsEnvelope.getDSDocuments(),
-					dsDocument -> _toJSONObject(dsDocument, themeDisplay), _log)
+					dsDocument -> _toJSONObject(
+						dsDocument.getDSDocumentId(), themeDisplay),
+					_log)
 			));
 	}
 
-	private JSONObject _toJSONObject(
-			DSDocument dsDocument, ThemeDisplay themeDisplay)
-		throws Exception {
+	@Reference(
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(type=" + DLProcessorConstants.IMAGE_PROCESSOR + ")",
+		unbind = "-"
+	)
+	protected void setDLProcessor(DLProcessor dlProcessor) {
+		_imageProcessor = (ImageProcessor)dlProcessor;
+	}
 
-		String dsDocumentId = dsDocument.getDSDocumentId();
+	private JSONObject _toJSONObject(
+			String dsDocumentId, ThemeDisplay themeDisplay)
+		throws Exception {
 
 		if (Objects.equals(dsDocumentId, "certificate")) {
 			return null;
@@ -95,16 +108,29 @@ public class GetDSEnvelopeMVCResourceCommand extends BaseMVCResourceCommand {
 
 		FileVersion fileVersion = fileEntry.getFileVersion();
 
-		return JSONUtil.put(
-			"initialPage", 1
-		).put(
-			"previewFileCount",
-			PDFProcessorUtil.getPreviewFileCount(fileVersion)
-		).put(
-			"previewFileURL",
-			DLURLHelperUtil.getPreviewURL(
-				fileEntry, fileVersion, themeDisplay, "&previewFileIndex=")
-		);
+		Set<String> imageMimeTypes = _imageProcessor.getImageMimeTypes();
+
+		if (imageMimeTypes.contains(fileEntry.getMimeType())) {
+			return JSONUtil.put(
+				"imageURL",
+				DLURLHelperUtil.getPreviewURL(
+					fileEntry, fileVersion, themeDisplay, "&imagePreview=1"));
+		}
+
+		if (PDFProcessorUtil.isDocumentSupported(fileVersion)) {
+			return JSONUtil.put(
+				"initialPage", 1
+			).put(
+				"previewFileCount",
+				PDFProcessorUtil.getPreviewFileCount(fileVersion)
+			).put(
+				"previewFileURL",
+				DLURLHelperUtil.getPreviewURL(
+					fileEntry, fileVersion, themeDisplay, "&previewFileIndex=")
+			);
+		}
+
+		return JSONUtil.put("title", fileEntry.getTitle());
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
@@ -115,5 +141,7 @@ public class GetDSEnvelopeMVCResourceCommand extends BaseMVCResourceCommand {
 
 	@Reference
 	private DSEnvelopeManager _dsEnvelopeManager;
+
+	private ImageProcessor _imageProcessor;
 
 }
