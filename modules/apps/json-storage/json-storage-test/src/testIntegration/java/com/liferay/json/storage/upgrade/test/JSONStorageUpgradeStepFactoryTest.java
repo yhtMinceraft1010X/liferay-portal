7dd23426c7375d785efe78732a1a0074de4387c5
Preston-Crary <preston.crary@liferay.com>
2021-06-22 11:50:47 -0700

    LPS-131321 Test coverage for upgrade factory.

---
+++ modules/apps/json-storage/json-storage-test/src/testIntegration/java/com/liferay/json/storage/upgrade/test/JSONStorageUpgradeStepFactoryTest.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.json.storage.upgrade.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.json.storage.service.JSONStorageEntryLocalService;
+import com.liferay.json.storage.upgrade.JSONStorageUpgradeStepFactory;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.sql.Connection;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Preston Crary
+ */
+@RunWith(Arquillian.class)
+public class JSONStorageUpgradeStepFactoryTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		DB db = DBManagerUtil.getDB();
+
+		db.runSQL(
+			StringBundler.concat(
+				"create table JSONStorageUpgrade (jsonStorageUpgradeId LONG ",
+				"not null, ctCollectionId LONG not null, companyId LONG, ",
+				"jsonString VARCHAR(75), primary key (jsonStorageUpgradeId, ",
+				"ctCollectionId));"));
+
+		db.runSQL(
+			StringBundler.concat(
+				"insert into JSONStorageUpgrade values (", _CLASS_PK, ", 0, ",
+				TestPropsValues.getCompanyId(), ", '", _JSON_STRING, "');"));
+
+		_className = _classNameLocalService.getClassName(
+			JSONStorageUpgradeStepFactoryTest.class.getName());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		DB db = DBManagerUtil.getDB();
+
+		db.runSQL("drop table JSONStorageUpgrade;");
+
+		_jsonStorageEntryLocalService.deleteJSONStorageEntries(
+			_className.getClassNameId(), _CLASS_PK);
+	}
+
+	@Test
+	public void testJSONStoreUpgradeStepFactory() throws Exception {
+		UpgradeProcess upgradeProcess =
+			(UpgradeProcess)_jsonStorageUpgradeStepFactory.createUpgradeStep(
+				JSONStorageUpgradeStepFactoryTest.class, "JSONStorageUpgrade",
+				"jsonStorageUpgradeId", "jsonString");
+
+		upgradeProcess.upgrade();
+
+		try (Connection connection = DataAccess.getConnection()) {
+			DBInspector dbInspector = new DBInspector(connection);
+
+			Assert.assertFalse(
+				dbInspector.hasColumn("JSONStorageUpgrade", "jsonString"));
+		}
+
+		Assert.assertEquals(
+			_JSON_STRING,
+			_jsonStorageEntryLocalService.getJSON(
+				_className.getClassNameId(), _CLASS_PK));
+	}
+
+	private static final long _CLASS_PK = 1;
+
+	private static final String _JSON_STRING =
+		"{\"array\":[1,2],\"object\":{\"key\":\"value\"}}";
+
+	@Inject
+	private static ClassNameLocalService _classNameLocalService;
+
+	@Inject
+	private static JSONStorageEntryLocalService _jsonStorageEntryLocalService;
+
+	@Inject
+	private static JSONStorageUpgradeStepFactory _jsonStorageUpgradeStepFactory;
+
+	@DeleteAfterTestRun
+	private ClassName _className;
+
+}
