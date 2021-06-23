deaacd9b890fa7fc2fab086466837c70db5e1bc2
Preston-Crary <preston.crary@liferay.com>
2021-06-23 15:15:38 -0700

    LPS-134165 Build Service.

--- modules/apps/json-storage/json-storage-service/src/main/java/com/liferay/json/storage/service/persistence/impl/JSONStorageEntryPersistenceImpl.java
+++ modules/apps/json-storage/json-storage-service/src/main/java/com/liferay/json/storage/service/persistence/impl/JSONStorageEntryPersistenceImpl.java
@@ -3084,7 +3084,8 @@ public class JSONStorageEntryPersistenceImpl
 	public Set<String> getCTColumnNames(
 		CTColumnResolutionType ctColumnResolutionType) {
 
-		return _ctColumnNamesMap.get(ctColumnResolutionType);
+		return _ctColumnNamesMap.getOrDefault(
+			ctColumnResolutionType, Collections.emptySet());
 	}
 
 	@Override
@@ -3117,8 +3118,6 @@ public class JSONStorageEntryPersistenceImpl
 
 	static {
 		Set<String> ctControlColumnNames = new HashSet<String>();
-		Set<String> ctIgnoreColumnNames = new HashSet<String>();
-		Set<String> ctMergeColumnNames = new HashSet<String>();
 		Set<String> ctStrictColumnNames = new HashSet<String>();
 
 		ctControlColumnNames.add("mvccVersion");
@@ -3135,9 +3134,6 @@ public class JSONStorageEntryPersistenceImpl
 
 		_ctColumnNamesMap.put(
 			CTColumnResolutionType.CONTROL, ctControlColumnNames);
-		_ctColumnNamesMap.put(
-			CTColumnResolutionType.IGNORE, ctIgnoreColumnNames);
-		_ctColumnNamesMap.put(CTColumnResolutionType.MERGE, ctMergeColumnNames);
 		_ctColumnNamesMap.put(
 			CTColumnResolutionType.PK,
 			Collections.singleton("jsonStorageEntryId"));
