db03a9075c3b4bde5b1327e7ab3bf260e0c34102
Matuzalém Teles <matuzalemteles@gmail.com>
2021-06-23 21:48:17 -0300

    LPS-134118 Refactors the segmentation of violations

--- modules/apps/frontend-js/frontend-js-a11y-web/src/main/resources/META-INF/resources/hooks/useA11y.ts
+++ modules/apps/frontend-js/frontend-js-a11y-web/src/main/resources/META-INF/resources/hooks/useA11y.ts
@@ -16,95 +16,81 @@ import {useEffect, useState} from 'react';
 
 import {A11yChecker} from '../A11yChecker';
 
-import type {CheckResult, ImpactValue, Result} from 'axe-core';
+import type {NodeResult, Result} from 'axe-core';
 
 import type {A11yCheckerOptions} from '../A11yChecker';
 
-export type Violation = {
-	all: Array<CheckResult>;
-	any: Array<CheckResult>;
-	help: string;
-	helpUrl: string;
-	id: string;
-	impact?: ImpactValue;
+type Target = string;
+
+type RuleId = string;
+
+export interface RuleRaw extends Omit<Result, 'nodes'> {
+	nodes: Array<Target>;
+}
+
+export type NodeViolations = Record<RuleId, NodeResult>;
+
+export type Violations = {
+	nodes: Record<Target, NodeViolations>;
+	rules: Record<RuleId, RuleRaw>;
 };
 
-type Violations = {
-	modifyIndex: number;
-	target: string;
-	violations: Array<Violation>;
-};
-
-function segmentViolationsByNode(
+function segmentViolationsByRulesAndNodes(
 	violations: Array<Result>,
-	prevViolations: Array<Violations>
+	prevViolations: Violations
 ) {
-	const prevNodes = prevViolations.reduce<Record<string, Violations>>(
+	return violations.reduce<Violations>(
 		(prev, current) => {
+			const {nodes, ...rule} = current;
 
-			// Revalidation if the target exists on the DOM
+			const targets = nodes.map((node) => {
+				const target = node.target[0];
 
-			if (document.querySelector(current.target)) {
-				prev[current.target] = current;
+				if (prev.nodes[target]) {
+					prev.nodes[target][current.id] = node;
+				}
+				else {
+					prev.nodes[target] = {[current.id]: node};
+				}
+
+				return target;
+			});
+
+			if (!prev.rules[current.id]) {
+				prev.rules[current.id] = {
+					...rule,
+					nodes: targets,
+				};
+			}
+			else {
+				prev.rules[current.id].nodes = [
+					...new Set([...prev.rules[current.id].nodes, ...targets]),
+				];
 			}
 
 			return prev;
 		},
-		{} as Record<string, Violations>
+		{...prevViolations}
 	);
-
-	const nodes = violations.reduce<Record<string, Violations>>(
-		(prev, current) => {
-			current.nodes.forEach((node) => {
-				const {help, helpUrl, id, impact} = current;
-
-				const violation = {
-					all: node.all,
-					any: node.any,
-					help,
-					helpUrl,
-					id,
-					impact,
-				};
-
-				const target = node.target[0];
-
-				if (!prev[target]) {
-					prev[target] = {
-						modifyIndex: 0,
-						target,
-						violations: [violation],
-					};
-				}
-				else {
-					const hasViolation = prev[target].violations.find(
-						(violation) => violation.id === id
-					);
-
-					if (!hasViolation) {
-						prev[target].modifyIndex++;
-						prev[target].violations.push(violation);
-					}
-				}
-			});
-
-			return prev;
-		},
-		prevNodes
-	);
-
-	return Object.values(nodes);
 }
 
+const defaultState = {
+	nodes: {},
+	rules: {},
+};
+
 export default function useA11y(props: Omit<A11yCheckerOptions, 'callback'>) {
-	const [violations, setViolations] = useState<Array<Violations>>([]);
+	const [violations, setViolations] = useState<Violations>(defaultState);
 
 	useEffect(() => {
 		const checker = new A11yChecker({
 			callback: (results) =>
 				results.violations.length &&
 				setViolations((prevViolations) =>
-					segmentViolationsByNode(results.violations, prevViolations)
+					segmentViolationsByRulesAndNodes(
+						results.violations,
+						prevViolations
+					)
 				),
 			...props,
 		});
