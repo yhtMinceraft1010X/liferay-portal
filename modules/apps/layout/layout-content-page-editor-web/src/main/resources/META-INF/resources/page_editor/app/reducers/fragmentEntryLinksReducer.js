8fddb29a187b15d6f50ba7573b12512e4dda58b1
Pablo Molina <contact@pablomolina.me>
2021-06-25 10:15:14 +0200

    LPS-134609 Refresh configuration definition

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/fragmentEntryLinksReducer.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/fragmentEntryLinksReducer.js
@@ -246,6 +246,7 @@ export default function fragmentEntryLinksReducer(
 				...fragmentEntryLinks,
 				[action.fragmentEntryLinkId]: {
 					...fragmentEntryLinks[action.fragmentEntryLinkId],
+					configuration: action.fragmentEntryLink.configuration,
 					content: action.fragmentEntryLink.content,
 					editableValues: action.fragmentEntryLink.editableValues,
 				},
