fb05eeadfe26bc6f5ea5b2f2d119a5a95f08d529
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2021-06-25 12:35:53 +0200

    LPS-118827 Move second script section and replace AUI dependencies

--- modules/apps/comment/comment-taglib/src/main/resources/META-INF/resources/discussion/js/Comments.js
+++ modules/apps/comment/comment-taglib/src/main/resources/META-INF/resources/discussion/js/Comments.js
@@ -437,4 +437,80 @@ export default function ({
 
 		editorInstance.dispose();
 	};
+
+	window[`${randomNamespace}onMessagePosted`] = function (
+		response,
+		refreshPage
+	) {
+		Liferay.onceAfter(`${portletDisplayId}:portletRefreshed`, () => {
+			const randomNamespaceNodes = document.querySelectorAll(
+				`input[id^="${namespace}"][id$="randomNamespace"]`
+			);
+
+			Array.prototype.forEach.call(
+				randomNamespaceNodes,
+				(node, index) => {
+					const randomId = node.value;
+
+					if (index === 0) {
+						window[`${randomNamespace}showStatusMessage`]({
+							id: randomId,
+							message: Liferay.Language.get(
+								'your-request-completed-successfully'
+							),
+							type: 'success',
+						});
+					}
+
+					const currentMessageSelector = `${randomId}message_${response.commentId}`;
+
+					const targetNode = document.getElementById(
+						currentMessageSelector
+					);
+
+					if (targetNode) {
+						location.hash = '#' + currentMessageSelector;
+					}
+				}
+			);
+		});
+
+		const className = Util.getFormElement(form, 'className').value;
+		const classPK = Util.getFormElement(form, 'classPK').value;
+
+		if (response.commentId) {
+			const messageTextNode = document.querySelector(
+				`input[name^="${namespace}${randomNamespace}body"]`
+			);
+
+			if (messageTextNode) {
+				Liferay.fire('messagePosted', {
+					className,
+					classPK,
+					commentId: response.commentId,
+					text: messageTextNode.value,
+				});
+			}
+		}
+
+		if (refreshPage) {
+			window.location.reload();
+		}
+		else {
+			const portletNodeId = `p_p_id_${portletDisplayId}_`;
+
+			const portletNode = document.getElementById(portletNodeId);
+
+			const data = Liferay.Util.ns(namespace, {
+				className,
+				classPK,
+				skipEditorLoading: true,
+			});
+
+			Liferay.Portlet.refresh('#' + portletNodeId, {
+				...data,
+				...(portletNode.refreshURLData || {}),
+			});
+		}
+	};
 }
--- modules/apps/comment/comment-taglib/src/main/resources/META-INF/resources/init.jsp
+++ modules/apps/comment/comment-taglib/src/main/resources/META-INF/resources/init.jsp
@@ -30,7 +30,6 @@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 <%@ page import="com.liferay.petra.portlet.url.builder.PortletURLBuilder" %><%@
 page import="com.liferay.petra.string.StringPool" %><%@
 page import="com.liferay.portal.kernel.language.LanguageUtil" %><%@
-page import="com.liferay.portal.kernel.language.UnicodeLanguageUtil" %><%@
 page import="com.liferay.portal.kernel.model.*" %><%@
 page import="com.liferay.portal.kernel.model.impl.*" %><%@
 page import="com.liferay.portal.kernel.portlet.LiferayWindowState" %><%@
