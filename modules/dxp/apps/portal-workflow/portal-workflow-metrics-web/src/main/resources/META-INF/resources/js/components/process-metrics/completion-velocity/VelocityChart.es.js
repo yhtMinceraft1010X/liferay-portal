a8789aaba28a75d1e31ae177f76f1ee49babb4d1
Joao Paulo Pontes <joao.pontes@liferay.com>
2021-06-28 18:25:45 -0300

    LPS-134779 Refactor tooltip

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-metrics/completion-velocity/VelocityChart.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-metrics/completion-velocity/VelocityChart.es.js
@@ -10,19 +10,31 @@
  */
 
 import React, {useContext} from 'react';
-import {CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
+import {
+	CartesianGrid,
+	Line,
+	LineChart,
+	ResponsiveContainer,
+	Tooltip,
+	XAxis,
+	YAxis,
+} from 'recharts';
 
 import moment from '../../../shared/util/moment.es';
 import {AppContext} from '../../AppContext.es';
 import {
+	formatMonthDate,
+	formatWeekDateWithYear,
 	formatXAxisDate,
+	formatYearDate,
 	getAxisMeasuresFromData,
 } from './util/chart.es';
+import {HOURS, MONTHS, WEEKS, YEARS} from './util/chartConstants.es';
 
 function VelocityChart({timeRange, velocityData = {}, velocityUnit}) {
 	const {isAmPm} = useContext(AppContext);
 
-	const {key: unitKey} = velocityUnit;
+	const {key: unitKey, name: unitName} = velocityUnit;
 
 	const {histograms = []} = velocityData;
 
@@ -32,15 +44,10 @@ function VelocityChart({timeRange, velocityData = {}, velocityUnit}) {
 
 	const dataChart = histograms.map((data) => {
 		const date = moment.utc(data.key).toDate();
-		data.name = formatXAxisDate(
-			date,
-			isAmPm,
-			unitKey,
-			timeRange
-		);
+		data.name = formatXAxisDate(date, isAmPm, unitKey, timeRange);
 
-		return data
-	})
+		return data;
+	});
 
 	return (
 		<div className="velocity-chart">
@@ -59,14 +66,103 @@ function VelocityChart({timeRange, velocityData = {}, velocityUnit}) {
 					>
 						<CartesianGrid strokeDasharray="3 3" />
 						<XAxis dataKey="name" interval="preserveStartEnd" />
-						<YAxis domain={[intervals, maxValue]} interval="preserveStartEnd"/>
-						<Tooltip />
-						<Line activeDot={{ r: 8 }} dataKey="value" type="monotone" />
+						<YAxis
+							domain={[intervals, maxValue]}
+							interval="preserveStartEnd"
+						/>
+						<Tooltip
+							content={
+								<CustomTooltip
+									isAmPm={isAmPm}
+									timeRange={timeRange}
+									unit={unitName}
+									unitKey={unitKey}
+								/>
+							}
+						/>
+						<Line
+							activeDot={{r: 8}}
+							dataKey="value"
+							type="monotone"
+						/>
 					</LineChart>
 				</ResponsiveContainer>
 			)}
 		</div>
 	);
 }
+
+const CLASSNAME = 'workflow-tooltip-chart';
+
+const CustomTooltip = ({active, isAmPm, payload, timeRange, unit, unitKey}) => {
+	const formatTooltipDate = (date, isAmPm, timeRange, unitKey) => {
+		let datePattern = Liferay.Language.get('ddd-mmm-d');
+
+		const dateUTC = moment.utc(date);
+
+		if (unitKey === HOURS) {
+			if (isAmPm) {
+				datePattern = Liferay.Language.get('mmm-dd-hh-mm-a');
+			}
+			else {
+				datePattern = Liferay.Language.get('mmm-dd-hh-mm');
+			}
+
+			return dateUTC.format(datePattern);
+		}
+		else if (unitKey === WEEKS) {
+			return formatWeekDateWithYear(date, timeRange);
+		}
+		else if (unitKey === MONTHS) {
+			return formatMonthDate(date, timeRange);
+		}
+		else if (unitKey === YEARS) {
+			return formatYearDate(date, timeRange);
+		}
+
+		return dateUTC.format(datePattern);
+	};
+
+	const getDateTitle = (date, isAmPm, timeRange, unitKey) => {
+		if (!(date instanceof Date && !isNaN(date.getTime()))) {
+
+			// is valid date
+
+			return '';
+		}
+
+		return formatTooltipDate(date, isAmPm, timeRange, unitKey);
+	};
+
+	if (active && payload?.length) {
+		const date = new Date(payload[0].payload.key);
+		const label = getDateTitle(date, isAmPm, timeRange, unitKey);
+
+		return (
+			<div className={CLASSNAME}>
+				<div className={`${CLASSNAME}-header`}>
+					<div
+						className={`${CLASSNAME}-content ${CLASSNAME}-column font-weight-semibold`}
+					>
+						{label}
+					</div>
+				</div>
+				<div className={`${CLASSNAME}-body`}>
+					<div className={`${CLASSNAME}-row`}>
+						<div
+							className={`${CLASSNAME}-content ${CLASSNAME}-column font-weight-normal`}
+						>
+							{payload[0].value} {unit}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	return null;
+};
+
+VelocityChart.Tooltip = CustomTooltip;
 
 export default VelocityChart;
