83bd71927ef99f141af480f59ee4d63347f6859f
Cheryl Tang <cheryl.tang@liferay.com>
2021-06-28 13:00:34 -0700

    LPS-131086 Create AssetTagCTDisplayRenderer

---
+++ modules/apps/asset/asset-tags-admin-web/src/main/java/com/liferay/asset/tags/admin/web/internal/change/tracking/spi/display/AssetTagCTDisplayRenderer.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.admin.web.internal.change.tracking.spi.display;
+
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
+import com.liferay.asset.tags.constants.AssetTagsAdminPortletKeys;
+import com.liferay.change.tracking.spi.display.BaseCTDisplayRenderer;
+import com.liferay.change.tracking.spi.display.CTDisplayRenderer;
+import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cheryl Tang
+ */
+@Component(immediate = true, service = CTDisplayRenderer.class)
+public class AssetTagCTDisplayRenderer extends BaseCTDisplayRenderer<AssetTag> {
+
+	@Override
+	public String getEditURL(
+			HttpServletRequest httpServletRequest, AssetTag assetTag)
+		throws Exception {
+
+		Group group = _groupLocalService.getGroup(assetTag.getGroupId());
+
+		if (group.isCompany()) {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)httpServletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			group = themeDisplay.getScopeGroup();
+		}
+
+		return PortletURLBuilder.create(
+			_portal.getControlPanelPortletURL(
+				httpServletRequest, group,
+				AssetTagsAdminPortletKeys.ASSET_TAGS_ADMIN, 0, 0,
+				PortletRequest.RENDER_PHASE)
+		).setMVCPath(
+			"/edit_tag.jsp"
+		).setRedirect(
+			_portal.getCurrentURL(httpServletRequest)
+		).setParameter(
+			"tagId", assetTag.getTagId()
+		).buildString();
+	}
+
+	@Override
+	public Class<AssetTag> getModelClass() {
+		return AssetTag.class;
+	}
+
+	@Override
+	public String getTitle(Locale locale, AssetTag assetTag) {
+		return assetTag.getName();
+	}
+
+	@Override
+	protected void buildDisplay(DisplayBuilder<AssetTag> displayBuilder) {
+		AssetTag assetTag = displayBuilder.getModel();
+
+		displayBuilder.display(
+			"name", assetTag.getName()
+		).display(
+			"usages",
+			() -> _assetTagLocalService.getTagsSize(
+				assetTag.getGroupId(), assetTag.getName())
+		);
+	}
+
+	@Reference
+	private AssetTagLocalService _assetTagLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
