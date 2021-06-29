ef2d95fbf3bfb1b83343bc68f90c863ecbc8607f
Hugo Huijser <hugo.huijser@liferay.com>
2021-06-29 12:53:06 -0700

    LPS-105380 Improve VariableNameCheck

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/VariableNameCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/VariableNameCheck.java
@@ -654,14 +654,34 @@ public class VariableNameCheck extends BaseCheck {
 			return;
 		}
 
+		String lowerCaseTrimmedTypeName = StringUtil.toLowerCase(
+			trimmedTypeName);
+
 		if (SourceUtil.hasTypo(
 				StringUtil.toLowerCase(trimmedName),
-				StringUtil.toLowerCase(trimmedTypeName))) {
+				lowerCaseTrimmedTypeName)) {
 
 			log(
 				detailAST, _MSG_TYPO_VARIABLE, variableName,
 				_getExpectedVariableName(
 					typeName, leadingUnderline, nameTrailingDigits));
+		}
+
+		Matcher matcher = _camelCaseNamePattern.matcher(trimmedName);
+
+		while (matcher.find()) {
+			int x = matcher.start() + 1;
+
+			if (SourceUtil.hasTypo(
+					StringUtil.toLowerCase(trimmedName.substring(x)),
+					lowerCaseTrimmedTypeName)) {
+
+				log(
+					detailAST, _MSG_TYPO_VARIABLE, variableName,
+					StringBundler.concat(
+						leadingUnderline, variableName.substring(0, x),
+						typeName, nameTrailingDigits));
+			}
 		}
 	}
 
@@ -819,6 +839,8 @@ public class VariableNameCheck extends BaseCheck {
 
 	private static final String _MSG_TYPO_VARIABLE = "variable.typo";
 
+	private static final Pattern _camelCaseNamePattern = Pattern.compile(
+		"[a-z][A-Z](?=[a-z])");
 	private static final Pattern _classNameVariableNamePattern =
 		Pattern.compile("^_(.+)_CLASS_NAME$");
 	private static final Pattern _countVariableNamePattern = Pattern.compile(
