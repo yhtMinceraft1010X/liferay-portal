4706146c4934f61e5d9dced7a3e5edfd93cb1606
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-06-29 11:28:29 -0300

    LPS-134159 Add ProcessVersionResource's integration test

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/ProcessVersionResourceTest.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.rest.resource.v1_0.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.search.test.util.IdempotentRetryAssert;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.workflow.metrics.rest.client.dto.v1_0.Process;
+import com.liferay.portal.workflow.metrics.rest.client.dto.v1_0.ProcessVersion;
+import com.liferay.portal.workflow.metrics.rest.client.pagination.Page;
+import com.liferay.portal.workflow.metrics.rest.client.serdes.v1_0.ProcessVersionSerDes;
+import com.liferay.portal.workflow.metrics.rest.resource.v1_0.test.helper.WorkflowMetricsRESTTestHelper;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Feliphe Marinho
+ */
+@DataGuard(scope = DataGuard.Scope.METHOD)
+@RunWith(Arquillian.class)
+public class ProcessVersionResourceTest
+	extends BaseProcessVersionResourceTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_process = _workflowMetricsRESTTestHelper.addProcess(
+			testGroup.getCompanyId());
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+
+		_workflowMetricsRESTTestHelper.deleteProcess(
+			testGroup.getCompanyId(), _process);
+	}
+
+	@Override
+	@Test
+	public void testGetProcessVersionsPage() throws Exception {
+		Page<ProcessVersion> page =
+			processVersionResource.getProcessVersionsPage(_process.getId());
+
+		assertEqualsIgnoringOrder(
+			Arrays.asList(
+				new ProcessVersion() {
+					{
+						name = "1.0";
+					}
+				}),
+			(List<ProcessVersion>)page.getItems());
+
+		_workflowMetricsRESTTestHelper.updateProcess(
+			testGroup.getCompanyId(), _process.getId(), "2.0");
+
+		IdempotentRetryAssert.retryAssert(
+			3, TimeUnit.SECONDS,
+			() -> {
+				Page<ProcessVersion> processVersionsPage =
+					processVersionResource.getProcessVersionsPage(
+						_process.getId());
+
+				assertEqualsIgnoringOrder(
+					Arrays.asList(
+						new ProcessVersion() {
+							{
+								name = "1.0";
+							}
+						},
+						new ProcessVersion() {
+							{
+								name = "2.0";
+							}
+						}),
+					(List<ProcessVersion>)processVersionsPage.getItems());
+
+				return null;
+			});
+	}
+
+	@Override
+	@Test
+	public void testGraphQLGetProcessVersionsPage() throws Exception {
+		BaseProcessVersionResourceTestCase.GraphQLField graphQLField =
+			new BaseProcessVersionResourceTestCase.GraphQLField(
+				"processVersions",
+				HashMapBuilder.<String, Object>put(
+					"processId", _process.getId()
+				).build(),
+				new BaseProcessVersionResourceTestCase.GraphQLField(
+					"items", getGraphQLFields()));
+
+		JSONObject processVersionsJSONObject = JSONUtil.getValueAsJSONObject(
+			invokeGraphQLQuery(graphQLField), "JSONObject/data",
+			"JSONObject/processVersions");
+
+		assertEqualsIgnoringOrder(
+			Arrays.asList(
+				new ProcessVersion() {
+					{
+						name = "1.0";
+					}
+				}),
+			Arrays.asList(
+				ProcessVersionSerDes.toDTOs(
+					processVersionsJSONObject.getString("items"))));
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"name"};
+	}
+
+	private Process _process;
+
+	@Inject
+	private WorkflowMetricsRESTTestHelper _workflowMetricsRESTTestHelper;
+
+}
