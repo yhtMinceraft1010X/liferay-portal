913288bb4d6dd9071167a223de3b848c0f92a98e
Brian Chan <brian.chan@liferay.com>
2021-06-29 14:15:13 -0700

    LPS-134168 copy and rename methods

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
@@ -45,7 +45,180 @@ public class ObjectDefinitionLocalServiceTest {
 		new LiferayIntegrationTestRule();
 
 	@Test
-	public void testAddObjectDefinition() throws Exception {
+	public void testAddCustomObjectDefinition() throws Exception {
+
+		// Name is null
+
+		try {
+			_testAddSystemObjectDefinition("");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"Name is null", objectDefinitionNameException.getMessage());
+		}
+
+		// System object definition names must not start with "C_"
+
+		try {
+			_testAddSystemObjectDefinition("C_Test");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"System object definition names must not start with \"C_\"",
+				objectDefinitionNameException.getMessage());
+		}
+
+		try {
+			_testAddSystemObjectDefinition("c_Test");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"System object definition names must not start with \"C_\"",
+				objectDefinitionNameException.getMessage());
+		}
+
+		// Custom object definition names are automatically prepended with
+		// with "C_"
+
+		try {
+			_testAddCustomObjectDefinition("Test");
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			throw objectDefinitionNameException;
+		}
+
+		// Name must only contain letters and digits
+
+		_testAddSystemObjectDefinition(" Test ");
+
+		try {
+			_testAddSystemObjectDefinition("Tes t");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"Name must only contain letters and digits",
+				objectDefinitionNameException.getMessage());
+		}
+
+		try {
+			_testAddSystemObjectDefinition("Tes-t");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"Name must only contain letters and digits",
+				objectDefinitionNameException.getMessage());
+		}
+
+		_testAddCustomObjectDefinition(" Test ");
+
+		try {
+			_testAddCustomObjectDefinition("Tes t");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"Name must only contain letters and digits",
+				objectDefinitionNameException.getMessage());
+		}
+
+		try {
+			_testAddCustomObjectDefinition("Tes-t");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"Name must only contain letters and digits",
+				objectDefinitionNameException.getMessage());
+		}
+
+		// The first character of a name must be an upper case letter
+
+		try {
+			_testAddSystemObjectDefinition("test");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"The first character of a name must be an upper case letter",
+				objectDefinitionNameException.getMessage());
+		}
+
+		try {
+			_testAddCustomObjectDefinition("test");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"The first character of a name must be an upper case letter",
+				objectDefinitionNameException.getMessage());
+		}
+
+		// Names must be less than 41 characters
+
+		_testAddSystemObjectDefinition(
+			"A123456789a123456789a123456789a1234567891");
+
+		try {
+			_testAddSystemObjectDefinition(
+				"A123456789a123456789a123456789a12345678912");
+
+			Assert.fail();
+		}
+		catch (ObjectDefinitionNameException objectDefinitionNameException) {
+			Assert.assertEquals(
+				"Names must be less than 41 characters",
+				objectDefinitionNameException.getMessage());
+		}
+
+		// Duplicate name
+
+		ObjectDefinitionLocalServiceUtil.addSystemObjectDefinition(
+			TestPropsValues.getUserId(), "Test", 1,
+			Collections.<ObjectField>emptyList());
+
+		try {
+			_testAddSystemObjectDefinition("Test");
+		}
+		catch (DuplicateObjectDefinitionException
+					duplicateObjectDefinitionException) {
+
+			Assert.assertEquals(
+				"Duplicate name Test",
+				duplicateObjectDefinitionException.getMessage());
+		}
+
+		ObjectDefinitionLocalServiceUtil.addCustomObjectDefinition(
+			TestPropsValues.getUserId(), "Test",
+			Collections.<ObjectField>emptyList());
+
+		try {
+			_testAddCustomObjectDefinition("Test");
+		}
+		catch (DuplicateObjectDefinitionException
+					duplicateObjectDefinitionException) {
+
+			Assert.assertEquals(
+				"Duplicate name C_Test",
+				duplicateObjectDefinitionException.getMessage());
+		}
+	}
+
+	@Test
+	public void testAddSystemObjectDefinition() throws Exception {
 
 		// Name is null
 
