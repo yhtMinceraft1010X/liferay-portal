21e4b7b58bdd77a186362a48867fa5847de0c314
tina.tian <tina.tian@liferay.com>
2021-06-28 17:58:22 -0700

    LPS-134874 Simplify logic, retrieving service directly from OSGi framework rather than PortalBeanLocatorUtil/PortletBeanLocatorUtil as all services generated by service builder can be retrieved from OSGi framework.

--- portal-impl/src/com/liferay/portal/template/ServiceLocator.java
+++ portal-impl/src/com/liferay/portal/template/ServiceLocator.java
@@ -14,11 +14,6 @@
 
 package com.liferay.portal.template;
 
-import com.liferay.portal.bean.BeanLocatorImpl;
-import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
-import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 
@@ -34,51 +29,17 @@ public class ServiceLocator {
 	}
 
 	public Object findService(String serviceName) {
-		Object bean = null;
+		Registry registry = RegistryUtil.getRegistry();
 
-		try {
-			Registry registry = RegistryUtil.getRegistry();
-
-			bean = registry.callService(serviceName, Function.identity());
-
-			if (bean == null) {
-				bean = PortalBeanLocatorUtil.locate(
-					_getServiceName(serviceName));
-			}
-		}
-		catch (Exception exception) {
-			_log.error(exception, exception);
-		}
-
-		return bean;
+		return registry.callService(serviceName, Function.identity());
 	}
 
 	public Object findService(String servletContextName, String serviceName) {
-		Object bean = null;
-
-		try {
-			bean = PortletBeanLocatorUtil.locate(
-				servletContextName, _getServiceName(serviceName));
-		}
-		catch (Exception exception) {
-			_log.error(exception, exception);
-		}
-
-		return bean;
+		return findService(serviceName);
 	}
 
 	private ServiceLocator() {
 	}
-
-	private String _getServiceName(String serviceName) {
-		if (!serviceName.endsWith(BeanLocatorImpl.VELOCITY_SUFFIX)) {
-			serviceName += BeanLocatorImpl.VELOCITY_SUFFIX;
-		}
-
-		return serviceName;
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(ServiceLocator.class);
 
 	private static final ServiceLocator _serviceLocator = new ServiceLocator();
 
