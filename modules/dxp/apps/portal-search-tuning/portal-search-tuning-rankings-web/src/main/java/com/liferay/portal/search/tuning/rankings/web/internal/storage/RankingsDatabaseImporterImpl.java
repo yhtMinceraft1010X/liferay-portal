5ed864b4cc41f3adf405042fa7b84a959c32efea
Bryan Engler <bryan.engler@liferay.com>
2021-06-30 12:07:56 -0700

    LPS-133587 portal-search-tuning-rankings-web: Add RankingsDatabaseImporterImpl so that any existing data in the ranking indexes can be copied to the database
    https://issues.liferay.com/browse/LPS-133587

---
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/storage/RankingsDatabaseImporterImpl.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.search.tuning.rankings.web.internal.storage;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.hits.SearchHit;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.tuning.rankings.storage.RankingsDatabaseImporter;
+import com.liferay.portal.search.tuning.rankings.web.internal.index.DocumentToRankingTranslator;
+import com.liferay.portal.search.tuning.rankings.web.internal.index.Ranking;
+import com.liferay.portal.search.tuning.rankings.web.internal.index.RankingIndexReindexer;
+import com.liferay.portal.search.tuning.rankings.web.internal.index.name.RankingIndexName;
+import com.liferay.portal.search.tuning.rankings.web.internal.index.name.RankingIndexNameBuilder;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Bryan Engler
+ */
+@Component(service = RankingsDatabaseImporter.class)
+public class RankingsDatabaseImporterImpl implements RankingsDatabaseImporter {
+
+	@Override
+	public void populateDatabase(long companyId) {
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
+
+		RankingIndexName rankingIndexName =
+			rankingIndexNameBuilder.getRankingIndexName(companyId);
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Importing documents from " + rankingIndexName.getIndexName());
+		}
+
+		searchSearchRequest.setIndexNames(rankingIndexName.getIndexName());
+
+		searchSearchRequest.setFetchSource(true);
+		searchSearchRequest.setQuery(queries.matchAll());
+
+		SearchSearchResponse searchSearchResponse = searchEngineAdapter.execute(
+			searchSearchRequest);
+
+		SearchHits searchHits = searchSearchResponse.getSearchHits();
+
+		List<SearchHit> searchHitsList = searchHits.getSearchHits();
+
+		for (SearchHit searchHit : searchHitsList) {
+			Ranking ranking = documentToRankingTranslator.translate(
+				searchHit.getDocument(), searchHit.getId());
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Adding database entry for document with id_ " +
+						ranking.getRankingDocumentId());
+			}
+
+			rankingJSONStorageHelper.addJSONStorageEntry(
+				companyId, ranking.getAliases(), ranking.getHiddenDocumentIds(),
+				ranking.isInactive(), ranking.getIndexName(), ranking.getName(),
+				ranking.getPins(), ranking.getQueryString());
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Reindexing " + rankingIndexName.getIndexName());
+		}
+
+		try {
+			rankingIndexReindexer.reindex(new long[] {companyId});
+		}
+		catch (Exception exception) {
+			_log.error("Unable to reindex " + rankingIndexName.getIndexName());
+		}
+	}
+
+	@Reference
+	protected DocumentToRankingTranslator documentToRankingTranslator;
+
+	@Reference
+	protected Queries queries;
+
+	@Reference
+	protected RankingIndexNameBuilder rankingIndexNameBuilder;
+
+	@Reference
+	protected RankingIndexReindexer rankingIndexReindexer;
+
+	@Reference
+	protected RankingJSONStorageHelper rankingJSONStorageHelper;
+
+	@Reference
+	protected SearchEngineAdapter searchEngineAdapter;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		RankingsDatabaseImporterImpl.class);
+
+}
