3e40c9b71c5d14406f1e1d18fa379df74ad94201
Riccardo Alberti <riccardo.alberti@liferay.com>
2021-07-01 18:47:24 +0200

    COMMERCE-5669 Restore CPInstance pricing related classes

---
+++ modules/apps/commerce/commerce-pricing-web/src/main/java/com/liferay/commerce/pricing/web/internal/model/InstancePriceEntry.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.pricing.web.internal.model;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class InstancePriceEntry {
+
+	public InstancePriceEntry(
+		long priceEntryId, String createDate, String name, String unitPrice) {
+
+		_priceEntryId = priceEntryId;
+		_createDate = createDate;
+		_name = name;
+		_unitPrice = unitPrice;
+	}
+
+	public String getCreateDate() {
+		return _createDate;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public long getPriceEntryId() {
+		return _priceEntryId;
+	}
+
+	public String getUnitPrice() {
+		return _unitPrice;
+	}
+
+	private final String _createDate;
+	private final String _name;
+	private final long _priceEntryId;
+	private final String _unitPrice;
+
+}
