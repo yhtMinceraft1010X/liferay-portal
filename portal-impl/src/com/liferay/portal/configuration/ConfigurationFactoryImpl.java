c42c1e77e4c813691392aabe71d820941c7c14d4
shuyangzhou <shuyang.zhou@liferay.com>
2021-07-01 23:28:25 -0700

    LPS-135438 Fix portal-ext.properties loading

--- portal-impl/src/com/liferay/portal/configuration/ConfigurationFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/configuration/ConfigurationFactoryImpl.java
@@ -17,17 +17,32 @@ package com.liferay.portal.configuration;
 import com.liferay.portal.kernel.configuration.Configuration;
 import com.liferay.portal.kernel.configuration.ConfigurationFactory;
 import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.util.AggregateClassLoader;
 import com.liferay.portal.util.PropsFiles;
+import com.liferay.portal.util.PropsUtil;
 
 /**
  * @author Brian Wing Shun Chan
  */
 public class ConfigurationFactoryImpl implements ConfigurationFactory {
 
-	public static final Configuration CONFIGURATION_PORTAL =
-		new ConfigurationImpl(
-			ConfigurationFactoryImpl.class.getClassLoader(), PropsFiles.PORTAL,
-			CompanyConstants.SYSTEM, null);
+	public static final Configuration CONFIGURATION_PORTAL;
+
+	static {
+		ClassLoader classLoader = PropsUtil.class.getClassLoader();
+
+		Class<?> clazz = classLoader.getClass();
+
+		ClassLoader classLoaderClassLoader = clazz.getClassLoader();
+
+		if (classLoaderClassLoader != null) {
+			classLoader = AggregateClassLoader.getAggregateClassLoader(
+				classLoader, classLoaderClassLoader);
+		}
+
+		CONFIGURATION_PORTAL = new ConfigurationImpl(
+			classLoader, PropsFiles.PORTAL, CompanyConstants.SYSTEM, null);
+	}
 
 	@Override
 	public Configuration getConfiguration(
