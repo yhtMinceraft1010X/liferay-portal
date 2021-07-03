99d2d65d0e0e95ca17c6348a3b854f414a3da943
Erick Monteiro <erickhenriquehmc@gmail.com>
2021-07-03 00:51:25 -0300

    LPS-121375 (user-associated-data-web): Replace configurationAdmin usage to configurationProvider

--- modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADAnonymizerHelper.java
+++ modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADAnonymizerHelper.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -39,7 +40,6 @@ import java.util.Locale;
 import java.util.Optional;
 
 import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -144,12 +144,8 @@ public class UADAnonymizerHelper {
 		if (!configurationOptional.isPresent()) {
 			User anonymousUser = _createAnonymousUser(companyId);
 
-			Configuration configuration =
-				_configurationAdmin.createFactoryConfiguration(
-					AnonymousUserConfiguration.class.getName(),
-					StringPool.QUESTION);
-
-			configuration.update(
+			_configurationProvider.saveCompanyConfiguration(
+				AnonymousUserConfiguration.class, companyId,
 				HashMapDictionaryBuilder.<String, Object>put(
 					"companyId", companyId
 				).put(
@@ -194,7 +190,7 @@ public class UADAnonymizerHelper {
 	private CompanyLocalService _companyLocalService;
 
 	@Reference
-	private ConfigurationAdmin _configurationAdmin;
+	private ConfigurationProvider _configurationProvider;
 
 	@Reference
 	private CounterLocalService _counterLocalService;
