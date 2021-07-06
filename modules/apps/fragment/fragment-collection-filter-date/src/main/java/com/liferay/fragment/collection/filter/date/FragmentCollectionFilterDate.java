4d25f0dcae383d9ccee8c4c0c76da96c3ff33c44
Pablo Molina <contact@pablomolina.me>
2021-07-06 14:08:57 +0200

    LPS-135067 Add fragment-collection-filter-date

---
+++ modules/apps/fragment/fragment-collection-filter-date/src/main/java/com/liferay/fragment/collection/filter/date/FragmentCollectionFilterDate.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.collection.filter.date;
+
+import com.liferay.fragment.collection.filter.FragmentCollectionFilter;
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pablo Molina
+ */
+@Component(immediate = true, service = FragmentCollectionFilter.class)
+public class FragmentCollectionFilterDate implements FragmentCollectionFilter {
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "date");
+	}
+
+}
