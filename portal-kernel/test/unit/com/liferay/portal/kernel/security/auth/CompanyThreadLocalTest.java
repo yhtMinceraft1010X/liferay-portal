b89554a3cfe965b23a7e765c2ede4c0d900f5fef
Alberto Chaparro <alberto.chaparro@liferay.com>
2021-07-06 11:48:56 +0200

    LPS-135081 Adding unit tests

---
+++ portal-kernel/test/unit/com/liferay/portal/kernel/security/auth/CompanyThreadLocalTest.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.security.auth;
+
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
+import com.liferay.portal.kernel.test.rule.NewEnvTestRule;
+import com.liferay.portal.kernel.util.ServiceProxyFactory;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Alberto Chaparro
+ */
+@PrepareForTest(ServiceProxyFactory.class)
+@RunWith(PowerMockRunner.class)
+public class CompanyThreadLocalTest extends PowerMockito {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			CodeCoverageAssertor.INSTANCE, NewEnvTestRule.INSTANCE);
+
+	@Before
+	public void setUp() {
+		mockStatic(ServiceProxyFactory.class);
+
+		when(
+			ServiceProxyFactory.newServiceTrackedInstance(
+				Mockito.anyObject(), Mockito.anyObject(), Mockito.anyString(),
+				Mockito.anyBoolean(), Mockito.anyBoolean())
+		).thenReturn(
+			null
+		);
+	}
+
+	@Test
+	public void testLock() {
+		SafeCloseable safeCloseable = CompanyThreadLocal.lock(
+			CompanyConstants.SYSTEM);
+
+		try {
+			CompanyThreadLocal.setCompanyId(1L);
+		}
+		catch (UnsupportedOperationException unsupportedOperationException) {
+			return;
+		}
+		finally {
+			safeCloseable.close();
+		}
+
+		Assert.fail("Should throw UnsupportedOperationException");
+	}
+
+	@Test
+	public void testLockWithSafeCloseable() {
+		SafeCloseable safeCloseable = CompanyThreadLocal.lock(
+			CompanyConstants.SYSTEM);
+
+		try {
+			CompanyThreadLocal.setWithSafeCloseable(1L);
+		}
+		catch (UnsupportedOperationException unsupportedOperationException) {
+			return;
+		}
+		finally {
+			safeCloseable.close();
+		}
+
+		Assert.fail("Should throw UnsupportedOperationException");
+	}
+
+}
