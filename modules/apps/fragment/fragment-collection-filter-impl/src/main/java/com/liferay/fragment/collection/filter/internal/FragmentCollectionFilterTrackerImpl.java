f5fc4aa466347f2c88241805034aa4daf62b52cc
Pablo Molina <contact@pablomolina.me>
2021-07-06 12:59:43 +0200

    LPS-135067 Add fragment-collection-filter-impl

---
+++ modules/apps/fragment/fragment-collection-filter-impl/src/main/java/com/liferay/fragment/collection/filter/internal/FragmentCollectionFilterTrackerImpl.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.collection.filter.internal;
+
+import com.liferay.fragment.collection.filter.FragmentCollectionFilter;
+import com.liferay.fragment.collection.filter.FragmentCollectionFilterTracker;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Pablo Molina
+ */
+@Component(immediate = true, service = FragmentCollectionFilterTracker.class)
+public class FragmentCollectionFilterTrackerImpl
+	implements FragmentCollectionFilterTracker {
+
+	@Override
+	public List<FragmentCollectionFilter> getFragmentCollectionFilters() {
+		return new ArrayList<>(_serviceTrackerMap.values());
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, FragmentCollectionFilter.class, null);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private ServiceTrackerMap<String, FragmentCollectionFilter>
+		_serviceTrackerMap;
+
+}
