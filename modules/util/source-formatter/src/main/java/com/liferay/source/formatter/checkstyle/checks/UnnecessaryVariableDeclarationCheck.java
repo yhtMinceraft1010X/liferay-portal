d016305e82852ef02dc4d39edde7fdb85d21b6f6
Alan Huang <alan.huang@liferay.com>
2021-07-07 19:52:52 +0800

    LPS-105380 Check Unnecessary list variable declaration before return

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/UnnecessaryVariableDeclarationCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/UnnecessaryVariableDeclarationCheck.java
@@ -15,9 +15,11 @@
 package com.liferay.source.formatter.checkstyle.checks;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FullIdent;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Hugo Huijser
@@ -50,6 +52,10 @@ public class UnnecessaryVariableDeclarationCheck
 		}
 
 		String variableName = nameDetailAST.getText();
+
+		_checkUnnecessaryListVariableDeclarationBeforeReturn(
+			detailAST, semiDetailAST, variableName,
+			_MSG_UNNECESSARY_LIST_VARIABLE_DECLARATION_BEFORE_RETURN);
 
 		checkUnnecessaryStatementBeforeReturn(
 			detailAST, semiDetailAST, variableName,
@@ -86,6 +92,139 @@ public class UnnecessaryVariableDeclarationCheck
 			secondVariableCallerDetailAST, parentDetailAST, variableName,
 			_MSG_UNNECESSARY_VARIABLE_DECLARATION_BEFORE_REASSIGN);
 	}
+
+	private void _checkUnnecessaryListVariableDeclarationBeforeReturn(
+		DetailAST detailAST, DetailAST semiDetailAST, String variableName,
+		String messageKey) {
+
+		String variableTypeName = getVariableTypeName(
+			detailAST, variableName, false);
+
+		if (!variableTypeName.equals("List") ||
+			!_isAssignNewArrayList(detailAST)) {
+
+			return;
+		}
+
+		DetailAST nextSiblingDetailAST = semiDetailAST.getNextSibling();
+
+		if (nextSiblingDetailAST == null) {
+			return;
+		}
+
+		while (nextSiblingDetailAST.getType() == TokenTypes.EXPR) {
+			DetailAST firstChildDetailAST =
+				nextSiblingDetailAST.getFirstChild();
+
+			if (firstChildDetailAST.getType() != TokenTypes.METHOD_CALL) {
+				return;
+			}
+
+			firstChildDetailAST = firstChildDetailAST.getFirstChild();
+
+			if (firstChildDetailAST.getType() != TokenTypes.DOT) {
+				return;
+			}
+
+			FullIdent fullIdent = FullIdent.createFullIdent(
+				firstChildDetailAST);
+
+			String fullyQualifiedName = fullIdent.getText();
+
+			if (!fullyQualifiedName.equals(variableName + ".add")) {
+				return;
+			}
+
+			nextSiblingDetailAST = nextSiblingDetailAST.getNextSibling();
+
+			if ((nextSiblingDetailAST != null) &&
+				(nextSiblingDetailAST.getType() != TokenTypes.SEMI)) {
+
+				return;
+			}
+
+			nextSiblingDetailAST = nextSiblingDetailAST.getNextSibling();
+
+			if (nextSiblingDetailAST == null) {
+				return;
+			}
+
+			if (nextSiblingDetailAST.getType() == TokenTypes.EXPR) {
+				continue;
+			}
+
+			if (nextSiblingDetailAST.getType() == TokenTypes.LITERAL_RETURN) {
+				firstChildDetailAST = nextSiblingDetailAST.getFirstChild();
+
+				if (firstChildDetailAST.getType() != TokenTypes.EXPR) {
+					return;
+				}
+
+				firstChildDetailAST = firstChildDetailAST.getFirstChild();
+
+				if ((firstChildDetailAST.getType() == TokenTypes.IDENT) &&
+					variableName.equals(firstChildDetailAST.getText())) {
+
+					log(detailAST, messageKey, variableName);
+				}
+			}
+
+			return;
+		}
+	}
+
+	private boolean _isAssignNewArrayList(DetailAST detailAST) {
+		DetailAST assignDetailAST = detailAST.findFirstToken(TokenTypes.ASSIGN);
+
+		if (assignDetailAST == null) {
+			return false;
+		}
+
+		DetailAST firstChildDetailAST = assignDetailAST.getFirstChild();
+
+		if (firstChildDetailAST.getType() != TokenTypes.EXPR) {
+			return false;
+		}
+
+		firstChildDetailAST = firstChildDetailAST.getFirstChild();
+
+		if (firstChildDetailAST.getType() != TokenTypes.LITERAL_NEW) {
+			return false;
+		}
+
+		DetailAST identDetailAST = firstChildDetailAST.getFirstChild();
+
+		if ((identDetailAST.getType() != TokenTypes.IDENT) ||
+			!Objects.equals(identDetailAST.getText(), "ArrayList")) {
+
+			return false;
+		}
+
+		DetailAST elistDetailAST = firstChildDetailAST.findFirstToken(
+			TokenTypes.ELIST);
+
+		if (elistDetailAST == null) {
+			return false;
+		}
+
+		firstChildDetailAST = elistDetailAST.getFirstChild();
+
+		if (firstChildDetailAST == null) {
+			return true;
+		}
+
+		firstChildDetailAST = firstChildDetailAST.getFirstChild();
+
+		if (firstChildDetailAST.getType() == TokenTypes.NUM_INT) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final String
+		_MSG_UNNECESSARY_LIST_VARIABLE_DECLARATION_BEFORE_RETURN =
+			"list.variable.declaration.unnecessary.before.return";
 
 	private static final String
 		_MSG_UNNECESSARY_VARIABLE_DECLARATION_BEFORE_REASSIGN =
