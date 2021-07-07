6106d8db6bcdad8a107f3e250727b64408473e93
Brian Chan <brian.chan@liferay.com>
2021-07-06 20:58:28 -0700

    LPS-134094 SF

--- modules/util/portal-tools-service-builder-test-service/src/main/java/com/liferay/portal/tools/service/builder/test/model/impl/CacheMissEntryImpl.java
+++ modules/util/portal-tools-service-builder-test-service/src/main/java/com/liferay/portal/tools/service/builder/test/model/impl/CacheMissEntryImpl.java
@@ -15,21 +15,10 @@
 package com.liferay.portal.tools.service.builder.test.model.impl;
 
 /**
- * The extended model implementation for the CacheMissEntry service. Represents a row in the &quot;CacheMissEntry&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.portal.tools.service.builder.test.model.CacheMissEntry</code> interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 public class CacheMissEntryImpl extends CacheMissEntryBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a cache miss entry model instance should use the {@link com.liferay.portal.tools.service.builder.test.model.CacheMissEntry} interface instead.
-	 */
 	public CacheMissEntryImpl() {
 	}
 
--- modules/util/portal-tools-service-builder-test-service/src/main/java/com/liferay/portal/tools/service/builder/test/service/impl/CacheMissEntryLocalServiceImpl.java
+++ modules/util/portal-tools-service-builder-test-service/src/main/java/com/liferay/portal/tools/service/builder/test/service/impl/CacheMissEntryLocalServiceImpl.java
@@ -17,25 +17,8 @@ package com.liferay.portal.tools.service.builder.test.service.impl;
 import com.liferay.portal.tools.service.builder.test.service.base.CacheMissEntryLocalServiceBaseImpl;
 
 /**
- * The implementation of the cache miss entry local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.portal.tools.service.builder.test.service.CacheMissEntryLocalService</code> interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see CacheMissEntryLocalServiceBaseImpl
  */
 public class CacheMissEntryLocalServiceImpl
 	extends CacheMissEntryLocalServiceBaseImpl {
-
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Use <code>com.liferay.portal.tools.service.builder.test.service.CacheMissEntryLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.portal.tools.service.builder.test.service.CacheMissEntryLocalServiceUtil</code>.
-	 */
-
 }
