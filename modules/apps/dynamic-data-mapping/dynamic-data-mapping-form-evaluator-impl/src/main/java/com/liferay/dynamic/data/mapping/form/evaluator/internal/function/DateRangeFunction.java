154fa06854f57ecab408f034edb9a4061ac44ce8
Carolina Barbosa <carolina.barbosa@liferay.com>
2021-07-08 13:40:50 -0300

    LPS-134235 Create new Function to validate range between Start Date and End Date

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/DateRangeFunction.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.evaluator.internal.function;
+
+import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
+
+/**
+ * @author Carolina Barbosa
+ */
+public class DateRangeFunction
+	implements DDMExpressionFunction.Function2<Object, Object, Boolean> {
+
+	public static final String NAME = "dateRange";
+
+	@Override
+	public Boolean apply(Object object1, Object object2) {
+		if (_futureDatesFunction.apply(object1, object2) &&
+			_pastDatesFunction.apply(object1, object2)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	private final FutureDatesFunction _futureDatesFunction =
+		new FutureDatesFunction();
+	private final PastDatesFunction _pastDatesFunction =
+		new PastDatesFunction();
+
+}
