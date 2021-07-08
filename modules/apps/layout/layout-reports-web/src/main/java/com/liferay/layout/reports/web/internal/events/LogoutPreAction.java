e80fbde1d4a1bb791c2a279154c7332d822a096d
Yurena Cabrera <yurena.cabrera@liferay.com>
2021-07-08 10:29:59 +0100

    LPS-132406 Create LogoutPreAction hook So the layoutReportsPanelState will be set to closed when the user logged out

---
+++ modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/events/LogoutPreAction.java
@@ -0,0 +1,30 @@
+package com.liferay.layout.reports.web.internal.events;
+
+import com.liferay.portal.kernel.events.Action;
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.events.LifecycleAction;
+import com.liferay.portal.kernel.util.SessionClicks;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Yurena Cabrera
+ */
+
+@Component(
+	enabled = false, immediate = true, property = "key=logout.events.pre",
+	service = LifecycleAction.class
+)
+public class LogoutPreAction extends Action {
+
+	@Override
+	public void run(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) throws ActionException {
+
+		SessionClicks.put(httpServletRequest, "com.liferay.layout.reports.web_layoutReportsPanelState", "closed");
+
+	}
+}
