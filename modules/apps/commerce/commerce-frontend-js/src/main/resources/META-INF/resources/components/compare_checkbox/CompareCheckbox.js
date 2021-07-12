f04ab1731b97a88ca360afa8883efc514a1f2e4e
Yang Cao <yang.cao@liferay.com>
2021-07-12 08:43:56 +0800

    Revert "COMMERCE-6746 According to ClayUI docs the parameter should be readOnly"
    This reverts commit bc1358d6a690e41de13d80a3fe79a8b42d41ffdf.

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/compare_checkbox/CompareCheckbox.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/compare_checkbox/CompareCheckbox.js
@@ -72,6 +72,7 @@ function CompareCheckbox({
 			aria-label={label}
 			checked={inCompare}
 			className="compare-checkbox-component"
+			disabled={disabled && !inCompare}
 			label={label}
 			onChange={() =>
 				setInCompare((currentlyInCompare) => {
@@ -83,7 +84,6 @@ function CompareCheckbox({
 					return !currentlyInCompare;
 				})
 			}
-			readOnly={disabled && !inCompare}
 		/>
 	);
 }
