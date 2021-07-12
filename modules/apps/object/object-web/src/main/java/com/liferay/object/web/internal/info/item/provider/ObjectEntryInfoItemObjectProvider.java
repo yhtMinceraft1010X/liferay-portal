5965fd2cffc260d46e662bda43019a3b518b4c47
Guilherme Camacho <guilherme.camacho@liferay.com>
2021-07-11 23:57:20 -0300

    LPS-135243 Implement InfoItemObjectProvider for ObjectEntry

---
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/provider/ObjectEntryInfoItemObjectProvider.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.web.internal.info.item.provider;
+
+import com.liferay.info.exception.NoSuchInfoItemException;
+import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.InfoItemIdentifier;
+import com.liferay.info.item.provider.InfoItemObjectProvider;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectEntryLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Guilherme Camacho
+ */
+@Component(
+	immediate = true,
+	property = {
+		"info.item.identifier=com.liferay.info.item.ClassPKInfoItemIdentifier",
+		"service.ranking:Integer=100"
+	},
+	service = InfoItemObjectProvider.class
+)
+public class ObjectEntryInfoItemObjectProvider
+	implements InfoItemObjectProvider<ObjectEntry> {
+
+	@Override
+	public ObjectEntry getInfoItem(InfoItemIdentifier infoItemIdentifier)
+		throws NoSuchInfoItemException {
+
+		if (!(infoItemIdentifier instanceof ClassPKInfoItemIdentifier)) {
+			throw new NoSuchInfoItemException("Invalid infoItemIdentifier");
+		}
+
+		ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
+			(ClassPKInfoItemIdentifier)infoItemIdentifier;
+
+		ObjectEntry objectEntry = _objectEntryLocalService.fetchObjectEntry(
+			classPKInfoItemIdentifier.getClassPK());
+
+		if (objectEntry == null) {
+			throw new NoSuchInfoItemException("Invalid infoItemIdentifier");
+		}
+
+		return objectEntry;
+	}
+
+	@Override
+	public ObjectEntry getInfoItem(long classPK)
+		throws NoSuchInfoItemException {
+
+		ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
+			new ClassPKInfoItemIdentifier(classPK);
+
+		return getInfoItem(classPKInfoItemIdentifier);
+	}
+
+	@Reference
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+}
