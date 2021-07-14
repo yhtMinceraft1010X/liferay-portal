0a6e69083e7a479f34aee7cf04b4b63ef40c72be
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-14 08:50:24 +0200

    LPS-135561 Permissions checker

---
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/security/permissions/resource/TemplatePermission.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.web.internal.security.permissions.resource;
+
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(immediate = true, service = {})
+public class TemplatePermission {
+
+	public static void check(
+			PermissionChecker permissionChecker, long ddmTemplateId,
+			String actionId)
+		throws PortalException {
+
+		_ddmTemplateModelResourcePermission.check(
+			permissionChecker, ddmTemplateId, actionId);
+	}
+
+	public static boolean contains(
+			PermissionChecker permissionChecker, DDMTemplate ddmTemplate,
+			String actionId)
+		throws PortalException {
+
+		return _ddmTemplateModelResourcePermission.contains(
+			permissionChecker, ddmTemplate, actionId);
+	}
+
+	public static boolean contains(
+			PermissionChecker permissionChecker, long ddmTemplateId,
+			String actionId)
+		throws PortalException {
+
+		return _ddmTemplateModelResourcePermission.contains(
+			permissionChecker, ddmTemplateId, actionId);
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.dynamic.data.mapping.model.DDMTemplate)",
+		unbind = "-"
+	)
+	protected void setModelResourcePermission(
+		ModelResourcePermission<DDMTemplate> modelResourcePermission) {
+
+		_ddmTemplateModelResourcePermission = modelResourcePermission;
+	}
+
+	private static ModelResourcePermission<DDMTemplate>
+		_ddmTemplateModelResourcePermission;
+
+}
