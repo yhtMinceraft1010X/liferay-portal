3e6a850f76795e9bf43189f6c60df8378ffca951
Igor Beslic <igor.beslic@liferay.com>
2021-07-14 16:00:40 +0200

    COMMERCE-6497 batch-planner-web - portlet

--- modules/apps/batch-planner/batch-planner-web/src/main/java/com/liferay/batch/planner/web/internal/portlet/BatchPlannerPortlet.java
+++ modules/apps/batch-planner/batch-planner-web/src/main/java/com/liferay/batch/planner/web/internal/portlet/BatchPlannerPortlet.java
@@ -15,11 +15,28 @@
 package com.liferay.batch.planner.web.internal.portlet;
 
 import com.liferay.batch.planner.constants.BatchPlannerPortletKeys;
+import com.liferay.batch.planner.web.internal.display.context.SelectHeadlessEndpointDisplayContext;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.jaxrs.runtime.JaxrsServiceRuntime;
+import org.osgi.service.jaxrs.runtime.dto.ApplicationDTO;
+import org.osgi.service.jaxrs.runtime.dto.ResourceDTO;
+import org.osgi.service.jaxrs.runtime.dto.ResourceMethodInfoDTO;
+import org.osgi.service.jaxrs.runtime.dto.RuntimeDTO;
 
 /**
  * @author Igor Beslic
@@ -29,22 +46,58 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-batch-planner",
 		"com.liferay.portlet.display-category=category.hidden",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.layout-cacheable=true",
-		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.preferences-unique-per-layout=false",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
-		"com.liferay.portlet.scopeable=true",
 		"javax.portlet.display-name=Batch Planner",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + BatchPlannerPortletKeys.BATCH_PLANNER,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator,power-user,user"
 	},
 	service = Portlet.class
 )
 public class BatchPlannerPortlet extends MVCPortlet {
+
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		RuntimeDTO runtimeDTO = _jaxrsServiceRuntime.getRuntimeDTO();
+
+		Map<String, String> headlessEndpoints = new HashMap<>();
+
+		for (ApplicationDTO applicationDTO : runtimeDTO.applicationDTOs) {
+			for (ResourceDTO resourceDTO : applicationDTO.resourceDTOs) {
+				for (ResourceMethodInfoDTO resourceMethodInfoDTO :
+						resourceDTO.resourceMethods) {
+
+					String openApi = StringBundler.concat(
+						"/o", applicationDTO.base, resourceMethodInfoDTO.path);
+
+					if (!openApi.contains("openapi")) {
+						continue;
+					}
+
+					headlessEndpoints.put(
+						applicationDTO.base,
+						openApi.replaceAll("\\{.+\\}", "json"));
+				}
+			}
+		}
+
+		renderRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			new SelectHeadlessEndpointDisplayContext(headlessEndpoints));
+
+		super.render(renderRequest, renderResponse);
+	}
+
+	@Reference
+	private JaxrsServiceRuntime _jaxrsServiceRuntime;
+
 }
