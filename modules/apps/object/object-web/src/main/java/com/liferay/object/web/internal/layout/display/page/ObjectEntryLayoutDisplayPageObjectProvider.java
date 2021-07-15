7152b338c10e01e5b8ce6ea672d8a3027ba7f614
Guilherme Camacho <guilherme.camacho@liferay.com>
2021-07-15 12:52:23 -0300

    LPS-135497 Implement LayoutDisplayPageProvider for ObjectEntry in preparation for display pages

---
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/layout/display/page/ObjectEntryLayoutDisplayPageObjectProvider.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.web.internal.layout.display.page;
+
+import com.liferay.layout.display.page.LayoutDisplayPageObjectProvider;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import java.util.Locale;
+
+/**
+ * @author Guilherme Camacho
+ */
+public class ObjectEntryLayoutDisplayPageObjectProvider
+	implements LayoutDisplayPageObjectProvider<ObjectEntry> {
+
+	public ObjectEntryLayoutDisplayPageObjectProvider(
+		ObjectEntry objectEntry, ObjectDefinition objectDefinition) {
+
+		_objectEntry = objectEntry;
+		_objectDefinition = objectDefinition;
+	}
+
+	@Override
+	public long getClassNameId() {
+		return PortalUtil.getClassNameId(ObjectEntry.class.getName());
+	}
+
+	@Override
+	public long getClassPK() {
+		return _objectEntry.getObjectEntryId();
+	}
+
+	@Override
+	public long getClassTypeId() {
+		return _objectEntry.getObjectDefinitionId();
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public ObjectEntry getDisplayObject() {
+		return _objectEntry;
+	}
+
+	@Override
+	public long getGroupId() {
+		return _objectEntry.getGroupId();
+	}
+
+	@Override
+	public String getKeywords(Locale locale) {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return _objectDefinition.getName();
+	}
+
+	@Override
+	public String getURLTitle(Locale locale) {
+		return String.valueOf(_objectEntry.getObjectEntryId());
+	}
+
+	private final ObjectDefinition _objectDefinition;
+	private final ObjectEntry _objectEntry;
+
+}
---
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/layout/display/page/ObjectEntryLayoutDisplayPageProvider.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.web.internal.layout.display.page;
+
+import com.liferay.info.item.InfoItemReference;
+import com.liferay.layout.display.page.LayoutDisplayPageObjectProvider;
+import com.liferay.layout.display.page.LayoutDisplayPageProvider;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Guilherme Camacho
+ */
+@Component(immediate = true, service = LayoutDisplayPageProvider.class)
+public class ObjectEntryLayoutDisplayPageProvider
+	implements LayoutDisplayPageProvider<ObjectEntry> {
+
+	@Override
+	public String getClassName() {
+		return ObjectEntry.class.getName();
+	}
+
+	@Override
+	public LayoutDisplayPageObjectProvider<ObjectEntry>
+		getLayoutDisplayPageObjectProvider(
+			InfoItemReference infoItemReference) {
+
+		try {
+			ObjectEntry objectEntry = _objectEntryLocalService.getObjectEntry(
+				infoItemReference.getClassPK());
+
+			if ((objectEntry == null) || objectEntry.isDraft()) {
+				return null;
+			}
+
+			ObjectDefinition objectDefinition =
+				_objectDefinitionLocalService.getObjectDefinition(
+					objectEntry.getObjectDefinitionId());
+
+			return new ObjectEntryLayoutDisplayPageObjectProvider(
+				objectEntry, objectDefinition);
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
+	}
+
+	@Override
+	public LayoutDisplayPageObjectProvider<ObjectEntry>
+		getLayoutDisplayPageObjectProvider(long groupId, String urlTitle) {
+
+		return getLayoutDisplayPageObjectProvider(
+			new InfoItemReference(
+				ObjectEntry.class.getName(), Long.valueOf(urlTitle)));
+	}
+
+	@Override
+	public String getURLSeparator() {
+		return "/o/";
+	}
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+}
