8f4efed53f28e5af87071afaacd1d301dc45d139
Patrick Yeo <patrick.yeo@liferay.com>
2021-07-15 16:41:54 -0700

    LPS-132737 frontend-css-cadmin-web rename main.css to clay-admin.css so it's easier to see where styles are coming from in browser's dev tools

--- modules/apps/frontend-css/frontend-css-cadmin-web/src/main/java/com/liferay/frontend/css/cadmin/web/internal/servlet/taglib/CadminTopHeadDynamicInclude.java
+++ modules/apps/frontend-css/frontend-css-cadmin-web/src/main/java/com/liferay/frontend/css/cadmin/web/internal/servlet/taglib/CadminTopHeadDynamicInclude.java
@@ -62,7 +62,7 @@ public class CadminTopHeadDynamicInclude extends BaseDynamicInclude {
 
 		printWriter.print(
 			absolutePortalURLBuilder.forModuleStylesheet(
-				_bundleContext.getBundle(), "main.css"
+				_bundleContext.getBundle(), "clay-admin.css"
 			).build());
 
 		printWriter.println("\" id=\"liferayCadminCSS\" rel=\"stylesheet\"");
