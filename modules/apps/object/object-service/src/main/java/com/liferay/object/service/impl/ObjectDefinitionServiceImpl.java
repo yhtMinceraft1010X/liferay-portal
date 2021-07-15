9b00a0c07b5d874d6dbd90b85be43f63f752fe37
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-07-15 19:12:33 -0300

    LPS-135488 Implement remote service

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionServiceImpl.java
@@ -14,10 +14,20 @@
 
 package com.liferay.object.service.impl;
 
+import com.liferay.object.constants.ObjectsConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.base.ObjectDefinitionServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marco Leo
@@ -32,4 +42,65 @@ import org.osgi.service.component.annotations.Component;
 )
 public class ObjectDefinitionServiceImpl
 	extends ObjectDefinitionServiceBaseImpl {
+
+	@Override
+	public ObjectDefinition addCustomObjectDefinition(long userId, String name)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), null, ActionKeys.ADD_ENTRY);
+
+		return _objectDefinitionLocalService.addCustomObjectDefinition(
+			userId, name, null);
+	}
+
+	@Override
+	public ObjectDefinition deleteObjectDefinition(long objectDefinitionId)
+		throws PortalException {
+
+		_objectDefinitionModelResourcePermission.check(
+			getPermissionChecker(), objectDefinitionId, ActionKeys.DELETE);
+
+		return _objectDefinitionLocalService.deleteObjectDefinition(
+			objectDefinitionId);
+	}
+
+	@Override
+	public ObjectDefinition getObjectDefinition(long objectDefinitionId)
+		throws PortalException {
+
+		_objectDefinitionModelResourcePermission.check(
+			getPermissionChecker(), objectDefinitionId, ActionKeys.VIEW);
+
+		return _objectDefinitionLocalService.getObjectDefinition(
+			objectDefinitionId);
+	}
+
+	@Override
+	public List<ObjectDefinition> getObjectDefinitions(int start, int end) {
+		return _objectDefinitionLocalService.getObjectDefinitions(start, end);
+	}
+
+	@Override
+	public int getObjectDefinitionsCount(long companyId)
+		throws PortalException {
+
+		return _objectDefinitionLocalService.getObjectDefinitionsCount(
+			companyId);
+	}
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.object.model.ObjectDefinition)"
+	)
+	private ModelResourcePermission<ObjectDefinition>
+		_objectDefinitionModelResourcePermission;
+
+	@Reference(
+		target = "(resource.name=" + ObjectsConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
 }
