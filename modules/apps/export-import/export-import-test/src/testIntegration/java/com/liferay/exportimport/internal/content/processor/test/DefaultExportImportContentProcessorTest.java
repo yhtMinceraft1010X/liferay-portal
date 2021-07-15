e658c3f060b306af88af747a4e7afc409c9dd43b
Michael Bowerman <michael.bowerman@liferay.com>
2021-07-15 13:39:59 -0700

    LPS-135706 Update tests.

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/internal/content/processor/test/DefaultExportImportContentProcessorTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/internal/content/processor/test/DefaultExportImportContentProcessorTest.java
@@ -286,7 +286,10 @@ public class DefaultExportImportContentProcessorTest {
 			if (entry.contains(DLFileEntryConstants.getClassName())) {
 				Assert.assertTrue(
 					content,
-					content.contains("[$dl-reference=" + entry + "$]"));
+					content.contains(
+						"[$dl-reference=" + entry + "$,$include-uuid=true$]") ||
+					content.contains(
+						"[$dl-reference=" + entry + "$,$include-uuid=false$]"));
 
 				count++;
 			}
