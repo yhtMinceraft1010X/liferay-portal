a7a7fb810c7c608c5a8891eabb7eaa256bd47c33
Javier Gamarra <nhpatt@gmail.com>
2021-07-18 12:55:29 +0200

    LPS-135609 add exceptionMapper for type to validate things like ‘string’ vs ‘String’

---
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/jaxrs/exception/mapper/ObjectFieldTypeExceptionMapper.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.admin.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.object.exception.ObjectFieldTypeException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Javier Gamarra
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Object.Admin.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Object.Admin.REST.ObjectFieldTypeExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class ObjectFieldTypeExceptionMapper
+	extends BaseExceptionMapper<ObjectFieldTypeException> {
+
+	@Override
+	protected Problem getProblem(
+		ObjectFieldTypeException objectFieldTypeException) {
+
+		return new Problem(
+			Response.Status.BAD_REQUEST, objectFieldTypeException.getMessage());
+	}
+
+}
