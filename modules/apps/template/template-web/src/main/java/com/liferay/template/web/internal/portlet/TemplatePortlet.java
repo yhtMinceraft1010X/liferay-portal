2acc3baac52e36f49d9e5f54a6460479533b9904
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-19 16:31:55 +0200

    LPS-135812 Disable add TemplatePortlet to page

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/TemplatePortlet.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/TemplatePortlet.java
@@ -37,6 +37,7 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
