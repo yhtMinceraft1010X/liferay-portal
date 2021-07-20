b48130c4ad5d8a181300b8b1dac77b908b675a58
Kenji Heigel <kenji.heigel@liferay.com>
2021-07-19 17:09:59 -0700

    LRCI-2321 Add 'liferay-fix-pack-builder-ee' to the repo whitelist

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/github/webhook/GitHubWebhookPayloadProcessor.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/github/webhook/GitHubWebhookPayloadProcessor.java
@@ -2434,8 +2434,8 @@ public class GitHubWebhookPayloadProcessor {
 		Collections.emptyList();
 	private static final MultiPattern _whiteListedRepositoryMultiPattern =
 		new MultiPattern(
-			"liferay-jenkins-ee", "liferay-plugins(-ee)?",
-			"liferay-portal(-ee)?", "com-liferay-.*");
+			"liferay-fix-pack-builder-ee", "liferay-jenkins-ee",
+			"liferay-plugins(-ee)?", "liferay-portal(-ee)?", "com-liferay-.*");
 
 	private boolean _ciForwardEligible;
 	private final Properties _jenkinsBuildProperties;
