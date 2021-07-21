f0ad6c2e2cbc9dab8669eb351484d2d4b81f47b2
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-21 16:20:32 +0200

    LPS-136002 Upgrade process to update resource permissions

---
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/upgrade/TemplateUpgrade.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.web.internal.upgrade;
+
+import com.liferay.portal.kernel.upgrade.BasePortletIdUpgradeProcess;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.template.constants.TemplatePortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	enabled = false, immediate = true, service = UpgradeStepRegistrator.class
+)
+public class TemplateUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register("0.0.0", "1.0.0", new DummyUpgradeStep());
+
+		registry.register(
+			"0.0.1", "1.0.1",
+			new BasePortletIdUpgradeProcess() {
+
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						{
+							PortletKeys.PORTLET_DISPLAY_TEMPLATE,
+							TemplatePortletKeys.TEMPLATE
+						}
+					};
+				}
+
+			});
+	}
+
+}
