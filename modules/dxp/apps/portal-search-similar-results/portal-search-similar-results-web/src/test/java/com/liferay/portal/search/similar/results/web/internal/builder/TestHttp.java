4ac75941336462be8c401dda65f1de03cbe00956
Hugo Huijser <hugo.huijser@liferay.com>
2021-07-21 17:11:49 -0600

    LPS-105380 Partial revert of 1ec70acd, merging these statement changes logic

--- modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/test/java/com/liferay/portal/search/similar/results/web/internal/builder/TestHttp.java
+++ modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/test/java/com/liferay/portal/search/similar/results/web/internal/builder/TestHttp.java
@@ -260,9 +260,11 @@ public class TestHttp {
 			sb.toString(), StringPool.AMPERSAND + StringPool.AMPERSAND,
 			StringPool.AMPERSAND);
 
-		if (url.endsWith(StringPool.AMPERSAND) ||
-			url.endsWith(StringPool.QUESTION)) {
+		if (url.endsWith(StringPool.AMPERSAND)) {
+			url = url.substring(0, url.length() - 1);
+		}
 
+		if (url.endsWith(StringPool.QUESTION)) {
 			url = url.substring(0, url.length() - 1);
 		}
 
