1fcf177c69baf81e8b9380fdf1e9f2ba53680b2f
Brian Chan <brian.chan@liferay.com>
2021-07-21 09:13:47 -0700

    LPS-110354 avoid breaking change

--- modules/apps/layout/layout-seo-api/src/main/java/com/liferay/layout/seo/open/graph/OpenGraphConfiguration.java
+++ modules/apps/layout/layout-seo-api/src/main/java/com/liferay/layout/seo/open/graph/OpenGraphConfiguration.java
@@ -18,9 +18,12 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 
+import org.osgi.annotation.versioning.ProviderType;
+
 /**
  * @author Adolfo Pérez
  */
+@ProviderType
 public interface OpenGraphConfiguration {
 
 	public boolean isLayoutTranslatedLanguagesEnabled(Company company)
