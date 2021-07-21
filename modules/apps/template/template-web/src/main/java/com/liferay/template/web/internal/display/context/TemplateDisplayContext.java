95d22148735bd53420ca2cd6b45e1ca53b75730e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-07-21 12:29:54 +0100

    LPS-135940 SF

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/display/context/TemplateDisplayContext.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/display/context/TemplateDisplayContext.java
@@ -66,7 +66,6 @@ public class TemplateDisplayContext {
 		LiferayPortletResponse liferayPortletResponse) {
 
 		_ddmWebConfiguration = ddmWebConfiguration;
-
 		_liferayPortletRequest = liferayPortletRequest;
 		_liferayPortletResponse = liferayPortletResponse;
 
