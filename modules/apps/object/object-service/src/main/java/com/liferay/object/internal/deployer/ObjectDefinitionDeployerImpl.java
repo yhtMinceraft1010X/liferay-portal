32ddf7e871e1b2f63499cee080d259ec1277fcdd
Marco Leo <marco.leo@liferay.com>
2021-07-22 15:30:00 +0200

    LPS-135650 Import resources for each object definition

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/deployer/ObjectDefinitionDeployerImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/deployer/ObjectDefinitionDeployerImpl.java
@@ -17,11 +17,25 @@ package com.liferay.object.internal.deployer;
 import com.liferay.info.collection.provider.InfoCollectionProvider;
 import com.liferay.object.deployer.ObjectDefinitionDeployer;
 import com.liferay.object.internal.info.collection.provider.ObjectEntrySingleFormVariationInfoCollectionProvider;
+import com.liferay.object.internal.security.permission.resource.ObjectEntryModelResourcePermission;
+import com.liferay.object.internal.security.permission.resource.ObjectEntryPortletPermissionLogic;
 import com.liferay.object.internal.workflow.ObjectEntryWorkflowHandler;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.portal.kernel.exception.ResourceActionsException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.permission.ResourceActions;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.workflow.WorkflowHandler;
+import com.liferay.portal.kernel.xml.DocumentException;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
+
+import java.io.IOException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -41,6 +55,12 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 	public List<ServiceRegistration<?>> deploy(
 		ObjectDefinition objectDefinition) {
 
+		try {
+			_importResourceAction(objectDefinition);
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+		}
 		return Arrays.asList(
 			_bundleContext.registerService(
 				InfoCollectionProvider.class,
@@ -61,9 +81,32 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 		_bundleContext = bundleContext;
 	}
 
+	private void _importResourceAction(ObjectDefinition objectDefinition)
+		throws DocumentException, IOException, ResourceActionsException {
+
+		String xml = StringUtil.read(
+			ObjectDefinitionDeployerImpl.class.getClassLoader(),
+			_DEPENDENCIES_PATH + "resource-actions.xml.tpl");
+
+		_resourceActions.populateModelResources(
+			SAXReaderUtil.read(
+				StringUtil.replace(
+					StringUtil.replace(
+						xml, "[$RESOURCE_NAME$]", objectDefinition.getName()),
+					"[$MODEL_NAME$]", objectDefinition.getDBTableName())));
+	}
+
+	private static final String _DEPENDENCIES_PATH = "resource-actions/";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ObjectDefinitionDeployerImpl.class);
+
 	private BundleContext _bundleContext;
 
 	@Reference
 	private ObjectEntryLocalService _objectEntryLocalService;
+
+	@Reference
+	private ResourceActions _resourceActions;
 
 }
---
+++ modules/apps/object/object-service/src/main/resources/resource-actions/resource-actions.xml.tpl
@@ -0,0 +1,48 @@
+<?xml version="1.0"?>
+<!DOCTYPE resource-action-mapping PUBLIC "-//Liferay//DTD Resource Action Mapping 7.4.0//EN" "http://www.liferay.com/dtd/liferay-resource-action-mapping_7_4_0.dtd">
+
+<resource-action-mapping>
+	<model-resource>
+		<model-name>[$MODEL_NAME$]</model-name>
+		<portlet-ref>
+		</portlet-ref>
+		<weight>2</weight>
+		<permissions>
+			<supports>
+				<action-key>DELETE</action-key>
+				<action-key>PERMISSIONS</action-key>
+				<action-key>UPDATE</action-key>
+				<action-key>VIEW</action-key>
+			</supports>
+			<site-member-defaults>
+				<action-key>VIEW</action-key>
+			</site-member-defaults>
+			<guest-defaults>
+			</guest-defaults>
+			<guest-unsupported>
+				<action-key>DELETE</action-key>
+				<action-key>PERMISSIONS</action-key>
+				<action-key>UPDATE</action-key>
+				<action-key>VIEW</action-key>
+			</guest-unsupported>
+		</permissions>
+	</model-resource>
+	<model-resource>
+		<model-name>[$RESOURCE_NAME$]</model-name>
+		<portlet-ref>
+		</portlet-ref>
+		<root>true</root>
+		<weight>1</weight>
+		<permissions>
+			<supports>
+				<action-key>ADD_ENTRY</action-key>
+				<action-key>PERMISSIONS</action-key>
+			</supports>
+			<guest-defaults />
+			<guest-unsupported>
+				<action-key>ADD_ENTRY</action-key>
+				<action-key>PERMISSIONS</action-key>
+			</guest-unsupported>
+		</permissions>
+	</model-resource>
+</resource-action-mapping>
