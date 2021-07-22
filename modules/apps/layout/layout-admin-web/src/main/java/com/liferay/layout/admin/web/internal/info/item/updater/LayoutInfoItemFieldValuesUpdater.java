234e686714530dcf6756a07793932969bed8700a
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-07-22 15:38:24 +0200

    LPS-136077 Update fragment entry link fields of the layout

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/info/item/updater/LayoutInfoItemFieldValuesUpdater.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/info/item/updater/LayoutInfoItemFieldValuesUpdater.java
@@ -14,17 +14,28 @@
 
 package com.liferay.layout.admin.web.internal.info.item.updater;
 
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.info.field.InfoField;
 import com.liferay.info.field.InfoFieldValue;
 import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.info.item.updater.InfoItemFieldValuesUpdater;
 import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.layout.admin.web.internal.info.item.LayoutInfoItemFields;
+import com.liferay.petra.reflect.ReflectionUtil;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -43,12 +54,26 @@ public class LayoutInfoItemFieldValuesUpdater
 	public Layout updateFromInfoItemFieldValues(
 		Layout layout, InfoItemFieldValues infoItemFieldValues) {
 
+		_updateFragmentEntryLinks(infoItemFieldValues);
+
 		layout.setNameMap(
 			_getFieldMap(
 				LayoutInfoItemFields.nameInfoField.getName(),
 				infoItemFieldValues, layout.getNameMap()));
 
 		return _layoutLocalService.updateLayout(layout);
+	}
+
+	private JSONObject _createEditableValuesJSONObject(
+		FragmentEntryLink fragmentEntryLink) {
+
+		try {
+			return JSONFactoryUtil.createJSONObject(
+				fragmentEntryLink.getEditableValues());
+		}
+		catch (JSONException jsonException) {
+			return ReflectionUtil.throwException(jsonException);
+		}
 	}
 
 	private Map<Locale, String> _getFieldMap(
@@ -69,6 +94,86 @@ public class LayoutInfoItemFieldValuesUpdater
 
 		return fieldMap;
 	}
+
+	private void _updateEditableValuesJSONObject(
+		JSONObject jsonObject, String fieldName,
+		InfoFieldValue<Object> infoFieldValue) {
+
+		for (String processorKey : jsonObject.keySet()) {
+			JSONObject processorJSONObject = jsonObject.getJSONObject(
+				processorKey);
+
+			if (!processorJSONObject.has(fieldName)) {
+				continue;
+			}
+
+			JSONObject valuesJSONObject = processorJSONObject.getJSONObject(
+				fieldName);
+
+			InfoLocalizedValue<String> infoLocalizedValue =
+				(InfoLocalizedValue<String>)infoFieldValue.getValue();
+
+			for (Locale locale : infoLocalizedValue.getAvailableLocales()) {
+				valuesJSONObject.put(
+					LanguageUtil.getLanguageId(locale),
+					infoLocalizedValue.getValue(locale));
+			}
+		}
+	}
+
+	private void _updateFragmentEntryLinks(
+		InfoItemFieldValues infoItemFieldValues) {
+
+		Map<FragmentEntryLink, JSONObject> editableValuesJSONObjects =
+			new HashMap<>();
+
+		for (InfoFieldValue<Object> infoFieldValue :
+				infoItemFieldValues.getInfoFieldValues()) {
+
+			InfoField infoField = infoFieldValue.getInfoField();
+
+			Matcher matcher = _fragmentEntryLinkInfoFieldPattern.matcher(
+				infoField.getName());
+
+			if (!matcher.matches()) {
+				continue;
+			}
+
+			FragmentEntryLink fragmentEntryLink =
+				_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+					GetterUtil.getLong(matcher.group("id")));
+
+			if (fragmentEntryLink == null) {
+				continue;
+			}
+
+			_updateEditableValuesJSONObject(
+				editableValuesJSONObjects.computeIfAbsent(
+					fragmentEntryLink, this::_createEditableValuesJSONObject),
+				matcher.group("name"), infoFieldValue);
+		}
+
+		for (Map.Entry<FragmentEntryLink, JSONObject> entry :
+				editableValuesJSONObjects.entrySet()) {
+
+			JSONObject jsonObject = entry.getValue();
+
+			if (jsonObject != null) {
+				FragmentEntryLink fragmentEntryLink = entry.getKey();
+
+				fragmentEntryLink.setEditableValues(jsonObject.toJSONString());
+
+				_fragmentEntryLinkLocalService.updateFragmentEntryLink(
+					fragmentEntryLink);
+			}
+		}
+	}
+
+	private static final Pattern _fragmentEntryLinkInfoFieldPattern =
+		Pattern.compile("(?<id>\\d+):(?<name>.+)");
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;
