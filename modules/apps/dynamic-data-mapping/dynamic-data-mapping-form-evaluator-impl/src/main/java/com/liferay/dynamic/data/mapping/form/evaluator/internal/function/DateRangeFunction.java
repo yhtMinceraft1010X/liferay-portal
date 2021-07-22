4eac2018cdaad8c550eeba2d1b68525469bede7c
Carolina Barbosa <carolina.barbosa@liferay.com>
2021-07-22 16:51:40 -0300

    LPS-135723 Must set DDMExpressionParameterAccessor for DateRangeFunction

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/DateRangeFunction.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/DateRangeFunction.java
@@ -15,17 +15,25 @@
 package com.liferay.dynamic.data.mapping.form.evaluator.internal.function;
 
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
+import com.liferay.dynamic.data.mapping.expression.DDMExpressionParameterAccessor;
+import com.liferay.dynamic.data.mapping.expression.DDMExpressionParameterAccessorAware;
 
 /**
  * @author Carolina Barbosa
  */
 public class DateRangeFunction
-	implements DDMExpressionFunction.Function2<Object, Object, Boolean> {
+	implements DDMExpressionFunction.Function2<Object, Object, Boolean>,
+			   DDMExpressionParameterAccessorAware {
 
 	public static final String NAME = "dateRange";
 
 	@Override
 	public Boolean apply(Object object1, Object object2) {
+		_futureDatesFunction.setDDMExpressionParameterAccessor(
+			_ddmExpressionParameterAccessor);
+		_pastDatesFunction.setDDMExpressionParameterAccessor(
+			_ddmExpressionParameterAccessor);
+
 		if (_futureDatesFunction.apply(object1, object2) &&
 			_pastDatesFunction.apply(object1, object2)) {
 
@@ -40,6 +48,14 @@ public class DateRangeFunction
 		return NAME;
 	}
 
+	@Override
+	public void setDDMExpressionParameterAccessor(
+		DDMExpressionParameterAccessor ddmExpressionParameterAccessor) {
+
+		_ddmExpressionParameterAccessor = ddmExpressionParameterAccessor;
+	}
+
+	private DDMExpressionParameterAccessor _ddmExpressionParameterAccessor;
 	private final FutureDatesFunction _futureDatesFunction =
 		new FutureDatesFunction();
 	private final PastDatesFunction _pastDatesFunction =
