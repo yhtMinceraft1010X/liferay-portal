bcdcc27584c9b083698ccd75f060caf9a6c25fe8
Marco Leo <marco.leo@liferay.com>
2021-07-22 15:28:02 +0200

    LPS-135650 Class to register for each object definition

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/security/permission/resource/ObjectEntryModelResourcePermission.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.internal.security.permission.resource;
+
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+/**
+ * @author Marco Leo
+ */
+public class ObjectEntryModelResourcePermission
+	implements ModelResourcePermission<ObjectEntry> {
+
+	public ObjectEntryModelResourcePermission(
+		String modelName, ObjectEntryLocalService objectEntryLocalService,
+		PortletResourcePermission portletResourcePermission) {
+
+		_modelName = modelName;
+		_objectEntryLocalService = objectEntryLocalService;
+		_portletResourcePermission = portletResourcePermission;
+	}
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, long objectEntryId,
+			String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, objectEntryId, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, _modelName, objectEntryId, actionId);
+		}
+	}
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, ObjectEntry objectEntry,
+			String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, objectEntry, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, _modelName, objectEntry.getObjectEntryId(),
+				actionId);
+		}
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, long objectEntryId,
+			String actionId)
+		throws PortalException {
+
+		ObjectEntry objectEntry = _objectEntryLocalService.fetchObjectEntry(
+			objectEntryId);
+
+		long objectEntryGroupId = 0;
+
+		if (objectEntry != null) {
+			objectEntryGroupId = objectEntry.getGroupId();
+		}
+
+		return permissionChecker.hasPermission(
+			objectEntryGroupId, _modelName, objectEntryId, actionId);
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, ObjectEntry accountEntry,
+			String actionId)
+		throws PortalException {
+
+		return contains(
+			permissionChecker, accountEntry.getObjectEntryId(), actionId);
+	}
+
+	@Override
+	public String getModelName() {
+		return _modelName;
+	}
+
+	@Override
+	public PortletResourcePermission getPortletResourcePermission() {
+		return _portletResourcePermission;
+	}
+
+	private final String _modelName;
+	private final ObjectEntryLocalService _objectEntryLocalService;
+	private final PortletResourcePermission _portletResourcePermission;
+
+}
