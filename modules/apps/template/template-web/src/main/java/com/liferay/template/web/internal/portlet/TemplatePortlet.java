a1cae5dbce40af19767d156ddb9a28112f1a9806
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-22 10:47:57 +0200

    LPS-135841 Set portlet property

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/TemplatePortlet.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/TemplatePortlet.java
@@ -47,6 +47,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.preferences-unique-per-layout=false",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
