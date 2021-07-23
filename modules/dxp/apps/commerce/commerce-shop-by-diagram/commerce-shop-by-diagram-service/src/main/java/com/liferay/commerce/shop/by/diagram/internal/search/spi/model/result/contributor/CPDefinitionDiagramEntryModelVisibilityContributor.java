257ce134f1c58d156677c96b2d2e3b283ac71c59
Alessio Rendina <asilcomando@gmail.com>
2021-07-23 09:31:08 +0200

    COMMERCE-6922 Added model visibility contributor implementation

---
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/internal/search/spi/model/result/contributor/CPDefinitionDiagramEntryModelVisibilityContributor.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.shop.by.diagram.internal.search.spi.model.result.contributor;
+
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramEntry;
+import com.liferay.commerce.shop.by.diagram.service.CPDefinitionDiagramEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.spi.model.result.contributor.ModelVisibilityContributor;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "indexer.class.name=com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramEntry",
+	service = ModelVisibilityContributor.class
+)
+public class CPDefinitionDiagramEntryModelVisibilityContributor
+	implements ModelVisibilityContributor {
+
+	@Override
+	public boolean isVisible(long classPK, int status) {
+		try {
+			CPDefinitionDiagramEntry cpDefinitionDiagramEntry =
+				_cpDefinitionDiagramEntryLocalService.
+					getCPDefinitionDiagramEntry(classPK);
+
+			CPDefinition cpDefinition =
+				cpDefinitionDiagramEntry.getCPDefinition();
+
+			return _isVisible(cpDefinition.getStatus(), status);
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to check visibility for commerce product " +
+						"definition diagram entry ",
+					portalException);
+			}
+
+			return false;
+		}
+	}
+
+	private boolean _isVisible(int entryStatus, int queryStatus) {
+		if (((queryStatus != WorkflowConstants.STATUS_ANY) &&
+			 (entryStatus == queryStatus)) ||
+			(entryStatus != WorkflowConstants.STATUS_IN_TRASH)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDefinitionDiagramEntryModelVisibilityContributor.class);
+
+	@Reference
+	private CPDefinitionDiagramEntryLocalService
+		_cpDefinitionDiagramEntryLocalService;
+
+}
