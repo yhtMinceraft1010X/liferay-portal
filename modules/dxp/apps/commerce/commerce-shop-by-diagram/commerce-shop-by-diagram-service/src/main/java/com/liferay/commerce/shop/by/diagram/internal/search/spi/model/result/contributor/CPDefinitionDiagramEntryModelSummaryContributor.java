c43d9da67c9f4ad90ad8dcaccfe1aea55e65d4ff
Alessio Rendina <asilcomando@gmail.com>
2021-07-23 09:30:48 +0200

    COMMERCE-6922 Added model summary contributor implementation

---
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/internal/search/spi/model/result/contributor/CPDefinitionDiagramEntryModelSummaryContributor.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.shop.by.diagram.internal.search.spi.model.result.contributor;
+
+import com.liferay.commerce.product.constants.CPField;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "indexer.class.name=com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramEntry",
+	service = ModelSummaryContributor.class
+)
+public class CPDefinitionDiagramEntryModelSummaryContributor
+	implements ModelSummaryContributor {
+
+	@Override
+	public Summary getSummary(
+		Document document, Locale locale, String snippet) {
+
+		Summary summary = new Summary(Field.ENTRY_CLASS_PK, CPField.SKU);
+
+		summary.setMaxContentLength(200);
+
+		return summary;
+	}
+
+}
