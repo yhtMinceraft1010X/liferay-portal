b5b16e2a76cfad6f884c813e103fa5402a816317
Alessio Rendina <asilcomando@gmail.com>
2021-07-23 09:31:27 +0200

    COMMERCE-6922 Added keyword query contributor implementation

---
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/internal/search/spi/model/query/contributor/CPDefinitionDiagramEntryKeywordQueryContributor.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.shop.by.diagram.internal.search.spi.model.query.contributor;
+
+import com.liferay.commerce.product.constants.CPField;
+import com.liferay.commerce.shop.by.diagram.internal.search.spi.model.index.contributor.CPDefinitionDiagramEntryModelDocumentContributor;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.search.query.QueryHelper;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "indexer.class.name=com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramEntry",
+	service = KeywordQueryContributor.class
+)
+public class CPDefinitionDiagramEntryKeywordQueryContributor
+	implements KeywordQueryContributor {
+
+	@Override
+	public void contribute(
+		String keywords, BooleanQuery booleanQuery,
+		KeywordQueryContributorHelper keywordQueryContributorHelper) {
+
+		SearchContext searchContext =
+			keywordQueryContributorHelper.getSearchContext();
+
+		_queryHelper.addSearchTerm(
+			booleanQuery, searchContext, Field.ENTRY_CLASS_PK, false);
+		_queryHelper.addSearchTerm(
+			booleanQuery, searchContext, CPField.SKU, false);
+		_queryHelper.addSearchTerm(
+			booleanQuery, searchContext,
+			CPDefinitionDiagramEntryModelDocumentContributor.FIELD_NUMBER,
+			false);
+	}
+
+	@Reference
+	private QueryHelper _queryHelper;
+
+}
