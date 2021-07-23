3b843f0aff2187459ff4cbe006ca3b7c5560664f
Alessio Rendina <asilcomando@gmail.com>
2021-07-23 09:29:07 +0200

    COMMERCE-6922 Added commerce product definition diagram searcher

---
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/internal/search/CPDefinitionDiagramSearcher.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.shop.by.diagram.internal.search;
+
+import com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramEntry;
+import com.liferay.portal.kernel.search.BaseSearcher;
+import com.liferay.portal.kernel.search.Field;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "model.class.name=com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramEntry",
+	service = BaseSearcher.class
+)
+public class CPDefinitionDiagramSearcher extends BaseSearcher {
+
+	public CPDefinitionDiagramSearcher() {
+		setDefaultSelectedFieldNames(
+			Field.COMPANY_ID, Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK,
+			Field.UID);
+		setFilterSearch(true);
+		setPermissionAware(true);
+		setSelectAllLocales(true);
+	}
+
+	@Override
+	public String getClassName() {
+		return _CLASS_NAME;
+	}
+
+	private static final String _CLASS_NAME =
+		CPDefinitionDiagramEntry.class.getName();
+
+}
