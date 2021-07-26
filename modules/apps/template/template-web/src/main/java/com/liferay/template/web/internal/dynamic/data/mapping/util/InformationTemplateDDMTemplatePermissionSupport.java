2fa415af4dd78f75d1b95c71619407bd369b4655
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-26 11:10:00 +0200

    LPS-136143 Add DDMTemplate permission support for InfoItemFormProvider

---
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/dynamic/data/mapping/util/InformationTemplateDDMTemplatePermissionSupport.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.web.internal.dynamic.data.mapping.util;
+
+import com.liferay.dynamic.data.mapping.constants.DDMActionKeys;
+import com.liferay.dynamic.data.mapping.util.DDMTemplatePermissionSupport;
+import com.liferay.template.constants.TemplatePortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"add.template.action.id=" + DDMActionKeys.ADD_TEMPLATE,
+		"default.model.resource.name=true",
+		"model.class.name=com.liferay.info.item.provider.InfoItemFormProvider"
+	},
+	service = DDMTemplatePermissionSupport.class
+)
+public class InformationTemplateDDMTemplatePermissionSupport
+	implements DDMTemplatePermissionSupport {
+
+	@Override
+	public String getResourceName(long classNameId) {
+		return TemplatePortletKeys.TEMPLATE;
+	}
+
+}
