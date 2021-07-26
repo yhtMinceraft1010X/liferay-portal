40beed00cab24d8392eda8c1d7b444850ad3a61a
Beltran Rengifo <beltran.rengifo@liferay.com>
2021-07-26 13:34:36 +0200

    LPS-136201 Fix styles for label element

--- modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/js/components/Translation.js
+++ modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/js/components/Translation.js
@@ -108,7 +108,10 @@ export default function Translation({
 									</ClayTooltipProvider>
 								</ClayLayout.ContentCol>
 								{defaultLanguageId === languageId && (
-									<ClayLabel displayType="primary">
+									<ClayLabel
+										className="flex-shrink-0 ml-1"
+										displayType="primary"
+									>
 										{Liferay.Language.get('default')}
 									</ClayLabel>
 								)}
