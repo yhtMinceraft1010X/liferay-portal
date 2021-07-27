10998e25dbf559b5b78a4f2862880db07a4f02d9
Pablo Molina <contact@pablomolina.me>
2021-07-27 17:57:10 +0200

    LPS-136304 Add CollectionFilterGeneralPanel

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page-structure/components/item-configuration-panels/CollectionFilterGeneralPanel.js
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayDropDown from '@clayui/drop-down';
+import ClayForm, {ClayCheckbox} from '@clayui/form';
+import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+
+import {LAYOUT_DATA_ITEM_TYPES} from '../../../../../../app/config/constants/layoutDataItemTypes';
+import {useHoverItem} from '../../../../../../app/contexts/ControlsContext';
+import {
+	useDispatch,
+	useSelector,
+	useSelectorCallback,
+} from '../../../../../../app/contexts/StoreContext';
+import selectSegmentsExperienceId from '../../../../../../app/selectors/selectSegmentsExperienceId';
+import updateItemConfig from '../../../../../../app/thunks/updateItemConfig';
+import {useId} from '../../../../../../app/utils/useId';
+import getLayoutDataItemPropTypes from '../../../../../../prop-types/getLayoutDataItemPropTypes';
+import {FragmentGeneralPanel} from './FragmentGeneralPanel';
+
+function TargetCollectionsField({onValueSelect, value}) {
+	const [active, setActive] = useState(false);
+	const inputId = useId();
+	const [nextValue, setNextValue] = useState(value || []);
+	const hoverItem = useHoverItem();
+
+	const inputValue = useSelectorCallback(
+		(state) => {
+			if (nextValue.length === 0) {
+				return '';
+			}
+			else if (nextValue.length === 1) {
+				return state.layoutData.items[nextValue[0]]?.config?.collection
+					?.title;
+			}
+
+			return Liferay.Language.get('multiple');
+		},
+		[nextValue]
+	);
+
+	const handleChange = (layoutItemId, checked) => {
+		const included = nextValue.includes(layoutItemId);
+		let selectedItems = nextValue;
+
+		if (checked && !included) {
+			selectedItems = [...nextValue, layoutItemId];
+
+			setNextValue(selectedItems);
+			onValueSelect('targetCollections', selectedItems);
+		}
+		else if (included) {
+			selectedItems = nextValue.filter(
+				(itemId) => itemId !== layoutItemId
+			);
+
+			setNextValue(selectedItems);
+			onValueSelect('targetCollections', selectedItems);
+		}
+	};
+
+	const items = useSelectorCallback(
+		(state) =>
+			Object.values(state.layoutData.items)
+				.filter(
+					(item) =>
+						item.type === LAYOUT_DATA_ITEM_TYPES.collection &&
+						item.config?.collection?.key
+				)
+				.map((item) => ({
+					checked: nextValue.includes(item.itemId),
+					label: item.config.collection.title,
+					onChange: (checked) => handleChange(item.itemId, checked),
+					type: 'checkbox',
+					value: item.itemId,
+				})),
+		[nextValue]
+	);
+
+	return (
+		<ClayForm.Group className="mt-1">
+			<label htmlFor={inputId}>
+				{Liferay.Language.get('target-collection')}
+			</label>
+
+			<ClayDropDown
+				active={active}
+				id={inputId}
+				onActiveChange={setActive}
+				trigger={
+					<ClayButton
+						aria-label={Liferay.Language.get('select')}
+						className="bg-light font-weight-normal form-control-select text-left w-100"
+						displayType="secondary"
+						small
+					>
+						{inputValue ? (
+							<span className="text-dark">{inputValue}</span>
+						) : (
+							Liferay.Language.get('select')
+						)}
+					</ClayButton>
+				}
+			>
+				{items.map((item) => (
+					<label
+						className="d-flex dropdown-item"
+						key={item.value}
+						onMouseLeave={() => hoverItem(null)}
+						onMouseOver={() => hoverItem(item.value)}
+					>
+						<ClayCheckbox
+							checked={item.checked}
+							onChange={item.onChange}
+						/>
+						<span className="font-weight-normal ml-2">
+							{item.label}
+						</span>
+					</label>
+				))}
+			</ClayDropDown>
+		</ClayForm.Group>
+	);
+}
+
+export const CollectionFilterGeneralPanel = ({item}) => {
+	const dispatch = useDispatch();
+	const segmentsExperienceId = useSelector(selectSegmentsExperienceId);
+
+	const onValueSelect = (name, value) => {
+		dispatch(
+			updateItemConfig({
+				itemConfig: {...item.config, [name]: value},
+				itemId: item.itemId,
+				segmentsExperienceId,
+			})
+		);
+	};
+
+	return (
+		<>
+			<TargetCollectionsField
+				onValueSelect={onValueSelect}
+				value={item.config.targetCollections}
+			/>
+
+			<FragmentGeneralPanel item={item} />
+		</>
+	);
+};
+
+CollectionFilterGeneralPanel.propTypes = {
+	item: getLayoutDataItemPropTypes({
+		config: PropTypes.shape({
+			fragmentEntryLinkId: PropTypes.string.isRequired,
+		}).isRequired,
+	}),
+};
