086cc32e33c22a5c97f5e25f5966ebf923432388
Alicia Garcia <alicia.garcia@liferay.com>
2021-07-27 13:47:05 +0200

    LPS-136275 create commerce image service to upgrade the image table to include correct companyId on commerce tables

---
+++ modules/apps/commerce/commerce-image-service/src/main/java/com/liferay/commerce/image/service/internal/upgrade/CommerceImageServiceUpgrade.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.image.service.internal.upgrade;
+
+import com.liferay.commerce.image.service.internal.upgrade.v1_0_0.CommerceImageCompanyIdUpgradeProcess;
+import com.liferay.commerce.model.CommerceShippingMethod;
+import com.liferay.commerce.payment.model.CommercePaymentMethodGroupRel;
+import com.liferay.commerce.payment.service.CommercePaymentMethodGroupRelLocalService;
+import com.liferay.commerce.service.CommerceShippingMethodLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ * @author Alicia García
+ */
+@Component(
+	enabled = false, immediate = true, service = UpgradeStepRegistrator.class
+)
+public class CommerceImageServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"0.0.0", "1.0.0",
+			new CommerceImageCompanyIdUpgradeProcess<>(
+				_commercePaymentMethodGroupRelLocalService::
+					getActionableDynamicQuery,
+				CommercePaymentMethodGroupRel::getCompanyId,
+				CommercePaymentMethodGroupRel::getImageId),
+			new CommerceImageCompanyIdUpgradeProcess<>(
+				_commerceShippingMethodLocalService::getActionableDynamicQuery,
+				CommerceShippingMethod::getCompanyId,
+				CommerceShippingMethod::getImageId));
+	}
+
+	@Reference
+	private CommercePaymentMethodGroupRelLocalService
+		_commercePaymentMethodGroupRelLocalService;
+
+	@Reference
+	private CommerceShippingMethodLocalService
+		_commerceShippingMethodLocalService;
+
+}
---
+++ modules/apps/commerce/commerce-image-service/src/main/java/com/liferay/commerce/image/service/internal/upgrade/v1_0_0/CommerceImageCompanyIdUpgradeProcess.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.image.service.internal.upgrade.v1_0_0;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.sql.PreparedStatement;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * @author Adolfo Pérez
+ * @author Alicia García
+ */
+public class CommerceImageCompanyIdUpgradeProcess<T> extends UpgradeProcess {
+
+	public CommerceImageCompanyIdUpgradeProcess(
+		Supplier<ActionableDynamicQuery> actionableDynamicQuerySupplier,
+		Function<T, Long> companyIdFunction,
+		Function<T, Long> imageIdFunction) {
+
+		_actionableDynamicQuerySupplier = actionableDynamicQuerySupplier;
+		_companyIdFunction = companyIdFunction;
+		_imageIdFunction = imageIdFunction;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement preparedStatement =
+				AutoBatchPreparedStatementUtil.autoBatch(
+					connection.prepareStatement(
+						"update Image set companyId = ? where imageId = ?"))) {
+
+			ActionableDynamicQuery actionableDynamicQuery =
+				_actionableDynamicQuerySupplier.get();
+
+			actionableDynamicQuery.setPerformActionMethod(
+				(T model) -> {
+					try {
+						preparedStatement.setLong(
+							1, _companyIdFunction.apply(model));
+						preparedStatement.setLong(
+							2, _imageIdFunction.apply(model));
+
+						preparedStatement.addBatch();
+					}
+					catch (Exception exception) {
+						_log.error(
+							StringBundler.concat(
+								"Cannot update image ",
+								_imageIdFunction.apply(model), " to company ID",
+								_companyIdFunction.apply(model)),
+							exception);
+					}
+				});
+
+			actionableDynamicQuery.performActions();
+
+			preparedStatement.executeBatch();
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceImageCompanyIdUpgradeProcess.class.getName());
+
+	private final Supplier<ActionableDynamicQuery>
+		_actionableDynamicQuerySupplier;
+	private final Function<T, Long> _companyIdFunction;
+	private final Function<T, Long> _imageIdFunction;
+
+}
