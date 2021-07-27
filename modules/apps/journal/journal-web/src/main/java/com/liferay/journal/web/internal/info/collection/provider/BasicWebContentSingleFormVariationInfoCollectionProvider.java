2eaaeb6a0d3ed0010e003de87594d54f2bdd2028
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-07-27 12:15:51 +0200

    LPS-136338 Change to take the configuration into account

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/collection/provider/BasicWebContentSingleFormVariationInfoCollectionProvider.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/collection/provider/BasicWebContentSingleFormVariationInfoCollectionProvider.java
@@ -19,21 +19,44 @@ import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.info.collection.provider.CollectionQuery;
 import com.liferay.info.collection.provider.InfoCollectionProvider;
 import com.liferay.info.collection.provider.SingleFormVariationInfoCollectionProvider;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.InfoFieldSet;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.info.pagination.InfoPage;
 import com.liferay.info.pagination.Pagination;
 import com.liferay.info.sort.Sort;
 import com.liferay.journal.model.JournalArticle;
-import com.liferay.journal.service.JournalArticleService;
-import com.liferay.journal.web.internal.util.JournalPortletUtil;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.journal.web.internal.search.JournalSearcher;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoader;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.QueryConfig;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
-import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -53,37 +76,77 @@ public class BasicWebContentSingleFormVariationInfoCollectionProvider
 	public InfoPage<JournalArticle> getCollectionInfoPage(
 		CollectionQuery collectionQuery) {
 
-		ServiceContext serviceContext =
-			ServiceContextThreadLocal.getServiceContext();
+		Indexer<?> indexer = JournalSearcher.getInstance();
 
-		Pagination pagination = collectionQuery.getPagination();
+		SearchContext searchContext = _buildSearchContext(collectionQuery);
 
-		OrderByComparator<JournalArticle> orderByComparator = null;
+		try {
+			Hits hits = indexer.search(searchContext);
 
-		Optional<Sort> sortOptional = collectionQuery.getSortOptional();
+			List<JournalArticle> articles = new ArrayList<>();
 
-		if (sortOptional.isPresent()) {
-			String orderByType = "ASC";
+			Document[] documents = hits.getDocs();
 
-			Sort sort = sortOptional.get();
+			for (Document document : documents) {
+				String className = document.get(Field.ENTRY_CLASS_NAME);
 
-			if (sort.isReverse()) {
-				orderByType = "DESC";
+				if (className.equals(JournalArticle.class.getName())) {
+					long classPK = GetterUtil.getLong(
+						document.get(Field.ENTRY_CLASS_PK));
+
+					JournalArticle article =
+						_journalArticleLocalService.fetchLatestArticle(
+							classPK, WorkflowConstants.STATUS_ANY, false);
+
+					articles.add(article);
+				}
 			}
 
-			orderByComparator = JournalPortletUtil.getArticleOrderByComparator(
-				sort.getFieldName(), orderByType);
+			return InfoPage.of(
+				articles, collectionQuery.getPagination(), hits.getLength());
+		}
+		catch (SearchException searchException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(searchException, searchException);
+			}
 		}
 
-		return InfoPage.of(
-			_journalArticleService.getArticlesByStructureId(
-				serviceContext.getScopeGroupId(), "BASIC-WEB-CONTENT",
-				WorkflowConstants.STATUS_APPROVED, pagination.getStart(),
-				pagination.getEnd(), orderByComparator),
-			pagination,
-			_journalArticleService.getArticlesCountByStructureId(
-				serviceContext.getScopeGroupId(), "BASIC-WEB-CONTENT",
-				WorkflowConstants.STATUS_APPROVED));
+		return null;
+	}
+
+	@Override
+	public InfoForm getConfigurationInfoForm() {
+		return InfoForm.builder(
+		).infoFieldSetEntry(
+			InfoFieldSet.builder(
+			).infoFieldSetEntry(
+				InfoField.builder(
+				).infoFieldType(
+					TextInfoFieldType.INSTANCE
+				).name(
+					Field.TITLE
+				).labelInfoLocalizedValue(
+					InfoLocalizedValue.localize(getClass(), "title")
+				).localizable(
+					true
+				).build()
+			).infoFieldSetEntry(
+				InfoField.builder(
+				).infoFieldType(
+					TextInfoFieldType.INSTANCE
+				).name(
+					Field.DESCRIPTION
+				).labelInfoLocalizedValue(
+					InfoLocalizedValue.localize(getClass(), "description")
+				).localizable(
+					true
+				).build()
+			).labelInfoLocalizedValue(
+				InfoLocalizedValue.localize(getClass(), "configuration")
+			).name(
+				"basic-information"
+			).build()
+		).build();
 	}
 
 	@Override
@@ -107,11 +170,96 @@ public class BasicWebContentSingleFormVariationInfoCollectionProvider
 		return LanguageUtil.get(resourceBundle, "basic-web-content");
 	}
 
+	private SearchContext _buildSearchContext(CollectionQuery collectionQuery) {
+		Pagination pagination = collectionQuery.getPagination();
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setAndSearch(true);
+
+		searchContext.setAttributes(
+			HashMapBuilder.<String, Serializable>put(
+				Field.STATUS, WorkflowConstants.STATUS_APPROVED
+			).put(
+				"ddmStructureKey", "BASIC-WEB-CONTENT"
+			).put(
+				"head", true
+			).put(
+				"latest", true
+			).build());
+
+		Optional<Map<String, String[]>> collectionConfigurationOptional =
+			collectionQuery.getCollectionConfiguration();
+
+		Map<String, String[]> collectionConfiguration =
+			collectionConfigurationOptional.orElse(null);
+
+		if (collectionConfiguration != null) {
+			for (Map.Entry<String, String[]> entry :
+					collectionConfiguration.entrySet()) {
+
+				String value = entry.getValue()[0];
+
+				if (Validator.isNotNull(value)) {
+					String localizedName = Field.getLocalizedName(
+						LocaleUtil.getSiteDefault(), entry.getKey());
+
+					searchContext.setAttribute(localizedName, value);
+				}
+			}
+		}
+
+		searchContext.setCompanyId(serviceContext.getCompanyId());
+		searchContext.setEnd(pagination.getEnd());
+		searchContext.setGroupIds(
+			new long[] {serviceContext.getScopeGroupId()});
+
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		queryConfig.setHighlightEnabled(false);
+		queryConfig.setScoreEnabled(false);
+
+		Optional<Sort> sortOptional = collectionQuery.getSortOptional();
+
+		com.liferay.portal.kernel.search.Sort searchSort = null;
+
+		if (sortOptional.isPresent()) {
+			boolean orderByAsc = true;
+
+			Sort sort = sortOptional.get();
+
+			if (sort.isReverse()) {
+				orderByAsc = false;
+			}
+
+			searchSort = new com.liferay.portal.kernel.search.Sort(
+				sort.getFieldName(),
+				com.liferay.portal.kernel.search.Sort.LONG_TYPE, !orderByAsc);
+		}
+
+		if (searchSort != null) {
+			searchContext.setSorts(searchSort);
+		}
+
+		searchContext.setStart(pagination.getStart());
+
+		return searchContext;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BasicWebContentSingleFormVariationInfoCollectionProvider.class);
+
 	@Reference
 	private DDMStructureLocalService _ddmStructureLocalService;
 
 	@Reference
-	private JournalArticleService _journalArticleService;
+	private InfoItemServiceTracker _infoItemServiceTracker;
+
+	@Reference
+	private JournalArticleLocalService _journalArticleLocalService;
 
 	@Reference
 	private Portal _portal;
