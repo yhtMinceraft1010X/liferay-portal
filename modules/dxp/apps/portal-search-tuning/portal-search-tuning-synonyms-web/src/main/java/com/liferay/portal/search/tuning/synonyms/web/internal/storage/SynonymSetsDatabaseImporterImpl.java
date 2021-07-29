8c450519e200ac66916f4738e0baf97157c8ce74
Bryan Engler <bryan.engler@liferay.com>
2021-07-29 08:22:58 -0700

    LPS-133588 portal-search-tuning-synonyms-web: Add SynonymSetsDatabaseImporterImpl so that any existing data in the synonym indexes can be copied to the database
    https://issues.liferay.com/browse/LPS-133588

---
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/storage/SynonymSetsDatabaseImporterImpl.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.search.tuning.synonyms.web.internal.storage;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.hits.SearchHit;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.tuning.synonyms.index.name.SynonymSetIndexName;
+import com.liferay.portal.search.tuning.synonyms.index.name.SynonymSetIndexNameBuilder;
+import com.liferay.portal.search.tuning.synonyms.storage.SynonymSetsDatabaseImporter;
+import com.liferay.portal.search.tuning.synonyms.web.internal.index.DocumentToSynonymSetTranslator;
+import com.liferay.portal.search.tuning.synonyms.web.internal.index.SynonymSet;
+import com.liferay.portal.search.tuning.synonyms.web.internal.index.SynonymSetIndexReindexer;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Bryan Engler
+ */
+@Component(service = SynonymSetsDatabaseImporter.class)
+public class SynonymSetsDatabaseImporterImpl
+	implements SynonymSetsDatabaseImporter {
+
+	@Override
+	public void populateDatabase(long companyId) {
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
+
+		SynonymSetIndexName synonymSetIndexName =
+			synonymSetIndexNameBuilder.getSynonymSetIndexName(companyId);
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Importing documents from " +
+					synonymSetIndexName.getIndexName());
+		}
+
+		searchSearchRequest.setIndexNames(synonymSetIndexName.getIndexName());
+
+		searchSearchRequest.setFetchSource(true);
+		searchSearchRequest.setQuery(queries.matchAll());
+
+		SearchSearchResponse searchSearchResponse = searchEngineAdapter.execute(
+			searchSearchRequest);
+
+		SearchHits searchHits = searchSearchResponse.getSearchHits();
+
+		List<SearchHit> searchHitsList = searchHits.getSearchHits();
+
+		for (SearchHit searchHit : searchHitsList) {
+			SynonymSet synonymSet = documentToSynonymSetTranslator.translate(
+				searchHit.getDocument(), searchHit.getId());
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Adding database entry for document with id_ " +
+						synonymSet.getSynonymSetDocumentId());
+			}
+
+			synonymSetJSONStorageHelper.addJSONStorageEntry(
+				companyId, synonymSetIndexName.getIndexName(),
+				synonymSet.getSynonyms());
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Reindexing " + synonymSetIndexName.getIndexName());
+		}
+
+		try {
+			synonymSetIndexReindexer.reindex(new long[] {companyId});
+		}
+		catch (Exception exception) {
+			_log.error(
+				"Unable to reindex " + synonymSetIndexName.getIndexName());
+		}
+	}
+
+	@Reference
+	protected DocumentToSynonymSetTranslator documentToSynonymSetTranslator;
+
+	@Reference
+	protected Queries queries;
+
+	@Reference
+	protected SearchEngineAdapter searchEngineAdapter;
+
+	@Reference
+	protected SynonymSetIndexNameBuilder synonymSetIndexNameBuilder;
+
+	@Reference
+	protected SynonymSetIndexReindexer synonymSetIndexReindexer;
+
+	@Reference
+	protected SynonymSetJSONStorageHelper synonymSetJSONStorageHelper;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SynonymSetsDatabaseImporterImpl.class);
+
+}
