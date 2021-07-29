a5b24a1bcea7a7dda9dc5f533627d5fa73585880
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-29 15:02:43 +0100

    LPS-136283 Add properties panel

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/display/context/EditDDMTemplateDisplayContext.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/display/context/EditDDMTemplateDisplayContext.java
@@ -14,10 +14,12 @@
 
 package com.liferay.template.web.internal.display.context;
 
-import com.liferay.petra.string.StringPool;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalServiceUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 /**
@@ -36,10 +38,30 @@ public class EditDDMTemplateDisplayContext {
 			WebKeys.THEME_DISPLAY);
 	}
 
-	public String getTemplateTypeLabel(long classNameId) {
-		return StringPool.BLANK;
+	public DDMTemplate getDDMTemplate() {
+		if ((_ddmTemplate != null) || (getDDMTemplateId() <= 0)) {
+			return _ddmTemplate;
+		}
+
+		_ddmTemplate = DDMTemplateLocalServiceUtil.fetchDDMTemplate(
+			getDDMTemplateId());
+
+		return _ddmTemplate;
 	}
 
+	protected long getDDMTemplateId() {
+		if (_ddmTemplateId != null) {
+			return _ddmTemplateId;
+		}
+
+		_ddmTemplateId = ParamUtil.getLong(
+			_liferayPortletRequest, "ddmTemplateId");
+
+		return _ddmTemplateId;
+	}
+
+	private DDMTemplate _ddmTemplate;
+	private Long _ddmTemplateId;
 	private final LiferayPortletRequest _liferayPortletRequest;
 	private final LiferayPortletResponse _liferayPortletResponse;
 	private final ThemeDisplay _themeDisplay;
