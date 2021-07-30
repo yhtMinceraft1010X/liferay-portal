90286a967b5ca207750855920e96eee65700b691
Matuzalém Teles <matuzalemteles@gmail.com>
2021-07-30 15:17:06 -0300

    LPS-131940 Regen TypeScript types

--- modules/apps/frontend-js/frontend-js-a11y-web/types/src/main/resources/META-INF/resources/SDK.d.ts
+++ modules/apps/frontend-js/frontend-js-a11y-web/types/src/main/resources/META-INF/resources/SDK.d.ts
@@ -19,12 +19,37 @@ export declare type ChannelEvent<T, K> = {
 	protocol: typeof PROTOCOL;
 };
 export declare type Recv<T, S> = (payload: T, kind?: S) => void;
+
+/**
+ * The communication channel is responsible for implementing the tx and rx
+ * methods, the channel is bidirectional so it will transmit (tx) and
+ * receive (rx) messages through the implementation of a simple protocol to
+ * differentiate messages on the same channel.
+ */
 declare class Channel<T, S> {
 	#private;
 	constructor(onRecv: Recv<T, S>, side: boolean);
+
+	/**
+	 * Transmit the event to the target following the communication protocol so
+	 * that the other side can accept. Kind is the type of event so it can be
+	 * differentiated in the receptor.
+	 */
 	tx<P, K>(target: Window, payload: P, kind?: K): void;
+
+	/**
+	 * Receive is not used directly but is used to verify the identity of
+	 * the protocol and attached to the window listener, if it satisfies
+	 * the condition it calls the callback.
+	 */
 	rx(event: MessageEvent<ChannelEvent<T, S>>): void;
 }
+
+/**
+ * SDK is a simple abstraction to create side channels of communication between
+ * the main window and the iframe the same happens inversely between the iframe
+ * and the main window.
+ */
 export declare class SDK<T, S> {
 	channel: Channel<T, S>;
 	constructor(onRecv: Recv<T, S>, side: boolean);
