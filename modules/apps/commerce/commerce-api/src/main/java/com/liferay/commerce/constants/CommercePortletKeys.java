d0ccebeaff57ffb6f19f0539645961944622d41e
Riccardo Alberti <riccardo.alberti@liferay.com>
2021-07-30 07:51:50 +0200

    COMMERCE-6974 Add order type portlet key

--- modules/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/constants/CommercePortletKeys.java
+++ modules/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/constants/CommercePortletKeys.java
@@ -63,6 +63,10 @@ public class CommercePortletKeys {
 		"com_liferay_commerce_order_content_web_internal_portlet_" +
 			"CommerceOrderContentPortlet";
 
+	public static final String COMMERCE_ORDER_TYPE =
+		"com_liferay_commerce_order_web_internal_portlet_" +
+			"CommerceOrderTypePortlet";
+
 	public static final String COMMERCE_SHIPMENT =
 		"com_liferay_commerce_shipment_web_internal_portlet_" +
 			"CommerceShipmentPortlet";
