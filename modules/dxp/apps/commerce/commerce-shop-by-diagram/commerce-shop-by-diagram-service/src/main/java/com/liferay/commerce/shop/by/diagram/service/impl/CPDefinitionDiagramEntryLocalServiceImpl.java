81f183d0e41c24d3592f352a2bffb85378dd5d76
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2021-07-30 09:35:04 +0200

    COMMERCE-6905 commerce-shop-by-diagram-service added fetch method

--- modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/service/impl/CPDefinitionDiagramEntryLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/service/impl/CPDefinitionDiagramEntryLocalServiceImpl.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 
@@ -127,6 +128,15 @@ public class CPDefinitionDiagramEntryLocalServiceImpl
 		throws PortalException {
 
 		return cpDefinitionDiagramEntryPersistence.findByCPDI_N(
+			cpDefinitionId, number);
+	}
+
+	@Override
+	public CPDefinitionDiagramEntry fetchCPDefinitionDiagramEntry(
+		long cpDefinitionId, int number)
+		throws PortalException {
+
+		return cpDefinitionDiagramEntryPersistence.fetchByCPDI_N(
 			cpDefinitionId, number);
 	}
 
--- modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/service/impl/CPDefinitionDiagramEntryServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/service/impl/CPDefinitionDiagramEntryServiceImpl.java
@@ -114,6 +114,18 @@ public class CPDefinitionDiagramEntryServiceImpl
 	}
 
 	@Override
+	public CPDefinitionDiagramEntry fetchCPDefinitionDiagramEntry(
+		long cpDefinitionId, int number)
+		throws PortalException {
+
+		_cpDefinitionModelResourcePermission.check(
+			getPermissionChecker(), cpDefinitionId, ActionKeys.UPDATE);
+
+		return cpDefinitionDiagramEntryLocalService.fetchCPDefinitionDiagramEntry(
+			cpDefinitionId, number);
+	}
+
+	@Override
 	public CPDefinitionDiagramEntry getCPDefinitionDiagramEntry(
 			long cpDefinitionId, int number)
 		throws PortalException {
