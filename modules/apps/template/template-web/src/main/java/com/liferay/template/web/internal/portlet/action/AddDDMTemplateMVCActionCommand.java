956a26bd37f62328bb97b8d35d929d10286ab9bf
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-30 10:41:55 +0200

    LPS-135490 Implement add action

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/action/AddDDMTemplateMVCActionCommand.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/action/AddDDMTemplateMVCActionCommand.java
@@ -14,14 +14,37 @@
 
 package com.liferay.template.web.internal.portlet.action;
 
+import com.liferay.dynamic.data.mapping.constants.DDMTemplateConstants;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateService;
+import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.template.TemplateHandler;
+import com.liferay.portal.kernel.template.TemplateHandlerRegistryUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.template.constants.TemplatePortletKeys;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Lourdes Fernández Besada
@@ -40,6 +63,65 @@ public class AddDDMTemplateMVCActionCommand extends BaseMVCActionCommand {
 	protected void doProcessAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
+
+		long classNameId = ParamUtil.getLong(actionRequest, "classNameId");
+		long classPK = ParamUtil.getLong(actionRequest, "classPK");
+		long resourceClassNameId = ParamUtil.getLong(
+			actionRequest, "resourceClassNameId");
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Map<Locale, String> nameMap = LocalizationUtil.getLocalizationMap(
+			new String[] {LocaleUtil.toLanguageId(themeDisplay.getLocale())},
+			new String[] {ParamUtil.getString(actionRequest, "name")});
+
+		String script = "<#-- Empty script -->";
+
+		TemplateHandler templateHandler =
+			TemplateHandlerRegistryUtil.getTemplateHandler(resourceClassNameId);
+
+		if (templateHandler == null) {
+			templateHandler = TemplateHandlerRegistryUtil.getTemplateHandler(
+				classNameId);
+		}
+
+		if (templateHandler != null) {
+			script = templateHandler.getTemplatesHelpContent(
+				TemplateConstants.LANG_TYPE_FTL);
+		}
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			DDMTemplate.class.getName(), actionRequest);
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+
+		DDMTemplate ddmTemplate = _ddmTemplateService.addTemplate(
+			themeDisplay.getScopeGroupId(), classNameId, classPK,
+			resourceClassNameId, nameMap, Collections.emptyMap(),
+			DDMTemplateConstants.TEMPLATE_TYPE_DISPLAY, StringPool.BLANK,
+			TemplateConstants.LANG_TYPE_FTL, script, serviceContext);
+
+		JSONPortletResponseUtil.writeJSON(
+			actionRequest, actionResponse,
+			JSONUtil.put(
+				"redirectURL",
+				PortletURLBuilder.createRenderURL(
+					_portal.getLiferayPortletResponse(actionResponse)
+				).setMVCPath(
+					"/edit_ddm_template.jsp"
+				).setRedirect(
+					ParamUtil.getString(actionRequest, "redirect")
+				).setParameter(
+					"ddmTemplateId", ddmTemplate.getTemplateId()
+				).buildString()));
 	}
+
+	@Reference
+	private DDMTemplateService _ddmTemplateService;
+
+	@Reference
+	private Portal _portal;
 
 }
