2cbc7ad2730d86e65fe2a50b7b836f07ded4d097
Riccardo Alberti <riccardo.alberti@liferay.com>
2021-07-30 16:07:40 +0200

    COMMERCE-6974 Add clay dataset views

---
+++ modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/frontend/CommerceOrderTypeClayTableDataSetDisplayView.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.web.internal.frontend;
+
+import com.liferay.commerce.order.web.internal.frontend.constants.CommerceOrderDataSetConstants;
+import com.liferay.frontend.taglib.clay.data.set.ClayDataSetDisplayView;
+import com.liferay.frontend.taglib.clay.data.set.view.table.BaseTableClayDataSetDisplayView;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchema;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaBuilder;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaBuilderFactory;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaField;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Riccardo Alberti
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "clay.data.set.display.name=" + CommerceOrderDataSetConstants.COMMERCE_DATA_SET_KEY_ORDER_TYPES,
+	service = ClayDataSetDisplayView.class
+)
+public class CommerceOrderTypeClayTableDataSetDisplayView
+	extends BaseTableClayDataSetDisplayView {
+
+	@Override
+	public ClayTableSchema getClayTableSchema() {
+		ClayTableSchemaBuilder clayTableSchemaBuilder =
+			_clayTableSchemaBuilderFactory.create();
+
+		ClayTableSchemaField nameClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField("name.LANG", "name");
+
+		nameClayTableSchemaField.setContentRenderer("actionLink");
+
+		clayTableSchemaBuilder.addClayTableSchemaField(
+			"displayOrder", "display-order");
+
+		ClayTableSchemaField activeField =
+			clayTableSchemaBuilder.addClayTableSchemaField("active", "active");
+
+		activeField.setContentRenderer("boolean");
+
+		ClayTableSchemaField startDateClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField(
+				"displayDate", "start-date");
+
+		startDateClayTableSchemaField.setContentRenderer("date");
+		startDateClayTableSchemaField.setSortable(true);
+
+		ClayTableSchemaField endDateClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField(
+				"expirationDate", "end-date");
+
+		endDateClayTableSchemaField.setContentRenderer("date");
+		endDateClayTableSchemaField.setSortable(true);
+
+		ClayTableSchemaField clayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField(
+				"workflowStatusInfo", "status");
+
+		clayTableSchemaField.setContentRenderer("status");
+
+		return clayTableSchemaBuilder.build();
+	}
+
+	@Reference
+	private ClayTableSchemaBuilderFactory _clayTableSchemaBuilderFactory;
+
+}
