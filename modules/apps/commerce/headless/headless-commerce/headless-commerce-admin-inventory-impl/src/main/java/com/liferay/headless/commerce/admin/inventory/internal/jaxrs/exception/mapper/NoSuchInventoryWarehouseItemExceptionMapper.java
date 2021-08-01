a65e92009d7ebc1fb7e3fe81eef1b5a09843db56
Javier Gamarra <nhpatt@gmail.com>
2021-08-01 17:27:18 +0200

    LPS-136542 fix name that was conflicting when an existing provider

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-inventory-impl/src/main/java/com/liferay/headless/commerce/admin/inventory/internal/jaxrs/exception/mapper/NoSuchInventoryWarehouseItemExceptionMapper.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-inventory-impl/src/main/java/com/liferay/headless/commerce/admin/inventory/internal/jaxrs/exception/mapper/NoSuchInventoryWarehouseItemExceptionMapper.java
@@ -31,7 +31,7 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Inventory)",
 		"osgi.jaxrs.extension=true",
-		"osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Inventory.NoSuchInventoryWarehouseException"
+		"osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Inventory.NoSuchInventoryWarehouseItemException"
 	},
 	service = ExceptionMapper.class
 )
