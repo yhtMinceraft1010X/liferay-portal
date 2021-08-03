d92e790554fbca94bdba915e82845b490890c342
Nara Andrade <nara.andrade@liferay.com>
2021-08-03 12:07:58 -0300

    LPS-136355 Create tests

---
+++ modules/apps/portal-workflow/portal-workflow-instance-tracker-web/test/components/state-node/StateNode.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {render} from '@testing-library/react';
+import React from 'react';
+
+import StateNode from '../../../src/main/resources/META-INF/resources/js/components/state-node/StateNode';
+
+describe('The StateNode component should', () => {
+	it('Be rendered without any icon and without current-icon class as default', () => {
+		const {container} = render(<StateNode />);
+
+		expect(container.querySelector('.lexicon-icon-live')).not.toBeTruthy();
+		expect(container.querySelector('.current-icon')).not.toBeTruthy();
+	});
+
+	it('Be rendered with live icon and current-icon class when it receives the current prop', () => {
+		const {container} = render(<StateNode current />);
+
+		expect(container.querySelector('.lexicon-icon-live')).toBeTruthy();
+		expect(container.querySelector('.current-icon')).toBeTruthy();
+	});
+});
