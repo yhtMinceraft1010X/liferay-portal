6a6f45cb37899c5956d98d33c23dfa62167d9fd0
Beltran Rengifo <beltran.rengifo@liferay.com>
2021-08-03 18:10:31 +0200

    LPS-132411 SelectTypeAndSubtype unit testing

---
+++ modules/apps/content-dashboard/content-dashboard-web/test/js/components/SelectTypeAndSubtype.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import SelectTypeAndSubtype from '../../../src/main/resources/META-INF/resources/js/SelectTypeAndSubtype';
+
+const mockProps = {
+	contentDashboardItemTypes: [
+		{
+			icon: 'web-content',
+			itemTypes: [
+				{
+					className:
+						'com.liferay.dynamic.data.mapping.model.DDMStructure',
+					classPK: '40873',
+					label: 'Basic Web Content',
+					selected: true,
+				},
+			],
+			label: 'Web Content Article',
+		},
+		{
+			icon: 'documents-and-media',
+			itemTypes: [
+				{
+					className:
+						'com.liferay.document.library.kernel.model.DLFileEntryType',
+					classPK: '0',
+					label: 'Basic Document',
+				},
+				{
+					className:
+						'com.liferay.document.library.kernel.model.DLFileEntryType',
+					classPK: '40709',
+					label: 'External Video Shortcut',
+				},
+				{
+					className:
+						'com.liferay.document.library.kernel.model.DLFileEntryType',
+					classPK: '40761',
+					label: 'Google Drive Shortcut',
+				},
+			],
+
+			label: 'Document',
+		},
+	],
+	itemSelectorSaveEvent:
+		'_com_liferay_content_dashboard_web_portlet_ContentDashboardAdminPortlet_selectedContentDashboardItemTypeItem',
+	portletNamespace:
+		'_com_liferay_item_selector_web_portlet_ItemSelectorPortlet_',
+};
+
+describe('SelectTypeAndSubtype', () => {
+	beforeEach(() => {
+		cleanup();
+
+		window.Liferay.Util.getOpener = jest.fn().mockReturnValue({
+			Liferay: {
+				fire: jest.fn(),
+			},
+		});
+	});
+
+	it('renders a Treeview with parent nodes and one child expanded', () => {
+		const {getByRole, getByText} = render(
+			<SelectTypeAndSubtype {...mockProps} />
+		);
+
+		const {className} = getByRole('tree');
+		expect(className).toContain('lfr-treeview-node-list');
+
+		expect(getByText('Web Content Article')).toBeInTheDocument();
+		expect(getByText('Document')).toBeInTheDocument();
+		expect(getByText('Basic Web Content')).toBeInTheDocument();
+	});
+
+	it('renders a Treeview with filtered nodes if query is set', () => {
+		const {getByPlaceholderText, getByText, queryByText} = render(
+			<SelectTypeAndSubtype {...mockProps} />
+		);
+
+		const input = getByPlaceholderText('search');
+		fireEvent.change(input, {target: {value: 'external'}});
+		expect(getByText('External Video Shortcut')).toBeInTheDocument();
+		expect(queryByText('Web Content Article')).not.toBeInTheDocument();
+	});
+
+	it('renders a Treeview with a selected nodes if selected is true', () => {
+		const {container} = render(<SelectTypeAndSubtype {...mockProps} />);
+
+		expect(container.getElementsByClassName('selected').length).toBe(1);
+	});
+});
