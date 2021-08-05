0c452486b82bfde33e6e71a90a8b686c06191163
Marco Leo <marco.leo@liferay.com>
2021-08-05 02:43:10 +0200

    LPS-136741 This class is used to show and use fields of the entry as values in the email template

---
+++ modules/apps/commerce/commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/term/contributor/ObjectDefinitionTermContributor.java
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.notification.internal.term.contributor;
+
+import com.liferay.commerce.order.CommerceDefinitionTermContributor;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Marco Leo
+ */
+public class ObjectDefinitionTermContributor
+	implements CommerceDefinitionTermContributor {
+
+	public ObjectDefinitionTermContributor(
+		long objectDefinitionId,
+		ObjectFieldLocalService objectFieldLocalService,
+		UserLocalService userLocalService) {
+
+		_objectDefinitionId = objectDefinitionId;
+		_objectFieldLocalService = objectFieldLocalService;
+		_userLocalService = userLocalService;
+
+		List<ObjectField> objectFields =
+			_objectFieldLocalService.getObjectFields(_objectDefinitionId);
+
+		for (ObjectField objectField : objectFields) {
+			_termsMap.put(
+				_formatTerm(objectField.getName()),
+				objectField.getObjectFieldId());
+		}
+	}
+
+	@Override
+	public Map<String, String> getDefinitionTerms(Locale locale) {
+		Map<String, String> map = new HashMap<>();
+
+		List<String> terms = getTerms();
+
+		for (String term : terms) {
+			map.put(term, getLabel(term, locale));
+		}
+
+		return map;
+	}
+
+	@Override
+	public String getFilledTerm(String term, Object object, Locale locale)
+		throws PortalException {
+
+		if (!(object instanceof ObjectEntry)) {
+			return term;
+		}
+
+		ObjectEntry objectEntry = (ObjectEntry)object;
+
+		if (term.equals(_ENTRY_CREATOR)) {
+			User user = _userLocalService.getUser(objectEntry.getUserId());
+
+			return user.getFullName(true, true);
+		}
+
+		if (term.equals(_ENTRY_ID)) {
+			return String.valueOf(objectEntry.getObjectEntryId());
+		}
+
+		long objectEntryId = _termsMap.get(term);
+
+		ObjectField objectField = _objectFieldLocalService.fetchObjectField(
+			objectEntryId);
+
+		if (objectField != null) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Processing " + objectField.getName() + "term");
+			}
+
+			Map<String, Serializable> values = objectEntry.getValues();
+
+			//TODO Once we support localization we should put code like this in
+
+			// a utility
+
+			return String.valueOf(values.get(objectField.getName()));
+		}
+
+		return term;
+	}
+
+	@Override
+	public String getLabel(String term, Locale locale) {
+		if (term.equals(_ENTRY_CREATOR)) {
+			return LanguageUtil.get(locale, "creator");
+		}
+
+		if (term.equals(_ENTRY_ID)) {
+			return LanguageUtil.get(locale, "id");
+		}
+
+		long objectFieldId = _termsMap.get(term);
+
+		ObjectField objectField = _objectFieldLocalService.fetchObjectField(
+			objectFieldId);
+
+		//TODO Replace with getLabel(locale) when available
+
+		return objectField.getName();
+	}
+
+	@Override
+	public List<String> getTerms() {
+		return new ArrayList<>(_termsMap.keySet());
+	}
+
+	private String _formatTerm(String term) {
+		term = StringUtil.replace(term, ' ', '_');
+		term = StringUtil.toUpperCase(term);
+
+		return "[%" + term + "%]";
+	}
+
+	private static final String _ENTRY_CREATOR = "[%ENTRY_CREATOR%]";
+
+	private static final String _ENTRY_ID = "[%ENTRY_ID%]";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ObjectDefinitionTermContributor.class);
+
+	private static final Map<String, Long> _termsMap = HashMapBuilder.put(
+		_ENTRY_CREATOR, 0L
+	).put(
+		_ENTRY_ID, 0L
+	).build();
+
+	private final long _objectDefinitionId;
+	private final ObjectFieldLocalService _objectFieldLocalService;
+	private final UserLocalService _userLocalService;
+
+}
