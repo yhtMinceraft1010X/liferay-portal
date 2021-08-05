dfb52cafc04661b588126e57751d59452882bebf
Marco Leo <marco.leo@liferay.com>
2021-08-05 02:42:25 +0200

    LPS-136741 Register to model events to send corresponding notification

---
+++ modules/apps/commerce/commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/model/listener/ObjectEntryModelListener.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.notification.internal.model.listener;
+
+import com.liferay.commerce.notification.util.CommerceNotificationHelper;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(enabled = false, immediate = true, service = ModelListener.class)
+public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
+
+	@Override
+	public void onAfterCreate(ObjectEntry objectEntry)
+		throws ModelListenerException {
+
+		super.onAfterCreate(objectEntry);
+
+		_sendNotificationKey("create", objectEntry);
+	}
+
+	@Override
+	public void onAfterRemove(ObjectEntry objectEntry)
+		throws ModelListenerException {
+
+		super.onAfterRemove(objectEntry);
+
+		_sendNotificationKey("delete", objectEntry);
+	}
+
+	@Override
+	public void onAfterUpdate(ObjectEntry objectEntry)
+		throws ModelListenerException {
+
+		super.onAfterUpdate(objectEntry);
+
+		_sendNotificationKey("update", objectEntry);
+	}
+
+	private void _sendNotificationKey(String action, ObjectEntry objectEntry) {
+		try {
+			ObjectDefinition objectDefinition =
+				_objectDefinitionLocalService.getObjectDefinition(
+					objectEntry.getObjectDefinitionId());
+
+			_commerceNotificationHelper.sendNotifications(
+				objectEntry.getGroupId(), objectEntry.getUserId(),
+				objectDefinition.getClassName() + "#" + action, objectEntry);
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(portalException, portalException);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ObjectEntryModelListener.class);
+
+	@Reference
+	private CommerceNotificationHelper _commerceNotificationHelper;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+}
