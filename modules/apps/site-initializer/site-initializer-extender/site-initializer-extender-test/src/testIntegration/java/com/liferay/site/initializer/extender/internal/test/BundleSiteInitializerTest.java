5a55954cec0489fd009c25923d6d0f812f0f7833
Brian Chan <brian.chan@liferay.com>
2021-08-09 20:31:28 -0300

    LPS-136723 Wiring for BundleSiteInitializer

---
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.initializer.extender.internal.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.site.initializer.SiteInitializer;
+import com.liferay.site.initializer.SiteInitializerRegistry;
+
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@RunWith(Arquillian.class)
+public class BundleSiteInitializerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Test
+	public void testInitialize() throws Exception {
+		Bundle testBundle = FrameworkUtil.getBundle(
+			BundleSiteInitializerTest.class);
+
+		Bundle bundle = _installBundle(
+			testBundle.getBundleContext(),
+			"/com.liferay.site.initializer.extender.test.bundle.jar");
+
+		bundle.start();
+
+		SiteInitializer siteInitializer =
+			_siteInitializerRegistry.getSiteInitializer(
+				_bundle.getSymbolicName());
+
+		Group group = GroupTestUtil.addGroup();
+
+		siteInitializer.initialize(group.getGroupId());
+
+		_assertGroup(group);
+
+		GroupLocalServiceUtil.deleteGroup(group);
+
+		bundle.uninstall();
+	}
+
+	private void _assertGroup(Group group) {
+
+		// TODO
+
+		Assert.assertNotNull(group);
+	}
+
+	private Bundle _installBundle(BundleContext bundleContext, String location)
+		throws Exception {
+
+		try (InputStream inputStream =
+				BundleSiteInitializerTest.class.getResourceAsStream(location)) {
+
+			return bundleContext.installBundle(location, inputStream);
+		}
+	}
+
+	private Bundle _bundle;
+
+	@Inject
+	private SiteInitializerRegistry _siteInitializerRegistry;
+
+}
