8f01692a642f8a50a00a85dbecddf34a79fd0b07
Milton Castro <milton.castro@liferay.com>
2021-08-10 20:22:36 -0300

    LPS-136519 Refactor Numeric test cases

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/js/Numeric/Numeric.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/js/Numeric/Numeric.js
@@ -12,6 +12,7 @@
  * details.
  */
 
+import '@testing-library/jest-dom/extend-expect';
 import {cleanup, render} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
@@ -40,80 +41,83 @@ describe('Field Numeric', () => {
 	it('has a name', () => {
 		const {container} = render(<Numeric name="numericField" />);
 
-		expect(container).toMatchSnapshot();
+		const input = container.querySelector('input');
+
+		expect(input).toHaveAttribute('name', 'numericField');
 	});
 
-	it('is not readOnly', () => {
-		const {container} = render(<Numeric readOnly={false} />);
+	it('enables input whenever readOnly is omitted', () => {
+		const {container} = render(<Numeric />);
 
-		expect(container).toMatchSnapshot();
+		const input = container.querySelector('input');
+
+		expect(input).toBeEnabled();
+	});
+
+	it('disables input whenever readOnly is set', () => {
+		const {container} = render(<Numeric readOnly />);
+
+		const input = container.querySelector('input');
+
+		expect(input).toBeDisabled();
 	});
 
 	it('has a helptext', () => {
-		const {container} = render(<Numeric tip="Type something" />);
+		const {getAllByText} = render(<Numeric tip="Type something" />);
 
-		expect(container).toMatchSnapshot();
+		expect(getAllByText('Type something')[0]).toBeInTheDocument();
 	});
 
 	it('has an id', () => {
 		const {container} = render(<Numeric id="ID" />);
 
-		expect(container).toMatchSnapshot();
+		const input = container.querySelector('input');
+
+		expect(input).toHaveAttribute('id', 'ID');
 	});
 
 	it('has a label', () => {
-		const {container} = render(<Numeric label="label" />);
+		const {getByText} = render(<Numeric label="label" />);
 
-		expect(container).toMatchSnapshot();
+		expect(getByText(/label/)).toBeInTheDocument();
 	});
 
 	it('has a placeholder', () => {
 		const {container} = render(<Numeric placeholder="Placeholder" />);
 
-		expect(container).toMatchSnapshot();
+		const input = container.querySelector('input');
+
+		expect(input).toHaveAttribute('placeholder', 'Placeholder');
 	});
 
-	it('is not required', () => {
-		const {container} = render(<Numeric required={false} />);
+	it('is required', () => {
+		const {getByText} = render(<Numeric required />);
 
-		expect(container).toMatchSnapshot();
+		expect(getByText(/required/)).toBeInTheDocument();
 	});
 
 	it('renders Label if showLabel is true', () => {
-		const {container} = render(
-			<Numeric label="Numeric Field" showLabel={true} />
-		);
+		const {getByText} = render(<Numeric label="Numeric Field" showLabel />);
 
-		expect(container).toMatchSnapshot();
+		expect(getByText(/Numeric Field/)).toHaveClass('ddm-label');
 	});
 
 	it('has a value', () => {
 		const {container} = render(<Numeric value="123" />);
 
-		expect(container).toMatchSnapshot();
-	});
+		const input = container.querySelector('input');
 
-	it('has a key', () => {
-		const {container} = render(<Numeric key="key" />);
-
-		expect(container).toMatchSnapshot();
+		expect(input).toHaveValue('123');
 	});
 
 	it('fills with an input number', () => {
-		let output;
-		const {container} = render(
-			<Numeric
-				onChange={({target: {value}}) => {
-					output = value;
-				}}
-			/>
-		);
+		const onChange = jest.fn();
+		const {container} = render(<Numeric onChange={onChange} />);
 
 		const input = container.querySelector('input');
-
 		userEvent.type(input, '2');
 
-		expect(output).toBe('2');
+		expect(onChange).toHaveBeenCalledWith({target: {value: '2'}});
 	});
 
 	it('changes the mask type', () => {
@@ -123,25 +127,15 @@ describe('Field Numeric', () => {
 	});
 
 	it('filters the non numeric characters when set to integer', () => {
-		let output = '';
-		const {container} = render(
-			<Numeric
-				onChange={({target: {value}}) => {
-					output += value;
-				}}
-			/>
-		);
+		const onChange = jest.fn();
+		const {container} = render(<Numeric onChange={onChange} />);
 
 		const input = container.querySelector('input');
+		userEvent.type(input, '3.0');
 
-		// due to an issue on jest was needed to call type() for each key
-		// https://github.com/testing-library/user-event/issues/387
-
-		userEvent.type(input, '3');
-		userEvent.type(input, '.');
-		userEvent.type(input, '0');
-
-		expect(output).toBe('30');
+		expect(onChange).toHaveBeenLastCalledWith({
+			target: {value: '30'},
+		});
 	});
 
 	it('check field value is the same without decimal symbol when fieldType is integer but it receives a double', () => {
@@ -171,7 +165,7 @@ describe('Field Numeric', () => {
 					confirmationValue="22.82"
 					dataType="double"
 					name="numericField"
-					requireConfirmation={true}
+					requireConfirmation
 				/>
 			);
 
@@ -187,7 +181,7 @@ describe('Field Numeric', () => {
 				<Numeric
 					confirmationValue="22.82"
 					name="numericField"
-					requireConfirmation={true}
+					requireConfirmation
 				/>
 			);
 
@@ -203,7 +197,7 @@ describe('Field Numeric', () => {
 		it('has an inputMaskFormat', () => {
 			const {container} = render(
 				<Numeric
-					inputMask={true}
+					inputMask
 					inputMaskFormat="+99 (99) 9999-9999"
 					name="numericField"
 					value="123456789012"
@@ -216,7 +210,7 @@ describe('Field Numeric', () => {
 		it('applies mask to value', () => {
 			const {container} = render(
 				<Numeric
-					inputMask={true}
+					inputMask
 					inputMaskFormat="+99 (99) 9999-9999"
 					value="123456789012"
 				/>
@@ -230,7 +224,7 @@ describe('Field Numeric', () => {
 		it('applies mask to predefined value', () => {
 			const {container} = render(
 				<Numeric
-					inputMask={true}
+					inputMask
 					inputMaskFormat="+99 (99) 9999-9999"
 					predefinedValue="123456789012"
 				/>
@@ -244,7 +238,7 @@ describe('Field Numeric', () => {
 		it('truncates values over mask digit limit', () => {
 			const {container} = render(
 				<Numeric
-					inputMask={true}
+					inputMask
 					inputMaskFormat="+99 (099) 9999-9999"
 					value="12345678901234"
 				/>
@@ -258,7 +252,7 @@ describe('Field Numeric', () => {
 		it('ignores optional digits whenever input is less than mandatory', () => {
 			const {container} = render(
 				<Numeric
-					inputMask={true}
+					inputMask
 					inputMaskFormat="+09 (099) 9999-9999"
 					value="12345"
 				/>
@@ -270,14 +264,12 @@ describe('Field Numeric', () => {
 		});
 
 		it('sends unmasked value though onChange event', () => {
-			let output = null;
+			const onChange = jest.fn();
 			const {container} = render(
 				<Numeric
-					inputMask={true}
+					inputMask
 					inputMaskFormat="+99 (99) 9999-9999"
-					onChange={({target: {value}}) => {
-						output = value;
-					}}
+					onChange={onChange}
 				/>
 			);
 
@@ -285,13 +277,15 @@ describe('Field Numeric', () => {
 
 			userEvent.type(input, '+55 (81) 2121-6000');
 
-			expect(output).toBe('558121216000');
+			expect(onChange).toHaveBeenLastCalledWith({
+				target: {value: '558121216000'},
+			});
 		});
 
 		it('limits predefined value size according to the mask', () => {
 			const {container} = render(
 				<Numeric
-					inputMask={true}
+					inputMask
 					inputMaskFormat="99-99"
 					name="LPS-134259"
 					predefinedValue="12345"
@@ -311,7 +305,7 @@ describe('Field Numeric', () => {
 		xit('it allows to delete non numeric characters from mask', () => {
 			const {container} = render(
 				<Numeric
-					inputMask={true}
+					inputMask
 					inputMaskFormat="99-99"
 					onChange={() => {}}
 					predefinedValue="12"
@@ -334,7 +328,7 @@ describe('Field Numeric', () => {
 					append="$"
 					appendType="suffix"
 					dataType="double"
-					inputMask={true}
+					inputMask
 					name="numericField"
 					value="123"
 				/>
@@ -351,7 +345,7 @@ describe('Field Numeric', () => {
 					append="$"
 					appendType="prefix"
 					dataType="double"
-					inputMask={true}
+					inputMask
 					name="numericField"
 					value="123"
 				/>
@@ -366,7 +360,7 @@ describe('Field Numeric', () => {
 			const {container} = render(
 				<Numeric
 					dataType="double"
-					inputMask={true}
+					inputMask
 					name="numericField"
 					symbols={{decimalSymbol: '.', thousandsSeparator: ','}}
 					value="1234"
@@ -383,7 +377,7 @@ describe('Field Numeric', () => {
 			const {container} = render(
 				<Numeric
 					dataType="double"
-					inputMask={true}
+					inputMask
 					name="numericField"
 					onChange={onChange}
 					symbols={{decimalSymbol: ','}}
@@ -394,6 +388,9 @@ describe('Field Numeric', () => {
 
 			userEvent.type(input, '1,234');
 
+			expect(onChange).toHaveBeenLastCalledWith({
+				target: {value: '1,23'},
+			});
 			expect(onChange.mock.calls[4][0].target.value).toBe('1,23');
 		});
 	});
