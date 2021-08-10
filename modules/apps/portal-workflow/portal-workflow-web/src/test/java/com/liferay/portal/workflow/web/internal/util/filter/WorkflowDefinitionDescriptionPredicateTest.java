f69e65758ebd56a441b8f983e6b7056e40cd49c0
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-08-10 17:35:54 -0300

    LPS-137017 Parameterize WorkflowDefinitionDescriptionPredicateTest

--- modules/apps/portal-workflow/portal-workflow-web/src/test/java/com/liferay/portal/workflow/web/internal/util/filter/WorkflowDefinitionDescriptionPredicateTest.java
+++ modules/apps/portal-workflow/portal-workflow-web/src/test/java/com/liferay/portal/workflow/web/internal/util/filter/WorkflowDefinitionDescriptionPredicateTest.java
@@ -14,17 +14,22 @@
 
 package com.liferay.portal.workflow.web.internal.util.filter;
 
-import com.liferay.portal.kernel.workflow.WorkflowDefinition;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  * @author Adam Brandizzi
  */
+@RunWith(Parameterized.class)
 public class WorkflowDefinitionDescriptionPredicateTest {
 
 	@ClassRule
@@ -32,70 +37,47 @@ public class WorkflowDefinitionDescriptionPredicateTest {
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
 
-	@Test
-	public void testFilterWithoutSpace1() {
-		WorkflowDefinitionDescriptionPredicate predicate =
-			new WorkflowDefinitionDescriptionPredicate("Default");
-
-		WorkflowDefinition workflowDefinition = new WorkflowDefinitionImpl(
-			null, "Single Approver", "Default Single Approver");
-
-		boolean result = predicate.test(workflowDefinition);
-
-		Assert.assertTrue(result);
+	@Parameterized.Parameters(
+		name = "description={0}, expectedResult={1}, keywords={2}, title={3}"
+	)
+	public static List<Object[]> data() {
+		return Arrays.asList(
+			new Object[][] {
+				{"Default Single Approver", true, "Default", "Single Approver"},
+				{"Default Single Approver", true, "Def", "Single Approver"},
+				{"Not that one", false, "Approver", "A Different Definition"},
+				{
+					"Single Approver by Default Default ", true,
+					"Single Approver", "Single Approver Definition"
+				},
+				{
+					"Not that one", false, "Single Approver",
+					"A Different Definition"
+				}
+			});
 	}
 
 	@Test
-	public void testFilterWithoutSpace2() {
+	public void testFilter() {
 		WorkflowDefinitionDescriptionPredicate predicate =
-			new WorkflowDefinitionDescriptionPredicate("Def");
+			new WorkflowDefinitionDescriptionPredicate(keywords);
 
-		WorkflowDefinition workflowDefinition = new WorkflowDefinitionImpl(
-			null, "Single Approver", "Default Single Approver");
-
-		boolean result = predicate.test(workflowDefinition);
-
-		Assert.assertTrue(result);
+		Assert.assertEquals(
+			expectedResult,
+			predicate.test(
+				new WorkflowDefinitionImpl(null, title, description)));
 	}
 
-	@Test
-	public void testFilterWithoutSpace3() {
-		WorkflowDefinitionDescriptionPredicate predicate =
-			new WorkflowDefinitionDescriptionPredicate("Approver");
+	@Parameterized.Parameter
+	public String description;
 
-		WorkflowDefinition workflowDefinition = new WorkflowDefinitionImpl(
-			null, "A Different Definition", "Not that one");
+	@Parameterized.Parameter(1)
+	public boolean expectedResult;
 
-		boolean result = predicate.test(workflowDefinition);
+	@Parameterized.Parameter(2)
+	public String keywords;
 
-		Assert.assertFalse(result);
-	}
-
-	@Test
-	public void testFilterWithSpace1() {
-		WorkflowDefinitionDescriptionPredicate predicate =
-			new WorkflowDefinitionDescriptionPredicate("Single Approver");
-
-		WorkflowDefinition workflowDefinition = new WorkflowDefinitionImpl(
-			null, "Single Approver Definition",
-			"Single Approver by Default Default ");
-
-		boolean result = predicate.test(workflowDefinition);
-
-		Assert.assertTrue(result);
-	}
-
-	@Test
-	public void testFilterWithSpace2() {
-		WorkflowDefinitionDescriptionPredicate predicate =
-			new WorkflowDefinitionDescriptionPredicate("Single Approver");
-
-		WorkflowDefinition workflowDefinition = new WorkflowDefinitionImpl(
-			null, "A Different Definition", "Not that one");
-
-		boolean result = predicate.test(workflowDefinition);
-
-		Assert.assertFalse(result);
-	}
+	@Parameterized.Parameter(3)
+	public String title;
 
 }
