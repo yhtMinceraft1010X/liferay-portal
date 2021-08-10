3afa017f556ab3b5391f7f4262b8721442ce5c32
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-08-10 14:20:17 +0200

    LPS-136289 Render a <script> node for each custom element source

---
+++ modules/apps/custom-elements/custom-elements-web/src/main/java/com/liferay/custom/elements/web/internal/servlet/taglib/CustomElementsSourceDynamicInclude.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.custom.elements.web.internal.servlet.taglib;
+
+import com.liferay.custom.elements.model.CustomElementsSource;
+import com.liferay.custom.elements.service.CustomElementsSourceLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class CustomElementsSourceDynamicInclude implements DynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		List<CustomElementsSource> customElementsSources =
+			_customElementsSourceLocalService.getCustomElementsSources(
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		for (CustomElementsSource customElementsSource :
+				customElementsSources) {
+
+			printWriter.print(
+				"<script src=\"" + customElementsSource.getUrl() +
+					"\"></script>");
+		}
+
+		printWriter.flush();
+	}
+
+	@Override
+	public void register(
+		DynamicInclude.DynamicIncludeRegistry dynamicIncludeRegistry) {
+
+		dynamicIncludeRegistry.register(
+			"/html/common/themes/top_js.jspf#resources");
+	}
+
+	@Reference
+	private CustomElementsSourceLocalService _customElementsSourceLocalService;
+
+}
