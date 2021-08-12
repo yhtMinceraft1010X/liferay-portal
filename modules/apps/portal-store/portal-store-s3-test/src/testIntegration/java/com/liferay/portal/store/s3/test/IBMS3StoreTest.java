2bc230ed9514ed8821e520c143cdb2f22becd306
Kyle Miho <kyle.miho@liferay.com>
2021-08-11 17:07:08 -0700

    LRQA-68051 Remove un-needed since configuration is handled at the batch level

--- modules/apps/portal-store/portal-store-s3-test/src/testIntegration/java/com/liferay/portal/store/s3/test/IBMS3StoreTest.java
+++ modules/apps/portal-store/portal-store-s3-test/src/testIntegration/java/com/liferay/portal/store/s3/test/IBMS3StoreTest.java
@@ -17,11 +17,8 @@ package com.liferay.portal.store.s3.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.document.library.kernel.store.Store;
 import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.AssumeTestRule;
-import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.store.test.util.BaseStoreTestCase;
@@ -30,15 +27,10 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import javax.annotation.Generated;
 
-import org.junit.AfterClass;
 import org.junit.Assume;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 
 /**
  * @author Preston Crary
@@ -65,47 +57,10 @@ public class IBMS3StoreTest extends BaseStoreTestCase {
 			dlStoreImpl.equals(s3StoreClassName));
 	}
 
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-		_configuration = _configurationAdmin.getConfiguration(
-			"com.liferay.portal.store.s3.configuration.S3StoreConfiguration",
-			StringPool.QUESTION);
-
-		ConfigurationTestUtil.saveConfiguration(
-			_configuration,
-			HashMapDictionaryBuilder.<String, Object>put(
-				"accessKey", ""
-			).put(
-				"bucketName", "test"
-			).put(
-				"httpClientMaxConnections", "50"
-			).put(
-				"s3Region", "us-east-1"
-			).put(
-				"s3StorageClass", "STANDARD"
-			).put(
-				"secretKey", ""
-			).put(
-				"tempDirCleanUpExpunge", "7"
-			).put(
-				"tempDirCleanUpFrequency", "100"
-			).build());
-	}
-
-	@AfterClass
-	public static void tearDownClass() throws Exception {
-		ConfigurationTestUtil.deleteConfiguration(_configuration);
-	}
-
 	@Override
 	protected Store getStore() {
 		return _store;
 	}
-
-	private static Configuration _configuration;
-
-	@Inject
-	private static ConfigurationAdmin _configurationAdmin;
 
 	@Inject(
 		filter = "store.type=com.liferay.portal.store.s3.IBMS3Store",
