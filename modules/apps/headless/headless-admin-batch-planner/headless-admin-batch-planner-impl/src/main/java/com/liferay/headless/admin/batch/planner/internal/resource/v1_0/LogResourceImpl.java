78ff5db154d6cfbac8ae824fb376fcd1bbf30174
Matija Petanjek <matija.petanjek@liferay.com>
2021-08-12 11:45:55 +0200

    COMMERCE-6491 headless-admin-batch-planner - buildREST

---
+++ modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-impl/src/main/java/com/liferay/headless/admin/batch/planner/internal/resource/v1_0/LogResourceImpl.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.batch.planner.internal.resource.v1_0;
+
+import com.liferay.headless.admin.batch.planner.resource.v1_0.LogResource;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Matija Petanjek
+ */
+@Component(
+	properties = "OSGI-INF/liferay/rest/v1_0/log.properties",
+	scope = ServiceScope.PROTOTYPE, service = LogResource.class
+)
+public class LogResourceImpl extends BaseLogResourceImpl {
+}
