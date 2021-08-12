df851363ce3b2cb625afb178cd265c4828bafe42
drewbrokke <drew.brokke@liferay.com>
2021-08-12 16:42:19 -0500

    LPS-135181 account-service: AccountEntryUserRelImpl: adds methods to fetch related models

--- modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryUserRelImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryUserRelImpl.java
@@ -14,8 +14,37 @@
 
 package com.liferay.account.model.impl;
 
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.service.AccountEntryLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+
 /**
  * @author Brian Wing Shun Chan
  */
 public class AccountEntryUserRelImpl extends AccountEntryUserRelBaseImpl {
+
+	@Override
+	public AccountEntry fetchAccountEntry() {
+		return AccountEntryLocalServiceUtil.fetchAccountEntry(
+			getAccountEntryId());
+	}
+
+	@Override
+	public User fetchUser() {
+		return UserLocalServiceUtil.fetchUser(getAccountUserId());
+	}
+
+	@Override
+	public AccountEntry getAccountEntry() throws PortalException {
+		return AccountEntryLocalServiceUtil.getAccountEntry(
+			getAccountEntryId());
+	}
+
+	@Override
+	public User getUser() throws PortalException {
+		return UserLocalServiceUtil.getUser(getAccountUserId());
+	}
+
 }
