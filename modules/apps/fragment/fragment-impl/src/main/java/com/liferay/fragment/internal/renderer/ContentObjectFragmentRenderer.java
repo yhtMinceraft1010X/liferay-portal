ade8903b5f4bae75a9e37af3f9aeb120099c0e6c
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-08-13 11:27:13 +0200

    LPS-132502 Use display object itemType request attribute

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/ContentObjectFragmentRenderer.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/ContentObjectFragmentRenderer.java
@@ -288,6 +288,13 @@ public class ContentObjectFragmentRenderer implements FragmentRenderer {
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
 
+		String itemType = (String)httpServletRequest.getAttribute(
+			InfoDisplayWebKeys.INFO_LIST_DISPLAY_OBJECT_ITEM_TYPE);
+
+		if (Validator.isNull(className) && Validator.isNotNull(itemType)) {
+			className = itemType;
+		}
+
 		LayoutDisplayPageProvider<?> layoutDisplayPageProvider =
 			(LayoutDisplayPageProvider<?>)httpServletRequest.getAttribute(
 				LayoutDisplayPageWebKeys.LAYOUT_DISPLAY_PAGE_PROVIDER);
