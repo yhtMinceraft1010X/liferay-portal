5e86ef0ac9abd701dbb2d4d877ad60a6ce86e795
pei-jung <pei-jung.lan@liferay.com>
2021-08-13 08:44:49 -0700

    LPS-132250 Exclude on-demand admin users from user searches

---
+++ modules/apps/on-demand-admin/on-demand-admin-impl/src/main/java/com/liferay/on/demand/admin/internal/search/spi/model/query/contributor/UserModelPreFilterContributor.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.on.demand.admin.internal.search.spi.model.query.contributor;
+
+import com.liferay.on.demand.admin.constants.OnDemandAdminConstants;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.PrefixFilter;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.portal.kernel.model.User",
+	service = ModelPreFilterContributor.class
+)
+public class UserModelPreFilterContributor
+	implements ModelPreFilterContributor {
+
+	@Override
+	public void contribute(
+		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
+		SearchContext searchContext) {
+
+		booleanFilter.add(
+			new PrefixFilter(
+				"screenName",
+				OnDemandAdminConstants.ON_DEMAND_ADMIN_SCREEN_NAME_PREFIX),
+			BooleanClauseOccur.MUST_NOT);
+	}
+
+}
