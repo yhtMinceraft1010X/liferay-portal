1de4fbaba345d3f4a0d6c7b04afe0f086c24da1f
Guilherme Camacho <guilherme.camacho@liferay.com>
2021-08-13 13:00:08 -0300

    LPS-137361 Create class LocalizedMapUtil

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/util/LocalizedMapUtil.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.util;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Guilherme Camacho
+ */
+public class LocalizedMapUtil {
+
+	public static Map<Locale, String> getLocalizedMap(
+		String labelKey, Locale locale) {
+
+		return Collections.singletonMap(
+			locale, LanguageUtil.get(locale, labelKey));
+	}
+
+}
