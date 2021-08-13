ed578b9ede846ae2ddd0c47458a7c211f17bc32c
pei-jung <pei-jung.lan@liferay.com>
2021-08-12 18:21:32 -0700

    LPS-132250 Create on-demand admin user and generate token for that user

---
+++ modules/apps/on-demand-admin/on-demand-admin-api/src/main/java/com/liferay/on/demand/admin/constants/OnDemandAdminConstants.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.on.demand.admin.constants;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public class OnDemandAdminConstants {
+
+	public static final String ON_DEMAND_ADMIN_SCREEN_NAME_PREFIX =
+		"on_demand_admin";
+
+	public static final int TICKET_TYPE_ON_DEMAND_ADMIN_LOGIN = 4;
+
+}
---
+++ modules/apps/on-demand-admin/on-demand-admin-impl/src/main/java/com/liferay/on/demand/admin/internal/ticket/generator/OnDemandAdminTicketGeneratorImpl.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.on.demand.admin.internal.ticket.generator;
+
+import com.liferay.on.demand.admin.constants.OnDemandAdminConstants;
+import com.liferay.on.demand.admin.ticket.generator.OnDemandAdminTicketGenerator;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.Ticket;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.TicketLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.util.PropsValues;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(immediate = true, service = OnDemandAdminTicketGenerator.class)
+public class OnDemandAdminTicketGeneratorImpl
+	implements OnDemandAdminTicketGenerator {
+
+	public Ticket generate(Company company, long requestorUserId)
+		throws PortalException {
+
+		User user = _addOnDemandAdminUser(company, requestorUserId);
+
+		return _ticketLocalService.addDistinctTicket(
+			user.getCompanyId(), User.class.getName(), user.getUserId(),
+			OnDemandAdminConstants.TICKET_TYPE_ON_DEMAND_ADMIN_LOGIN, null,
+			new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(5)),
+			null);
+	}
+
+	private User _addOnDemandAdminUser(Company company, long userId)
+		throws PortalException {
+
+		User requestorUser = _userLocalService.getUser(userId);
+
+		Date date = requestorUser.getBirthday();
+
+		Role role = _roleLocalService.getRole(
+			company.getCompanyId(), RoleConstants.ADMINISTRATOR);
+
+		User user = _userLocalService.addUser(
+			requestorUser.getUserId(), company.getCompanyId(), false,
+			PropsValues.DEFAULT_ADMIN_PASSWORD,
+			PropsValues.DEFAULT_ADMIN_PASSWORD, true, null,
+			requestorUser.getEmailAddress(), requestorUser.getLocale(),
+			requestorUser.getFirstName(), requestorUser.getMiddleName(),
+			requestorUser.getLastName(), 0, 0, requestorUser.getMale(),
+			date.getMonth(), date.getDay(), date.getYear(), null, null, null,
+			new long[] {role.getRoleId()}, null, false, new ServiceContext());
+
+		String screenName = _getScreenName(
+			requestorUser.getUserId(), user.getUserId());
+
+		user.setScreenName(screenName);
+		user.setEmailAddress(screenName + StringPool.AT + company.getMx());
+
+		user.setEmailAddressVerified(true);
+
+		return _userLocalService.updateUser(user);
+	}
+
+	private String _getScreenName(long requestorUserId, long userId)
+		throws PortalException {
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append(OnDemandAdminConstants.ON_DEMAND_ADMIN_SCREEN_NAME_PREFIX);
+		sb.append(StringPool.UNDERLINE);
+		sb.append(requestorUserId);
+		sb.append(StringPool.UNDERLINE);
+		sb.append(userId);
+
+		return sb.toString();
+	}
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+	@Reference
+	private TicketLocalService _ticketLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
