be0b9a07a3fe87271dfd9b3108a36d415adf4863
Guilherme Camacho <guilherme.camacho@liferay.com>
2021-08-13 12:57:30 -0300

    LPS-137361 Create and export class ObjectFieldUtil

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/util/ObjectFieldUtil.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.util;
+
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectFieldLocalServiceUtil;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Guilherme Camacho
+ */
+public class ObjectFieldUtil {
+
+	public static ObjectField createObjectField(
+		Locale defaultLocale, boolean indexed, boolean indexedAsKeyword,
+		String label, String name, boolean required, String type) {
+
+		return createObjectField(
+			null, indexed, indexedAsKeyword, null,
+			Collections.singletonMap(defaultLocale, label), name, required,
+			type);
+	}
+
+	public static ObjectField createObjectField(
+		Locale defaultLocale, String label, String name, boolean required,
+		String type) {
+
+		return createObjectField(
+			null, Collections.singletonMap(defaultLocale, label), name,
+			required, type);
+	}
+
+	public static ObjectField createObjectField(
+		String dbColumnName, boolean indexed, boolean indexedAsKeyword,
+		String indexedLanguageId, Map<Locale, String> labelMap, String name,
+		boolean required, String type) {
+
+		ObjectField objectField = ObjectFieldLocalServiceUtil.createObjectField(
+			0);
+
+		objectField.setDBColumnName(dbColumnName);
+		objectField.setIndexed(indexed);
+		objectField.setIndexedAsKeyword(indexedAsKeyword);
+		objectField.setIndexedLanguageId(indexedLanguageId);
+		objectField.setLabelMap(labelMap);
+		objectField.setName(name);
+		objectField.setRequired(required);
+		objectField.setType(type);
+
+		return objectField;
+	}
+
+	public static ObjectField createObjectField(
+		String dbColumnName, Map<Locale, String> labelMap, String name,
+		boolean required, String type) {
+
+		return createObjectField(
+			dbColumnName, false, false, null, labelMap, name, required, type);
+	}
+
+}
