cf1aff183b932d0ccd293bf4fc7bbd3b096e5691
Brian Chan <brian.chan@liferay.com>
2021-08-13 13:45:46 -0300

    LPS-124898 wordsmith

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/scheduler/OpenIdConnectTokenRefreshScheduler.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/scheduler/OpenIdConnectTokenRefreshScheduler.java
@@ -143,7 +143,7 @@ public class OpenIdConnectTokenRefreshScheduler {
 			return;
 		}
 
-		// Offset is changed. Reschedule every OpenId Connect token refresh.
+		// Offset changed. Reschedule every OpenId Connect token refresh.
 		// For each token refresh, access token lifetime is not changed.
 
 		for (SchedulerResponse schedulerResponse :
