fe4292635117891adb86d2ccf2756b6b2e0da434
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2021-08-16 19:32:11 -0300

    LPS-135943 Add integration test for new MVCResourceCommand

---
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/web/internal/portlet/action/test/ExportDataDefinitionMVCActionCommandTest.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.portlet.action.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.data.engine.rest.dto.v2_0.DataDefinition;
+import com.liferay.data.engine.rest.test.util.DataDefinitionTestUtil;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.test.portlet.MockLiferayResourceRequest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayResourceResponse;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.ByteArrayOutputStream;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.skyscreamer.jsonassert.JSONAssert;
+
+/**
+ * @author Rodrigo Paulino
+ */
+@RunWith(Arquillian.class)
+public class ExportDataDefinitionMVCActionCommandTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testServeResource() throws Exception {
+		Class<?> clazz = getClass();
+
+		String json = StringUtil.read(
+			clazz.getResourceAsStream(
+				"dependencies/valid_data_definition.json"));
+
+		DataDefinition dataDefinition =
+			DataDefinitionTestUtil.addDataDefinition(
+				"journal", TestPropsValues.getGroupId(), json,
+				TestPropsValues.getUser());
+
+		MockLiferayResourceRequest mockLiferayResourceRequest =
+			_createMockLiferayResourceRequest(dataDefinition.getId());
+
+		MockLiferayResourceResponse mockLiferayResourceResponse =
+			new MockLiferayResourceResponse();
+
+		_mvcResourceCommand.serveResource(
+			mockLiferayResourceRequest, mockLiferayResourceResponse);
+
+		_assertResponse(json, mockLiferayResourceResponse);
+	}
+
+	private void _assertResponse(
+			String json,
+			MockLiferayResourceResponse mockLiferayResourceResponse)
+		throws Exception {
+
+		ByteArrayOutputStream byteArrayOutputStream =
+			(ByteArrayOutputStream)
+				mockLiferayResourceResponse.getPortletOutputStream();
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			new String(byteArrayOutputStream.toByteArray()));
+
+		JSONAssert.assertEquals(json, jsonObject.toJSONString(), false);
+	}
+
+	private MockLiferayResourceRequest _createMockLiferayResourceRequest(
+			long dataDefinitionId)
+		throws Exception {
+
+		MockLiferayResourceRequest mockLiferayResourceRequest =
+			new MockLiferayResourceRequest();
+
+		mockLiferayResourceRequest.addParameter(
+			"dataDefinitionId", String.valueOf(dataDefinitionId));
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		mockLiferayResourceRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		return mockLiferayResourceRequest;
+	}
+
+	@Inject(filter = "mvc.command.name=/journal/export_data_definition")
+	private MVCResourceCommand _mvcResourceCommand;
+
+}
