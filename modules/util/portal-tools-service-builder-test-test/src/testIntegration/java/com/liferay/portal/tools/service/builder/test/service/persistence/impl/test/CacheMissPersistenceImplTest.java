fbcdd53cfe6fb890c6d090db95307db5bac40ca7
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2021-08-16 12:32:24 +0200

    LPS-133366 Ensure that database.in.max.parameters gets honored if CTPersistenceHelperUtil.isProductionMode is false.

--- modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/service/persistence/impl/test/CacheMissPersistenceImplTest.java
+++ modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/service/persistence/impl/test/CacheMissPersistenceImplTest.java
@@ -15,13 +15,32 @@
 package com.liferay.portal.tools.service.builder.test.service.persistence.impl.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.constants.CTConstants;
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.model.CTEntry;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTCollectionService;
+import com.liferay.change.tracking.service.CTEntryLocalService;
+import com.liferay.petra.function.UnsafeFunction;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.change.tracking.CTService;
+import com.liferay.portal.kernel.service.persistence.change.tracking.CTPersistence;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.TransactionalTestRule;
 import com.liferay.portal.tools.service.builder.test.model.CacheMissEntry;
 import com.liferay.portal.tools.service.builder.test.service.persistence.CacheMissEntryPersistence;
 import com.liferay.portal.tools.service.builder.test.service.persistence.CacheMissEntryUtil;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceRegistration;
 
 import java.io.Serializable;
 
@@ -29,8 +48,9 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -51,25 +71,100 @@ public class CacheMissPersistenceImplTest {
 				Propagation.REQUIRED,
 				"com.liferay.portal.tools.service.builder.test.service"));
 
-	@Before
-	public void setUp() {
+	@BeforeClass
+	public static void setUpClass() throws Exception {
 		_persistence = CacheMissEntryUtil.getPersistence();
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		_serviceRegistration = registry.registerService(
+			CTService.class,
+			new CTService<CacheMissEntry>() {
+
+				public CTPersistence<CacheMissEntry> getCTPersistence() {
+					return _persistence;
+				}
+
+				public Class<CacheMissEntry> getModelClass() {
+					return CacheMissEntry.class;
+				}
+
+				public <R, E extends Throwable> R updateWithUnsafeFunction(
+					UnsafeFunction<CTPersistence<CacheMissEntry>, R, E>
+						updateUnsafeFunction) {
+
+					return null;
+				}
+
+			});
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_serviceRegistration.unregister();
 	}
 
 	@Test
-	public void testCacheMiss() throws Throwable {
+	public void testCacheMissIfCTProductionModeDisabled() throws Throwable {
+		_ctCollection = _ctCollectionLocalService.addCTCollection(
+			TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
+			CacheMissPersistenceImplTest.class.getSimpleName(), null);
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection.getCtCollectionId())) {
+
+			_ctEntryLocalService.addCTEntry(
+				_ctCollection.getCtCollectionId(),
+				_classNameLocalService.getClassNameId(
+					CacheMissEntry.class.getName()),
+				CacheMissEntryUtil.create(RandomTestUtil.nextLong()),
+				TestPropsValues.getUserId(),
+				CTConstants.CT_CHANGE_TYPE_ADDITION);
+
+			Assert.assertTrue(_fetchByPrimaryKeys().isEmpty());
+		}
+	}
+
+	@Test
+	public void testCacheMissIfCTProductionModeEnabled() throws Throwable {
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					CTConstants.CT_COLLECTION_ID_PRODUCTION)) {
+
+			Assert.assertTrue(_fetchByPrimaryKeys().isEmpty());
+		}
+	}
+
+	private Map<Serializable, CacheMissEntry> _fetchByPrimaryKeys() {
 		Set<Serializable> primaryKeys = new HashSet<>();
 
 		for (long pk = -1; pk > -2000; pk--) {
 			primaryKeys.add(pk);
 		}
 
-		Map<Serializable, CacheMissEntry> cacheMissEntries =
-			_persistence.fetchByPrimaryKeys(primaryKeys);
-
-		Assert.assertTrue(cacheMissEntries.isEmpty());
+		return _persistence.fetchByPrimaryKeys(primaryKeys);
 	}
 
-	private CacheMissEntryPersistence _persistence;
+	@Inject
+	private static ClassNameLocalService _classNameLocalService;
+
+	@Inject
+	private static CTCollectionLocalService _ctCollectionLocalService;
+
+	@Inject
+	private static CTCollectionService _ctCollectionService;
+
+	@Inject
+	private static CTEntryLocalService _ctEntryLocalService;
+
+	private static CacheMissEntryPersistence _persistence;
+	private static ServiceRegistration<CTService> _serviceRegistration;
+
+	@DeleteAfterTestRun
+	private CTCollection _ctCollection;
+
+	@DeleteAfterTestRun
+	private CTEntry _ctEntry;
 
 }
