630b5b3958b3355d5eea4327d943c53c18bc700c
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-08-16 14:39:41 +0200

    LPS-137400 Adds a related info item collection for blogs fort a given category

---
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/info/collection/provider/BlogsEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider.java
@@ -0,0 +1,225 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.web.internal.info.collection.provider;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
+import com.liferay.asset.util.AssetHelper;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryService;
+import com.liferay.info.collection.provider.CollectionQuery;
+import com.liferay.info.collection.provider.RelatedInfoItemCollectionProvider;
+import com.liferay.info.pagination.InfoPage;
+import com.liferay.info.pagination.Pagination;
+import com.liferay.info.sort.Sort;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoader;
+import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoaderUtil;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchContextFactory;
+import com.liferay.portal.kernel.search.SearchResult;
+import com.liferay.portal.kernel.search.SearchResultUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.io.Serializable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = RelatedInfoItemCollectionProvider.class)
+public class BlogsEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider
+	implements RelatedInfoItemCollectionProvider<AssetCategory, BlogsEntry> {
+
+	@Override
+	public InfoPage<BlogsEntry> getCollectionInfoPage(
+		CollectionQuery collectionQuery) {
+
+		Optional<Object> relatedItemOptional =
+			collectionQuery.getRelatedItemObjectOptional();
+
+		Object relatedItem = relatedItemOptional.orElse(null);
+
+		if (!(relatedItem instanceof AssetCategory)) {
+			return InfoPage.of(
+				Collections.emptyList(), collectionQuery.getPagination(), 0);
+		}
+
+		AssetEntryQuery assetEntryQuery = _getAssetEntryQuery(collectionQuery);
+
+		try {
+			AssetCategory assetCategory = (AssetCategory)relatedItem;
+
+			SearchContext searchContext = _getSearchContext(assetCategory);
+
+			Hits hits = _assetHelper.search(
+				searchContext, assetEntryQuery, assetEntryQuery.getStart(),
+				assetEntryQuery.getEnd());
+
+			Long count = _assetHelper.searchCount(
+				searchContext, assetEntryQuery);
+
+			List<SearchResult> searchResults =
+				SearchResultUtil.getSearchResults(
+					hits, LocaleUtil.getDefault());
+
+			Stream<SearchResult> stream = searchResults.stream();
+
+			return InfoPage.of(
+				stream.map(
+					this::_toBlogsEntryOptional
+				).filter(
+					Optional::isPresent
+				).map(
+					Optional::get
+				).collect(
+					Collectors.toList()
+				),
+				collectionQuery.getPagination(), count.intValue());
+		}
+		catch (Exception exception) {
+			_log.error("Unable to get blogs entries", exception);
+		}
+
+		return InfoPage.of(
+			Collections.emptyList(), collectionQuery.getPagination(), 0);
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		Bundle bundle = FrameworkUtil.getBundle(getClass());
+
+		ResourceBundleLoader resourceBundleLoader =
+			ResourceBundleLoaderUtil.
+				getResourceBundleLoaderByBundleSymbolicName(
+					bundle.getSymbolicName());
+
+		ResourceBundle resourceBundle = resourceBundleLoader.loadResourceBundle(
+			locale);
+
+		return LanguageUtil.get(resourceBundle, "blogs-with-this-category");
+	}
+
+	private AssetEntryQuery _getAssetEntryQuery(
+		CollectionQuery collectionQuery) {
+
+		AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		assetEntryQuery.setClassNameIds(
+			new long[] {_portal.getClassNameId(BlogsEntry.class.getName())});
+		assetEntryQuery.setEnablePermissions(true);
+		assetEntryQuery.setGroupIds(
+			new long[] {serviceContext.getScopeGroupId()});
+
+		Pagination pagination = collectionQuery.getPagination();
+
+		if (pagination != null) {
+			assetEntryQuery.setStart(pagination.getStart());
+			assetEntryQuery.setEnd(pagination.getEnd());
+		}
+
+		assetEntryQuery.setOrderByCol1(Field.MODIFIED_DATE);
+
+		Optional<Sort> sortOptional = collectionQuery.getSortOptional();
+
+		Sort sort = sortOptional.orElse(null);
+
+		if ((sort != null) && sort.isReverse()) {
+			assetEntryQuery.setOrderByType1("ASC");
+		}
+		else {
+			assetEntryQuery.setOrderByType1("DESC");
+		}
+
+		return assetEntryQuery;
+	}
+
+	private SearchContext _getSearchContext(AssetCategory assetCategory) {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		return SearchContextFactory.getInstance(
+			new long[] {assetCategory.getCategoryId()}, new String[0],
+			HashMapBuilder.<String, Serializable>put(
+				Field.STATUS, WorkflowConstants.STATUS_APPROVED
+			).put(
+				"head", true
+			).put(
+				"latest", true
+			).build(),
+			serviceContext.getCompanyId(), null, themeDisplay.getLayout(), null,
+			serviceContext.getScopeGroupId(), null, serviceContext.getUserId());
+	}
+
+	private Optional<BlogsEntry> _toBlogsEntryOptional(
+		SearchResult searchResult) {
+
+		try {
+			return Optional.of(
+				_blogsEntryService.getEntry(searchResult.getClassPK()));
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Blogs search index is stale and contains entry " +
+						searchResult.getClassPK(),
+					exception);
+			}
+
+			return Optional.empty();
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BlogsEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider.
+			class);
+
+	@Reference
+	private AssetHelper _assetHelper;
+
+	@Reference
+	private BlogsEntryService _blogsEntryService;
+
+	@Reference
+	private Portal _portal;
+
+}
