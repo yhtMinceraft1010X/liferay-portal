e747bd1a02bdd1d90742268fb44d49b902f893dc
Brian Chan <brian.chan@liferay.com>
2021-08-16 11:15:14 -0300

    COMMERCE-3777 Service Buidler *abc-service should not use abc.service.internal, but just abc.internal, Hugo SF, please enforce

--- modules/apps/portal-instances/portal-instances-service/src/main/java/com/liferay/portal/instances/internal/initializer/PortalInstanceInitializerRegistryImpl.java
+++ modules/apps/portal-instances/portal-instances-service/src/main/java/com/liferay/portal/instances/internal/initializer/PortalInstanceInitializerRegistryImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.instances.service.internal.initializer;
+package com.liferay.portal.instances.internal.initializer;
 
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
