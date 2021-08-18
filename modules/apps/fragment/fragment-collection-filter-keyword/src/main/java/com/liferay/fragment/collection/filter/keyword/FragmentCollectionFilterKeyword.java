7cbebe094cb50035ee25a2d38965cac0895d382f
Victor Galan <victor.galan@liferay.com>
2021-08-18 11:48:27 +0200

    LPS-137562 Add filter implementation

---
+++ modules/apps/fragment/fragment-collection-filter-keyword/src/main/java/com/liferay/fragment/collection/filter/keyword/FragmentCollectionFilterKeyword.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.collection.filter.keyword;
+
+import com.liferay.fragment.collection.filter.FragmentCollectionFilter;
+import com.liferay.fragment.renderer.FragmentRendererContext;
+import com.liferay.fragment.util.configuration.FragmentEntryConfigurationParser;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Víctor Galán
+ */
+@Component(immediate = true, service = FragmentCollectionFilter.class)
+public class FragmentCollectionFilterKeyword
+	implements FragmentCollectionFilter {
+
+	@Override
+	public String getConfiguration() {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", LocaleUtil.getMostRelevantLocale(), getClass());
+
+		try {
+			String json = StringUtil.read(
+				getClass(),
+				"/com/liferay/fragment/collection/filter/keyword/dependencies" +
+					"/configuration.json");
+
+			return _fragmentEntryConfigurationParser.translateConfiguration(
+				JSONFactoryUtil.createJSONObject(json), resourceBundle);
+		}
+		catch (JSONException jsonException) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Override
+	public String getFilterKey() {
+		return "keywords";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "keywords");
+	}
+
+	@Override
+	public void render(
+		FragmentRendererContext fragmentRendererContext,
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+	}
+
+	@Reference
+	private FragmentEntryConfigurationParser _fragmentEntryConfigurationParser;
+
+}
