a37e2e20d31fe92490ef37caab089394bfc36bf0
Luca Pellizzon <luca.pellizzon@liferay.com>
2021-08-18 15:14:00 +0200

    COMMERCE-5675 externalReferenceCode is readOnly property

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/resource/v1_0/UserAccountResourceImpl.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/resource/v1_0/UserAccountResourceImpl.java
@@ -550,13 +550,6 @@ public class UserAccountResourceImpl
 			user = _userService.getUserById(user.getUserId());
 		}
 
-		if (userAccount.getExternalReferenceCode() != null) {
-			user.setExternalReferenceCode(
-				userAccount.getExternalReferenceCode());
-
-			user = _userLocalService.updateUser(user);
-		}
-
 		return _toUserAccount(user);
 	}
 
