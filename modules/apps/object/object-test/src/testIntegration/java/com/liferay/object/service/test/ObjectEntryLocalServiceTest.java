2124c095b5f6a3ba3a28632c6b13e9efc7ede8ca
Brian Chan <brian.chan@liferay.com>
2021-08-19 11:16:26 -0300

    LPS-130662 Ignore for now

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -67,6 +67,7 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -837,6 +838,7 @@ public class ObjectEntryLocalServiceTest {
 			ServiceContextTestUtil.getServiceContext());
 	}
 
+	@Ignore
 	@Test
 	public void testUpdateStatus() throws Exception {
 		PermissionChecker permissionChecker =
