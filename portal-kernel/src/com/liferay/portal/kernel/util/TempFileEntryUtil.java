6bbdb49b5df2e0941b06df3b02fc9633585d9dba
Cheryl Tang <cheryl.tang@liferay.com>
2021-08-19 13:51:46 -0700

    LPS-136729 Revert "LPS-119611 Default portlet repository must be created in production."
    This reverts commit 64aa6079

--- portal-kernel/src/com/liferay/portal/kernel/util/TempFileEntryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/TempFileEntryUtil.java
@@ -16,10 +16,8 @@ package com.liferay.portal.kernel.util;
 
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.util.DLAppHelperThreadLocal;
-import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Repository;
@@ -215,9 +213,7 @@ public class TempFileEntryUtil {
 
 		boolean dlAppHelperEnabled = DLAppHelperThreadLocal.isEnabled();
 
-		try (SafeCloseable safeCloseable =
-				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(0)) {
-
+		try {
 			DLAppHelperThreadLocal.setEnabled(false);
 
 			repository = RepositoryLocalServiceUtil.addRepository(
