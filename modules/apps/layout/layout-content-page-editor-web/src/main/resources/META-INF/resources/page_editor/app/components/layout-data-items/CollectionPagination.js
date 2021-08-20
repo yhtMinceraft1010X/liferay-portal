e91679e18e7d747ddd3fdd57ffbac797f4b7c821
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2021-08-20 13:41:41 +0200

    LPS-137216 Add pagination overlay when the collection is empty

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/CollectionPagination.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/CollectionPagination.js
@@ -52,9 +52,8 @@ export default function CollectionPagination({
 			className={classNames(
 				'page-editor__collection__pagination d-flex',
 				{
-					'page-editor__collection__pagination__overlay': !isActive(
-						collectionId
-					),
+					'page-editor__collection__pagination__overlay':
+						totalNumberOfItems < 1 || !isActive(collectionId),
 					'pt-3 pb-2': paginationType === 'numeric',
 					'py-3': paginationType === 'simple',
 				}
