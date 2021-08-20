eb91418019dce1d4e774a1527434712e5ff09d1f
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-08-20 12:16:41 -0300

    LPS-137617 putObjectDefinition integration test

--- modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionResourceTest.java
+++ modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionResourceTest.java
@@ -170,6 +170,13 @@ public class ObjectDefinitionResourceTest
 		return _addObjectDefinition(randomObjectDefinition());
 	}
 
+	@Override
+	protected ObjectDefinition testPutObjectDefinition_addObjectDefinition()
+		throws Exception {
+
+		return _addObjectDefinition(randomObjectDefinition());
+	}
+
 	private ObjectDefinition _addObjectDefinition(
 			ObjectDefinition randomObjectDefinition)
 		throws Exception {
