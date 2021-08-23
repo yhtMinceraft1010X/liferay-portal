f74466c62da05dc8d416665d018020faa0c9b284
Matija Petanjek <matija.petanjek@liferay.com>
2021-08-23 12:06:15 +0200

    COMMERCE-6491 headless-admin-batch-planner-impl - implement Log resource

--- modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-impl/src/main/java/com/liferay/headless/admin/batch/planner/internal/resource/v1_0/LogResourceImpl.java
+++ modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-impl/src/main/java/com/liferay/headless/admin/batch/planner/internal/resource/v1_0/LogResourceImpl.java
@@ -14,9 +14,15 @@
 
 package com.liferay.headless.admin.batch.planner.internal.resource.v1_0;
 
+import com.liferay.batch.planner.model.BatchPlannerLog;
+import com.liferay.batch.planner.service.BatchPlannerLogService;
+import com.liferay.headless.admin.batch.planner.dto.v1_0.Log;
 import com.liferay.headless.admin.batch.planner.resource.v1_0.LogResource;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +33,38 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = LogResource.class
 )
 public class LogResourceImpl extends BaseLogResourceImpl {
+
+	public Page<Log> getPlanLogsPage(Long id, Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			transform(
+				_batchPlannerLogService.getBatchPlannerLogs(
+					id, pagination.getStartPosition(),
+					pagination.getEndPosition()),
+				this::_toLog),
+			pagination, _batchPlannerLogService.getBatchPlannerLogsCount(id));
+	}
+
+	private Log _toLog(BatchPlannerLog batchPlannerLog) {
+		return new Log() {
+			{
+				dispatchTriggerExternalReferenceCode =
+					batchPlannerLog.getDispatchTriggerERC();
+				exportTaskExternalReferenceCode =
+					batchPlannerLog.getBatchEngineExportTaskERC();
+				id = batchPlannerLog.getBatchPlannerLogId();
+				importTaskExternalReferenceCode =
+					batchPlannerLog.getBatchEngineImportTaskERC();
+				planId = batchPlannerLog.getBatchPlannerPlanId();
+				size = batchPlannerLog.getSize();
+				status = batchPlannerLog.getStatus();
+				total = batchPlannerLog.getTotal();
+			}
+		};
+	}
+
+	@Reference
+	private BatchPlannerLogService _batchPlannerLogService;
+
 }
