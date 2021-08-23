a584a68ff1c96c636daafdef6c0a1d1253a97b5a
Hugo Huijser <hugo.huijser@liferay.com>
2021-08-23 09:37:47 -0600

    LPS-137720 Fix version for DummyUpgradeSteo

--- modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/upgrade/UserAssociatedDataWebUpgrade.java
+++ modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/upgrade/UserAssociatedDataWebUpgrade.java
@@ -31,7 +31,7 @@ public class UserAssociatedDataWebUpgrade implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
-		registry.register("0.0.0", "1.0.0", new DummyUpgradeStep());
+		registry.register("0.0.0", "1.1.0", new DummyUpgradeStep());
 
 		registry.register("0.0.1", "1.0.0", new ResourceActionUpgradeProcess());
 
