60d4d63262cffca7420f2e3b5185f907774276e9
Michael Bowerman <michael.bowerman@liferay.com>
2021-08-23 16:23:40 -0700

    LPS-137856 SF

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -942,8 +942,9 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 		if (!includeSystem) {
 			deleteLayouts(groupId, privateLayout, serviceContext);
 		}
-		// Layouts
 		else {
+			// Layouts
+
 			List<Layout> layouts = layoutPersistence.findByG_P_P_S(
 				groupId, privateLayout,
 				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
