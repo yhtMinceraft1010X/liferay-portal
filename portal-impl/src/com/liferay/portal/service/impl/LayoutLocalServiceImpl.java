81060b108db40b6c6fa09e872c5e630224ce7bbd
Michael Bowerman <michael.bowerman@liferay.com>
2021-08-23 16:49:19 -0700

    LPS-137856 Fix Javadoc formatting

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -895,14 +895,14 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 	 * Deletes the group's private or non-private layouts, also deleting the
 	 * layouts' child layouts, and associated resources.
 	 *
-	 * @param groupId        the primary key of the group
-	 * @param privateLayout  whether the layout is private to the group
-	 * @param includeSystem  whether or not system layouts should be included
-	 *                       with the layouts to be deleted
+	 * @param groupId the primary key of the group
+	 * @param privateLayout whether the layout is private to the group
+	 * @param includeSystem whether or not system layouts should be included
+	 *        with the layouts to be deleted
 	 * @param serviceContext the service context to be applied. The parent
-	 *                       layout set's page count will be updated by default, unless an
-	 *                       attribute named <code>updatePageCount</code> is set to
-	 *                       <code>false</code>.
+	 *        layout set's page count will be updated by default, unless an
+	 *        attribute named <code>updatePageCount</code> is set to
+	 *        <code>false</code>.
 	 * @throws PortalException if a portal exception occurred
 	 */
 	@Override
@@ -951,12 +951,12 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 	 * Deletes the group's private or non-private layouts, also deleting the
 	 * layouts' child layouts, and associated resources.
 	 *
-	 * @param groupId        the primary key of the group
-	 * @param privateLayout  whether the layout is private to the group
+	 * @param groupId the primary key of the group
+	 * @param privateLayout whether the layout is private to the group
 	 * @param serviceContext the service context to be applied. The parent
-	 *                       layout set's page count will be updated by default, unless an
-	 *                       attribute named <code>updatePageCount</code> is set to
-	 *                       <code>false</code>.
+	 *        layout set's page count will be updated by default, unless an
+	 *        attribute named <code>updatePageCount</code> is set to
+	 *        <code>false</code>.
 	 * @throws PortalException if a portal exception occurred
 	 */
 	@Override
