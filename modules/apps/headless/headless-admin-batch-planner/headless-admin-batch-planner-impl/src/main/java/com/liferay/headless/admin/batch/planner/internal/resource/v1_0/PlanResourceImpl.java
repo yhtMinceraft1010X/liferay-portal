323509bf6b456d5b47e1fb12d43fc42da4f7597d
Matija Petanjek <matija.petanjek@liferay.com>
2021-08-23 12:24:14 +0200

    COMMERCE-6491 headless-admin-batch-planner-impl - implement Plan resource

--- modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-impl/src/main/java/com/liferay/headless/admin/batch/planner/internal/resource/v1_0/PlanResourceImpl.java
+++ modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-impl/src/main/java/com/liferay/headless/admin/batch/planner/internal/resource/v1_0/PlanResourceImpl.java
@@ -14,9 +14,23 @@
 
 package com.liferay.headless.admin.batch.planner.internal.resource.v1_0;
 
+import com.liferay.batch.planner.model.BatchPlannerMapping;
+import com.liferay.batch.planner.model.BatchPlannerPlan;
+import com.liferay.batch.planner.model.BatchPlannerPolicy;
+import com.liferay.batch.planner.service.BatchPlannerMappingService;
+import com.liferay.batch.planner.service.BatchPlannerPlanService;
+import com.liferay.batch.planner.service.BatchPlannerPolicyService;
+import com.liferay.headless.admin.batch.planner.dto.v1_0.Mapping;
+import com.liferay.headless.admin.batch.planner.dto.v1_0.Plan;
+import com.liferay.headless.admin.batch.planner.dto.v1_0.Policy;
 import com.liferay.headless.admin.batch.planner.resource.v1_0.PlanResource;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +41,116 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = PlanResource.class
 )
 public class PlanResourceImpl extends BasePlanResourceImpl {
+
+	@Override
+	public void deletePlan(Long id) throws Exception {
+		_batchPlannerPlanService.deleteBatchPlannerPlan(id);
+	}
+
+	@Override
+	public Plan getPlan(Long id) throws Exception {
+		return _toPlan(_batchPlannerPlanService.getBatchPlannerPlan(id));
+	}
+
+	@Override
+	public Page<Plan> getPlansPage(Pagination pagination) throws Exception {
+		return Page.of(
+			transform(
+				_batchPlannerPlanService.getBatchPlannerPlans(
+					contextCompany.getCompanyId(),
+					pagination.getStartPosition(), pagination.getEndPosition()),
+				this::_toPlan),
+			pagination,
+			_batchPlannerPlanService.getBatchPlannerPlansCount(
+				contextCompany.getCompanyId()));
+	}
+
+	@Override
+	public Plan patchPlan(Long id, Plan plan) throws Exception {
+		BatchPlannerPlan batchPlannerPlan =
+			_batchPlannerPlanService.updateBatchPlannerPlan(id, plan.getName());
+
+		for (Mapping mapping : plan.getMappings()) {
+			_batchPlannerMappingService.updateBatchPlannerMapping(
+				mapping.getId(), mapping.getExternalFieldName(),
+				mapping.getExternalFieldType(), mapping.getScript());
+		}
+
+		for (Policy policy : plan.getPolicies()) {
+			_batchPlannerPolicyService.updateBatchPlannerPolicy(
+				id, policy.getName(), policy.getValue());
+		}
+
+		return _toPlan(batchPlannerPlan);
+	}
+
+	@Override
+	public Plan postPlan(Plan plan) throws Exception {
+		BatchPlannerPlan batchPlannerPlan =
+			_batchPlannerPlanService.addBatchPlannerPlan(
+				plan.getExport(), plan.getExternalType(), plan.getExternalURL(),
+				plan.getInternalClassName(), plan.getName());
+
+		for (Mapping mapping : plan.getMappings()) {
+			_batchPlannerMappingService.addBatchPlannerMapping(
+				batchPlannerPlan.getBatchPlannerPlanId(),
+				mapping.getExternalFieldName(), mapping.getExternalFieldType(),
+				mapping.getInternalFieldName(), mapping.getInternalFieldType(),
+				mapping.getScript());
+		}
+
+		for (Policy policy : plan.getPolicies()) {
+			_batchPlannerPolicyService.addBatchPlannerPolicy(
+				batchPlannerPlan.getBatchPlannerPlanId(), policy.getName(),
+				policy.getValue());
+		}
+
+		return _toPlan(batchPlannerPlan);
+	}
+
+	private Mapping[] _getMappings(BatchPlannerPlan batchPlannerPlan)
+		throws Exception {
+
+		List<BatchPlannerMapping> batchPlannerMappings =
+			_batchPlannerMappingService.getBatchPlannerMappings(
+				batchPlannerPlan.getBatchPlannerPlanId());
+
+		return batchPlannerMappings.toArray(new Mapping[0]);
+	}
+
+	private Policy[] _getPolicies(BatchPlannerPlan batchPlannerPlan)
+		throws Exception {
+
+		List<BatchPlannerPolicy> batchPlannerPolicies =
+			_batchPlannerPolicyService.getBatchPlannerPolicies(
+				batchPlannerPlan.getBatchPlannerPlanId());
+
+		return batchPlannerPolicies.toArray(new Policy[0]);
+	}
+
+	private Plan _toPlan(BatchPlannerPlan batchPlannerPlan) throws Exception {
+		return new Plan() {
+			{
+				active = batchPlannerPlan.isActive();
+				export = batchPlannerPlan.isExport();
+				externalType = batchPlannerPlan.getExternalType();
+				externalURL = batchPlannerPlan.getExternalURL();
+				id = batchPlannerPlan.getBatchPlannerPlanId();
+				internalClassName = batchPlannerPlan.getInternalClassName();
+				mappings = _getMappings(batchPlannerPlan);
+				name = batchPlannerPlan.getName();
+				policies = _getPolicies(batchPlannerPlan);
+			}
+		};
+	}
+
+	@Reference
+	private BatchPlannerMappingService _batchPlannerMappingService;
+
+	@Reference
+	private BatchPlannerPlanService _batchPlannerPlanService;
+
+	@Reference
+	private BatchPlannerPolicyService _batchPlannerPolicyService;
+
 }
