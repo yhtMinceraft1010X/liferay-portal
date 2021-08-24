9b0fbd0f4616570808a1052d18342d812cde9b98
Alicia Garcia <alicia.garcia@liferay.com>
2021-08-24 11:17:50 +0200

    LPS-136140 Use a unsafe supplier because now we can return an exception

--- modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/display/context/TranslateDisplayContext.java
+++ modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/display/context/TranslateDisplayContext.java
@@ -23,6 +23,7 @@ import com.liferay.info.field.type.TextInfoFieldType;
 import com.liferay.info.form.InfoForm;
 import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.petra.function.UnsafeSupplier;
 import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.bean.BeanParamUtil;
@@ -65,7 +66,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.BooleanSupplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -81,7 +81,8 @@ public class TranslateDisplayContext {
 	public TranslateDisplayContext(
 		List<String> availableSourceLanguageIds,
 		List<String> availableTargetLanguageIds,
-		BooleanSupplier booleanSupplier, String className, long classPK,
+		UnsafeSupplier<Boolean, PortalException> booleanUnsafeSupplier,
+		String className, long classPK,
 		FFLayoutExperienceSelectorConfiguration
 			ffLayoutExperienceSelectorConfiguration,
 		InfoForm infoForm, LiferayPortletRequest liferayPortletRequest,
@@ -93,7 +94,7 @@ public class TranslateDisplayContext {
 
 		_availableSourceLanguageIds = availableSourceLanguageIds;
 		_availableTargetLanguageIds = availableTargetLanguageIds;
-		_booleanSupplier = booleanSupplier;
+		_booleanUnsafeSupplier = booleanUnsafeSupplier;
 		_className = className;
 		_classPK = classPK;
 		_ffLayoutExperienceSelectorConfiguration =
@@ -438,8 +439,8 @@ public class TranslateDisplayContext {
 		return true;
 	}
 
-	public boolean isAutoTranslateEnabled() {
-		if (_booleanSupplier.getAsBoolean() && hasTranslationPermission()) {
+	public boolean isAutoTranslateEnabled() throws PortalException {
+		if (_booleanUnsafeSupplier.get() && hasTranslationPermission()) {
 			return true;
 		}
 
@@ -593,7 +594,8 @@ public class TranslateDisplayContext {
 
 	private final List<String> _availableSourceLanguageIds;
 	private final List<String> _availableTargetLanguageIds;
-	private final BooleanSupplier _booleanSupplier;
+	private final UnsafeSupplier<Boolean, PortalException>
+		_booleanUnsafeSupplier;
 	private final String _className;
 	private final long _classPK;
 	private final FFLayoutExperienceSelectorConfiguration
