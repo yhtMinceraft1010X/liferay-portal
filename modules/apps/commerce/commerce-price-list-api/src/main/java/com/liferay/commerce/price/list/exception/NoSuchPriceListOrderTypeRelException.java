a12532e3329922b5e8376d10079773d918b91e09
Riccardo Alberti <riccardo.alberti@liferay.com>
2021-08-24 15:32:32 +0200

    COMMERCE-6992 Auto SF

--- modules/apps/commerce/commerce-price-list-api/src/main/java/com/liferay/commerce/price/list/exception/NoSuchPriceListOrderTypeRelException.java
+++ modules/apps/commerce/commerce-price-list-api/src/main/java/com/liferay/commerce/price/list/exception/NoSuchPriceListOrderTypeRelException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.commerce.price.list.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -27,7 +28,9 @@ public class NoSuchPriceListOrderTypeRelException extends NoSuchModelException {
 		super(msg);
 	}
 
-	public NoSuchPriceListOrderTypeRelException(String msg, Throwable throwable) {
+	public NoSuchPriceListOrderTypeRelException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
