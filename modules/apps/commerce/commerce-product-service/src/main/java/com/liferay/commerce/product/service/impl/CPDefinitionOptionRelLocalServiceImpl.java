c9a365e5e719a3951f11328a7208ac9f37bea4b1
Alan Huang <alan.huang@liferay.com>
2021-08-24 23:05:08 +0800

    LPS-105380 SF, use ListUtil.isEmpty

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionOptionRelLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionOptionRelLocalServiceImpl.java
@@ -57,6 +57,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.spring.extender.service.ServiceReference;
@@ -898,9 +899,7 @@ public class CPDefinitionOptionRelLocalServiceImpl
 			cpDefinitionOptionValueRelLocalService.
 				getCPDefinitionOptionValueRels(cpDefinitionOptionRelId);
 
-		if ((cpDefinitionOptionValueRels == null) ||
-			cpDefinitionOptionValueRels.isEmpty()) {
-
+		if (ListUtil.isEmpty(cpDefinitionOptionValueRels)) {
 			return;
 		}
 
