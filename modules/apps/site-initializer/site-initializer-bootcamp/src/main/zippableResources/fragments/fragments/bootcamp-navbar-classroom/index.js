3c84f11f313456107c2ad16a2254149438c027cf
Brian Chan <brian.chan@liferay.com>
2021-08-24 10:13:20 -0300

    LPS-137508 SF

--- modules/apps/site-initializer/site-initializer-bootcamp/src/main/zippableResources/fragments/fragments/bootcamp-navbar-classroom/index.js
+++ modules/apps/site-initializer/site-initializer-bootcamp/src/main/zippableResources/fragments/fragments/bootcamp-navbar-classroom/index.js
@@ -5,9 +5,9 @@ const navbarCollapse = fragmentElement.querySelector('.navbar-collapse');
 const siteNavbar = fragmentElement.querySelector('.bootcamp-navbar');
 
 navbarButton.addEventListener('click', function () {
+	body.classList.toggle('overflow-hidden');
 	navbarCollapse.classList.toggle('show');
 	siteNavbar.classList.toggle('open');
-	body.classList.toggle('overflow-hidden');
 
 	navbarButton.setAttribute(
 		'aria-expanded',
