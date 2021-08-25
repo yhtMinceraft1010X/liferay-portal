2604baa40ab8c9ba11f879193e755984c3f8fa7b
Alberto Chaparro <alberto.chaparro@liferay.com>
2021-08-25 11:41:36 +0200

    LPS-134196 Now it is an integration test

---
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/UpgradeProcessTest.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.sql.Connection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ * @author Alberto Chaparro
+ */
+@RunWith(Arquillian.class)
+public class UpgradeProcessTest {
+
+	public static final String TABLE_NAME = "UpgradeProcessTest";
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_connection = DataAccess.getConnection();
+
+		_dbInspector = new DBInspector(_connection);
+
+		_db = DBManagerUtil.getDB();
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		DataAccess.cleanUp(_connection);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		_db.runSQL(
+			StringBundler.concat(
+				"create table ", TABLE_NAME, " (id LONG not null primary key, ",
+				"typeVarchar VARCHAR(75) not null);"));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_db.runSQL("drop table " + TABLE_NAME);
+	}
+
+	@Test
+	public void testDeprecatedAlterColumnName() throws Exception {
+		UpgradeProcess upgradeProcess = new UpgradeProcess() {
+
+			@Override
+			protected void doUpgrade() throws Exception {
+				alter(
+					UpgradeProcessTest.class,
+					new UpgradeProcess.AlterColumnName(
+						"typeVarchar", "typeVarcharTest VARCHAR(75) not null"));
+			}
+
+		};
+
+		upgradeProcess.upgrade();
+
+		Assert.assertTrue(
+			_dbInspector.hasColumn(TABLE_NAME, "typeVarcharTest"));
+	}
+
+	@Test
+	public void testAlterColumnType() throws Exception {
+		UpgradeProcess upgradeProcess = new UpgradeProcess() {
+
+			@Override
+			protected void doUpgrade() throws Exception {
+				alter(
+					UpgradeProcessTest.class,
+					new AlterColumnType("typeVarchar", "LONG null"));
+			}
+
+		};
+
+		upgradeProcess.upgrade();
+
+		Assert.assertTrue(
+			_dbInspector.hasColumnType(TABLE_NAME, "typeVarchar", "LONG null"));
+	}
+
+	@Test
+	public void testAlterTableAddColumn() throws Exception {
+		UpgradeProcess upgradeProcess = new UpgradeProcess() {
+
+			@Override
+			protected void doUpgrade() throws Exception {
+				alter(
+					UpgradeProcessTest.class,
+					new AlterTableAddColumn("testColumn", "STRING null"));
+			}
+
+		};
+
+		upgradeProcess.upgrade();
+
+		Assert.assertTrue(_dbInspector.hasColumn(TABLE_NAME, "testColumn"));
+	}
+
+	@Test
+	public void testAlterTableDropColumn() throws Exception {
+		UpgradeProcess upgradeProcess = new UpgradeProcess() {
+
+			@Override
+			protected void doUpgrade() throws Exception {
+				alter(
+					UpgradeProcessTest.class,
+					new AlterTableDropColumn("typeVarchar"));
+			}
+
+		};
+
+		upgradeProcess.upgrade();
+
+		Assert.assertFalse(_dbInspector.hasColumn(TABLE_NAME, "typeVarchar"));
+	}
+
+	private static Connection _connection;
+	private static DB _db;
+	private static DBInspector _dbInspector;
+
+}
