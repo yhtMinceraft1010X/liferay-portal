ded2a6b81c9801cd6de714097e09c0312ec46fc3
Brian Chan <brian.chan@liferay.com>
2021-08-26 16:04:24 -0300

    Regen

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-api/src/main/java/com/liferay/headless/commerce/admin/pricing/resource/v2_0/OrderTypeResource.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-api/src/main/java/com/liferay/headless/commerce/admin/pricing/resource/v2_0/OrderTypeResource.java
@@ -16,6 +16,8 @@ package com.liferay.headless.commerce.admin.pricing.resource.v2_0;
 
 import com.liferay.headless.commerce.admin.pricing.dto.v2_0.OrderType;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
 
@@ -72,6 +74,12 @@ public interface OrderTypeResource {
 
 	public void setGroupLocalService(GroupLocalService groupLocalService);
 
+	public void setResourceActionLocalService(
+		ResourceActionLocalService resourceActionLocalService);
+
+	public void setResourcePermissionLocalService(
+		ResourcePermissionLocalService resourcePermissionLocalService);
+
 	public void setRoleLocalService(RoleLocalService roleLocalService);
 
 	public static class FactoryHolder {
@@ -89,6 +97,9 @@ public interface OrderTypeResource {
 
 		public Builder httpServletRequest(
 			HttpServletRequest httpServletRequest);
+
+		public Builder httpServletResponse(
+			HttpServletResponse httpServletResponse);
 
 		public Builder preferredLocale(Locale preferredLocale);
 
--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/factory/PriceListOrderTypeResourceFactoryImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/factory/PriceListOrderTypeResourceFactoryImpl.java
@@ -22,6 +22,10 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ProxyUtil;
@@ -38,6 +42,7 @@ import java.util.Locale;
 import javax.annotation.Generated;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.ComponentServiceObjects;
 import org.osgi.service.component.annotations.Activate;
@@ -73,7 +78,8 @@ public class PriceListOrderTypeResourceFactoryImpl
 					new Class<?>[] {PriceListOrderTypeResource.class},
 					(proxy, method, arguments) -> _invoke(
 						method, arguments, _checkPermissions,
-						_httpServletRequest, _preferredLocale, _user));
+						_httpServletRequest, _httpServletResponse,
+						_preferredLocale, _user));
 			}
 
 			@Override
@@ -90,6 +96,15 @@ public class PriceListOrderTypeResourceFactoryImpl
 				HttpServletRequest httpServletRequest) {
 
 				_httpServletRequest = httpServletRequest;
+
+				return this;
+			}
+
+			@Override
+			public PriceListOrderTypeResource.Builder httpServletResponse(
+				HttpServletResponse httpServletResponse) {
+
+				_httpServletResponse = httpServletResponse;
 
 				return this;
 			}
@@ -112,6 +127,7 @@ public class PriceListOrderTypeResourceFactoryImpl
 
 			private boolean _checkPermissions = true;
 			private HttpServletRequest _httpServletRequest;
+			private HttpServletResponse _httpServletResponse;
 			private Locale _preferredLocale;
 			private User _user;
 
@@ -130,7 +146,8 @@ public class PriceListOrderTypeResourceFactoryImpl
 
 	private Object _invoke(
 			Method method, Object[] arguments, boolean checkPermissions,
-			HttpServletRequest httpServletRequest, Locale preferredLocale,
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, Locale preferredLocale,
 			User user)
 		throws Throwable {
 
@@ -162,7 +179,15 @@ public class PriceListOrderTypeResourceFactoryImpl
 
 		priceListOrderTypeResource.setContextHttpServletRequest(
 			httpServletRequest);
+		priceListOrderTypeResource.setContextHttpServletResponse(
+			httpServletResponse);
 		priceListOrderTypeResource.setContextUser(user);
+		priceListOrderTypeResource.setGroupLocalService(_groupLocalService);
+		priceListOrderTypeResource.setResourceActionLocalService(
+			_resourceActionLocalService);
+		priceListOrderTypeResource.setResourcePermissionLocalService(
+			_resourcePermissionLocalService);
+		priceListOrderTypeResource.setRoleLocalService(_roleLocalService);
 
 		try {
 			return method.invoke(priceListOrderTypeResource, arguments);
@@ -189,8 +214,20 @@ public class PriceListOrderTypeResourceFactoryImpl
 	@Reference
 	private PermissionCheckerFactory _defaultPermissionCheckerFactory;
 
+	@Reference
+	private GroupLocalService _groupLocalService;
+
 	@Reference(target = "(permission.checker.type=liberal)")
 	private PermissionCheckerFactory _liberalPermissionCheckerFactory;
+
+	@Reference
+	private ResourceActionLocalService _resourceActionLocalService;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
