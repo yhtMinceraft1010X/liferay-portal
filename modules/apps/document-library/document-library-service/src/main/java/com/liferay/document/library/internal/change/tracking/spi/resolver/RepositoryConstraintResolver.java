fc7c78dc8a3a6be37578d3e707d46e569f09b281
Cheryl Tang <cheryl.tang@liferay.com>
2021-08-26 17:53:38 -0700

    LPS-136729 Create RepositoryConstraintResolver

---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/change/tracking/spi/resolver/RepositoryConstraintResolver.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.change.tracking.spi.resolver;
+
+import com.liferay.change.tracking.spi.resolver.ConstraintResolver;
+import com.liferay.change.tracking.spi.resolver.context.ConstraintResolverContext;
+import com.liferay.portal.kernel.model.Repository;
+import com.liferay.portal.kernel.service.RepositoryLocalService;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.TempFileEntryUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cheryl Tang
+ */
+@Component(service = ConstraintResolver.class)
+public class RepositoryConstraintResolver
+	implements ConstraintResolver<Repository> {
+
+	@Override
+	public String getConflictDescriptionKey() {
+		return "duplicate-repository";
+	}
+
+	@Override
+	public Class<Repository> getModelClass() {
+		return Repository.class;
+	}
+
+	@Override
+	public String getResolutionDescriptionKey() {
+		if (_resolved) {
+			return "duplicate-repository-was-removed";
+		}
+
+		return "duplicate-repository-conflict-was-not-automatically-resolved";
+	}
+
+	@Override
+	public ResourceBundle getResourceBundle(Locale locale) {
+		return ResourceBundleUtil.getBundle(
+			locale, RepositoryConstraintResolver.class);
+	}
+
+	@Override
+	public String[] getUniqueIndexColumnNames() {
+		return new String[] {"groupId", "name", "portletId"};
+	}
+
+	@Override
+	public void resolveConflict(
+		ConstraintResolverContext<Repository> constraintResolverContext) {
+
+		Repository sourceCTModel = constraintResolverContext.getSourceCTModel();
+
+		Repository targetCTModel = constraintResolverContext.getTargetCTModel();
+
+		if (StringUtil.equals(
+				sourceCTModel.getName(), TempFileEntryUtil.class.getName()) &&
+			StringUtil.equals(
+				targetCTModel.getName(), TempFileEntryUtil.class.getName())) {
+
+			_repositoryLocalService.deleteRepository(targetCTModel);
+
+			_resolved = true;
+		}
+		else {
+			_resolved = false;
+		}
+	}
+
+	@Reference
+	private RepositoryLocalService _repositoryLocalService;
+
+	private boolean _resolved;
+
+}
