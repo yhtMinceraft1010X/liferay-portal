f268c58826514dede344be30cb68a5eda79940b2
arthurchan35 <arthurchan35@gmail.com>
2021-08-30 14:36:58 +0800

    LPS-138196 Do not clean up before all nodes are done

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/session/manager/OfflineOpenIdConnectSessionManager.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/session/manager/OfflineOpenIdConnectSessionManager.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.security.sso.openid.connect.internal.session.manager;
 
 import com.liferay.counter.kernel.service.CounterLocalService;
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
@@ -48,6 +49,7 @@ import com.nimbusds.openid.connect.sdk.token.OIDCTokens;
 
 import java.util.Date;
 import java.util.Dictionary;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -204,6 +206,12 @@ public class OfflineOpenIdConnectSessionManager {
 		}
 
 		_bundleContext = null;
+
+		List<?> clusterNodes = ClusterExecutorUtil.getClusterNodes();
+
+		if ((clusterNodes != null) && (clusterNodes.size() > 1)) {
+			return;
+		}
 
 		for (OpenIdConnectSession openIdConnectSession :
 				_openIdConnectSessionLocalService.getOpenIdConnectSessions(
