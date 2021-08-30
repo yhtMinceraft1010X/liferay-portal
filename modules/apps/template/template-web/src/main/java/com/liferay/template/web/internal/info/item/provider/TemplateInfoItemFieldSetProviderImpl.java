5e6d8044e5bf5111d4bdb339988c6a5b5cbb438a
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-08-30 09:22:56 +0200

    LPS-137569 Create a provider for information templates InfoFieldSet

---
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/info/item/provider/TemplateInfoItemFieldSetProviderImpl.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.web.internal.info.item.provider;
+
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.InfoFieldSet;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.template.info.item.provider.TemplateInfoItemFieldSetProvider;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(immediate = true, service = TemplateInfoItemFieldSetProvider.class)
+public class TemplateInfoItemFieldSetProviderImpl
+	implements TemplateInfoItemFieldSetProvider {
+
+	@Override
+	public InfoFieldSet getInfoFieldSet(
+		String itemClassName, String itemVariationKey) {
+
+		return InfoFieldSet.builder(
+		).infoFieldSetEntry(
+			consumer -> {
+				for (DDMTemplate ddmTemplate :
+						_getDDMTemplates(itemClassName, itemVariationKey)) {
+
+					InfoLocalizedValue<String> labelInfoLocalizedValue =
+						InfoLocalizedValue.<String>builder(
+						).value(
+							LocaleUtil.getDefault(),
+							ddmTemplate.getName(LocaleUtil.getDefault())
+						).defaultLocale(
+							LocaleUtil.getDefault()
+						).build();
+
+					consumer.accept(
+						InfoField.builder(
+						).infoFieldType(
+							TextInfoFieldType.INSTANCE
+						).name(
+							"informationTemplate_" + ddmTemplate.getTemplateId()
+						).labelInfoLocalizedValue(
+							labelInfoLocalizedValue
+						).build());
+				}
+			}
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(getClass(), "information-templates")
+		).name(
+			"information-templates"
+		).build();
+	}
+
+	private List<DDMTemplate> _getDDMTemplates(
+		String itemClassName, String itemVariationKey) {
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext == null) {
+			return Collections.emptyList();
+		}
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		try {
+			return _ddmTemplateLocalService.getTemplates(
+					themeDisplay.getScopeGroupId(),
+					_portal.getClassNameId(itemClassName),
+					GetterUtil.getLong(itemVariationKey), true);
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(
+				"Caught unexpected exception", portalException);
+		}
+	}
+
+	@Reference
+	private DDMTemplateLocalService _ddmTemplateLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
