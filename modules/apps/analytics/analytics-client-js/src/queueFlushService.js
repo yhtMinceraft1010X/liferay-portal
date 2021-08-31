b2677c907881a2c3f9b2901e7e36039c4c369d28
Francisco Dias <francisco.dias@liferay.com>
2021-08-31 19:23:25 -0300

    LRAC-7748 Create flushService

---
+++ modules/apps/analytics/analytics-client-js/src/queueFlushService.js
@@ -0,0 +1,195 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {
+	FLUSH_INTERVAL,
+	LIMIT_FAILED_ATTEMPTS,
+	QUEUE_PRIORITY_DEFAULT,
+} from './utils/constants';
+import {getRetryDelay} from './utils/delay';
+
+/**
+ * A Queue Config.
+ *
+ * @typedef {Object} QueueConfig
+ * @property {string} endpointUrl - Url to send the items
+ * @property {string} name - The name to idetify the queue, it must be unique.
+ * @property {number} [priority=1] - Priority of this queue.
+ */
+
+/**
+ * QueueFlushService is used to organize the queues and set the properly time a queue should send their events.
+ * It exposes methods to add and remove queues.
+ * Queues are ordered by priority and flushed synchronously. What means if one queue fail the other will not
+ * be proccessed and a delay will be added to loop.
+ */
+class QueueFlushService {
+	constructor(config = {}) {
+		this.attemptNumber = 1;
+		this.initialFlushInterval = config.flushInterval || FLUSH_INTERVAL;
+		this.processing = false;
+		this.queues = [];
+
+		this.flushInterval = this.initialFlushInterval;
+
+		this._startsFlushLoop();
+	}
+
+	/**
+	 * Add a queue to be processed by the client
+	 * @param {MessageQueue} queueInstance
+	 * @param {QueueConfig} config
+	 */
+	addQueue(queueInstance, config = {}) {
+		this.queues.push(
+			Object.assign(config, {
+				instance: queueInstance,
+				name: queueInstance.name,
+			})
+		);
+		this.queues.sort(this._prioritize);
+	}
+
+	/**
+	 * Remove a queue
+	 * @param {string} queueName
+	 */
+	removeQueue(queueName) {
+		this.queues = this.queues.filter(({name}) => queueName !== name);
+	}
+
+	/**
+	 * Function to order queues by priority
+	 * @param {Object} queueA
+	 * @param {Object} queueB
+	 */
+	_prioritize(
+		{priority: pA = QUEUE_PRIORITY_DEFAULT},
+		{priority: pB = QUEUE_PRIORITY_DEFAULT}
+	) {
+		return pB - pA;
+	}
+
+	/**
+	 * Increase the interval time and restart processing loop
+	 */
+	_onFlushFail() {
+		this.flushInterval = getRetryDelay(
+			++this.attemptNumber,
+			LIMIT_FAILED_ATTEMPTS
+		);
+		this._startsFlushLoop();
+	}
+
+	/**
+	 * Reset interval time and restart processing loop
+	 */
+	_onFlushSuccess() {
+		this.attemptNumber = 1;
+
+		if (this.flushInterval !== this.initialFlushInterval) {
+			this.flushInterval = this.initialFlushInterval;
+			this._startsFlushLoop();
+		}
+	}
+
+	/**
+	 * Go through queues and execute their callbacks.
+	 * If a queue fail, the processing loop is delayed
+	 * and the next queues are not processed.
+	 *
+	 * Note: Because we are using a ProcessLock, no other process should
+	 * be able to acquire a lock for a particular key to run its callback
+	 * until the process with the active lock releases it.
+	 *
+	 */
+	flush() {
+		if (this.processing) {
+			return;
+		}
+
+		this.queues
+			.reduce((previousPromise, {instance: queue}) => {
+				return previousPromise
+					.then(() => {
+						if (!queue.hasItems() || !queue.shouldFlush()) {
+							return Promise.resolve();
+						}
+
+						return queue.acquireLock().then((success) => {
+							if (!success) {
+								return Promise.resolve();
+							}
+
+							this.processing = true;
+
+							const releaseLock = () =>
+								queue.releaseLock().then(() => {
+									this.processing = false;
+								});
+
+							return Promise.all(queue.onFlush())
+								.then(() => {
+									this._onFlushSuccess();
+									queue.onFlushSuccess();
+									releaseLock();
+
+									return Promise.resolve();
+								})
+								.catch(() => {
+									releaseLock();
+
+									return Promise.reject();
+								});
+						});
+					})
+					.catch(() => {
+						this._onFlushFail();
+						queue.onFLushFail();
+
+						return Promise.reject();
+					});
+			}, Promise.resolve())
+			.catch(() => {});
+	}
+
+	/**
+	 * Start a timer to process messages at a specified interval.
+	 */
+	_startsFlushLoop() {
+		if (this.processInterval) {
+			clearInterval(this.processInterval);
+		}
+
+		this.processInterval = setInterval(
+			() => this.flush(),
+			this.flushInterval
+		);
+	}
+
+	/**
+	 * Method for clearing queues and any scheduled processing.
+	 */
+	dispose() {
+		if (this.processInterval) {
+			clearInterval(this.processInterval);
+		}
+
+		this.flushInterval = this.initialFlushInterval;
+		this.queues = [];
+	}
+}
+
+export {QueueFlushService};
+export default QueueFlushService;
---
+++ modules/apps/analytics/analytics-client-js/test/queueFlushService.js
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import fetchMock from 'fetch-mock';
+
+import QueueFlushService from '../src/QueueFlushService';
+import Analytics from '../src/analytics';
+import EventMessageQueue from '../src/queues/EventMessageQueue';
+import IdentityMessageQueue from '../src/queues/IdentityMessageQueue';
+import {INITIAL_ANALYTICS_CONFIG, wait} from './helpers';
+
+// We don't need any of the timing callbacks to run during these tests.
+
+jest.mock('../src/plugins/timing');
+
+const analyticsInstance = Analytics.create(INITIAL_ANALYTICS_CONFIG);
+
+const FLUSH_INTERVAL = 100;
+
+const INITIAL_CONFIG = {
+	flushInterval: FLUSH_INTERVAL,
+};
+
+const getMockMessageItem = (id = 0, data = {}) => {
+	return {
+		foo: 'bar',
+		id: `${id}`,
+		...data,
+	};
+};
+
+describe('QueueFlushService', () => {
+	let queueFlushService;
+	let identityQueue;
+
+	afterEach(() => {
+		fetchMock.restore();
+
+		queueFlushService.dispose();
+		identityQueue.reset();
+	});
+
+	beforeEach(() => {
+		queueFlushService = new QueueFlushService(INITIAL_CONFIG);
+		identityQueue = new IdentityMessageQueue({
+			analyticsInstance,
+			name: 'IdentityQueueTest',
+		});
+	});
+
+	it('flush queue items', async () => {
+		let fetchCalled = 0;
+
+		fetchMock.mock(/ac-server/i, () => {
+			fetchCalled += 1;
+
+			return Promise.resolve(200);
+		});
+
+		queueFlushService.addQueue(identityQueue);
+
+		const item = getMockMessageItem('test-1');
+
+		identityQueue.addItem(item);
+
+		await wait(FLUSH_INTERVAL);
+
+		expect(fetchCalled).toBe(1);
+	});
+
+	it('flush queue items ordered by queue priority', async () => {
+		const sentEvents = [];
+		const eventQueue = new EventMessageQueue({
+			analyticsInstance,
+			name: 'EventQueueTest',
+		});
+		const priorityItem = getMockMessageItem('priority');
+
+		fetchMock.mock(/ac-server/i, (url, {body}) => {
+			sentEvents.push(JSON.parse(body));
+
+			return Promise.resolve(200);
+		});
+
+		queueFlushService.addQueue(eventQueue);
+		queueFlushService.addQueue(identityQueue, {
+			priority: 10,
+		});
+
+		eventQueue.addItem(getMockMessageItem('test-1'));
+		identityQueue.addItem(priorityItem);
+
+		await wait(FLUSH_INTERVAL);
+
+		expect(sentEvents[0]).toEqual(priorityItem);
+	});
+});
