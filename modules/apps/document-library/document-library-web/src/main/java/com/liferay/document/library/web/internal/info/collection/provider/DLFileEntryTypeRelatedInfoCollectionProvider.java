3571e79fdeed1823158d4f07eedfa94c7511530f
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-08-31 14:10:43 +0200

    LPS-138283 RelatedInfoCollectionProvider based on JournalArticleDDMStructureRelatedInfoCollectionProvider (just changing model, services and indexer)

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/info/collection/provider/DLFileEntryTypeRelatedInfoCollectionProvider.java
@@ -0,0 +1,246 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.info.collection.provider;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.depot.util.SiteConnectedGroupGroupProviderUtil;
+import com.liferay.document.library.kernel.model.DLFileEntryConstants;
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.info.collection.provider.CollectionQuery;
+import com.liferay.info.collection.provider.RelatedInfoItemCollectionProvider;
+import com.liferay.info.collection.provider.SingleFormVariationInfoCollectionProvider;
+import com.liferay.info.pagination.InfoPage;
+import com.liferay.info.pagination.Pagination;
+import com.liferay.info.sort.Sort;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.QueryConfig;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class DLFileEntryTypeRelatedInfoCollectionProvider
+	implements RelatedInfoItemCollectionProvider<AssetCategory, FileEntry>,
+			   SingleFormVariationInfoCollectionProvider<FileEntry> {
+
+	public DLFileEntryTypeRelatedInfoCollectionProvider(
+		DLAppLocalService dlAppLocalService, DLFileEntryType dlFileEntryType) {
+
+		_dlAppLocalService = dlAppLocalService;
+		_dlFileEntryType = dlFileEntryType;
+	}
+
+	@Override
+	public InfoPage<FileEntry> getCollectionInfoPage(
+		CollectionQuery collectionQuery) {
+
+		return getFileEntryInfoPage(collectionQuery);
+	}
+
+	@Override
+	public String getCollectionItemClassName() {
+		return FileEntry.class.getName();
+	}
+
+	public InfoPage<FileEntry> getFileEntryInfoPage(
+		CollectionQuery collectionQuery) {
+
+		Optional<Object> relatedItemOptional =
+			collectionQuery.getRelatedItemObjectOptional();
+
+		Object relatedItem = relatedItemOptional.orElse(null);
+
+		if (!(relatedItem instanceof AssetCategory)) {
+			return InfoPage.of(
+				Collections.emptyList(), collectionQuery.getPagination(), 0);
+		}
+
+		Indexer<?> indexer = IndexerRegistryUtil.getIndexer(
+			DLFileEntryConstants.getClassName());
+
+		SearchContext searchContext = _buildSearchContext(
+			(AssetCategory)relatedItem, collectionQuery);
+
+		try {
+			Hits hits = indexer.search(searchContext);
+
+			List<FileEntry> fileEntries = new ArrayList<>();
+
+			for (Document document : hits.getDocs()) {
+				long classPK = GetterUtil.getLong(
+					document.get(Field.ENTRY_CLASS_PK));
+
+				fileEntries.add(_dlAppLocalService.getFileEntry(classPK));
+			}
+
+			return InfoPage.of(
+				fileEntries, collectionQuery.getPagination(), hits.getLength());
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(portalException, portalException);
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	public String getFormVariationKey() {
+		return String.valueOf(_dlFileEntryType.getFileEntryTypeId());
+	}
+
+	@Override
+	public String getKey() {
+		return RelatedInfoItemCollectionProvider.class.getName() +
+			StringPool.DASH + _dlFileEntryType.getFileEntryTypeId();
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, getClass());
+
+		return LanguageUtil.get(
+			resourceBundle, _dlFileEntryType.getName(locale));
+	}
+
+	@Override
+	public String getSourceItemClassName() {
+		return AssetCategory.class.getName();
+	}
+
+	@Override
+	public boolean isAvailable() {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		try {
+			long[] currentAndAncestorSiteAndDepotGroupIds =
+				SiteConnectedGroupGroupProviderUtil.
+					getCurrentAndAncestorSiteAndDepotGroupIds(
+						serviceContext.getScopeGroupId(), true);
+
+			if ((_dlFileEntryType.getGroupId() == 0) ||
+				ArrayUtil.contains(
+					currentAndAncestorSiteAndDepotGroupIds,
+					_dlFileEntryType.getGroupId())) {
+
+				return true;
+			}
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException, portalException);
+			}
+		}
+
+		return false;
+	}
+
+	private SearchContext _buildSearchContext(
+		AssetCategory assetCategory, CollectionQuery collectionQuery) {
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setAndSearch(true);
+		searchContext.setAssetCategoryIds(
+			new long[] {assetCategory.getCategoryId()});
+		searchContext.setAttributes(
+			HashMapBuilder.<String, Serializable>put(
+				Field.STATUS, WorkflowConstants.STATUS_APPROVED
+			).put(
+				"fileEntryTypeId",
+				String.valueOf(_dlFileEntryType.getFileEntryTypeId())
+			).put(
+				"head", true
+			).put(
+				"latest", true
+			).build());
+
+		searchContext.setClassTypeIds(
+			new long[] {_dlFileEntryType.getFileEntryTypeId()});
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		searchContext.setCompanyId(serviceContext.getCompanyId());
+
+		Pagination pagination = collectionQuery.getPagination();
+
+		searchContext.setEnd(pagination.getEnd());
+
+		searchContext.setEntryClassNames(
+			new String[] {DLFileEntryConstants.getClassName()});
+
+		searchContext.setGroupIds(
+			new long[] {serviceContext.getScopeGroupId()});
+
+		Optional<Sort> sortOptional = collectionQuery.getSortOptional();
+
+		if (sortOptional.isPresent()) {
+			Sort sort = sortOptional.get();
+
+			searchContext.setSorts(
+				new com.liferay.portal.kernel.search.Sort(
+					sort.getFieldName(),
+					com.liferay.portal.kernel.search.Sort.LONG_TYPE,
+					sort.isReverse()));
+		}
+
+		searchContext.setStart(pagination.getStart());
+
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		queryConfig.setHighlightEnabled(false);
+		queryConfig.setScoreEnabled(false);
+
+		return searchContext;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DLFileEntryTypeRelatedInfoCollectionProvider.class);
+
+	private final DLAppLocalService _dlAppLocalService;
+	private final DLFileEntryType _dlFileEntryType;
+
+}
