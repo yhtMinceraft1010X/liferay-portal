04aa84550606706b4cd68d71f89c85bdc30e983a
Luca Pellizzon <luca.pellizzon@liferay.com>
2021-09-01 17:30:27 +0200

    LPS-135027 remove unnecessary tables and indexes

--- modules/apps/commerce/commerce-account-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,3 +0,0 @@
-create unique index IX_1F92C081 on CAccountGroupCAccountRel (commerceAccountGroupId, commerceAccountId);
-create index IX_9808F83D on CAccountGroupCAccountRel (commerceAccountId);
-create index IX_41A0DD5 on CAccountGroupCAccountRel (companyId, externalReferenceCode[$COLUMN_LENGTH:75$]);
