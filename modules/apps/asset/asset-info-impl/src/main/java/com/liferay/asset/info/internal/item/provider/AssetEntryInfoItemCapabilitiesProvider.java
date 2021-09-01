3aefb365ebb6701a4da361b09c622525b6ccedba
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-09-01 13:26:20 +0200

    LPS-138156 Add AssetEntry support for template capability

---
+++ modules/apps/asset/asset-info-impl/src/main/java/com/liferay/asset/info/internal/item/provider/AssetEntryInfoItemCapabilitiesProvider.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.info.internal.item.provider;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.info.item.capability.InfoItemCapability;
+import com.liferay.info.item.provider.InfoItemCapabilitiesProvider;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.template.info.item.capability.TemplateInfoItemCapability;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = InfoItemCapabilitiesProvider.class)
+public class AssetEntryInfoItemCapabilitiesProvider
+	implements InfoItemCapabilitiesProvider<AssetEntry> {
+
+	@Override
+	public List<InfoItemCapability> getInfoItemCapabilities() {
+		return ListUtil.fromArray(_templateInfoItemCapability);
+	}
+
+	@Reference
+	private TemplateInfoItemCapability _templateInfoItemCapability;
+
+}
