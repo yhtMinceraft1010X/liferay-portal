0c90e8c81ef507fa3eda2bceaea4b161e0cca735
Michael Bowerman <michael.bowerman@liferay.com>
2021-09-01 10:38:05 -0700

    LPS-137424 Inline

--- portal-impl/src/com/liferay/portal/service/persistence/impl/GroupFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/GroupFinderImpl.java
@@ -1201,14 +1201,13 @@ public class GroupFinderImpl
 
 				Long userId = (Long)params.get("userId");
 
-				boolean isAdmin = RoleLocalServiceUtil.hasUserRole(
-					userId, admin.getRoleId());
-
 				ResourceAction resourceAction =
 					ResourceActionLocalServiceUtil.getResourceAction(
 						Group.class.getName(), (String)entry.getValue());
 
-				queryPos.add(isAdmin);
+				queryPos.add(
+					RoleLocalServiceUtil.hasUserRole(
+						userId, admin.getRoleId()));
 				queryPos.add(userId);
 
 				queryPos.add(siteAdmin.getRoleId());
