e19daa05d317ff56a17b673898066dd4807ef686
Michael Bowerman <michael.bowerman@liferay.com>
2021-09-01 10:11:57 -0700

    LPS-137424 Sort

--- portal-impl/src/com/liferay/portal/service/persistence/impl/GroupFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/GroupFinderImpl.java
@@ -1189,14 +1189,7 @@ public class GroupFinderImpl
 		for (Map.Entry<String, Object> entry : params.entrySet()) {
 			String key = entry.getKey();
 
-			if (key.equals("active") || key.equals("layout") ||
-				key.equals("manualMembership") || key.equals("site")) {
-
-				Boolean value = (Boolean)entry.getValue();
-
-				queryPos.add(value);
-			}
-			else if (key.equals("actionId")) {
+			if (key.equals("actionId")) {
 				Long companyId = CompanyThreadLocal.getCompanyId();
 
 				Role siteAdmin = RoleLocalServiceUtil.fetchRole(
@@ -1211,6 +1204,13 @@ public class GroupFinderImpl
 				queryPos.add(siteAdmin.getRoleId());
 				queryPos.add(siteOwner.getRoleId());
 				queryPos.add(resourceAction.getBitwiseValue());
+			}
+			else if (key.equals("active") || key.equals("layout") ||
+					 key.equals("manualMembership") || key.equals("site")) {
+
+				Boolean value = (Boolean)entry.getValue();
+
+				queryPos.add(value);
 			}
 			else if (key.equals("classNameIds")) {
 				if (entry.getValue() instanceof Long) {
