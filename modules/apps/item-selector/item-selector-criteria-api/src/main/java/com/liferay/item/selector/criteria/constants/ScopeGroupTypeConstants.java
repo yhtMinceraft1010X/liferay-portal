e4d98269a7da1332ffb37f219120817d7ec484ec
jesseyeh-liferay <jesse.yeh@liferay.com>
2021-09-01 11:51:44 -0700

    LPS-137143 Add ScopeGroupTypeConstants

---
+++ modules/apps/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/constants/ScopeGroupTypeConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.item.selector.criteria.constants;
+
+/**
+ * @author Jesse Yeh
+ */
+public class ScopeGroupTypeConstants {
+
+	public static final String ASSET_LIBRARY = "asset-library";
+
+	public static final String GLOBAL = "global";
+
+	public static final String PAGE = "page";
+
+	public static final String SITE = "site";
+
+}
