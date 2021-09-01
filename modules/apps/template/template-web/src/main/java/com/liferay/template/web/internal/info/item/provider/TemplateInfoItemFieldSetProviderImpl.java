5e7c796c034e17a58a88ea51467c7c8297f31b5f
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-09-01 12:52:58 +0200

    LPS-138258 Use InfoItemFieldValuesProvider method to get field values.

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/info/item/provider/TemplateInfoItemFieldSetProviderImpl.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/info/item/provider/TemplateInfoItemFieldSetProviderImpl.java
@@ -19,6 +19,8 @@ import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
 import com.liferay.info.field.InfoFieldSet;
 import com.liferay.info.field.InfoFieldValue;
 import com.liferay.info.item.InfoItemFieldValues;
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
 import com.liferay.info.item.provider.InfoItemFormProvider;
 import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
@@ -81,11 +83,21 @@ public class TemplateInfoItemFieldSetProviderImpl
 			return null;
 		}
 
+		InfoItemFieldValues infoItemFieldValues = InfoItemFieldValues.builder(
+		).build();
+
+		InfoItemFieldValuesProvider<Object> infoItemFieldValuesProvider =
+			_infoItemServiceTracker.getFirstInfoItemService(
+				InfoItemFieldValuesProvider.class,
+				_portal.getClassName(ddmTemplate.getClassNameId()));
+
+		if (infoItemFieldValuesProvider != null) {
+			infoItemFieldValues =
+				infoItemFieldValuesProvider.getInfoItemFieldValues(itemObject);
+		}
+
 		TemplateInfoItemFieldReader templateInfoItemFieldReader =
-			new TemplateInfoItemFieldReader(
-				ddmTemplate,
-				InfoItemFieldValues.builder(
-				).build());
+			new TemplateInfoItemFieldReader(ddmTemplate, infoItemFieldValues);
 
 		return new InfoFieldValue<>(
 			templateInfoItemFieldReader.getInfoField(),
@@ -137,6 +149,9 @@ public class TemplateInfoItemFieldSetProviderImpl
 
 	@Reference
 	private DDMTemplateLocalService _ddmTemplateLocalService;
+
+	@Reference
+	private InfoItemServiceTracker _infoItemServiceTracker;
 
 	@Reference
 	private Portal _portal;
