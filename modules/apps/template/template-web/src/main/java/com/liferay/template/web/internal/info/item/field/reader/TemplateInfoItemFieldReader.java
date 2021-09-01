6920083e76681b0b3ded971ffa77022802a4071d
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-09-01 10:29:59 +0200

    LPS-138258 Add a LocalizedInfoItemFieldReader implementation to handle InfoField logic

---
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/info/item/field/reader/TemplateInfoItemFieldReader.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.web.internal.info.item.field.reader;
+
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.item.field.reader.LocalizedInfoItemFieldReader;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.util.Locale;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class TemplateInfoItemFieldReader
+	implements LocalizedInfoItemFieldReader {
+
+	public TemplateInfoItemFieldReader(DDMTemplate ddmTemplate) {
+		_ddmTemplate = ddmTemplate;
+	}
+
+	/**
+	 *   @deprecated As of Cavanaugh (7.4.x), replaced by {@link
+	 *          #getInfoField()}
+	 */
+	@Deprecated
+	@Override
+	public InfoField getField() {
+		return getInfoField();
+	}
+
+	@Override
+	public InfoField getInfoField() {
+		return InfoField.builder(
+		).infoFieldType(
+			TextInfoFieldType.INSTANCE
+		).name(
+			"informationTemplate_" + _ddmTemplate.getTemplateId()
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.<String>builder(
+			).value(
+				LocaleUtil.getDefault(),
+				_ddmTemplate.getName(LocaleUtil.getDefault())
+			).defaultLocale(
+				LocaleUtil.getDefault()
+			).build()
+		).build();
+	}
+
+	@Override
+	public Object getValue(Object model) {
+		return getValue(model, LocaleUtil.getDefault());
+	}
+
+	@Override
+	public Object getValue(Object model, Locale locale) {
+		return StringPool.BLANK;
+	}
+
+	private final DDMTemplate _ddmTemplate;
+
+}
