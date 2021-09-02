a8845e0093b0869553d4dae4c96d5cdd356d7d2e
Matija Petanjek <matija.petanjek@liferay.com>
2021-09-03 01:09:04 +0200

    COMMERCE-6491 headless-admin-batch-planner-test - add basic resource tests

--- modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-test/src/testIntegration/java/com/liferay/headless/admin/batch/planner/resource/v1_0/test/PlanResourceTest.java
+++ modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-test/src/testIntegration/java/com/liferay/headless/admin/batch/planner/resource/v1_0/test/PlanResourceTest.java
@@ -15,14 +15,95 @@
 package com.liferay.headless.admin.batch.planner.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.admin.batch.planner.client.dto.v1_0.Plan;
+import com.liferay.headless.admin.batch.planner.client.pagination.Page;
+import com.liferay.headless.admin.batch.planner.client.pagination.Pagination;
+import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
-import org.junit.Ignore;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Matija Petanjek
  */
-@Ignore
+@DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class PlanResourceTest extends BasePlanResourceTestCase {
+
+	@Override
+	@Test
+	public void testGetPlansPage() throws Exception {
+		Page<Plan> plansPage = planResource.getPlansPage(Pagination.of(1, 5));
+
+		Assert.assertEquals(0, plansPage.getTotalCount());
+
+		Collection<Plan> plans = plansPage.getItems();
+
+		Assert.assertTrue(plans.isEmpty());
+
+		List<Plan> randomPlans = new ArrayList<>();
+		int randomPlansCount = RandomTestUtil.randomInt(5, 10);
+
+		for (int i = 0; i < randomPlansCount; i++) {
+			randomPlans.add(_addPlan());
+		}
+
+		plansPage = planResource.getPlansPage(Pagination.of(1, 5));
+
+		Assert.assertEquals(randomPlansCount, plansPage.getTotalCount());
+
+		plans = plansPage.getItems();
+
+		Assert.assertEquals(plans.toString(), 5, plans.size());
+		Assert.assertTrue(randomPlans.containsAll(plans));
+	}
+
+	@Override
+	protected Plan randomPlan() {
+		return new Plan() {
+			{
+				active = RandomTestUtil.randomBoolean();
+				export = RandomTestUtil.randomBoolean();
+				externalType = "JSON";
+				externalURL = StringUtil.toLowerCase(
+					RandomTestUtil.randomString());
+				id = RandomTestUtil.randomLong();
+				internalClassName = StringUtil.toLowerCase(
+					RandomTestUtil.randomString());
+				name = StringUtil.toLowerCase(RandomTestUtil.randomString());
+			}
+		};
+	}
+
+	@Override
+	protected Plan testDeletePlan_addPlan() throws Exception {
+		return _addPlan();
+	}
+
+	@Override
+	protected Plan testGetPlan_addPlan() throws Exception {
+		return _addPlan();
+	}
+
+	@Override
+	protected Plan testPatchPlan_addPlan() throws Exception {
+		return _addPlan();
+	}
+
+	@Override
+	protected Plan testPostPlan_addPlan(Plan plan) throws Exception {
+		return _addPlan();
+	}
+
+	private Plan _addPlan() throws Exception {
+		return planResource.postPlan(randomPlan());
+	}
+
 }
