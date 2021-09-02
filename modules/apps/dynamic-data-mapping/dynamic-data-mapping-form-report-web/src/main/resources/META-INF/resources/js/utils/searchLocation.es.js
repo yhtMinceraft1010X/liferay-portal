fb53176f87cdcb0b93666e2ad074d2008f3806d7
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2021-09-02 12:58:43 -0300

    LPS-138055 Iterate through all fields to get the formatted data and fields

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/utils/searchLocation.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/utils/searchLocation.es.js
@@ -14,14 +14,6 @@
 
 import {toArray} from './data.es';
 
-const hasSearchLocationValue = (fields) => {
-	const searchLocationField = fields.find(
-		(field) => field.type === 'search_location'
-	);
-
-	return !!searchLocationField;
-};
-
 const parse = (value, defaultValue) => {
 	try {
 		return JSON.parse(value);
@@ -34,84 +26,77 @@ const parse = (value, defaultValue) => {
 };
 
 const transformSearchLocationValues = (fields, data) => {
-	if (hasSearchLocationValue(fields)) {
-		const labels = {
-			...{place: 'Search Location'},
-			address: 'Address',
-			city: 'City',
-			country: 'Country',
-			['postal-code']: 'Postal Code',
-			state: 'State',
-		};
-		const searchLocationFieldName = Object.keys(data).find((key) => {
-			if (typeof data[key] === 'object') {
-				return data[key]?.type === 'search_location';
-			}
+	let newData = {};
+	let newFields = [...fields];
+	let searchFields = [];
 
-			return false;
-		});
-		const visibleFields = Object.keys(labels);
-		const searchLocationFieldValues = {};
-		const dataSearchLocationFields = {};
-		const {values} = data[searchLocationFieldName];
+	Object.keys(fields).forEach((key) => {
+		const {name, type} = fields[key];
 
-		const searchLocationFields = visibleFields.map((visibleField) => {
-			toArray(values).forEach((value) => {
-				const existentValues =
-					searchLocationFieldValues[visibleField] || [];
-				const searchLocationValue = parse(value, {});
+		if (type === 'search_location') {
+			const labels = {
+				...{place: 'Search Location'},
+				address: 'Address',
+				city: 'City',
+				country: 'Country',
+				['postal-code']: 'Postal Code',
+				state: 'State',
+			};
 
-				searchLocationFieldValues[visibleField] = [
-					...existentValues,
-					{value: searchLocationValue[visibleField]},
-				];
+			const visibleFields = Object.keys(labels);
+			const searchLocationFieldValues = {};
+			const dataSearchLocationFields = {};
+			const {values} = data[name];
+
+			const searchLocationFields = visibleFields.map((visibleField) => {
+				toArray(values).forEach((value) => {
+					const existentValues =
+						searchLocationFieldValues[visibleField] || [];
+					const searchLocationValue = parse(value, {});
+
+					searchLocationFieldValues[visibleField] = [
+						...existentValues,
+						{value: searchLocationValue[visibleField]},
+					];
+				});
+
+				dataSearchLocationFields[visibleField] = {
+					totalEntries: Object.keys(
+						searchLocationFieldValues[visibleField]
+					).length,
+					type: visibleField,
+					values: searchLocationFieldValues[visibleField],
+				};
+
+				return {
+					columns: {},
+					label: labels[visibleField],
+					name: visibleField,
+					options: {},
+					parentFieldName: name,
+					rows: {},
+					type: visibleField,
+				};
 			});
 
-			dataSearchLocationFields[visibleField] = {
-				totalEntries: Object.keys(
-					searchLocationFieldValues[visibleField]
-				).length,
-				type: visibleField,
-				values: searchLocationFieldValues[visibleField],
-			};
+			newData[name] = dataSearchLocationFields;
 
-			return {
-				columns: {},
-				label: labels[visibleField],
-				name: visibleField,
-				options: {},
-				rows: {},
-				type: visibleField,
-			};
-		});
+			searchFields = searchFields.concat([...searchLocationFields]);
+		}
+		else {
+			newData = data;
+			newFields = fields;
+		}
+	});
 
-		const newFields = [...fields];
-		const searchLocationFieldIndex = newFields.findIndex(
-			(field) => field.type === 'search_location'
-		);
-
-		newFields.splice(searchLocationFieldIndex, 1, ...searchLocationFields);
-
-		delete data[searchLocationFieldName];
-
-		const newData = {
-			...data,
-			...dataSearchLocationFields,
-		};
-
-		newData.totalItems = Object.keys(newData).length - 1;
-
-		return {
-			data: newData,
-			fields: newFields,
-		};
-	}
-	else {
-		return {
-			data,
-			fields,
-		};
-	}
+	return {
+		data: newData,
+		fields: searchFields.length
+			? newFields
+					.filter((field) => field.type !== 'search_location')
+					.concat(searchFields)
+			: newFields,
+	};
 };
 
 export {transformSearchLocationValues};
