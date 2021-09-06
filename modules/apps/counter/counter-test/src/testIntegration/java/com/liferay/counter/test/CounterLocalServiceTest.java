2dcbf8c7ee5f3cbd0f20dab589b14dda35f15078
shuyangzhou <shuyang.zhou@liferay.com>
2021-09-05 19:26:21 -0700

    LPS-138126 Fix classpath for SPI loading.

--- modules/apps/counter/counter-test/src/testIntegration/java/com/liferay/counter/test/CounterLocalServiceTest.java
+++ modules/apps/counter/counter-test/src/testIntegration/java/com/liferay/counter/test/CounterLocalServiceTest.java
@@ -140,25 +140,18 @@ public class CounterLocalServiceTest {
 		ProcessConfig portalProcessConfig =
 			PortalClassPathUtil.getPortalProcessConfig();
 
-		ProtectionDomain protectionDomain =
-			CounterLocalServiceTest.class.getProtectionDomain();
-
-		CodeSource codeSource = protectionDomain.getCodeSource();
-
-		URL url = codeSource.getLocation();
-
-		File file = new File(url.toURI());
-
 		ProcessConfig.Builder builder = new ProcessConfig.Builder();
 
 		builder.setArguments(arguments);
 		builder.setBootstrapClassPath(
-			portalProcessConfig.getBootstrapClassPath());
+			_prependClassPath(
+				portalProcessConfig.getBootstrapClassPath(),
+				LiferayIntegrationTestRule.class));
 		builder.setReactClassLoader(PortalClassLoaderUtil.getClassLoader());
 		builder.setRuntimeClassPath(
-			StringBundler.concat(
-				file.getPath(), File.pathSeparator,
-				portalProcessConfig.getRuntimeClassPath()));
+			_prependClassPath(
+				portalProcessConfig.getRuntimeClassPath(),
+				CounterLocalServiceTest.class));
 
 		ProcessConfig processConfig = builder.build();
 
@@ -195,6 +188,21 @@ public class CounterLocalServiceTest {
 			Assert.assertEquals(
 				i + 1 + PropsValues.COUNTER_INCREMENT, id.intValue());
 		}
+	}
+
+	private String _prependClassPath(String baseClassPath, Class<?> clazz)
+		throws Exception {
+
+		ProtectionDomain protectionDomain = clazz.getProtectionDomain();
+
+		CodeSource codeSource = protectionDomain.getCodeSource();
+
+		URL url = codeSource.getLocation();
+
+		File file = new File(url.toURI());
+
+		return StringBundler.concat(
+			file.getPath(), File.pathSeparator, baseClassPath);
 	}
 
 	private static final String _COUNTER_NAME =
