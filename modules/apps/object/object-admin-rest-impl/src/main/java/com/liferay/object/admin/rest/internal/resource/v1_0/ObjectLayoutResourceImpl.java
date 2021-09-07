0e48fca05a27f6d9e00e85e57e30a12d188d2ca4
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-09-07 18:54:47 -0300

    LPS-138860 Implement Layout resources

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectLayoutResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectLayoutResourceImpl.java
@@ -14,9 +14,24 @@
 
 package com.liferay.object.admin.rest.internal.resource.v1_0;
 
+import com.liferay.object.admin.rest.dto.v1_0.ObjectLayout;
+import com.liferay.object.admin.rest.dto.v1_0.ObjectLayoutBox;
+import com.liferay.object.admin.rest.dto.v1_0.ObjectLayoutColumn;
+import com.liferay.object.admin.rest.dto.v1_0.ObjectLayoutRow;
+import com.liferay.object.admin.rest.dto.v1_0.ObjectLayoutTab;
 import com.liferay.object.admin.rest.resource.v1_0.ObjectLayoutResource;
+import com.liferay.object.service.ObjectLayoutLocalService;
+import com.liferay.object.service.persistence.ObjectLayoutBoxPersistence;
+import com.liferay.object.service.persistence.ObjectLayoutColumnPersistence;
+import com.liferay.object.service.persistence.ObjectLayoutRowPersistence;
+import com.liferay.object.service.persistence.ObjectLayoutTabPersistence;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +42,233 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = ObjectLayoutResource.class
 )
 public class ObjectLayoutResourceImpl extends BaseObjectLayoutResourceImpl {
+
+	@Override
+	public Page<ObjectLayout> getObjectDefinitionObjectLayoutsPage(
+		Long objectDefinitionId, Pagination pagination) {
+
+		return Page.of(
+			transform(
+				_objectLayoutLocalService.getObjectLayouts(
+					objectDefinitionId, pagination.getStartPosition(),
+					pagination.getEndPosition()),
+				this::_toObjectLayout),
+			pagination,
+			_objectLayoutLocalService.getObjectLayoutsCount(
+				objectDefinitionId));
+	}
+
+	@Override
+	public ObjectLayout getObjectLayout(Long objectLayoutId) throws Exception {
+		return _toObjectLayout(
+			_objectLayoutLocalService.getObjectLayout(objectLayoutId));
+	}
+
+	@Override
+	public ObjectLayout postObjectDefinitionObjectLayout(
+			Long objectDefinitionId, ObjectLayout objectLayout)
+		throws Exception {
+
+		return _toObjectLayout(
+			_objectLayoutLocalService.addObjectLayout(
+				contextUser.getUserId(), objectDefinitionId,
+				GetterUtil.getBoolean(objectLayout.getDefaultObjectLayout()),
+				LocalizedMapUtil.getLocalizedMap(objectLayout.getName()),
+				transformToList(
+					objectLayout.getObjectLayoutTabs(),
+					this::_toObjectLayoutTab)));
+	}
+
+	@Override
+	public ObjectLayout putObjectLayout(
+			Long objectLayoutId, ObjectLayout objectLayout)
+		throws Exception {
+
+		return _toObjectLayout(
+			_objectLayoutLocalService.updateObjectLayout(
+				objectLayoutId, objectLayout.getDefaultObjectLayout(),
+				LocalizedMapUtil.getLocalizedMap(objectLayout.getName()),
+				transformToList(
+					objectLayout.getObjectLayoutTabs(),
+					this::_toObjectLayoutTab)));
+	}
+
+	private ObjectLayout _toObjectLayout(
+		com.liferay.object.model.ObjectLayout serviceBuilderObjectLayout) {
+
+		return new ObjectLayout() {
+			{
+				dateCreated = serviceBuilderObjectLayout.getCreateDate();
+				dateModified = serviceBuilderObjectLayout.getModifiedDate();
+				defaultObjectLayout =
+					serviceBuilderObjectLayout.getDefaultObjectLayout();
+				id = serviceBuilderObjectLayout.getObjectLayoutId();
+				name = LocalizedMapUtil.getI18nMap(
+					serviceBuilderObjectLayout.getNameMap());
+				objectDefinitionId =
+					serviceBuilderObjectLayout.getObjectDefinitionId();
+				objectLayoutTabs = transformToArray(
+					serviceBuilderObjectLayout.getObjectLayoutTabs(),
+					objectLayoutTab -> _toObjectLayoutTab(objectLayoutTab),
+					ObjectLayoutTab.class);
+			}
+		};
+	}
+
+	private ObjectLayoutBox _toObjectLayoutBox(
+		com.liferay.object.model.ObjectLayoutBox
+			serviceBuilderObjectLayoutBox) {
+
+		return new ObjectLayoutBox() {
+			{
+				collapsable = serviceBuilderObjectLayoutBox.getCollapsable();
+				id = serviceBuilderObjectLayoutBox.getObjectLayoutBoxId();
+				name = LocalizedMapUtil.getI18nMap(
+					serviceBuilderObjectLayoutBox.getNameMap());
+				objectLayoutRows = transformToArray(
+					serviceBuilderObjectLayoutBox.getObjectLayoutRows(),
+					objectLayoutRow -> _toObjectLayoutRow(objectLayoutRow),
+					ObjectLayoutRow.class);
+				priority = serviceBuilderObjectLayoutBox.getPriority();
+			}
+		};
+	}
+
+	private com.liferay.object.model.ObjectLayoutBox _toObjectLayoutBox(
+		ObjectLayoutBox objectLayoutBox) {
+
+		com.liferay.object.model.ObjectLayoutBox serviceBuilderObjectLayoutBox =
+			_objectLayoutBoxPersistence.create(0L);
+
+		serviceBuilderObjectLayoutBox.setCollapsable(
+			objectLayoutBox.getCollapsable());
+		serviceBuilderObjectLayoutBox.setNameMap(
+			LocalizedMapUtil.getLocalizedMap(objectLayoutBox.getName()));
+		serviceBuilderObjectLayoutBox.setObjectLayoutRows(
+			transformToList(
+				objectLayoutBox.getObjectLayoutRows(),
+				this::_toObjectLayoutRow));
+		serviceBuilderObjectLayoutBox.setPriority(
+			objectLayoutBox.getPriority());
+
+		return serviceBuilderObjectLayoutBox;
+	}
+
+	private ObjectLayoutColumn _toObjectLayoutColumn(
+		com.liferay.object.model.ObjectLayoutColumn
+			serviceBuilderObjectLayoutColumn) {
+
+		return new ObjectLayoutColumn() {
+			{
+				id = serviceBuilderObjectLayoutColumn.getObjectLayoutColumnId();
+				objectFieldId =
+					serviceBuilderObjectLayoutColumn.getObjectFieldId();
+				priority = serviceBuilderObjectLayoutColumn.getPriority();
+			}
+		};
+	}
+
+	private com.liferay.object.model.ObjectLayoutColumn _toObjectLayoutColumn(
+		ObjectLayoutColumn objectLayoutColumn) {
+
+		com.liferay.object.model.ObjectLayoutColumn
+			serviceBuilderObjectLayoutColumn =
+				_objectLayoutColumnPersistence.create(0L);
+
+		serviceBuilderObjectLayoutColumn.setObjectFieldId(
+			objectLayoutColumn.getObjectFieldId());
+		serviceBuilderObjectLayoutColumn.setPriority(
+			objectLayoutColumn.getPriority());
+
+		return serviceBuilderObjectLayoutColumn;
+	}
+
+	private ObjectLayoutRow _toObjectLayoutRow(
+		com.liferay.object.model.ObjectLayoutRow
+			serviceBuilderObjectLayoutRow) {
+
+		return new ObjectLayoutRow() {
+			{
+				id = serviceBuilderObjectLayoutRow.getObjectLayoutRowId();
+				objectLayoutColumns = transformToArray(
+					serviceBuilderObjectLayoutRow.getObjectLayoutColumns(),
+					objectLayoutColumn -> _toObjectLayoutColumn(
+						objectLayoutColumn),
+					ObjectLayoutColumn.class);
+				priority = serviceBuilderObjectLayoutRow.getPriority();
+			}
+		};
+	}
+
+	private com.liferay.object.model.ObjectLayoutRow _toObjectLayoutRow(
+		ObjectLayoutRow objectLayoutRow) {
+
+		com.liferay.object.model.ObjectLayoutRow serviceBuilderObjectLayoutRow =
+			_objectLayoutRowPersistence.create(0L);
+
+		serviceBuilderObjectLayoutRow.setObjectLayoutColumns(
+			transformToList(
+				objectLayoutRow.getObjectLayoutColumns(),
+				this::_toObjectLayoutColumn));
+		serviceBuilderObjectLayoutRow.setPriority(
+			objectLayoutRow.getPriority());
+
+		return serviceBuilderObjectLayoutRow;
+	}
+
+	private ObjectLayoutTab _toObjectLayoutTab(
+		com.liferay.object.model.ObjectLayoutTab
+			serviceBuilderObjectLayoutTab) {
+
+		return new ObjectLayoutTab() {
+			{
+				id = serviceBuilderObjectLayoutTab.getObjectLayoutTabId();
+				name = LocalizedMapUtil.getI18nMap(
+					serviceBuilderObjectLayoutTab.getNameMap());
+				objectLayoutBoxes = transformToArray(
+					serviceBuilderObjectLayoutTab.getObjectLayoutBoxes(),
+					objectLayoutBox -> _toObjectLayoutBox(objectLayoutBox),
+					ObjectLayoutBox.class);
+				objectRelationshipId =
+					serviceBuilderObjectLayoutTab.getObjectRelationshipId();
+				priority = serviceBuilderObjectLayoutTab.getPriority();
+			}
+		};
+	}
+
+	private com.liferay.object.model.ObjectLayoutTab _toObjectLayoutTab(
+		ObjectLayoutTab objectLayoutTab) {
+
+		com.liferay.object.model.ObjectLayoutTab serviceBuilderObjectLayoutTab =
+			_objectLayoutTabPersistence.create(0L);
+
+		serviceBuilderObjectLayoutTab.setNameMap(
+			LocalizedMapUtil.getLocalizedMap(objectLayoutTab.getName()));
+		serviceBuilderObjectLayoutTab.setObjectLayoutBoxes(
+			transformToList(
+				objectLayoutTab.getObjectLayoutBoxes(),
+				this::_toObjectLayoutBox));
+		serviceBuilderObjectLayoutTab.setObjectRelationshipId(
+			objectLayoutTab.getObjectRelationshipId());
+		serviceBuilderObjectLayoutTab.setPriority(
+			objectLayoutTab.getPriority());
+
+		return serviceBuilderObjectLayoutTab;
+	}
+
+	@Reference
+	private ObjectLayoutBoxPersistence _objectLayoutBoxPersistence;
+
+	@Reference
+	private ObjectLayoutColumnPersistence _objectLayoutColumnPersistence;
+
+	@Reference
+	private ObjectLayoutLocalService _objectLayoutLocalService;
+
+	@Reference
+	private ObjectLayoutRowPersistence _objectLayoutRowPersistence;
+
+	@Reference
+	private ObjectLayoutTabPersistence _objectLayoutTabPersistence;
+
 }
