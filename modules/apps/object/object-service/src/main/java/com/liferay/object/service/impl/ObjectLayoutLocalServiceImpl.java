35a97160226965d453e425bd05dd548423d9612f
Brian Chan <brian.chan@liferay.com>
2021-09-07 18:36:33 -0300

    LPS-138735 Use JSON object, but you see how this is hard to read?

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectLayoutLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectLayoutLocalServiceImpl.java
@@ -14,10 +14,23 @@
 
 package com.liferay.object.service.impl;
 
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectLayout;
 import com.liferay.object.service.base.ObjectLayoutLocalServiceBaseImpl;
+import com.liferay.object.service.persistence.ObjectDefinitionPersistence;
+import com.liferay.object.service.persistence.ObjectLayoutTabPersistence;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marco Leo
@@ -29,4 +42,83 @@ import org.osgi.service.component.annotations.Component;
 )
 public class ObjectLayoutLocalServiceImpl
 	extends ObjectLayoutLocalServiceBaseImpl {
+
+	public ObjectLayout addObjectLayout(
+			long userId, long objectDefinitionId, boolean defaultObjectLayout,
+			Map<Locale, String> nameMap, JSONArray objectLayoutTabsJSONArray)
+		throws PortalException {
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionPersistence.findByPrimaryKey(objectDefinitionId);
+
+		ObjectLayout objectLayout = objectLayoutPersistence.create(
+			counterLocalService.increment());
+
+		User user = _userLocalService.getUser(userId);
+
+		objectLayout.setCompanyId(user.getCompanyId());
+		objectLayout.setUserId(user.getUserId());
+		objectLayout.setUserName(user.getFullName());
+
+		objectLayout.setObjectDefinitionId(
+			objectDefinition.getObjectDefinitionId());
+		objectLayout.setDefaultObjectLayout(defaultObjectLayout);
+		objectLayout.setNameMap(nameMap);
+
+		objectLayout = objectLayoutPersistence.update(objectLayout);
+
+		_addObjectLayoutTabs(objectLayoutTabsJSONArray);
+
+		return objectLayout;
+	}
+
+	private void _addObjectLayoutBox(JSONObject jsonObject) {
+		_addObjectLayoutBoxRows(jsonObject.getJSONArray("objectLayoutBoxRow"));
+	}
+
+	private void _addObjectLayoutBoxColumn(JSONObject jsonObject) {
+	}
+
+	private void _addObjectLayoutBoxColumns(JSONArray jsonArray) {
+		for (int i = 0; i < jsonArray.length(); i++) {
+			_addObjectLayoutBoxColumn(jsonArray.getJSONObject(i));
+		}
+	}
+
+	private void _addObjectLayoutBoxes(JSONArray jsonArray) {
+		for (int i = 0; i < jsonArray.length(); i++) {
+			_addObjectLayoutBox(jsonArray.getJSONObject(i));
+		}
+	}
+
+	private void _addObjectLayoutBoxRow(JSONObject jsonObject) {
+		_addObjectLayoutBoxColumns(
+			jsonObject.getJSONArray("objectLayoutBoxColumn"));
+	}
+
+	private void _addObjectLayoutBoxRows(JSONArray jsonArray) {
+		for (int i = 0; i < jsonArray.length(); i++) {
+			_addObjectLayoutBoxRow(jsonArray.getJSONObject(i));
+		}
+	}
+
+	private void _addObjectLayoutTab(JSONObject jsonObject) {
+		_addObjectLayoutBoxes(jsonObject.getJSONArray("objectLayoutBox"));
+	}
+
+	private void _addObjectLayoutTabs(JSONArray jsonArray) {
+		for (int i = 0; i < jsonArray.length(); i++) {
+			_addObjectLayoutTab(jsonArray.getJSONObject(i));
+		}
+	}
+
+	@Reference
+	private ObjectDefinitionPersistence _objectDefinitionPersistence;
+
+	@Reference
+	private ObjectLayoutTabPersistence _objectLayoutTabPersistence;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
