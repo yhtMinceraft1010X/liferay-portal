c294c114390f0b9d831718c211eae0347e8cae49
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-09-09 11:38:48 -0300

    LPS-134559 Add WorkflowTaskTransitionsResourceTest's integration test

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-test/src/testIntegration/java/com/liferay/headless/admin/workflow/resource/v1_0/test/WorkflowTaskTransitionsResourceTest.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-test/src/testIntegration/java/com/liferay/headless/admin/workflow/resource/v1_0/test/WorkflowTaskTransitionsResourceTest.java
@@ -15,15 +15,88 @@
 package com.liferay.headless.admin.workflow.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.admin.workflow.client.dto.v1_0.Transition;
+import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowDefinition;
+import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowInstance;
+import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowTask;
+import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowTaskIds;
+import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowTaskTransition;
+import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowTaskTransitions;
+import com.liferay.headless.admin.workflow.resource.v1_0.test.util.ObjectReviewedTestUtil;
+import com.liferay.headless.admin.workflow.resource.v1_0.test.util.WorkflowDefinitionTestUtil;
+import com.liferay.headless.admin.workflow.resource.v1_0.test.util.WorkflowInstanceTestUtil;
+import com.liferay.headless.admin.workflow.resource.v1_0.test.util.WorkflowTaskTestUtil;
+import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.kernel.util.ArrayUtil;
 
-import org.junit.Ignore;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
+@DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class WorkflowTaskTransitionsResourceTest
 	extends BaseWorkflowTaskTransitionsResourceTestCase {
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		BaseWorkflowTaskTransitionsResourceTestCase.setUpClass();
+
+		_workflowDefinition =
+			WorkflowDefinitionTestUtil.addWorkflowDefinition();
+	}
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_workflowInstance = WorkflowInstanceTestUtil.addWorkflowInstance(
+			testGroup.getGroupId(), ObjectReviewedTestUtil.addObjectReviewed(),
+			_workflowDefinition);
+	}
+
+	@Override
+	@Test
+	public void testPostWorkflowTaskTransition() throws Exception {
+		WorkflowTask workflowTask = WorkflowTaskTestUtil.getWorkflowTask(
+			_workflowInstance.getId());
+
+		WorkflowTaskTransitions workflowTaskTransitions =
+			workflowTaskTransitionsResource.postWorkflowTaskTransition(
+				new WorkflowTaskIds() {
+					{
+						workflowTaskIds = new Long[] {workflowTask.getId()};
+					}
+				});
+
+		WorkflowTaskTransition workflowTaskTransition =
+			(WorkflowTaskTransition)ArrayUtil.getValue(
+				workflowTaskTransitions.getWorkflowTaskTransitions(), 0);
+
+		Transition[] transitions = workflowTaskTransition.getTransitions();
+
+		Assert.assertEquals(
+			Arrays.toString(transitions), 2, transitions.length);
+
+		String[] expectedTransitionNames = {"approve", "reject"};
+
+		for (Transition transition : transitions) {
+			Assert.assertTrue(
+				ArrayUtil.contains(
+					expectedTransitionNames, transition.getName()));
+		}
+	}
+
+	private static WorkflowDefinition _workflowDefinition;
+
+	private WorkflowInstance _workflowInstance;
+
 }
