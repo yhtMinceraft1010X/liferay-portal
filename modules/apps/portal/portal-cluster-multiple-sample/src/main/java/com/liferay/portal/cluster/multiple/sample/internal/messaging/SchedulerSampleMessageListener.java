55c1bebc6d0166e3fe2ca63395d65b268ebbda1a
Kyle Miho <kyle.miho@liferay.com>
2021-09-09 15:28:09 -0700

    LPS-131312 Use better package name

--- modules/apps/portal/portal-cluster-multiple-sample/src/main/java/com/liferay/portal/cluster/multiple/sample/internal/messaging/SchedulerSampleMessageListener.java
+++ modules/apps/portal/portal-cluster-multiple-sample/src/main/java/com/liferay/portal/cluster/multiple/sample/internal/messaging/SchedulerSampleMessageListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.cluster.multiple.sample.internal;
+package com.liferay.portal.cluster.multiple.sample.internal.messaging;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
