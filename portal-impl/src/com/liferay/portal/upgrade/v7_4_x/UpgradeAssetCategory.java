14cf7cb6b3d3eca4c324e0a5b7ebd51f4e75f57d
Vendel Toreki <vendel.toreki@liferay.com>
2021-09-09 17:05:21 +0200

    LPS-135261 Add upgrade step

---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeAssetCategory.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_4_x;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.upgrade.v7_4_x.util.AssetCategoryTable;
+
+/**
+ * @author Vendel Toreki
+ */
+public class UpgradeAssetCategory extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		if (!hasColumnType(
+				getTableName(AssetCategoryTable.class), "title", "TEXT null")) {
+
+			alter(
+				AssetCategoryTable.class,
+				new AlterColumnType("title", "TEXT null"));
+		}
+
+		if (!hasColumnType(
+				getTableName(AssetCategoryTable.class), "description",
+				"TEXT null")) {
+
+			alter(
+				AssetCategoryTable.class,
+				new AlterColumnType("description", "TEXT null"));
+		}
+	}
+
+}
