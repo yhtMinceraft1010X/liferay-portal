38ec8fafa852d3903fe4eb060b1b2556b3b4b4f5
Javier de Arcos <javier.dearcos@liferay.com>
2021-09-09 15:24:03 +0200

    LPS-131087 Add the testGetPlansPage_addPlan method and use the autogenerated testGetPlansPage

--- modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-test/src/testIntegration/java/com/liferay/headless/admin/batch/planner/resource/v1_0/test/PlanResourceTest.java
+++ modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-test/src/testIntegration/java/com/liferay/headless/admin/batch/planner/resource/v1_0/test/PlanResourceTest.java
@@ -16,18 +16,10 @@ package com.liferay.headless.admin.batch.planner.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.headless.admin.batch.planner.client.dto.v1_0.Plan;
-import com.liferay.headless.admin.batch.planner.client.pagination.Page;
-import com.liferay.headless.admin.batch.planner.client.pagination.Pagination;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -36,34 +28,6 @@ import org.junit.runner.RunWith;
 @DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class PlanResourceTest extends BasePlanResourceTestCase {
-
-	@Override
-	@Test
-	public void testGetPlansPage() throws Exception {
-		Page<Plan> plansPage = planResource.getPlansPage(Pagination.of(1, 5));
-
-		Assert.assertEquals(0, plansPage.getTotalCount());
-
-		Collection<Plan> plans = plansPage.getItems();
-
-		Assert.assertTrue(plans.isEmpty());
-
-		List<Plan> randomPlans = new ArrayList<>();
-		int randomPlansCount = RandomTestUtil.randomInt(5, 10);
-
-		for (int i = 0; i < randomPlansCount; i++) {
-			randomPlans.add(_addPlan());
-		}
-
-		plansPage = planResource.getPlansPage(Pagination.of(1, 5));
-
-		Assert.assertEquals(randomPlansCount, plansPage.getTotalCount());
-
-		plans = plansPage.getItems();
-
-		Assert.assertEquals(plans.toString(), 5, plans.size());
-		Assert.assertTrue(randomPlans.containsAll(plans));
-	}
 
 	@Override
 	protected Plan randomPlan() {
@@ -90,6 +54,11 @@ public class PlanResourceTest extends BasePlanResourceTestCase {
 	@Override
 	protected Plan testGetPlan_addPlan() throws Exception {
 		return _addPlan();
+	}
+
+	@Override
+	protected Plan testGetPlansPage_addPlan(Plan plan) throws Exception {
+		return planResource.postPlan(plan);
 	}
 
 	@Override
