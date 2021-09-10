b3a9fcb5345c6fc24a2693b0dbf0888553bc4a93
Javier Gamarra <nhpatt@gmail.com>
2021-09-10 12:37:59 +0200

    LPS-131305 small SF

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
@@ -31,7 +31,6 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.List;
@@ -99,9 +98,8 @@ public class ObjectRelationshipLocalServiceImpl
 
 			objectRelationship.setDBTableName(
 				StringBundler.concat(
-					"R_", user.getCompanyId(),
-					objectDefinition1.getShortName(), "_",
-					objectDefinition2.getShortName(), "_", name));
+					"R_", user.getCompanyId(), objectDefinition1.getShortName(),
+					"_", objectDefinition2.getShortName(), "_", name));
 
 			runSQL(
 				StringBundler.concat(
