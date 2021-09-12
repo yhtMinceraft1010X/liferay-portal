5472e6d698bd68366ee2892397a712131be56070
Jose Abelenda <jose.abelenda@liferay.com>
2021-09-12 12:31:53 -0300

    LPS-142384 Define GET and POST methods in DSEnvelopeResourceImpl

--- modules/apps/digital-signature/digital-signature-rest-impl/src/main/java/com/liferay/digital/signature/rest/internal/resource/v1_0/DSEnvelopeResourceImpl.java
+++ modules/apps/digital-signature/digital-signature-rest-impl/src/main/java/com/liferay/digital/signature/rest/internal/resource/v1_0/DSEnvelopeResourceImpl.java
@@ -14,9 +14,14 @@
 
 package com.liferay.digital.signature.rest.internal.resource.v1_0;
 
+import com.liferay.digital.signature.manager.DSEnvelopeManager;
+import com.liferay.digital.signature.rest.dto.v1_0.DSEnvelope;
+import com.liferay.digital.signature.rest.internal.dto.v1_0.util.DSEnvelopeUtil;
 import com.liferay.digital.signature.rest.resource.v1_0.DSEnvelopeResource;
+import com.liferay.portal.kernel.util.Validator;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +32,33 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = DSEnvelopeResource.class
 )
 public class DSEnvelopeResourceImpl extends BaseDSEnvelopeResourceImpl {
+
+	@Override
+	public DSEnvelope getDSEnvelope(
+			Long companyId, Long groupId, String envelopeId)
+		throws Exception {
+
+		com.liferay.digital.signature.model.DSEnvelope dsEnvelope =
+			_dsEnvelopeManager.getDSEnvelope(companyId, groupId, envelopeId);
+
+		if (Validator.isNull(dsEnvelope.getDSEnvelopeId())) {
+			throw new Exception("Envelope does not exist!");
+		}
+
+		return DSEnvelopeUtil.toDSEnvelope(dsEnvelope);
+	}
+
+	@Override
+	public DSEnvelope postDSEnvelope(
+			Long companyId, Long groupId, DSEnvelope dsEnvelope)
+		throws Exception {
+
+		return DSEnvelopeUtil.toDSEnvelope(
+			_dsEnvelopeManager.addDSEnvelope(
+				companyId, groupId, DSEnvelopeUtil.toDSEnvelope(dsEnvelope)));
+	}
+
+	@Reference
+	private DSEnvelopeManager _dsEnvelopeManager;
+
 }
