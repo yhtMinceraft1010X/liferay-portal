2bfb0075b39c9e9b00d7a6ae83f7c5409f9afb61
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-09-13 09:47:49 +0200

    LPS-138914 Adds configuration for widget template available language types

---
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/configuration/TemplateConfiguration.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@ExtendedObjectClassDefinition(category = "templates")
+@Meta.OCD(
+	id = "com.liferay.template.web.internal.configuration.TemplateConfiguration"
+)
+public interface TemplateConfiguration {
+
+	@Meta.AD(
+		deflt = "ftl", description = "widget-template-language-types-help",
+		name = "widget-template-language-types", required = false
+	)
+	public String[] widgetTemplateLanguageTypes();
+
+}
