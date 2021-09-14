a24eefb3e1347422459520d7b1425028cf76c263
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-09-14 00:36:41 -0300

    LPS-139284 Define permissions for List Type Definition

---
+++ modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/internal/security/permission/resource/ListTypeDefinitionModelResourcePermission.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.list.type.internal.security.permission.resource;
+
+import com.liferay.list.type.constants.ListTypeConstants;
+import com.liferay.list.type.model.ListTypeDefinition;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.list.type.model.ListTypeDefinition",
+	service = ModelResourcePermission.class
+)
+public class ListTypeDefinitionModelResourcePermission
+	implements ModelResourcePermission<ListTypeDefinition> {
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker,
+			ListTypeDefinition listTypeDefinition, String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, listTypeDefinition, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, ListTypeDefinition.class.getName(),
+				listTypeDefinition.getPrimaryKey(), actionId);
+		}
+	}
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, long objectDefinitionId,
+			String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, objectDefinitionId, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, ListTypeDefinition.class.getName(),
+				objectDefinitionId, actionId);
+		}
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker,
+			ListTypeDefinition listTypeDefinition, String actionId)
+		throws PortalException {
+
+		return permissionChecker.hasPermission(
+			null, ListTypeDefinition.class.getName(),
+			listTypeDefinition.getPrimaryKey(), actionId);
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, long objectDefinitionId,
+			String actionId)
+		throws PortalException {
+
+		return permissionChecker.hasPermission(
+			null, ListTypeDefinition.class.getName(), objectDefinitionId,
+			actionId);
+	}
+
+	@Override
+	public String getModelName() {
+		return ListTypeDefinition.class.getName();
+	}
+
+	@Override
+	public PortletResourcePermission getPortletResourcePermission() {
+		return _portletResourcePermission;
+	}
+
+	@Reference(
+		target = "(resource.name=" + ListTypeConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+}
---
+++ modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/internal/security/permission/resource/ListTypePortletResourcePermissionRegistrar.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.list.type.internal.security.permission.resource;
+
+import com.liferay.list.type.constants.ListTypeConstants;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionLogic;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+@Component(immediate = true, service = {})
+public class ListTypePortletResourcePermissionRegistrar {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceRegistration = bundleContext.registerService(
+			PortletResourcePermission.class,
+			PortletResourcePermissionFactory.create(
+				ListTypeConstants.RESOURCE_NAME,
+				new ListTypePortletResourcePermissionLogic()),
+			HashMapDictionaryBuilder.<String, Object>put(
+				"resource.name", ListTypeConstants.RESOURCE_NAME
+			).build());
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	private ServiceRegistration<PortletResourcePermission> _serviceRegistration;
+
+	private static class ListTypePortletResourcePermissionLogic
+		implements PortletResourcePermissionLogic {
+
+		@Override
+		public Boolean contains(
+			PermissionChecker permissionChecker, String name, Group group,
+			String actionId) {
+
+			if (permissionChecker.hasPermission(group, name, 0, actionId)) {
+				return true;
+			}
+
+			return false;
+		}
+
+	}
+
+}
