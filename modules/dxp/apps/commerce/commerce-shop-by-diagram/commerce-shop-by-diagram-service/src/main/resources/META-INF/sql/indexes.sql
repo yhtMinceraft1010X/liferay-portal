93f49d21e10a080ff83715aff6d4187a8ffc7986
Alessio Rendina <asilcomando@gmail.com>
2021-09-14 17:05:07 +0200

    COMMERCE-7217 Removed no more necessary modules

--- modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,6 +0,0 @@
-create unique index IX_C8D3E29 on CPDefinitionDiagramEntry (CPDefinitionId, sequence[$COLUMN_LENGTH:75$]);
-
-create index IX_CD3A657 on CPDefinitionDiagramPin (CPDefinitionId);
-
-create unique index IX_13D84BD2 on CPDefinitionDiagramSetting (CPDefinitionId);
-create index IX_78A0FB77 on CPDefinitionDiagramSetting (uuid_[$COLUMN_LENGTH:75$], companyId);
