0729adca85965efe1dfae52ed2c82614ce7f87d7
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-09-13 23:51:41 -0300

    LPS-138741 Create Picklists portlet and Picklists Panel App

--- modules/apps/object/object-api/src/main/java/com/liferay/object/constants/ObjectPortletKeys.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/constants/ObjectPortletKeys.java
@@ -19,6 +19,10 @@ package com.liferay.object.constants;
  */
 public class ObjectPortletKeys {
 
+	public static final String LIST_TYPE_DEFINITIONS =
+		"com_liferay_object_web_internal_object_list_type_portlet_portlet_" +
+			"ListTypeDefinitionsPortlet";
+
 	public static final String OBJECT_DEFINITIONS =
 		"com_liferay_object_web_internal_object_definitions_portlet_" +
 			"ObjectDefinitionsPortlet";
