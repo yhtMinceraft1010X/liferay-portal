21b642cbd484d88b32b6b3dce9be36e6bd374563
Alessio Rendina <asilcomando@gmail.com>
2021-09-15 12:52:28 +0200

    COMMERCE-7217 Fixed compile errors

--- modules/dxp/apps/commerce/headless/headless-commerce-shop-by-diagram-impl/src/main/java/com/liferay/headless/commerce/shop/by/diagram/internal/dto/v1_0/converter/DiagramEntryDTOConverter.java
+++ modules/dxp/apps/commerce/headless/headless-commerce-shop-by-diagram-impl/src/main/java/com/liferay/headless/commerce/shop/by/diagram/internal/dto/v1_0/converter/DiagramEntryDTOConverter.java
@@ -14,10 +14,17 @@
 
 package com.liferay.headless.commerce.shop.by.diagram.internal.dto.v1_0.converter;
 
-import com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramEntry;
-import com.liferay.commerce.shop.by.diagram.service.CPDefinitionDiagramEntryService;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.model.CProduct;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.commerce.product.service.CPInstanceService;
+import com.liferay.commerce.shop.by.diagram.model.CSDiagramEntry;
+import com.liferay.commerce.shop.by.diagram.service.CSDiagramEntryService;
 import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.headless.commerce.shop.by.diagram.dto.v1_0.DiagramEntry;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 
@@ -29,11 +36,11 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	enabled = false,
-	property = "model.class.name=com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramEntry",
+	property = "model.class.name=com.liferay.commerce.shop.by.diagram.model.CSDiagramEntry",
 	service = {DiagramEntryDTOConverter.class, DTOConverter.class}
 )
 public class DiagramEntryDTOConverter
-	implements DTOConverter<CPDefinitionDiagramEntry, DiagramEntry> {
+	implements DTOConverter<CSDiagramEntry, DiagramEntry> {
 
 	@Override
 	public String getContentType() {
@@ -44,28 +51,62 @@ public class DiagramEntryDTOConverter
 	public DiagramEntry toDTO(DTOConverterContext dtoConverterContext)
 		throws Exception {
 
-		CPDefinitionDiagramEntry cpDefinitionDiagramEntry =
-			_cpDefinitionDiagramEntryService.getCPDefinitionDiagramEntry(
+		CSDiagramEntry csDiagramEntry =
+			_csDiagramEntryService.getCSDiagramEntry(
 				(Long)dtoConverterContext.getId());
 
-		ExpandoBridge expandoBridge =
-			cpDefinitionDiagramEntry.getExpandoBridge();
+		ExpandoBridge expandoBridge = csDiagramEntry.getExpandoBridge();
 
 		return new DiagramEntry() {
 			{
-				diagram = cpDefinitionDiagramEntry.isDiagram();
+				diagram = csDiagramEntry.isDiagram();
 				expando = expandoBridge.getAttributes();
-				id = cpDefinitionDiagramEntry.getCPDefinitionDiagramEntryId();
-				productId = cpDefinitionDiagramEntry.getCProductId();
-				quantity = cpDefinitionDiagramEntry.getQuantity();
-				sequence = cpDefinitionDiagramEntry.getSequence();
-				sku = cpDefinitionDiagramEntry.getSku();
-				skuUuid = cpDefinitionDiagramEntry.getCPInstanceUuid();
+				id = csDiagramEntry.getCSDiagramEntryId();
+				productId = csDiagramEntry.getCProductId();
+				quantity = csDiagramEntry.getQuantity();
+				sequence = csDiagramEntry.getSequence();
+				sku = csDiagramEntry.getSku();
+				skuUuid = String.valueOf(csDiagramEntry.getCPInstanceId());
+
+				setProductExternalReferenceCode(
+					() -> {
+						CPDefinition cpDefinition =
+							_cpDefinitionService.fetchCPDefinitionByCProductId(
+								csDiagramEntry.getCProductId());
+
+						if (cpDefinition == null) {
+							return StringPool.BLANK;
+						}
+
+						CProduct cProduct = cpDefinition.getCProduct();
+
+						return cProduct.getExternalReferenceCode();
+					});
+
+				setSkuExternalReferenceCode(
+					() -> {
+						CPInstance cpInstance =
+							_cpInstanceService.fetchCPInstance(
+								GetterUtil.getLong(
+									csDiagramEntry.getCPInstanceId()));
+
+						if (cpInstance == null) {
+							return StringPool.BLANK;
+						}
+
+						return cpInstance.getExternalReferenceCode();
+					});
 			}
 		};
 	}
 
 	@Reference
-	private CPDefinitionDiagramEntryService _cpDefinitionDiagramEntryService;
+	private CPDefinitionService _cpDefinitionService;
+
+	@Reference
+	private CPInstanceService _cpInstanceService;
+
+	@Reference
+	private CSDiagramEntryService _csDiagramEntryService;
 
 }
