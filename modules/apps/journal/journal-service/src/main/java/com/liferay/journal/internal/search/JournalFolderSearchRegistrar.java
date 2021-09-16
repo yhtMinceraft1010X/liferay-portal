969db2cc0d910e625e39f3ccc2de3c768c2ef1a9
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-09-16 14:33:10 +0200

    LPS-127799 Registering a ModelSearchDefinitionContributor for JournalFolder.
    Copy from AssetCategorySearchRegistrar, no logic changes.

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalFolderSearchRegistrar.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.search;
+
+import com.liferay.journal.model.JournalFolder;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchRegistrarHelper;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(immediate = true, service = {})
+public class JournalFolderSearchRegistrar {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceRegistration = modelSearchRegistrarHelper.register(
+			JournalFolder.class, bundleContext,
+			modelSearchDefinition -> {
+				modelSearchDefinition.setDefaultSelectedFieldNames(
+					Field.COMPANY_ID, Field.ENTRY_CLASS_NAME,
+					Field.ENTRY_CLASS_PK, Field.UID);
+				modelSearchDefinition.setDefaultSelectedLocalizedFieldNames(
+					Field.DESCRIPTION, Field.TITLE);
+				modelSearchDefinition.setModelIndexWriteContributor(
+					modelIndexWriterContributor);
+				modelSearchDefinition.setSelectAllLocales(true);
+			});
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference(
+		target = "(indexer.class.name=com.liferay.journal.model.JournalFolder)"
+	)
+	protected ModelIndexerWriterContributor<JournalFolder>
+		modelIndexWriterContributor;
+
+	@Reference
+	protected ModelSearchRegistrarHelper modelSearchRegistrarHelper;
+
+	private ServiceRegistration<?> _serviceRegistration;
+
+}
