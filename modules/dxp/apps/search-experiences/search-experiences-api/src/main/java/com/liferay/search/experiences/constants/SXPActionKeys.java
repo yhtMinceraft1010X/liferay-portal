4406eb9c770347daf842c20be84357d665683667
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-09-16 16:04:17 +0300

    LPS-112856 search-experiences-api: Action keys

---
+++ modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/constants/SXPActionKeys.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.constants;
+
+/**
+ * @author Petteri Karttunen
+ */
+public class SXPActionKeys {
+
+	public static final String ADD_SXP_BLUEPRINT = "ADD_SXP_BLUEPRINT";
+
+	public static final String ADD_SXP_ELEMENT = "ADD_SXP_ELEMENT";
+
+	public static final String APPLY_SXP_BLUEPRINT = "APPLY_SXP_BLUEPRINT";
+
+}
