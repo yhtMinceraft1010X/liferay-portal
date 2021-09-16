3bbb0de543f2b4acd2cf30c12c7eb7692f848768
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-09-16 13:12:13 +0300

    LPS-112856 search-experiences-api: Validator interfaces

---
+++ modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/validator/SXPBlueprintValidator.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.validator;
+
+import com.liferay.search.experiences.exception.SXPBlueprintConfigurationsJSONException;
+import com.liferay.search.experiences.exception.SXPBlueprintTitleException;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Petteri Karttunen
+ */
+public interface SXPBlueprintValidator {
+
+	public void validateConfigurationJSON(String configurationJSON)
+		throws SXPBlueprintConfigurationsJSONException;
+
+	public void validateSXPBlueprint(
+			Map<Locale, String> titleMap, String configurationJSON)
+		throws SXPBlueprintConfigurationsJSONException,
+			   SXPBlueprintTitleException;
+
+}
