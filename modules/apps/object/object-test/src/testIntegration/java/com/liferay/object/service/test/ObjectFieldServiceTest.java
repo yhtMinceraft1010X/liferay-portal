edff33f79c6c94a4b6282a5b28d8ca731cec5d15
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-09-15 23:57:17 -0300

    LPS-137827 Integration tests for remote service

---
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectFieldServiceTest.java
@@ -0,0 +1,271 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.service.ObjectFieldService;
+import com.liferay.object.util.LocalizedMapUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+@RunWith(Arquillian.class)
+public class ObjectFieldServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_defaultUser = _userLocalService.getDefaultUser(
+			TestPropsValues.getCompanyId());
+		_originalName = PrincipalThreadLocal.getName();
+		_originalPermissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+		_user = TestPropsValues.getUser();
+
+		_objectDefinition =
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				LocalizedMapUtil.getLocalizedMap("Test"), "Test", null, null,
+				LocalizedMapUtil.getLocalizedMap("Tests"),
+				ObjectDefinitionConstants.SCOPE_COMPANY, null);
+	}
+
+	@After
+	public void tearDown() {
+		PermissionThreadLocal.setPermissionChecker(_originalPermissionChecker);
+
+		PrincipalThreadLocal.setName(_originalName);
+	}
+
+	@Test
+	public void testAddCustomObjectField() throws Exception {
+		try {
+			_testAddCustomObjectField(_defaultUser);
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			String message = principalException.getMessage();
+
+			Assert.assertTrue(
+				message.contains(
+					"User " + _defaultUser.getUserId() +
+						" must have UPDATE permission for"));
+		}
+
+		_testAddCustomObjectField(_user);
+	}
+
+	@Test
+	public void testDeleteListTypeDefinition() throws Exception {
+		try {
+			_testDeleteObjectField(_defaultUser);
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			String message = principalException.getMessage();
+
+			Assert.assertTrue(
+				message.contains(
+					"User " + _defaultUser.getUserId() +
+						" must have UPDATE permission for"));
+		}
+
+		_testDeleteObjectField(_user);
+	}
+
+	@Test
+	public void testGetListTypeDefinition() throws Exception {
+		try {
+			_testGetObjectField(_defaultUser);
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			String message = principalException.getMessage();
+
+			Assert.assertTrue(
+				message.contains(
+					"User " + _defaultUser.getUserId() +
+						" must have VIEW permission for"));
+		}
+
+		_testGetObjectField(_user);
+	}
+
+	@Test
+	public void testUpdateListTypeDefinition() throws Exception {
+		try {
+			_testUpdateCustomObjectField(_defaultUser);
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			String message = principalException.getMessage();
+
+			Assert.assertTrue(
+				message.contains(
+					"User " + _defaultUser.getUserId() +
+						" must have UPDATE permission for"));
+		}
+
+		_testUpdateCustomObjectField(_user);
+	}
+
+	private ObjectField _addObjectField(User user) throws Exception {
+		return _objectFieldLocalService.addCustomObjectField(
+			user.getUserId(), 0, _objectDefinition.getObjectDefinitionId(),
+			false, false, null,
+			LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+			StringUtil.randomId(), true, "String");
+	}
+
+	private void _setUser(User user) {
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+
+		PrincipalThreadLocal.setName(user.getUserId());
+	}
+
+	private void _testAddCustomObjectField(User user) throws Exception {
+		ObjectField objectField = null;
+
+		try {
+			_setUser(user);
+
+			objectField = _objectFieldService.addCustomObjectField(
+				TestPropsValues.getUserId(), 0,
+				_objectDefinition.getObjectDefinitionId(), false, false, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				StringUtil.randomId(), true, "String");
+		}
+		finally {
+			if (objectField != null) {
+				_objectFieldLocalService.deleteObjectField(objectField);
+			}
+		}
+	}
+
+	private void _testDeleteObjectField(User user) throws Exception {
+		ObjectField deleteObjectField = null;
+		ObjectField objectField = null;
+
+		try {
+			_setUser(user);
+
+			objectField = _addObjectField(user);
+
+			deleteObjectField = _objectFieldService.deleteObjectField(
+				objectField.getObjectFieldId());
+		}
+		finally {
+			if (deleteObjectField == null) {
+				_objectFieldLocalService.deleteObjectField(objectField);
+			}
+		}
+	}
+
+	private void _testGetObjectField(User user) throws Exception {
+		ObjectField objectField = null;
+
+		try {
+			_setUser(user);
+
+			objectField = _addObjectField(user);
+
+			_objectFieldService.getObjectField(objectField.getObjectFieldId());
+		}
+		finally {
+			if (objectField != null) {
+				_objectFieldLocalService.deleteObjectField(objectField);
+			}
+		}
+	}
+
+	private void _testUpdateCustomObjectField(User user) throws Exception {
+		ObjectField objectField = null;
+
+		try {
+			_setUser(user);
+
+			objectField = _addObjectField(user);
+
+			objectField = _objectFieldService.updateCustomObjectField(
+				objectField.getObjectFieldId(), 0, true, false,
+				LanguageUtil.getLanguageId(LocaleUtil.getDefault()),
+				LocalizedMapUtil.getLocalizedMap("baker"), "baker", true,
+				"String");
+		}
+		finally {
+			if (objectField != null) {
+				_objectFieldLocalService.deleteObjectField(objectField);
+			}
+		}
+	}
+
+	private User _defaultUser;
+
+	@DeleteAfterTestRun
+	private ObjectDefinition _objectDefinition;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private ObjectFieldLocalService _objectFieldLocalService;
+
+	@Inject
+	private ObjectFieldService _objectFieldService;
+
+	private String _originalName;
+	private PermissionChecker _originalPermissionChecker;
+	private User _user;
+
+	@Inject(type = UserLocalService.class)
+	private UserLocalService _userLocalService;
+
+}
---
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectLayoutServiceTest.java
@@ -0,0 +1,231 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectLayout;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectLayoutLocalService;
+import com.liferay.object.service.ObjectLayoutService;
+import com.liferay.object.util.LocalizedMapUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Collections;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+@RunWith(Arquillian.class)
+public class ObjectLayoutServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_defaultUser = _userLocalService.getDefaultUser(
+			TestPropsValues.getCompanyId());
+		_originalName = PrincipalThreadLocal.getName();
+		_originalPermissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+		_user = TestPropsValues.getUser();
+
+		_objectDefinition =
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				LocalizedMapUtil.getLocalizedMap("Test"), "Test", null, null,
+				LocalizedMapUtil.getLocalizedMap("Tests"),
+				ObjectDefinitionConstants.SCOPE_COMPANY, null);
+	}
+
+	@After
+	public void tearDown() {
+		PermissionThreadLocal.setPermissionChecker(_originalPermissionChecker);
+
+		PrincipalThreadLocal.setName(_originalName);
+	}
+
+	@Test
+	public void testAddObjectLayout() throws Exception {
+		try {
+			_testAddObjectLayout(_defaultUser);
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			String message = principalException.getMessage();
+
+			Assert.assertTrue(
+				message.contains(
+					"User " + _defaultUser.getUserId() +
+						" must have UPDATE permission for"));
+		}
+
+		_testAddObjectLayout(_user);
+	}
+
+	@Test
+	public void testGetObjectLayout() throws Exception {
+		try {
+			_testGetObjectLayout(_defaultUser);
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			String message = principalException.getMessage();
+
+			Assert.assertTrue(
+				message.contains(
+					"User " + _defaultUser.getUserId() +
+						" must have VIEW permission for"));
+		}
+
+		_testGetObjectLayout(_user);
+	}
+
+	@Test
+	public void testUpdateObjectLayout() throws Exception {
+		try {
+			_testUpdateObjectLayout(_defaultUser);
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			String message = principalException.getMessage();
+
+			Assert.assertTrue(
+				message.contains(
+					"User " + _defaultUser.getUserId() +
+						" must have UPDATE permission for"));
+		}
+
+		_testUpdateObjectLayout(_user);
+	}
+
+	private ObjectLayout _addObjectLayout(User user) throws Exception {
+		return _objectLayoutLocalService.addObjectLayout(
+			user.getUserId(), _objectDefinition.getObjectDefinitionId(), true,
+			LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+			Collections.emptyList());
+	}
+
+	private void _setUser(User user) {
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+
+		PrincipalThreadLocal.setName(user.getUserId());
+	}
+
+	private void _testAddObjectLayout(User user) throws Exception {
+		ObjectLayout objectLayout = null;
+
+		try {
+			_setUser(user);
+
+			objectLayout = _objectLayoutService.addObjectLayout(
+				user.getUserId(), _objectDefinition.getObjectDefinitionId(),
+				true,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				Collections.emptyList());
+		}
+		finally {
+			if (objectLayout != null) {
+				_objectLayoutLocalService.deleteObjectLayout(objectLayout);
+			}
+		}
+	}
+
+	private void _testGetObjectLayout(User user) throws Exception {
+		ObjectLayout objectLayout = null;
+
+		try {
+			_setUser(user);
+
+			objectLayout = _addObjectLayout(user);
+
+			_objectLayoutService.getObjectLayout(
+				objectLayout.getObjectLayoutId());
+		}
+		finally {
+			if (objectLayout != null) {
+				_objectLayoutLocalService.deleteObjectLayout(objectLayout);
+			}
+		}
+	}
+
+	private void _testUpdateObjectLayout(User user) throws Exception {
+		ObjectLayout objectLayout = null;
+
+		try {
+			_setUser(user);
+
+			objectLayout = _addObjectLayout(user);
+
+			objectLayout = _objectLayoutService.updateObjectLayout(
+				objectLayout.getObjectLayoutId(), true,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				Collections.emptyList());
+		}
+		finally {
+			if (objectLayout != null) {
+				_objectLayoutLocalService.deleteObjectLayout(objectLayout);
+			}
+		}
+	}
+
+	private User _defaultUser;
+
+	@DeleteAfterTestRun
+	private ObjectDefinition _objectDefinition;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private ObjectLayoutLocalService _objectLayoutLocalService;
+
+	@Inject
+	private ObjectLayoutService _objectLayoutService;
+
+	private String _originalName;
+	private PermissionChecker _originalPermissionChecker;
+	private User _user;
+
+	@Inject(type = UserLocalService.class)
+	private UserLocalService _userLocalService;
+
+}
