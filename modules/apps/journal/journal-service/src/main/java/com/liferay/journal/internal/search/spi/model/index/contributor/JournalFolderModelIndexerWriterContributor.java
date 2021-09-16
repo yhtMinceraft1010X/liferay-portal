49df343a33a8c4d65a38cff89524978bb05b1411
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-09-16 13:40:27 +0200

    LPS-127799 Creates the ModelIndexerWriterContributor to handle JournalFolder

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/spi/model/index/contributor/JournalFolderModelIndexerWriterContributor.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.search.spi.model.index.contributor;
+
+import com.liferay.journal.model.JournalFolder;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.journal.model.JournalFolder",
+	service = ModelIndexerWriterContributor.class
+)
+public class JournalFolderModelIndexerWriterContributor
+	implements ModelIndexerWriterContributor<JournalFolder> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return null;
+	}
+
+	@Override
+	public long getCompanyId(JournalFolder journalFolder) {
+		return journalFolder.getCompanyId();
+	}
+
+}
