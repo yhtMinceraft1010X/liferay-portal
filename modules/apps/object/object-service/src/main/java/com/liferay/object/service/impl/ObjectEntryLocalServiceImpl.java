bd1866a736af865fa0f5b5210f34f02786db0315
Marco Leo <marco.leo@liferay.com>
2021-09-20 01:37:18 +0200

    LPS-135400 Add method to save directly informations inside the extension table

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -190,6 +190,34 @@ public class ObjectEntryLocalServiceImpl
 	}
 
 	@Override
+	public void addOrUpdateExtensionTableEntry(
+			long primaryKey, long objectDefinitionId,
+			Map<String, Serializable> values)
+		throws PortalException {
+
+		DynamicObjectDefinitionTable dynamicObjectDefinitionTable =
+			_getExtensionDynamicObjectDefinitionTable(objectDefinitionId);
+
+		Column<DynamicObjectDefinitionTable, Long> primaryKeyColumn =
+			dynamicObjectDefinitionTable.getPrimaryKeyColumn();
+
+		int modelCount = objectEntryPersistence.dslQueryCount(
+			DSLQueryFactoryUtil.count(
+			).from(
+				dynamicObjectDefinitionTable
+			).where(
+				primaryKeyColumn.eq(primaryKey)
+			));
+
+		if (modelCount > 0) {
+			_updateTable(dynamicObjectDefinitionTable, primaryKey, values);
+		}
+		else {
+			_insertIntoTable(dynamicObjectDefinitionTable, primaryKey, values);
+		}
+	}
+
+	@Override
 	public ObjectEntry addOrUpdateObjectEntry(
 			String externalReferenceCode, long userId, long groupId,
 			long objectDefinitionId, Map<String, Serializable> values,
