3af4619a9be9d0872848325805d1ab8d17a5e7a5
Brian Chan <brian.chan@liferay.com>
2021-09-20 19:47:11 -0300

    LPS-139555 remove unused indexes

--- modules/apps/object/object-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/object/object-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,4 +1,3 @@
-create index IX_384C6F1F on ObjectDefinition (companyId, active_, status);
 create index IX_5C293E0D on ObjectDefinition (companyId, active_, system_, status);
 create index IX_3E56F38F on ObjectDefinition (companyId, name[$COLUMN_LENGTH:75$]);
 create index IX_55C39BCE on ObjectDefinition (system_, status);
@@ -30,10 +29,8 @@ create index IX_644C876B on ObjectLayoutRow (uuid_[$COLUMN_LENGTH:75$], companyI
 create index IX_F01F1EEA on ObjectLayoutTab (objectLayoutId);
 create index IX_94D361A6 on ObjectLayoutTab (uuid_[$COLUMN_LENGTH:75$], companyId);
 
-create index IX_72BFC24A on ObjectRelationship (companyId, name[$COLUMN_LENGTH:75$]);
 create index IX_A71785B6 on ObjectRelationship (objectDefinitionId1, name[$COLUMN_LENGTH:75$]);
 create index IX_F6F370B8 on ObjectRelationship (objectDefinitionId1, objectDefinitionId2, name[$COLUMN_LENGTH:75$], type_[$COLUMN_LENGTH:75$]);
-create index IX_2A3F6ED7 on ObjectRelationship (objectDefinitionId1, objectDefinitionId2, type_[$COLUMN_LENGTH:75$]);
 create index IX_DE3EBEF8 on ObjectRelationship (objectDefinitionId2);
 create index IX_F1DC092D on ObjectRelationship (objectFieldId2);
 create index IX_11DAE1F1 on ObjectRelationship (uuid_[$COLUMN_LENGTH:75$], companyId);
