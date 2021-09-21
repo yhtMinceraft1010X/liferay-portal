ee746f55f04e50f12eb3b2c7d8ab4920ff212717
Matija Petanjek <matija.petanjek@liferay.com>
2021-09-21 19:17:15 +0200

    COMMERCE-7116 batch-planner-web - add MVCRenderCommand for batch planner plan jsp

---
+++ modules/apps/batch-planner/batch-planner-web/src/main/java/com/liferay/batch/planner/web/internal/portlet/action/ViewBatchPlannerPlansMVCRenderCommand.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.batch.planner.web.internal.portlet.action;
+
+import com.liferay.batch.planner.constants.BatchPlannerPortletKeys;
+import com.liferay.batch.planner.web.internal.display.context.BatchPlannerPlanDisplayContext;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Matija Petanjek
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + BatchPlannerPortletKeys.BATCH_PLANNER,
+		"mvc.command.name=/batch_planner/view_batch_planner_plans"
+	},
+	service = MVCRenderCommand.class
+)
+public class ViewBatchPlannerPlansMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		renderRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			new BatchPlannerPlanDisplayContext(renderRequest, renderResponse));
+
+		return "/view_batch_planner_plans.jsp";
+	}
+
+}
