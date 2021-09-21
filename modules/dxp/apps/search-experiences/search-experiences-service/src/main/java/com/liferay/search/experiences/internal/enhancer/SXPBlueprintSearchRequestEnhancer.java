46f6231bd3f579d1ae76f05cfc14119d1d20e268
André de Oliveira <andre.oliveira@liferay.com>
2021-09-21 15:22:08 -0500

    LPS-112856 Enhance: agg types with distinct properties and child aggregations
    https://issues.liferay.com/browse/LPS-112856

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/enhancer/SXPBlueprintSearchRequestEnhancer.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/enhancer/SXPBlueprintSearchRequestEnhancer.java
@@ -14,19 +14,28 @@
 
 package com.liferay.search.experiences.internal.enhancer;
 
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.search.aggregation.Aggregations;
+import com.liferay.portal.search.aggregation.metrics.CardinalityAggregation;
 import com.liferay.portal.search.filter.ComplexQueryPartBuilderFactory;
 import com.liferay.portal.search.query.Queries;
 import com.liferay.portal.search.searcher.SearchRequestBuilder;
 import com.liferay.search.experiences.rest.dto.v1_0.Aggregation;
+import com.liferay.search.experiences.rest.dto.v1_0.Avg;
+import com.liferay.search.experiences.rest.dto.v1_0.Cardinality;
 import com.liferay.search.experiences.rest.dto.v1_0.Clause;
 import com.liferay.search.experiences.rest.dto.v1_0.Configuration;
 import com.liferay.search.experiences.rest.dto.v1_0.General;
 import com.liferay.search.experiences.rest.dto.v1_0.Query;
 import com.liferay.search.experiences.rest.dto.v1_0.SXPBlueprint;
 
+import java.beans.ExceptionListener;
+
+import java.util.Arrays;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 /**
  * @author Petteri Karttunen
@@ -51,16 +60,73 @@ public class SXPBlueprintSearchRequestEnhancer {
 		processAggregations(_configuration.getAggregations());
 		processGeneral(_configuration.getGeneral());
 		processQueries(_configuration.getQueries());
+
+		if (!ArrayUtil.isEmpty(_runtimeException.getSuppressed())) {
+			throw _runtimeException;
+		}
+	}
+
+	protected static <V> void forEachCatching(
+		Iterable<V> iterable, Consumer<V> consumer,
+		ExceptionListener exceptionListener) {
+
+		for (V value : iterable) {
+			try {
+				consumer.accept(value);
+			}
+			catch (Exception exception) {
+				exceptionListener.exceptionThrown(exception);
+			}
+		}
+	}
+
+	protected static <K, V> void forEachCatching(
+		Map<K, V> map, BiConsumer<K, V> biConsumer,
+		ExceptionListener exceptionListener) {
+
+		map.forEach(
+			(key, value) -> {
+				try {
+					biConsumer.accept(key, value);
+				}
+				catch (Exception exception) {
+					exceptionListener.exceptionThrown(exception);
+				}
+			});
+	}
+
+	protected com.liferay.portal.search.aggregation.Aggregation getAggregation(
+		String name, Aggregation aggregation) {
+
+		if (aggregation.getAvg() != null) {
+			Avg avg = aggregation.getAvg();
+
+			return _aggregations.avg(name, avg.getField());
+		}
+
+		if (aggregation.getCardinality() != null) {
+			Cardinality cardinality = aggregation.getCardinality();
+
+			CardinalityAggregation cardinalityAggregation =
+				_aggregations.cardinality(name, cardinality.getField());
+
+			cardinalityAggregation.setPrecisionThreshold(
+				cardinality.getPrecision_threshold());
+
+			return cardinalityAggregation;
+		}
+
+		throw new IllegalArgumentException();
 	}
 
 	protected void processAggregation(String name, Aggregation aggregation) {
 		_searchRequestBuilder.addAggregation(
-			_aggregations.avg(name, String.valueOf(aggregation)));
+			translateAggregation(name, aggregation));
 	}
 
-	protected void processAggregations(Map<String, Aggregation> aggregations) {
-		aggregations.forEach(
-			(name, aggregation) -> processAggregation(name, aggregation));
+	protected void processAggregations(Map<String, Aggregation> map) {
+		forEachCatching(
+			map, this::processAggregation, _runtimeException::addSuppressed);
 	}
 
 	protected void processClause(Clause clause) {
@@ -79,8 +145,10 @@ public class SXPBlueprintSearchRequestEnhancer {
 	}
 
 	protected void processClauses(Clause[] clauses) {
-		for (Clause clause : clauses) {
-			processClause(clause);
+		if (!ArrayUtil.isEmpty(clauses)) {
+			forEachCatching(
+				Arrays.asList(clauses), this::processClause,
+				_runtimeException::addSuppressed);
 		}
 	}
 
@@ -94,8 +162,10 @@ public class SXPBlueprintSearchRequestEnhancer {
 	}
 
 	protected void processQueries(Query[] queries) {
-		for (Query query : queries) {
-			processQuery(query);
+		if (!ArrayUtil.isEmpty(queries)) {
+			forEachCatching(
+				Arrays.asList(queries), this::processQuery,
+				_runtimeException::addSuppressed);
 		}
 	}
 
@@ -109,11 +179,29 @@ public class SXPBlueprintSearchRequestEnhancer {
 		return _queries.wrapper(queryJSON);
 	}
 
+	protected com.liferay.portal.search.aggregation.Aggregation
+		translateAggregation(String name, Aggregation aggregation1) {
+
+		com.liferay.portal.search.aggregation.Aggregation aggregation2 =
+			getAggregation(name, aggregation1);
+
+		if (aggregation1.getAggs() != null) {
+			forEachCatching(
+				aggregation1.getAggs(),
+				(name1, aggregation) -> aggregation2.addChildAggregation(
+					translateAggregation(name1, aggregation)),
+				_runtimeException::addSuppressed);
+		}
+
+		return aggregation2;
+	}
+
 	private final Aggregations _aggregations;
 	private final ComplexQueryPartBuilderFactory
 		_complexQueryPartBuilderFactory;
 	private final Configuration _configuration;
 	private final Queries _queries;
+	private final RuntimeException _runtimeException = new RuntimeException();
 	private final SearchRequestBuilder _searchRequestBuilder;
 
 }
---
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/test/java/com/liferay/search/experiences/internal/enhancer/SXPBlueprintSearchRequestEnhancerTest.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.internal.enhancer;
+
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.search.aggregation.Aggregations;
+import com.liferay.portal.search.filter.ComplexQueryPart;
+import com.liferay.portal.search.filter.ComplexQueryPartBuilderFactory;
+import com.liferay.portal.search.internal.aggregation.AggregationsImpl;
+import com.liferay.portal.search.internal.filter.ComplexQueryPartBuilderFactoryImpl;
+import com.liferay.portal.search.internal.legacy.searcher.SearchRequestBuilderImpl;
+import com.liferay.portal.search.internal.query.QueriesImpl;
+import com.liferay.portal.search.internal.searcher.SearchRequestBuilderFactoryImpl;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.query.WrapperQuery;
+import com.liferay.portal.search.searcher.SearchRequest;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.search.experiences.rest.dto.v1_0.Aggregation;
+import com.liferay.search.experiences.rest.dto.v1_0.Avg;
+import com.liferay.search.experiences.rest.dto.v1_0.Cardinality;
+import com.liferay.search.experiences.rest.dto.v1_0.Clause;
+import com.liferay.search.experiences.rest.dto.v1_0.Configuration;
+import com.liferay.search.experiences.rest.dto.v1_0.General;
+import com.liferay.search.experiences.rest.dto.v1_0.Query;
+import com.liferay.search.experiences.rest.dto.v1_0.SXPBlueprint;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author André de Oliveira
+ */
+public class SXPBlueprintSearchRequestEnhancerTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	public SXPBlueprintSearchRequestEnhancerTest() {
+		_searchRequestBuilder = new SearchRequestBuilderImpl(
+			_searchRequestBuilderFactory);
+	}
+
+	@Test
+	public void testEnhance() {
+		SXPBlueprint sxpBlueprint = new SXPBlueprint();
+
+		Configuration configuration = new Configuration();
+
+		configuration.setAggregations(
+			HashMapBuilder.put(
+				"avg-test",
+				() -> {
+					Aggregation aggregation = new Aggregation();
+
+					Avg avg = new Avg();
+
+					avg.setField(RandomTestUtil.randomString());
+
+					aggregation.setAvg(avg);
+
+					return aggregation;
+				}
+			).put(
+				"cardinality-test",
+				() -> {
+					Aggregation aggregation = new Aggregation();
+
+					Cardinality cardinality = new Cardinality();
+
+					cardinality.setField(RandomTestUtil.randomString());
+					cardinality.setPrecision_threshold(
+						RandomTestUtil.randomInt());
+
+					aggregation.setCardinality(cardinality);
+
+					return aggregation;
+				}
+			).build());
+		configuration.setGeneral(new General());
+
+		Query query1 = new Query();
+
+		Clause clause = new Clause();
+
+		clause.setQueryJSON("{ \"term\": { \"status\": 0 } }");
+		clause.setOccur("must_not");
+
+		query1.setClauses(new Clause[] {clause});
+
+		query1.setEnabled(true);
+
+		configuration.setQueries(new Query[] {query1});
+
+		sxpBlueprint.setConfiguration(configuration);
+
+		SXPBlueprintSearchRequestEnhancer sxpBlueprintSearchRequestEnhancer =
+			new SXPBlueprintSearchRequestEnhancer(
+				_aggregations, _complexQueryPartBuilderFactory, _queries,
+				_searchRequestBuilder, sxpBlueprint);
+
+		sxpBlueprintSearchRequestEnhancer.enhance();
+
+		SearchRequest searchRequest = _searchRequestBuilder.build();
+
+		Map<String, com.liferay.portal.search.aggregation.Aggregation>
+			aggregationsMap = searchRequest.getAggregationsMap();
+
+		Assert.assertEquals(
+			aggregationsMap.toString(), 2, aggregationsMap.size());
+
+		List<ComplexQueryPart> complexQueryParts =
+			searchRequest.getComplexQueryParts();
+
+		ComplexQueryPart complexQueryPart = complexQueryParts.get(0);
+
+		Assert.assertEquals("must_not", complexQueryPart.getOccur());
+
+		com.liferay.portal.search.query.Query query2 =
+			complexQueryPart.getQuery();
+
+		WrapperQuery wrapperQuery = (WrapperQuery)query2;
+
+		Assert.assertEquals(
+			"{ \"term\": { \"status\": 0 } }",
+			new String(wrapperQuery.getSource()));
+	}
+
+	private final Aggregations _aggregations = new AggregationsImpl();
+	private final ComplexQueryPartBuilderFactory
+		_complexQueryPartBuilderFactory =
+			new ComplexQueryPartBuilderFactoryImpl();
+	private final Queries _queries = new QueriesImpl();
+	private final SearchRequestBuilder _searchRequestBuilder;
+	private final SearchRequestBuilderFactory _searchRequestBuilderFactory =
+		new SearchRequestBuilderFactoryImpl();
+
+}
