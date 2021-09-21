67e5ce95cbeb20ab78c784c9dc0a4ac1ee18e1ee
Brian Chan <brian.chan@liferay.com>
2021-09-20 21:14:53 -0300

    LPS-139169 rename test method

--- portal-impl/test/unit/com/liferay/portal/json/jabsorb/serializer/LiferayJSONSerializerTest.java
+++ portal-impl/test/unit/com/liferay/portal/json/jabsorb/serializer/LiferayJSONSerializerTest.java
@@ -42,7 +42,7 @@ public class LiferayJSONSerializerTest {
 		LiferayUnitTestRule.INSTANCE;
 
 	@Test
-	public void testGetClassFromHintSucceedsWhenClassCantBeLoadedFromContext()
+	public void testGetClassFromHintSucceedsWhenClassCannotBeLoadedFromContext()
 		throws Exception {
 
 		ClassLoaderPool.register(
