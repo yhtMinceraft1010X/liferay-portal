34070a49f2f95c3e783b76c9db443d13dacc72e3
Marco Leo <marco.leo@liferay.com>
2021-09-25 22:48:18 +0200

    LPS-139008 Implement Action Entry services

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionEntryLocalServiceImpl.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.service.impl;
+
+import com.liferay.object.model.ObjectActionEntry;
+import com.liferay.object.service.base.ObjectActionEntryLocalServiceBaseImpl;
+import com.liferay.portal.aop.AopService;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	property = "model.class.name=com.liferay.object.model.ObjectActionEntry",
+	service = AopService.class
+)
+public class ObjectActionEntryLocalServiceImpl
+	extends ObjectActionEntryLocalServiceBaseImpl {
+
+	@Override
+	public List<ObjectActionEntry> getObjectActionEntries(
+		long objectDefinitionId, String triggerName) {
+
+		return objectActionEntryPersistence.findByO_A_T(
+			objectDefinitionId, true, triggerName);
+	}
+
+}
