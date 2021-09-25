4f644b8992375b8892d832447f6735eb0382c438
Marco Leo <marco.leo@liferay.com>
2021-09-25 22:46:32 +0200

    LPS-139008 Call directly engine from the model listener, ensure is called in a diff thread after transaction

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/model/listener/ObjectEntryModelListener.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/model/listener/ObjectEntryModelListener.java
@@ -14,24 +14,25 @@
 
 package com.liferay.object.internal.model.listener;
 
-import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.action.ObjectActionEngine;
 import com.liferay.object.model.ObjectEntry;
-import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
+
+import java.io.Serializable;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
+ * @author Marco Leo
  * @author Brian Wing Shun Chan
  */
 @Component(immediate = true, service = ModelListener.class)
@@ -41,14 +42,14 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 	public void onAfterCreate(ObjectEntry objectEntry)
 		throws ModelListenerException {
 
-		_sendMessage("onAfterCreate", null, objectEntry);
+		_executeAction("onAfterCreate", null, objectEntry);
 	}
 
 	@Override
 	public void onAfterRemove(ObjectEntry objectEntry)
 		throws ModelListenerException {
 
-		_sendMessage("onAfterRemove", null, objectEntry);
+		_executeAction("onAfterRemove", null, objectEntry);
 	}
 
 	@Override
@@ -56,92 +57,57 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 			ObjectEntry originalObjectEntry, ObjectEntry objectEntry)
 		throws ModelListenerException {
 
-		_sendMessage("onAfterUpdate", originalObjectEntry, objectEntry);
+		_executeAction("onAfterUpdate", originalObjectEntry, objectEntry);
 	}
 
-	@Override
-	public void onBeforeCreate(ObjectEntry objectEntry)
-		throws ModelListenerException {
-
-		_sendMessage("onBeforeCreate", null, objectEntry);
-	}
-
-	@Override
-	public void onBeforeRemove(ObjectEntry objectEntry)
-		throws ModelListenerException {
-
-		_sendMessage("onBeforeRemove", null, objectEntry);
-	}
-
-	@Override
-	public void onBeforeUpdate(
-			ObjectEntry originalObjectEntry, ObjectEntry objectEntry)
-		throws ModelListenerException {
-
-		_sendMessage("onBeforeUpdate", originalObjectEntry, objectEntry);
-	}
-
-	private void _sendMessage(
-			String webhookEventKey, ObjectEntry originalObjectEntry,
+	private void _executeAction(
+			String triggerName, ObjectEntry originalObjectEntry,
 			ObjectEntry objectEntry)
 		throws ModelListenerException {
 
-		try {
-			ObjectDefinition objectDefinition =
-				_objectDefinitionLocalService.fetchObjectDefinition(
-					objectEntry.getObjectDefinitionId());
+		TransactionCommitCallbackUtil.registerCallback(
+			() -> {
+				_objectActionEngine.executeObjectActions(
+					PrincipalThreadLocal.getUserId(),
+					objectEntry.getModelClassName(), triggerName,
+					_getPayload(triggerName, originalObjectEntry, objectEntry));
 
-			if (objectDefinition == null) {
-				return;
-			}
-
-			JSONObject payloadJSONObject = JSONUtil.put(
-				"webhookEventKey", webhookEventKey);
-
-			JSONObject objectEntryJSONObject = _jsonFactory.createJSONObject(
-				objectEntry.toString());
-
-			objectEntryJSONObject.put("values", objectEntry.getValues());
-
-			payloadJSONObject.put("objectEntry", objectEntryJSONObject);
-
-			if (originalObjectEntry != null) {
-				JSONObject originalObjectEntryJSONObject =
-					_jsonFactory.createJSONObject(
-						originalObjectEntry.toString());
-
-				originalObjectEntryJSONObject.put(
-					"values", originalObjectEntry.getValues());
-
-				payloadJSONObject.put(
-					"originalObjectEntry", originalObjectEntryJSONObject);
-			}
-
-			_messageBus.sendMessage(
-				objectDefinition.getDestinationName(),
-				new Message() {
-					{
-						setPayload(payloadJSONObject.toString());
-					}
-				});
-		}
-		catch (Exception exception) {
-			_log.error(exception, exception);
-
-			throw new ModelListenerException(exception);
-		}
+				return null;
+			});
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		ObjectEntryModelListener.class);
+	private Serializable _getPayload(
+			String triggerName, ObjectEntry originalObjectEntry,
+			ObjectEntry objectEntry)
+		throws JSONException {
+
+		JSONObject payloadJSONObject = JSONUtil.put("trigger", triggerName);
+
+		JSONObject objectEntryJSONObject = _jsonFactory.createJSONObject(
+			objectEntry.toString());
+
+		objectEntryJSONObject.put("values", objectEntry.getValues());
+
+		payloadJSONObject.put("objectEntry", objectEntryJSONObject);
+
+		if (originalObjectEntry != null) {
+			JSONObject originalObjectEntryJSONObject =
+				_jsonFactory.createJSONObject(originalObjectEntry.toString());
+
+			originalObjectEntryJSONObject.put(
+				"values", originalObjectEntry.getValues());
+
+			payloadJSONObject.put(
+				"originalObjectEntry", originalObjectEntryJSONObject);
+		}
+
+		return payloadJSONObject.toString();
+	}
 
 	@Reference
 	private JSONFactory _jsonFactory;
 
 	@Reference
-	private MessageBus _messageBus;
-
-	@Reference
-	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+	private ObjectActionEngine _objectActionEngine;
 
 }
