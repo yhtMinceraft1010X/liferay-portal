6a6f3c36c6ae921d394f70a4d3e347894c2b6b54
Rubén Pulido <ruben.pulido@liferay.com>
2021-09-27 17:56:50 +0200

    LPS-138102 Create helper method getCollectionStyledLayoutStructureItemIds to obtain array of collection layout structure ids ancestors given a fragment entry link id

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/layout/structure/LayoutStructureUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/layout/structure/LayoutStructureUtil.java
@@ -22,6 +22,7 @@ import com.liferay.layout.page.template.model.LayoutPageTemplateStructureRel;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalServiceUtil;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureRelLocalServiceUtil;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureServiceUtil;
+import com.liferay.layout.util.constants.LayoutDataItemTypeConstants;
 import com.liferay.layout.util.structure.DeletedLayoutStructureItem;
 import com.liferay.layout.util.structure.FragmentStyledLayoutStructureItem;
 import com.liferay.layout.util.structure.LayoutStructure;
@@ -32,7 +33,9 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.ArrayUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Víctor Galán
@@ -87,6 +90,48 @@ public class LayoutStructureUtil {
 					layoutPageTemplateStructureRel.getSegmentsExperienceId(),
 					layoutStructure.toString());
 		}
+	}
+
+	public static List<String> getCollectionStyledLayoutStructureItemIds(
+		long fragmentEntryLinkId, LayoutStructure layoutStructure) {
+
+		if (layoutStructure == null) {
+			return Collections.emptyList();
+		}
+
+		LayoutStructureItem layoutStructureItem =
+			layoutStructure.getLayoutStructureItemByFragmentEntryLinkId(
+				fragmentEntryLinkId);
+
+		if (layoutStructureItem == null) {
+			return Collections.emptyList();
+		}
+
+		LayoutStructureItem currentLayoutStructureItem =
+			layoutStructure.getLayoutStructureItem(
+				layoutStructureItem.getParentItemId());
+
+		List<String> collectionStyledLayoutStructureItemIds = new ArrayList<>();
+
+		while (!Objects.equals(
+					currentLayoutStructureItem.getItemId(),
+					layoutStructure.getMainItemId())) {
+
+			if (Objects.equals(
+					LayoutDataItemTypeConstants.TYPE_COLLECTION,
+					currentLayoutStructureItem.getItemType())) {
+
+				collectionStyledLayoutStructureItemIds.add(
+					currentLayoutStructureItem.getItemId());
+			}
+
+			currentLayoutStructureItem = layoutStructure.getLayoutStructureItem(
+				currentLayoutStructureItem.getParentItemId());
+		}
+
+		Collections.reverse(collectionStyledLayoutStructureItemIds);
+
+		return collectionStyledLayoutStructureItemIds;
 	}
 
 	public static long[] getFragmentEntryLinkIds(
