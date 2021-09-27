e7db9ad47fe4876ac4312723c9df9a98d0f43098
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-09-27 13:11:42 +0200

    LPS-139878 Adds diamond operator

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/item/InfoItemServiceTrackerImpl.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/item/InfoItemServiceTrackerImpl.java
@@ -145,7 +145,7 @@ public class InfoItemServiceTrackerImpl implements InfoItemServiceTracker {
 	public List<InfoItemCapability> getInfoItemCapabilities(
 		String itemClassName) {
 
-		InfoItemCapabilitiesProvider infoItemCapabilitiesProvider =
+		InfoItemCapabilitiesProvider<?> infoItemCapabilitiesProvider =
 			getFirstInfoItemService(
 				InfoItemCapabilitiesProvider.class, itemClassName, null);
 
@@ -185,7 +185,7 @@ public class InfoItemServiceTrackerImpl implements InfoItemServiceTracker {
 		for (InfoItemClassDetails curInfoItemClassDetails :
 				getInfoItemClassDetails(InfoItemCapabilitiesProvider.class)) {
 
-			InfoItemCapabilitiesProvider infoItemCapabilitiesProvider =
+			InfoItemCapabilitiesProvider<?> infoItemCapabilitiesProvider =
 				getFirstInfoItemService(
 					InfoItemCapabilitiesProvider.class,
 					curInfoItemClassDetails.getClassName(), null);
@@ -306,7 +306,7 @@ public class InfoItemServiceTrackerImpl implements InfoItemServiceTracker {
 	private InfoItemClassDetails _getInfoItemClassDetails(
 		String itemClassName) {
 
-		InfoItemDetailsProvider infoItemDetailsProvider =
+		InfoItemDetailsProvider<?> infoItemDetailsProvider =
 			getFirstInfoItemService(
 				InfoItemDetailsProvider.class, itemClassName, null);
 
