e09abca8f59fe6d6438b64b48707b3548b817c7a
Marco Leo <marco.leo@liferay.com>
2021-09-27 18:12:07 +0200

    LPS-139008 Implement search framework

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectActionModelDocumentContributor.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.internal.search.spi.model.index.contributor;
+
+import com.liferay.object.model.ObjectAction;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.object.model.ObjectAction",
+	service = ModelDocumentContributor.class
+)
+public class ObjectActionModelDocumentContributor
+	implements ModelDocumentContributor<ObjectAction> {
+
+	@Override
+	public void contribute(Document document, ObjectAction objectAction) {
+		document.addText(Field.NAME, objectAction.getName());
+		document.addKeyword(
+			"objectDefinitionId", objectAction.getObjectDefinitionId());
+		document.remove(Field.USER_NAME);
+	}
+
+	@Reference
+	protected ClassNameLocalService classNameLocalService;
+
+}
---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectActionModelIndexerWriterContributor.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.internal.search.spi.model.index.contributor;
+
+import com.liferay.object.model.ObjectAction;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.object.model.ObjectAction",
+	service = ModelIndexerWriterContributor.class
+)
+public class ObjectActionModelIndexerWriterContributor
+	implements ModelIndexerWriterContributor<ObjectAction> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(ObjectAction objectAction) -> batchIndexingActionable.addDocuments(
+				modelIndexerWriterDocumentHelper.getDocument(objectAction)));
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				_objectFieldLocalService.getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(ObjectAction objectAction) {
+		return objectAction.getCompanyId();
+	}
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+	@Reference
+	private ObjectFieldLocalService _objectFieldLocalService;
+
+}
