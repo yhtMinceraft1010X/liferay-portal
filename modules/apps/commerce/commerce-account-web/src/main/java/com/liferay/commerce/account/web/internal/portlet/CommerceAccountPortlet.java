037471f1e7c38b1ec742ca05a0bf2e94b5be3518
Ivica Cardic <ivica.cardic@liferay.com>
2021-09-28 18:58:10 +0200

    COMMERCE-7288 - Keep hiden CommerceAccountPortlet to satisfy current account permissions

---
+++ modules/apps/commerce/commerce-account-web/src/main/java/com/liferay/commerce/account/web/internal/portlet/CommerceAccountPortlet.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.web.internal.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"javax.portlet.name=com_liferay_commerce_account_web_internal_portlet_CommerceAccountPortlet"
+	},
+	service = {CommerceAccountPortlet.class, Portlet.class}
+)
+public class CommerceAccountPortlet extends MVCPortlet {
+}
