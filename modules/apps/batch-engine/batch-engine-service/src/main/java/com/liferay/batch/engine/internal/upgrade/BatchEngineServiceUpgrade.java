3b6d29a7bc4e8b23d8d62e6bc4aae57d10d13200
joemduffy <89780329+joemduffy@users.noreply.github.com>
2021-09-28 13:25:15 +0100

    COMMERCE-7146 batch-engine-service upgrade java

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/upgrade/BatchEngineServiceUpgrade.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/upgrade/BatchEngineServiceUpgrade.java
@@ -19,6 +19,7 @@ import com.liferay.batch.engine.internal.upgrade.v4_0_1.ClassNameUpgradeProcess;
 import com.liferay.batch.engine.internal.upgrade.v4_1_0.TaskItemDelegateNameUpgradeProcess;
 import com.liferay.batch.engine.internal.upgrade.v4_2_0.BatchEngineImportTaskUpgradeProcess;
 import com.liferay.batch.engine.internal.upgrade.v4_3_0.BatchEngineExportTaskUpgradeProcess;
+import com.liferay.batch.engine.internal.upgrade.v4_3_1.BatchEngineTaskUpgradeProcess;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
@@ -48,6 +49,9 @@ public class BatchEngineServiceUpgrade implements UpgradeStepRegistrator {
 
 		registry.register(
 			"4.2.0", "4.3.0", new BatchEngineExportTaskUpgradeProcess());
+
+		registry.register(
+			"4.3.0", "4.3.1", new BatchEngineTaskUpgradeProcess());
 	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED)
