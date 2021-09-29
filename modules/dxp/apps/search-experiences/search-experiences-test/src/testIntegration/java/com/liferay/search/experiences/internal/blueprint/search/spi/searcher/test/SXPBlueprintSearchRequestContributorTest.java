713e94d765c3fc35c5cdc8ab1fe7d3707fcb2ad7
André de Oliveira <andre.oliveira@liferay.com>
2021-09-28 23:44:50 -0500

    LPS-112856 search-experiences-test: Search with a blueprint now returning actual results. Queries properly handled by specialized request contributor.
    https://issues.liferay.com/browse/LPS-112856

---
+++ modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/search/spi/searcher/test/SXPBlueprintSearchRequestContributorTest.java
@@ -0,0 +1,185 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.internal.blueprint.search.spi.searcher.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.portal.search.test.util.DocumentsAssert;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.search.experiences.model.SXPBlueprint;
+import com.liferay.search.experiences.service.SXPBlueprintLocalService;
+
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Wade Cao
+ * @author André de Oliveira
+ */
+@DataGuard(scope = DataGuard.Scope.METHOD)
+@RunWith(Arquillian.class)
+public class SXPBlueprintSearchRequestContributorTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		WorkflowThreadLocal.setEnabled(false);
+
+		_group = GroupTestUtil.addGroup();
+
+		_user = TestPropsValues.getUser();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group, TestPropsValues.getUserId());
+	}
+
+	@Test
+	public void testQueryConfiguration() throws Exception {
+		_addSXPBlueprint(_readConfiguration());
+
+		_addJournalArticles("alpha", "beta", "charlie");
+
+		_assertSearch("[beta]", "localized_title_en_US", "beta");
+	}
+
+	@Test
+	public void testSortConfiguration() throws Exception {
+		_addSXPBlueprint(_readConfiguration());
+
+		_addJournalArticles("alpha delta", "beta delta", "charlie delta");
+
+		_assertSearch(
+			"[charlie delta, beta delta, alpha delta]", "localized_title_en_US",
+			"delta");
+	}
+
+	@Rule
+	public TestName testName = new TestName();
+
+	private void _addJournalArticles(String... titles) throws Exception {
+		for (String title : titles) {
+			JournalTestUtil.addArticle(
+				_group.getGroupId(), 0,
+				PortalUtil.getClassNameId(JournalArticle.class),
+				HashMapBuilder.put(
+					LocaleUtil.US, title
+				).build(),
+				null,
+				HashMapBuilder.put(
+					LocaleUtil.US, ""
+				).build(),
+				LocaleUtil.getSiteDefault(), false, true, _serviceContext);
+		}
+	}
+
+	private SXPBlueprint _addSXPBlueprint(String configurationString)
+		throws Exception {
+
+		SXPBlueprint sxpBlueprint = _sxpBlueprintLocalService.addSXPBlueprint(
+			_user.getUserId(), configurationString,
+			Collections.singletonMap(LocaleUtil.US, "testDescription"), "",
+			Collections.singletonMap(LocaleUtil.US, "testTitle"),
+			_serviceContext);
+
+		_sxpBlueprint = sxpBlueprint;
+
+		return sxpBlueprint;
+	}
+
+	private void _assertSearch(
+			String expected, String fieldName, String keywords)
+		throws Exception {
+
+		SearchResponse searchResponse = _searcher.search(
+			_searchRequestBuilderFactory.builder(
+			).companyId(
+				TestPropsValues.getCompanyId()
+			).queryString(
+				keywords
+			).withSearchContext(
+				searchContext -> searchContext.setAttribute(
+					"search.experiences.blueprint.id",
+					_sxpBlueprint.getSXPBlueprintId())
+			).build());
+
+		DocumentsAssert.assertValues(
+			searchResponse.getRequestString(),
+			searchResponse.getDocumentsStream(), fieldName, expected);
+	}
+
+	private String _readConfiguration() {
+		Class<?> clazz = getClass();
+
+		return StringUtil.read(
+			clazz,
+			StringBundler.concat(
+				clazz.getSimpleName(), StringPool.PERIOD,
+				testName.getMethodName(), ".json"));
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private Searcher _searcher;
+
+	@Inject
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+	private ServiceContext _serviceContext;
+
+	@DeleteAfterTestRun
+	private SXPBlueprint _sxpBlueprint;
+
+	@Inject
+	private SXPBlueprintLocalService _sxpBlueprintLocalService;
+
+	private User _user;
+
+}
