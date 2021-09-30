93817f30e4af86cd549f7ae74ed9ab6de0063611
Brian Chan <brian.chan@liferay.com>
2021-09-30 10:14:04 -0300

    LPS-138761 Hugo SF

--- modules/apps/portal-security-audit/portal-security-audit-storage-service/src/main/java/com/liferay/portal/security/audit/storage/internal/model/listener/CompanyModelListener.java
+++ modules/apps/portal-security-audit/portal-security-audit-storage-service/src/main/java/com/liferay/portal/security/audit/storage/internal/model/listener/CompanyModelListener.java
@@ -52,7 +52,6 @@ public class CompanyModelListener extends BaseModelListener<Company> {
 			dynamicQuery -> dynamicQuery.add(
 				RestrictionsFactoryUtil.eq(
 					"companyId", company.getCompanyId())));
-
 		actionableDynamicQuery.setPerformActionMethod(
 			auditEvent -> _auditEventLocalService.deleteAuditEvent(
 				(AuditEvent)auditEvent));
