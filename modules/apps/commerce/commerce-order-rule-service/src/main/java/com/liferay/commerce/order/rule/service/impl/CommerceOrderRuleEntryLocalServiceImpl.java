5c034a5faf178b64aba6a4fd4d4a5474f122986f
Alessio Rendina <asilcomando@gmail.com>
2021-10-01 14:47:04 +0200

    COMMERCE-7414 Added commerce order rule entry local and remote service implementation

--- modules/apps/commerce/commerce-order-rule-service/src/main/java/com/liferay/commerce/order/rule/service/impl/CommerceOrderRuleEntryLocalServiceImpl.java
+++ modules/apps/commerce/commerce-order-rule-service/src/main/java/com/liferay/commerce/order/rule/service/impl/CommerceOrderRuleEntryLocalServiceImpl.java
@@ -14,8 +14,19 @@
 
 package com.liferay.commerce.order.rule.service.impl;
 
+import com.liferay.commerce.order.rule.model.CommerceOrderRuleEntry;
 import com.liferay.commerce.order.rule.service.base.CommerceOrderRuleEntryLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -29,4 +40,105 @@ import org.osgi.service.component.annotations.Component;
 )
 public class CommerceOrderRuleEntryLocalServiceImpl
 	extends CommerceOrderRuleEntryLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CommerceOrderRuleEntry addCommerceOrderRuleEntry(
+			String externalReferenceCode, long userId, boolean active,
+			String description, String name, int priority, String type,
+			String typeSettings)
+		throws PortalException {
+
+		long commerceOrderRuleEntryId = counterLocalService.increment();
+
+		CommerceOrderRuleEntry commerceOrderRuleEntry =
+			commerceOrderRuleEntryPersistence.create(commerceOrderRuleEntryId);
+
+		commerceOrderRuleEntry.setExternalReferenceCode(externalReferenceCode);
+
+		User user = userLocalService.getUser(userId);
+
+		commerceOrderRuleEntry.setCompanyId(user.getCompanyId());
+		commerceOrderRuleEntry.setUserId(user.getUserId());
+		commerceOrderRuleEntry.setUserName(user.getFullName());
+
+		commerceOrderRuleEntry.setActive(active);
+		commerceOrderRuleEntry.setDescription(description);
+		commerceOrderRuleEntry.setName(name);
+		commerceOrderRuleEntry.setPriority(priority);
+		commerceOrderRuleEntry.setType(type);
+
+		UnicodeProperties typeSettingsUnicodeProperties =
+			UnicodePropertiesBuilder.fastLoad(
+				typeSettings
+			).build();
+
+		commerceOrderRuleEntry.setTypeSettingsProperties(
+			typeSettingsUnicodeProperties);
+
+		return commerceOrderRuleEntryPersistence.update(commerceOrderRuleEntry);
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CommerceOrderRuleEntry deleteCommerceOrderRuleEntry(
+			long commerceOrderRuleEntryId)
+		throws PortalException {
+
+		return commerceOrderRuleEntryPersistence.remove(
+			commerceOrderRuleEntryId);
+	}
+
+	@Override
+	public List<CommerceOrderRuleEntry> getCommerceOrderRuleEntries(
+		long companyId, boolean active, int start, int end) {
+
+		return commerceOrderRuleEntryPersistence.findByC_A(
+			companyId, active, start, end);
+	}
+
+	@Override
+	public List<CommerceOrderRuleEntry> getCommerceOrderRuleEntries(
+		long companyId, boolean active, String type, int start, int end) {
+
+		return commerceOrderRuleEntryPersistence.findByC_A_LikeType(
+			companyId, active, type, start, end);
+	}
+
+	@Override
+	public List<CommerceOrderRuleEntry> getCommerceOrderRuleEntries(
+		long companyId, String type, int start, int end) {
+
+		return commerceOrderRuleEntryPersistence.findByC_LikeType(
+			companyId, type, start, end);
+	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CommerceOrderRuleEntry updateCommerceOrderRuleEntry(
+			long commerceOrderRuleEntryId, boolean active, String description,
+			String name, int priority, String typeSettings)
+		throws PortalException {
+
+		CommerceOrderRuleEntry commerceOrderRuleEntry =
+			commerceOrderRuleEntryLocalService.getCommerceOrderRuleEntry(
+				commerceOrderRuleEntryId);
+
+		commerceOrderRuleEntry.setActive(active);
+		commerceOrderRuleEntry.setDescription(description);
+		commerceOrderRuleEntry.setName(name);
+		commerceOrderRuleEntry.setPriority(priority);
+
+		UnicodeProperties typeSettingsUnicodeProperties =
+			UnicodePropertiesBuilder.fastLoad(
+				typeSettings
+			).build();
+
+		commerceOrderRuleEntry.setTypeSettingsProperties(
+			typeSettingsUnicodeProperties);
+
+		return commerceOrderRuleEntryPersistence.update(commerceOrderRuleEntry);
+	}
+
 }
--- modules/apps/commerce/commerce-order-rule-service/src/main/java/com/liferay/commerce/order/rule/service/impl/CommerceOrderRuleEntryServiceImpl.java
+++ modules/apps/commerce/commerce-order-rule-service/src/main/java/com/liferay/commerce/order/rule/service/impl/CommerceOrderRuleEntryServiceImpl.java
@@ -14,8 +14,17 @@
 
 package com.liferay.commerce.order.rule.service.impl;
 
+import com.liferay.commerce.order.rule.constants.CommerceOrderRuleEntryActionKeys;
+import com.liferay.commerce.order.rule.model.CommerceOrderRuleEntry;
 import com.liferay.commerce.order.rule.service.base.CommerceOrderRuleEntryServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -32,4 +41,110 @@ import org.osgi.service.component.annotations.Component;
 )
 public class CommerceOrderRuleEntryServiceImpl
 	extends CommerceOrderRuleEntryServiceBaseImpl {
+
+	@Override
+	public CommerceOrderRuleEntry addCommerceOrderRuleEntry(
+			String externalReferenceCode, boolean active, String description,
+			String name, int priority, String type, String typeSettings)
+		throws PortalException {
+
+		PortletResourcePermission portletResourcePermission =
+			_commerceOrderRuleEntryModelResourcePermission.
+				getPortletResourcePermission();
+
+		portletResourcePermission.check(
+			getPermissionChecker(), null,
+			CommerceOrderRuleEntryActionKeys.ADD_COMMERCE_ORDER_RULE);
+
+		return commerceOrderRuleEntryLocalService.addCommerceOrderRuleEntry(
+			externalReferenceCode, getUserId(), active, description, name,
+			priority, type, typeSettings);
+	}
+
+	@Override
+	public CommerceOrderRuleEntry deleteCommerceOrderRuleEntry(
+			long commerceOrderRuleEntryId)
+		throws PortalException {
+
+		_commerceOrderRuleEntryModelResourcePermission.check(
+			getPermissionChecker(), commerceOrderRuleEntryId,
+			ActionKeys.DELETE);
+
+		return commerceOrderRuleEntryLocalService.deleteCommerceOrderRuleEntry(
+			commerceOrderRuleEntryId);
+	}
+
+	@Override
+	public List<CommerceOrderRuleEntry> getCommerceOrderRuleEntries(
+			long companyId, boolean active, int start, int end)
+		throws PortalException {
+
+		PortletResourcePermission portletResourcePermission =
+			_commerceOrderRuleEntryModelResourcePermission.
+				getPortletResourcePermission();
+
+		portletResourcePermission.check(
+			getPermissionChecker(), null,
+			CommerceOrderRuleEntryActionKeys.VIEW_COMMERCE_ORDER_RULES);
+
+		return commerceOrderRuleEntryLocalService.getCommerceOrderRuleEntries(
+			companyId, active, start, end);
+	}
+
+	@Override
+	public List<CommerceOrderRuleEntry> getCommerceOrderRuleEntries(
+			long companyId, boolean active, String type, int start, int end)
+		throws PortalException {
+
+		PortletResourcePermission portletResourcePermission =
+			_commerceOrderRuleEntryModelResourcePermission.
+				getPortletResourcePermission();
+
+		portletResourcePermission.check(
+			getPermissionChecker(), null,
+			CommerceOrderRuleEntryActionKeys.VIEW_COMMERCE_ORDER_RULES);
+
+		return commerceOrderRuleEntryLocalService.getCommerceOrderRuleEntries(
+			companyId, active, type, start, end);
+	}
+
+	@Override
+	public List<CommerceOrderRuleEntry> getCommerceOrderRuleEntries(
+			long companyId, String type, int start, int end)
+		throws PortalException {
+
+		PortletResourcePermission portletResourcePermission =
+			_commerceOrderRuleEntryModelResourcePermission.
+				getPortletResourcePermission();
+
+		portletResourcePermission.check(
+			getPermissionChecker(), null,
+			CommerceOrderRuleEntryActionKeys.VIEW_COMMERCE_ORDER_RULES);
+
+		return commerceOrderRuleEntryLocalService.getCommerceOrderRuleEntries(
+			companyId, type, start, end);
+	}
+
+	@Override
+	public CommerceOrderRuleEntry updateCommerceOrderRuleEntry(
+			long commerceOrderRuleEntryId, boolean active, String description,
+			String name, int priority, String typeSettings)
+		throws PortalException {
+
+		_commerceOrderRuleEntryModelResourcePermission.check(
+			getPermissionChecker(), commerceOrderRuleEntryId,
+			ActionKeys.UPDATE);
+
+		return commerceOrderRuleEntryLocalService.updateCommerceOrderRuleEntry(
+			commerceOrderRuleEntryId, active, description, name, priority,
+			typeSettings);
+	}
+
+	private static volatile ModelResourcePermission<CommerceOrderRuleEntry>
+		_commerceOrderRuleEntryModelResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				CommerceOrderRuleEntryServiceImpl.class,
+				"_commerceOrderRuleEntryModelResourcePermission",
+				CommerceOrderRuleEntry.class);
+
 }
