4a5f1d6b852f43a2d626e1983403a70e468c78be
Alessio Rendina <asilcomando@gmail.com>
2021-10-01 14:44:42 +0200

    COMMERCE-7414 Added new constant

---
+++ modules/apps/commerce/commerce-order-rule-api/src/main/java/com/liferay/commerce/order/rule/constants/CommerceOrderRuleEntryActionKeys.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.rule.constants;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceOrderRuleEntryActionKeys {
+
+	public static final String ADD_COMMERCE_ORDER_RULE =
+		"ADD_COMMERCE_ORDER_RULE";
+
+	public static final String VIEW_COMMERCE_ORDER_RULES =
+		"VIEW_COMMERCE_ORDER_RULES";
+
+}
