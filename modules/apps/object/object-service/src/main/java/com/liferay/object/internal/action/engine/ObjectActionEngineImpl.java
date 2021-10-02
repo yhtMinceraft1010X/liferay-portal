fc45320c09b1cde0b482c32e90459810e52fc075
Brian Chan <brian.chan@liferay.com>
2021-10-02 08:41:19 -0300

    LPS-139008 use registry

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/engine/ObjectActionEngineImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/engine/ObjectActionEngineImpl.java
@@ -16,13 +16,12 @@ package com.liferay.object.internal.action.engine;
 
 import com.liferay.object.action.engine.ObjectActionEngine;
 import com.liferay.object.action.executor.ObjectActionExecutor;
+import com.liferay.object.action.executor.ObjectActionExecutorRegistry;
 import com.liferay.object.action.request.ObjectActionRequest;
 import com.liferay.object.model.ObjectAction;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectActionLocalService;
 import com.liferay.object.service.ObjectDefinitionLocalService;
-import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
-import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
@@ -34,10 +33,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -58,23 +54,6 @@ public class ObjectActionEngineImpl implements ObjectActionEngine {
 		catch (Exception exception) {
 			_log.error(exception, exception);
 		}
-	}
-
-	@Activate
-	protected void activate(BundleContext bundleContext) {
-		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
-			bundleContext, ObjectActionExecutor.class, null,
-			(serviceReference, emitter) -> {
-				ObjectActionExecutor objectActionExecutor =
-					bundleContext.getService(serviceReference);
-
-				emitter.emit(objectActionExecutor.getKey());
-			});
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_serviceTrackerMap.close();
 	}
 
 	private void _executeObjectActions(
@@ -107,7 +86,7 @@ public class ObjectActionEngineImpl implements ObjectActionEngine {
 
 		for (ObjectAction objectAction : objectActions) {
 			ObjectActionExecutor objectActionExecutor =
-				_serviceTrackerMap.getService(
+				_objectActionExecutorRegistry.getObjectActionExecutor(
 					objectAction.getObjectActionExecutorKey());
 
 			objectActionExecutor.execute(
@@ -125,12 +104,13 @@ public class ObjectActionEngineImpl implements ObjectActionEngine {
 		ObjectActionEngineImpl.class);
 
 	@Reference
+	private ObjectActionExecutorRegistry _objectActionExecutorRegistry;
+
+	@Reference
 	private ObjectActionLocalService _objectActionLocalService;
 
 	@Reference
 	private ObjectDefinitionLocalService _objectDefinitionLocalService;
-
-	private ServiceTrackerMap<String, ObjectActionExecutor> _serviceTrackerMap;
 
 	@Reference
 	private UserLocalService _userLocalService;
