ece8999dd45eab42a24b2b6109c8fe6a4dd6374d
André de Oliveira <andre.oliveira@liferay.com>
2021-10-04 18:20:13 -0500

    LPS-112856 search-experiences-rest-impl: Can search over REST taking a Blueprint Configuration as input (same URL params as Search Page)
    https://issues.liferay.com/browse/LPS-112856

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SearchResponseResourceImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SearchResponseResourceImpl.java
@@ -14,17 +14,89 @@
 
 package com.liferay.search.experiences.rest.internal.resource.v1_0;
 
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.search.experiences.rest.dto.v1_0.SXPBlueprint;
+import com.liferay.search.experiences.rest.dto.v1_0.SearchResponse;
+import com.liferay.search.experiences.rest.dto.v1_0.enhancer.SXPBlueprintSearchRequestEnhancer;
+import com.liferay.search.experiences.rest.dto.v1_0.translator.SearchResponseTranslator;
+import com.liferay.search.experiences.rest.dto.v1_0.util.ConfigurationUtil;
 import com.liferay.search.experiences.rest.resource.v1_0.SearchResponseResource;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author André de Oliveira
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v1_0/search-response.properties",
 	scope = ServiceScope.PROTOTYPE, service = SearchResponseResource.class
 )
 public class SearchResponseResourceImpl extends BaseSearchResponseResourceImpl {
+
+	@Override
+	public SearchResponse getSearch(
+			Integer delta, String q, Integer start, String sxpBlueprintJSON)
+		throws Exception {
+
+		return _searchResponseTranslator.translate(
+			_searcher.search(
+				_searchRequestBuilderFactory.builder(
+				).emptySearchEnabled(
+					true
+				).includeResponseString(
+					true
+				).companyId(
+					contextCompany.getCompanyId()
+				).from(
+					_getFrom(delta, start)
+				).queryString(
+					q
+				).size(
+					delta
+				).withSearchRequestBuilder(
+					searchRequestBuilder -> {
+						if (sxpBlueprintJSON != null) {
+							_sxpBlueprintSearchRequestEnhancer.enhance(
+								searchRequestBuilder,
+								_toSXPBlueprint(sxpBlueprintJSON));
+						}
+					}
+				).build()));
+	}
+
+	private Integer _getFrom(Integer delta, Integer start) {
+		if ((delta == null) || (start == null)) {
+			return null;
+		}
+
+		return start * delta;
+	}
+
+	private SXPBlueprint _toSXPBlueprint(String sxpBlueprintJSON) {
+		SXPBlueprint sxpBlueprint = SXPBlueprint.toDTO(sxpBlueprintJSON);
+
+		sxpBlueprint.setConfiguration(
+			ConfigurationUtil.toConfiguration(
+				String.valueOf(sxpBlueprint.getConfiguration())));
+
+		return sxpBlueprint;
+	}
+
+	@Reference
+	private Searcher _searcher;
+
+	@Reference
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+	@Reference
+	private SearchResponseTranslator _searchResponseTranslator;
+
+	@Reference
+	private SXPBlueprintSearchRequestEnhancer
+		_sxpBlueprintSearchRequestEnhancer;
+
 }
