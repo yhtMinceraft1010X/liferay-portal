cb4fc2c8b657cce55c4fcead7295f00c98aed099
Diego Clemente <diego.clemente@liferay.com>
2021-10-04 15:43:58 -0300

    LPS-140212 Source format

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/SearchLocation/SearchLocation.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/SearchLocation/SearchLocation.es.js
@@ -155,8 +155,7 @@ const Main = ({
 	useEffect(() => {
 		window.gm_authFailure = function () {
 			Liferay.Util.openToast({
-				message:
-					Liferay.Language.get('api-provider-error-message'),
+				message: Liferay.Language.get('api-provider-error-message'),
 				title: Liferay.Language.get('error'),
 				type: 'danger',
 			});
