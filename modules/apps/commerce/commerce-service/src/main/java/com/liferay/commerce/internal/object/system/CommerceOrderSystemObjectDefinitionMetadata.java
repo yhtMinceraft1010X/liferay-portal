3fc01737dd32a5e96ffcf26b0a0cfcd41d4baca7
Brian Chan <brian.chan@liferay.com>
2021-10-04 10:23:21 -0300

    LPS-135400 revert commit b28a99cdcccd280a28c4709f836b655f8fa824eb

---
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/object/system/CommerceOrderSystemObjectDefinitionMetadata.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.object.system;
+
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderTable;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.system.BaseSystemObjectDefinitionMetadata;
+import com.liferay.object.system.SystemObjectDefinitionMetadata;
+import com.liferay.petra.sql.dsl.Column;
+import com.liferay.petra.sql.dsl.Table;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ * @author Brian Wing Shun Chan
+ */
+@Component(
+	enabled = false, immediate = true,
+	service = SystemObjectDefinitionMetadata.class
+)
+public class CommerceOrderSystemObjectDefinitionMetadata
+	extends BaseSystemObjectDefinitionMetadata {
+
+	@Override
+	public String getClassName() {
+		return CommerceOrder.class.getName();
+	}
+
+	@Override
+	public Map<Locale, String> getLabelMap() {
+		return createLabelMap("commerce-order");
+	}
+
+	@Override
+	public List<ObjectField> getObjectFields() {
+		return Arrays.asList(
+			createObjectField("order-status", "orderStatus", true, "Integer"),
+			createObjectField(
+				"shipping-amount", "shippingAmount", true, "BigDecimal"));
+	}
+
+	@Override
+	public Map<Locale, String> getPluralLabelMap() {
+		return createLabelMap("commerce-orders");
+	}
+
+	@Override
+	public Column<?, Long> getPrimaryKeyColumn() {
+		return CommerceOrderTable.INSTANCE.commerceOrderId;
+	}
+
+	@Override
+	public String getScope() {
+		return ObjectDefinitionConstants.SCOPE_COMPANY;
+	}
+
+	@Override
+	public Table getTable() {
+		return CommerceOrderTable.INSTANCE;
+	}
+
+	@Override
+	public int getVersion() {
+		return 1;
+	}
+
+}
