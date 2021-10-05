934fe007dea14e3d91c44ac36daf5c702bcc301b
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-10-05 14:35:29 +0200

    LPS-140066 Adding utils methods to create entity models

---
+++ modules/apps/template/template-web-test/src/testIntegration/java/com/liferay/template/web/internal/portlet/action/test/TemplateTestUtil.java
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.web.internal.portlet.action.test;
+
+import com.liferay.dynamic.data.mapping.constants.DDMTemplateConstants;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateServiceUtil;
+import com.liferay.info.item.InfoItemClassDetails;
+import com.liferay.info.item.InfoItemFormVariation;
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.info.item.provider.InfoItemFormVariationsProvider;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.template.info.item.capability.TemplateInfoItemCapability;
+import com.liferay.template.model.TemplateEntry;
+import com.liferay.template.service.TemplateEntryLocalServiceUtil;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class TemplateTestUtil {
+
+	public static TemplateEntry addAnyTemplateEntry(
+			InfoItemServiceTracker infoItemServiceTracker,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		InfoItemClassDetails infoItemClassDetails =
+			getFirstTemplateInfoItemClassDetails(
+				infoItemServiceTracker, serviceContext.getScopeGroupId());
+
+		InfoItemFormVariation infoItemFormVariation =
+			getFirstInfoItemFormVariation(
+				infoItemClassDetails, infoItemServiceTracker,
+				serviceContext.getScopeGroupId());
+
+		return addTemplateEntry(
+			infoItemClassDetails.getClassName(), infoItemFormVariation.getKey(),
+			serviceContext);
+	}
+
+	public static DDMTemplate addDDMTemplate(
+			long classNameId, long classPK, long resourceClassNameId,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		return DDMTemplateServiceUtil.addTemplate(
+			serviceContext.getScopeGroupId(), classNameId, classPK,
+			resourceClassNameId,
+			HashMapBuilder.put(
+				PortalUtil.getSiteDefaultLocale(
+					serviceContext.getScopeGroupId()),
+				RandomTestUtil.randomString()
+			).build(),
+			HashMapBuilder.put(
+				PortalUtil.getSiteDefaultLocale(
+					serviceContext.getScopeGroupId()),
+				RandomTestUtil.randomString()
+			).build(),
+			DDMTemplateConstants.TEMPLATE_TYPE_DISPLAY, StringPool.BLANK,
+			TemplateConstants.LANG_TYPE_FTL, RandomTestUtil.randomString(),
+			serviceContext);
+	}
+
+	public static TemplateEntry addTemplateEntry(
+			String infoItemClassName, String infoItemFormVariationKey,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		DDMTemplate ddmTemplate = addDDMTemplate(
+			PortalUtil.getClassNameId(InfoItemFormProvider.class), 0,
+			PortalUtil.getClassNameId(InfoItemFormProvider.class),
+			serviceContext);
+
+		return TemplateEntryLocalServiceUtil.addTemplateEntry(
+			serviceContext.getUserId(), serviceContext.getScopeGroupId(),
+			ddmTemplate.getTemplateId(), infoItemClassName,
+			infoItemFormVariationKey);
+	}
+
+	public static InfoItemFormVariation getFirstInfoItemFormVariation(
+		InfoItemClassDetails infoItemClassDetails,
+		InfoItemServiceTracker infoItemServiceTracker, long groupId) {
+
+		InfoItemFormVariationsProvider<?> infoItemFormVariationsProvider =
+			infoItemServiceTracker.getFirstInfoItemService(
+				InfoItemFormVariationsProvider.class,
+				infoItemClassDetails.getClassName());
+
+		if (infoItemFormVariationsProvider != null) {
+			Collection<InfoItemFormVariation> infoItemFormVariations =
+				infoItemFormVariationsProvider.getInfoItemFormVariations(
+					groupId);
+
+			Stream<InfoItemFormVariation> infoItemFormVariationsStream =
+				infoItemFormVariations.stream();
+
+			return infoItemFormVariationsStream.findFirst(
+			).orElse(
+				null
+			);
+		}
+
+		return null;
+	}
+
+	public static InfoItemClassDetails getFirstTemplateInfoItemClassDetails(
+		InfoItemServiceTracker infoItemServiceTracker, long groupId) {
+
+		for (InfoItemClassDetails infoItemClassDetails :
+				infoItemServiceTracker.getInfoItemClassDetails(
+					TemplateInfoItemCapability.KEY)) {
+
+			InfoItemFormVariationsProvider<?> infoItemFormVariationsProvider =
+				infoItemServiceTracker.getFirstInfoItemService(
+					InfoItemFormVariationsProvider.class,
+					infoItemClassDetails.getClassName());
+
+			if (infoItemFormVariationsProvider != null) {
+				Collection<InfoItemFormVariation> infoItemFormVariations =
+					infoItemFormVariationsProvider.getInfoItemFormVariations(
+						groupId);
+
+				Stream<InfoItemFormVariation> infoItemFormVariationsStream =
+					infoItemFormVariations.stream();
+
+				InfoItemFormVariation infoItemFormVariation =
+					infoItemFormVariationsStream.findFirst(
+					).orElse(
+						null
+					);
+
+				if (infoItemFormVariation == null) {
+					continue;
+				}
+
+				return infoItemClassDetails;
+			}
+
+			return infoItemClassDetails;
+		}
+
+		return null;
+	}
+
+}
