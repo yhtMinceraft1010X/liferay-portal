1c9c55f53772d8389f20a8bb74dd19838eb750fe
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-10-05 14:36:11 +0200

    LPS-140066 Implements UpdateTemplateEntryMVCActionCommand integration test

---
+++ modules/apps/template/template-web-test/src/testIntegration/java/com/liferay/template/web/internal/portlet/action/test/UpdateTemplateEntryMVCActionCommandTest.java
@@ -0,0 +1,208 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.web.internal.portlet.action.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.template.model.TemplateEntry;
+import com.liferay.template.service.TemplateEntryLocalService;
+
+import java.util.Date;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockMultipartHttpServletRequest;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class UpdateTemplateEntryMVCActionCommandTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_company = _companyLocalService.getCompany(_group.getCompanyId());
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId(), TestPropsValues.getUserId());
+
+		_serviceContext.setCompanyId(TestPropsValues.getCompanyId());
+
+		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
+
+		_templateEntry = TemplateTestUtil.addAnyTemplateEntry(
+			_infoItemServiceTracker, _serviceContext);
+	}
+
+	@Test
+	public void testUpdateTemplateEntry() throws Exception {
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			_getMockLiferayPortletActionRequest();
+
+		String name = RandomTestUtil.randomString();
+
+		String description = RandomTestUtil.randomString();
+
+		String script = "<#-- Modified script content -->";
+
+		String languageId = LocaleUtil.toLanguageId(
+			_portal.getSiteDefaultLocale(_group.getGroupId()));
+
+		mockLiferayPortletActionRequest.addParameter(
+			"name_" + languageId, name);
+		mockLiferayPortletActionRequest.addParameter(
+			"description_" + languageId, description);
+
+		mockLiferayPortletActionRequest.addParameter(
+			"ddmTemplateId", String.valueOf(_templateEntry.getDDMTemplateId()));
+		mockLiferayPortletActionRequest.addParameter("scriptContent", script);
+		mockLiferayPortletActionRequest.addParameter(
+			"templateEntryId",
+			String.valueOf(_templateEntry.getTemplateEntryId()));
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "doProcessAction",
+			new Class<?>[] {ActionRequest.class, ActionResponse.class},
+			mockLiferayPortletActionRequest,
+			new MockLiferayPortletActionResponse());
+
+		TemplateEntry modifiedTemplateEntry =
+			_templateEntryLocalService.getTemplateEntry(
+				_templateEntry.getTemplateEntryId());
+
+		Assert.assertNotNull(modifiedTemplateEntry);
+
+		Date currentModifiedDate = modifiedTemplateEntry.getModifiedDate();
+
+		Assert.assertTrue(
+			currentModifiedDate.after(_templateEntry.getModifiedDate()));
+
+		DDMTemplate ddmTemplate = _ddmTemplateLocalService.getTemplate(
+			_templateEntry.getDDMTemplateId());
+
+		Assert.assertNotNull(ddmTemplate);
+
+		Assert.assertEquals(name, ddmTemplate.getName(languageId));
+		Assert.assertEquals(
+			description, ddmTemplate.getDescription(languageId));
+		Assert.assertEquals(script, ddmTemplate.getScript());
+	}
+
+	private MockLiferayPortletActionRequest
+			_getMockLiferayPortletActionRequest()
+		throws Exception {
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest(
+				new MockMultipartHttpServletRequest());
+
+		mockLiferayPortletActionRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay());
+
+		mockLiferayPortletActionRequest.addParameter(
+			"groupId", String.valueOf(_group.getGroupId()));
+
+		return mockLiferayPortletActionRequest;
+	}
+
+	private ThemeDisplay _getThemeDisplay() throws Exception {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setCompany(_company);
+		themeDisplay.setPermissionChecker(
+			PermissionThreadLocal.getPermissionChecker());
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		return themeDisplay;
+	}
+
+	private Company _company;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@DeleteAfterTestRun
+	private DDMTemplate _ddmTemplate;
+
+	@Inject
+	private DDMTemplateLocalService _ddmTemplateLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private InfoItemServiceTracker _infoItemServiceTracker;
+
+	@Inject(filter = "mvc.command.name=/template/update_template_entry")
+	private MVCActionCommand _mvcActionCommand;
+
+	@Inject
+	private Portal _portal;
+
+	private ServiceContext _serviceContext;
+
+	@DeleteAfterTestRun
+	private TemplateEntry _templateEntry;
+
+	@Inject
+	private TemplateEntryLocalService _templateEntryLocalService;
+
+}
