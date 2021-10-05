aacf36161a14ebc79fbac40be851f2f3ede780b1
Brian Chan <brian.chan@liferay.com>
2021-10-05 09:25:45 -0300

    LPS-112856 Inline in *SearchResponseImpl until it is reused. Please move test logic to *SearchResponseResourceTest.java

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SearchResponseResourceImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SearchResponseResourceImpl.java
@@ -14,14 +14,25 @@
 
 package com.liferay.search.experiences.rest.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.search.document.Field;
+import com.liferay.portal.search.searcher.SearchRequest;
 import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.search.experiences.rest.dto.v1_0.Document;
+import com.liferay.search.experiences.rest.dto.v1_0.DocumentField;
 import com.liferay.search.experiences.rest.dto.v1_0.SXPBlueprint;
 import com.liferay.search.experiences.rest.dto.v1_0.SearchResponse;
 import com.liferay.search.experiences.rest.dto.v1_0.enhancer.SXPBlueprintSearchRequestEnhancer;
-import com.liferay.search.experiences.rest.dto.v1_0.translator.SearchResponseTranslator;
 import com.liferay.search.experiences.rest.dto.v1_0.util.ConfigurationUtil;
 import com.liferay.search.experiences.rest.resource.v1_0.SearchResponseResource;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -42,7 +53,7 @@ public class SearchResponseResourceImpl extends BaseSearchResponseResourceImpl {
 			Integer delta, String q, Integer start, String sxpBlueprintJSON)
 		throws Exception {
 
-		return _searchResponseTranslator.translate(
+		return _toSearchResponse(
 			_searcher.search(
 				_searchRequestBuilderFactory.builder(
 				).emptySearchEnabled(
@@ -76,6 +87,67 @@ public class SearchResponseResourceImpl extends BaseSearchResponseResourceImpl {
 		return start * delta;
 	}
 
+	private Map<String, DocumentField> _toDocumentFields(
+		Map<String, Field> fields) {
+
+		Map<String, DocumentField> documentFields = new LinkedHashMap<>();
+
+		MapUtil.isNotEmptyForEach(
+			fields,
+			(name, field) -> {
+				List<Object> valuesList = field.getValues();
+
+				documentFields.put(
+					name,
+					new DocumentField() {
+						{
+							values = valuesList.toArray();
+						}
+					});
+			});
+
+		return documentFields;
+	}
+
+	private Document[] _toDocuments(
+		Stream<com.liferay.portal.search.document.Document> stream) {
+
+		List<Document> documents = new ArrayList<>();
+
+		stream.forEach(
+			document -> documents.add(
+				new Document() {
+					{
+						documentFields = _toDocumentFields(
+							document.getFields());
+					}
+				}));
+
+		return documents.toArray(new Document[0]);
+	}
+
+	private SearchResponse _toSearchResponse(
+			com.liferay.portal.search.searcher.SearchResponse searchResponse)
+		throws Exception {
+
+		SearchRequest portalSearchRequest = searchResponse.getRequest();
+
+		return new SearchResponse() {
+			{
+				documents = _toDocuments(searchResponse.getDocumentsStream());
+				page = portalSearchRequest.getFrom();
+				pageSize = portalSearchRequest.getSize();
+				request = JSONFactoryUtil.createJSONObject(
+					searchResponse.getRequestString());
+				requestString = searchResponse.getRequestString();
+				response = JSONFactoryUtil.createJSONObject(
+					searchResponse.getResponseString());
+				responseString = searchResponse.getResponseString();
+				totalHits = searchResponse.getTotalHits();
+			}
+		};
+	}
+
 	private SXPBlueprint _toSXPBlueprint(String sxpBlueprintJSON) {
 		SXPBlueprint sxpBlueprint = SXPBlueprint.toDTO(sxpBlueprintJSON);
 
@@ -91,9 +163,6 @@ public class SearchResponseResourceImpl extends BaseSearchResponseResourceImpl {
 
 	@Reference
 	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
-
-	@Reference
-	private SearchResponseTranslator _searchResponseTranslator;
 
 	@Reference
 	private SXPBlueprintSearchRequestEnhancer
