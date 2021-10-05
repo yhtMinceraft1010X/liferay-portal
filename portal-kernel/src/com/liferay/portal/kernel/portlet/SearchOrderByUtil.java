4c3a5c59f60282de12fb6c8329dbff8e114f1256
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2021-10-05 16:45:30 +0200

    LPS-138464 Create pattern to retrieve/save management sort bar choosen order options.

---
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/SearchOrderByUtil.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.portlet;
+
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.portlet.PortletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class SearchOrderByUtil {
+
+	public static String getOrderByCol(
+		HttpServletRequest httpServletRequest, String portletName,
+		String defaultValue) {
+
+		return getOrderByCol(
+			httpServletRequest, portletName, "order-by-col", defaultValue);
+	}
+
+	public static String getOrderByCol(
+		HttpServletRequest httpServletRequest, String portletName, String key,
+		String defaultValue) {
+
+		String orderByCol = ParamUtil.getString(
+			httpServletRequest, "orderByCol");
+
+		PortalPreferences portalPreferences =
+			PortletPreferencesFactoryUtil.getPortalPreferences(
+				httpServletRequest);
+
+		if (Validator.isNull(orderByCol)) {
+			orderByCol = portalPreferences.getValue(
+				portletName, key, defaultValue);
+		}
+
+		portalPreferences.setValue(portletName, key, orderByCol);
+
+		return orderByCol;
+	}
+
+	public static String getOrderByCol(
+		PortletRequest portletRequest, String portletName,
+		String defaultValue) {
+
+		return getOrderByCol(
+			PortalUtil.getHttpServletRequest(portletRequest), portletName,
+			defaultValue);
+	}
+
+	public static String getOrderByCol(
+		PortletRequest portletRequest, String portletName, String key,
+		String defaultValue) {
+
+		return getOrderByCol(
+			PortalUtil.getHttpServletRequest(portletRequest), portletName, key,
+			defaultValue);
+	}
+
+	public static String getOrderByType(
+		HttpServletRequest httpServletRequest, String portletName,
+		String defaultValue) {
+
+		return getOrderByType(
+			httpServletRequest, portletName, "order-by-type", defaultValue);
+	}
+
+	public static String getOrderByType(
+		HttpServletRequest httpServletRequest, String portletName, String key,
+		String defaultValue) {
+
+		String orderByType = ParamUtil.getString(
+			httpServletRequest, "orderByType");
+
+		PortalPreferences portalPreferences =
+			PortletPreferencesFactoryUtil.getPortalPreferences(
+				httpServletRequest);
+
+		if (Validator.isNull(orderByType)) {
+			orderByType = portalPreferences.getValue(
+				portletName, key, defaultValue);
+		}
+
+		portalPreferences.setValue(portletName, key, orderByType);
+
+		return orderByType;
+	}
+
+	public static String getOrderByType(
+		PortletRequest portletRequest, String portletName,
+		String defaultValue) {
+
+		return getOrderByType(
+			PortalUtil.getHttpServletRequest(portletRequest), portletName,
+			defaultValue);
+	}
+
+	public static String getOrderByType(
+		PortletRequest portletRequest, String portletName, String key,
+		String defaultValue) {
+
+		return getOrderByType(
+			PortalUtil.getHttpServletRequest(portletRequest), portletName, key,
+			defaultValue);
+	}
+
+}
