51bb9c8d4bd145513a4bb131c09f2b2803108d0a
Alessio Rendina <asilcomando@gmail.com>
2021-10-08 16:16:08 +0200

    COMMERCE-7414 Renamed commerce order rule entry rel local and remote service implementation

---
+++ modules/apps/commerce/commerce-order-rule-service/src/main/java/com/liferay/commerce/order/rule/service/impl/COREntryRelLocalServiceImpl.java
@@ -0,0 +1,327 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.rule.service.impl;
+
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.model.AccountEntryTable;
+import com.liferay.account.model.AccountGroup;
+import com.liferay.account.model.AccountGroupTable;
+import com.liferay.commerce.model.CommerceOrderType;
+import com.liferay.commerce.model.CommerceOrderTypeTable;
+import com.liferay.commerce.order.rule.model.COREntry;
+import com.liferay.commerce.order.rule.model.COREntryRel;
+import com.liferay.commerce.order.rule.model.COREntryRelTable;
+import com.liferay.commerce.order.rule.model.COREntryTable;
+import com.liferay.commerce.order.rule.service.base.COREntryRelLocalServiceBaseImpl;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.model.CommerceChannelTable;
+import com.liferay.petra.sql.dsl.DSLFunctionFactoryUtil;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
+import com.liferay.petra.sql.dsl.Table;
+import com.liferay.petra.sql.dsl.expression.Expression;
+import com.liferay.petra.sql.dsl.expression.Predicate;
+import com.liferay.petra.sql.dsl.query.FromStep;
+import com.liferay.petra.sql.dsl.query.GroupByStep;
+import com.liferay.petra.sql.dsl.query.JoinStep;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luca Pellizzon
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false,
+	property = "model.class.name=com.liferay.commerce.order.rule.model.COREntryRel",
+	service = AopService.class
+)
+public class COREntryRelLocalServiceImpl
+	extends COREntryRelLocalServiceBaseImpl {
+
+	@Override
+	public COREntryRel addCOREntryRel(
+			long userId, String className, long classPK, long corEntryId)
+		throws PortalException {
+
+		COREntryRel corEntryRel = corEntryRelPersistence.create(
+			counterLocalService.increment());
+
+		User user = userLocalService.getUser(userId);
+
+		corEntryRel.setCompanyId(user.getCompanyId());
+		corEntryRel.setUserId(user.getUserId());
+		corEntryRel.setUserName(user.getFullName());
+
+		corEntryRel.setClassNameId(
+			classNameLocalService.getClassNameId(className));
+		corEntryRel.setClassPK(classPK);
+		corEntryRel.setCOREntryId(corEntryId);
+
+		corEntryRel = corEntryRelPersistence.update(corEntryRel);
+
+		reindexCOREntry(corEntryId);
+
+		return corEntryRel;
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public COREntryRel deleteCOREntryRel(COREntryRel corEntryRel)
+		throws PortalException {
+
+		corEntryRelPersistence.remove(corEntryRel);
+
+		reindexCOREntry(corEntryRel.getCOREntryId());
+
+		return corEntryRel;
+	}
+
+	@Override
+	public COREntryRel deleteCOREntryRel(long corEntryRelId)
+		throws PortalException {
+
+		COREntryRel corEntryRel = corEntryRelPersistence.findByPrimaryKey(
+			corEntryRelId);
+
+		return corEntryRelLocalService.deleteCOREntryRel(corEntryRel);
+	}
+
+	@Override
+	public void deleteCOREntryRels(long corEntryId) throws PortalException {
+		List<COREntryRel> corEntryRels =
+			corEntryRelPersistence.findByCOREntryId(corEntryId);
+
+		for (COREntryRel corEntryRel : corEntryRels) {
+			corEntryRelLocalService.deleteCOREntryRel(corEntryRel);
+		}
+	}
+
+	@Override
+	public COREntryRel fetchCOREntryRel(
+		String className, long classPK, long corEntryId) {
+
+		return corEntryRelPersistence.fetchByC_C_C(
+			classNameLocalService.getClassNameId(className), classPK,
+			corEntryId);
+	}
+
+	@Override
+	public List<COREntryRel> getAccountEntryCOREntryRels(
+		long corEntryId, String keywords, int start, int end) {
+
+		return dslQuery(
+			_getGroupByStep(
+				DSLQueryFactoryUtil.selectDistinct(COREntryRelTable.INSTANCE),
+				AccountEntryTable.INSTANCE,
+				AccountEntryTable.INSTANCE.accountEntryId.eq(
+					COREntryRelTable.INSTANCE.classPK),
+				corEntryId, AccountEntry.class.getName(), keywords,
+				AccountEntryTable.INSTANCE.name
+			).limit(
+				start, end
+			));
+	}
+
+	@Override
+	public int getAccountEntryCOREntryRelsCount(
+		long corEntryId, String keywords) {
+
+		return dslQueryCount(
+			_getGroupByStep(
+				DSLQueryFactoryUtil.countDistinct(
+					COREntryRelTable.INSTANCE.COREntryId),
+				AccountEntryTable.INSTANCE,
+				AccountEntryTable.INSTANCE.accountEntryId.eq(
+					COREntryRelTable.INSTANCE.classPK),
+				corEntryId, AccountEntry.class.getName(), keywords,
+				AccountEntryTable.INSTANCE.name));
+	}
+
+	@Override
+	public List<COREntryRel> getAccountGroupCOREntryRels(
+		long corEntryId, String keywords, int start, int end) {
+
+		return dslQuery(
+			_getGroupByStep(
+				DSLQueryFactoryUtil.selectDistinct(COREntryRelTable.INSTANCE),
+				AccountGroupTable.INSTANCE,
+				AccountGroupTable.INSTANCE.accountGroupId.eq(
+					COREntryRelTable.INSTANCE.classPK),
+				corEntryId, AccountGroup.class.getName(), keywords,
+				AccountGroupTable.INSTANCE.name
+			).limit(
+				start, end
+			));
+	}
+
+	@Override
+	public int getAccountGroupCOREntryRelsCount(
+		long corEntryId, String keywords) {
+
+		return dslQueryCount(
+			_getGroupByStep(
+				DSLQueryFactoryUtil.countDistinct(
+					COREntryRelTable.INSTANCE.COREntryId),
+				AccountGroupTable.INSTANCE,
+				AccountGroupTable.INSTANCE.accountGroupId.eq(
+					COREntryRelTable.INSTANCE.classPK),
+				corEntryId, AccountGroup.class.getName(), keywords,
+				AccountGroupTable.INSTANCE.name));
+	}
+
+	@Override
+	public List<COREntryRel> getCommerceChannelCOREntryRels(
+		long corEntryId, String keywords, int start, int end) {
+
+		return dslQuery(
+			_getGroupByStep(
+				DSLQueryFactoryUtil.selectDistinct(COREntryRelTable.INSTANCE),
+				CommerceChannelTable.INSTANCE,
+				CommerceChannelTable.INSTANCE.commerceChannelId.eq(
+					COREntryRelTable.INSTANCE.classPK),
+				corEntryId, CommerceChannel.class.getName(), keywords,
+				CommerceChannelTable.INSTANCE.name
+			).limit(
+				start, end
+			));
+	}
+
+	@Override
+	public int getCommerceChannelCOREntryRelsCount(
+		long corEntryId, String keywords) {
+
+		return dslQueryCount(
+			_getGroupByStep(
+				DSLQueryFactoryUtil.countDistinct(
+					COREntryRelTable.INSTANCE.COREntryId),
+				CommerceChannelTable.INSTANCE,
+				CommerceChannelTable.INSTANCE.commerceChannelId.eq(
+					COREntryRelTable.INSTANCE.classPK),
+				corEntryId, CommerceChannel.class.getName(), keywords,
+				CommerceChannelTable.INSTANCE.name));
+	}
+
+	@Override
+	public List<COREntryRel> getCommerceOrderTypeCOREntryRels(
+		long corEntryId, String keywords, int start, int end) {
+
+		return dslQuery(
+			_getGroupByStep(
+				DSLQueryFactoryUtil.selectDistinct(COREntryRelTable.INSTANCE),
+				CommerceOrderTypeTable.INSTANCE,
+				CommerceOrderTypeTable.INSTANCE.commerceOrderTypeId.eq(
+					COREntryRelTable.INSTANCE.classPK),
+				corEntryId, CommerceOrderType.class.getName(), keywords,
+				CommerceOrderTypeTable.INSTANCE.name
+			).limit(
+				start, end
+			));
+	}
+
+	@Override
+	public int getCommerceOrderTypeCOREntryRelsCount(
+		long corEntryId, String keywords) {
+
+		return dslQueryCount(
+			_getGroupByStep(
+				DSLQueryFactoryUtil.countDistinct(
+					COREntryRelTable.INSTANCE.COREntryId),
+				CommerceOrderTypeTable.INSTANCE,
+				CommerceOrderTypeTable.INSTANCE.commerceOrderTypeId.eq(
+					COREntryRelTable.INSTANCE.classPK),
+				corEntryId, CommerceOrderType.class.getName(), keywords,
+				CommerceOrderTypeTable.INSTANCE.name));
+	}
+
+	@Override
+	public List<COREntryRel> getCOREntryRels(long corEntryId) {
+		return corEntryRelPersistence.findByCOREntryId(corEntryId);
+	}
+
+	@Override
+	public List<COREntryRel> getCOREntryRels(
+		long corEntryId, int start, int end,
+		OrderByComparator<COREntryRel> orderByComparator) {
+
+		return corEntryRelPersistence.findByCOREntryId(
+			corEntryId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCOREntryRelsCount(long corEntryId) {
+		return corEntryRelPersistence.countByCOREntryId(corEntryId);
+	}
+
+	protected void reindexCOREntry(long corEntryId) throws PortalException {
+		Indexer<COREntry> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+			COREntry.class);
+
+		indexer.reindex(COREntry.class.getName(), corEntryId);
+	}
+
+	private GroupByStep _getGroupByStep(
+		FromStep fromStep, Table innerJoinTable, Predicate innerJoinPredicate,
+		Long corEntryId, String className, String keywords,
+		Expression<String> keywordsPredicateExpression) {
+
+		JoinStep joinStep = fromStep.from(
+			COREntryRelTable.INSTANCE
+		).innerJoinON(
+			COREntryTable.INSTANCE,
+			COREntryTable.INSTANCE.COREntryId.eq(
+				COREntryRelTable.INSTANCE.COREntryId)
+		).innerJoinON(
+			innerJoinTable, innerJoinPredicate
+		);
+
+		return joinStep.where(
+			() -> {
+				Predicate predicate = COREntryRelTable.INSTANCE.COREntryId.eq(
+					corEntryId
+				).and(
+					COREntryRelTable.INSTANCE.classNameId.eq(
+						classNameLocalService.getClassNameId(className))
+				);
+
+				if (Validator.isNotNull(keywords)) {
+					predicate = predicate.and(
+						Predicate.withParentheses(
+							_customSQL.getKeywordsPredicate(
+								DSLFunctionFactoryUtil.lower(
+									keywordsPredicateExpression),
+								_customSQL.keywords(keywords, true))));
+				}
+
+				return predicate;
+			});
+	}
+
+	@Reference
+	private CustomSQL _customSQL;
+
+}
---
+++ modules/apps/commerce/commerce-order-rule-service/src/main/java/com/liferay/commerce/order/rule/service/impl/COREntryRelServiceImpl.java
@@ -0,0 +1,238 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.rule.service.impl;
+
+import com.liferay.commerce.order.rule.model.COREntry;
+import com.liferay.commerce.order.rule.model.COREntryRel;
+import com.liferay.commerce.order.rule.service.base.COREntryRelServiceBaseImpl;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luca Pellizzon
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false,
+	property = {
+		"json.web.service.context.name=commerce",
+		"json.web.service.context.path=COREntryRel"
+	},
+	service = AopService.class
+)
+public class COREntryRelServiceImpl extends COREntryRelServiceBaseImpl {
+
+	@Override
+	public COREntryRel addCOREntryRel(
+			String className, long classPK, long corEntryId)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.UPDATE);
+
+		return corEntryRelLocalService.addCOREntryRel(
+			getUserId(), className, classPK, corEntryId);
+	}
+
+	@Override
+	public void deleteCOREntryRel(long corEntryRelId) throws PortalException {
+		COREntryRel corEntryRel = corEntryRelLocalService.getCOREntryRel(
+			corEntryRelId);
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryRel.getCOREntryId(),
+			ActionKeys.UPDATE);
+
+		corEntryRelLocalService.deleteCOREntryRel(corEntryRel);
+	}
+
+	@Override
+	public void deleteCOREntryRelsByCOREntryId(long corEntryId)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.UPDATE);
+
+		corEntryRelLocalService.deleteCOREntryRels(corEntryId);
+	}
+
+	@Override
+	public COREntryRel fetchCOREntryRel(
+			String className, long classPK, long corEntryId)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.fetchCOREntryRel(
+			className, classPK, corEntryId);
+	}
+
+	@Override
+	public List<COREntryRel> getAccountEntryCOREntryRels(
+			long corEntryId, String keywords, int start, int end)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getAccountEntryCOREntryRels(
+			corEntryId, keywords, start, end);
+	}
+
+	@Override
+	public int getAccountEntryCOREntryRelsCount(
+			long corEntryId, String keywords)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getAccountEntryCOREntryRelsCount(
+			corEntryId, keywords);
+	}
+
+	@Override
+	public List<COREntryRel> getAccountGroupCOREntryRels(
+			long corEntryId, String keywords, int start, int end)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getAccountGroupCOREntryRels(
+			corEntryId, keywords, start, end);
+	}
+
+	@Override
+	public int getAccountGroupCOREntryRelsCount(
+			long corEntryId, String keywords)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getAccountGroupCOREntryRelsCount(
+			corEntryId, keywords);
+	}
+
+	@Override
+	public List<COREntryRel> getCommerceChannelCOREntryRels(
+			long corEntryId, String keywords, int start, int end)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getCommerceChannelCOREntryRels(
+			corEntryId, keywords, start, end);
+	}
+
+	@Override
+	public int getCommerceChannelCOREntryRelsCount(
+			long corEntryId, String keywords)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getCommerceChannelCOREntryRelsCount(
+			corEntryId, keywords);
+	}
+
+	@Override
+	public List<COREntryRel> getCommerceOrderTypeCOREntryRels(
+			long corEntryId, String keywords, int start, int end)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getCommerceOrderTypeCOREntryRels(
+			corEntryId, keywords, start, end);
+	}
+
+	@Override
+	public int getCommerceOrderTypeCOREntryRelsCount(
+			long corEntryId, String keywords)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getCommerceOrderTypeCOREntryRelsCount(
+			corEntryId, keywords);
+	}
+
+	@Override
+	public COREntryRel getCOREntryRel(long corEntryRelId)
+		throws PortalException {
+
+		COREntryRel corEntryRel = corEntryRelLocalService.getCOREntryRel(
+			corEntryRelId);
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryRel.getCOREntryId(),
+			ActionKeys.VIEW);
+
+		return corEntryRel;
+	}
+
+	@Override
+	public List<COREntryRel> getCOREntryRels(long corEntryId)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getCOREntryRels(corEntryId);
+	}
+
+	@Override
+	public List<COREntryRel> getCOREntryRels(
+			long corEntryId, int start, int end,
+			OrderByComparator<COREntryRel> orderByComparator)
+		throws PortalException {
+
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getCOREntryRels(
+			corEntryId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCOREntryRelsCount(long corEntryId) throws PortalException {
+		_corEntryModelResourcePermission.check(
+			getPermissionChecker(), corEntryId, ActionKeys.VIEW);
+
+		return corEntryRelLocalService.getCOREntryRelsCount(corEntryId);
+	}
+
+	private static volatile ModelResourcePermission<COREntry>
+		_corEntryModelResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				COREntryRelServiceImpl.class,
+				"_corEntryModelResourcePermission", COREntry.class);
+
+}
