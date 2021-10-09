cf6a1bfd906186d1b8688920d2b42c853a881f99
Peter Richards <peter.richards@thefuturetoday.net>
2021-10-09 14:19:41 +0100

    LPS-141854 Dropdown fixes for content page editor

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/EditModeSelector.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/EditModeSelector.js
@@ -39,6 +39,7 @@ export default function EditModeSelector() {
 			active={active}
 			alignmentPosition={Align.BottomLeft}
 			menuElementAttrs={{
+				className: 'page-editor__edit-mode-dropdown-menu',
 				containerProps: {
 					className: 'cadmin',
 				},
