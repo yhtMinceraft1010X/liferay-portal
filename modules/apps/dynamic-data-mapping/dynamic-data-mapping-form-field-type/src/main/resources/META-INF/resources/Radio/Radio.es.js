292e939923fc4a002d7f02246a8090afd885fd81
Daniel Bonasser <daniel.bonasser@liferay.com>
2021-10-11 20:41:18 -0300

    LPS-139807 Add css styles for Radio

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Radio/Radio.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Radio/Radio.es.js
@@ -19,6 +19,8 @@ import {FieldBase} from '../FieldBase/ReactFieldBase.es';
 import {useSyncValue} from '../hooks/useSyncValue.es';
 import {setJSONArrayValue} from '../util/setters.es';
 
+import './Radio.scss';
+
 const Radio = ({
 	editingLanguageId,
 	inline,
@@ -60,7 +62,7 @@ const Radio = ({
 
 	return (
 		<FieldBase {...otherProps} name={name} readOnly={disabled}>
-			<div className="ddm-radio" onBlur={onBlur} onFocus={onFocus}>
+			<div className="ddm__radio" onBlur={onBlur} onFocus={onFocus}>
 				{options.map((option, index) => (
 					<ClayRadio
 						checked={currentValue === option.value}
