c06fddeadb398c142cc2a5cca79eeaf2caf3981e
Kevin Tan <kevin.tan@liferay.com>
2021-10-12 12:35:43 -0700

    LPS-112856 search-experiences-web: Add view MVC render commands

---
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/portlet/action/ViewSXPBlueprintsMVCRenderCommand.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.web.internal.blueprint.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.search.experiences.constants.SXPPortletKeys;
+import com.liferay.search.experiences.web.internal.blueprint.constants.SXPBlueprintMVCCommandNames;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Petteri Karttunen
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + SXPPortletKeys.SXP_BLUEPRINT,
+		"mvc.command.name=" + SXPBlueprintMVCCommandNames.VIEW_SXP_BLUEPRINTS,
+		"mvc.command.name=/"
+	},
+	service = MVCRenderCommand.class
+)
+public class ViewSXPBlueprintsMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		return "/view.jsp";
+	}
+
+}
