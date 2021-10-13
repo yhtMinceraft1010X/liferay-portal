8e06a9a3b966f092f552cfbb18bc40a442122983
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-10-13 15:02:20 +0200

    LPS-140364 Adds integration tests for TemplateEntryStagedModelDataHandler

---
+++ modules/apps/template/template-test/src/testIntegration/java/com/liferay/template/lar/test/TemplateEntryStagedModelDataHandlerTest.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.template.lar.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.constants.DDMTemplateConstants;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.exportimport.test.util.lar.BaseStagedModelDataHandlerTestCase;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.template.model.TemplateEntry;
+import com.liferay.template.service.TemplateEntryLocalService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+public class TemplateEntryStagedModelDataHandlerTest
+	extends BaseStagedModelDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Override
+	protected StagedModel addStagedModel(
+			Group group,
+			Map<String, List<StagedModel>> dependentStagedModelsMap)
+		throws Exception {
+
+		DDMTemplate ddmTemplate = _ddmTemplateLocalService.addTemplate(
+			TestPropsValues.getUserId(), group.getGroupId(),
+			_portal.getClassNameId(TemplateEntry.class), 0,
+			_portal.getClassNameId(TemplateEntry.class),
+			Collections.singletonMap(LocaleUtil.US, "name"),
+			Collections.emptyMap(), DDMTemplateConstants.TEMPLATE_TYPE_DISPLAY,
+			StringPool.BLANK, TemplateConstants.LANG_TYPE_FTL,
+			"<#-- Empty script -->", new ServiceContext());
+
+		return _templateEntryLocalService.addTemplateEntry(
+			TestPropsValues.getUserId(), group.getGroupId(),
+			ddmTemplate.getTemplateId(), StringPool.BLANK, StringPool.BLANK,
+			new ServiceContext());
+	}
+
+	@Override
+	protected StagedModel getStagedModel(String uuid, Group group)
+		throws PortalException {
+
+		return _templateEntryLocalService.getTemplateEntryByUuidAndGroupId(
+			uuid, group.getGroupId());
+	}
+
+	@Override
+	protected Class<? extends StagedModel> getStagedModelClass() {
+		return TemplateEntry.class;
+	}
+
+	@Inject
+	private DDMTemplateLocalService _ddmTemplateLocalService;
+
+	@Inject
+	private Portal _portal;
+
+	@Inject
+	private TemplateEntryLocalService _templateEntryLocalService;
+
+}
