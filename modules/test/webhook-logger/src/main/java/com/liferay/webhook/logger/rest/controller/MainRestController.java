37df7e8c848b16a0e262e02bd356c6cff315560d
Inácio Nery <inacio.nery@liferay.com>
2021-10-14 12:03:53 -0300

    LPS-140778 Add MainRestController

---
+++ modules/test/webhook-logger/src/main/java/com/liferay/webhook/logger/rest/controller/MainRestController.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.webhook.logger.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Inácio Nery
+ */
+@RequestMapping("/")
+@RestController
+public class MainRestController {
+
+	@GetMapping("{value}")
+	public String getValue(@PathVariable(required = false) String value) {
+		return value;
+	}
+
+}
