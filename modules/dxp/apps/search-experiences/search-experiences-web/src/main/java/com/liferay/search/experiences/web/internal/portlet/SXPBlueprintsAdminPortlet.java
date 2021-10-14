effedb9c73ccddb7c3595056f2dd16dcc73dd764
Kevin Tan <kevin.tan@liferay.com>
2021-10-14 16:08:03 -0700

    LPS-112856 search-experiences-web: Rename SXPBlueprintPortlet to SXPBlueprintsAdminPortlet
    Follows naming similar to other admin portlets for example:
    - AccountUsersAdminPortlet
    - AssetTagsAdminPortlet
    - AnnouncementsAdminPortlet
    - BookmarksAdminPortlet
    - BlogsAdminPortlet
    - RolesAdminPortlet
    - KaleoFormsAdminPortlet

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/portlet/SXPBlueprintsAdminPortlet.java
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/portlet/SXPBlueprintsAdminPortlet.java
@@ -41,5 +41,5 @@ import org.osgi.service.component.annotations.Component;
 	},
 	service = Portlet.class
 )
-public class SXPBlueprintPortlet extends MVCPortlet {
+public class SXPBlueprintsAdminPortlet extends MVCPortlet {
 }
