297175739152446ac9f76ba3e377af0668f0744d
Alessio Rendina <asilcomando@gmail.com>
2021-10-14 18:30:23 +0200

    COMMERCE-7414 Added new dataset constants

---
+++ modules/apps/commerce/commerce-order-rule-web/src/main/java/com/liferay/commerce/order/rule/web/internal/frontend/constants/COREntryDataSetConstants.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.rule.web.internal.frontend.constants;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class COREntryDataSetConstants {
+
+	public static final String COMMERCE_DATA_SET_KEY_ORDER_RULE_ENTRIES =
+		"corEntries";
+
+	public static final String
+		COMMERCE_DATA_SET_KEY_ORDER_RULE_ENTRY_QUALIFIER_ACCOUNT_ENTRIES =
+			"corEntryQualifierAccountEntries";
+
+	public static final String
+		COMMERCE_DATA_SET_KEY_ORDER_RULE_ENTRY_QUALIFIER_ACCOUNT_GROUPS =
+			"corEntryQualifierAccountGroups";
+
+	public static final String
+		COMMERCE_DATA_SET_KEY_ORDER_RULE_ENTRY_QUALIFIER_COMMERCE_CHANNELS =
+			"corEntryQualifierCommerceChannels";
+
+	public static final String
+		COMMERCE_DATA_SET_KEY_ORDER_RULE_ENTRY_QUALIFIER_COMMERCE_ORDER_TYPES =
+			"corEntryQualifierCommerceOrderTypes";
+
+}
