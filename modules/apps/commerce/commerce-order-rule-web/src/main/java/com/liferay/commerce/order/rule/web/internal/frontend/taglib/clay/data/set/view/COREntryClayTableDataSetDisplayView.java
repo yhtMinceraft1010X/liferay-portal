7213aec5b15fdd0fb6d6303a77b1f9c089ec9f86
Alessio Rendina <asilcomando@gmail.com>
2021-10-14 18:30:56 +0200

    COMMERCE-7414 Added commerce order rule entry clay table

---
+++ modules/apps/commerce/commerce-order-rule-web/src/main/java/com/liferay/commerce/order/rule/web/internal/frontend/taglib/clay/data/set/view/COREntryClayTableDataSetDisplayView.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.rule.web.internal.frontend.taglib.clay.data.set.view;
+
+import com.liferay.commerce.order.rule.web.internal.frontend.constants.COREntryDataSetConstants;
+import com.liferay.frontend.taglib.clay.data.set.ClayDataSetDisplayView;
+import com.liferay.frontend.taglib.clay.data.set.view.table.BaseTableClayDataSetDisplayView;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchema;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaBuilder;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaBuilderFactory;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaField;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "clay.data.set.display.name=" + COREntryDataSetConstants.COMMERCE_DATA_SET_KEY_ORDER_RULE_ENTRIES,
+	service = ClayDataSetDisplayView.class
+)
+public class COREntryClayTableDataSetDisplayView
+	extends BaseTableClayDataSetDisplayView {
+
+	@Override
+	public ClayTableSchema getClayTableSchema() {
+		ClayTableSchemaBuilder clayTableSchemaBuilder =
+			_clayTableSchemaBuilderFactory.create();
+
+		ClayTableSchemaField nameClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField("name", "name");
+
+		nameClayTableSchemaField.setContentRenderer("actionLink");
+
+		ClayTableSchemaField activeClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField("active", "active");
+
+		activeClayTableSchemaField.setContentRenderer("boolean");
+
+		ClayTableSchemaField startDateClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField(
+				"displayDate", "start-date");
+
+		startDateClayTableSchemaField.setContentRenderer("date");
+		startDateClayTableSchemaField.setSortable(true);
+
+		ClayTableSchemaField endDateClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField(
+				"expirationDate", "end-date");
+
+		endDateClayTableSchemaField.setContentRenderer("date");
+		endDateClayTableSchemaField.setSortable(true);
+
+		ClayTableSchemaField statusClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField(
+				"workflowStatusInfo", "status");
+
+		statusClayTableSchemaField.setContentRenderer("status");
+
+		return clayTableSchemaBuilder.build();
+	}
+
+	@Reference
+	private ClayTableSchemaBuilderFactory _clayTableSchemaBuilderFactory;
+
+}
