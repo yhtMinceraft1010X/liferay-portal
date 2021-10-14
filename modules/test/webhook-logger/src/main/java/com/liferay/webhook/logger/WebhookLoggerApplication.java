912ef4c0098d88547b82c40d7d4e2963960b3786
Inácio Nery <inacio.nery@liferay.com>
2021-10-14 11:58:59 -0300

    LPS-140778 Init module

---
+++ modules/test/webhook-logger/src/main/java/com/liferay/webhook/logger/WebhookLoggerApplication.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.webhook.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Inácio Nery
+ */
+@SpringBootApplication
+public class WebhookLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookLoggerApplication.class, args);
+	}
+
+}
