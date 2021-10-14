471b02cd737fc3a0aecad363488f798bc989afc3
Alessio Rendina <asilcomando@gmail.com>
2021-10-14 18:34:48 +0200

    COMMERCE-7414 Added details screen navigation category

---
+++ modules/apps/commerce/commerce-order-rule-web/src/main/java/com/liferay/commerce/order/rule/web/internal/frontend/taglib/servlet/taglib/COREntryDetailsScreenNavigationCategory.java
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.rule.web.internal.frontend.taglib.servlet.taglib;
+
+import com.liferay.commerce.currency.service.CommerceCurrencyService;
+import com.liferay.commerce.order.rule.entry.type.COREntryTypeJSPContributorRegistry;
+import com.liferay.commerce.order.rule.entry.type.COREntryTypeRegistry;
+import com.liferay.commerce.order.rule.model.COREntry;
+import com.liferay.commerce.order.rule.service.COREntryService;
+import com.liferay.commerce.order.rule.web.internal.display.context.COREntryDisplayContext;
+import com.liferay.commerce.order.rule.web.internal.frontend.taglib.servlet.taglib.constants.COREntryScreenNavigationConstants;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false,
+	property = {
+		"screen.navigation.category.order:Integer=10",
+		"screen.navigation.entry.order:Integer=10"
+	},
+	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+)
+public class COREntryDetailsScreenNavigationCategory
+	implements ScreenNavigationCategory, ScreenNavigationEntry<COREntry> {
+
+	@Override
+	public String getCategoryKey() {
+		return COREntryScreenNavigationConstants.CATEGORY_KEY_DETAILS;
+	}
+
+	@Override
+	public String getEntryKey() {
+		return COREntryScreenNavigationConstants.CATEGORY_KEY_DETAILS;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(
+			resourceBundle,
+			COREntryScreenNavigationConstants.CATEGORY_KEY_DETAILS);
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return COREntryScreenNavigationConstants.
+			SCREEN_NAVIGATION_KEY_COR_ENTRY_GENERAL;
+	}
+
+	@Override
+	public boolean isVisible(User user, COREntry corEntry) {
+		if (corEntry == null) {
+			return false;
+		}
+
+		boolean hasPermission = false;
+
+		try {
+			hasPermission = _corEntryModelResourcePermission.contains(
+				PermissionThreadLocal.getPermissionChecker(),
+				corEntry.getCOREntryId(), ActionKeys.UPDATE);
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception, exception);
+			}
+		}
+
+		return hasPermission;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		COREntryDisplayContext corEntryDisplayContext =
+			new COREntryDisplayContext(
+				_commerceCurrencyService, _corEntryModelResourcePermission,
+				_corEntryService, _corEntryTypeJSPContributorRegistry,
+				_corEntryTypeRegistry, httpServletRequest, _portal);
+
+		httpServletRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT, corEntryDisplayContext);
+
+		_jspRenderer.renderJSP(
+			httpServletRequest, httpServletResponse, "/cor_entry/details.jsp");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		COREntryDetailsScreenNavigationCategory.class);
+
+	@Reference
+	private CommerceCurrencyService _commerceCurrencyService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.commerce.order.rule.model.COREntry)"
+	)
+	private ModelResourcePermission<COREntry> _corEntryModelResourcePermission;
+
+	@Reference
+	private COREntryService _corEntryService;
+
+	@Reference
+	private COREntryTypeJSPContributorRegistry
+		_corEntryTypeJSPContributorRegistry;
+
+	@Reference
+	private COREntryTypeRegistry _corEntryTypeRegistry;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference
+	private Portal _portal;
+
+}
---
+++ modules/apps/commerce/commerce-order-rule-web/src/main/resources/META-INF/resources/cor_entry/details.jsp
@@ -0,0 +1,123 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+COREntryDisplayContext corEntryDisplayContext = (COREntryDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+
+COREntry corEntry = corEntryDisplayContext.getCOREntry();
+long corEntryId = corEntryDisplayContext.getCOREntryId();
+
+boolean neverExpire = ParamUtil.getBoolean(request, "neverExpire", true);
+
+if ((corEntry != null) && (corEntry.getExpirationDate() != null)) {
+	neverExpire = false;
+}
+
+String type = BeanParamUtil.getString(corEntry, renderRequest, "type", COREntryConstants.TYPE_MINIMUM_ORDER_AMOUNT);
+%>
+
+<portlet:actionURL name="/cor_entry/edit_cor_entry" var="editCOREntryActionURL" />
+
+<aui:form action="<%= editCOREntryActionURL %>" cssClass="pt-4" method="post" name="fm">
+	<aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= (corEntry == null) ? Constants.ADD : Constants.UPDATE %>" />
+	<aui:input name="redirect" type="hidden" value="<%= currentURL %>" />
+	<aui:input name="externalReferenceCode" type="hidden" value="<%= corEntry.getExternalReferenceCode() %>" />
+	<aui:input name="corEntryId" type="hidden" value="<%= corEntryId %>" />
+	<aui:input name="workflowAction" type="hidden" value="<%= String.valueOf(WorkflowConstants.ACTION_SAVE_DRAFT) %>" />
+
+	<aui:model-context bean="<%= corEntry %>" model="<%= COREntry.class %>" />
+
+	<div class="row">
+		<div class="col-12 col-xl-8">
+			<commerce-ui:panel
+				bodyClasses="flex-fill"
+				collapsed="<%= false %>"
+				collapsible="<%= false %>"
+				title='<%= LanguageUtil.get(request, "details") %>'
+			>
+				<div class="row">
+					<div class="col">
+						<aui:input autoFocus="<%= true %>" label="name" name="name" required="<%= true %>" />
+					</div>
+
+					<div class="col-auto">
+						<aui:select label="type" name="type" required="<%= true %>">
+
+							<%
+							for (COREntryType corEntryType : corEntryDisplayContext.getCOREntryTypes()) {
+							%>
+
+								<aui:option label="<%= corEntryType.getLabel(locale) %>" value="<%= corEntryType.getKey() %>" />
+
+							<%
+							}
+							%>
+
+						</aui:select>
+					</div>
+
+					<div class="col-auto">
+						<aui:input label='<%= HtmlUtil.escape("active") %>' name="active" type="toggle-switch" value="<%= corEntry.isActive() %>" />
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col">
+						<aui:input name="description" type="textarea" value="<%= corEntry.getDescription() %>" />
+					</div>
+				</div>
+			</commerce-ui:panel>
+
+			<%
+			COREntryTypeJSPContributor corEntryTypeJSPContributor = corEntryDisplayContext.getCOREntryTypeJSPContributor(type);
+			%>
+
+			<c:if test="<%= corEntryTypeJSPContributor != null %>">
+
+				<%
+				corEntryTypeJSPContributor.render(corEntryId, request, PipingServletResponseFactory.createPipingServletResponse(pageContext));
+				%>
+
+			</c:if>
+		</div>
+
+		<div class="col-12 col-xl-4">
+			<commerce-ui:panel
+				bodyClasses="flex-fill"
+				title='<%= LanguageUtil.get(request, "schedule") %>'
+			>
+				<liferay-ui:error exception="<%= COREntryExpirationDateException.class %>" message="please-select-a-valid-expiration-date" />
+
+				<aui:input formName="fm" label="publish-date" name="displayDate" />
+
+				<aui:input dateTogglerCheckboxLabel="never-expire" disabled="<%= neverExpire %>" formName="fm" name="expirationDate" />
+			</commerce-ui:panel>
+		</div>
+	</div>
+</aui:form>
+
+<liferay-frontend:component
+	context='<%=
+		HashMapBuilder.<String, Object>put(
+			"corEntryId", corEntryId
+		).put(
+			"currentURL", currentURL
+		).build()
+	%>'
+	module="js/details"
+/>
