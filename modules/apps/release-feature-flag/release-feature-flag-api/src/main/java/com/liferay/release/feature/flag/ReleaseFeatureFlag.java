57b8136dfa80fef6feb3a07b7f43a2c0035f125f
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-10-18 16:51:43 +0200

    LPS-140911 Adding javadoc to show how to use RFFs

--- modules/apps/release-feature-flag/release-feature-flag-api/src/main/java/com/liferay/release/feature/flag/ReleaseFeatureFlag.java
+++ modules/apps/release-feature-flag/release-feature-flag-api/src/main/java/com/liferay/release/feature/flag/ReleaseFeatureFlag.java
@@ -16,6 +16,34 @@ package com.liferay.release.feature.flag;
 
 /**
  * @author Alejandro Tardín
+ *
+ * Release feature flags are meant to reduce the impact of new changes to
+ * existing DXP customers. During the upgrade process, all the new release
+ * feature flags will be automatically disabled. Administrators will then be
+ * able to manually enable those they consider useful.
+ *
+ * This enum will hold all the release feature flags. To add one follow these
+ * steps:
+ *
+ * 1. Add a constant here.
+ *
+ * 2. Add an upgrade step to {@link com.liferay.release.feature.flag.web.internal.upgrade.ReleaseFeatureFlagUpgrade }:
+ * <code>
+ *     registry.register(
+ *     	"0.0.0", "1.0.0",
+ *     	new UpgradeReleaseFeatureFlag(ReleaseFeatureFlag.FEATURE_FLAG_NAME))
+ * </code>
+ * This will ensure the feature flag is disabled for existing DXP customers.
+ *
+ * 3. Use your feature flag in your code like this:
+ * <code>
+ * 	if (ReleaseFeatureFlagManagerUtil.isEnabled(ReleaseFeatureFlag.FEATURE_FLAG_NAME)) {
+ *		// Code for the new behaviour
+ *	}
+ *	else {
+ *		// Code for the old behaviour
+ *	}
+ * 	</code>
  */
 public enum ReleaseFeatureFlag {
 }
