9b8979cd33ef38c4cd3280402bff8000dacb02cc
Kevin Tan <kevin.tan@liferay.com>
2021-10-20 11:09:05 -0700

    LPS-140994 search-experiences-web: Add SXPBlueprintPermission

---
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/security/permission/resource/SXPBlueprintPermission.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.web.internal.security.permission.resource;
+
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.search.experiences.constants.SXPConstants;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Petteri Karttunen
+ */
+@Component(immediate = true, service = {})
+public class SXPBlueprintPermission {
+
+	public static boolean contains(
+		PermissionChecker permissionChecker, long groupId, String actionKey) {
+
+		return _portletResourcePermission.contains(
+			permissionChecker, groupId, actionKey);
+	}
+
+	@Reference(
+		target = "(resource.name=" + SXPConstants.RESOURCE_NAME + ")",
+		unbind = "-"
+	)
+	protected void setPortletResourcePermission(
+		PortletResourcePermission portletResourcePermission) {
+
+		_portletResourcePermission = portletResourcePermission;
+	}
+
+	private static PortletResourcePermission _portletResourcePermission;
+
+}
