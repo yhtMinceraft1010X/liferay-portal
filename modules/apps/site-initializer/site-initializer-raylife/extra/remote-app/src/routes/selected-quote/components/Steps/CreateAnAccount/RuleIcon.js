c7dc9d37ddf701713f6f5dd07f8b268b63832146
Keven Leone <keven.leone@liferay.com>
2021-10-21 15:17:30 -0300

    LPS-141248 Replace fixed colors by variables

--- modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/routes/selected-quote/components/Steps/CreateAnAccount/RuleIcon.js
+++ modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/routes/selected-quote/components/Steps/CreateAnAccount/RuleIcon.js
@@ -1,26 +1,21 @@
 import ClayIcon from '@clayui/icon';
+import classNames from 'classnames';
 
 import {CHECK_VALUE, NATURAL_VALUE, UNCHECKED_VALUE} from './constants';
 
 const COLORS = {
-	[CHECK_VALUE]: {color: '#39C38D', symbol: 'check'},
-	[NATURAL_VALUE]: {fill: '#606167', symbol: 'check'},
-	[UNCHECKED_VALUE]: {color: '#EA6136', symbol: 'hr'},
+	[CHECK_VALUE]: {className: 'checked', symbol: 'check'},
+	[NATURAL_VALUE]: {className: 'neutral', symbol: 'check'},
+	[UNCHECKED_VALUE]: {className: 'unchecked', symbol: 'hr'},
 };
 
 export const RuleIcon = ({label, status}) => {
 	const ruleConfig = COLORS[status] || {};
 
-	const color = ruleConfig.fill ? ruleConfig.fill : ruleConfig.color;
-
 	return (
-		<a style={{color}}>
+		<a className={classNames('ca-rule-anchor', ruleConfig.className)}>
 			<ClayIcon
-				className="ca-icon-rule"
-				style={{
-					...(ruleConfig.fill && {fill: ruleConfig.fill}),
-					...(!ruleConfig.fill && {background: ruleConfig.color}),
-				}}
+				className={classNames('ca-icon-rule', ruleConfig.className)}
 				symbol={ruleConfig.symbol}
 			/>
 			{label}
