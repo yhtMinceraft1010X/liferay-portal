08ff6a31dfcc2a250ad0772b091966663c5f4b7f
Cheryl Tang <cheryl.tang@liferay.com>
2021-10-20 21:41:03 -0700

    LPS-140646 Create BlogsStatsUserDAO

---
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/model/impl/BlogsStatsUserDAOImpl.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.model.impl;
+
+import com.liferay.blogs.model.BlogsStatsUserDAO;
+
+import java.util.Date;
+
+/**
+ * @author Cheryl Tang
+ */
+public class BlogsStatsUserDAOImpl implements BlogsStatsUserDAO {
+
+	public BlogsStatsUserDAOImpl(
+		long groupId, long userId, Date lastPostDate, long entryCount,
+		long ratingsTotalEntries, double ratingsAverageScore,
+		double ratingsTotalScore) {
+
+		_groupId = groupId;
+		_userId = userId;
+		_lastPostDate = lastPostDate;
+		_entryCount = entryCount;
+		_ratingsTotalEntries = ratingsTotalEntries;
+		_ratingsAverageScore = ratingsAverageScore;
+		_ratingsTotalScore = ratingsTotalScore;
+	}
+
+	@Override
+	public long getEntryCount() {
+		return _entryCount;
+	}
+
+	@Override
+	public long getGroupId() {
+		return _groupId;
+	}
+
+	@Override
+	public Date getlastPostDate() {
+		return _lastPostDate;
+	}
+
+	@Override
+	public double getRatingsAverageScore() {
+		return _ratingsAverageScore;
+	}
+
+	@Override
+	public long getRatingsTotalEntries() {
+		return _ratingsTotalEntries;
+	}
+
+	@Override
+	public double getRatingsTotalScore() {
+		return _ratingsTotalScore;
+	}
+
+	@Override
+	public long getUserId() {
+		return _userId;
+	}
+
+	private final long _entryCount;
+	private final long _groupId;
+	private final Date _lastPostDate;
+	private final double _ratingsAverageScore;
+	private final long _ratingsTotalEntries;
+	private final double _ratingsTotalScore;
+	private final long _userId;
+
+}
