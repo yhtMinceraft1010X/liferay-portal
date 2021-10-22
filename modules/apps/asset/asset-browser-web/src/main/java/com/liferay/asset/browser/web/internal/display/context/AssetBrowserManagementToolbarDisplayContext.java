69bfea1959481a560ce158c3765fa15eb7bc3750
Jorge Díaz <jorge.diaz@liferay.com>
2021-10-22 10:18:27 +0200

    LPS-141268 Add the relevance option to the user interface in the same way it is done in JournalArticleItemSelectorViewManagementToolbarDisplayContext

--- modules/apps/asset/asset-browser-web/src/main/java/com/liferay/asset/browser/web/internal/display/context/AssetBrowserManagementToolbarDisplayContext.java
+++ modules/apps/asset/asset-browser-web/src/main/java/com/liferay/asset/browser/web/internal/display/context/AssetBrowserManagementToolbarDisplayContext.java
@@ -37,6 +37,7 @@ import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.PortletURLUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -248,7 +249,13 @@ public class AssetBrowserManagementToolbarDisplayContext
 
 	@Override
 	protected String[] getOrderByKeys() {
-		return new String[] {"title", "modified-date"};
+		String[] orderColumns = {"title", "modified-date"};
+
+		if (_assetBrowserDisplayContext.isSearch()) {
+			orderColumns = ArrayUtil.append(orderColumns, "relevance");
+		}
+
+		return orderColumns;
 	}
 
 	@Override
