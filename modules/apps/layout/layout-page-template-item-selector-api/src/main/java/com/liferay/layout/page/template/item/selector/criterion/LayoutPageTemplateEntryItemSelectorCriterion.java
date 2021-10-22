d85334be0e350481ffa49089fde0c74e3be05fa5
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-10-22 12:17:32 +0200

    LPS-141013 Adds selection criteria for LayoutPageTemplateEntry model

---
+++ modules/apps/layout/layout-page-template-item-selector-api/src/main/java/com/liferay/layout/page/template/item/selector/criterion/LayoutPageTemplateEntryItemSelectorCriterion.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.item.selector.criterion;
+
+import com.liferay.item.selector.BaseItemSelectorCriterion;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class LayoutPageTemplateEntryItemSelectorCriterion
+	extends BaseItemSelectorCriterion {
+
+	public int getLayoutType() {
+		return _layoutType;
+	}
+
+	public void setLayoutType(int layoutType) {
+		_layoutType = layoutType;
+	}
+
+	private int _layoutType;
+
+}
