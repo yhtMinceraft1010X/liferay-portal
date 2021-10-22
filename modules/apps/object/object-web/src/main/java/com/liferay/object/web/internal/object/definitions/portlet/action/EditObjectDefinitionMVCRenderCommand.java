a862722efd17f3ea95ef4ded689bb439ad35afc1
Guilherme Camacho <guilherme.camacho@liferay.com>
2021-10-22 16:01:26 -0300

    LPS-140085 Do not allow to set the relationship field for the object title field

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/action/EditObjectDefinitionMVCRenderCommand.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/action/EditObjectDefinitionMVCRenderCommand.java
@@ -21,7 +21,9 @@ import com.liferay.object.web.internal.constants.ObjectWebKeys;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
@@ -58,7 +60,11 @@ public class EditObjectDefinitionMVCRenderCommand implements MVCRenderCommand {
 					objectDefinitionId));
 			renderRequest.setAttribute(
 				ObjectWebKeys.OBJECT_FIELDS,
-				_objectFieldLocalService.getObjectFields(objectDefinitionId));
+				ListUtil.filter(
+					_objectFieldLocalService.getObjectFields(
+						objectDefinitionId),
+					objectField -> Validator.isNull(
+						objectField.getRelationshipType())));
 		}
 		catch (PortalException portalException) {
 			SessionErrors.add(renderRequest, portalException.getClass());
