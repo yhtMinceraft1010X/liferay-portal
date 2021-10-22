0ed8ecda4776dfbea3c72277f810f80fa852a325
Jair Medeiros <jair.medeiros@liferay.com>
2021-10-22 17:55:42 -0300

    LPS-140634 Remove useless spread operator for SearchProject Component

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SearchProject.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SearchProject.js
@@ -1,10 +1,10 @@
 import { ClayInput } from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 
-const SearchProject = ({ ...props }) => {
+const SearchProject = ({ props }) => {
   return (
     <div className="position-relative">
-      <ClayInput className="font-weight-semi-bold h5 rounded-pill search-project shadow-lg" {...props} type="text" />
+      <ClayInput className="font-weight-semi-bold h5 rounded-pill search-project shadow-lg" type="text" {...props} />
 
       <ClayIcon className="position-absolute text-brand-primary" symbol="search" />
     </div>
