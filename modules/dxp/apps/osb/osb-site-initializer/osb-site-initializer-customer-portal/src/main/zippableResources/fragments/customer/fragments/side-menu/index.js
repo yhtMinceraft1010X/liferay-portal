98905869673c0a9b8faf8b046451da70a1ae5e62
Jair Medeiros <jair.medeiros@liferay.com>
2021-10-26 16:25:54 -0300

    LPS-140634 SF

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/src/main/zippableResources/fragments/customer/fragments/side-menu/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/src/main/zippableResources/fragments/customer/fragments/side-menu/index.js
@@ -1,5 +1,17 @@
-fragmentElement.addEventListener("click", function (event) {
-  let items = fragmentElement.querySelectorAll(".menu-item-content");
+/* eslint-disable no-undef */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+fragmentElement.addEventListener("click", (event) => {
+  const items = fragmentElement.querySelectorAll(".menu-item-content");
 
   if (event.target.classList.contains("menu-item-content")) {
     event.target.classList.toggle("active");
@@ -7,16 +19,15 @@ fragmentElement.addEventListener("click", function (event) {
 
   items.forEach((f) => {
     if (f.classList.contains("active") != event.target) {
-      if (f == event.target) {
-      } else {
+      if (f != event.target) {
         f.classList.remove("active");
       }
     }
   });
   if (event.target.id == "subMenu") {
-    let arrow = fragmentElement.querySelector(".arrow");
-    let menu = fragmentElement.querySelector(".sub-menu");
- 		arrow.classList.toggle("arrow-left");
+    const arrow = fragmentElement.querySelector(".arrow");
+    const menu = fragmentElement.querySelector(".sub-menu");
+    arrow.classList.toggle("arrow-left");
     arrow.classList.toggle("arrow-down");
     menu.classList.toggle("sub-menu-closed");
     menu.classList.toggle("sub-menu-open");
