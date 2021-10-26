cbfc13ed95fc0e3b7b0f61befba02d5cf9ae8752
Jair Medeiros <jair.medeiros@liferay.com>
2021-10-26 12:01:54 -0300

    LPS-140634 Fix conditional implementation to render components

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/StatusTag.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/StatusTag.js
@@ -2,17 +2,15 @@ import ClayLabel from '@clayui/label';
 import { status } from '../utils/constants'
 
 const StatusTag = ({ currentStatus }) => {
-    switch (currentStatus) {
-        case status.active: {
-            return (<ClayLabel className="border-0 font-weight-normal m-0 px-2 status-tag text-capitalize text-paragraph-sm" displayType="success">Active</ClayLabel>);
-        }
-        case status.expired: {
-            return (<div>Expired</div>);
-        }
-        default: {
-            return (<div>No Status</div>);
-        }
+    if (currentStatus === status.active) {
+        return <ClayLabel className="border-0 font-weight-normal m-0 px-2 status-tag text-capitalize text-paragraph-sm" displayType="success">Active</ClayLabel>;
     }
+
+    if (currentStatus === status.expired) {
+        return <div>Expired</div>;
+    }
+
+    return <div>No Status</div>;
 };
 
 export default StatusTag;
--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/onboarding/pages/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/onboarding/pages/index.js
@@ -8,18 +8,15 @@ import Welcome from "./welcome";
 const Pages = () => {
     const [{ step }] = useContext(AppContext);
 
-    switch (step) {
-        case steps.invites: {
-            return <Invites />;
-        }
-
-        case steps.dxp: {
-            return <SetupDXP />
-        }
-        default: {
-            return <Welcome />;
-        }
+    if (step === steps.invites) {
+        return <Invites />;
     }
+
+    if (step === steps.dxp) {
+        return <SetupDXP />;
+    }
+
+    return <Welcome />;
 };
 
 export default Pages;
