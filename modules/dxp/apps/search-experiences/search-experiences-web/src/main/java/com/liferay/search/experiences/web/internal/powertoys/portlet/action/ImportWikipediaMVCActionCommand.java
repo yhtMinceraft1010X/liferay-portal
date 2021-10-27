f2fee396a9096b0f70f149b94daf40523ed20b18
Brian Chan <brian.chan@liferay.com>
2021-10-27 10:49:22 -0300

    LPS-141123 Add import wikipedia action

---
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/powertoys/portlet/action/ImportWikipediaMVCActionCommand.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.web.internal.powertoys.portlet.action;
+
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.search.experiences.constants.SXPPortletKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Petteri Karttunen
+ * @author André de Oliveira
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + SXPPortletKeys.SXP_POWER_TOYS,
+		"mvc.command.name=/sxp_power_toys/import_wikipedia"
+	},
+	service = MVCActionCommand.class
+)
+public class ImportWikipediaMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+		ActionRequest actionRequest, ActionResponse actionResponse) {
+	}
+
+	@Reference
+	private JournalArticleLocalService _journalArticleLocalService;
+
+}
