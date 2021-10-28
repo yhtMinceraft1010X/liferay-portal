8fdfe537a80ec6c346db0a52a9baf65714a2658a
Alessio Rendina <asilcomando@gmail.com>
2021-10-28 15:41:20 +0200

    COMMERCE-7305 Added new dataset constant

--- modules/apps/commerce/commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/frontend/constants/CommerceOrderDataSetConstants.java
+++ modules/apps/commerce/commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/frontend/constants/CommerceOrderDataSetConstants.java
@@ -23,6 +23,9 @@ public class CommerceOrderDataSetConstants {
 	public static final String COMMERCE_DATA_SET_KEY_COMMERCE_WISH_LISTS =
 		"commerceWishLists";
 
+	public static final String COMMERCE_DATA_SET_KEY_IMPORT_PLACED_ORDERS =
+		"commerceImportPlacedOrders";
+
 	public static final String COMMERCE_DATA_SET_KEY_PENDING_ORDER_ITEMS =
 		"commercePendingOrderItems";
 
