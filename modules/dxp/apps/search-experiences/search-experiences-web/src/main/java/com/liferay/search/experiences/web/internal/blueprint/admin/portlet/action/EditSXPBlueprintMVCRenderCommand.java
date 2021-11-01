bea307151f9047603112338bc741b7ddf6428175
Kevin Tan <kevin.tan@liferay.com>
2021-11-01 12:01:06 -0700

    LPS-141045 Render edit blueprint page

---
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/admin/portlet/action/EditSXPBlueprintMVCRenderCommand.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.web.internal.blueprint.admin.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.search.experiences.constants.SXPPortletKeys;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kevin Tan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + SXPPortletKeys.SXP_BLUEPRINT_ADMIN,
+		"mvc.command.name=/sxp_blueprint_admin/edit_sxp_blueprint"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditSXPBlueprintMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		return "/sxp_blueprint_admin/edit_sxp_blueprint.jsp";
+	}
+
+}
