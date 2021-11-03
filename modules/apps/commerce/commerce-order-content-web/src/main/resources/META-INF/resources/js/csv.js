ea41f58df34df7e215eceb1b5c07aa79414d1993
Alessio Rendina <asilcomando@gmail.com>
2021-11-03 12:06:33 +0100

    COMMERCE-7303 Added csv commerce order importer type jsp

---
+++ modules/apps/commerce/commerce-order-content-web/src/main/resources/META-INF/resources/js/csv.js
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {createPortletURL, navigate} from 'frontend-js-web';
+
+export default function ({currentURL, itemSelectorURL, namespace}) {
+	const fileEntryIdInput = document.getElementById(`${namespace}fileEntryId`);
+	const fileEntryNameInput = document.getElementById(`${namespace}fileEntryNameInput`);
+	const fileEntryRemoveIcon = document.getElementById(`${namespace}fileEntryRemoveIcon`);
+	const importButton = document.getElementById(`${namespace}importButton`);
+	const selectFileButton = document.getElementById(`${namespace}selectFileButton`);
+
+	if (importButton) {
+		importButton.addEventListener('click', (event) => {
+			event.preventDefault();
+
+			if (fileEntryIdInput) {
+				navigate(
+					createPortletURL(
+						currentURL,
+						{fileEntryId: fileEntryIdInput.value}));
+			}
+		});
+	}
+
+	if (fileEntryNameInput && fileEntryRemoveIcon && selectFileButton) {
+		selectFileButton.addEventListener('click', (event) => {
+			event.preventDefault();
+
+			Liferay.Util.openSelectionModal({
+				onSelect: (selectedItem) => {
+					if (!selectedItem) {
+						return;
+					}
+
+					const value = JSON.parse(selectedItem.value);
+
+					if (fileEntryIdInput) {
+						fileEntryIdInput.value = value.fileEntryId;
+					}
+
+					fileEntryRemoveIcon.classList.remove('hide');
+
+					fileEntryNameInput.innerHTML =
+						'<a>' + Liferay.Util.escape(value.title) + '</a>';
+				},
+				selectEventName: 'addFileEntry',
+				title: Liferay.Language.get('select-file'),
+				url: itemSelectorURL,
+			});
+		});
+
+		fileEntryRemoveIcon.addEventListener('click', (event) => {
+			event.preventDefault();
+
+			if (fileEntryIdInput) {
+				fileEntryIdInput.value = 0;
+			}
+
+			fileEntryNameInput.innerText = Liferay.Language.get('none');
+
+			fileEntryRemoveIcon.classList.add('hide');
+		});
+	}
+}
--- modules/apps/commerce/commerce-order-content-web/src/main/resources/META-INF/resources/pending_commerce_orders/importer_type/csv.jsp
+++ modules/apps/commerce/commerce-order-content-web/src/main/resources/META-INF/resources/pending_commerce_orders/importer_type/csv.jsp
@@ -16,4 +16,37 @@
 
 <%@ include file="/init.jsp" %>
 
-<h1>CSV</h1>
+<portlet:actionURL name="/commerce_open_order_content/import_csv_file_entry" var="importCSVFileEntryActionURL" />
+
+<aui:form action="<%= importCSVFileEntryActionURL %>" method="post" name="fm">
+	<aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= Constants.IMPORT %>" />
+	<aui:input name="fileEntryId" type="hidden" />
+
+	<p class="text-default">
+		<span class="hide" id="<portlet:namespace />fileEntryRemoveIcon" role="button">
+			<aui:icon cssClass="icon-monospaced" image="times" markupView="lexicon" />
+		</span>
+		<span id="<portlet:namespace />fileEntryNameInput">
+			<span class="text-muted"><liferay-ui:message key="none" /></span>
+		</span>
+	</p>
+
+	<aui:button name="selectFileButton" value="select" />
+
+	<aui:button-row>
+		<aui:button cssClass="btn-lg" name="importButton" primary="<%= true %>" type="submit" value='<%= LanguageUtil.get(request, "import") %>' />
+
+		<aui:button cssClass="btn-lg" href="<%= redirect %>" type="cancel" />
+	</aui:button-row>
+</aui:form>
+
+<liferay-frontend:component
+	context='<%=
+		HashMapBuilder.<String, Object>put(
+			"currentURL", currentURL
+		).put(
+			"itemSelectorURL", commerceOrderContentDisplayContext.getCSVFileEntryItemSelectorURL()
+		).build()
+	%>'
+	module="js/csv"
+/>
