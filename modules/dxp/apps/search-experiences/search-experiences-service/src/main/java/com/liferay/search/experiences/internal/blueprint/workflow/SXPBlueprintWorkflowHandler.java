a2579d6db8f94b5ac57faafbaa7ccd4f3de7955c
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-11-04 15:40:17 +0200

    LPS-141937 search-experiences-service: SXPBlueprint workflow handler

---
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/blueprint/workflow/SXPBlueprintWorkflowHandler.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.internal.blueprint.workflow;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.BaseWorkflowHandler;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.workflow.WorkflowHandler;
+import com.liferay.search.experiences.model.SXPBlueprint;
+import com.liferay.search.experiences.service.SXPBlueprintLocalService;
+
+import java.io.Serializable;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Petteri Karttunen
+ */
+@Component(
+	property = "model.class.name=com.liferay.search.experiences.model.SXPBlueprint",
+	service = WorkflowHandler.class
+)
+public class SXPBlueprintWorkflowHandler
+	extends BaseWorkflowHandler<SXPBlueprint> {
+
+	@Override
+	public String getClassName() {
+		return SXPBlueprint.class.getName();
+	}
+
+	@Override
+	public String getType(Locale locale) {
+		return ResourceActionsUtil.getModelResource(locale, getClassName());
+	}
+
+	@Override
+	public SXPBlueprint updateStatus(
+			int status, Map<String, Serializable> workflowContext)
+		throws PortalException {
+
+		return _sxpBlueprintLocalService.updateStatus(
+			GetterUtil.getLong(
+				(String)workflowContext.get(WorkflowConstants.CONTEXT_USER_ID)),
+			GetterUtil.getLong(
+				(String)workflowContext.get(
+					WorkflowConstants.CONTEXT_ENTRY_CLASS_PK)),
+			status, (ServiceContext)workflowContext.get("serviceContext"));
+	}
+
+	@Reference
+	private SXPBlueprintLocalService _sxpBlueprintLocalService;
+
+}
