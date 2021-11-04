39a79c77e26bcc4c53a5f55457021981e363e1d8
pei-jung <pei-jung.lan@liferay.com>
2021-11-04 11:06:40 -0700

    LPS-141830 (account-service) - add new methods to AccountGroupService

--- modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountGroupServiceImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountGroupServiceImpl.java
@@ -14,10 +14,17 @@
 
 package com.liferay.account.service.impl;
 
+import com.liferay.account.constants.AccountActionKeys;
+import com.liferay.account.model.AccountGroup;
 import com.liferay.account.service.base.AccountGroupServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.service.permission.PortalPermissionUtil;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -30,4 +37,54 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class AccountGroupServiceImpl extends AccountGroupServiceBaseImpl {
+
+	@Override
+	public AccountGroup addAccountGroup(
+			long userId, String description, String name)
+		throws PortalException {
+
+		PortalPermissionUtil.check(
+			getPermissionChecker(), AccountActionKeys.ADD_ACCOUNT_GROUP);
+
+		return accountGroupLocalService.addAccountGroup(
+			userId, description, name);
+	}
+
+	@Override
+	public AccountGroup deleteAccountGroup(long accountGroupId)
+		throws PortalException {
+
+		_accountGroupModelResourcePermission.check(
+			getPermissionChecker(), accountGroupId, ActionKeys.DELETE);
+
+		return accountGroupLocalService.deleteAccountGroup(accountGroupId);
+	}
+
+	@Override
+	public void deleteAccountGroups(long[] accountGroupIds)
+		throws PortalException {
+
+		for (long accountGroupId : accountGroupIds) {
+			deleteAccountGroup(accountGroupId);
+		}
+	}
+
+	@Override
+	public AccountGroup updateAccountGroup(
+			long accountGroupId, String description, String name)
+		throws PortalException {
+
+		_accountGroupModelResourcePermission.check(
+			getPermissionChecker(), accountGroupId, ActionKeys.UPDATE);
+
+		return accountGroupLocalService.updateAccountGroup(
+			accountGroupId, description, name);
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.account.model.AccountGroup)"
+	)
+	private ModelResourcePermission<AccountGroup>
+		_accountGroupModelResourcePermission;
+
 }
