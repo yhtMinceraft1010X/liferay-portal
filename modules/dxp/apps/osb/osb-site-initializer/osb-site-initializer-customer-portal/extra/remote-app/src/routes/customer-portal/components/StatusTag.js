e6b896b4aec034166be04efa4a3c211b19e8e2b5
Aguinus Santana <aguinus.santana@liferay.com>
2021-11-04 10:27:35 -0300

    LPS-140156 Add new layouts styles for the labels.

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/StatusTag.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/StatusTag.js
@@ -1,23 +1,21 @@
 import ClayLabel from '@clayui/label';
-import {status} from '../utils/constants';
+import { status } from '../utils/constants';
 
-const StatusTag = ({currentStatus}) => {
-	if (currentStatus === status.active) {
-		return (
-			<ClayLabel
-				className="border-0 font-weight-normal m-0 px-2 status-tag text-capitalize text-paragraph-sm"
-				displayType="success"
-			>
-				Active
-			</ClayLabel>
-		);
-	}
-
-	if (currentStatus === status.expired) {
-		return <div>Expired</div>;
-	}
-
-	return <div>No Status</div>;
+const StatusTag = ({ currentStatus }) => {
+    switch (currentStatus) {
+        case status.active: {
+            return (<ClayLabel className="border-0 font-weight-normal m-0 px-2 status-tag text-capitalize text-paragraph-sm" displayType="success">Active</ClayLabel>);
+        }
+        case status.expired: {
+            return (<ClayLabel className="border-0 font-weight-normal m-0 px-2 status-tag text-capitalize text-paragraph-sm" displayType="danger">Expired</ClayLabel>);
+        }
+        case status.future: {
+            return (<ClayLabel className="border-0 font-weight-normal m-0 px-2 status-tag text-capitalize text-paragraph-sm" displayType="info">Future</ClayLabel>);
+        }
+        default: {
+            return (<div>No Status</div>);
+        };
+    };
 };
 
 export default StatusTag;
