e68cf1f5d551770cf1ad29728b481efdb88a5573
Ricardo Moura <ricardo.moura@liferay.com>
2021-11-04 09:19:41 -0300

    LPS-141932 Fix onclick behavior to replace the plus icon with the minus icon

--- modules/apps/site-initializer/site-initializer-raylife/src/main/zippableResources/fragments/raylife/fragments/list-item-with-icon/index.js
+++ modules/apps/site-initializer/site-initializer-raylife/src/main/zippableResources/fragments/raylife/fragments/list-item-with-icon/index.js
@@ -1,12 +1,23 @@
-const icon = fragmentElement.querySelector("#plus-icon");
-const textDiv = fragmentElement.querySelector("#collapseExample");
+const plusIcon = fragmentElement.querySelector('#plus-icon');
+const minusIcon = fragmentElement.querySelector('#minus-icon');
+const customIcons = fragmentElement.getElementsByClassName('custom-icon');
+const collapseDiv = fragmentElement.querySelector('#collapseDiv');
 
-icon.onclick = function(){
-	let flag = textDiv.classList.contains("collapse");
-	if(flag){
-			textDiv.classList.remove("collapse");
-	}
-	else{
-		textDiv.classList.add("collapse");
-	}
+for(let i = 0; i < customIcons.length; i++){
+	const icon = customIcons[i];
+	
+	icon.onclick = function () {
+		
+		let flag = collapseDiv.classList.contains('collapse');
+		if (flag) {
+			collapseDiv.classList.remove('collapse');
+			plusIcon.classList.add('d-none');
+			minusIcon.classList.remove('d-none');
+		}
+		else {
+			collapseDiv.classList.add('collapse');
+			plusIcon.classList.remove('d-none');
+			minusIcon.classList.add('d-none');
+		}
+	};
 }
