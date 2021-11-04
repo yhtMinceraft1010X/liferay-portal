8af59be7ed5c8a05edf909536989f945a816de8b
Aguinus Santana <aguinus.santana@liferay.com>
2021-11-04 11:00:47 -0300

    LPS-140156 Add adjustments for new home config

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Home.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Home.js
@@ -1,21 +1,22 @@
 import classNames from 'classnames';
-import {useEffect} from 'react';
-import useGraphQL from '~/common/hooks/useGraphQL';
-import {LiferayTheme} from '~/common/services/liferay';
-import {getKoroneikiAccountsByFilter} from '~/common/services/liferay/graphql/koroneiki-accounts';
-import {getUserAccountById} from '~/common/services/liferay/graphql/user-accounts';
-import {STORAGE_KEYS, Storage} from '~/common/services/liferay/storage';
-import {REACT_APP_LIFERAY_API} from '~/common/utils';
+import { useContext, useEffect, useState } from 'react';
+import useGraphQL from '~/common/hooks/useGraphql';
+import { LiferayTheme } from '~/common/services/liferay';
+import { getKoroneikiAccountsByFilter } from '~/common/services/liferay/graphql/koroneiki-accounts';
+import { getUserAccountById } from '~/common/services/liferay/graphql/user-accounts';
+import { STORAGE_KEYS, Storage } from '~/common/services/liferay/storage';
+import { REACT_APP_LIFERAY_API } from '~/common/utils';
+import Banner from '../components/Banner';
 import ProjectCard from '../components/ProjectCard';
 import SearchProject from '../components/SearchProject';
-import {status} from '../utils/constants';
+import { AppContext } from "../context";
+import { status } from '../utils/constants';
 
 const PROJECT_THRESHOLD_COUNT = 4;
 
 const Home = () => {
-	const {data: userAccount} = useGraphQL(
-		getUserAccountById(LiferayTheme.getUserId())
-	);
+	const { data: userAccount } = useGraphQL(getUserAccountById(LiferayTheme.getUserId()));
+	const [{ assetsPath }] = useContext(AppContext);
 
 	useEffect(() => {
 		if (userAccount) {
@@ -35,11 +36,11 @@ const Home = () => {
 
 	const accountBriefs = userAccount?.accountBriefs || [];
 
-	const {data: koroneikiAccounts} =
+	const { data: koroneikiAccounts } =
 		useGraphQL(
 			getKoroneikiAccountsByFilter({
 				accountKeys: accountBriefs.map(
-					({externalReferenceCode}) => externalReferenceCode
+					({ externalReferenceCode }) => externalReferenceCode
 				),
 			})
 		) || [];
@@ -67,12 +68,14 @@ const Home = () => {
 				},
 				status: slaCurrent ? status.active : status.expired,
 				title: accountBriefs.find(
-					({externalReferenceCode}) =>
+					({ externalReferenceCode }) =>
 						externalReferenceCode === accountKey
 				).name,
 			})
 		) || [];
 
+	const [currentProjects, setProjects] = useState(projects);
+	const changeInput = (value) => setProjects(projects.filter((project) => project.title.toLowerCase().includes(value.toLowerCase())));
 	const withManyProjects = projects.length > PROJECT_THRESHOLD_COUNT;
 
 	const nextPage = (project) => {
@@ -84,36 +87,49 @@ const Home = () => {
 
 	return (
 		<>
-			<div
-				className={classNames('display-4', 'font-weight-bold', 'mb-5', {
-					'pb-2': withManyProjects,
-				})}
-			>
-				Projects
-			</div>
-			{withManyProjects && (
-				<div className="align-items-center d-flex justify-content-between mb-4">
-					<SearchProject placeholder="Find a project" />
+			<div className="d-flex flex-column ml-2 pl-5">
+				<div
+					className={classNames('display-4 font-weight-bold mb-5', {
+						'pb-2': withManyProjects
+					})}
+				>
+					<Banner />
 
-					<h5 className="m-0 text-neutral-3">
-						{projects.length} projects
-					</h5>
-				</div>
-			)}
-			<div
-				className={classNames('d-flex', 'flex-wrap', {
-					'home-projects': !withManyProjects,
-					'home-projects-sm pt-2': withManyProjects,
-				})}
-			>
-				{projects.map((project, index) => (
-					<ProjectCard
-						key={index}
-						onClick={() => nextPage(project)}
-						small={withManyProjects}
-						{...project}
+					<img
+						src={`${assetsPath}/teste/banner_background_customer_portal.svg`}
 					/>
-				))}
+				</div>
+			</div>
+			<div className={classNames("mx-auto", {
+				"col-5": withManyProjects,
+				"col-8 pl-6": !withManyProjects,
+			})}>
+				<div className="d-flex flex-column">
+					{withManyProjects && (
+						<div className="align-items-center d-flex justify-content-between mb-4 mr-5 pr-3">
+							<SearchProject onChange={changeInput} />
+
+							<h5 className="m-0 text-neutral-3">
+								{projects.length} projects
+							</h5>
+						</div>
+					)}
+					<div
+						className={classNames("d-flex", "flex-wrap", {
+							"home-projects": !withManyProjects,
+							"home-projects-sm pt-2": withManyProjects,
+						})}
+					>
+						{currentProjects.map((project, index) => (
+							<ProjectCard
+								key={index}
+								onClick={() => nextPage(project)}
+								small={withManyProjects}
+								{...project}
+							/>
+						))}
+					</div>
+				</div>
 			</div>
 		</>
 	);
