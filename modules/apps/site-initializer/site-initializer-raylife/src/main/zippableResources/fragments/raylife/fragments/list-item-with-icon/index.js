5c08d47fa45ecc1ef62e80c37ccc83546f7b3d04
Ricardo <35113296+Odrakir1@users.noreply.github.com>
2021-11-05 09:23:39 -0300

    LPS-141932 Apply suggestions from code review
    Co-authored-by: Keven Leone <keven.santos.sz@gmail.com>

--- modules/apps/site-initializer/site-initializer-raylife/src/main/zippableResources/fragments/raylife/fragments/list-item-with-icon/index.js
+++ modules/apps/site-initializer/site-initializer-raylife/src/main/zippableResources/fragments/raylife/fragments/list-item-with-icon/index.js
@@ -1,13 +1,14 @@
 const plusIcon = fragmentElement.querySelector('#plus-icon');
 const minusIcon = fragmentElement.querySelector('#minus-icon');
 const customIcons = fragmentElement.getElementsByClassName('custom-icon');
-const collapseDiv = fragmentElement.querySelector('#collapseDiv');
+const faqList = fragmentElement.querySelector('#faq-list');
 
 for (let i = 0; i < customIcons.length; i++) {
 	const icon = customIcons[i];
 
 	icon.onclick = function () {
 		let flag = collapseDiv.classList.contains('collapse');
+
 		if (flag) {
 			collapseDiv.classList.remove('collapse');
 			plusIcon.classList.add('d-none');
