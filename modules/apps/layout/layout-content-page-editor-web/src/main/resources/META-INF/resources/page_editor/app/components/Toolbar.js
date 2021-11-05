6421bc368f4820847e057d8d300719b81a2da345
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-11-05 12:30:18 +0100

    LPS-141999 Adds button to the toolbar and adapt styles

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/Toolbar.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/Toolbar.js
@@ -22,6 +22,7 @@ import React, {useEffect, useState} from 'react';
 import useLazy from '../../core/hooks/useLazy';
 import useLoad from '../../core/hooks/useLoad';
 import usePlugins from '../../core/hooks/usePlugins';
+import CreateLayoutPageTemplateEntryButton from '../../plugins/create-layout-page-template-entry-modal/components/CreateLayoutPageTemplateEntryButton';
 import * as Actions from '../actions/index';
 import {LAYOUT_TYPES} from '../config/constants/layoutTypes';
 import {SERVICE_NETWORK_STATUS_TYPES} from '../config/constants/serviceNetworkStatusTypes';
@@ -263,31 +264,42 @@ function ToolbarBody({className}) {
 			</ul>
 
 			<ul className="end navbar-nav" onClick={deselectItem}>
-				<div className="nav-item">
+				<li className="nav-item">
 					<NetworkStatusBar {...network} />
-				</div>
+				</li>
 
-				<div className="nav-item">
+				<li className="nav-item">
 					<Undo onRedo={onRedo} onUndo={onUndo} />
-				</div>
+				</li>
 
 				<li className="nav-item">
 					<EditModeSelector />
 				</li>
 
 				<li className="nav-item">
-					<ClayButtonWithIcon
-						className="btn btn-secondary"
-						displayType="secondary"
-						onClick={() => setOpenPreviewModal(true)}
-						small
-						symbol="view"
-						title={Liferay.Language.get('preview')}
-						type="button"
-					>
-						{Liferay.Language.get('preview')}
-					</ClayButtonWithIcon>
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<ClayButtonWithIcon
+								className="btn btn-secondary"
+								displayType="secondary"
+								onClick={() => setOpenPreviewModal(true)}
+								small
+								symbol="view"
+								title={Liferay.Language.get('preview')}
+								type="button"
+							>
+								{Liferay.Language.get('preview')}
+							</ClayButtonWithIcon>
+						</li>
+
+						{config.layoutType === LAYOUT_TYPES.content && (
+							<li className="nav-item">
+								<CreateLayoutPageTemplateEntryButton />
+							</li>
+						)}
+					</ul>
 				</li>
+
 				{config.singleSegmentsExperienceMode && (
 					<li className="nav-item">
 						<form action={config.discardDraftURL} method="POST">
