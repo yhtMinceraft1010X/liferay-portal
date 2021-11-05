07eb23f740a0b6f29dfe739e78187b9a1332a694
Luca Pellizzon <luca.pellizzon@liferay.com>
2021-11-05 15:01:55 +0100

    COMMERCE-6387 add checkout redirect render command

---
+++ modules/apps/commerce/commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/internal/portlet/action/CheckoutRedirectMVCRenderCommand.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.checkout.web.internal.portlet.action;
+
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.order.CommerceOrderHttpHelper;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = {
+		"javax.portlet.name=" + CommercePortletKeys.COMMERCE_CHECKOUT,
+		"mvc.command.name=/commerce_checkout/checkout_redirect"
+	},
+	service = MVCRenderCommand.class
+)
+public class CheckoutRedirectMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			renderRequest);
+
+		HttpServletRequest originalHttpServletRequest =
+			_portal.getOriginalServletRequest(httpServletRequest);
+
+		HttpServletResponse originalHttpServletResponse =
+			_portal.getHttpServletResponse(renderResponse);
+
+		while (originalHttpServletResponse instanceof
+					HttpServletResponseWrapper) {
+
+			HttpServletResponseWrapper httpServletResponseWrapper =
+				(HttpServletResponseWrapper)originalHttpServletResponse;
+
+			originalHttpServletResponse =
+				(HttpServletResponse)httpServletResponseWrapper.getResponse();
+		}
+
+		try {
+			originalHttpServletResponse.sendRedirect(
+				String.valueOf(
+					_commerceOrderHttpHelper.getCommerceCheckoutPortletURL(
+						originalHttpServletRequest)));
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception.getMessage(), exception);
+			}
+		}
+
+		return "/view.jsp";
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CheckoutRedirectMVCRenderCommand.class);
+
+	@Reference
+	private CommerceOrderHttpHelper _commerceOrderHttpHelper;
+
+	@Reference
+	private Portal _portal;
+
+}
