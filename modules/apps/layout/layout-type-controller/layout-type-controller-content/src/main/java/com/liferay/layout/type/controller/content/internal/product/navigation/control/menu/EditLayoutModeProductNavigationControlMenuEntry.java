b6e202c32fb58fca5a54f9b4b5adb21351f67a42
Brian Chan <brian.chan@liferay.com>
2021-11-08 14:26:50 -0300

    LPS-141773 sort

--- modules/apps/layout/layout-type-controller/layout-type-controller-content/src/main/java/com/liferay/layout/type/controller/content/internal/product/navigation/control/menu/EditLayoutModeProductNavigationControlMenuEntry.java
+++ modules/apps/layout/layout-type-controller/layout-type-controller-content/src/main/java/com/liferay/layout/type/controller/content/internal/product/navigation/control/menu/EditLayoutModeProductNavigationControlMenuEntry.java
@@ -138,6 +138,12 @@ public class EditLayoutModeProductNavigationControlMenuEntry
 				redirect = _portal.getLayoutFullURL(draftLayout, themeDisplay);
 			}
 
+			redirect = _http.setParameter(
+				redirect, "p_l_back_url",
+				_portal.getLayoutFullURL(
+					themeDisplay.getLayout(), themeDisplay));
+			redirect = _http.setParameter(redirect, "p_l_mode", Constants.EDIT);
+
 			long segmentsExperienceId = ParamUtil.getLong(
 				httpServletRequest, "p_s_e_id", -1);
 
@@ -146,12 +152,7 @@ public class EditLayoutModeProductNavigationControlMenuEntry
 					redirect, "p_s_e_id", segmentsExperienceId);
 			}
 
-			redirect = _http.setParameter(
-				redirect, "p_l_back_url",
-				_portal.getLayoutFullURL(
-					themeDisplay.getLayout(), themeDisplay));
-
-			return _http.setParameter(redirect, "p_l_mode", Constants.EDIT);
+			return redirect;
 		}
 		catch (Exception exception) {
 			if (_log.isDebugEnabled()) {
