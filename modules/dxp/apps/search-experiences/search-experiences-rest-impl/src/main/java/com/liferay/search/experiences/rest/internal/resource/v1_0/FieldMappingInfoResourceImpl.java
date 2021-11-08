8747b81e4aefc774fcb3bb8807e973fd61ceb193
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-11-08 19:30:19 +0200

    LPS-112856 search-experiences-rest-impl: Implement methods for field mapping info

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/FieldMappingInfoResourceImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/FieldMappingInfoResourceImpl.java
@@ -14,9 +14,29 @@
 
 package com.liferay.search.experiences.rest.internal.resource.v1_0;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.index.IndexInformation;
+import com.liferay.portal.search.index.IndexNameBuilder;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.search.experiences.rest.dto.v1_0.FieldMappingInfo;
+import com.liferay.search.experiences.rest.internal.web.cache.FieldMappingsWebCacheItem;
 import com.liferay.search.experiences.rest.resource.v1_0.FieldMappingInfoResource;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,4 +48,117 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class FieldMappingInfoResourceImpl
 	extends BaseFieldMappingInfoResourceImpl {
+
+	@Override
+	public Page<FieldMappingInfo> getFieldMappingInfosPage(String query)
+		throws Exception {
+
+		return Page.of(getFieldMappings(query));
+	}
+
+	public List<FieldMappingInfo> getFieldMappings(String query) {
+		JSONObject jsonObject = FieldMappingsWebCacheItem.get(
+			_indexInformation,
+			_indexNameBuilder.getIndexName(contextCompany.getCompanyId()));
+
+		if (jsonObject.length() == 0) {
+			return Collections.<FieldMappingInfo>emptyList();
+		}
+
+		List<FieldMappingInfo> fieldMappingInfos = new ArrayList<>();
+
+		_addFieldMappingInfos(
+			fieldMappingInfos, new HashSet<String>(), jsonObject,
+			StringPool.BLANK, query);
+
+		return fieldMappingInfos;
+	}
+
+	private void _addFieldMappingInfo(
+		List<FieldMappingInfo> fieldMappingInfos, String fieldName,
+		Set<String> fieldNames, JSONObject jsonObject,
+		int languageIdPosition1) {
+
+		String fieldNameWithPosition = fieldName + languageIdPosition1;
+
+		if (!fieldNames.contains(fieldNameWithPosition)) {
+			fieldMappingInfos.add(
+				new FieldMappingInfo() {
+					{
+						languageIdPosition = languageIdPosition1;
+						name = fieldName;
+						type = jsonObject.getString("type");
+					}
+				});
+			fieldNames.add(fieldNameWithPosition);
+		}
+	}
+
+	private void _addFieldMappingInfos(
+		List<FieldMappingInfo> fieldMappingInfos, Set<String> fieldNames,
+		JSONObject jsonObject, String path, String query) {
+
+		for (String fieldName : jsonObject.keySet()) {
+			JSONObject fieldJSONObject = jsonObject.getJSONObject(fieldName);
+
+			String fieldPath = fieldName;
+
+			String fieldPathLowerCase = StringUtil.toLowerCase(fieldPath);
+
+			if (!Validator.isBlank(query) &&
+				!fieldPathLowerCase.contains(StringUtil.toLowerCase(query))) {
+
+				continue;
+			}
+
+			if (!Validator.isBlank(path)) {
+				fieldPath = path + "." + fieldName;
+			}
+
+			if (Objects.equals(fieldJSONObject.getString("type"), "nested")) {
+				_addFieldMappingInfos(
+					fieldMappingInfos, fieldNames,
+					fieldJSONObject.getJSONObject("properties"), fieldPath,
+					query);
+			}
+			else {
+				String languageId = _getLanguageId(fieldName);
+
+				int languageIdPosition = -1;
+
+				if (!Validator.isBlank(languageId)) {
+					languageIdPosition = fieldName.lastIndexOf(languageId);
+					fieldPath = StringUtil.removeSubstring(
+						fieldPath, languageId);
+				}
+
+				_addFieldMappingInfo(
+					fieldMappingInfos, fieldPath, fieldNames, fieldJSONObject,
+					languageIdPosition);
+			}
+		}
+	}
+
+	private String _getLanguageId(String fieldName) {
+		Matcher matcher = _languageIdPattern.matcher(fieldName);
+
+		if (matcher.matches()) {
+			return matcher.group(2);
+		}
+
+		return null;
+	}
+
+	private static final Pattern _languageIdPattern = Pattern.compile(
+		"(.*)(_[a-z]{2}_[A-Z]{2})(_.*)?");
+
+	@Reference
+	private IndexInformation _indexInformation;
+
+	@Reference
+	private IndexNameBuilder _indexNameBuilder;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
 }
---
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/web/cache/FieldMappingsWebCacheItem.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.rest.internal.web.cache;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.webcache.WebCacheItem;
+import com.liferay.portal.kernel.webcache.WebCachePoolUtil;
+import com.liferay.portal.search.index.IndexInformation;
+
+/**
+ * @author Petteri Karttunen
+ */
+public class FieldMappingsWebCacheItem implements WebCacheItem {
+
+	public static JSONObject get(
+		IndexInformation indexInformation, String indexName) {
+
+		return (JSONObject)WebCachePoolUtil.get(
+			FieldMappingsWebCacheItem.class.getName() + StringPool.POUND +
+				indexName,
+			new FieldMappingsWebCacheItem(indexInformation, indexName));
+	}
+
+	public FieldMappingsWebCacheItem(
+		IndexInformation indexInformation, String indexName) {
+
+		_indexInformation = indexInformation;
+		_indexName = indexName;
+	}
+
+	@Override
+	public JSONObject convert(String key) {
+		try {
+			return JSONUtil.getValueAsJSONObject(
+				JSONFactoryUtil.createJSONObject(
+					_indexInformation.getFieldMappings(_indexName)),
+				"JSONObject/" + _indexName, "JSONObject/mappings",
+				"JSONObject/properties");
+		}
+		catch (JSONException jsonException) {
+			_log.error(jsonException.getMessage(), jsonException);
+		}
+
+		return JSONFactoryUtil.createJSONObject();
+	}
+
+	@Override
+	public long getRefreshTime() {
+		return _REFRESH_TIME;
+	}
+
+	private static final long _REFRESH_TIME = Time.MINUTE * 30;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		FieldMappingsWebCacheItem.class);
+
+	private final IndexInformation _indexInformation;
+	private final String _indexName;
+
+}
