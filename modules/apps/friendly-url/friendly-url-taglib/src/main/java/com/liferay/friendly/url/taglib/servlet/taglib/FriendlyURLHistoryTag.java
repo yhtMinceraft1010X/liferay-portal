79a96f5fe6e0c226d7b090fb83501e5ad4d48b2d
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-11-08 13:00:33 +0100

    LPS-140308 Create tag class and resources

---
+++ modules/apps/friendly-url/friendly-url-taglib/src/main/java/com/liferay/friendly/url/taglib/servlet/taglib/FriendlyURLHistoryTag.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.friendly.url.taglib.servlet.taglib;
+
+import com.liferay.friendly.url.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class FriendlyURLHistoryTag extends IncludeTag {
+
+	public String getClassName() {
+		return _className;
+	}
+
+	public long getClassPK() {
+		return _classPK;
+	}
+
+	public String getElementId() {
+		return _elementId;
+	}
+
+	public void setClassName(String className) {
+		_className = className;
+	}
+
+	public void setClassPK(long classPK) {
+		_classPK = classPK;
+	}
+
+	public void setElementId(String elementId) {
+		_elementId = elementId;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		setServletContext(ServletContextUtil.getServletContext());
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_className = null;
+		_classPK = 0;
+		_elementId = null;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest httpServletRequest) {
+		super.setAttributes(httpServletRequest);
+
+		httpServletRequest.setAttribute(
+			"liferay-friendly-url:history:className", getClassName());
+		httpServletRequest.setAttribute(
+			"liferay-friendly-url:history:classPK", getClassPK());
+		httpServletRequest.setAttribute(
+			"liferay-friendly-url:history:elementId", getElementId());
+	}
+
+	private static final String _PAGE = "/page.jsp";
+
+	private String _className;
+	private long _classPK;
+	private String _elementId;
+
+}
