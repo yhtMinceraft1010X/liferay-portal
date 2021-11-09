2e2fa225351357bb1998daa6c104d196772e5e37
Bryan Engler <bryan.engler@liferay.com>
2021-11-08 17:19:16 -0800

    LPS-142022 search-experiences-service: Use SXPBlueprintDTOConverter
    https://issues.liferay.com/browse/LPS-142022

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/blueprint/search/request/enhancer/SXPBlueprintSearchRequestEnhancerImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/blueprint/search/request/enhancer/SXPBlueprintSearchRequestEnhancerImpl.java
@@ -27,6 +27,8 @@ import com.liferay.portal.search.script.Scripts;
 import com.liferay.portal.search.searcher.SearchRequestBuilder;
 import com.liferay.portal.search.significance.SignificanceHeuristics;
 import com.liferay.portal.search.sort.Sorts;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.search.experiences.blueprint.search.request.enhancer.SXPBlueprintSearchRequestEnhancer;
 import com.liferay.search.experiences.internal.blueprint.highlight.HighlightConverter;
 import com.liferay.search.experiences.internal.blueprint.parameter.SXPParameterData;
@@ -64,9 +66,21 @@ public class SXPBlueprintSearchRequestEnhancerImpl
 		SearchRequestBuilder searchRequestBuilder,
 		com.liferay.search.experiences.model.SXPBlueprint sxpBlueprint) {
 
-		_enhance(
-			searchRequestBuilder,
-			SXPBlueprintUtil.toSXPBlueprint(null, sxpBlueprint));
+		DTOConverter
+			<com.liferay.search.experiences.model.SXPBlueprint, SXPBlueprint>
+				dtoConverter =
+					(DTOConverter
+						<com.liferay.search.experiences.model.SXPBlueprint,
+						 SXPBlueprint>)_dtoConverterRegistry.getDTOConverter(
+							 com.liferay.search.experiences.model.SXPBlueprint.
+								 class.getName());
+
+		try {
+			_enhance(searchRequestBuilder, dtoConverter.toDTO(sxpBlueprint));
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
 	}
 
 	@Override
@@ -163,6 +177,9 @@ public class SXPBlueprintSearchRequestEnhancerImpl
 
 	@Reference
 	private ComplexQueryPartBuilderFactory _complexQueryPartBuilderFactory;
+
+	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
 
 	@Reference
 	private FieldConfigBuilderFactory _fieldConfigBuilderFactory;
