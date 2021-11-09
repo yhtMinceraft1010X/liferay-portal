91a34c9ce71bf82c769ab338bb951c4656919b0c
André de Oliveira <andre.oliveira@liferay.com>
2021-11-09 16:25:42 -0600

    LPS-142350 search-experiences-rest-api: Test: Fail fast if any of the well known reference Blueprints cannot toDTO-toString-toDTO for any reason
    https://issues.liferay.com/browse/LPS-142350

---
+++ modules/dxp/apps/search-experiences/search-experiences-rest-api/src/test/java/com/liferay/search/experiences/rest/dto/v1_0/util/SXPBlueprintUtilTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.rest.dto.v1_0.util;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.search.experiences.rest.dto.v1_0.SXPBlueprint;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author André de Oliveira
+ */
+public class SXPBlueprintUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testToSXPBlueprint() throws Exception {
+		String[] fileNames = {
+			"advanced_configuration_test",
+			"aggregations_test",
+
+			// TODO "content_driven_test",
+
+			"highlights_test",
+			"personalization_test",
+			"sorts_tests"
+		};
+
+		for (String fileName : fileNames) {
+			try {
+				_testToSXPBlueprint(fileName);
+			}
+			catch (Exception exception) {
+				throw new RuntimeException(fileName, exception);
+			}
+		}
+	}
+
+	private void _testToSXPBlueprint(String fileName) throws Exception {
+		String json = StringUtil.read(
+			getClass(), "dependencies/" + fileName + ".json");
+
+		SXPBlueprint sxpBlueprint = SXPBlueprintUtil.toSXPBlueprint(json);
+
+		Assert.assertEquals(
+			sxpBlueprint.toString(),
+			String.valueOf(
+				SXPBlueprintUtil.toSXPBlueprint(sxpBlueprint.toString())));
+	}
+
+}
