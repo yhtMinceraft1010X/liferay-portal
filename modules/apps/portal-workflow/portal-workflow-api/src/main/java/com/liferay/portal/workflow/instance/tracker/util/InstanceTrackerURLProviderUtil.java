cb66f60949a554fd3a1c4dad7cb7fd2915f2994f
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-11-10 20:24:11 -0300

    LPS-141069 Add InstanceTrackerURLProviderUtil

---
+++ modules/apps/portal-workflow/portal-workflow-api/src/main/java/com/liferay/portal/workflow/instance/tracker/util/InstanceTrackerURLProviderUtil.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.instance.tracker.util;
+
+import com.liferay.osgi.util.ServiceTrackerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Feliphe Marinho
+ */
+public class InstanceTrackerURLProviderUtil {
+
+	public static String getURL(
+			Object bean, HttpServletRequest httpServletRequest, Class<?> model,
+			boolean useDialog)
+		throws Exception {
+
+		InstanceTrackerURLProvider instanceTrackerURLProvider =
+			_serviceTracker.getService();
+
+		return instanceTrackerURLProvider.getURL(
+			bean, httpServletRequest, model, useDialog);
+	}
+
+	private static final ServiceTracker
+		<InstanceTrackerURLProvider, InstanceTrackerURLProvider>
+			_serviceTracker = ServiceTrackerFactory.open(
+				FrameworkUtil.getBundle(InstanceTrackerURLProviderUtil.class),
+				InstanceTrackerURLProvider.class);
+
+}
