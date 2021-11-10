6ce7d83879aa5be1b9c664911fb1476d85335cf0
Jair Medeiros <jair.medeiros@liferay.com>
2021-11-10 17:45:31 -0300

    LPS-140150 Add page guard to redirect to right page

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/index.js
@@ -12,6 +12,27 @@ const getUserId = () => {
 	}
 };
 
+const getLiferaySiteName = () => {
+	let siteName = '/web/customer-portal';
+	try {
+
+		// eslint-disable-next-line no-undef
+		if (!themeDisplay) {
+			new Error('themeDisplay is not defined');
+		}
+
+		// eslint-disable-next-line no-undef
+		const {pathname} = new URL(themeDisplay.getCanonicalURL());
+		const pathSplit = pathname.split('/').filter(Boolean);
+		siteName = `/${pathSplit.slice(0, pathSplit.length - 1).join('/')}`;
+	} catch (error) {
+		console.warn('Not able to find Liferay PathName\n', error);
+	}
+
+	return siteName;
+};
+
 export const LiferayTheme = {
+	getLiferaySiteName,
     getUserId
 };
