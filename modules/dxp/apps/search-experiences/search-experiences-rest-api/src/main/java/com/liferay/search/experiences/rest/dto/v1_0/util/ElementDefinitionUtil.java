7bb8a1f873343f9b2e08c94307f9340b8fd1ae92
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-11-10 11:39:18 +0200

    LPS-142227 search-experiences-rest-api: Add helper methods to ElementDefinitionUtil

--- modules/dxp/apps/search-experiences/search-experiences-rest-api/src/main/java/com/liferay/search/experiences/rest/dto/v1_0/util/ElementDefinitionUtil.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-api/src/main/java/com/liferay/search/experiences/rest/dto/v1_0/util/ElementDefinitionUtil.java
@@ -14,6 +14,9 @@
 
 package com.liferay.search.experiences.rest.dto.v1_0.util;
 
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.search.experiences.rest.dto.v1_0.ElementDefinition;
 import com.liferay.search.experiences.rest.dto.v1_0.SXPBlueprint;
 
@@ -24,6 +27,22 @@ public class ElementDefinitionUtil {
 
 	public static ElementDefinition toElementDefinition(String json) {
 		return unpack(ElementDefinition.unsafeToDTO(json));
+	}
+
+	public static ElementDefinition[] toElementDefinitions(String json) {
+		if (json == null) {
+			return null;
+		}
+
+		try {
+			return JSONUtil.toArray(
+				JSONFactoryUtil.createJSONArray(json),
+				jsonObject -> toElementDefinition(jsonObject.toString()),
+				ElementDefinition.class);
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
 	}
 
 	public static ElementDefinition unpack(
@@ -41,6 +60,15 @@ public class ElementDefinitionUtil {
 		}
 
 		return elementDefinition;
+	}
+
+	public static ElementDefinition[] unpack(
+		ElementDefinition[] elementDefinitions) {
+
+		ArrayUtil.isNotEmptyForEach(
+			elementDefinitions, elementDefinition -> unpack(elementDefinition));
+
+		return elementDefinitions;
 	}
 
 }
