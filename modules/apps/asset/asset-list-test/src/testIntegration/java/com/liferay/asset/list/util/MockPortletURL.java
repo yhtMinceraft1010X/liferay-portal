1ec4b556bef9719e1c2fde3a8997506f447ebd7f
Yurena Cabrera <yurena.cabrera@liferay.com>
2021-11-10 18:17:38 +0000

    LPS-101448 Extracted MockPortletURL class, used in different test classes

---
+++ modules/apps/asset/asset-list-test/src/testIntegration/java/com/liferay/asset/list/util/MockPortletURL.java
@@ -0,0 +1,113 @@
+package com.liferay.asset.list.util;
+
+import com.liferay.asset.list.internal.portlet.action.test.AddAssetListEntryMVCActionCommandTest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
+
+import javax.portlet.MutableRenderParameters;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletModeException;
+import javax.portlet.PortletSecurityException;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderURL;
+import javax.portlet.WindowState;
+import javax.portlet.WindowStateException;
+import javax.portlet.annotations.PortletSerializable;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+public class MockPortletURL implements PortletURL, RenderURL {
+
+		@Override
+		public void addProperty(String key, String value) {
+		}
+
+		@Override
+		public Appendable append(Appendable appendable) throws IOException {
+			return null;
+		}
+
+		@Override
+		public Appendable append(Appendable appendable, boolean escapeXML)
+			throws IOException {
+
+			return null;
+		}
+
+		@Override
+		public String getFragmentIdentifier() {
+			return null;
+		}
+
+		@Override
+		public Map<String, String[]> getParameterMap() {
+			return null;
+		}
+
+		@Override
+		public PortletMode getPortletMode() {
+			return null;
+		}
+
+		@Override
+		public MutableRenderParameters getRenderParameters() {
+			return null;
+		}
+
+		@Override
+		public WindowState getWindowState() {
+			return null;
+		}
+
+		@Override
+		public void removePublicRenderParameter(String name) {
+		}
+
+		@Override
+		public void setBeanParameter(PortletSerializable portletSerializable) {
+		}
+
+		@Override
+		public void setFragmentIdentifier(String fragment) {
+		}
+
+		@Override
+		public void setParameter(String name, String value) {
+		}
+
+		@Override
+		public void setParameter(String name, String... values) {
+		}
+
+		@Override
+		public void setParameters(Map<String, String[]> map) {
+		}
+
+		@Override
+		public void setPortletMode(PortletMode portletMode)
+			throws PortletModeException {
+		}
+
+		@Override
+		public void setProperty(String key, String value) {
+		}
+
+		@Override
+		public void setSecure(boolean secure) throws PortletSecurityException {
+		}
+
+		@Override
+		public void setWindowState(WindowState windowState)
+			throws WindowStateException {
+		}
+
+		@Override
+		public void write(Writer writer) throws IOException {
+		}
+
+		@Override
+		public void write(Writer writer, boolean escapeXML) throws IOException {
+		}
+
+	}
+}
