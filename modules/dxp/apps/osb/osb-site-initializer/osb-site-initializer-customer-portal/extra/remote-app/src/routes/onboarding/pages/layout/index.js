68a8b8fff43e261b09d1f3686d0a7b250a648671
Jair Medeiros <jair.medeiros@liferay.com>
2021-11-10 17:51:44 -0300

    LPS-140150 Fix Layout to Skeleton Welcome Page

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/onboarding/pages/layout/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/onboarding/pages/layout/index.js
@@ -1,15 +1,12 @@
 import classNames from 'classnames';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-
-const Layout = ({ children, footerProps, headerProps, mainStyles }) => (
+const Layout = ({ children, footerProps, headerProps, headerSkeleton, mainStyles }) => (
   <div className="border d-flex flex-column mt-5 mx-auto onboarding rounded-lg shadow-lg">
-    <Header {...headerProps} />
-
+    {headerProps ? <Header {...headerProps} /> : headerSkeleton }
     <main className={classNames("flex-grow-1", "overflow-auto", mainStyles)}>{children}</main>
 
     <Footer {...footerProps} />
   </div>
 );
-
 export default Layout;
