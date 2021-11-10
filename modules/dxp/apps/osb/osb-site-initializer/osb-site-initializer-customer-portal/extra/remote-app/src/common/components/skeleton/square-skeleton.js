55bb9d59d51bb130530b1e1bae0ff5b128d1abac
Jair Medeiros <jair.medeiros@liferay.com>
2021-11-10 17:40:41 -0300

    LPS-141934  Add Skeletons to loading

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/components/skeleton/square-skeleton.js
@@ -0,0 +1,5 @@
+const SquareSkeleton = ({ height, width }) => {
+    return (<div className="skeleton" style={{ height: `${height}px`, width: `${width}px` }}></div>);
+}
+
+export default SquareSkeleton;
