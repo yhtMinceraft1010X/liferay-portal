a1df25ede5ddbbfabb4c565e6666b5653d8a7214
hai.yu <8219754@liferay.com>
2021-11-12 15:47:04 +0800

    LPS-84865 Add test to expose the issue.

--- modules/apps/mobile-device-rules/mobile-device-rules-test/src/testIntegration/java/com/liferay/mobile/device/rules/service/test/MDRRuleGroupLocalServiceTest.java
+++ modules/apps/mobile-device-rules/mobile-device-rules-test/src/testIntegration/java/com/liferay/mobile/device/rules/service/test/MDRRuleGroupLocalServiceTest.java
@@ -24,10 +24,19 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.ResourcePermission;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RoleTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -63,6 +72,63 @@ public class MDRRuleGroupLocalServiceTest {
 		_ruleGroup = MDRTestUtil.addRuleGroup(companyGroup.getGroupId());
 
 		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testCopyRuleGroup() throws Exception {
+		Role ownerRole = _roleLocalService.getRole(
+			_ruleGroup.getCompanyId(), RoleConstants.OWNER);
+
+		ResourcePermission oldResourcePermission =
+			_resourcePermissionLocalService.getResourcePermission(
+				_ruleGroup.getCompanyId(), MDRRuleGroup.class.getName(),
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(_ruleGroup.getPrimaryKey()),
+				ownerRole.getRoleId());
+
+		Assert.assertTrue(oldResourcePermission.isViewActionId());
+
+		RoleTestUtil.removeResourcePermission(
+			RoleConstants.OWNER, MDRRuleGroup.class.getName(),
+			ResourceConstants.SCOPE_INDIVIDUAL,
+			String.valueOf(_ruleGroup.getPrimaryKey()), ActionKeys.VIEW);
+
+		oldResourcePermission =
+			_resourcePermissionLocalService.getResourcePermission(
+				_ruleGroup.getCompanyId(), MDRRuleGroup.class.getName(),
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(_ruleGroup.getPrimaryKey()),
+				ownerRole.getRoleId());
+
+		Assert.assertFalse(oldResourcePermission.isViewActionId());
+
+		MDRRuleGroup newRuleGroup = _mdrRuleGroupLocalService.copyRuleGroup(
+			_ruleGroup, _ruleGroup.getGroupId(),
+			ServiceContextTestUtil.getServiceContext());
+
+		ResourcePermission newResourcePermission =
+			_resourcePermissionLocalService.getResourcePermission(
+				_ruleGroup.getCompanyId(), MDRRuleGroup.class.getName(),
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(newRuleGroup.getPrimaryKey()),
+				ownerRole.getRoleId());
+
+		Assert.assertFalse(newResourcePermission.isViewActionId());
+
+		Assert.assertNotEquals(oldResourcePermission, newResourcePermission);
+
+		Assert.assertEquals(
+			oldResourcePermission.getRoleId(),
+			newResourcePermission.getRoleId());
+		Assert.assertEquals(
+			oldResourcePermission.getOwnerId(),
+			newResourcePermission.getOwnerId());
+		Assert.assertEquals(
+			oldResourcePermission.getActionIds(),
+			newResourcePermission.getActionIds());
+		Assert.assertEquals(
+			oldResourcePermission.isViewActionId(),
+			newResourcePermission.isViewActionId());
 	}
 
 	@Test
@@ -109,6 +175,12 @@ public class MDRRuleGroupLocalServiceTest {
 
 	@Inject
 	private MDRRuleGroupLocalService _mdrRuleGroupLocalService;
+
+	@Inject
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Inject
+	private RoleLocalService _roleLocalService;
 
 	private MDRRuleGroup _ruleGroup;
 
