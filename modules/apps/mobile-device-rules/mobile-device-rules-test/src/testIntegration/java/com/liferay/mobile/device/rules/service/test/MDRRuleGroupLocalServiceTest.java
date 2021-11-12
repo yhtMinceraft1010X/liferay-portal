490e5a7169432d7f41cb4d072d1741e3eb76a700
hai.yu <8219754@liferay.com>
2021-11-12 15:08:23 +0800

    LPS-84865 SF, use Inject

--- modules/apps/mobile-device-rules/mobile-device-rules-test/src/testIntegration/java/com/liferay/mobile/device/rules/service/test/MDRRuleGroupLocalServiceTest.java
+++ modules/apps/mobile-device-rules/mobile-device-rules-test/src/testIntegration/java/com/liferay/mobile/device/rules/service/test/MDRRuleGroupLocalServiceTest.java
@@ -17,18 +17,19 @@ package com.liferay.mobile.device.rules.service.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.mobile.device.rules.model.MDRRuleGroup;
-import com.liferay.mobile.device.rules.service.MDRRuleGroupLocalServiceUtil;
+import com.liferay.mobile.device.rules.service.MDRRuleGroupLocalService;
 import com.liferay.mobile.device.rules.util.test.MDRTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.LinkedHashMap;
@@ -54,7 +55,7 @@ public class MDRRuleGroupLocalServiceTest {
 
 	@Before
 	public void setUp() throws Exception {
-		Company company = CompanyLocalServiceUtil.getCompany(
+		Company company = _companyLocalService.getCompany(
 			TestPropsValues.getCompanyId());
 
 		Group companyGroup = company.getGroup();
@@ -86,7 +87,7 @@ public class MDRRuleGroupLocalServiceTest {
 		}
 
 		List<MDRRuleGroup> ruleGroups =
-			MDRRuleGroupLocalServiceUtil.searchByKeywords(
+			_mdrRuleGroupLocalService.searchByKeywords(
 				layout.getGroupId(), StringPool.BLANK, params, false,
 				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 
@@ -100,8 +101,14 @@ public class MDRRuleGroupLocalServiceTest {
 		}
 	}
 
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
 	@DeleteAfterTestRun
 	private Group _group;
+
+	@Inject
+	private MDRRuleGroupLocalService _mdrRuleGroupLocalService;
 
 	private MDRRuleGroup _ruleGroup;
 
