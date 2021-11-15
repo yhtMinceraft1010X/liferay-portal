f4ddad9ce7c099221d5f1dbc3a1536af06ccb52e
Cheryl Tang <cheryl.tang@liferay.com>
2021-11-15 14:05:47 -0800

    COMMERCE-7815 Make commerce-tax-service MVCC enabled

---
+++ modules/apps/commerce/commerce-tax-service/src/main/java/com/liferay/commerce/tax/internal/upgrade/CommerceTaxServiceUpgradeStepRegistrator.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.tax.internal.upgrade;
+
+import com.liferay.portal.kernel.upgrade.MVCCVersionUpgradeProcess;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Cheryl Tang
+ */
+@Component(
+	enabled = false, immediate = true, service = UpgradeStepRegistrator.class
+)
+public class CommerceTaxServiceUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"1.0.0", "1.1.0",
+			new MVCCVersionUpgradeProcess() {
+
+				@Override
+				protected String[] getModuleTableNames() {
+					return new String[] {"CommerceTaxMethod"};
+				}
+
+			});
+	}
+
+}
