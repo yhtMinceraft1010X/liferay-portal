6dca03c694bb498c4cfe09778ff3ad03f95dae27
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2021-11-15 12:26:38 +0100

    COMMERCE-7826 commerce-warehouse-web Added screen navigations to warehouses

---
+++ modules/apps/commerce/commerce-warehouse-web/src/main/java/com/liferay/commerce/warehouse/web/internal/frontend/taglib/servlet/taglib/CommerceInventoryWarehouseDetailsScreenNavigationCategory.java
@@ -0,0 +1,170 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.warehouse.web.internal.frontend.taglib.servlet.taglib;
+
+import com.liferay.commerce.country.CommerceCountryManager;
+import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
+import com.liferay.commerce.inventory.service.CommerceInventoryWarehouseService;
+import com.liferay.commerce.product.service.CommerceChannelRelService;
+import com.liferay.commerce.product.service.CommerceChannelService;
+import com.liferay.commerce.warehouse.web.internal.display.context.CommerceInventoryWarehousesDisplayContext;
+import com.liferay.commerce.warehouse.web.internal.servlet.taglib.ui.constants.CommerceInventoryWarehouseScreenNavigationConstants;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.service.CountryService;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Sbarra
+ */
+@Component(
+	enabled = false,
+	property = {
+		"screen.navigation.category.order:Integer=10",
+		"screen.navigation.entry.order:Integer=10"
+	},
+	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+)
+public class CommerceInventoryWarehouseDetailsScreenNavigationCategory
+	implements ScreenNavigationCategory,
+			   ScreenNavigationEntry<CommerceInventoryWarehouse> {
+
+	@Override
+	public String getCategoryKey() {
+		return CommerceInventoryWarehouseScreenNavigationConstants.
+			CATEGORY_KEY_DETAILS;
+	}
+
+	@Override
+	public String getEntryKey() {
+		return CommerceInventoryWarehouseScreenNavigationConstants.
+			CATEGORY_KEY_DETAILS;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(
+			resourceBundle,
+			CommerceInventoryWarehouseScreenNavigationConstants.
+				CATEGORY_KEY_DETAILS);
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return CommerceInventoryWarehouseScreenNavigationConstants.
+			SCREEN_NAVIGATION_KEY_COMMERCE_INVENTORY_WAREHOUSE_GENERAL;
+	}
+
+	@Override
+	public boolean isVisible(
+		User user, CommerceInventoryWarehouse commerceInventoryWarehouse) {
+
+		if (commerceInventoryWarehouse == null) {
+			return false;
+		}
+
+		boolean hasPermission = false;
+
+		try {
+			hasPermission =
+				_commerceInventoryWarehouseModelResourcePermission.contains(
+					PermissionThreadLocal.getPermissionChecker(),
+					commerceInventoryWarehouse.
+						getCommerceInventoryWarehouseId(),
+					ActionKeys.UPDATE);
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception, exception);
+			}
+		}
+
+		return hasPermission;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		CommerceInventoryWarehousesDisplayContext
+			commerceInventoryWarehousesDisplayContext =
+				new CommerceInventoryWarehousesDisplayContext(
+					_commerceChannelRelService, _commerceChannelService,
+					_commerceCountryManager, _commerceInventoryWarehouseService,
+					_countryService, httpServletRequest);
+
+		httpServletRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			commerceInventoryWarehousesDisplayContext);
+
+		_jspRenderer.renderJSP(
+			httpServletRequest, httpServletResponse,
+			"/commerce_inventory_warehouse/details.jsp");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceInventoryWarehouseDetailsScreenNavigationCategory.class);
+
+	@Reference
+	private CommerceChannelRelService _commerceChannelRelService;
+
+	@Reference
+	private CommerceChannelService _commerceChannelService;
+
+	@Reference
+	private CommerceCountryManager _commerceCountryManager;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.commerce.inventory.model.CommerceInventoryWarehouse)"
+	)
+	private ModelResourcePermission<CommerceInventoryWarehouse>
+		_commerceInventoryWarehouseModelResourcePermission;
+
+	@Reference
+	private CommerceInventoryWarehouseService
+		_commerceInventoryWarehouseService;
+
+	@Reference
+	private CountryService _countryService;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+}
---
+++ modules/apps/commerce/commerce-warehouse-web/src/main/java/com/liferay/commerce/warehouse/web/internal/frontend/taglib/servlet/taglib/CommerceInventoryWarehouseQualifiersScreenNavigationCategory.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.warehouse.web.internal.frontend.taglib.servlet.taglib;
+
+import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
+import com.liferay.commerce.warehouse.web.internal.display.context.CommerceInventoryWarehouseQualifiersDisplayContext;
+import com.liferay.commerce.warehouse.web.internal.servlet.taglib.ui.constants.CommerceInventoryWarehouseScreenNavigationConstants;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Sbarra
+ */
+@Component(
+	enabled = false,
+	property = {
+		"screen.navigation.category.order:Integer=20",
+		"screen.navigation.entry.order:Integer=10"
+	},
+	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+)
+public class CommerceInventoryWarehouseQualifiersScreenNavigationCategory
+	implements ScreenNavigationCategory,
+			   ScreenNavigationEntry<CommerceInventoryWarehouse> {
+
+	@Override
+	public String getCategoryKey() {
+		return CommerceInventoryWarehouseScreenNavigationConstants.
+			CATEGORY_KEY_QUALIFIERS;
+	}
+
+	@Override
+	public String getEntryKey() {
+		return CommerceInventoryWarehouseScreenNavigationConstants.
+			CATEGORY_KEY_QUALIFIERS;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "eligibility");
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return CommerceInventoryWarehouseScreenNavigationConstants.
+			SCREEN_NAVIGATION_KEY_COMMERCE_INVENTORY_WAREHOUSE_GENERAL;
+	}
+
+	@Override
+	public boolean isVisible(
+		User user, CommerceInventoryWarehouse commerceInventoryWarehouse) {
+
+		if (commerceInventoryWarehouse == null) {
+			return false;
+		}
+
+		boolean hasPermission = false;
+
+		try {
+			hasPermission =
+				_commerceInventoryWarehouseModelResourcePermission.contains(
+					PermissionThreadLocal.getPermissionChecker(),
+					commerceInventoryWarehouse.
+						getCommerceInventoryWarehouseId(),
+					ActionKeys.UPDATE);
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception, exception);
+			}
+		}
+
+		return hasPermission;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		httpServletRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			new CommerceInventoryWarehouseQualifiersDisplayContext());
+
+		_jspRenderer.renderJSP(
+			httpServletRequest, httpServletResponse,
+			"/commerce_inventory_warehouse/qualifiers.jsp");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceInventoryWarehouseQualifiersScreenNavigationCategory.class);
+
+	@Reference(
+		target = "(model.class.name=com.liferay.commerce.inventory.model.CommerceInventoryWarehouse)"
+	)
+	private ModelResourcePermission<CommerceInventoryWarehouse>
+		_commerceInventoryWarehouseModelResourcePermission;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+}
