d73306f3a471581f2cee93d54a090e3f05894213
Erick Monteiro <erickhenriquehmc@gmail.com>
2021-11-16 13:56:44 -0300

    LPS-139992 (account-service): Adds jobTitle as parameter on local method addAccountEntryUserRel

--- modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryUserRelLocalServiceImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryUserRelLocalServiceImpl.java
@@ -124,7 +124,8 @@ public class AccountEntryUserRelLocalServiceImpl
 	public AccountEntryUserRel addAccountEntryUserRel(
 			long accountEntryId, long creatorUserId, String screenName,
 			String emailAddress, Locale locale, String firstName,
-			String middleName, String lastName, long prefixId, long suffixId)
+			String middleName, String lastName, long prefixId, long suffixId,
+			String jobTitle)
 		throws PortalException {
 
 		long companyId = CompanyThreadLocal.getCompanyId();
@@ -146,7 +147,6 @@ public class AccountEntryUserRelLocalServiceImpl
 		int birthdayMonth = Month.JANUARY.getValue();
 		int birthdayDay = 1;
 		int birthdayYear = 1970;
-		String jobTitle = null;
 		long[] groupIds = null;
 		long[] organizationIds = null;
 		long[] roleIds = null;
