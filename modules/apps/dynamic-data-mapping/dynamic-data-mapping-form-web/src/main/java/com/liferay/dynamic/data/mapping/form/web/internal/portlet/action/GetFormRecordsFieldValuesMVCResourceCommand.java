b1e50b06472e92b6f54f6e34bd917c50ace95750
Carolina Barbosa <carolina.barbosa@liferay.com>
2021-11-16 10:11:23 -0300

    LPS-142265 Create GetFormRecordsFieldValuesMVCResourceCommand for DDMForm portlet

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/GetFormRecordsFieldValuesMVCResourceCommand.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.portlet.action;
+
+import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalService;
+import com.liferay.dynamic.data.mapping.util.DDMFormReportDataUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carolina Barbosa
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + DDMPortletKeys.DYNAMIC_DATA_MAPPING_FORM,
+		"mvc.command.name=/dynamic_data_mapping_form/get_form_records_field_values"
+	},
+	service = MVCResourceCommand.class
+)
+public class GetFormRecordsFieldValuesMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse,
+			_getFieldValuesJSONArray(
+				_portal.getHttpServletRequest(resourceRequest)));
+	}
+
+	private JSONArray _getFieldValuesJSONArray(
+			HttpServletRequest httpServletRequest)
+		throws Exception {
+
+		String fieldName = ParamUtil.getString(httpServletRequest, "fieldName");
+
+		BaseModelSearchResult<DDMFormInstanceRecord> baseModelSearchResult =
+			_ddmFormInstanceRecordLocalService.searchFormInstanceRecords(
+				ParamUtil.getLong(httpServletRequest, "formInstanceId"),
+				new String[] {fieldName}, WorkflowConstants.STATUS_APPROVED,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				new Sort(Field.MODIFIED_DATE, Sort.LONG_TYPE, true));
+
+		return DDMFormReportDataUtil.getFieldValuesJSONArray(
+			baseModelSearchResult.getBaseModels(), fieldName);
+	}
+
+	@Reference
+	private DDMFormInstanceRecordLocalService
+		_ddmFormInstanceRecordLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
