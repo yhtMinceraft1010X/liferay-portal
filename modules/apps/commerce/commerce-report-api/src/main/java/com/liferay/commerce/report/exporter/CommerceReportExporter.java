1ec0106f4f1705cda8eaabd1de95938ac7044f60
Brian Chan <brian.chan@liferay.com>
2021-11-16 20:26:27 -0800

    COMMERCE-7310 Copy build.gradle code from modules/dxp/apps/portal-reports-engine-console/portal-reports-engine-console-jasper/build.gradle

---
+++ modules/apps/commerce/commerce-report-api/src/main/java/com/liferay/commerce/report/exporter/CommerceReportExporter.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.report.exporter;
+
+import aQute.bnd.annotation.ProviderType;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author Marco Leo
+ * @author Brian Wing Shun Chan
+ */
+@ProviderType
+public interface CommerceReportExporter {
+
+	public byte[] export(
+		Collection<?> beanCollection, Map<String, Object> parameters);
+
+}
---
+++ modules/apps/commerce/commerce-report-impl/src/main/java/com/liferay/commerce/report/internal/exporter/CommerceReportExporterImpl.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.report.internal.exporter;
+
+import com.liferay.commerce.report.exporter.CommerceReportExporter;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author Marco Leo
+ * @author Brian Wing Shun Chan
+ */
+public class CommerceReportExporterImpl implements CommerceReportExporter {
+
+	public byte[] export(
+		Collection<?> beanCollection, Map<String, Object> parameters) {
+
+		return null;
+	}
+
+}
