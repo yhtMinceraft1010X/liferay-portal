81210a3409e683023e36f601f27b9cdc215198c8
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-11-17 14:56:30 +0100

    LPS-143422 Rely for now on ServiceContext to change the title of the tab if there are no private pages

--- modules/apps/layout/layout-item-selector-web/src/main/java/com/liferay/layout/item/selector/web/internal/PublicLayoutsItemSelectorView.java
+++ modules/apps/layout/layout-item-selector-web/src/main/java/com/liferay/layout/item/selector/web/internal/PublicLayoutsItemSelectorView.java
@@ -15,6 +15,12 @@
 package com.liferay.layout.item.selector.web.internal;
 
 import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
@@ -41,8 +47,28 @@ public class PublicLayoutsItemSelectorView extends BaseLayoutsItemSelectorView {
 
 	@Override
 	public String getTitle(Locale locale) {
+		String title = "public-pages";
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext != null) {
+			try {
+				Group scopeGroup = serviceContext.getScopeGroup();
+
+				if (!scopeGroup.isPrivateLayoutsEnabled()) {
+					title = "pages";
+				}
+			}
+			catch (PortalException portalException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(portalException, portalException);
+				}
+			}
+		}
+
 		return ResourceBundleUtil.getString(
-			_portal.getResourceBundle(locale), "public-pages");
+			_portal.getResourceBundle(locale), title);
 	}
 
 	@Override
@@ -57,6 +83,9 @@ public class PublicLayoutsItemSelectorView extends BaseLayoutsItemSelectorView {
 	public void setServletContext(ServletContext servletContext) {
 		_servletContext = servletContext;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PublicLayoutsItemSelectorView.class);
 
 	@Reference
 	private Portal _portal;
