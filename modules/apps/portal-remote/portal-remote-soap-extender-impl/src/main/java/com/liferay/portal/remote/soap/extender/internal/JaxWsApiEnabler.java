630edc99a852478f4f31fedd582941de56e24232
Eric Yan <eric.yan@liferay.com>
2021-11-16 17:51:28 -0800

    LPS-142611 Remove timeout restriction for setting up a JAX-WS Provider

--- modules/apps/portal-remote/portal-remote-soap-extender-impl/src/main/java/com/liferay/portal/remote/soap/extender/internal/JaxWsApiEnabler.java
+++ modules/apps/portal-remote/portal-remote-soap-extender-impl/src/main/java/com/liferay/portal/remote/soap/extender/internal/JaxWsApiEnabler.java
@@ -30,6 +30,7 @@ import org.apache.cxf.BusFactory;
 import org.apache.cxf.jaxws22.spi.ProviderImpl;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -38,6 +39,7 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Carlos Sierra Andrés
@@ -50,53 +52,33 @@ public class JaxWsApiEnabler {
 
 	@Activate
 	protected void activate(
-			BundleContext bundleContext, Map<String, Object> properties)
-		throws InterruptedException {
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		_bundleContext = bundleContext;
 
 		JaxWsApiConfiguration jaxWsApiConfiguration =
 			ConfigurableUtil.createConfigurable(
 				JaxWsApiConfiguration.class, properties);
 
-		String contextPath = jaxWsApiConfiguration.contextPath();
+		_contextPath = jaxWsApiConfiguration.contextPath();
 
 		_serviceTracker = ServiceTrackerFactory.open(
 			bundleContext,
 			StringBundler.concat(
 				"(&(objectClass=org.apache.cxf.Bus)(",
 				HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_PATH, "=",
-				contextPath, "))"));
-
-		_bus = _serviceTracker.waitForService(jaxWsApiConfiguration.timeout());
-
-		if (_bus != null) {
-			BusFactory.setDefaultBus(_bus);
-
-			ProviderImpl providerImpl = new ProviderImpl();
-
-			Dictionary<String, Object> providerProperties = new Hashtable<>();
-
-			providerProperties.put(
-				HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_PATH,
-				contextPath);
-
-			_serviceRegistration = bundleContext.registerService(
-				Provider.class, providerImpl, providerProperties);
-		}
+				_contextPath, "))"),
+			new CXFBusServiceTrackerCustomizer());
 	}
 
 	@Deactivate
 	protected void deactivate() {
-		if (_serviceRegistration != null) {
-			_serviceRegistration.unregister();
-		}
-
 		_serviceTracker.close();
 	}
 
 	@Modified
 	protected void modified(
-			BundleContext bundleContext, Map<String, Object> properties)
-		throws InterruptedException {
+		BundleContext bundleContext, Map<String, Object> properties) {
 
 		deactivate();
 
@@ -105,8 +87,54 @@ public class JaxWsApiEnabler {
 		activate(bundleContext, properties);
 	}
 
+	private BundleContext _bundleContext;
 	private Bus _bus;
+	private String _contextPath;
 	private ServiceRegistration<Provider> _serviceRegistration;
 	private ServiceTracker<Bus, Bus> _serviceTracker;
+
+	private class CXFBusServiceTrackerCustomizer
+		implements ServiceTrackerCustomizer<Bus, Bus> {
+
+		@Override
+		public Bus addingService(ServiceReference<Bus> serviceReference) {
+			if (_bus == null) {
+				_bus = _bundleContext.getService(serviceReference);
+
+				BusFactory.setDefaultBus(_bus);
+
+				ProviderImpl providerImpl = new ProviderImpl();
+
+				Dictionary<String, Object> providerProperties =
+					new Hashtable<>();
+
+				providerProperties.put(
+					HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_PATH,
+					_contextPath);
+
+				_serviceRegistration = _bundleContext.registerService(
+					Provider.class, providerImpl, providerProperties);
+
+				return _bus;
+			}
+
+			return null;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<Bus> serviceReference, Bus bus) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<Bus> serviceReference, Bus bus) {
+
+			_serviceRegistration.unregister();
+
+			_bus = null;
+		}
+
+	}
 
 }
