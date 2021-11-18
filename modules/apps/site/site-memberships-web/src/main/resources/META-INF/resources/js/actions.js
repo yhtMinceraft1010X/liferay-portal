6b7f9e5145fa9e6e5a4dc1d138a1ddf1d48d38da
ChrisKian <christopher.kian@liferay.com>
2021-11-17 18:17:41 -0800

    LPS-130813 Using the 'getSelectedItemsOnly' param, we can now retrieve all elements and determine which boxes were explictly checked or unchecked.

--- modules/apps/site/site-memberships-web/src/main/resources/META-INF/resources/js/actions.js
+++ modules/apps/site/site-memberships-web/src/main/resources/META-INF/resources/js/actions.js
@@ -18,8 +18,9 @@ export const ACTIONS = {
 	assignRoles(itemData, portletNamespace) {
 		openSelectionModal({
 			buttonAddLabel: Liferay.Language.get('done'),
+			getSelectedItemsOnly: false,
 			multiple: true,
-			onSelect: (selectedItems) => {
+			onSelect: (items) => {
 				const editUserGroupRoleFm = document.getElementById(
 					`${portletNamespace}editUserGroupRoleFm`
 				);
@@ -28,12 +29,21 @@ export const ACTIONS = {
 					return;
 				}
 
-				const input = document.createElement('input');
+				const allItems = document.createElement('input');
 
-				input.name = `${portletNamespace}rowIds`;
-				input.value = selectedItems.map((item) => item.value);
+				allItems.name = `${portletNamespace}availableRowIds`;
+				allItems.value = items.map((item) => item.value);
 
-				editUserGroupRoleFm.appendChild(input);
+				editUserGroupRoleFm.appendChild(allItems);
+
+				const selectedItems = document.createElement('input');
+
+				selectedItems.name = `${portletNamespace}rowIds`;
+				selectedItems.value = items
+					.filter((item) => item.checked)
+					.map((item) => item.value);
+
+				editUserGroupRoleFm.appendChild(selectedItems);
 
 				submitForm(editUserGroupRoleFm, itemData.editUserGroupRoleURL);
 			},
