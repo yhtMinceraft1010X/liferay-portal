590f5d5f6a07cc58c8d21d7c1d2ba2201b5937d2
Brian Chan <brian.chan@liferay.com>
2021-11-19 08:33:34 -0800

    LPS-137635 Sort

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -1015,7 +1015,7 @@ public class ServicePreAction extends Action {
 						sourceGroupId);
 
 					if (sourceGroup.isUser() &&
-						(user.getUserId() != sourceGroup.getClassPK()) &&
+						(sourceGroup.getClassPK() != user.getUserId()) &&
 						!GroupPermissionUtil.contains(
 							permissionChecker, sourceGroup, ActionKeys.VIEW)) {
 
