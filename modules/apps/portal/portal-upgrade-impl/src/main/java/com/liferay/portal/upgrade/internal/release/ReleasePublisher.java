b7cc938359e77a92b16e01e44f72a3cf58dcc8a5
shuyangzhou <shuyang.zhou@liferay.com>
2021-11-20 10:44:49 -0800

    LPS-141790 Fix ReleasePublisher bug. When a release is deleted from db, its osgi servvice remains dangling until portal restart.

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/release/ReleasePublisher.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/release/ReleasePublisher.java
@@ -15,11 +15,15 @@
 package com.liferay.portal.upgrade.internal.release;
 
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.service.ReleaseLocalService;
+import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Dictionary;
@@ -40,8 +44,26 @@ import org.osgi.service.component.annotations.Reference;
  * @author Miguel Pastor
  * @author Carlos Sierra Andrés
  */
-@Component(immediate = true, service = ReleasePublisher.class)
-public final class ReleasePublisher {
+@Component(
+	immediate = true, service = {ModelListener.class, ReleasePublisher.class}
+)
+public final class ReleasePublisher extends BaseModelListener<Release> {
+
+	@Override
+	public void onAfterRemove(Release release) throws ModelListenerException {
+		TransactionCommitCallbackUtil.registerCallback(
+			() -> {
+				ServiceRegistration<Release> serviceRegistration =
+					_serviceConfiguratorRegistrations.remove(
+						release.getServletContextName());
+
+				if (serviceRegistration != null) {
+					serviceRegistration.unregister();
+				}
+
+				return null;
+			});
+	}
 
 	public void publish(Release release) {
 		ServiceRegistration<Release> oldServiceRegistration =
