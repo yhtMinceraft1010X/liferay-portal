9b63a8a7941868d190efcb18fe6862b5d02513e1
Nara Andrade <nara.andrade@liferay.com>
2021-11-22 15:47:24 -0300

    LPS-142439 Create node types and descriptions

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/nodes/utils.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import EndNode from './state/EndNode';
+import StartNode from './state/StartNode';
+import StateNode from './state/StateNode';
+
+const nodeDescription = {
+	end: Liferay.Language.get('conclude-the-workflow'),
+	start: Liferay.Language.get('begin-a-workflow'),
+	state: Liferay.Language.get('execute-actions-in-the-workflow'),
+};
+
+const nodeTypes = {
+	end: EndNode,
+	start: StartNode,
+	state: StateNode,
+};
+
+export {nodeDescription, nodeTypes};
