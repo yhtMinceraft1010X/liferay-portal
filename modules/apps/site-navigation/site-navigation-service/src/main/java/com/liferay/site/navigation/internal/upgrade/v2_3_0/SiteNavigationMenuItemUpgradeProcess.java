01c5d0086dca87df7953a37e236cba22958dde60
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-11-23 09:42:06 +0100

    LPS-142432 Adds an upgrade process to change the type of the display_page's site navigation menu items to a type that matches its class name

---
+++ modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/internal/upgrade/v2_3_0/SiteNavigationMenuItemUpgradeProcess.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.internal.upgrade.v2_3_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class SiteNavigationMenuItemUpgradeProcess extends UpgradeProcess {
+
+	public SiteNavigationMenuItemUpgradeProcess(Portal portal) {
+		_portal = portal;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement selectPreparedStatement =
+				connection.prepareStatement(
+					"select siteNavigationMenuItemId, typeSettings from " +
+						"SiteNavigationMenuItem where type_ = 'display_page'");
+			PreparedStatement updatePreparedStatement =
+				connection.prepareStatement(
+					"update SiteNavigationMenuItem set type_ = ? where " +
+						"siteNavigationMenuItemId = ?");
+			ResultSet resultSet = selectPreparedStatement.executeQuery()) {
+
+			while (resultSet.next()) {
+				long siteNavigationMenuItemId = resultSet.getLong(
+					"siteNavigationMenuItemId");
+
+				String typeSettings = resultSet.getString("typeSettings");
+
+				UnicodeProperties typeSettingsUnicodeProperties =
+					UnicodePropertiesBuilder.fastLoad(
+						typeSettings
+					).build();
+
+				long classNameId = GetterUtil.getLong(
+					typeSettingsUnicodeProperties.getProperty("classNameId"));
+
+				String newType = _portal.getClassName(classNameId);
+
+				updatePreparedStatement.setString(1, newType);
+
+				updatePreparedStatement.setLong(2, siteNavigationMenuItemId);
+
+				updatePreparedStatement.executeUpdate();
+			}
+		}
+	}
+
+	private final Portal _portal;
+
+}
