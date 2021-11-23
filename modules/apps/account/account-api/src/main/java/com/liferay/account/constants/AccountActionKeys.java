f6145c1aca3f4a5678afad3c8c18af9033ef0b31
pei-jung <pei-jung.lan@liferay.com>
2021-11-22 17:02:09 -0800

    LPS-142654 (account-api) - add new keys for AccountRole actions

--- modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountActionKeys.java
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountActionKeys.java
@@ -23,7 +23,11 @@ public class AccountActionKeys {
 
 	public static final String ADD_ACCOUNT_GROUP = "ADD_ACCOUNT_GROUP";
 
+	public static final String ADD_ACCOUNT_ROLE = "ADD_ACCOUNT_ROLE";
+
 	public static final String ASSIGN_ACCOUNTS = "ASSIGN_ACCOUNTS";
+
+	public static final String ASSIGN_USERS = "ASSIGN_USERS";
 
 	public static final String MANAGE_ACCOUNTS = "MANAGE_ACCOUNTS";
 
@@ -31,6 +35,8 @@ public class AccountActionKeys {
 
 	public static final String MANAGE_SUBORGANIZATIONS_ACCOUNTS =
 		"MANAGE_SUBORGANIZATIONS_ACCOUNTS";
+
+	public static final String VIEW_ACCOUNT_ROLES = "VIEW_ACCOUNT_ROLES";
 
 	public static final String VIEW_ACCOUNTS = "VIEW_ACCOUNTS";
 
