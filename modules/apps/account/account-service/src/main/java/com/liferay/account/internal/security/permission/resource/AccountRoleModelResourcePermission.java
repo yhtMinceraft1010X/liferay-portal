1d5116372e680d1b978778da05c405603240101b
pei-jung <pei-jung.lan@liferay.com>
2021-11-22 16:59:48 -0800

    LPS-142654 (account-service) - implement model resource permission for AccountRole

---
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/security/permission/resource/AccountRoleModelResourcePermission.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.internal.security.permission.resource;
+
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.model.AccountRole;
+import com.liferay.account.service.AccountRoleLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.account.model.AccountRole",
+	service = ModelResourcePermission.class
+)
+public class AccountRoleModelResourcePermission
+	implements ModelResourcePermission<AccountRole> {
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, AccountRole accountRole,
+			String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, accountRole, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, AccountRole.class.getName(),
+				accountRole.getAccountRoleId(), actionId);
+		}
+	}
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, long accountRoleId,
+			String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, accountRoleId, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, AccountRole.class.getName(), accountRoleId,
+				actionId);
+		}
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, AccountRole accountRole,
+			String actionId)
+		throws PortalException {
+
+		return contains(
+			permissionChecker, accountRole.getAccountRoleId(), actionId);
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, long accountRoleId,
+			String actionId)
+		throws PortalException {
+
+		AccountRole accountRole = _accountRoleLocalService.fetchAccountRole(
+			accountRoleId);
+
+		if (accountRole != null) {
+			Role role = accountRole.getRole();
+
+			if (permissionChecker.hasOwnerPermission(
+					permissionChecker.getCompanyId(),
+					AccountRole.class.getName(), accountRoleId,
+					role.getUserId(), actionId)) {
+
+				return true;
+			}
+		}
+
+		return permissionChecker.hasPermission(
+			null, AccountRole.class.getName(), accountRoleId, actionId);
+	}
+
+	@Override
+	public String getModelName() {
+		return AccountRole.class.getName();
+	}
+
+	@Override
+	public PortletResourcePermission getPortletResourcePermission() {
+		return _portletResourcePermission;
+	}
+
+	@Reference
+	private AccountRoleLocalService _accountRoleLocalService;
+
+	@Reference(
+		target = "(resource.name=" + AccountConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+}
