43749a0d4f5b9a76ccd5adca1bbdd48d1136d6bc
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-11-24 13:46:59 +0100

    LPS-142295 Create sample Vanilla Counter remote app on DB creation
    We create a sample row pointing to our Vanilla Counter app per company,
    but only when the table is empty (ie: on fresh installations).
    
    Note that this will recreate the sample if it is deleted, no other
    remote app is created and portal is restarted. PM thinks this is a fair
    behaviour but, if we see it creates any problem, we may change it in
    the future.

---
+++ modules/apps/remote-app/remote-app-service/src/main/java/com/liferay/remote/app/internal/instance/lifecycle/RemoteAppPortalInstanceLifecycleListener.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.remote.app.internal.instance.lifecycle;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.settings.LocalizedValuesMap;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.remote.app.service.RemoteAppEntryLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@Component(immediate = true, service = PortalInstanceLifecycleListener.class)
+public class RemoteAppPortalInstanceLifecycleListener
+	extends BasePortalInstanceLifecycleListener {
+
+	public void portalInstanceRegistered(Company company) throws Exception {
+		long count = _remoteAppEntryLocalService.countByCompanyId(
+			company.getCompanyId());
+
+		if (count != 0) {
+			return;
+		}
+
+		_remoteAppEntryLocalService.addCustomElementRemoteAppEntry(
+			_userLocalService.getDefaultUserId(company.getCompanyId()),
+			StringPool.BLANK, "vanilla-counter",
+			"https://liferay.github.io/liferay-frontend-projects" +
+				"/vanilla-counter/index.js",
+			StringPool.BLANK, "vanilla_counter", false,
+			LocalizationUtil.getMap(new LocalizedValuesMap("Vanilla Counter")),
+			"category.remote-apps", "friendly-url-mapping=vanilla_counter",
+			"https://liferay.github.io/liferay-frontend-projects");
+	}
+
+	@Reference
+	private RemoteAppEntryLocalService _remoteAppEntryLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
