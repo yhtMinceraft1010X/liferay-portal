3edafc73cc284758c72633f6b310acffbb092147
Tibor Lipusz <tibor.lipusz@liferay.com>
2021-11-25 12:39:12 +0100

    LPS-142948 search-experiences-web: Remove ModulePortalProfile which is conflicting with the enteprise module activation logic preventing the @Components of the -web modules to be enabled
    https://issues.liferay.com/browse/LPS-142948
    
    Signed-off-by: Tibor Lipusz <tibor.lipusz@liferay.com>

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/power/tools/portlet/SXPPowerToolsPortlet.java
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/power/tools/portlet/SXPPowerToolsPortlet.java
@@ -40,4 +40,7 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class SXPPowerToolsPortlet extends MVCPortlet {
+
+	// TODO: Add FF or other method to enable/disable the related @Components.
+
 }
