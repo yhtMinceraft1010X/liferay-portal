8602cba7821b06ef0f5af575b88b3fcd30a54997
Erick Monteiro <erickhenriquehmc@gmail.com>
2021-11-25 14:26:28 -0300

    LPS-143044 (headless-admin-user-impl): Adds UserGroupResourceDTOConverter

---
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/UserGroupResourceDTOConverter.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.user.internal.dto.v1_0.converter;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.service.UserGroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Erick Monteiro
+ */
+@Component(
+	property = "dto.class.name=com.liferay.portal.kernel.model.UserGroup",
+	service = {DTOConverter.class, UserGroupResourceDTOConverter.class}
+)
+public class UserGroupResourceDTOConverter
+	implements DTOConverter
+		<UserGroup, com.liferay.headless.admin.user.dto.v1_0.UserGroup> {
+
+	@Override
+	public String getContentType() {
+		return com.liferay.headless.admin.user.dto.v1_0.UserGroup.class.
+			getSimpleName();
+	}
+
+	@Override
+	public com.liferay.headless.admin.user.dto.v1_0.UserGroup toDTO(
+			UserGroup userGroup)
+		throws PortalException {
+
+		if (userGroup == null) {
+			return null;
+		}
+
+		return new com.liferay.headless.admin.user.dto.v1_0.UserGroup() {
+			{
+				description = userGroup.getDescription();
+				id = userGroup.getUserGroupId();
+				name = userGroup.getName();
+				usersCount = _userLocalService.getUserGroupUsersCount(
+					userGroup.getUserGroupId(),
+					WorkflowConstants.STATUS_APPROVED);
+			}
+		};
+	}
+
+	@Reference
+	private UserGroupLocalService _userGroupLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
