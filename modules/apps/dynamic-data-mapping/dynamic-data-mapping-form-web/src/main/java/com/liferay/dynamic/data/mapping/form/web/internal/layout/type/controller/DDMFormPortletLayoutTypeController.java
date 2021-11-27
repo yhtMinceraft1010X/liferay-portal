02bd5dd8eea543e01d1ae4e29fb5713e0d7a2b77
Alan Huang <alan.huang@liferay.com>
2021-11-27 14:20:50 +0800

    LPS-142890 Auto SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/layout/type/controller/DDMFormPortletLayoutTypeController.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/layout/type/controller/DDMFormPortletLayoutTypeController.java
@@ -84,6 +84,15 @@ public class DDMFormPortletLayoutTypeController
 		return true;
 	}
 
+	@Override
+	protected ServletResponse createServletResponse(
+		HttpServletResponse httpServletResponse,
+		UnsyncStringWriter unsyncStringWriter) {
+
+		return new PipingServletResponse(
+			httpServletResponse, unsyncStringWriter);
+	}
+
 	/**
 	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
 	 *             #createServletResponse(HttpServletResponse,
@@ -95,15 +104,6 @@ public class DDMFormPortletLayoutTypeController
 		HttpServletResponse httpServletResponse,
 		com.liferay.portal.kernel.io.unsync.UnsyncStringWriter
 			unsyncStringWriter) {
-
-		return new PipingServletResponse(
-			httpServletResponse, unsyncStringWriter);
-	}
-
-	@Override
-	protected ServletResponse createServletResponse(
-		HttpServletResponse httpServletResponse,
-		UnsyncStringWriter unsyncStringWriter) {
 
 		return new PipingServletResponse(
 			httpServletResponse, unsyncStringWriter);
