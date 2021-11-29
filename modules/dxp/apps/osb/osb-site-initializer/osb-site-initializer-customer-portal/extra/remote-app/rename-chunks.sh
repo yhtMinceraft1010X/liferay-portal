60c4f4e63280ce019e6820ead6dce5665a469787
Jair Medeiros <jair.medeiros@liferay.com>
2021-11-29 16:58:19 -0300

    LPS-143055 Remove rename-chunks.sh

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/rename-chunks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ORIGIN=build/static
-
-mv $ORIGIN/js/main*.js $ORIGIN/js/main.js
-mv $ORIGIN/js/2*.chunk.js $ORIGIN/js/2.js
-mv $ORIGIN/js/runtime-main*.js $ORIGIN/js/runtime-main.js
-mv $ORIGIN/css/main*.css $ORIGIN/css/main.css
