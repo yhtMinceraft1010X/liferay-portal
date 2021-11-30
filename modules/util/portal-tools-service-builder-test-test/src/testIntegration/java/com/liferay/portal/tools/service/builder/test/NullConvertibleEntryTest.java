6fd5ec2b5908daccf9fa55e8f8b0dc554a8701e5
Kyle Miho <kyle.miho@liferay.com>
2021-11-30 15:17:16 -0800

    LRQA-71581 Add integration tests to cover issues with null convertible strings

---
+++ modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/NullConvertibleEntryTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.service.builder.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.TransactionalTestRule;
+import com.liferay.portal.tools.service.builder.test.model.NullConvertibleEntry;
+import com.liferay.portal.tools.service.builder.test.service.NullConvertibleEntryLocalService;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Kyle Miho
+ */
+@RunWith(Arquillian.class)
+public class NullConvertibleEntryTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			new TransactionalTestRule(
+				Propagation.REQUIRED,
+				"com.liferay.portal.tools.service.builder.test.service"));
+
+	@Test
+	public void testCountNullConvertibleString() {
+		int initialCount = _nullConvertibleEntryLocalService.countByName(null);
+
+		_nullConvertibleEntryLocalService.addNullConvertibleEntry((String)null);
+
+		int actualCount = _nullConvertibleEntryLocalService.countByName(null);
+
+		Assert.assertEquals(initialCount + 1, actualCount);
+	}
+
+	@Test
+	public void testFetchNullConvertibleString() {
+		_nullConvertibleEntryLocalService.addNullConvertibleEntry((String)null);
+
+		NullConvertibleEntry nullConvertibleEntry =
+			_nullConvertibleEntryLocalService.fetchByName(null);
+
+		Assert.assertNotNull(nullConvertibleEntry);
+	}
+
+	@Inject
+	private NullConvertibleEntryLocalService _nullConvertibleEntryLocalService;
+
+}
