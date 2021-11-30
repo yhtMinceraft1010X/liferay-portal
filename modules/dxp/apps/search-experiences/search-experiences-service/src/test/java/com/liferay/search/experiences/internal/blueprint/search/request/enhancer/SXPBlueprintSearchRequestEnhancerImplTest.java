8228908a0f3c018851f078edcbd98a038e0fb98a
Brian Chan <brian.chan@liferay.com>
2021-11-29 20:59:13 -0800

    LPS-143195 Sort

--- modules/dxp/apps/search-experiences/search-experiences-service/src/test/java/com/liferay/search/experiences/internal/blueprint/search/request/enhancer/SXPBlueprintSearchRequestEnhancerImplTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/test/java/com/liferay/search/experiences/internal/blueprint/search/request/enhancer/SXPBlueprintSearchRequestEnhancerImplTest.java
@@ -172,12 +172,13 @@ public class SXPBlueprintSearchRequestEnhancerImplTest {
 		Assert.assertTrue(MapUtil.isEmpty(searchRequest.getAggregationsMap()));
 		Assert.assertTrue(
 			ListUtil.isEmpty(searchRequest.getComplexQueryParts()));
-		Assert.assertTrue(ListUtil.isEmpty(searchRequest.getSorts()));
 
 		com.liferay.portal.search.highlight.Highlight highlight =
 			searchRequest.getHighlight();
 
 		Assert.assertTrue(ListUtil.isEmpty(highlight.getFieldConfigs()));
+
+		Assert.assertTrue(ListUtil.isEmpty(searchRequest.getSorts()));
 
 		_assert(sxpBlueprint);
 	}
