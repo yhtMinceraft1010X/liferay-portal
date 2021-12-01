d8cbc2ee951daf37ca29f2c99efffdb29cabd37d
Mateus Santana <mateus.santana@liferay.com>
2021-12-01 08:50:47 -0300

    LPS-143228 ListType fields must save optionsReferences as HashMap

--- modules/apps/object/object-dynamic-data-mapping/src/main/java/com/liferay/object/dynamic/data/mapping/internal/storage/ObjectDDMStorageAdapter.java
+++ modules/apps/object/object-dynamic-data-mapping/src/main/java/com/liferay/object/dynamic/data/mapping/internal/storage/ObjectDDMStorageAdapter.java
@@ -40,6 +40,7 @@ import com.liferay.object.scope.ObjectScopeProviderRegistry;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryService;
 import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.util.ObjectFieldUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -51,6 +52,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
@@ -303,6 +305,9 @@ public class ObjectDDMStorageAdapter implements DDMStorageAdapter {
 		Map<String, String> objectFieldTypes = stream.collect(
 			Collectors.toMap(ObjectField::getName, ObjectField::getType));
 
+		Map<String, ObjectField> objectFieldsMap =
+			ObjectFieldUtil.getObjectFieldsMap(objectFields);
+
 		for (DDMFormFieldValue ddmFormFieldValue : ddmFormFieldValues) {
 			if (StringUtil.equals(
 					ddmFormFieldValue.getType(),
@@ -320,11 +325,25 @@ public class ObjectDDMStorageAdapter implements DDMStorageAdapter {
 
 				Value value = ddmFormFieldValue.getValue();
 
-				properties.put(
-					objectFieldName,
-					_getOptionReferenceValue(
-						ddmFormFieldValue, ddmFormFieldsMap, objectFieldName,
-						objectFieldTypes, value));
+				ObjectField objectField = objectFieldsMap.get(objectFieldName);
+
+				if (objectField.getListTypeDefinitionId() > 0) {
+					properties.put(
+						objectFieldName,
+						HashMapBuilder.put(
+							"key",
+							_getOptionReferenceValue(
+								ddmFormFieldValue, ddmFormFieldsMap,
+								objectFieldName, objectFieldTypes, value)
+						).build());
+				}
+				else {
+					properties.put(
+						objectFieldName,
+						_getOptionReferenceValue(
+							ddmFormFieldValue, ddmFormFieldsMap,
+							objectFieldName, objectFieldTypes, value));
+				}
 			}
 		}
 
