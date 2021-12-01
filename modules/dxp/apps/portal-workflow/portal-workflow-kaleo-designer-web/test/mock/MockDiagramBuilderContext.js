f19c87205fabf1540378aa69583256d00438a0ef
Nara Andrade <nara.andrade@liferay.com>
2021-12-01 20:22:19 -0300

    LPS-142442 Create mocks for components' contexts

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/test/mock/MockDiagramBuilderContext.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import React, {useState} from 'react';
+
+import {DiagramBuilderContextProvider} from '../../src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/DiagramBuilderContext';
+import {defaultNodes} from '../../src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/nodes/utils';
+
+export default function MockDiagramBuilderContext({
+	children,
+	mockSelectedNode = null,
+}) {
+	const [availableArea] = useState(null);
+	const [, setElements] = useState(defaultNodes);
+	const [selectedNode, setSelectedNode] = useState(mockSelectedNode);
+	const [selectedNodeNewId, setSelectedNodeNewId] = useState(null);
+
+	const contextProps = {
+		availableArea,
+		selectedNode,
+		selectedNodeNewId,
+		setElements,
+		setSelectedNode,
+		setSelectedNodeNewId,
+	};
+
+	return (
+		<DiagramBuilderContextProvider {...contextProps}>
+			{children}
+		</DiagramBuilderContextProvider>
+	);
+}
