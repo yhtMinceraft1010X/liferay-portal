9d41357b14b8c2fb5362ae6a84f508996b973fc0
Brian Chan <brian.chan@liferay.com>
2021-12-01 20:06:20 -0800

    LPS-143202 do we really need Modified?

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/instance/lifecycle/SXPPortalInstanceLifecycleListener.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/instance/lifecycle/SXPPortalInstanceLifecycleListener.java
@@ -67,7 +67,6 @@ public class SXPPortalInstanceLifecycleListener
 	}
 
 	@Activate
-	@Modified
 	protected void activate(Map<String, Object> properties) {
 		try {
 			_addSXPElements(_companyLocalService.getCompanies());
