e88c486904e892f13cf71e09f362e31a5f53ba91
Hugo Huijser <hugo.huijser@liferay.com>
2021-12-03 09:44:13 -0700

    LPS-143349 Find method calls to methods that no longer exist in upgrade version

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/UpgradeRemovedAPICheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/UpgradeRemovedAPICheck.java
@@ -15,16 +15,22 @@
 package com.liferay.source.formatter.checkstyle.checks;
 
 import com.liferay.portal.json.JSONObjectImpl;
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.source.formatter.util.SourceFormatterUtil;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import com.puppycrawl.tools.checkstyle.utils.AnnotationUtil;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Hugo Huijser
@@ -57,32 +63,80 @@ public class UpgradeRemovedAPICheck extends BaseCheck {
 			JSONObject upgradeToJavaClassesJSONObject =
 				_getJavaClassesJSONObject(upgradeToVersion);
 
-			_checkRemovedImportNames(
+			List<String> removedImportNames = _getRemovedImportNames(
 				detailAST, upgradeFromJavaClassesJSONObject,
+				upgradeToJavaClassesJSONObject, upgradeToVersion);
+
+			_checkRemovedMethods(
+				detailAST, removedImportNames, upgradeFromJavaClassesJSONObject,
 				upgradeToJavaClassesJSONObject, upgradeToVersion);
 		}
 		catch (Exception exception) {
 		}
 	}
 
-	private void _checkRemovedImportNames(
-		DetailAST detailAST, JSONObject fromUpgradeJavaClassesJSONObject,
-		JSONObject toUpgradeJavaClassesJSONObject, String upgradeToVersion) {
+	private void _checkRemovedMethods(
+		DetailAST detailAST, List<String> removedImportNames,
+		JSONObject upgradeFromJavaClassesJSONObject,
+		JSONObject upgradeToJavaClassesJSONObject, String upgradeToVersion) {
 
-		List<String> importNames = getImportNames(detailAST);
+		List<DetailAST> methodCallDetailASTList = getAllChildTokens(
+			detailAST, true, TokenTypes.METHOD_CALL);
 
-		for (String importName : importNames) {
-			JSONObject fromUpgradeClassJSONObject =
-				fromUpgradeJavaClassesJSONObject.getJSONObject(importName);
-			JSONObject toUpgradeClassJSONObject =
-				toUpgradeJavaClassesJSONObject.getJSONObject(importName);
+		for (DetailAST methodCallDetailAST : methodCallDetailASTList) {
+			DetailAST dotDetailAST = methodCallDetailAST.getFirstChild();
 
-			if ((fromUpgradeClassJSONObject != null) &&
-				(toUpgradeClassJSONObject == null)) {
+			if (dotDetailAST.getType() != TokenTypes.DOT) {
+				continue;
+			}
+
+			DetailAST firstChildDetailAST = dotDetailAST.getFirstChild();
+			DetailAST lastChildDetailAST = dotDetailAST.getLastChild();
+
+			if ((firstChildDetailAST.getType() != TokenTypes.IDENT) ||
+				(lastChildDetailAST.getType() != TokenTypes.IDENT)) {
+
+				continue;
+			}
+
+			String variableTypeName = getVariableTypeName(
+				firstChildDetailAST, firstChildDetailAST.getText(), false,
+				true);
+
+			if (!variableTypeName.startsWith("com.liferay.") ||
+				removedImportNames.contains(variableTypeName)) {
+
+				continue;
+			}
+
+			String methodName = lastChildDetailAST.getText();
+
+			List<String> parameterTypes = _getParameterTypes(
+				methodCallDetailAST);
+
+			MethodStatus fromUpgradeMethodStatus = _getMethodStatus(
+				upgradeFromJavaClassesJSONObject, variableTypeName, methodName,
+				parameterTypes);
+
+			if (!fromUpgradeMethodStatus.equals(MethodStatus.METHOD_FOUND)) {
+				continue;
+			}
+
+			MethodStatus toUpgradeMethodStatus = _getMethodStatus(
+				upgradeToJavaClassesJSONObject, variableTypeName, methodName,
+				parameterTypes);
+
+			if (toUpgradeMethodStatus.equals(MethodStatus.NO_CLASS_FOUND)) {
+				log(
+					methodCallDetailAST, _MSG_CLASS_NOT_FOUND, variableTypeName,
+					upgradeToVersion);
+			}
+			else if (toUpgradeMethodStatus.equals(
+						MethodStatus.NO_METHOD_FOUND)) {
 
 				log(
-					detailAST, _MSG_CLASS_NOT_FOUND, importName,
-					upgradeToVersion);
+					methodCallDetailAST, _MSG_METHOD_NOT_FOUND, methodName,
+					variableTypeName, upgradeToVersion);
 			}
 		}
 	}
@@ -113,9 +167,276 @@ public class UpgradeRemovedAPICheck extends BaseCheck {
 		return javaClassesJSONObject;
 	}
 
+	private MethodStatus _getMethodStatus(
+		JSONObject jsonObject, String fullyQualifiedClassName,
+		String methodName, List<String> parameterTypes) {
+
+		JSONObject classJSONObject = jsonObject.getJSONObject(
+			fullyQualifiedClassName);
+
+		if (classJSONObject == null) {
+			return MethodStatus.NO_CLASS_FOUND;
+		}
+
+		JSONArray methodsJSONArray = classJSONObject.getJSONArray("methods");
+
+		if (methodsJSONArray != null) {
+			Iterator<JSONObject> iterator = methodsJSONArray.iterator();
+
+			outerLoop:
+			while (iterator.hasNext()) {
+				JSONObject methodJSONObject = iterator.next();
+
+				if (!methodName.equals(methodJSONObject.getString("name"))) {
+					continue;
+				}
+
+				JSONArray parametersJSONArray = methodJSONObject.getJSONArray(
+					"parameters");
+
+				if (parametersJSONArray == null) {
+					if (parameterTypes.isEmpty()) {
+						return MethodStatus.METHOD_FOUND;
+					}
+
+					continue;
+				}
+
+				if (parametersJSONArray.length() != parameterTypes.size()) {
+					continue;
+				}
+
+				for (int i = 0; i < parameterTypes.size(); i++) {
+					String actualType = parameterTypes.get(i);
+					String methodType = parametersJSONArray.getString(i);
+
+					if (!StringUtil.equalsIgnoreCase(actualType, methodType) &&
+						!methodType.equals("Object")) {
+
+						continue outerLoop;
+					}
+				}
+
+				return MethodStatus.METHOD_FOUND;
+			}
+		}
+
+		JSONArray extendedClassNamesJSONArray = classJSONObject.getJSONArray(
+			"extendedClassNames");
+
+		if (extendedClassNamesJSONArray != null) {
+			Iterator<String> iterator = extendedClassNamesJSONArray.iterator();
+
+			while (iterator.hasNext()) {
+				String extendedClassName = iterator.next();
+
+				if (!extendedClassName.startsWith("com.liferay.") &&
+					Objects.equals(
+						_getMethodStatus(
+							jsonObject, extendedClassName, methodName,
+							parameterTypes),
+						MethodStatus.METHOD_FOUND)) {
+
+					return MethodStatus.METHOD_FOUND;
+				}
+			}
+		}
+
+		return MethodStatus.NO_METHOD_FOUND;
+	}
+
+	private String _getParameterType(DetailAST detailAST) {
+		if (detailAST.getType() == TokenTypes.IDENT) {
+			return getVariableTypeName(
+				detailAST, detailAST.getText(), true, true);
+		}
+
+		if (detailAST.getType() == TokenTypes.STRING_LITERAL) {
+			return "java.lang.String";
+		}
+
+		if (detailAST.getType() == TokenTypes.CHAR_LITERAL) {
+			return "char";
+		}
+
+		if ((detailAST.getType() == TokenTypes.LITERAL_FALSE) ||
+			(detailAST.getType() == TokenTypes.LITERAL_TRUE) ||
+			(detailAST.getType() == TokenTypes.LNOT)) {
+
+			return "boolean";
+		}
+
+		if (detailAST.getType() == TokenTypes.PLUS) {
+			DetailAST stringLiteralDetailAST = detailAST.findFirstToken(
+				TokenTypes.STRING_LITERAL);
+
+			if (stringLiteralDetailAST != null) {
+				return "java.lang.String";
+			}
+
+			return null;
+		}
+
+		if (detailAST.getType() == TokenTypes.LITERAL_NEW) {
+			String parameterType = null;
+
+			DetailAST firstChildDetailAST = detailAST.getFirstChild();
+
+			if (firstChildDetailAST.getType() == TokenTypes.IDENT) {
+				parameterType = firstChildDetailAST.getText();
+			}
+			else {
+				parameterType = _getParameterType(firstChildDetailAST);
+			}
+
+			if (parameterType == null) {
+				return parameterType;
+			}
+
+			DetailAST curDetailAST = firstChildDetailAST.getNextSibling();
+
+			while (true) {
+				if (curDetailAST.getType() != TokenTypes.ARRAY_DECLARATOR) {
+					return parameterType;
+				}
+
+				parameterType += "[]";
+
+				curDetailAST = curDetailAST.getFirstChild();
+			}
+		}
+
+		if (detailAST.getType() == TokenTypes.INDEX_OP) {
+			String parameterType = _getParameterType(detailAST.getFirstChild());
+
+			if (parameterType != null) {
+				parameterType = StringUtil.replaceLast(parameterType, "[]", "");
+			}
+
+			return parameterType;
+		}
+
+		if (detailAST.getType() == TokenTypes.TYPECAST) {
+			return getTypeName(detailAST, true, true);
+		}
+
+		if (ArrayUtil.contains(
+				UNARY_OPERATOR_TOKEN_TYPES, detailAST.getType())) {
+
+			return _getParameterType(detailAST.getFirstChild());
+		}
+
+		if (detailAST.getType() == TokenTypes.LITERAL_THIS) {
+			DetailAST parentDetailAST = getParentWithTokenType(
+				detailAST, TokenTypes.CLASS_DEF);
+
+			if (parentDetailAST == null) {
+				return null;
+			}
+
+			DetailAST identDetailAST = parentDetailAST.findFirstToken(
+				TokenTypes.IDENT);
+
+			return identDetailAST.getText();
+		}
+
+		if (ArrayUtil.contains(
+				CONDITIONAL_OPERATOR_TOKEN_TYPES, detailAST.getType()) ||
+			ArrayUtil.contains(
+				RELATIONAL_OPERATOR_TOKEN_TYPES, detailAST.getType())) {
+
+			return "boolean";
+		}
+
+		if (detailAST.getType() == TokenTypes.NUM_DOUBLE) {
+			return "double";
+		}
+
+		if (detailAST.getType() == TokenTypes.NUM_FLOAT) {
+			return "float";
+		}
+
+		if (detailAST.getType() == TokenTypes.NUM_LONG) {
+			return "long";
+		}
+
+		if (detailAST.getType() == TokenTypes.NUM_INT) {
+			return "int";
+		}
+
+		if ((detailAST.getType() == TokenTypes.LITERAL_BOOLEAN) ||
+			(detailAST.getType() == TokenTypes.LITERAL_BYTE) ||
+			(detailAST.getType() == TokenTypes.LITERAL_DOUBLE) ||
+			(detailAST.getType() == TokenTypes.LITERAL_FLOAT) ||
+			(detailAST.getType() == TokenTypes.LITERAL_INT) ||
+			(detailAST.getType() == TokenTypes.LITERAL_LONG) ||
+			(detailAST.getType() == TokenTypes.LITERAL_SHORT)) {
+
+			return detailAST.getText();
+		}
+
+		if (detailAST.getType() == TokenTypes.QUESTION) {
+			return _getParameterType(detailAST.getLastChild());
+		}
+
+		return null;
+	}
+
+	private List<String> _getParameterTypes(DetailAST detailAST) {
+		List<String> parameterTypes = new ArrayList<>();
+
+		DetailAST elistDetailAST = detailAST.findFirstToken(TokenTypes.ELIST);
+
+		List<DetailAST> exprDetailASTList = getAllChildTokens(
+			elistDetailAST, false, TokenTypes.EXPR);
+
+		for (DetailAST exprDetailAST : exprDetailASTList) {
+			parameterTypes.add(
+				_getParameterType(exprDetailAST.getFirstChild()));
+		}
+
+		return parameterTypes;
+	}
+
+	private List<String> _getRemovedImportNames(
+		DetailAST detailAST, JSONObject fromUpgradeJavaClassesJSONObject,
+		JSONObject toUpgradeJavaClassesJSONObject, String upgradeToVersion) {
+
+		List<String> removedImportNames = new ArrayList<>();
+
+		List<String> importNames = getImportNames(detailAST);
+
+		for (String importName : importNames) {
+			JSONObject fromUpgradeClassJSONObject =
+				fromUpgradeJavaClassesJSONObject.getJSONObject(importName);
+			JSONObject toUpgradeClassJSONObject =
+				toUpgradeJavaClassesJSONObject.getJSONObject(importName);
+
+			if ((fromUpgradeClassJSONObject != null) &&
+				(toUpgradeClassJSONObject == null)) {
+
+				log(
+					detailAST, _MSG_CLASS_NOT_FOUND, importName,
+					upgradeToVersion);
+
+				removedImportNames.add(importName);
+			}
+		}
+
+		return removedImportNames;
+	}
+
 	private static final String _MSG_CLASS_NOT_FOUND = "class.not.found";
+
+	private static final String _MSG_METHOD_NOT_FOUND = "method.not.found";
 
 	private final Map<String, JSONObject> _javaClassesJSONObjectMap =
 		new HashMap<>();
+
+	private enum MethodStatus {
+
+		METHOD_FOUND, NO_CLASS_FOUND, NO_METHOD_FOUND
+
+	}
 
 }
