0a4f860d8a52c47942ed4ed1942af4a7d375d9e9
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2021-12-08 13:22:22 -0800

    LPS-139343 Use CompanyUtil instead of CompanyLocalServiceUtil

--- modules/apps/headless/headless-portal-instances/headless-portal-instances-impl/src/main/java/com/liferay/headless/portal/instances/internal/resource/v1_0/PortalInstanceResourceImpl.java
+++ modules/apps/headless/headless-portal-instances/headless-portal-instances-impl/src/main/java/com/liferay/headless/portal/instances/internal/resource/v1_0/PortalInstanceResourceImpl.java
@@ -20,7 +20,7 @@ import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.instances.service.PortalInstancesLocalService;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
-import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.CompanyService;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.vulcan.pagination.Page;
 
@@ -44,10 +44,9 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 
 	@Override
 	public void deletePortalInstance(String portalInstanceId) throws Exception {
-		Company company = _companyLocalService.getCompanyByWebId(
-			portalInstanceId);
+		Company company = _companyService.getCompanyByWebId(portalInstanceId);
 
-		_companyLocalService.deleteCompany(company.getCompanyId());
+		_companyService.deleteCompany(company.getCompanyId());
 
 		_portalInstancesLocalService.synchronizePortalInstances();
 	}
@@ -57,7 +56,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 		throws Exception {
 
 		return _toPortalInstance(
-			_companyLocalService.getCompanyByWebId(portalInstanceId));
+			_companyService.getCompanyByWebId(portalInstanceId));
 	}
 
 	@Override
@@ -68,7 +67,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 
 		List<PortalInstance> portalInstances = new ArrayList<>();
 
-		_companyLocalService.forEachCompany(
+		_companyService.forEachCompany(
 			company -> {
 				if (!finalSkipDefault ||
 					(_portalInstancesLocalService.getDefaultCompanyId() !=
@@ -86,8 +85,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 			String portalInstanceId, PortalInstance portalInstance)
 		throws Exception {
 
-		Company company = _companyLocalService.getCompanyByWebId(
-			portalInstanceId);
+		Company company = _companyService.getCompanyByWebId(portalInstanceId);
 
 		String virtualHostname = GetterUtil.getString(
 			portalInstance.getVirtualHost(), company.getVirtualHostname());
@@ -95,7 +93,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 			portalInstance.getDomain(), company.getMx());
 
 		return _toPortalInstance(
-			_companyLocalService.updateCompany(
+			_companyService.updateCompany(
 				company.getCompanyId(), virtualHostname, domain,
 				company.getMaxUsers(), company.isActive()));
 	}
@@ -104,7 +102,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 	public PortalInstance postPortalInstance(PortalInstance portalInstance)
 		throws Exception {
 
-		Company company = _companyLocalService.addCompany(
+		Company company = _companyService.addCompany(
 			portalInstance.getCompanyId(), portalInstance.getPortalInstanceId(),
 			portalInstance.getVirtualHost(), portalInstance.getDomain(), false,
 			0, true);
@@ -127,10 +125,9 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 	public void putPortalInstanceActivate(String portalInstanceId)
 		throws Exception {
 
-		Company company = _companyLocalService.getCompanyByWebId(
-			portalInstanceId);
+		Company company = _companyService.getCompanyByWebId(portalInstanceId);
 
-		_companyLocalService.updateCompany(
+		_companyService.updateCompany(
 			company.getCompanyId(), company.getVirtualHostname(),
 			company.getMx(), company.getMaxUsers(), true);
 	}
@@ -139,10 +136,9 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 	public void putPortalInstanceDeactivate(String portalInstanceId)
 		throws Exception {
 
-		Company company = _companyLocalService.getCompanyByWebId(
-			portalInstanceId);
+		Company company = _companyService.getCompanyByWebId(portalInstanceId);
 
-		_companyLocalService.updateCompany(
+		_companyService.updateCompany(
 			company.getCompanyId(), company.getVirtualHostname(),
 			company.getMx(), company.getMaxUsers(), false);
 	}
@@ -160,7 +156,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 	}
 
 	@Reference
-	private CompanyLocalService _companyLocalService;
+	private CompanyService _companyService;
 
 	@Reference
 	private PortalInstancesLocalService _portalInstancesLocalService;
