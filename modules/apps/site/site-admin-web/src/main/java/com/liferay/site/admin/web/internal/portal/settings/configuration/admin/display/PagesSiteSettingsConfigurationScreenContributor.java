bcda0ebc9f66685b41c7917fe08fbf07fd7dc6e5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-12-08 14:30:22 +0000

    LPS-143563 Copy files from public_private_pages.jsp and PublicPrivatePagesSiteSettingsConfigurationScreenContributor.java, no logic changes

---
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portal/settings/configuration/admin/display/PagesSiteSettingsConfigurationScreenContributor.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.admin.web.internal.portal.settings.configuration.admin.display;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.LayoutSetPrototype;
+import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.site.settings.configuration.admin.display.SiteSettingsConfigurationScreenContributor;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = SiteSettingsConfigurationScreenContributor.class)
+public class PagesSiteSettingsConfigurationScreenContributor
+	implements SiteSettingsConfigurationScreenContributor {
+
+	@Override
+	public String getCategoryKey() {
+		return "pages";
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/site_settings/public_private_pages.jsp";
+	}
+
+	@Override
+	public String getKey() {
+		return "site-configuration-pages";
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return LanguageUtil.get(locale, "pages");
+	}
+
+	@Override
+	public String getSaveMVCActionCommandName() {
+		return "/site_admin/edit_pages";
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public boolean isVisible(Group group) {
+		if ((group != null) && group.isCompany()) {
+			return false;
+		}
+
+		if (group != null) {
+			return true;
+		}
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		HttpServletRequest httpServletRequest = themeDisplay.getRequest();
+
+		long layoutSetPrototypeId = ParamUtil.getLong(
+			httpServletRequest, "layoutSetPrototypeId");
+
+		if (layoutSetPrototypeId > 0) {
+			LayoutSetPrototype layoutSetPrototype =
+				_layoutSetPrototypeLocalService.fetchLayoutSetPrototype(
+					layoutSetPrototypeId);
+
+			if (layoutSetPrototype != null) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@Reference
+	private LayoutSetPrototypeLocalService _layoutSetPrototypeLocalService;
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.site.admin.web)")
+	private ServletContext _servletContext;
+
+}
