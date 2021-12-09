2c95b1930a12de6cb17f51ea040094fcc9224916
Brian Chan <brian.chan@liferay.com>
2021-12-09 08:47:06 -0800

    LPS-122862 SF

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutSetPrototypePropagationTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutSetPrototypePropagationTest.java
@@ -622,7 +622,6 @@ public class LayoutSetPrototypePropagationTest
 		for (String propertyKey : prototypeThemeSettings.keySet()) {
 			String prototypeValue = prototypeLayoutSet.getThemeSetting(
 				propertyKey, device);
-
 			String targetValue = targetLayoutSet.getThemeSetting(
 				propertyKey, device);
 
