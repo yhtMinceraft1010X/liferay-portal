aeda55e5f12605117b0e6b49590976c9e9e911dc
drewbrokke <drew.brokke@liferay.com>
2021-12-09 16:05:22 -0600

    LPS-141798 portal-language-override-service: ModelListener: clear the PortalCache when a PLOEntry is created, updated, or removed

---
+++ modules/apps/portal-language/portal-language-override-service/src/main/java/com/liferay/portal/language/override/internal/PLOEntryModelListener.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.language.override.internal;
+
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.language.override.model.PLOEntry;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(service = ModelListener.class)
+public class PLOEntryModelListener extends BaseModelListener<PLOEntry> {
+
+	@Override
+	public void onAfterCreate(PLOEntry model) throws ModelListenerException {
+		_ploLanguageOverrideProvider.clear(
+			model.getCompanyId(),
+			LocaleUtil.fromLanguageId(model.getLanguageId()));
+	}
+
+	@Override
+	public void onAfterRemove(PLOEntry model) throws ModelListenerException {
+		_ploLanguageOverrideProvider.clear(
+			model.getCompanyId(),
+			LocaleUtil.fromLanguageId(model.getLanguageId()));
+	}
+
+	@Override
+	public void onAfterUpdate(PLOEntry originalModel, PLOEntry model)
+		throws ModelListenerException {
+
+		_ploLanguageOverrideProvider.clear(
+			model.getCompanyId(),
+			LocaleUtil.fromLanguageId(model.getLanguageId()));
+	}
+
+	@Reference
+	private PLOLanguageOverrideProvider _ploLanguageOverrideProvider;
+
+}
