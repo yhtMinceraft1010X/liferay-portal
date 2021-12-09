0d422b2372060ee40a1fb8a6c5042d1b50f1e105
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-12-09 15:35:33 +0000

    LPS-143612 Copy from public private site url, no logic changes

---
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portal/settings/configuration/admin/display/SiteURLSiteSettingsConfigurationScreenContributor.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.admin.web.internal.portal.settings.configuration.admin.display;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.site.settings.configuration.admin.display.SiteSettingsConfigurationScreenContributor;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = SiteSettingsConfigurationScreenContributor.class)
+public class SiteURLSiteSettingsConfigurationScreenContributor
+	implements SiteSettingsConfigurationScreenContributor {
+
+	@Override
+	public String getCategoryKey() {
+		return "site-configuration";
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/site_settings/site_url.jsp";
+	}
+
+	@Override
+	public String getKey() {
+		return "site-configuration-site-url";
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return LanguageUtil.get(locale, "site-url");
+	}
+
+	@Override
+	public String getSaveMVCActionCommandName() {
+		return "/site_admin/edit_site_url";
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public boolean isVisible(Group group) {
+		if (group.isCompany()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.site.admin.web)")
+	private ServletContext _servletContext;
+
+}
