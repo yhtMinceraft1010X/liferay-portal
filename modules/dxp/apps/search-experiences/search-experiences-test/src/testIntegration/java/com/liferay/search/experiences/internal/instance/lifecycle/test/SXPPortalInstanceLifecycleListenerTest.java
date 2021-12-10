5d8df2fee809878be9b2e70adbcca30fe4093f96
shuyangzhou <shuyang.zhou@liferay.com>
2021-12-09 22:27:02 -0800

    LPS-143603 Change to integration test.

---
+++ modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/instance/lifecycle/test/SXPPortalInstanceLifecycleListenerTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.internal.instance.lifecycle.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.search.experiences.rest.dto.v1_0.SXPElement;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author André de Oliveira
+ */
+@RunWith(Arquillian.class)
+public class SXPPortalInstanceLifecycleListenerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testSXPElements() throws Exception {
+		List<SXPElement> sxpElements = ReflectionTestUtil.getFieldValue(
+			_portalInstanceLifecycleListener, "sxpElements");
+
+		Assert.assertNotEquals(0, sxpElements.size());
+
+		for (SXPElement sxpElement : sxpElements) {
+			Assert.assertNotNull(sxpElement);
+		}
+	}
+
+	@Inject(
+		filter = "component.name=com.liferay.search.experiences.internal.instance.lifecycle.SXPPortalInstanceLifecycleListener"
+	)
+	private PortalInstanceLifecycleListener _portalInstanceLifecycleListener;
+
+}
