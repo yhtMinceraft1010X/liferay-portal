cbd86b678397d1153a06df56214be642598d5273
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-12-10 16:26:21 +0100

    LPS-142850 Adds service methods to count and find published pages

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -21,6 +21,8 @@ import com.liferay.layout.admin.kernel.model.LayoutTypePortletConstants;
 import com.liferay.petra.lang.CentralizedThreadLocal;
 import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.petra.sql.dsl.query.DSLQuery;
+import com.liferay.petra.sql.dsl.query.FromStep;
+import com.liferay.petra.sql.dsl.query.GroupByStep;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.bean.BeanReference;
@@ -49,6 +51,7 @@ import com.liferay.portal.kernel.model.LayoutPrototype;
 import com.liferay.portal.kernel.model.LayoutReference;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
+import com.liferay.portal.kernel.model.LayoutTable;
 import com.liferay.portal.kernel.model.LayoutType;
 import com.liferay.portal.kernel.model.LayoutTypePortlet;
 import com.liferay.portal.kernel.model.PortalPreferenceValueTable;
@@ -2113,6 +2116,36 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 	}
 
 	@Override
+	public List<Layout> getPublishedLayouts(
+		long groupId, int start, int end,
+		OrderByComparator<Layout> orderByComparator) {
+
+		GroupByStep groupByStep = _getPublishedPagesDSLQuery(
+			groupId, DSLQueryFactoryUtil.select(LayoutTable.INSTANCE));
+
+		return dslQuery(
+			groupByStep.orderBy(
+				orderByStep -> {
+					if (orderByComparator == null) {
+						return orderByStep.orderBy(
+							LayoutTable.INSTANCE.priority.ascending());
+					}
+
+					return orderByStep.orderBy(
+						LayoutTable.INSTANCE, orderByComparator);
+				}
+			).limit(
+				start, end
+			));
+	}
+
+	@Override
+	public int getPublishedLayoutsCount(long groupId) {
+		return dslQueryCount(
+			_getPublishedPagesDSLQuery(groupId, DSLQueryFactoryUtil.count()));
+	}
+
+	@Override
 	public List<Layout> getScopeGroupLayouts(long parentGroupId)
 		throws PortalException {
 
@@ -3657,6 +3690,21 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 		return newFriendlyURLMap;
 	}
 
+	private DSLQuery _getDraftLayoutsClassPKsDSLQuery(long groupId) {
+		return DSLQueryFactoryUtil.select(
+			LayoutTable.INSTANCE.classPK
+		).from(
+			LayoutTable.INSTANCE
+		).where(
+			LayoutTable.INSTANCE.groupId.eq(
+				groupId
+			).and(
+				LayoutTable.INSTANCE.classNameId.eq(
+					_classNameLocalService.getClassNameId(Layout.class))
+			)
+		);
+	}
+
 	private long _getParentPlid(
 		long groupId, boolean privateLayout, long parentLayoutId) {
 
@@ -3672,6 +3720,57 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 		}
 
 		return parentLayout.getPlid();
+	}
+
+	private DSLQuery _getPublishedDraftLayoutsClassPKsDSLQuery(long groupId) {
+		return DSLQueryFactoryUtil.select(
+			LayoutTable.INSTANCE.classPK
+		).from(
+			LayoutTable.INSTANCE
+		).where(
+			LayoutTable.INSTANCE.groupId.eq(
+				groupId
+			).and(
+				LayoutTable.INSTANCE.classNameId.eq(
+					_classNameLocalService.getClassNameId(Layout.class)
+				).and(
+					LayoutTable.INSTANCE.typeSettings.like("%published=true%")
+				)
+			)
+		);
+	}
+
+	private GroupByStep _getPublishedPagesDSLQuery(
+		long groupId, FromStep fromStep) {
+
+		return fromStep.from(
+			LayoutTable.INSTANCE
+		).where(
+			LayoutTable.INSTANCE.groupId.eq(
+				groupId
+			).and(
+				LayoutTable.INSTANCE.system.eq(false)
+			).and(
+				LayoutTable.INSTANCE.type.notIn(
+					new String[] {
+						LayoutConstants.TYPE_COLLECTION,
+						LayoutConstants.TYPE_CONTENT
+					}
+				).or(
+					LayoutTable.INSTANCE.plid.in(
+						_getPublishedDraftLayoutsClassPKsDSLQuery(groupId))
+				).or(
+					LayoutTable.INSTANCE.status.eq(
+						WorkflowConstants.STATUS_APPROVED
+					).and(
+						LayoutTable.INSTANCE.hidden.eq(false)
+					).and(
+						LayoutTable.INSTANCE.plid.notIn(
+							_getDraftLayoutsClassPKsDSLQuery(groupId))
+					)
+				).withParentheses()
+			)
+		);
 	}
 
 	private Sort _getSortFromComparator(
