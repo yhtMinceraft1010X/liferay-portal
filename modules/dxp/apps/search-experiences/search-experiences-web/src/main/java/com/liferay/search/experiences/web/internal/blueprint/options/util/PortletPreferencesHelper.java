f574df0d90a9763e3690ad577049802613898526
Kevin Tan <kevin.tan@liferay.com>
2021-12-10 11:42:53 -0800

    LPS-141119 search-experiences-web: Remove unused methods

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/options/util/PortletPreferencesHelper.java
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/options/util/PortletPreferencesHelper.java
@@ -15,7 +15,6 @@
 package com.liferay.search.experiences.web.internal.blueprint.options.util;
 
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.util.Optional;
 
@@ -32,38 +31,8 @@ public class PortletPreferencesHelper {
 		_portletPreferencesOptional = portletPreferencesOptional;
 	}
 
-	public Optional<Boolean> getBoolean(String key) {
-		Optional<String> value = getValue(key);
-
-		return value.map(GetterUtil::getBoolean);
-	}
-
-	public boolean getBoolean(String key, boolean defaultValue) {
-		Optional<Boolean> value = getBoolean(key);
-
-		return value.orElse(defaultValue);
-	}
-
-	public Optional<Integer> getInteger(String key) {
-		Optional<String> value = getValue(key);
-
-		return value.map(GetterUtil::getInteger);
-	}
-
-	public int getInteger(String key, int defaultValue) {
-		Optional<Integer> value = getInteger(key);
-
-		return value.orElse(defaultValue);
-	}
-
 	public Optional<String> getString(String key) {
 		return getValue(key);
-	}
-
-	public String getString(String key, String defaultValue) {
-		Optional<String> value = getString(key);
-
-		return value.orElse(defaultValue);
 	}
 
 	protected Optional<String> getValue(String key) {
--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/options/util/SearchStringUtil.java
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/options/util/SearchStringUtil.java
@@ -14,12 +14,10 @@
 
 package com.liferay.search.experiences.web.internal.blueprint.options.util;
 
-import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Optional;
-import java.util.stream.Stream;
 
 /**
  * @author André de Oliveira
@@ -34,30 +32,6 @@ public class SearchStringUtil {
 		}
 
 		return Optional.of(s);
-	}
-
-	public static String[] splitAndUnquote(Optional<String> optional) {
-		return Optional.ofNullable(
-			optional
-		).orElse(
-			Optional.empty()
-		).map(
-			SearchStringUtil::splitAndUnquote
-		).orElse(
-			new String[0]
-		);
-	}
-
-	public static String[] splitAndUnquote(String s) {
-		return Stream.of(
-			StringUtil.split(s.trim(), CharPool.COMMA)
-		).map(
-			String::trim
-		).map(
-			StringUtil::unquote
-		).toArray(
-			String[]::new
-		);
 	}
 
 }
