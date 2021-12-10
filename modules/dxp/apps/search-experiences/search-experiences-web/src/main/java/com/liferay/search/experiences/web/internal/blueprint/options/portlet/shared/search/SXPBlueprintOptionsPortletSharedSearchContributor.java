02c174388b53dd3cedc0e292c9a07f95c8773b84
Kevin Tan <kevin.tan@liferay.com>
2021-12-10 10:57:38 -0800

    LPS-141119 search-experiences-web: Add SXPBlueprintOptionsPortletSharedSearchContributor

---
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/options/portlet/shared/search/SXPBlueprintOptionsPortletSharedSearchContributor.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.web.internal.blueprint.options.portlet.shared.search;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchContributor;
+import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchSettings;
+import com.liferay.search.experiences.constants.SXPPortletKeys;
+import com.liferay.search.experiences.web.internal.blueprint.options.portlet.preferences.SXPBlueprintOptionsPortletPreferences;
+import com.liferay.search.experiences.web.internal.blueprint.options.portlet.preferences.SXPBlueprintOptionsPortletPreferencesImpl;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Kevin Tan
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + SXPPortletKeys.SXP_BLUEPRINT_OPTIONS,
+	service = PortletSharedSearchContributor.class
+)
+public class SXPBlueprintOptionsPortletSharedSearchContributor
+	implements PortletSharedSearchContributor {
+
+	@Override
+	public void contribute(
+		PortletSharedSearchSettings portletSharedSearchSettings) {
+
+		SXPBlueprintOptionsPortletPreferences
+			sxpBlueprintOptionsPortletPreferences =
+				new SXPBlueprintOptionsPortletPreferencesImpl(
+					portletSharedSearchSettings.
+						getPortletPreferencesOptional());
+
+		SearchRequestBuilder searchRequestBuilder =
+			portletSharedSearchSettings.getFederatedSearchRequestBuilder(
+				sxpBlueprintOptionsPortletPreferences.
+					getFederatedSearchKeyOptional());
+
+		searchRequestBuilder.withSearchContext(
+			searchContext -> {
+				String sxpBlueprintId = GetterUtil.getString(
+					searchContext.getAttribute(
+						"search.experiences.blueprint.id"));
+
+				if (Validator.isBlank(sxpBlueprintId)) {
+					searchContext.setAttribute(
+						"search.experiences.blueprint.id",
+						sxpBlueprintOptionsPortletPreferences.
+							getSXPBlueprintIdString());
+				}
+
+				searchContext.setAttribute(
+					"federatedSearchKey",
+					sxpBlueprintOptionsPortletPreferences.
+						getFederatedSearchKeyString());
+
+				HttpServletRequest httpServletRequest =
+					_portal.getHttpServletRequest(
+						portletSharedSearchSettings.getRenderRequest());
+
+				searchContext.setAttribute(
+					"search.experiences.ipaddress",
+					httpServletRequest.getRemoteAddr());
+			});
+	}
+
+	@Reference
+	private Portal _portal;
+
+}
