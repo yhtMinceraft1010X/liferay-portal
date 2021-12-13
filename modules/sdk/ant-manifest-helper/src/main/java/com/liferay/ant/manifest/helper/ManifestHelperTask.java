beb05449a8e72fbbdce6e0a136f7a63fd5f82da2
shuyangzhou <shuyang.zhou@liferay.com>
2021-12-12 22:12:48 -0800

    LPS-143953 Extract to ant-manifest-helper module.

--- modules/sdk/ant-manifest-helper/src/main/java/com/liferay/ant/manifest/helper/ManifestHelperTask.java
+++ modules/sdk/ant-manifest-helper/src/main/java/com/liferay/ant/manifest/helper/ManifestHelperTask.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.util.ant;
+package com.liferay.ant.manifest.helper;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.OSDetector;
