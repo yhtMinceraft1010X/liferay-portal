53876749803a9909276c2ca57ecc6b4681082ebe
Aildson Ferreira <aildson.ferreira@liferay.com>
2021-12-13 11:18:20 -0300

    LPS-140164 Overall adjustments for component return and modal close btn

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/ModalCardSubscription/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/ModalCardSubscription/index.js
@@ -1,8 +1,7 @@
 import {useQuery} from '@apollo/client';
-import ClayButton from '@clayui/button';
-import ClayIcon from '@clayui/icon';
 import ClayModal from '@clayui/modal';
 import React, {useMemo, useState} from 'react';
+import BaseButton from '../../../../common/components/BaseButton';
 import Table from '../../../../common/components/Table';
 import {getAccountSubscriptionsTerms} from '../../../../common/services/liferay/graphql/queries';
 import {status} from '../../utils/constants';
@@ -96,43 +95,34 @@ const ModalCardSubscription = ({
 	);
 
 	return (
-		<>
-			<ClayModal center={true} observer={observer} size="lg">
-				<div className="pt-4 px-4">
-					<div className="d-flex justify-content-between mb-4 teste-cursor">
-						<div className="flex-row mb-1">
-							<h6 className="text-brand-primary">
-								SUBSCRIPTION TERMS
-							</h6>
+		<ClayModal center={true} observer={observer} size="lg">
+			<div className="pt-4 px-4">
+				<div className="d-flex justify-content-between mb-4">
+					<div className="flex-row mb-1">
+						<h6 className="text-brand-primary">
+							SUBSCRIPTION TERMS
+						</h6>
 
-							<h2 className="text-neutral-10">{`${subscriptionGroup} ${subscriptionName}`}</h2>
-						</div>
-
-						<ClayButton
-							aria-label="close"
-							className="close"
-							displayType="unstyled"
-							onClick={onClose}
-						>
-							<ClayIcon symbol="times" />
-						</ClayButton>
+						<h2 className="text-neutral-10">{`${subscriptionGroup} ${subscriptionName}`}</h2>
 					</div>
 
-					<div>
-						<Table
-							activePage={activePage}
-							columns={columns}
-							data={accountSubscriptionTermsItems}
-							hasPagination={totalCount >= 5}
-							itemsPerPage={5}
-							setActivePage={setActivePage}
-							tableVerticalAlignment="middle"
-							totalCount={totalCount}
-						/>
-					</div>
+					<BaseButton appendIcon='times' aria-label="close" className="align-self-start" displayType="unstyled" onClick={onClose} />
 				</div>
-			</ClayModal>
-		</>
+
+				<div>
+					<Table
+						activePage={activePage}
+						columns={columns}
+						data={accountSubscriptionTermsItems}
+						hasPagination={totalCount >= 5}
+						itemsPerPage={5}
+						setActivePage={setActivePage}
+						tableVerticalAlignment="middle"
+						totalCount={totalCount}
+					/>
+				</div>
+			</div>
+		</ClayModal>
 	);
 };
 
