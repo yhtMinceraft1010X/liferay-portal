b10304d9f51808915de06e6fdc3427f5bc938bbf
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-12-13 11:19:57 -0300

    LPS-143737 Disable comments

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/workflow/MBDiscussionWorkflowHandler.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/workflow/MBDiscussionWorkflowHandler.java
@@ -61,6 +61,11 @@ public class MBDiscussionWorkflowHandler extends MBMessageWorkflowHandler {
 	}
 
 	@Override
+	public boolean isCommentable() {
+		return false;
+	}
+
+	@Override
 	public void startWorkflowInstance(
 			long companyId, long groupId, long userId, long classPK,
 			MBMessage model, Map<String, Serializable> workflowContext)
