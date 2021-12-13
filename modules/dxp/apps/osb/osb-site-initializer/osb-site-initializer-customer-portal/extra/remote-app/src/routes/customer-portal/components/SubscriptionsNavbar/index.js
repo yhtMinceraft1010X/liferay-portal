5f6a00468ebac743f4e58b60458679ba55e53c9a
Aildson Ferreira <aildson.ferreira@liferay.com>
2021-12-13 14:02:06 -0300

    LPS-140319 Updated component props

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SubscriptionsNavbar/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SubscriptionsNavbar/index.js
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from 'react';
 import {getAccountSubscriptionGroups} from '../../../../common/services/liferay/graphql/queries';
 import SubscriptionsFilterByStatus from '../SubscriptionsFilterByStatus';
 
-const SubscriptionsNavbar = ({accountKey, setSelectedStatus, setSelectedSubscriptionGroup}) => {
+const SubscriptionsNavbar = ({accountKey, selectedStatus, setSelectedStatus, setSelectedSubscriptionGroup}) => {
 	const [subscriptionsTags, setSubscriptionsTags] = useState(() => []);
 
 	const {
@@ -52,6 +52,7 @@ const SubscriptionsNavbar = ({accountKey, setSelectedStatus, setSelectedSubscrip
 					</nav>
 
 					<SubscriptionsFilterByStatus
+						selectedStatus={selectedStatus}
 						setSelectedStatus={setSelectedStatus}
 					/>
 				</>
