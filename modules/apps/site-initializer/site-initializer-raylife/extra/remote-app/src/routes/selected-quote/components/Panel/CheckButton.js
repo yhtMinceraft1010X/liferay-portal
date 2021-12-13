42f90aff1e88ffc370fd3fceee30909d76641faa
Silas Silva <silasbsi@gmail.com>
2021-12-13 16:32:43 -0300

    LPS-143686 Update Check button component with Dialect theme

--- modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/routes/selected-quote/components/Panel/CheckButton.js
+++ modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/routes/selected-quote/components/Panel/CheckButton.js
@@ -1,16 +1,24 @@
 import ClayIcon from '@clayui/icon';
 import classNames from 'classnames';
 
-const CheckButton = ({checked, expanded, hasError = false}) => (
-	<div className="panel-right">
-		<div
-			className={classNames('panel-right-icon', {
-				'step-checked': checked && !hasError && !expanded,
-			})}
-		>
-			<ClayIcon symbol="check" />
+const CheckButton = ({checked, expanded, hasError = false}) => {
+	const isChecked = checked && !hasError && !expanded;
+
+	return (
+		<div className="panel-right">
+			<div
+				className={classNames(
+					'align-items-center d-flex icon justify-content-center rounded-circle',
+					{
+						'bg-neutral-3': !isChecked,
+						'bg-success': isChecked,
+					}
+				)}
+			>
+				<ClayIcon className="text-neutral-0" symbol="check" />
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default CheckButton;
