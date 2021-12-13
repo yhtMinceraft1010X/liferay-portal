48d49a95a880604d9a92b7fb5ef3b4b599ae4c8d
clara izquierdo <claraizquierdosanz@gmail.com>
2021-12-13 11:25:44 +0100

    LPS-143212 Extract iframe url creation to a new function

--- modules/apps/product-navigation/product-navigation-simulation-device/src/main/resources/META-INF/resources/js/product_navigation_simulation_device.js
+++ modules/apps/product-navigation/product-navigation-simulation-device/src/main/resources/META-INF/resources/js/product_navigation_simulation_device.js
@@ -77,6 +77,17 @@ AUI.add(
 
 		var WIN = A.config.win;
 
+		var createIframeURL = () => {
+			const url = new URL(WIN.location.href);
+			const searchParams = new URLSearchParams(url.search);
+			if (searchParams.has('segmentsExperienceId')) {
+				searchParams.delete('segmentsExperienceId');
+			}
+			searchParams.append('p_l_mode', 'preview');
+
+			return `${url.origin}${url.pathname}?${searchParams.toString()}`;
+		};
+
 		var SimulationDevice = A.Component.create({
 			ATTRS: {
 				devices: {
@@ -361,13 +372,6 @@ AUI.add(
 							width,
 						};
 
-						const url = new URL(WIN.location.href);
-						const searchParams = new URLSearchParams(url.search);
-						if (searchParams.has('segmentsExperienceId')) {
-							searchParams.delete('segmentsExperienceId');
-						}
-						searchParams.append('p_l_mode', 'preview');
-
 						Liferay.Util.openWindow(
 							{
 								cache: false,
@@ -381,9 +385,7 @@ AUI.add(
 								title: Liferay.Language.get(
 									'simulation-preview'
 								),
-								uri: `${url.origin}${
-									url.pathname
-								}?${searchParams.toString()}`,
+								uri: createIframeURL(),
 							},
 							(dialogWindow) => {
 								var dialogBoundingBox = dialogWindow.get(
