4fcf5b089939ce486a5d1a86a14e05d09a82cba4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-12-14 22:12:45 +0000

    LPS-143942 We need to update the layout status to draft when we are updating the editable values for a fragment entry link

---
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/service/LayoutPageTemplateStructureRelFragmentEntryLinkLocalServiceWrapper.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.internal.service;
+
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalServiceWrapper;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(immediate = true, service = ServiceWrapper.class)
+public class LayoutPageTemplateStructureRelFragmentEntryLinkLocalServiceWrapper
+	extends FragmentEntryLinkLocalServiceWrapper {
+
+	@Override
+	public FragmentEntryLink updateFragmentEntryLink(
+			long fragmentEntryLinkId, String editableValues)
+		throws PortalException {
+
+		FragmentEntryLink fragmentEntryLink = super.updateFragmentEntryLink(
+			fragmentEntryLinkId, editableValues);
+
+		_layoutLocalService.updateStatus(
+			PrincipalThreadLocal.getUserId(), fragmentEntryLink.getPlid(),
+			WorkflowConstants.STATUS_DRAFT,
+			ServiceContextThreadLocal.getServiceContext());
+
+		return fragmentEntryLink;
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+}
