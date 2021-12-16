7d2f5fc9766dd02b0935de15d2709b5a1048eccd
Marcos Martins <marcos.martins@liferay.com>
2021-12-16 08:45:02 -0300

    LRAC-9627 watch color dropdown click for sending to Analytics

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/ColorPicker/ColorPicker.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/ColorPicker/ColorPicker.es.js
@@ -86,23 +86,75 @@ const ColorPicker = ({
 	spritemap,
 	value,
 	...otherProps
-}) => (
-	<FieldBase
-		name={name}
-		readOnly={readOnly}
-		spritemap={spritemap}
-		{...otherProps}
-	>
-		<ClayColorPickerWithState
-			inputValue={value ? value : predefinedValue}
+}) => {
+	let colorDropDownClickEvent;
+	let previousShow = false;
+
+	const observer = new MutationObserver((mutationsList, observer) => {
+		for (const mutation of mutationsList) {
+			if (
+				mutation.type === 'attributes' &&
+				mutation.attributeName === 'class'
+			) {
+				const show = mutation.target.classList.contains('show');
+
+				if (show === previousShow) {
+					return;
+				}
+
+				if (show) {
+					onFocus(colorDropDownClickEvent);
+				}
+				else {
+					onBlur(colorDropDownClickEvent);
+
+					observer.disconnect();
+				}
+
+				previousShow = show;
+			}
+		}
+	});
+
+	// watch dropdown click for sending to Analytics
+
+	const handleColorDropDownClicked = (event) => {
+		if (!event.target.classList.contains('dropdown-toggle')) {
+			return;
+		}
+
+		const colorDropdownNode = document.querySelector(
+			'.clay-color-dropdown-menu'
+		);
+
+		if (!colorDropdownNode) {
+			return;
+		}
+
+		colorDropDownClickEvent = event;
+
+		observer.observe(colorDropdownNode, {attributes: true});
+	};
+
+	return (
+		<FieldBase
 			name={name}
-			onBlur={onBlur}
-			onFocus={onFocus}
-			onValueChange={(value) => onChange({}, value)}
+			onClick={handleColorDropDownClicked}
 			readOnly={readOnly}
 			spritemap={spritemap}
-		/>
-	</FieldBase>
-);
+			{...otherProps}
+		>
+			<ClayColorPickerWithState
+				inputValue={value ? value : predefinedValue}
+				name={name}
+				onBlur={onBlur}
+				onFocus={onFocus}
+				onValueChange={(value) => onChange({}, value)}
+				readOnly={readOnly}
+				spritemap={spritemap}
+			/>
+		</FieldBase>
+	);
+};
 
 export default ColorPicker;
