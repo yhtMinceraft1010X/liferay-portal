9e392154f631f9a2474936bbae3ed0accdf7d8e7
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-12-17 15:57:30 +0100

    LPS-142508 Groups categories by vocabulary.

--- modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/layout/display/page/AssetCategoryLayoutDisplayPageMultiSelectionProvider.java
+++ modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/layout/display/page/AssetCategoryLayoutDisplayPageMultiSelectionProvider.java
@@ -15,12 +15,22 @@
 package com.liferay.asset.categories.internal.layout.display.page;
 
 import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.InfoItemReference;
 import com.liferay.layout.display.page.LayoutDisplayPageMultiSelectionProvider;
 import com.liferay.portal.kernel.language.LanguageUtil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Lourdes Fernández Besada
@@ -39,5 +49,55 @@ public class AssetCategoryLayoutDisplayPageMultiSelectionProvider
 	public String getPluralLabel(Locale locale) {
 		return LanguageUtil.get(locale, "categories");
 	}
+
+	@Override
+	public List<InfoItemReference> process(List<InfoItemReference> list) {
+		Stream<InfoItemReference> stream = list.stream();
+
+		Map<Long, List<InfoItemReference>> itemsByVocabularyIdMap =
+			stream.filter(
+				infoItemReference ->
+					Objects.equals(
+						getClassName(), infoItemReference.getClassName()) &&
+					(_getClassPK(infoItemReference) > 0)
+			).collect(
+				Collectors.groupingBy(
+					infoItemReference -> {
+						AssetCategory assetCategory =
+							_assetCategoryLocalService.fetchAssetCategory(
+								_getClassPK(infoItemReference));
+
+						return assetCategory.getVocabularyId();
+					},
+					Collectors.toList())
+			);
+
+		List<InfoItemReference> itemsHierarchy = new ArrayList<>();
+
+		for (List<InfoItemReference> vocabularyItems :
+				itemsByVocabularyIdMap.values()) {
+
+			itemsHierarchy.addAll(vocabularyItems);
+		}
+
+		return itemsHierarchy;
+	}
+
+	private long _getClassPK(InfoItemReference infoItemReference) {
+		if (infoItemReference.getInfoItemIdentifier() instanceof
+				ClassPKInfoItemIdentifier) {
+
+			ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
+				(ClassPKInfoItemIdentifier)
+					infoItemReference.getInfoItemIdentifier();
+
+			return classPKInfoItemIdentifier.getClassPK();
+		}
+
+		return 0;
+	}
+
+	@Reference
+	private AssetCategoryLocalService _assetCategoryLocalService;
 
 }
