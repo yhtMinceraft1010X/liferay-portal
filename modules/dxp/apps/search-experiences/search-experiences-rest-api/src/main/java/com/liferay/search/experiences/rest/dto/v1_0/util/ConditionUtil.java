c74458dd61167269dd41edfa90682779296dceda
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-12-17 13:36:03 +0200

    LPS-144069 Update ConditionUtil.java
    Value check minor fix

--- modules/dxp/apps/search-experiences/search-experiences-rest-api/src/main/java/com/liferay/search/experiences/rest/dto/v1_0/util/ConditionUtil.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-api/src/main/java/com/liferay/search/experiences/rest/dto/v1_0/util/ConditionUtil.java
@@ -104,7 +104,7 @@ public class ConditionUtil {
 
 		Object value = supplier.get();
 
-		if (supplier.get() != null) {
+		if (value != null) {
 			consumer.accept(UnpackUtil.unpack(value));
 		}
 	}
