049fe1422817c6006c9adfb16d27560569230d9c
Dante Wang <dante.wang@liferay.com>
2021-12-20 15:44:03 +0800

    LPS-143660 Fix NPE: DataSourceFactoryImpl is executed before Spring context startup, so at the moment the wiring is not available

--- portal-impl/src/com/liferay/portal/dao/jdbc/DataSourceFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/dao/jdbc/DataSourceFactoryImpl.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.jndi.JNDIUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
+import com.liferay.portal.kernel.util.DigesterUtil;
 import com.liferay.portal.kernel.util.JavaDetector;
 import com.liferay.portal.kernel.util.PropertiesUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -41,6 +42,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.spring.hibernate.DialectDetector;
+import com.liferay.portal.util.DigesterImpl;
 import com.liferay.portal.util.JarUtil;
 import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
@@ -620,6 +622,10 @@ public class DataSourceFactoryImpl implements DataSourceFactory {
 			}
 
 			try {
+				DigesterUtil digesterUtil = new DigesterUtil();
+
+				digesterUtil.setDigester(new DigesterImpl());
+
 				JarUtil.downloadAndInstallJar(
 					new URL(url),
 					Paths.get(
