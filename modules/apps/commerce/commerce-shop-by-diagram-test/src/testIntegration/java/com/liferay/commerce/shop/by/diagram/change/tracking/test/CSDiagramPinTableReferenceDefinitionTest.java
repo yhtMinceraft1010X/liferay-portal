adaa692126cc421abd66aa28bdcdfc77fa8ba462
Cheryl Tang <cheryl.tang@liferay.com>
2021-12-20 10:09:06 -0800

    COMMERCE-8019 Create test coverage for commerce-shop-by-diagram-service TableReferenceDefinition classes

---
+++ modules/apps/commerce/commerce-shop-by-diagram-test/src/testIntegration/java/com/liferay/commerce/shop/by/diagram/change/tracking/test/CSDiagramPinTableReferenceDefinitionTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.shop.by.diagram.change.tracking.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.test.util.BaseTableReferenceDefinitionTestCase;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.test.util.CPTestUtil;
+import com.liferay.commerce.shop.by.diagram.service.CSDiagramPinLocalService;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Cheryl Tang
+ */
+@RunWith(Arquillian.class)
+public class CSDiagramPinTableReferenceDefinitionTest
+	extends BaseTableReferenceDefinitionTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_cpInstance = CPTestUtil.addCPInstance(group.getGroupId());
+	}
+
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		return _csDiagramPinLocalService.addCSDiagramPin(
+			TestPropsValues.getUserId(), _cpInstance.getCPDefinitionId(),
+			RandomTestUtil.randomDouble(), RandomTestUtil.randomDouble(),
+			CSDiagramPinTableReferenceDefinitionTest.class.getSimpleName());
+	}
+
+	@Inject
+	private static CSDiagramPinLocalService _csDiagramPinLocalService;
+
+	private CPInstance _cpInstance;
+
+}
---
+++ modules/apps/commerce/commerce-shop-by-diagram-test/src/testIntegration/java/com/liferay/commerce/shop/by/diagram/change/tracking/test/CSDiagramSettingTableReferenceDefinitionTest.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.shop.by.diagram.change.tracking.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.test.util.BaseTableReferenceDefinitionTestCase;
+import com.liferay.commerce.product.constants.CPAttachmentFileEntryConstants;
+import com.liferay.commerce.product.model.CPAttachmentFileEntry;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CommerceCatalog;
+import com.liferay.commerce.product.service.CPAttachmentFileEntryLocalService;
+import com.liferay.commerce.product.service.CommerceCatalogLocalService;
+import com.liferay.commerce.product.test.util.CPTestUtil;
+import com.liferay.commerce.shop.by.diagram.service.CSDiagramSettingLocalService;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileEntryTypeConstants;
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.document.library.kernel.service.DLFolderLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.constants.TestDataConstants;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Cheryl Tang
+ */
+@RunWith(Arquillian.class)
+public class CSDiagramSettingTableReferenceDefinitionTest
+	extends BaseTableReferenceDefinitionTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			group.getGroupId());
+
+		DLFolder dlFolder = _dlFolderLocalService.addFolder(
+			TestPropsValues.getUserId(), group.getGroupId(), group.getGroupId(),
+			false, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(), false,
+			_serviceContext);
+
+		byte[] bytes = TestDataConstants.TEST_BYTE_ARRAY;
+
+		InputStream inputStream = new ByteArrayInputStream(bytes);
+
+		_dlFileEntry = _dlFileEntryLocalService.addFileEntry(
+			null, TestPropsValues.getUserId(), group.getGroupId(),
+			dlFolder.getRepositoryId(), dlFolder.getFolderId(),
+			RandomTestUtil.randomString(), ContentTypes.TEXT_PLAIN,
+			RandomTestUtil.randomString(), StringPool.BLANK, StringPool.BLANK,
+			StringPool.BLANK,
+			DLFileEntryTypeConstants.FILE_ENTRY_TYPE_ID_BASIC_DOCUMENT, null,
+			null, inputStream, bytes.length, null, null, _serviceContext);
+
+		CommerceCatalog commerceCatalog =
+			_commerceCatalogLocalService.addCommerceCatalog(
+				null, RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(),
+				LocaleUtil.US.getDisplayLanguage(),
+				ServiceContextTestUtil.getServiceContext(group.getGroupId()));
+
+		_cpDefinition = CPTestUtil.addCPDefinitionFromCatalog(
+			commerceCatalog.getGroupId(), "simple", false, false);
+
+		_cpAttachmentFileEntry =
+			_cpAttachmentFileEntryLocalService.addCPAttachmentFileEntry(
+				null, TestPropsValues.getUserId(), group.getGroupId(),
+				PortalUtil.getClassNameId(CPDefinition.class.getName()),
+				_cpDefinition.getCPDefinitionId(),
+				_dlFileEntry.getFileEntryId(), false, null, 1, 1, 2020, 1, 1, 2,
+				2, 2021, 2, 2, true, RandomTestUtil.randomLocaleStringMap(),
+				null, 0D, CPAttachmentFileEntryConstants.TYPE_IMAGE,
+				_serviceContext);
+	}
+
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		return _csDiagramSettingLocalService.addCSDiagramSetting(
+			TestPropsValues.getUserId(), _cpDefinition.getCPDefinitionId(),
+			_cpAttachmentFileEntry.getCPAttachmentFileEntryId(),
+			RandomTestUtil.randomString(), RandomTestUtil.nextDouble(),
+			RandomTestUtil.randomString());
+	}
+
+	@Inject
+	private static CSDiagramSettingLocalService _csDiagramSettingLocalService;
+
+	@Inject
+	private static DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Inject
+	private static DLFolderLocalService _dlFolderLocalService;
+
+	@Inject
+	private CommerceCatalogLocalService _commerceCatalogLocalService;
+
+	private CPAttachmentFileEntry _cpAttachmentFileEntry;
+
+	@Inject
+	private CPAttachmentFileEntryLocalService
+		_cpAttachmentFileEntryLocalService;
+
+	private CPDefinition _cpDefinition;
+	private DLFileEntry _dlFileEntry;
+	private ServiceContext _serviceContext;
+
+}
