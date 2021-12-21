cfb242523bac59083a68f0d2926d542eb68f54ae
Cheryl Tang <cheryl.tang@liferay.com>
2021-12-21 15:32:21 -0800

    COMMERCE-8019 Create TableReferenceDefinitions for CSDiagramEntry, CSDiagramPin, CSDiagramSetting

---
+++ modules/apps/commerce/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/internal/change/tracking/spi/reference/CSDiagramPinTableReferenceDefinition.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.shop.by.diagram.internal.change.tracking.spi.reference;
+
+import com.liferay.change.tracking.spi.reference.TableReferenceDefinition;
+import com.liferay.change.tracking.spi.reference.builder.ChildTableReferenceInfoBuilder;
+import com.liferay.change.tracking.spi.reference.builder.ParentTableReferenceInfoBuilder;
+import com.liferay.commerce.product.model.CPDefinitionTable;
+import com.liferay.commerce.shop.by.diagram.model.CSDiagramPinTable;
+import com.liferay.commerce.shop.by.diagram.service.persistence.CSDiagramPinPersistence;
+import com.liferay.portal.kernel.service.persistence.BasePersistence;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cheryl Tang
+ */
+@Component(enabled = false, service = TableReferenceDefinition.class)
+public class CSDiagramPinTableReferenceDefinition
+	implements TableReferenceDefinition<CSDiagramPinTable> {
+
+	@Override
+	public void defineChildTableReferences(
+		ChildTableReferenceInfoBuilder<CSDiagramPinTable>
+			childTableReferenceInfoBuilder) {
+
+		childTableReferenceInfoBuilder.singleColumnReference(
+			CSDiagramPinTable.INSTANCE.CPDefinitionId,
+			CPDefinitionTable.INSTANCE.CPDefinitionId);
+	}
+
+	@Override
+	public void defineParentTableReferences(
+		ParentTableReferenceInfoBuilder<CSDiagramPinTable>
+			parentTableReferenceInfoBuilder) {
+	}
+
+	@Override
+	public BasePersistence<?> getBasePersistence() {
+		return _csDiagramPinPersistence;
+	}
+
+	@Override
+	public CSDiagramPinTable getTable() {
+		return CSDiagramPinTable.INSTANCE;
+	}
+
+	@Reference
+	private CSDiagramPinPersistence _csDiagramPinPersistence;
+
+}
