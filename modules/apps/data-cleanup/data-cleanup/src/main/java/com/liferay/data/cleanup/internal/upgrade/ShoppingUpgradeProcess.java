6b411bc4f70c8a0c76a12a5938068a052ddf074c
Kevin Lee <kevin.lee@liferay.com>
2021-12-21 12:36:27 -0800

    LPS-144207 Missed some class names and portlets

--- modules/apps/data-cleanup/data-cleanup/src/main/java/com/liferay/data/cleanup/internal/upgrade/ShoppingUpgradeProcess.java
+++ modules/apps/data-cleanup/data-cleanup/src/main/java/com/liferay/data/cleanup/internal/upgrade/ShoppingUpgradeProcess.java
@@ -38,11 +38,22 @@ public class ShoppingUpgradeProcess extends BaseUpgradeProcess {
 
 		removePortletData(
 			new String[] {"com.liferay.shopping.web"}, null,
-			new String[] {"com_liferay_shopping_web_portlet_ShoppingPortlet"});
+			new String[] {
+				"com_liferay_shopping_web_portlet_ShoppingPortlet",
+				"com.liferay.portlet.shopping"
+			});
 
 		removeServiceData(
 			"Shopping", new String[] {"com.liferay.shopping.service"},
 			new String[] {
+				"com.liferay.portlet.shopping.model.ShoppingCart",
+				"com.liferay.portlet.shopping.model.ShoppingCategory",
+				"com.liferay.portlet.shopping.model.ShoppingCoupon",
+				"com.liferay.portlet.shopping.model.ShoppingItem",
+				"com.liferay.portlet.shopping.model.ShoppingItemField",
+				"com.liferay.portlet.shopping.model.ShoppingItemPrice",
+				"com.liferay.portlet.shopping.model.ShoppingOrder",
+				"com.liferay.portlet.shopping.model.ShoppingOrderItem",
 				"com.liferay.shopping.model.ShoppingCart",
 				"com.liferay.shopping.model.ShoppingCategory",
 				"com.liferay.shopping.model.ShoppingCoupon",
