36a2fcb4fe3a96575a26055a7c26aac9cba3f899
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-12-23 16:02:58 +0100

    LPS-143509 Copies required vocabularies logic from layout-admin-web, no logic changes
    Copied from modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/add_layout.jsp and modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java

---
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/display/context/AddGroupDisplayContext.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.admin.web.internal.display.context;
+
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetCategoryServiceUtil;
+import com.liferay.asset.kernel.service.AssetVocabularyServiceUtil;
+import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.List;
+
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class AddGroupDisplayContext {
+
+	public AddGroupDisplayContext(
+		HttpServletRequest httpServletRequest, RenderResponse renderResponse) {
+
+		_httpServletRequest = httpServletRequest;
+		_renderResponse = renderResponse;
+	}
+
+	public String getAddGroupURL() {
+		return PortletURLBuilder.createActionURL(
+			_renderResponse
+		).setActionName(
+			"/site_admin/add_group"
+		).setBackURL(
+			ParamUtil.getString(
+				_httpServletRequest, "backURL",
+				ParamUtil.getString(_httpServletRequest, "redirect"))
+		).setParameter(
+			"creationType",
+			ParamUtil.getString(_httpServletRequest, "creationType")
+		).setParameter(
+			"layoutSetPrototypeId",
+			ParamUtil.getLong(_httpServletRequest, "layoutSetPrototypeId")
+		).setParameter(
+			"parentGroupId", _getParentGroupId()
+		).setParameter(
+			"siteInitializerKey",
+			ParamUtil.getString(_httpServletRequest, "siteInitializerKey")
+		).buildString();
+	}
+
+	public long[] getGroupIds() {
+		if (_groupsIds != null) {
+			return _groupsIds;
+		}
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)_httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		_groupsIds = new long[] {themeDisplay.getCompanyGroupId()};
+
+		return _groupsIds;
+	}
+
+	public boolean hasRequiredVocabularies() {
+		long classNameId = PortalUtil.getClassNameId(Group.class);
+
+		List<AssetVocabulary> assetVocabularies =
+			AssetVocabularyServiceUtil.getGroupVocabularies(getGroupIds());
+
+		for (AssetVocabulary assetVocabulary : assetVocabularies) {
+			if (assetVocabulary.isAssociatedToClassNameId(classNameId) &&
+				assetVocabulary.isRequired(classNameId, 0)) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean isShowCategorization() {
+		long classNameId = PortalUtil.getClassNameId(Group.class);
+
+		List<AssetVocabulary> assetVocabularies =
+			AssetVocabularyServiceUtil.getGroupVocabularies(getGroupIds());
+
+		for (AssetVocabulary assetVocabulary : assetVocabularies) {
+			if (assetVocabulary.isAssociatedToClassNameId(classNameId) &&
+				assetVocabulary.isRequired(classNameId, 0)) {
+
+				int assetVocabularyCategoriesCount =
+					AssetCategoryServiceUtil.getVocabularyCategoriesCount(
+						assetVocabulary.getGroupId(),
+						assetVocabulary.getVocabularyId());
+
+				if (assetVocabularyCategoriesCount > 0) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private long _getParentGroupId() {
+		if (_parentGroupId != null) {
+			return _parentGroupId;
+		}
+
+		_parentGroupId = ParamUtil.getLong(
+			_httpServletRequest, "parentGroupId");
+
+		return _parentGroupId;
+	}
+
+	private long[] _groupsIds;
+	private final HttpServletRequest _httpServletRequest;
+	private Long _parentGroupId;
+	private final RenderResponse _renderResponse;
+
+}
--- modules/apps/site/site-admin-web/src/main/resources/META-INF/resources/add_group.jsp
+++ modules/apps/site/site-admin-web/src/main/resources/META-INF/resources/add_group.jsp
@@ -16,3 +16,55 @@
 
 <%@ include file="/init.jsp" %>
 
+<%
+AddGroupDisplayContext addGroupDisplayContext = new AddGroupDisplayContext(request, renderResponse);
+%>
+
+<clay:container-fluid>
+	<liferay-frontend:edit-form
+		action="<%= addGroupDisplayContext.getAddGroupURL() %>"
+		method="post"
+		name="fm"
+		onSubmit="event.preventDefault();"
+	>
+		<liferay-frontend:edit-form-body>
+			<aui:input autoFocus="<%= true %>" label="name" name="name" required="<%= true %>" />
+
+			<c:if test="<%= addGroupDisplayContext.hasRequiredVocabularies() %>">
+				<aui:fieldset cssClass="mb-4">
+					<div class="h3 sheet-subtitle"><liferay-ui:message key="categorization" /></div>
+
+					<c:choose>
+						<c:when test="<%= addGroupDisplayContext.isShowCategorization() %>">
+							<liferay-asset:asset-categories-selector
+								className="<%= Group.class.getName() %>"
+								classPK="<%= 0 %>"
+								showOnlyRequiredVocabularies="<%= true %>"
+								visibilityTypes="<%= AssetVocabularyConstants.VISIBILITY_TYPES %>"
+							/>
+						</c:when>
+						<c:otherwise>
+							<div class="alert alert-warning text-justify">
+								<liferay-ui:message key="sites-have-required-vocabularies.-you-need-to-create-at-least-one-category-in-all-required-vocabularies-in-order-to-create-a-site" />
+							</div>
+						</c:otherwise>
+					</c:choose>
+				</aui:fieldset>
+			</c:if>
+		</liferay-frontend:edit-form-body>
+
+		<liferay-frontend:edit-form-footer>
+			<clay:button
+				id='<%= liferayPortletResponse.getNamespace() + "addButton" %>'
+				label="add"
+				type="submit"
+			/>
+
+			<clay:button
+				cssClass="btn-cancel"
+				displayType="secondary"
+				label="cancel"
+			/>
+		</liferay-frontend:edit-form-footer>
+	</liferay-frontend:edit-form>
+</clay:container-fluid>
