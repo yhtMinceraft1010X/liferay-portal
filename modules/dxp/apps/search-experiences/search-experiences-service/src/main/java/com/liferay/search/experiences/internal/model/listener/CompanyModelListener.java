5ee9af283660e6c5e37f84e4601135fdc906018b
Bryan Engler <bryan.engler@liferay.com>
2021-12-23 14:30:38 -0800

    LPS-144380 search-experiences-service: Set schema version in CompanyModelListener
    https://issues.liferay.com/browse/LPS-144380
    
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/model/listener/CompanyModelListener.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/model/listener/CompanyModelListener.java
@@ -88,6 +88,7 @@ public class CompanyModelListener extends BaseModelListener<Company> {
 				LocalizedMapUtil.getLocalizedMap(
 					sxpElement.getDescription_i18n()),
 				String.valueOf(sxpElement.getElementDefinition()), true,
+				_SCHEMA_VERSION,
 				LocalizedMapUtil.getLocalizedMap(sxpElement.getTitle_i18n()), 0,
 				new ServiceContext() {
 					{
@@ -158,6 +159,12 @@ public class CompanyModelListener extends BaseModelListener<Company> {
 
 		return sxpElements;
 	}
+
+	private static final String _SCHEMA_VERSION = StringUtil.replace(
+		StringUtil.extractFirst(
+			StringUtil.extractLast(SXPElement.class.getName(), ".v"),
+			CharPool.PERIOD),
+		CharPool.UNDERLINE, CharPool.PERIOD);
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		CompanyModelListener.class);
