84b9cd334bdf060517f17384d963a077181de363
Joao Paulo Pontes <joao.pontes@liferay.com>
2021-12-28 18:12:13 -0300

    LPS-144542 Create TaskNode Component

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/nodes/state/TaskNode.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import PropTypes from 'prop-types';
+import React, {useContext} from 'react';
+
+import {DefinitionBuilderContext} from '../../../../DefinitionBuilderContext';
+import BaseNode from '../BaseNode';
+
+export default function TaskNode({
+	data: {description, label, newNode} = {},
+	descriptionSidebar,
+	id,
+	...otherProps
+}) {
+	const {defaultLanguageId} = useContext(DefinitionBuilderContext);
+
+	if (!label || !label[defaultLanguageId]) {
+		const defaultLanguageId = themeDisplay.getLanguageId();
+
+		label = {
+			[defaultLanguageId]: Liferay.Language.get('task'),
+		};
+	}
+
+	return (
+		<BaseNode
+			className="task-node"
+			description={description}
+			descriptionSidebar={descriptionSidebar}
+			icon="check-circle-full"
+			id={id}
+			label={label}
+			newNode={newNode}
+			type="task"
+			{...otherProps}
+		/>
+	);
+}
+
+TaskNode.propTypes = {
+	data: PropTypes.object,
+	descriptionSidebar: PropTypes.string,
+	id: PropTypes.string.isRequired,
+};
