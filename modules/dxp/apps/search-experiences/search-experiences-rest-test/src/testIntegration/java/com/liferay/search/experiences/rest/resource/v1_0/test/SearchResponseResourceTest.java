0e47366a64f534c6ed057853b69d8858700c3af3
André de Oliveira <andre.oliveira@liferay.com>
2021-12-28 16:49:26 -0600

    Revert "LPS-143971 search-experiences-rest-test: Cleanup SearchResponseResource test case"
    This reverts commit 52d84e9e66c026e92d652658564ec645c2cf10d3.

--- modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/SearchResponseResourceTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/SearchResponseResourceTest.java
@@ -154,6 +154,8 @@ public class SearchResponseResourceTest
 
 				SearchResponse searchResponse = _postSearch(_read());
 
+				Assert.assertNull(searchResponse.getResponse());
+
 				Assert.assertThat(
 					searchResponse.getResponseString(),
 					CoreMatchers.containsString(message));
