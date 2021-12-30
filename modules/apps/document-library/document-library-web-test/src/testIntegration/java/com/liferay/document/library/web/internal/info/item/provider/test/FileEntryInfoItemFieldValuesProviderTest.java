71bc66ae109083c7a7055606c9d1dbc95a27fcfa
Yang Cao <yang.cao@liferay.com>
2021-12-30 17:50:19 +0800

    LPS-141754 Add test to assert file entry info item field reader

--- modules/apps/document-library/document-library-web-test/src/testIntegration/java/com/liferay/document/library/web/internal/info/item/provider/test/FileEntryInfoItemFieldValuesProviderTest.java
+++ modules/apps/document-library/document-library-web-test/src/testIntegration/java/com/liferay/document/library/web/internal/info/item/provider/test/FileEntryInfoItemFieldValuesProviderTest.java
@@ -17,9 +17,13 @@ package com.liferay.document.library.web.internal.info.item.provider.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.info.field.InfoField;
 import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.field.type.TextInfoFieldType;
 import com.liferay.info.item.InfoItemFieldValues;
+import com.liferay.info.item.field.reader.InfoItemFieldReader;
 import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
@@ -29,6 +33,7 @@ import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -41,6 +46,11 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Cristina González
@@ -58,6 +68,28 @@ public class FileEntryInfoItemFieldValuesProviderTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testFileEntryInfoItemFieldReader() throws PortalException {
+		_registerFileEntryInfoItemFieldReader();
+
+		FileEntry fileEntry = _dlAppService.addFileEntry(
+			null, _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString(),
+			MimeTypes.MIME_APPLICATION_OCTET_STREAM,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			StringPool.BLANK, (byte[])null, null, null,
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+		InfoItemFieldValues infoItemFieldValues =
+			_infoItemFieldValuesProvider.getInfoItemFieldValues(fileEntry);
+
+		InfoFieldValue<Object> testInfoFieldValue =
+			infoItemFieldValues.getInfoFieldValue("Test");
+
+		Assert.assertEquals("Test", testInfoFieldValue.getValue());
 	}
 
 	@Test
@@ -93,6 +125,16 @@ public class FileEntryInfoItemFieldValuesProviderTest {
 			fileEntry.getModifiedDate(), publishDateInfoFieldValue.getValue());
 	}
 
+	private void _registerFileEntryInfoItemFieldReader() {
+		Bundle bundle = FrameworkUtil.getBundle(InfoItemFieldReader.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_serviceRegistration = bundleContext.registerService(
+			InfoItemFieldReader.class, new TestFileEntryInfoItemFieldReader(),
+			new HashMapDictionary<String, String>());
+	}
+
 	@Inject
 	private DLAppService _dlAppService;
 
@@ -101,5 +143,34 @@ public class FileEntryInfoItemFieldValuesProviderTest {
 
 	@Inject(filter = "component.name=*.FileEntryInfoItemFieldValuesProvider")
 	private InfoItemFieldValuesProvider _infoItemFieldValuesProvider;
+
+	private ServiceRegistration<InfoItemFieldReader> _serviceRegistration;
+
+	private static class TestFileEntryInfoItemFieldReader
+		implements InfoItemFieldReader<FileEntry> {
+
+		@Override
+		public InfoField getField() {
+			return null;
+		}
+
+		@Override
+		public InfoField getInfoField() {
+			return InfoField.builder(
+			).infoFieldType(
+				TextInfoFieldType.INSTANCE
+			).name(
+				"Test"
+			).labelInfoLocalizedValue(
+				InfoLocalizedValue.localize(getClass(), "Test")
+			).build();
+		}
+
+		@Override
+		public Object getValue(FileEntry model) {
+			return "Test";
+		}
+
+	}
 
 }
