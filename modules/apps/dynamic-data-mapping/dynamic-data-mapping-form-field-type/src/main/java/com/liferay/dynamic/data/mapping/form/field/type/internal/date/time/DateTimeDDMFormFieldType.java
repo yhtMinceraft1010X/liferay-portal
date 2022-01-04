5719026c9c0a16acda2aaf796a5d99d73aac2c46
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2022-01-04 15:11:25 -0300

    LPS-144714 Create new DateTime field type

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/date/time/DateTimeDDMFormFieldType.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.date.time;
+
+import com.liferay.dynamic.data.mapping.form.field.type.BaseDDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeSettings;
+import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rodrigo Paulino
+ */
+@Component(
+	immediate = true,
+	property = {
+		"ddm.form.field.type.description=date-time-field-type-description",
+		"ddm.form.field.type.display.order:Integer=6",
+		"ddm.form.field.type.group=basic", "ddm.form.field.type.icon=time",
+		"ddm.form.field.type.label=date-and-time",
+		"ddm.form.field.type.name=" + DDMFormFieldTypeConstants.DATE_TIME,
+		"ddm.form.field.type.scope=forms"
+	},
+	service = DDMFormFieldType.class
+)
+public class DateTimeDDMFormFieldType extends BaseDDMFormFieldType {
+
+	@Override
+	public Class<? extends DDMFormFieldTypeSettings>
+		getDDMFormFieldTypeSettings() {
+
+		return DateTimeDDMFormFieldTypeSettings.class;
+	}
+
+	@Override
+	public String getModuleName() {
+		return "dynamic-data-mapping-form-field-type/DatePicker/DatePicker.es";
+	}
+
+	@Override
+	public String getName() {
+		return DDMFormFieldTypeConstants.DATE_TIME;
+	}
+
+}
