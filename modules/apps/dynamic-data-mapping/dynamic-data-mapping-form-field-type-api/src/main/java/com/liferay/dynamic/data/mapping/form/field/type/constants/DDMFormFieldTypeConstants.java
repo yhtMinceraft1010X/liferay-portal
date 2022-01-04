74559dd9401f599f1d3d7912b0525bc6b1ecf678
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2022-01-04 11:56:02 -0300

    LPS-144712 Add date_time constant

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type-api/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/constants/DDMFormFieldTypeConstants.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type-api/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/constants/DDMFormFieldTypeConstants.java
@@ -29,6 +29,8 @@ public class DDMFormFieldTypeConstants {
 
 	public static final String DATE = "date";
 
+	public static final String DATE_TIME = "date_time";
+
 	public static final String DDM_IMAGE = "ddm-image";
 
 	public static final String DOCUMENT_LIBRARY = "document_library";
