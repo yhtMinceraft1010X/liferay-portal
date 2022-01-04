f19ee9ce67fc22306db731cea0d43515123f1ae1
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2022-01-04 19:05:52 -0300

    LPS-144717 Add test

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/form/validation/util/DateParameterUtilTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/form/validation/util/DateParameterUtilTest.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 import org.junit.Assert;
@@ -59,6 +60,17 @@ public class DateParameterUtilTest {
 
 		Assert.assertNull(DateParameterUtil.getLocalDate(null));
 		Assert.assertNull(DateParameterUtil.getLocalDate(StringPool.BLANK));
+	}
+
+	@Test
+	public void testGetLocalDateTime() {
+		LocalDateTime localDateTime = DateParameterUtil.getLocalDateTime(
+			"2021-10-28 1:00");
+
+		Assert.assertEquals("2021-10-28T01:00", localDateTime.toString());
+
+		Assert.assertNull(DateParameterUtil.getLocalDateTime(null));
+		Assert.assertNull(DateParameterUtil.getLocalDateTime(StringPool.BLANK));
 	}
 
 	@Test
