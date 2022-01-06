49b32fd40284d4dcff897aef2b45ba86593bacc6
Nara Andrade <nara.andrade@liferay.com>
2022-01-06 19:07:05 -0300

    LPS-144900 Create join node

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/nodes/JoinNode.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import {defaultLanguageId} from '../../../constants';
+import BaseNode from './BaseNode';
+
+export default function JoinNode({
+	data: {description, label, newNode} = {},
+	descriptionSidebar,
+	id,
+	...otherProps
+}) {
+	if (!label || !label[defaultLanguageId]) {
+		label = {
+			[defaultLanguageId]: Liferay.Language.get('join'),
+		};
+	}
+
+	return (
+		<BaseNode
+			className="join-node"
+			description={description}
+			descriptionSidebar={descriptionSidebar}
+			icon="radio-button"
+			id={id}
+			label={label}
+			newNode={newNode}
+			type="join"
+			{...otherProps}
+		/>
+	);
+}
+
+JoinNode.propTypes = {
+	data: PropTypes.object,
+	descriptionSidebar: PropTypes.string,
+	id: PropTypes.string.isRequired,
+};
