0b4a20503d9511dfe3f9c8f7d3f61a30bf85960d
Nara Andrade <nara.andrade@liferay.com>
2022-01-06 16:51:48 -0300

    LPS-144899 Create tests for ForkNode

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/test/js/designer/definition-builder/diagram-builder/components/nodes/ForkNode.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {render} from '@testing-library/react';
+import React from 'react';
+
+import ForkNode from '../../../../../../../src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/nodes/ForkNode';
+import MockDefinitionBuilderContext from '../../../../../../mock/MockDefinitionBuilderContext';
+import MockDiagramBuilderContext from '../../../../../../mock/MockDiagramBuilderContext';
+
+describe('The ForkNode component should', () => {
+	let container;
+
+	it('Be rendered with correct icon and default values for label and description, if they are not set', () => {
+		const renderResult = render(
+			<MockDefinitionBuilderContext>
+				<MockDiagramBuilderContext>
+					<ForkNode />
+				</MockDiagramBuilderContext>
+			</MockDefinitionBuilderContext>
+		);
+
+		container = renderResult.container;
+
+		const icon = container.querySelector('svg.lexicon-icon-arrow-split');
+
+		expect(icon).toBeTruthy();
+
+		const label = container.querySelector('span.node-label');
+		const description = container.querySelector('span.node-description');
+
+		expect(label).toHaveTextContent('fork');
+		expect(description).toHaveTextContent(
+			'split-the-workflow-into-multiple-paths'
+		);
+	});
+
+	it('Be rendered with custom label and description', () => {
+		const renderResult = render(
+			<MockDefinitionBuilderContext>
+				<MockDiagramBuilderContext>
+					<ForkNode
+						data={{
+							description: 'test description',
+							label: {'en-US': 'test label'},
+						}}
+					/>
+				</MockDiagramBuilderContext>
+			</MockDefinitionBuilderContext>
+		);
+
+		container = renderResult.container;
+
+		const label = container.querySelector('span.node-label');
+		const description = container.querySelector('span.node-description');
+
+		expect(label).toHaveTextContent('test label');
+		expect(description).toHaveTextContent('test description');
+	});
+});
