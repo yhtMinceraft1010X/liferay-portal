eb69beb8f3d4aba745f4bc7b45c36881ba19f0cb
Keven Leone <keven.leone@liferay.com>
2022-01-07 14:48:39 -0300

    LPS-144994 Modal adjust styles
    Co-authored-by: Silas Silva <silas.silva@liferay.com>

--- modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/common/components/modal/index.js
+++ modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/common/components/modal/index.js
@@ -6,6 +6,7 @@ import useImperativeDisableScroll from '../../hooks/useImperativeDisableScroll';
 const MODAL_BACKDROP_ID = 'modal-backdrop';
 
 const Modal = ({
+	closeable = true,
 	children,
 	footer,
 	onClose = () => {},
@@ -42,38 +43,47 @@ const Modal = ({
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [show]);
 
+	if (!show) {
+		return null;
+	}
+
 	return (
-		<div
-			className={classNames(
-				'h-100 w-100 overflow-auto position-fixed backdrop',
-				{
-					'd-block': show,
-					'd-none': !show,
-				}
-			)}
-			id={MODAL_BACKDROP_ID}
-		>
+		<>
 			<div
 				className={classNames(
-					'align-items-stretch bg-neutral-0 d-flex flex-column m-auto modal-content-body position-absolute pb-4 pt-3 px-3 rounded',
+					'h-100 w-100 overflow-auto position-fixed backdrop',
+					{
+						'd-block': show,
+						'd-none': !show,
+					}
+				)}
+				id={MODAL_BACKDROP_ID}
+			/>
+
+			<div
+				className={classNames(
+					'align-items-stretch bg-neutral-0 d-flex flex-column m-auto modal-content-body position-fixed pb-4 pt-3 px-3 rounded',
 					{
 						'modal-large': size === 'large',
 						'modal-medium': size === 'medium',
 						'modal-small': size === 'small',
+						'modal-small-mobile': size === 'small-mobile',
 					}
 				)}
 			>
-				<div className="align-items-center border-bottom-0 d-flex justify-content-end p-0">
-					<div className="close-modal" onClick={onClose}>
-						<ClayIcon symbol="times" />
+				{closeable && (
+					<div className="align-items-center border-bottom-0 d-flex justify-content-end p-0">
+						<div className="close-modal" onClick={onClose}>
+							<ClayIcon symbol="times" />
+						</div>
 					</div>
-				</div>
+				)}
 
 				{children}
 
 				{footer}
 			</div>
-		</div>
+		</>
 	);
 };
 
