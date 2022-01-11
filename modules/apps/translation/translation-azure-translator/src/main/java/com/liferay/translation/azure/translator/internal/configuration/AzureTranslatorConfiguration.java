09f777e2fa11e353e17e4c68ae8a11ef4a507df5
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2022-01-11 15:17:16 +0100

    LPS-139938 Add Azure translator configuration

---
+++ modules/apps/translation/translation-azure-translator/src/main/java/com/liferay/translation/azure/translator/internal/configuration/AzureTranslatorConfiguration.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.translation.azure.translator.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Adolfo Pérez
+ */
+@ExtendedObjectClassDefinition(
+	category = "translation",
+	scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.translation.azure.translator.internal.configuration.AzureTranslatorConfiguration",
+	localization = "content/Language",
+	name = "azure-translator-configuration-name"
+)
+public interface AzureTranslatorConfiguration {
+
+	@Meta.AD(deflt = "false", name = "enabled", required = false)
+	public boolean enabled();
+
+	@Meta.AD(deflt = "", name = "subscription-key-name", required = false)
+	public String subscriptionKey();
+
+	@Meta.AD(deflt = "", name = "resource-location-name", required = false)
+	public String resourceLocation();
+
+}
---
+++ modules/apps/translation/translation-azure-translator/src/main/java/com/liferay/translation/azure/translator/internal/configuration/persistence/listener/AzureTranslatorConfigurationModelListener.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.translation.azure.translator.internal.configuration.persistence.listener;
+
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListenerException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.translation.azure.translator.internal.configuration.AzureTranslatorConfiguration;
+
+import java.util.Dictionary;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.translation.azure.translator.internal.configuration.AzureTranslatorConfiguration",
+	service = ConfigurationModelListener.class
+)
+public class AzureTranslatorConfigurationModelListener
+	implements ConfigurationModelListener {
+
+	@Override
+	public void onBeforeSave(String pid, Dictionary<String, Object> properties)
+		throws ConfigurationModelListenerException {
+
+		boolean enabled = GetterUtil.getBoolean(properties.get("enabled"));
+		String subscriptionKey = GetterUtil.getString(
+			properties.get("subscriptionKey"));
+		String resourceLocation = GetterUtil.getString(
+			properties.get("resourceLocation"));
+
+		if (enabled &&
+			(Validator.isNull(subscriptionKey) ||
+			 Validator.isNull(resourceLocation))) {
+
+			throw new ConfigurationModelListenerException(
+				ResourceBundleUtil.getString(
+					ResourceBundleUtil.getBundle(
+						LocaleThreadLocal.getThemeDisplayLocale(), getClass()),
+					"the-subscription-key-and-resource-location-must-not-be-" +
+						"empty"),
+				AzureTranslatorConfiguration.class, getClass(), properties);
+		}
+	}
+
+}
