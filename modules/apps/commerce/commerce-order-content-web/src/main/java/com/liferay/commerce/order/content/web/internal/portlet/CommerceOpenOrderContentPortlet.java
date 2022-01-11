d829a43e0ba8fc9634eef1e7a563726a9ae39caf
Danny Situ <danny.situ@liferay.com>
2022-01-10 16:59:29 -0800

    COMMERCE-8127 Fix ResourceBuilderURL portlet version 3 opt in error

--- modules/apps/commerce/commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/portlet/CommerceOpenOrderContentPortlet.java
+++ modules/apps/commerce/commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/portlet/CommerceOpenOrderContentPortlet.java
@@ -85,7 +85,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/pending_commerce_orders/view.jsp",
 		"javax.portlet.name=" + CommercePortletKeys.COMMERCE_OPEN_ORDER_CONTENT,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = {CommerceOpenOrderContentPortlet.class, Portlet.class}
 )
