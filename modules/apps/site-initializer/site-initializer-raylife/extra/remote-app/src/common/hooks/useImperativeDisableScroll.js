5b4ea70cdc1729ed0ee873d684c65611a678d85e
Keven Leone <keven.leone@liferay.com>
2022-01-12 14:39:24 -0300

    LPS-145242 Disable Scroll for Mobile Phone when Modal is opened

--- modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/common/hooks/useImperativeDisableScroll.js
+++ modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/common/hooks/useImperativeDisableScroll.js
@@ -14,16 +14,34 @@
 
 import {useEffect} from 'react';
 
+const PROPERTY = {
+	overflowY: {
+		off: 'hidden',
+		on: 'scroll',
+	},
+	touchAction: {
+		off: 'none',
+		on: 'auto',
+	},
+};
+
 const useImperativeDisableScroll = ({disabled, element}) => {
 	useEffect(() => {
 		if (!element) {
 			return;
 		}
 
-		element.style.overflowY = disabled ? 'hidden' : 'scroll';
+		element.style['touch-action'] = disabled // on/off scroll for Mobile Phone
+			? PROPERTY.touchAction.off
+			: PROPERTY.touchAction.on;
+
+		element.style.overflowY = disabled // on/off scroll for Desktop Browser
+			? PROPERTY.overflowY.off
+			: PROPERTY.overflowY.on;
 
 		return () => {
-			element.style.overflowY = 'scroll';
+			element.style['touch-action'] = PROPERTY.overflowY.on;
+			element.style.overflowY = PROPERTY.overflowY.on;
 		};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [disabled]);
