6d9ffe21bd7da3afb854eac922223bb2fe3b2cff
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-01-13 15:52:38 +0000

    LPS-142961 Adds new resource command to get the markup of a field item

---
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/action/GetFieldItemMVCResourceCommand.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.web.internal.portlet.action;
+
+import com.liferay.asset.list.constants.AssetListPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + AssetListPortletKeys.ASSET_LIST,
+		"mvc.command.name=/asset_list/get_field_item"
+	},
+	service = MVCResourceCommand.class
+)
+public class GetFieldItemMVCResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		include(
+			resourceRequest, resourceResponse,
+			"/asset_list/select_structure_field_item.jsp");
+	}
+
+}
