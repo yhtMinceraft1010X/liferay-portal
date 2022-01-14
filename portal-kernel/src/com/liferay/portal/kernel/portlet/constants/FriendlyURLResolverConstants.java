425e085f565de22c838a951af6edb54d4af92662
Guilherme Camacho <guilherme.camacho@liferay.com>
2022-01-14 12:26:19 -0300

    LPS-145408 Create a class of constants to centralize friendly url separators

---
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/constants/FriendlyURLResolverConstants.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.portlet.constants;
+
+import com.liferay.portal.kernel.util.Portal;
+
+/**
+ * @author Guilherme Camacho
+ */
+public class FriendlyURLResolverConstants {
+
+	public static final String URL_SEPARATOR_ASSET_CATEGORY = "/v/";
+
+	public static final String URL_SEPARATOR_BLOGS_ENTRY = "/b/";
+
+	public static final String URL_SEPARATOR_FILE_ENTRY = "/d/";
+
+	public static final String URL_SEPARATOR_JOURNAL_ARTICLE = "/w/";
+
+	public static final String URL_SEPARATOR_OBJECT_ENTRY = "/l/";
+
+	public static final String URL_SEPARATOR_PORTAL_RESERVED =
+		Portal.PATH_MODULE;
+
+}
