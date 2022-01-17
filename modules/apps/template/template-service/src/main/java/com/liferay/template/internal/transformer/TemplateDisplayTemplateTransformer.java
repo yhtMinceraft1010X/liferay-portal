9fe945b16c2e9c921cdcaec9a7f470db5d5130df
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-01-17 16:05:56 +0100

    LPS-145467 Handles properly CategoriesInfoFieldType.

--- modules/apps/template/template-service/src/main/java/com/liferay/template/internal/transformer/TemplateDisplayTemplateTransformer.java
+++ modules/apps/template/template-service/src/main/java/com/liferay/template/internal/transformer/TemplateDisplayTemplateTransformer.java
@@ -18,8 +18,11 @@ import com.liferay.dynamic.data.mapping.model.DDMTemplate;
 import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalServiceUtil;
 import com.liferay.info.field.InfoField;
 import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.field.type.CategoriesInfoFieldType;
+import com.liferay.info.field.type.InfoFieldType;
 import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.info.type.categorization.Category;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -31,14 +34,17 @@ import com.liferay.portal.kernel.templateparser.TemplateNode;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.templateparser.Transformer;
 import com.liferay.portlet.display.template.PortletDisplayTemplate;
 import com.liferay.portlet.display.template.constants.PortletDisplayTemplateConstants;
 import com.liferay.template.model.TemplateEntry;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Lourdes Fernández Besada
@@ -89,10 +95,8 @@ public class TemplateDisplayTemplateTransformer {
 				continue;
 			}
 
-			TemplateNode templateNode = new TemplateNode(
-				themeDisplay, infoField.getName(),
-				String.valueOf(infoFieldValue.getValue(locale)),
-				StringPool.BLANK, new HashMap<>());
+			TemplateNode templateNode = _createTemplateNode(
+				infoField, infoFieldValue, themeDisplay);
 
 			contextObjects.put(infoField.getName(), templateNode);
 		}
@@ -110,6 +114,67 @@ public class TemplateDisplayTemplateTransformer {
 			themeDisplay, contextObjects, ddmTemplate.getScript(),
 			TemplateConstants.LANG_TYPE_FTL, new UnsyncStringWriter(),
 			themeDisplay.getRequest(), themeDisplay.getResponse());
+	}
+
+	private TemplateNode _createTemplateNode(
+		InfoField<?> infoField, InfoFieldValue<Object> infoFieldValue,
+		ThemeDisplay themeDisplay) {
+
+		Object data = infoFieldValue.getValue(themeDisplay.getLocale());
+
+		if (Validator.isNull(data)) {
+			return new TemplateNode(
+				themeDisplay, infoField.getName(), StringPool.BLANK,
+				StringPool.BLANK, new HashMap<>());
+		}
+
+		InfoFieldType infoFieldType = infoField.getInfoFieldType();
+
+		if (Objects.equals(CategoriesInfoFieldType.INSTANCE, infoFieldType)) {
+			List<Category> categories = (List<Category>)data;
+
+			if (categories.isEmpty()) {
+				return new TemplateNode(
+					themeDisplay, infoField.getName(), StringPool.BLANK,
+					StringPool.BLANK, new HashMap<>());
+			}
+
+			Category firstCategory = categories.get(0);
+
+			TemplateNode templateNode = new TemplateNode(
+				themeDisplay, infoField.getName(),
+				firstCategory.getLabel(themeDisplay.getLocale()),
+				infoFieldType.getName(),
+				HashMapBuilder.put(
+					"key", firstCategory.getKey()
+				).put(
+					"label", firstCategory.getLabel(themeDisplay.getLocale())
+				).build());
+
+			templateNode.appendSibling(templateNode);
+
+			for (int i = 1; i < categories.size(); i++) {
+				Category siblingCategory = categories.get(i);
+
+				templateNode.appendSibling(
+					new TemplateNode(
+						themeDisplay, infoField.getName(),
+						siblingCategory.getLabel(themeDisplay.getLocale()),
+						infoFieldType.getName(),
+						HashMapBuilder.put(
+							"key", siblingCategory.getKey()
+						).put(
+							"label",
+							siblingCategory.getLabel(themeDisplay.getLocale())
+						).build()));
+			}
+
+			return templateNode;
+		}
+
+		return new TemplateNode(
+			themeDisplay, infoField.getName(), String.valueOf(data),
+			StringPool.BLANK, new HashMap<>());
 	}
 
 	private final InfoItemFieldValues _infoItemFieldValues;
