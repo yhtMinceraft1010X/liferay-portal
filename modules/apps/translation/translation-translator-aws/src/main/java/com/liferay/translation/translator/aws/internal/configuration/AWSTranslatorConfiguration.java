60e6ebf2fe4f7f26b3c4d1528e5cd6cf07ec59e1
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2022-01-17 15:18:00 +0100

    LPS-139939 Create configuration for translator

---
+++ modules/apps/translation/translation-translator-aws/src/main/java/com/liferay/translation/translator/aws/internal/configuration/AWSTranslatorConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.translation.translator.aws.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Adolfo Pérez
+ */
+@ExtendedObjectClassDefinition(
+	category = "translation",
+	scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.translation.translator.aws.internal.configuration.AWSTranslatorConfiguration",
+	localization = "content/Language",
+	name = "aws-translator-configuration-name"
+)
+public interface AWSTranslatorConfiguration {
+
+	@Meta.AD(deflt = "false", name = "enabled", required = false)
+	public boolean enabled();
+
+	@Meta.AD(deflt = "", name = "access-key", required = false)
+	public String accessKey();
+
+	@Meta.AD(deflt = "", name = "secret-key", required = false)
+	public String secretKey();
+
+	@Meta.AD(deflt = "us-west-1", name = "region", required = false)
+	public String region();
+
+}
---
+++ modules/apps/translation/translation-translator-aws/src/main/java/com/liferay/translation/translator/aws/internal/configuration/persistence/listener/AWSTranslatorConfigurationModelListener.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.translation.translator.aws.internal.configuration.persistence.listener;
+
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListenerException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.translation.translator.aws.internal.configuration.AWSTranslatorConfiguration;
+
+import java.util.Dictionary;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.translation.translator.aws.internal.configuration.AWSTranslatorConfiguration",
+	service = ConfigurationModelListener.class
+)
+public class AWSTranslatorConfigurationModelListener
+	implements ConfigurationModelListener {
+
+	@Override
+	public void onBeforeSave(String pid, Dictionary<String, Object> properties)
+		throws ConfigurationModelListenerException {
+
+		if (!GetterUtil.getBoolean(properties.get("enabled"))) {
+			return;
+		}
+
+		if (Validator.isNull(
+				GetterUtil.getString(properties.get("accessKey"))) ||
+			Validator.isNull(
+				GetterUtil.getString(properties.get("secretKey")))) {
+
+			throw new ConfigurationModelListenerException(
+				"access-key-and-secret-key-must-not-be-empty",
+				AWSTranslatorConfiguration.class, getClass(), properties);
+		}
+	}
+
+}
