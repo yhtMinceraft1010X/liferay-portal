b0797f76307f2d5e80efbc8596f8600035e38cb7
arthurchan35 <arthurchan35@gmail.com>
2022-01-18 16:49:33 +0800

    LPS-145190 Track in-assertion configuration to build verifier and userSubject

--- modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/grant/handler/LiferayJWTBearerGrantHandler.java
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/grant/handler/LiferayJWTBearerGrantHandler.java
@@ -15,19 +15,36 @@
 package com.liferay.oauth2.provider.rest.internal.endpoint.access.token.grant.handler;
 
 import com.liferay.oauth2.provider.configuration.OAuth2ProviderConfiguration;
+import com.liferay.oauth2.provider.rest.internal.configuration.OAuth2InAssertionConfiguration;
 import com.liferay.oauth2.provider.rest.internal.endpoint.liferay.LiferayOAuthDataProvider;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.util.GetterUtil;
+import org.apache.cxf.rs.security.jose.jwk.JsonWebKey;
+import org.apache.cxf.rs.security.jose.jwk.JsonWebKeys;
+import org.apache.cxf.rs.security.jose.jwk.JwkUtils;
+import org.apache.cxf.rs.security.jose.jwk.PublicKeyUse;
+import org.apache.cxf.rs.security.jose.jws.JwsSignatureVerifier;
+import org.apache.cxf.rs.security.jose.jws.JwsUtils;
 import org.apache.cxf.rs.security.oauth2.common.Client;
 import org.apache.cxf.rs.security.oauth2.grants.jwt.Constants;
 import org.apache.cxf.rs.security.oauth2.grants.jwt.JwtBearerGrantHandler;
 import org.apache.cxf.rs.security.oauth2.provider.AccessTokenGrantHandler;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedServiceFactory;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.ws.rs.core.MultivaluedMap;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -35,9 +52,65 @@ import java.util.Map;
  */
 @Component(
 	configurationPid = "com.liferay.oauth2.provider.configuration.OAuth2ProviderConfiguration",
-	service = {AccessTokenGrantHandler.class}
+	property = org.osgi.framework.Constants.SERVICE_PID + "=com.liferay.oauth2.provider.rest.internal.configuration.OAuth2InAssertionConfiguration",
+	service = {AccessTokenGrantHandler.class, ManagedServiceFactory.class}
 )
-public class LiferayJWTBearerGrantHandler extends BaseAccessTokenGrantHandler {
+public class LiferayJWTBearerGrantHandler
+	extends BaseAccessTokenGrantHandler implements ManagedServiceFactory {
+
+	@Override
+	public void deleted(String pid) {
+		Dictionary<String, ?> properties = _configurationPidsProperties.remove(
+			pid);
+
+		long companyId = GetterUtil.getLong(properties.get("companyId"));
+
+		if (companyId == CompanyConstants.SYSTEM) {
+			_rebuild();
+		}
+		else {
+			_rebuild(companyId);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public void updated(String pid, Dictionary<String, ?> properties)
+		throws ConfigurationException {
+
+		Dictionary<String, ?> oldProperties = _configurationPidsProperties.put(
+			pid, properties);
+
+		long companyId = GetterUtil.getLong(
+			properties.get("companyId"), CompanyConstants.SYSTEM);
+
+		if (companyId == CompanyConstants.SYSTEM) {
+			_rebuild();
+
+			return;
+		}
+
+		if (oldProperties != null) {
+			long oldCompanyId = GetterUtil.getLong(
+				oldProperties.get("companyId"));
+
+			if (oldCompanyId == CompanyConstants.SYSTEM) {
+				_rebuild();
+
+				return;
+			}
+
+			if (oldCompanyId != companyId) {
+				_rebuild(oldCompanyId);
+			}
+		}
+
+		_rebuild(companyId);
+	}
 
 	@Activate
 	protected void activate(Map<String, Object> properties) {
@@ -70,14 +143,161 @@ public class LiferayJWTBearerGrantHandler extends BaseAccessTokenGrantHandler {
 		return _oAuth2ProviderConfiguration.allowJWTBearerGrant();
 	}
 
+	private void _rebuild() {
+		_rebuildHelper(CompanyConstants.SYSTEM);
+
+		for (Long key : _jwsSignatureVerifiers.keySet()) {
+			if (key == CompanyConstants.SYSTEM) {
+				continue;
+			}
+
+			_rebuild(key);
+		}
+	}
+
+	private void _rebuild(long companyId) {
+		_rebuildHelper(companyId);
+
+		// Merge signature verifiers from system settings
+
+		Map<String, Map<String, JwsSignatureVerifier>> jwsSignatureVerifiers =
+			_jwsSignatureVerifiers.get(companyId);
+
+		Map<String, Map<String, JwsSignatureVerifier>>
+			systemJWSSignatureVerifiers = _jwsSignatureVerifiers.get(
+				CompanyConstants.SYSTEM);
+
+		for (Map.Entry<String, Map<String, JwsSignatureVerifier>> entry :
+				systemJWSSignatureVerifiers.entrySet()) {
+
+			if (jwsSignatureVerifiers.containsKey(entry.getKey())) {
+				continue;
+			}
+
+			jwsSignatureVerifiers.put(entry.getKey(), entry.getValue());
+		}
+
+		// Merge user auth types from system settings
+
+		Map<String, String> userAuthTypes = _userAuthTypes.get(companyId);
+
+		Map<String, String> systemUserAuthTypes = _userAuthTypes.get(
+			CompanyConstants.SYSTEM);
+
+		for (Map.Entry<String, String> entry : systemUserAuthTypes.entrySet()) {
+			if (userAuthTypes.containsKey(entry.getKey())) {
+				continue;
+			}
+
+			userAuthTypes.put(entry.getKey(), entry.getValue());
+		}
+	}
+
+	private Map<String, Map<String, JwsSignatureVerifier>> _rebuildHelper(
+		long companyId) {
+
+		_jwsSignatureVerifiers.put(companyId, new HashMap<>());
+
+		_userAuthTypes.put(companyId, new HashMap<>());
+
+		Map<String, Map<String, JwsSignatureVerifier>> jwsSignatureVerifiers =
+			_jwsSignatureVerifiers.get(companyId);
+
+		Map<String, String> userAuthTypes = _userAuthTypes.get(companyId);
+
+		for (Dictionary<String, ?> properties :
+				_configurationPidsProperties.values()) {
+
+			if (companyId != GetterUtil.getLong(properties.get("companyId"))) {
+				continue;
+			}
+
+			OAuth2InAssertionConfiguration oAuth2InAssertionConfiguration =
+				ConfigurableUtil.createConfigurable(
+					OAuth2InAssertionConfiguration.class, properties);
+
+			String issuer = oAuth2InAssertionConfiguration.issuer();
+
+			if (jwsSignatureVerifiers.containsKey(issuer)) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						StringBundler.concat(
+							"Duplicated issuer name {", issuer, "} will be ",
+							"discarded. Please check your OAuth In-assertion",
+							"configuration."));
+				}
+
+				continue;
+			}
+
+			jwsSignatureVerifiers.put(issuer, new HashMap<>());
+
+			userAuthTypes.put(
+				issuer, oAuth2InAssertionConfiguration.userAuthType());
+
+			Map<String, JwsSignatureVerifier> kidsJwsSignatureVerifiers =
+				jwsSignatureVerifiers.get(issuer);
+
+			JsonWebKeys jsonWebKeys = JwkUtils.readJwkSet(
+				oAuth2InAssertionConfiguration.signatureJSONWebKeySet());
+
+			for (JsonWebKey jsonWebKey : jsonWebKeys.getKeys()) {
+				PublicKeyUse publicKeyUse = jsonWebKey.getPublicKeyUse();
+
+				if ((publicKeyUse != null) &&
+					(publicKeyUse.compareTo(PublicKeyUse.ENCRYPT) == 0)) {
+
+					if (_log.isInfoEnabled()) {
+						_log.info(
+							StringBundler.concat(
+								"There is an encryption key {",
+								jsonWebKey.getKeyId(),
+								"} in your OAuth In-assertion configuration."));
+					}
+
+					continue;
+				}
+
+				if (kidsJwsSignatureVerifiers.containsKey(
+						jsonWebKey.getKeyId())) {
+
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							StringBundler.concat(
+								"Duplicated Assertion signature key {",
+								jsonWebKey.getKeyId(),
+								"} will be discarded. Please check your OAuth ",
+								"In-assertion configuration."));
+					}
+
+					continue;
+				}
+
+				kidsJwsSignatureVerifiers.put(
+					jsonWebKey.getKeyId(),
+					JwsUtils.getSignatureVerifier(jsonWebKey));
+			}
+		}
+
+		return jwsSignatureVerifiers;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		LiferayJWTBearerGrantHandler.class);
 
+	private final Map<String, Dictionary<String, ?>>
+		_configurationPidsProperties = Collections.synchronizedMap(
+			new LinkedHashMap<>());
+	private final Map<Long, Map<String, Map<String, JwsSignatureVerifier>>>
+		_jwsSignatureVerifiers = Collections.synchronizedMap(
+			new LinkedHashMap<>());
 	private JwtBearerGrantHandler _jwtBearerGrantHandler;
 
 	@Reference
 	private LiferayOAuthDataProvider _liferayOAuthDataProvider;
 
 	private OAuth2ProviderConfiguration _oAuth2ProviderConfiguration;
+	private final Map<Long, Map<String, String>> _userAuthTypes =
+		Collections.synchronizedMap(new LinkedHashMap<>());
 
 }
