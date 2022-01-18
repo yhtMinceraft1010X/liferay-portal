11d918a71221eeba7b144e93aededc7c2877a07a
marcelabc <marcela.cunha@liferay.com>
2022-01-18 15:54:59 -0300

    LPS-144868 Add object field type constants

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/constants/ObjectFieldTypeConstants.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.constants;
+
+/**
+ * @author Marcela Cunha
+ */
+public class ObjectFieldTypeConstants {
+
+	public static final String BUSINESS_TYPE_BOOLEAN = "Boolean";
+
+	public static final String BUSINESS_TYPE_DATE = "Date";
+
+	public static final String BUSINESS_TYPE_DECIMAL = "Decimal";
+
+	public static final String BUSINESS_TYPE_INTEGER = "Integer";
+
+	public static final String BUSINESS_TYPE_LARGE_FILE = "LargeFile";
+
+	public static final String BUSINESS_TYPE_LONG_INTEGER = "LongInteger";
+
+	public static final String BUSINESS_TYPE_LONG_TEXT = "LongText";
+
+	public static final String BUSINESS_TYPE_PICKLIST = "Picklist";
+
+	public static final String BUSINESS_TYPE_PRECISION_DECIMAL =
+		"PrecisionDecimal";
+
+	public static final String BUSINESS_TYPE_RELATIONSHIP = "Relationship";
+
+	public static final String BUSINESS_TYPE_TEXT = "Text";
+
+	public static final String DB_TYPE_BIG_DECIMAL = "BigDecimal";
+
+	public static final String DB_TYPE_BLOB = "Blob";
+
+	public static final String DB_TYPE_BOOLEAN = "Boolean";
+
+	public static final String DB_TYPE_CLOB = "Clob";
+
+	public static final String DB_TYPE_DATE = "Date";
+
+	public static final String DB_TYPE_DOUBLE = "Double";
+
+	public static final String DB_TYPE_INTEGER = "Integer";
+
+	public static final String DB_TYPE_LONG = "Long";
+
+	public static final String DB_TYPE_STRING = "String";
+
+}
