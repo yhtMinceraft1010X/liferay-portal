08d6b93dacd1e51c01fc56a44348ae7f1aa71811
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2022-01-17 21:10:18 -0300

    LPS-144896 Object View Resource Integration tests

--- modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectViewResourceTest.java
+++ modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectViewResourceTest.java
@@ -15,14 +15,142 @@
 package com.liferay.object.admin.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.admin.rest.client.dto.v1_0.ObjectView;
+import com.liferay.object.admin.rest.client.dto.v1_0.ObjectViewColumn;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.util.LocalizedMapUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
 
+import java.util.Collections;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class ObjectViewResourceTest extends BaseObjectViewResourceTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		String value = "A" + RandomTestUtil.randomString();
+
+		_objectDefinition =
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				LocalizedMapUtil.getLocalizedMap(value), value, null, null,
+				LocalizedMapUtil.getLocalizedMap(value),
+				ObjectDefinitionConstants.SCOPE_COMPANY,
+				Collections.emptyList());
+
+		_objectField = _objectFieldLocalService.addCustomObjectField(
+			TestPropsValues.getUserId(), 0,
+			_objectDefinition.getObjectDefinitionId(), "Text", "String", false,
+			false, null, LocalizedMapUtil.getLocalizedMap("Able"), "able",
+			true);
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+
+		if (_objectDefinition != null) {
+			_objectDefinitionLocalService.deleteObjectDefinition(
+				_objectDefinition.getObjectDefinitionId());
+		}
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetObjectView() {
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetObjectViewNotFound() {
+	}
+
+	@Override
+	protected ObjectView randomObjectView() throws Exception {
+		ObjectView objectView = super.randomObjectView();
+
+		objectView.setDefaultObjectView(false);
+		objectView.setName(
+			Collections.singletonMap("en-US", RandomTestUtil.randomString()));
+		objectView.setObjectDefinitionId(
+			_objectDefinition.getObjectDefinitionId());
+		objectView.setObjectViewColumns(
+			new ObjectViewColumn[] {_randomObjectViewColumn()});
+
+		return objectView;
+	}
+
+	@Override
+	protected ObjectView testDeleteObjectView_addObjectView() throws Exception {
+		return objectViewResource.postObjectDefinitionObjectView(
+			_objectDefinition.getObjectDefinitionId(), randomObjectView());
+	}
+
+	@Override
+	protected Long
+		testGetObjectDefinitionObjectViewsPage_getObjectDefinitionId() {
+
+		return _objectDefinition.getObjectDefinitionId();
+	}
+
+	@Override
+	protected ObjectView testGetObjectView_addObjectView() throws Exception {
+		return objectViewResource.postObjectDefinitionObjectView(
+			_objectDefinition.getObjectDefinitionId(), randomObjectView());
+	}
+
+	@Override
+	protected ObjectView testGraphQLObjectView_addObjectView()
+		throws Exception {
+
+		return objectViewResource.postObjectDefinitionObjectView(
+			_objectDefinition.getObjectDefinitionId(), randomObjectView());
+	}
+
+	@Override
+	protected ObjectView testPutObjectView_addObjectView() throws Exception {
+		return objectViewResource.postObjectDefinitionObjectView(
+			_objectDefinition.getObjectDefinitionId(), randomObjectView());
+	}
+
+	private ObjectViewColumn _randomObjectViewColumn() {
+		return new ObjectViewColumn() {
+			{
+				objectFieldName = _objectField.getName();
+				priority = RandomTestUtil.randomInt();
+			}
+		};
+	}
+
+	private ObjectDefinition _objectDefinition;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	private ObjectField _objectField;
+
+	@Inject
+	private ObjectFieldLocalService _objectFieldLocalService;
+
 }
