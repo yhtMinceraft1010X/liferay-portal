b7fb353f6772e56ee31795cda5d22652e1b77b93
Jorge García Jiménez <jorge.garcia@liferay.com>
2022-01-19 14:35:22 +0100

    LPS-145648 portal-kernel don't index empty keyword fields

--- portal-kernel/src/com/liferay/portal/kernel/search/DocumentImpl.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/DocumentImpl.java
@@ -48,6 +48,7 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * @author Brian Wing Shun Chan
@@ -400,9 +401,19 @@ public class DocumentImpl implements Document {
 			return;
 		}
 
-		createField(name, values);
+		Stream<String> valuesStream = Arrays.stream(values);
 
-		_createSortableTextField(name, true, values);
+		valuesStream.filter(value -> Validator.isNotNull(value));
+
+		String[] filteredValues = valuesStream.toArray(String[]::new);
+
+		if (ArrayUtil.isEmpty(filteredValues)) {
+			return;
+		}
+
+		createField(name, filteredValues);
+
+		_createSortableTextField(name, true, filteredValues);
 	}
 
 	@Override
