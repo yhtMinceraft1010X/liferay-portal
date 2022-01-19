419729e2afbfacf38fe4780577fbd206f82dfae7
Alessio Rendina <asilcomando@gmail.com>
2022-01-19 14:46:52 +0100

    COMMERCE-8215 Added commerce term portlet and portlet provider

---
+++ modules/apps/commerce/commerce-term-web/src/main/java/com/liferay/commerce/term/web/internal/portlet/CommerceTermEntryPortlet.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.term.web.internal.portlet;
+
+import com.liferay.commerce.term.constants.CommerceTermEntryPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=false",
+		"com.liferay.portlet.preferences-unique-per-layout=false",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.scopeable=true",
+		"javax.portlet.display-name=Terms and Conditions",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + CommerceTermEntryPortletKeys.COMMERCE_TERM_ENTRY,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = {CommerceTermEntryPortlet.class, Portlet.class}
+)
+public class CommerceTermEntryPortlet extends MVCPortlet {
+}
