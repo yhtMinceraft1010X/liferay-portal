e19dc13beb5cc936024d3426f9e53e20c06ad732
Erick Monteiro <erickhenriquehmc@gmail.com>
2022-01-20 17:29:03 -0300

    LPS-143802 (headless-admin-user-impl): Adds exception mapper NoSuchUserGroupExceptionMapper

---
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/jaxrs/exception/mapper/NoSuchUserGroupExceptionMapper.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.user.internal.jaxrs.exception.mapper;
+
+import com.liferay.portal.kernel.exception.NoSuchUserGroupException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Erick Monteiro
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.User)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.User.NoSuchUserGroupExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class NoSuchUserGroupExceptionMapper
+	extends BaseExceptionMapper<NoSuchUserGroupException> {
+
+	@Override
+	protected Problem getProblem(
+		NoSuchUserGroupException noSuchUserGroupException) {
+
+		return new Problem(Response.Status.NOT_FOUND, "User group not found");
+	}
+
+}
