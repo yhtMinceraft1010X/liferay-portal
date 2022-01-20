2a53ed21291e6be063ddf0d00f3bfae49db12115
Daniel Bonasser <daniel.bonasser@liferay.com>
2022-01-20 16:19:14 -0300

    LPS-144852 Create context to custom view
    Co-authored-by: Aline Cantarelli <aline.cantarelli@liferay.com>

---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/custom-view/context.tsx
@@ -0,0 +1,324 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React, {createContext, useReducer} from 'react';
+
+import {
+	TAction,
+	TObjectField,
+	TObjectView,
+	TObjectViewColumn,
+	TState,
+} from './types';
+interface IViewContextProps extends Array<TState | Function> {
+	0: typeof initialState;
+	1: React.Dispatch<React.ReducerAction<React.Reducer<TState, TAction>>>;
+}
+
+const ViewContext = createContext({} as IViewContextProps);
+
+const defaultLanguageId = Liferay.ThemeDisplay.getDefaultLanguageId();
+
+const METADATAS = [
+	{
+		checked: false,
+		filtered: true,
+		id: 1,
+		inLayout: true,
+		indexed: true,
+		indexedAsKeyword: true,
+		indexedLanguageId: '',
+		label: {[defaultLanguageId]: Liferay.Language.get('author')},
+		listTypeDefinitionId: true,
+		name: 'author',
+		required: false,
+		type: 'metadata',
+	},
+	{
+		checked: false,
+		filtered: true,
+		id: 2,
+		inLayout: true,
+		indexed: true,
+		indexedAsKeyword: true,
+		indexedLanguageId: '',
+		label: {[defaultLanguageId]: Liferay.Language.get('creation-date')},
+		listTypeDefinitionId: true,
+		name: 'creationDate',
+		required: false,
+		type: 'metadata',
+	},
+	{
+		checked: false,
+		filtered: true,
+		id: 3,
+		inLayout: true,
+		indexed: true,
+		indexedAsKeyword: true,
+		indexedLanguageId: '',
+		label: {[defaultLanguageId]: Liferay.Language.get('modified-date')},
+		listTypeDefinitionId: true,
+		name: 'modifiedDate',
+		required: false,
+		type: 'metadata',
+	},
+	{
+		checked: false,
+		filtered: true,
+		id: 4,
+		inLayout: true,
+		indexed: true,
+		indexedAsKeyword: true,
+		indexedLanguageId: '',
+		label: {
+			[defaultLanguageId]: Liferay.Language.get(
+				'workflow-status[object]'
+			),
+		},
+		listTypeDefinitionId: true,
+		name: 'workflowStatus',
+		required: false,
+		type: 'metadata',
+	},
+	{
+		checked: false,
+		filtered: true,
+		id: 5,
+		inLayout: true,
+		indexed: true,
+		indexedAsKeyword: true,
+		indexedLanguageId: '',
+		label: {[defaultLanguageId]: Liferay.Language.get('id')},
+		listTypeDefinitionId: true,
+		name: 'id',
+		required: false,
+		type: 'metadata',
+	},
+];
+
+export enum TYPES {
+	ADD_OBJECT_FIELDS = 'ADD_OBJECT_FIELDS',
+	ADD_OBJECT_VIEW = 'ADD_OBJECT_VIEW',
+	ADD_OBJECT_CUSTOM_VIEW_FIELD = 'ADD_OBJECT_CUSTOM_VIEW_FIELD',
+	ADD_OBJECT_VIEW_COLUMN = 'ADD_OBJECT_VIEW_COLUMN',
+	CHANGE_OBJECT_VIEW_NAME = 'CHANGE_OBJECT_VIEW_NAME',
+	CHANGE_OBJECT_VIEW_COLUMN_ORDER = 'CHANGE_OBJECT_VIEW_COLUMN_ORDER',
+	DELETE_OBJECT_VIEW_COLUMN = 'DELETE_OBJECT_VIEW_COLUMN',
+	DELETE_OBJECT_CUSTOM_VIEW_FIELD = 'DELETE_OBJECT_CUSTOM_VIEW_FIELD',
+	SET_OBJECT_VIEW_AS_DEFAULT = 'SET_OBJECT_VIEW_AS_DEFAULT',
+}
+
+const initialState = {
+	objectFields: [] as TObjectField[],
+	objectView: {} as TObjectView,
+} as TState;
+
+const viewReducer = (state: TState, action: TAction) => {
+	switch (action.type) {
+		case TYPES.ADD_OBJECT_VIEW: {
+			const {objectView} = action.payload;
+
+			return {
+				...state,
+				objectView,
+			};
+		}
+		case TYPES.ADD_OBJECT_VIEW_COLUMN: {
+			const {filteredItems} = action.payload;
+
+			const viewColumn: TObjectViewColumn[] = [];
+
+			filteredItems.map((field: TObjectField, index: number) => {
+				if (field.checked === true) {
+					viewColumn.push({
+						objectFieldName: field.label[defaultLanguageId],
+						priority: index,
+					});
+				}
+			});
+
+			const newViewColumn = viewColumn.map((viewColumn, index) => {
+				return {
+					...viewColumn,
+					priority: index,
+				};
+			});
+
+			const newObjectView = {
+				...state.objectView,
+				objectViewColumns: newViewColumn,
+			};
+
+			return {
+				...state,
+				objectFields: filteredItems,
+				objectView: newObjectView,
+			};
+		}
+		case TYPES.ADD_OBJECT_FIELDS: {
+			const {objectFields, objectViewColumns} = action.payload;
+
+			const objectFieldsWithCheck = objectFields.map(
+				(field: TObjectField) => {
+					return {
+						...field,
+						checked: false,
+						filtered: true,
+					};
+				}
+			);
+
+			const newObjectFields: TObjectField[] = [];
+
+			METADATAS.map((field) => {
+				newObjectFields.push(field);
+			});
+
+			objectFieldsWithCheck.map((field: TObjectField) => {
+				newObjectFields.push(field);
+			});
+
+			newObjectFields.forEach((field) => {
+				objectViewColumns.forEach(
+					(column: {objectFieldName: string}) => {
+						if (
+							column.objectFieldName ===
+							field.label[defaultLanguageId]
+						) {
+							field.checked = true;
+						}
+					}
+				);
+			});
+
+			return {
+				...state,
+				objectFields: newObjectFields,
+			};
+		}
+		case TYPES.CHANGE_OBJECT_VIEW_NAME: {
+			const {newName} = action.payload;
+
+			const newObjectView = {
+				...state.objectView,
+				name: {
+					[defaultLanguageId]: newName,
+				},
+			};
+
+			return {
+				...state,
+				objectView: newObjectView,
+			};
+		}
+		case TYPES.CHANGE_OBJECT_VIEW_COLUMN_ORDER: {
+			const {draggedIndex, targetIndex} = action.payload;
+
+			const newState = {...state};
+
+			const viewColumns = newState.objectView.objectViewColumns;
+
+			const dragged = viewColumns[draggedIndex];
+
+			viewColumns.splice(draggedIndex, 1);
+			viewColumns.splice(targetIndex, 0, dragged);
+
+			const newViewColumn = viewColumns.map((viewColumn, index) => {
+				return {
+					...viewColumn,
+					priority: index,
+				};
+			});
+
+			const newObjectView = {
+				...state.objectView,
+				objectViewColumns: newViewColumn,
+			};
+
+			return {
+				...state,
+				objectView: newObjectView,
+			};
+		}
+		case TYPES.DELETE_OBJECT_VIEW_COLUMN: {
+			const {objectFieldName} = action.payload;
+
+			const newState = {...state};
+
+			const viewColumn = newState.objectView?.objectViewColumns.filter(
+				(viewColumn) => viewColumn.objectFieldName !== objectFieldName
+			);
+
+			const newViewColumn = viewColumn.map((viewColumn, index) => {
+				return {
+					...viewColumn,
+					priority: index,
+				};
+			});
+
+			const newObjectView = {
+				...state.objectView,
+				objectViewColumns: newViewColumn,
+			};
+
+			return {
+				...state,
+				objectView: newObjectView,
+			};
+		}
+		case TYPES.SET_OBJECT_VIEW_AS_DEFAULT: {
+			const {checked} = action.payload;
+
+			const newObjectView = {
+				...state.objectView,
+				defaultObjectView: checked,
+			};
+
+			return {
+				...state,
+				objectView: newObjectView,
+			};
+		}
+		default:
+			return state;
+	}
+};
+
+interface IViewContextProviderProps extends React.HTMLAttributes<HTMLElement> {
+	value: {
+		isViewOnly: boolean;
+		objectViewId: string;
+	};
+}
+
+export function ViewContextProvider({
+	children,
+	value,
+}: IViewContextProviderProps) {
+	const [state, dispatch] = useReducer<React.Reducer<TState, TAction>>(
+		viewReducer,
+		{
+			...initialState,
+			...value,
+		}
+	);
+
+	return (
+		<ViewContext.Provider value={[state, dispatch]}>
+			{children}
+		</ViewContext.Provider>
+	);
+}
+
+export default ViewContext;
---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/custom-view/types.ts
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {TYPES} from './context';
+
+export type TName = {
+	[key: string]: string;
+};
+
+export type TObjectField = {
+	checked?: boolean;
+	filtered?: boolean | undefined;
+	id: number;
+	indexed: boolean;
+	indexedAsKeyword: boolean;
+	indexedLanguageId: string;
+	inLayout?: boolean; // eslint-disable-line @typescript-eslint/member-ordering
+	label: TName;
+	listTypeDefinitionId: boolean;
+	name: string;
+	required: boolean;
+	type: string;
+};
+
+export type TObjectViewColumn = {
+	objectFieldName: string;
+	priority?: number;
+};
+
+export type TObjectView = {
+	defaultObjectView: boolean;
+	name: TName;
+	objectViewColumns: TObjectViewColumn[];
+};
+
+export type TState = {
+	isViewOnly: boolean;
+	objectFields: TObjectField[];
+	objectView: TObjectView;
+	objectViewId: string;
+};
+
+export type TAction = {
+	payload: {[key: string]: any};
+	type: keyof typeof TYPES;
+};
