01bcf3ce95d22a39b04967f382471be003d7320b
Jair Medeiros <jair.medeiros@liferay.com>
2022-01-19 22:29:10 -0300

    LPS-145156 Add DXP page to Developer Keys

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/DXP/index.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+import DeveloperKeysLayouts from '../../components/DeveloperKeysLayout';
+
+const DXP = ({project, sessionId}) => {
+	return (
+		<div className="mr-4">
+			<DeveloperKeysLayouts>
+				<DeveloperKeysLayouts.Inputs
+					accountKey={project.accountKey}
+					downloadTextHelper="Select the Liferay DXP version for which you want to download a developer key."
+					dxpVersion={project.dxpVersion}
+					productTitle="DXP"
+					sessionId={sessionId}
+				></DeveloperKeysLayouts.Inputs>
+			</DeveloperKeysLayouts>
+		</div>
+	);
+};
+
+export default DXP;
