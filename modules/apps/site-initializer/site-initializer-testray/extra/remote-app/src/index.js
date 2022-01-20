51ebd347f0a758b1037b6327a5f47f1644e22d44
Keven Leone <keven.leone@liferay.com>
2022-01-20 13:38:36 -0300

    LPS-145837 Use React Router Dom to manage routes

--- modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/index.js
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/index.js
@@ -4,23 +4,24 @@ import ReactDOM from 'react-dom';
 import HelloBar from './routes/hello-bar/pages/HelloBar';
 import HelloFoo from './routes/hello-foo/pages/HelloFoo';
 import HelloWorld from './routes/hello-world/pages/HelloWorld';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import './common/styles/index.scss';
 
-const App = ({route}) => {
-	if (route === 'hello-bar') {
-		return <HelloBar />;
-	}
-
-	if (route === 'hello-foo') {
-		return <HelloFoo />;
-	}
-
-	return <HelloWorld />;
+const App = () => {
+	return (
+		<HashRouter>
+			<Routes>
+				<Route index element={<HelloWorld />} />
+				<Route path="/hello-foo" element={<HelloFoo />} />
+				<Route path="/hello-bar" element={<HelloBar />} />
+			</Routes>
+		</HashRouter>
+	);
 };
 
 class WebComponent extends HTMLElement {
 	connectedCallback() {
-		ReactDOM.render(<App route={this.getAttribute('route')} />, this);
+		ReactDOM.render(<App />, this);
 	}
 }
 
