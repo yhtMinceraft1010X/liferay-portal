0eff8d806fea4e7d5d2d00498d2564dcf88129f5
Brian Chan <brian.chan@liferay.com>
2022-01-21 06:03:03 -0800

    Revert "LPS-137761 Adds ballooneditor configuration conditionally"
    This reverts commit db8660590721165903801920d49e181f26e24ea0.

--- modules/apps/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsContentEditorConfigContributor.java
+++ modules/apps/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsContentEditorConfigContributor.java
@@ -84,9 +84,6 @@ public class BlogsContentEditorConfigContributor
 				"liferay-ui:input-editor:namespace"));
 		String name = GetterUtil.getString(
 			inputEditorTaglibAttributes.get("liferay-ui:input-editor:name"));
-		String editorName = GetterUtil.getString(
-			inputEditorTaglibAttributes.get(
-				"liferay-ui:input-editor:editorName"));
 
 		_populateFileBrowserURL(
 			jsonObject, requestBackedPortletURLFactory,
@@ -107,17 +104,15 @@ public class BlogsContentEditorConfigContributor
 				).buildString());
 		}
 
-		if (editorName.equals("ballooneditor")) {
-			jsonObject.put(
-				"extraPlugins",
-				"itemselector,stylescombo,ballooneditor," +
-					"videoembed,insertbutton,codemirror"
-			).put(
-				"toolbarText",
-				"Styles,Bold,Italic,Underline,BulletedList" +
-					",NumberedList,TextLink,SourceEditor"
-			);
-		}
+		jsonObject.put(
+			"extraPlugins",
+			"itemselector,stylescombo,ballooneditor," +
+				"videoembed,insertbutton,codemirror"
+		).put(
+			"toolbarText",
+			"Styles,Bold,Italic,Underline,BulletedList" +
+				",NumberedList,TextLink,SourceEditor"
+		);
 	}
 
 	private String _getAllowedContentLists() {
