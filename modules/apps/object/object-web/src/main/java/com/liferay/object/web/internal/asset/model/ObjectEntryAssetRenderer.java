c7993c7cbeee29680dd0541025652976ea2b4bc8
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-01-24 10:03:47 -0300

    LPS-145572 Create ObjectEntryAssetRenderer

---
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/asset/model/ObjectEntryAssetRenderer.java
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.web.internal.asset.model;
+
+import com.liferay.asset.kernel.model.BaseJSPAssetRenderer;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectEntryService;
+import com.liferay.object.web.internal.constants.ObjectWebKeys;
+import com.liferay.object.web.internal.object.entries.display.context.ObjectEntryDisplayContextFactory;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ * @author Feliphe Marinho
+ */
+public class ObjectEntryAssetRenderer
+	extends BaseJSPAssetRenderer<ObjectEntry> {
+
+	public ObjectEntryAssetRenderer(
+			ObjectDefinition objectDefinition, ObjectEntry objectEntry,
+			ObjectEntryDisplayContextFactory objectEntryDisplayContextFactory,
+			ObjectEntryService objectEntryService)
+		throws PortalException {
+
+		_objectDefinition = objectDefinition;
+		_objectEntry = objectEntry;
+		_objectEntryDisplayContextFactory = objectEntryDisplayContextFactory;
+		_objectEntryService = objectEntryService;
+	}
+
+	@Override
+	public ObjectEntry getAssetObject() {
+		return _objectEntry;
+	}
+
+	@Override
+	public String getClassName() {
+		return _objectEntry.getModelClassName();
+	}
+
+	@Override
+	public long getClassPK() {
+		return _objectEntry.getObjectEntryId();
+	}
+
+	@Override
+	public long getGroupId() {
+		return _objectEntry.getGroupId();
+	}
+
+	@Override
+	public String getJspPath(
+		HttpServletRequest httpServletRequest, String template) {
+
+		if (template.equals(TEMPLATE_ABSTRACT) ||
+			template.equals(TEMPLATE_FULL_CONTENT)) {
+
+			return "/object_entries/edit_object_entry.jsp";
+		}
+
+		return null;
+	}
+
+	@Override
+	public String getSummary(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		try {
+			return _objectEntry.getTitleValue();
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(portalException, portalException);
+			}
+		}
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public long getUserId() {
+		return _objectEntry.getUserId();
+	}
+
+	@Override
+	public String getUserName() {
+		return _objectEntry.getUserName();
+	}
+
+	@Override
+	public String getUuid() {
+		return _objectEntry.getUuid();
+	}
+
+	@Override
+	public boolean hasViewPermission(PermissionChecker permissionChecker)
+		throws PortalException {
+
+		try {
+			return _objectEntryService.hasModelResourcePermission(
+				_objectEntry, ActionKeys.VIEW);
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException, portalException);
+			}
+
+			return false;
+		}
+	}
+
+	@Override
+	public boolean include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String template)
+		throws Exception {
+
+		httpServletRequest.setAttribute(
+			ObjectWebKeys.OBJECT_DEFINITION, _objectDefinition);
+		httpServletRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			_objectEntryDisplayContextFactory.create(httpServletRequest, true));
+		httpServletRequest.setAttribute(
+			"objectEntryId", _objectEntry.getObjectEntryId());
+
+		return super.include(httpServletRequest, httpServletResponse, template);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ObjectEntryAssetRenderer.class);
+
+	private final ObjectDefinition _objectDefinition;
+	private final ObjectEntry _objectEntry;
+	private final ObjectEntryDisplayContextFactory
+		_objectEntryDisplayContextFactory;
+	private final ObjectEntryService _objectEntryService;
+
+}
