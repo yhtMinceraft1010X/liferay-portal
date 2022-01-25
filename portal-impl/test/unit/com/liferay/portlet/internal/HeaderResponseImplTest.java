35b8f1e0d733c2bf43767335a85dde6330c79551
shuyangzhou <shuyang.zhou@liferay.com>
2022-01-25 08:19:17 -0800

    LPS-145073 Don't pollute console output with the expected error message.

--- portal-impl/test/unit/com/liferay/portlet/internal/HeaderResponseImplTest.java
+++ portal-impl/test/unit/com/liferay/portlet/internal/HeaderResponseImplTest.java
@@ -17,7 +17,15 @@ package com.liferay.portlet.internal;
 import com.liferay.portal.kernel.security.xml.SecureXMLFactoryProviderUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.security.xml.SecureXMLFactoryProviderImpl;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.List;
+import java.util.logging.Level;
+
+import javax.xml.stream.XMLStreamException;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -52,18 +60,32 @@ public class HeaderResponseImplTest {
 			"<script id=\"dt\" type=\"data/template\">+<p>foo</p><p>bar</p>" +
 				"</script>");
 
-		try {
-			ReflectionTestUtil.invoke(
-				new HeaderResponseImpl(), "_validateParsedElements",
-				new Class<?>[] {String.class},
-				"<script id=\"dt\" type=\"text/javascript\"><p>foo</p><p>bar" +
-					"</p></script>");
+		try (LogCapture logCapture = LoggerTestUtil.configureJDKLogger(
+				HeaderResponseImpl.class.getName(), Level.SEVERE)) {
 
-			Assert.fail(
-				"Was able to add a <script type=\"text-javascript\"> with " +
-					"multiple child elements");
-		}
-		catch (IllegalArgumentException illegalArgumentException) {
+			try {
+				ReflectionTestUtil.invoke(
+					new HeaderResponseImpl(), "_validateParsedElements",
+					new Class<?>[] {String.class},
+					"<script id=\"dt\" type=\"text/javascript\"><p>foo</p><p>" +
+						"bar</p></script>");
+
+				Assert.fail(
+					"Was able to add a <script type=\"text-javascript\"> " +
+						"with multiple child elements");
+			}
+			catch (IllegalArgumentException illegalArgumentException) {
+			}
+
+			List<LogEntry> logEntries = logCapture.getLogEntries();
+
+			Assert.assertEquals(logEntries.toString(), 1, logEntries.size());
+
+			LogEntry logEntry = logEntries.get(0);
+
+			Throwable throwable = logEntry.getThrowable();
+
+			Assert.assertSame(XMLStreamException.class, throwable.getClass());
 		}
 	}
 
