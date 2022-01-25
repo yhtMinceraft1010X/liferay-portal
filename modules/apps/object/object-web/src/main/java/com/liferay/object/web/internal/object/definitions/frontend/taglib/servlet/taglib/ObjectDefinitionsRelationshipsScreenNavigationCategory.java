a06e3b53f78930864c2ff20d915931fc97ea29ab
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-01-25 10:12:00 -0300

    LPS-146073 Create feature flag

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/frontend/taglib/servlet/taglib/ObjectDefinitionsRelationshipsScreenNavigationCategory.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/frontend/taglib/servlet/taglib/ObjectDefinitionsRelationshipsScreenNavigationCategory.java
@@ -18,6 +18,7 @@ import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
 import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.web.internal.configuration.activator.FFOneToOneRelationshipConfigurationActivator;
+import com.liferay.object.web.internal.configuration.activator.FFSystemObjectRelationshipConfigurationActivator;
 import com.liferay.object.web.internal.object.definitions.constants.ObjectDefinitionsScreenNavigationEntryConstants;
 import com.liferay.object.web.internal.object.definitions.display.context.ObjectDefinitionsRelationshipsDisplayContext;
 import com.liferay.portal.kernel.language.LanguageUtil;
@@ -73,7 +74,11 @@ public class ObjectDefinitionsRelationshipsScreenNavigationCategory
 
 	@Override
 	public boolean isVisible(User user, ObjectDefinition objectDefinition) {
-		return !objectDefinition.isSystem();
+		if (objectDefinition.isSystem()) {
+			return _ffSystemObjectRelationshipConfiguration.enabled();
+		}
+
+		return true;
 	}
 
 	@Override
@@ -94,6 +99,10 @@ public class ObjectDefinitionsRelationshipsScreenNavigationCategory
 	@Reference
 	private FFOneToOneRelationshipConfigurationActivator
 		_ffOneToOneRelationshipConfigurationActivator;
+
+	@Reference
+	private FFSystemObjectRelationshipConfigurationActivator
+		_ffSystemObjectRelationshipConfiguration;
 
 	@Reference(
 		target = "(model.class.name=com.liferay.object.model.ObjectDefinition)"
