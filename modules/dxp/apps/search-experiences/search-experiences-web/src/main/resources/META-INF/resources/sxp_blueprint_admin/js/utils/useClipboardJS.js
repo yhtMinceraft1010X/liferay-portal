95d0b0064ddd85da27fc3340169f3e558edcf217
Kevin Tan <kevin.tan@liferay.com>
2022-01-24 16:43:33 -0800

    LPS-140041 search-experiences-web: Formatting and docs

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/resources/META-INF/resources/sxp_blueprint_admin/js/utils/useClipboardJS.js
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/resources/META-INF/resources/sxp_blueprint_admin/js/utils/useClipboardJS.js
@@ -14,6 +14,24 @@ import {useEffect} from 'react';
 
 import {openErrorToast, openSuccessToast} from './toasts';
 
+/**
+ * This only should be used once per page since this will enable the clipboard
+ * for all elements matching the given selector.
+ *
+ * To create a copy-to-clipboard button, add the class
+ * `${COPY_BUTTON_CSS_CLASS}` to the element and `data-clipboard-text` attribute
+ * to define the text to be copied.
+ *
+ * For example:
+ * 		<ClayButton
+ * 			className={COPY_BUTTON_CSS_CLASS}
+ * 			data-clipboard-text={text}
+ * 		>
+ * 			Copy to Clipboard
+ * 		</ClayButton>
+ *
+ * @param {String} selector The selector for a copy button
+ */
 export default function useClipboardJS(selector) {
 	useEffect(() => {
 		const clipboardJS = new ClipboardJS(selector);
@@ -23,6 +41,7 @@ export default function useClipboardJS(selector) {
 				message: Liferay.Language.get('copied-to-clipboard'),
 			})
 		);
+
 		clipboardJS.on('error', () => openErrorToast());
 
 		return () => {
