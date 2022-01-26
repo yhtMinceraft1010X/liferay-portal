7552490b1dd2560fa3d3c3d8eeba298dbea2eeb4
Pablo Molina <contact@pablomolina.me>
2022-01-26 09:27:05 +0100

    LPS-145046 Create FragmentCollectionPreview component

---
+++ modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/fragment-collection-preview/FragmentPreview.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React, {useMemo} from 'react';
+
+import {VariationPreview} from './VariationPreview';
+import {combine} from './combine';
+
+export function FragmentPreview({fragment, namespace}) {
+	const variations = useMemo(() => getFragmentVariations(fragment), [
+		fragment,
+	]);
+
+	return (
+		<section>
+			<h4>{fragment.name}</h4>
+
+			{variations.map((variation) => {
+				const label = `${fragment.name} ${variation
+					.map((part) => part.label)
+					.join(' ')}`;
+
+				return (
+					<VariationPreview
+						fragmentEntryKey={fragment.fragmentEntryKey}
+						key={label}
+						label={label}
+						namespace={namespace}
+						previewURL={fragment.previewURL}
+						variation={variation}
+					/>
+				);
+			})}
+		</section>
+	);
+}
+
+function getFragmentVariations(fragment) {
+	const configurationValues = fragment.configuration.fieldSets.flatMap(
+		(fieldSet) =>
+			fieldSet.fields
+				.filter(
+					(field) =>
+						field.type === 'select' &&
+						Array.isArray(field.typeOptions?.validValues)
+				)
+				.map((field) =>
+					field.typeOptions.validValues.map((validValue) => ({
+						label: validValue.label || validValue.value,
+						name: field.name,
+						value: validValue.value,
+					}))
+				)
+	);
+
+	return combine(...configurationValues);
+}
---
+++ modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/fragment-collection-preview/VariationPreview.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {fetch, objectToFormData} from 'frontend-js-web';
+import React, {useEffect, useState} from 'react';
+
+export function VariationPreview({
+	fragmentEntryKey,
+	label,
+	namespace,
+	previewURL,
+	variation,
+}) {
+	const [html, setHTML] = useState('');
+
+	useEffect(() => {
+		fetch(previewURL, {
+			body: objectToFormData({
+				[`_${namespace}_configurationValues`]: JSON.stringify(
+					variation.reduce(
+						(configurationValues, {name, value}) => ({
+							...configurationValues,
+							[name]: value,
+						}),
+						{}
+					)
+				),
+			}),
+			method: 'POST',
+		})
+			.then((response) => response.text())
+			.then((data) => {
+				setHTML(data);
+			})
+			.catch((error) => {
+				console.error(error);
+				setHTML('');
+			});
+	}, [fragmentEntryKey, namespace, previewURL, variation]);
+
+	return (
+		<article>
+			<h5>{label}</h5>
+
+			<div dangerouslySetInnerHTML={{__html: html}} />
+		</article>
+	);
+}
