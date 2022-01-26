8987b20ae7c40497f4371493e701d7db854b9b17
iagochaves <iagovieirachaves@gmail.com>
2022-01-26 15:26:39 -0300

    LPS-146207 Split common functions file to specific files

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/utils/getCurrentEndDate.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/utils/getCurrentEndDate.js
@@ -9,28 +9,11 @@
  * distribution rights of the Software.
  */
 
-const {REACT_APP_LIFERAY_API = window.location.origin} = process.env;
-
-const API_BASE_URL = REACT_APP_LIFERAY_API;
-
-export {API_BASE_URL};
-
-const getCurrentEndDate = (currentEndDate) => {
+export default function getCurrentEndDate(currentEndDate) {
 	const date = new Date(currentEndDate);
 	const month = date.toLocaleDateString('default', {month: 'short'});
 	const day = date.getDate();
 	const year = date.getFullYear();
 
 	return `${month} ${day}, ${year}`;
-};
-
-const downloadFromBlob = (blob, filename) => {
-	const a = document.createElement('a');
-	a.href = window.URL.createObjectURL(blob);
-	a.download = filename;
-	document.body.appendChild(a);
-	a.click();
-	a.remove();
-};
-
-export {getCurrentEndDate, downloadFromBlob};
+}
---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/utils/getDownloadFromBlob.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+export default function getDownloadFromBlob(blob, filename) {
+	const a = document.createElement('a');
+	a.href = window.URL.createObjectURL(blob);
+	a.download = filename;
+	document.body.appendChild(a);
+	a.click();
+	a.remove();
+}
