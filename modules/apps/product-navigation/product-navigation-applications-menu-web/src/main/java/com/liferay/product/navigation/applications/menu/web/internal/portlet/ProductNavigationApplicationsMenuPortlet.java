574a3041040a31663708c53a515b17df4330d24a
Rubén Pulido <ruben.pulido@liferay.com>
2022-01-26 09:27:08 +0100

    LPS-145046 Add javax.portlet.version=3.0 to ProductNavigationApplicationsMenuPortlet to prevent UnsupportedOperationException when creating new Site as shown here https://github.com/liferay-echo/liferay-portal/pull/7016#issuecomment-1019878753

--- modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/java/com/liferay/product/navigation/applications/menu/web/internal/portlet/ProductNavigationApplicationsMenuPortlet.java
+++ modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/java/com/liferay/product/navigation/applications/menu/web/internal/portlet/ProductNavigationApplicationsMenuPortlet.java
@@ -38,7 +38,8 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.display-name=Simulation",
 		"javax.portlet.name=" + ProductNavigationApplicationsMenuPortletKeys.PRODUCT_NAVIGATION_APPLICATIONS_MENU,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
