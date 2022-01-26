6cb95149dd27a247d3bc95b7815df9d6163a080b
Rubén Pulido <ruben.pulido@liferay.com>
2022-01-26 09:27:07 +0100

    LPS-145046 Create PreviewFragmentCollectionMVCResourceCommand rendering /preview_fragment_collection.jsp following similar approach as used in RenderFragmentEntryStrutsAction

---
+++ modules/apps/style-book/style-book-web/src/main/java/com/liferay/style/book/web/internal/portlet/action/PreviewFragmentCollectionMVCResourceCommand.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.style.book.web.internal.portlet.action;
+
+import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.petra.io.unsync.UnsyncStringWriter;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.service.LayoutSetLocalService;
+import com.liferay.portal.kernel.servlet.PipingServletResponse;
+import com.liferay.portal.kernel.servlet.ServletContextPool;
+import com.liferay.portal.kernel.servlet.ServletResponseUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.theme.ThemeUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.style.book.constants.StyleBookPortletKeys;
+import com.liferay.style.book.web.internal.constants.StyleBookWebKeys;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rubén Pulido
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + StyleBookPortletKeys.STYLE_BOOK,
+		"mvc.command.name=/style_book/preview_fragment_collection"
+	},
+	service = MVCResourceCommand.class
+)
+public class PreviewFragmentCollectionMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			resourceRequest);
+
+		httpServletRequest.setAttribute(
+			StyleBookWebKeys.FRAGMENT_COLLECTION_CONTRIBUTOR_TRACKER,
+			_fragmentCollectionContributorTracker);
+
+		HttpServletResponse httpServletResponse =
+			_portal.getHttpServletResponse(resourceResponse);
+
+		String renderedPreviewFragmentCollection =
+			_renderPreviewFragmentCollection(
+				httpServletRequest, httpServletResponse);
+
+		String renderedPortalNormal = _renderPortalNormal(
+			httpServletRequest, httpServletResponse);
+
+		Document document = Jsoup.parse(renderedPortalNormal);
+
+		Element element = document.body();
+
+		element.html(renderedPreviewFragmentCollection);
+
+		ServletResponseUtil.write(httpServletResponse, document.html());
+	}
+
+	private String _renderPortalNormal(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		LayoutSet layoutSet = _layoutSetLocalService.getLayoutSet(
+			themeDisplay.getScopeGroupId(), false);
+
+		themeDisplay.setLayoutSet(layoutSet);
+		themeDisplay.setLookAndFeel(
+			layoutSet.getTheme(), layoutSet.getColorScheme());
+
+		return ThemeUtil.include(
+			ServletContextPool.get(StringPool.BLANK), httpServletRequest,
+			httpServletResponse, "portal_normal.ftl", layoutSet.getTheme(),
+			false);
+	}
+
+	private String _renderPreviewFragmentCollection(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher(
+				"/preview_fragment_collection.jsp");
+
+		UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
+
+		PipingServletResponse pipingServletResponse = new PipingServletResponse(
+			httpServletResponse, unsyncStringWriter);
+
+		requestDispatcher.include(httpServletRequest, pipingServletResponse);
+
+		return unsyncStringWriter.toString();
+	}
+
+	@Reference
+	private FragmentCollectionContributorTracker
+		_fragmentCollectionContributorTracker;
+
+	@Reference
+	private LayoutSetLocalService _layoutSetLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.style.book.web)")
+	private ServletContext _servletContext;
+
+}
