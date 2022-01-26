9929f4668dc2e38ddeb5a20da57ce3138aebeb40
Rubén Pulido <ruben.pulido@liferay.com>
2022-01-26 16:31:49 +0100

    LPS-145046 Add javax.portlet.version=3.0 to StagingProcessesPortlet to prevent UnsupportedOperationException when enabling Staging Local Live as shown here https://github.com/liferay-echo/liferay-portal/pull/7051#issuecomment-1021887210

--- modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/portlet/StagingProcessesPortlet.java
+++ modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/portlet/StagingProcessesPortlet.java
@@ -54,7 +54,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + StagingProcessesPortletKeys.STAGING_PROCESSES,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
