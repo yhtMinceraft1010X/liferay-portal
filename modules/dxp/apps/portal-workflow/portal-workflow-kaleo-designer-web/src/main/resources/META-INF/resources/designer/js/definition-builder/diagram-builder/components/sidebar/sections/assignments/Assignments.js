805896546333d17d0ab6de48bf4fbdd237d7b7ef
Carlos Montenegro <carlos.montenegro@liferay.com>
2022-01-28 09:56:40 -0300

    LPS-145491 Display current assignments in node sidebar

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/assignments/Assignments.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/assignments/Assignments.js
@@ -9,23 +9,77 @@
  * distribution rights of the Software.
  */
 
-import ClayButton from '@clayui/button';
+import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
+import ClayLayout from '@clayui/layout';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useContext} from 'react';
 
+import {DiagramBuilderContext} from '../../../../DiagramBuilderContext';
 import SidebarPanel from '../../SidebarPanel';
+import {options} from './SelectAssignment';
 
-const Assignments = ({setContentName}) => (
-	<SidebarPanel panelTitle={Liferay.Language.get('assignments')}>
-		<ClayButton
-			className="mr-3"
-			displayType="secondary"
-			onClick={() => setContentName('assignments')}
-		>
-			{Liferay.Language.get('new')}
-		</ClayButton>
-	</SidebarPanel>
-);
+const Assignments = ({setContentName}) => {
+	const {selectedItem} = useContext(DiagramBuilderContext);
+
+	return (
+		<SidebarPanel panelTitle={Liferay.Language.get('assignments')}>
+			{!selectedItem.data.assignments ? (
+				<ClayButton
+					className="mr-3"
+					displayType="secondary"
+					onClick={() => setContentName('assignments')}
+				>
+					{Liferay.Language.get('new')}
+				</ClayButton>
+			) : (
+				<CurrentAssignments
+					assignments={{...selectedItem.data.assignments}}
+				/>
+			)}
+		</SidebarPanel>
+	);
+};
+
+const CurrentAssignments = ({assignments}) => {
+	const {setSelectedItem} = useContext(DiagramBuilderContext);
+	const deleteCurrentAssignments = () => {
+		setSelectedItem((previousValue) => ({
+			...previousValue,
+			data: {
+				...previousValue.data,
+				assignments: null,
+			},
+		}));
+	};
+
+	return (
+		<ClayLayout.ContentCol className="current-assignments-area" float>
+			<ClayLayout.Row
+				className="current-assignments-row"
+				justify="between"
+			>
+				<span>
+					{
+						options.find(
+							(option) =>
+								option.assignmentType ===
+								assignments.assignmentType[0]
+						)?.label
+					}
+
+					{/* {Liferay.Language.get(assignments.assignmentType[0])} */}
+				</span>
+
+				<ClayButtonWithIcon
+					className="delete-button text-secondary trash-button"
+					displayType="unstyled"
+					onClick={deleteCurrentAssignments}
+					symbol="trash"
+				/>
+			</ClayLayout.Row>
+		</ClayLayout.ContentCol>
+	);
+};
 
 Assignments.propTypes = {
 	setContentName: PropTypes.func.isRequired,
