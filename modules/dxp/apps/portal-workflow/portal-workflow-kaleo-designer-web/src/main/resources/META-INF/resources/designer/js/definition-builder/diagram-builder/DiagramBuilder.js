3bd05c7423ddb77873d76ff3227904586405894f
Carlos Montenegro <carlos.montenegro@liferay.com>
2022-01-31 17:28:30 -0300

    LPS-145491 Add function to retrieve extra data when diagram builder loads

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/DiagramBuilder.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/DiagramBuilder.js
@@ -28,10 +28,15 @@ import {DefinitionBuilderContext} from '../DefinitionBuilderContext';
 import {defaultLanguageId} from '../constants';
 import DeserializeUtil from '../source-builder/deserializeUtil';
 import {singleEventObserver} from '../util/EventObserver';
-import {retrieveDefinitionRequest} from '../util/fetchUtil';
+import {
+	retrieveDefinitionRequest,
+	retrieveRolesBy,
+	retrieveUsersBy,
+} from '../util/fetchUtil';
 import {DiagramBuilderContextProvider} from './DiagramBuilderContext';
 import {nodeTypes} from './components/nodes/utils';
 import Sidebar from './components/sidebar/Sidebar';
+import {getAssignmentType} from './components/sidebar/sections/assignments/utils';
 import {isIdDuplicated} from './components/sidebar/utils';
 import edgeTypes from './components/transitions/Edge';
 import FloatingConnectionLine from './components/transitions/FloatingConnectionLine';
@@ -261,8 +266,7 @@ export default function DiagramBuilder({version}) {
 						setSelectedItemNewId(null);
 
 						setSelectedItem(element);
-					}
-					else if (isEdge(element)) {
+					} else if (isEdge(element)) {
 						element = {
 							...element,
 							...(selectedItem.id === element.source && {
@@ -282,6 +286,62 @@ export default function DiagramBuilder({version}) {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [selectedItem, selectedItemNewId]);
 
+	const addRetrievedData = useCallback(
+		async (initialElements) => {
+			for (let index = 0; index < initialElements.length; index++) {
+				const element = initialElements[index];
+				if (element.data.assignments) {
+					const assignmentType = getAssignmentType(
+						element.data.assignments
+					);
+					if (assignmentType === 'user') {
+						const usersData = [];
+						await retrieveUsersBy(
+							'emailAddress',
+							element.data.assignments.emailAddress
+						)
+							.then((response) => response.json())
+							.then(({items}) => {
+								items.forEach((item, index) => {
+									usersData.push({
+										emailAddress: item.emailAddress,
+										identifier: `${Date.now()}-${index}`,
+										name: item.name,
+										screenName: item.alternateName,
+										userId: item.id,
+									});
+								});
+							})
+							.then(() => {
+								initialElements[
+									index
+								].data.assignments.usersData = usersData;
+								setElements([...initialElements]);
+							});
+					}
+					if (assignmentType === 'roleId') {
+						await retrieveRolesBy(
+							'roleId',
+							element.data.assignments.roleId
+						)
+							.then((response) => response.json())
+							.then((response) => {
+								initialElements[
+									index
+								].data.assignments.rolesData = {
+									id: response.id,
+									name: response.name,
+									roleType: response.roleType,
+								};
+								setElements([...initialElements]);
+							});
+					}
+				}
+			}
+		},
+		[setElements]
+	);
+
 	useEffect(() => {
 		if (deserialize && currentEditor) {
 			const xmlDefinition = currentEditor.getData();
@@ -297,9 +357,12 @@ export default function DiagramBuilder({version}) {
 
 			setElements(elements);
 
+			addRetrievedData(elements);
+
 			setDeserialize(false);
 		}
 	}, [
+		addRetrievedData,
 		currentEditor,
 		definitionTitle,
 		deserialize,
@@ -324,6 +387,8 @@ export default function DiagramBuilder({version}) {
 					const elements = deserializeUtil.getElements();
 
 					setElements(elements);
+
+					addRetrievedData(elements);
 				});
 		}
 
