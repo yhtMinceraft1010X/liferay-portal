a0ebfa156a01686a23c0a71ffacfa8594d076717
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-02-01 12:34:35 +0100

    Revert "LPS-143841 Uses ResourceURLBuilder"
    This reverts commit e413a96e

--- modules/apps/site-navigation/site-navigation-menu-item-display-page/src/main/java/com/liferay/site/navigation/menu/item/display/page/internal/display/context/DisplayPageTypeSiteNavigationMenuTypeDisplayContext.java
+++ modules/apps/site-navigation/site-navigation-menu-item-display-page/src/main/java/com/liferay/site/navigation/menu/item/display/page/internal/display/context/DisplayPageTypeSiteNavigationMenuTypeDisplayContext.java
@@ -25,7 +25,6 @@ import com.liferay.item.selector.criteria.InfoItemItemSelectorReturnType;
 import com.liferay.item.selector.criteria.info.item.criterion.InfoItemItemSelectorCriterion;
 import com.liferay.layout.display.page.LayoutDisplayPageInfoItemFieldValuesProvider;
 import com.liferay.layout.display.page.LayoutDisplayPageObjectProvider;
-import com.liferay.petra.portlet.url.builder.ResourceURLBuilder;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
@@ -263,13 +262,10 @@ public class DisplayPageTypeSiteNavigationMenuTypeDisplayContext {
 
 	public String getItemDetailsURL() {
 		LiferayPortletURL itemDetailsURL =
-			(LiferayPortletURL)ResourceURLBuilder.createResourceURL(
-				_liferayPortletResponse
-			).setResourceID(
-				"/navigation_menu/get_item_details"
-			);
+			(LiferayPortletURL)_liferayPortletResponse.createResourceURL();
 
 		itemDetailsURL.setCopyCurrentRenderParameters(false);
+		itemDetailsURL.setResourceID("/navigation_menu/get_item_details");
 
 		return itemDetailsURL.toString();
 	}
