e10659e06f89fbf1900bf89d773007ecfab8863d
Aline Cantarelli <aline.cantarelli@liferay.com>
2022-01-31 22:29:50 -0300

    LPS-146527 Display alert when a transition is selected as a default

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/EdgeInformation.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/EdgeInformation.js
@@ -9,10 +9,11 @@
  * distribution rights of the Software.
  */
 
+import ClayAlert from '@clayui/alert';
 import ClayForm, {ClayInput, ClayToggle} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import PropTypes from 'prop-types';
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {isEdge} from 'react-flow-renderer';
 
 import {DefinitionBuilderContext} from '../../../../DefinitionBuilderContext';
@@ -32,8 +33,19 @@ export default function EdgeInformation({errors, setErrors}) {
 		setSelectedItemNewId,
 	} = useContext(DiagramBuilderContext);
 
+	const [showWarningAlert, setShowWarningAlert] = useState(false);
+
 	const onToggleDefault = (defaultEdge) => {
 		if (defaultEdge) {
+			setShowWarningAlert(
+				elements.find(
+					(element) =>
+						isEdge(element) &&
+						element.source === selectedItem.source &&
+						element.data.defaultEdge
+				)
+			);
+
 			setElements((previousElements) =>
 				previousElements.map((element) => {
 					if (
@@ -58,128 +70,150 @@ export default function EdgeInformation({errors, setErrors}) {
 	};
 
 	return (
-		<SidebarPanel panelTitle={Liferay.Language.get('transitions')}>
-			<ClayForm.Group className={errors.label ? 'has-error' : ''}>
-				<label htmlFor="nodeLabel">
-					{Liferay.Language.get('label')}
+		<>
+			<SidebarPanel panelTitle={Liferay.Language.get('transitions')}>
+				<ClayForm.Group className={errors.label ? 'has-error' : ''}>
+					<label htmlFor="nodeLabel">
+						{Liferay.Language.get('label')}
 
-					<span className="ml-1 mr-1 text-warning">*</span>
+						<span className="ml-1 mr-1 text-warning">*</span>
 
-					<span title={Liferay.Language.get('label-name')}>
-						<ClayIcon
-							className="text-muted"
-							symbol="question-circle-full"
-						/>
-					</span>
-				</label>
+						<span title={Liferay.Language.get('label-name')}>
+							<ClayIcon
+								className="text-muted"
+								symbol="question-circle-full"
+							/>
+						</span>
+					</label>
 
-				<ClayInput
-					id="edgeLabel"
-					onChange={({target}) => {
-						setErrors(checkLabelErrors(errors, target));
+					<ClayInput
+						id="edgeLabel"
+						onChange={({target}) => {
+							setErrors(checkLabelErrors(errors, target));
 
-						const key =
-							selectedLanguageId !== ''
-								? selectedLanguageId
-								: defaultLanguageId;
+							const key =
+								selectedLanguageId !== ''
+									? selectedLanguageId
+									: defaultLanguageId;
 
-						setSelectedItem(
-							getUpdatedLabelItem(key, selectedItem, target)
-						);
-					}}
-					type="text"
-					value={
-						(selectedLanguageId
-							? selectedItem?.data.label[selectedLanguageId]
-							: selectedItem?.data.label[defaultLanguageId]) || ''
-					}
-				/>
-
-				<ClayForm.FeedbackItem>
-					{errors.label && (
-						<>
-							<ClayForm.FeedbackIndicator symbol="exclamation-full" />
-
-							{Liferay.Language.get('this-field-is-required')}
-						</>
-					)}
-				</ClayForm.FeedbackItem>
-			</ClayForm.Group>
-
-			<ClayForm.Group
-				className={
-					errors.id.duplicated || errors.id.empty ? 'has-error' : ''
-				}
-			>
-				<label htmlFor="transitionId">
-					<span>
-						{`${Liferay.Language.get(
-							'transition'
-						)} ${Liferay.Language.get('id')}`}
-					</span>
-
-					<span className="ml-1 mr-1 text-warning">*</span>
-
-					<span
-						title={Liferay.Language.get(
-							'id-is-the-transition-identifier'
-						)}
-					>
-						<ClayIcon
-							className="text-muted"
-							symbol="question-circle-full"
-						/>
-					</span>
-				</label>
-
-				<ClayInput
-					id="transitionId"
-					onChange={({target}) => {
-						setErrors(checkIdErrors(elements, errors, target));
-						setSelectedItemNewId(target.value);
-					}}
-					type="text"
-					value={(selectedItemNewId ?? selectedItem?.id) || ''}
-				/>
-
-				<ClayForm.FeedbackItem>
-					{(errors.id.duplicated || errors.id.empty) && (
-						<>
-							<ClayForm.FeedbackIndicator symbol="exclamation-full" />
-
-							{errors.id.duplicated
-								? Liferay.Language.get(
-										'a-transition-with-that-id-already-exists'
-								  )
-								: Liferay.Language.get(
-										'this-field-is-required'
-								  )}
-						</>
-					)}
-				</ClayForm.FeedbackItem>
-			</ClayForm.Group>
-
-			<ClayForm.Group>
-				<label htmlFor="toggleDefault">
-					{Liferay.Language.get('default')}
-
-					<span className="ml-1 mr-1 text-warning">*</span>
-				</label>
-
-				<div>
-					<ClayToggle
-						id="toggleDefault"
-						label={
-							selectedItem?.data?.defaultEdge
-								? Liferay.Language.get('true')
-								: Liferay.Language.get('false')
+							setSelectedItem(
+								getUpdatedLabelItem(key, selectedItem, target)
+							);
+						}}
+						type="text"
+						value={
+							(selectedLanguageId
+								? selectedItem?.data.label[selectedLanguageId]
+								: selectedItem?.data.label[
+										defaultLanguageId
+								  ]) || ''
 						}
-						onToggle={onToggleDefault}
-						toggled={selectedItem?.data?.defaultEdge}
 					/>
-				</div>
-			</ClayForm.Group>
-		</SidebarPanel>
+
+					<ClayForm.FeedbackItem>
+						{errors.label && (
+							<>
+								<ClayForm.FeedbackIndicator symbol="exclamation-full" />
+
+								{Liferay.Language.get('this-field-is-required')}
+							</>
+						)}
+					</ClayForm.FeedbackItem>
+				</ClayForm.Group>
+
+				<ClayForm.Group
+					className={
+						errors.id.duplicated || errors.id.empty
+							? 'has-error'
+							: ''
+					}
+				>
+					<label htmlFor="transitionId">
+						<span>
+							{`${Liferay.Language.get(
+								'transition'
+							)} ${Liferay.Language.get('id')}`}
+						</span>
+
+						<span className="ml-1 mr-1 text-warning">*</span>
+
+						<span
+							title={Liferay.Language.get(
+								'id-is-the-transition-identifier'
+							)}
+						>
+							<ClayIcon
+								className="text-muted"
+								symbol="question-circle-full"
+							/>
+						</span>
+					</label>
+
+					<ClayInput
+						id="transitionId"
+						onChange={({target}) => {
+							setErrors(checkIdErrors(elements, errors, target));
+							setSelectedItemNewId(target.value);
+						}}
+						type="text"
+						value={(selectedItemNewId ?? selectedItem?.id) || ''}
+					/>
+
+					<ClayForm.FeedbackItem>
+						{(errors.id.duplicated || errors.id.empty) && (
+							<>
+								<ClayForm.FeedbackIndicator symbol="exclamation-full" />
+
+								{errors.id.duplicated
+									? Liferay.Language.get(
+											'a-transition-with-that-id-already-exists'
+									  )
+									: Liferay.Language.get(
+											'this-field-is-required'
+									  )}
+							</>
+						)}
+					</ClayForm.FeedbackItem>
+				</ClayForm.Group>
+
+				<ClayForm.Group>
+					<label htmlFor="toggleDefault">
+						{Liferay.Language.get('default')}
+
+						<span className="ml-1 mr-1 text-warning">*</span>
+					</label>
+
+					<div>
+						<ClayToggle
+							id="toggleDefault"
+							label={
+								selectedItem?.data?.defaultEdge
+									? Liferay.Language.get('true')
+									: Liferay.Language.get('false')
+							}
+							onToggle={onToggleDefault}
+							toggled={selectedItem?.data?.defaultEdge}
+						/>
+					</div>
+				</ClayForm.Group>
+			</SidebarPanel>
+			{showWarningAlert && (
+				<ClayAlert.ToastContainer>
+					<ClayAlert
+						autoClose={5000}
+						displayType="warning"
+						onClose={() => setShowWarningAlert(false)}
+						title={`${Liferay.Language.get(
+							'default-has-been-changed'
+						)}:`}
+					>
+						{Liferay.Language.get(
+							'this-can-lead-to-a-series-of-changes-in-your-workflow'
+						)}
+					</ClayAlert>
+				</ClayAlert.ToastContainer>
+			)}
+		</>
 	);
 }
 
