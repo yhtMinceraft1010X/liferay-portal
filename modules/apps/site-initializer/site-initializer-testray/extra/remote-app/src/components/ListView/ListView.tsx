31242958bdb158e9d54f9a6ee5fb54d87191047e
Keven Leone <keven.leone@liferay.com>
2022-02-02 16:46:53 -0300

    LPS-146629 Create ListView Component

---
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/components/ListView/ListView.tsx
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {TypedDocumentNode, useQuery} from '@apollo/client';
+import {ClayPaginationBarWithBasicItems} from '@clayui/pagination-bar';
+
+import {PAGINATION} from '../../util/constants';
+import EmptyState from '../EmptyState';
+import Table, {TableProps} from '../Table';
+
+type LiferayQueryResponse = {
+	items: any[];
+	lastPage: number;
+	page: number;
+	pageSize: number;
+	totalCount: number;
+};
+
+type ListViewProps = {
+	query: TypedDocumentNode;
+	tableProps: Omit<TableProps, 'items'>;
+	transformData: (data: any) => LiferayQueryResponse;
+	variables: any;
+};
+
+const ListView: React.FC<ListViewProps> = ({
+	query,
+	tableProps,
+	transformData,
+	variables,
+}) => {
+	const {data, error, loading, refetch} = useQuery(query, {
+		variables,
+	});
+
+	const {items = [], page, pageSize, totalCount} = transformData(data) || {};
+
+	const deltas = PAGINATION.delta.map((label) => ({label}));
+
+	const onRefetch = (newVariables: any) => {
+		refetch({...variables, ...newVariables});
+	};
+
+	const Pagination = () => (
+		<ClayPaginationBarWithBasicItems
+			activeDelta={pageSize}
+			activePage={page}
+			deltas={deltas}
+			ellipsisBuffer={PAGINATION.ellipsisBuffer}
+			onDeltaChange={(delta) => onRefetch({pageSize: delta})}
+			onPageChange={(page) => onRefetch({page})}
+			totalItems={totalCount}
+		/>
+	);
+
+	if (error) {
+		return <span>{error.message}</span>;
+	}
+
+	if (loading) {
+		return <span>Loading...</span>;
+	}
+
+	if (!items.length) {
+		return <EmptyState />;
+	}
+
+	return (
+		<>
+			<Pagination />
+			<Table {...tableProps} items={items} />
+			<Pagination />
+		</>
+	);
+};
+
+export default ListView;
