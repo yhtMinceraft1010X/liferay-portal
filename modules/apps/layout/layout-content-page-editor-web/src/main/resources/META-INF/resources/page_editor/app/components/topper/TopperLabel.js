5414bd2b9f23f347e26754e07a7bf97cc6eb9aaf
Pablo Molina <contact@pablomolina.me>
2022-02-02 13:52:37 +0100

    LPS-142497 Update TopperLabel positioning behavior

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/topper/TopperLabel.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/topper/TopperLabel.js
@@ -12,59 +12,166 @@
  * details.
  */
 
+import {ReactPortal} from '@liferay/frontend-js-react-web';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React, {useEffect, useState} from 'react';
 
+import {config} from '../../config/index';
+import {useGlobalContext} from '../../contexts/GlobalContext';
+import {useSelector} from '../../contexts/StoreContext';
+import selectLanguageId from '../../selectors/selectLanguageId';
+
 const TOPPER_BAR_HEIGHT = 24;
+const TOPPER_BAR_BORDER_WIDTH = 2;
 
 export function TopperLabel({children, itemElement}) {
-	const [isInset, setIsInset] = useState(false);
-	const [windowScrollPosition, setWindowScrollPosition] = useState(0);
-
-	useEffect(() => {
-		const handleWindowScroll = () => {
-			setWindowScrollPosition(window.scrollY);
-		};
-
-		window.addEventListener('scroll', handleWindowScroll);
-
-		return () => {
-			window.removeEventListener('scroll', handleWindowScroll);
-		};
-	}, []);
+	const languageId = useSelector(selectLanguageId);
+	const globalContext = useGlobalContext();
+	const [positionConfig, setPositionConfig] = useState({
+		isInset: false,
+		style: {},
+	});
 
 	useEffect(() => {
 		if (itemElement) {
-			const itemTop =
-				itemElement.getBoundingClientRect().top - TOPPER_BAR_HEIGHT;
+			const controlMenuContainer = globalContext.document.querySelector(
+				'.control-menu-container'
+			);
+			const pageEditorToolbar = globalContext.document.getElementById(
+				`${config.portletNamespace}pageEditorToolbar`
+			);
 
-			const controlMenuContainerHeight =
-				document.querySelector('.control-menu-container')
-					?.offsetHeight ?? 0;
+			let controlMenuContainerHeight = 0;
+			let itemElementLeft = 0;
+			let itemElementRight = 0;
+			let itemElementTop = 0;
+			let itemElementMarginLeft = 0;
+			let itemElementMarginRight = 0;
+			let pageEditorToolbarHeight = 0;
+			let scrollY = globalContext.window.scrollY;
 
-			if (itemTop < controlMenuContainerHeight) {
-				setIsInset(true);
+			const updatePosition = () => {
+				const languageDirection =
+					Liferay.Language.direction[themeDisplay?.getLanguageId()] ||
+					'ltr';
+
+				const left =
+					itemElementLeft -
+					itemElementMarginLeft +
+					TOPPER_BAR_BORDER_WIDTH;
+
+				const right =
+					itemElementRight -
+					itemElementMarginRight +
+					TOPPER_BAR_BORDER_WIDTH;
+
+				const isInset =
+					itemElementTop - scrollY <
+					controlMenuContainerHeight + pageEditorToolbarHeight;
+
+				const top = isInset
+					? itemElementTop + TOPPER_BAR_BORDER_WIDTH
+					: itemElementTop - TOPPER_BAR_HEIGHT;
+
+				setPositionConfig({
+					isInset,
+					style:
+						languageDirection === 'rtl'
+							? {right, top}
+							: {left, top},
+				});
+			};
+
+			const handleScroll = () => {
+				scrollY = globalContext.window.scrollY;
+				updatePosition();
+			};
+
+			const resizeObserver = new globalContext.window.ResizeObserver(
+				(entries) => {
+					entries.forEach((entry) => {
+						if (entry.target === itemElement) {
+							const boundingClientRect = itemElement.getBoundingClientRect();
+							const computedStyle = globalContext.window.getComputedStyle(
+								itemElement
+							);
+
+							itemElementMarginRight =
+								(computedStyle.marginRight.endsWith('px') &&
+									parseInt(computedStyle.marginRight, 10)) ||
+								0;
+
+							itemElementMarginLeft =
+								(computedStyle.marginLeft.endsWith('px') &&
+									parseInt(computedStyle.marginLeft, 10)) ||
+								0;
+
+							itemElementLeft =
+								globalContext.window.scrollX +
+								boundingClientRect.left;
+
+							// We use here body instead of window.innerWidth
+							// to prevent issues if the scrollbar is present.
+
+							itemElementRight =
+								globalContext.document.body.getBoundingClientRect()
+									.width -
+								(boundingClientRect.right -
+									globalContext.window.scrollX);
+
+							itemElementTop =
+								globalContext.window.scrollY +
+								boundingClientRect.top;
+						}
+						else if (entry.target === controlMenuContainer) {
+							controlMenuContainerHeight =
+								entry.contentRect.height;
+						}
+						else if (entry.target === pageEditorToolbar) {
+							pageEditorToolbarHeight = entry.contentRect.height;
+						}
+					});
+
+					updatePosition();
+				}
+			);
+
+			globalContext.window.addEventListener('scroll', handleScroll);
+			resizeObserver.observe(itemElement);
+
+			if (controlMenuContainer) {
+				resizeObserver.observe(controlMenuContainer);
 			}
-			else {
-				setIsInset(false);
+
+			if (pageEditorToolbar) {
+				resizeObserver.observe(pageEditorToolbar);
 			}
+
+			return () => {
+				globalContext.window.removeEventListener(
+					'scroll',
+					handleScroll
+				);
+				resizeObserver.disconnect();
+			};
 		}
-	}, [itemElement, windowScrollPosition]);
+	}, [itemElement, languageId, globalContext]);
 
 	return (
-		<div
-			className={classNames(
-				'cadmin',
-				'page-editor__topper__bar',
-				'tbar',
-				{
-					'page-editor__topper__bar--inset': isInset,
-				}
-			)}
-		>
-			{children}
-		</div>
+		<ReactPortal container={globalContext.document.body} wrapper={false}>
+			<div
+				className={classNames(
+					'cadmin',
+					'page-editor__topper__bar',
+					'tbar',
+					{'page-editor__topper__bar--inset': positionConfig.isInset}
+				)}
+				style={positionConfig.style}
+			>
+				{children}
+			</div>
+		</ReactPortal>
 	);
 }
 
