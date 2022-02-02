09b19f8c7aba94df5564130979e16487491edb9d
CalumR23 <searagan@gmail.com>
2022-02-02 11:27:47 -0800

    POSHI-242 Modify class to now extend PoshiElementException

--- modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/script/PoshiScriptParserException.java
+++ modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/script/PoshiScriptParserException.java
@@ -15,6 +15,7 @@
 package com.liferay.poshi.core.script;
 
 import com.liferay.poshi.core.elements.PoshiElement;
+import com.liferay.poshi.core.elements.PoshiElementException;
 import com.liferay.poshi.core.elements.PoshiNode;
 import com.liferay.poshi.core.util.StringUtil;
 import com.liferay.poshi.core.util.Validator;
@@ -28,7 +29,7 @@ import java.util.Set;
 /**
  * @author Kenji Heigel
  */
-public class PoshiScriptParserException extends Exception {
+public class PoshiScriptParserException extends PoshiElementException {
 
 	public static final String TRANSLATION_LOSS_MESSAGE =
 		"Poshi Script syntax is not preserved in translation";
@@ -129,120 +130,9 @@ public class PoshiScriptParserException extends Exception {
 		setFilePath(url.getPath());
 	}
 
-	public int getErrorLineNumber() {
-		return _errorLineNumber;
-	}
-
-	public String getErrorSnippet() {
-		PoshiElement rootPoshiElement = getRootPoshiElement(getPoshiNode());
-
-		int errorLineNumber = getErrorLineNumber();
-
-		int startingLineNumber = Math.max(
-			errorLineNumber - _ERROR_SNIPPET_PREFIX_SIZE, 1);
-
-		String poshiScript = rootPoshiElement.getPoshiScript();
-
-		String[] lines = poshiScript.split("\n");
-
-		int endingLineNumber = lines.length;
-
-		endingLineNumber = Math.min(
-			errorLineNumber + _ERROR_SNIPPET_POSTFIX_SIZE, endingLineNumber);
-
-		StringBuilder sb = new StringBuilder();
-
-		int currentLineNumber = startingLineNumber;
-
-		String lineNumberString = String.valueOf(endingLineNumber);
-
-		int pad = lineNumberString.length() + 2;
-
-		while (currentLineNumber <= endingLineNumber) {
-			StringBuilder prefix = new StringBuilder();
-
-			if (currentLineNumber == errorLineNumber) {
-				prefix.append(">");
-			}
-			else {
-				prefix.append(" ");
-			}
-
-			prefix.append(" ");
-
-			prefix.append(currentLineNumber);
-
-			sb.append(String.format("%" + pad + "s", prefix.toString()));
-			sb.append(" |");
-
-			String line = lines[currentLineNumber - 1];
-
-			sb.append(StringUtil.replace(line, "\t", "    "));
-
-			sb.append("\n");
-
-			currentLineNumber++;
-		}
-
-		return sb.toString();
-	}
-
-	public String getFilePath() {
-		return _filePath;
-	}
-
-	@Override
-	public String getMessage() {
-		StringBuilder sb = new StringBuilder();
-
-		sb.append(super.getMessage());
-		sb.append(" at:\n");
-		sb.append(getFilePath());
-		sb.append(":");
-		sb.append(getErrorLineNumber());
-		sb.append("\n");
-		sb.append(getErrorSnippet());
-
-		return sb.toString();
-	}
-
-	public PoshiNode<?, ?> getPoshiNode() {
-		return _poshiNode;
-	}
-
-	public PoshiElement getRootPoshiElement(PoshiNode<?, ?> poshiNode) {
-		if (Validator.isNotNull(poshiNode.getParent())) {
-			PoshiElement parentPoshiElement =
-				(PoshiElement)poshiNode.getParent();
-
-			return getRootPoshiElement(parentPoshiElement);
-		}
-
-		return (PoshiElement)poshiNode;
-	}
-
-	public void setErrorLineNumber(int errorLineNumber) {
-		_errorLineNumber = errorLineNumber;
-	}
-
-	public void setFilePath(String filePath) {
-		_filePath = filePath;
-	}
-
-	public void setPoshiNode(PoshiNode<?, ?> poshiNode) {
-		_poshiNode = poshiNode;
-	}
-
-	private static final int _ERROR_SNIPPET_POSTFIX_SIZE = 10;
-
-	private static final int _ERROR_SNIPPET_PREFIX_SIZE = 10;
 
 	private static final Set<PoshiScriptParserException>
 		_poshiScriptParserExceptions = Collections.synchronizedSet(
 			new HashSet<>());
-
-	private int _errorLineNumber;
-	private String _filePath = "Unknown file";
-	private PoshiNode<?, ?> _poshiNode;
 
 }
