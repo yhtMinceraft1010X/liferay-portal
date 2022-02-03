1423bac19b355539e941ddd731f795fe9a922a8d
Brian Chan <brian.chan@liferay.com>
2022-02-03 08:37:39 -0300

    LPS-145792 Rename

--- modules/apps/frontend-js/frontend-js-react-web/src/main/java/com/liferay/frontend/js/react/web/internal/importmap/FrontendJSReactWebJSImportmapsContributor.java
+++ modules/apps/frontend-js/frontend-js-react-web/src/main/java/com/liferay/frontend/js/react/web/internal/importmap/FrontendJSReactWebJSImportmapsContributor.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Iván Zaera Avellón
  */
 @Component(immediate = true, service = JSImportmapsContributor.class)
-public class FrontendJsReactWebJSImportmapsContributor
+public class FrontendJSReactWebJSImportmapsContributor
 	implements JSImportmapsContributor {
 
 	@Override
