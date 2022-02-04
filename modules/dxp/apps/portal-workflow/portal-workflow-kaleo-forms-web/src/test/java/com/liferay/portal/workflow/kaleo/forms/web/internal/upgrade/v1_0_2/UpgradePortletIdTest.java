09519d5a819e869f4ddb4f7e766796e5ce8869e3
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-02-04 12:06:09 -0300

    LPS-146415 Mock UPGRADE_CONCURRENT_FETCH_SIZE to avoid NPE

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-forms-web/src/test/java/com/liferay/portal/workflow/kaleo/forms/web/internal/upgrade/v1_0_2/UpgradePortletIdTest.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-forms-web/src/test/java/com/liferay/portal/workflow/kaleo/forms/web/internal/upgrade/v1_0_2/UpgradePortletIdTest.java
@@ -14,10 +14,14 @@
 
 package com.liferay.portal.workflow.kaleo.forms.web.internal.upgrade.v1_0_2;
 
+import com.liferay.portal.kernel.test.util.PropsTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.PropsKeys;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -29,6 +33,14 @@ import org.powermock.modules.junit4.PowerMockRunner;
  */
 @RunWith(PowerMockRunner.class)
 public class UpgradePortletIdTest extends PowerMockito {
+
+	@BeforeClass
+	public static void setUpClass() {
+		PropsTestUtil.setProps(
+			HashMapBuilder.<String, Object>put(
+				PropsKeys.UPGRADE_CONCURRENT_FETCH_SIZE, "1000"
+			).build());
+	}
 
 	@Before
 	public void setUp() throws Exception {
