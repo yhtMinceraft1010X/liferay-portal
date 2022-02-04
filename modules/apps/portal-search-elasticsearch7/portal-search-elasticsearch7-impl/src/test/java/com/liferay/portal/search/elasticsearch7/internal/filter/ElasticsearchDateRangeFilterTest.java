b5e25642ad3fb5b7fba7b87cbe28f61688e9e33e
Joshua Cords <joshua.cords@liferay.com>
2022-02-04 12:07:51 -0800

    LPS-145631 portal-search-elasticsearch7-impl: Elasticsearch exception isn't thrown immediately
    https://issues.liferay.com/browse/LPS-145631

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/filter/ElasticsearchDateRangeFilterTest.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/filter/ElasticsearchDateRangeFilterTest.java
@@ -21,6 +21,7 @@ import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import org.elasticsearch.ElasticsearchStatusException;
 
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -61,28 +62,22 @@ public class ElasticsearchDateRangeFilterTest
 
 	@Test
 	public void testMalformed() throws Exception {
-		expectedException.expect(ElasticsearchStatusException.class);
-		expectedException.expectMessage("all shards failed");
-
 		addDocument(getDate(2000, 11, 22));
 
 		dateRangeFilterBuilder.setFrom("11212000000000");
 		dateRangeFilterBuilder.setTo("11232000000000");
 
-		assertNoHits();
+		assertElasticsearchException();
 	}
 
 	@Test
 	public void testMalformedMultiple() throws Exception {
-		expectedException.expect(ElasticsearchStatusException.class);
-		expectedException.expectMessage("all shards failed");
-
 		addDocument(getDate(2000, 11, 22));
 
 		dateRangeFilterBuilder.setFrom("2000");
 		dateRangeFilterBuilder.setTo("11232000000000");
 
-		assertNoHits();
+		assertElasticsearchException();
 	}
 
 	@Test
@@ -98,6 +93,27 @@ public class ElasticsearchDateRangeFilterTest
 
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
+
+	protected void assertElasticsearchException() {
+		assertSearch(
+			indexingTestHelper -> {
+				indexingTestHelper.setFilter(dateRangeFilterBuilder.build());
+
+				try {
+					indexingTestHelper.search();
+					Assert.fail();
+				}
+				catch (ElasticsearchStatusException
+							elasticsearchStatusException) {
+
+					Assert.assertEquals(
+						"Elasticsearch exception [" +
+							"type=search_phase_execution_exception, " +
+								"reason=all shards failed]",
+						elasticsearchStatusException.getMessage());
+				}
+			});
+	}
 
 	@Override
 	protected IndexingFixture createIndexingFixture() throws Exception {
