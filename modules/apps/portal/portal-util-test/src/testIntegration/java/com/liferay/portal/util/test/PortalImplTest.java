1a2baf2898c262c53dd5b14d09cd1f97a482afc2
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-02-06 09:14:06 +0100

    LPS-146536 Ensure that no NullPointerException is thrown when PortletRequest has an undeployed portletId as attribute.

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplTest.java
@@ -15,12 +15,23 @@
 package com.liferay.portal.util.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.servlet.PortletServlet;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadServletRequest;
 import com.liferay.portal.kernel.util.File;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.osgi.web.portlet.container.test.util.PortletContainerTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -38,6 +49,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.springframework.mock.web.MockHttpServletRequest;
+
 /**
  * @author Peter Fellwock
  */
@@ -48,6 +61,26 @@ public class PortalImplTest {
 	@Rule
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testGetPortletTitleFromPortletRequestWithDeployedPortletId()
+		throws Exception {
+
+		Assert.assertEquals(
+			"Server Administration",
+			_portal.getPortletTitle(
+				_mockPortletRequest(PortletKeys.SERVER_ADMIN)));
+	}
+
+	@Test
+	public void testGetPortletTitleFromPortletRequestWithUndeployedPortletId()
+		throws Exception {
+
+		String portletId = "TEST_PORTLET_" + RandomTestUtil.randomString();
+
+		Assert.assertEquals(
+			portletId, _portal.getPortletTitle(_mockPortletRequest(portletId)));
+	}
 
 	@Test
 	public void testGetPortletTitleWithDeployedPortletId() {
@@ -104,8 +137,63 @@ public class PortalImplTest {
 		}
 	}
 
+	private ThemeDisplay _getThemeDisplay() throws Exception {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		Company company = _companyLocalService.getCompany(
+			TestPropsValues.getCompanyId());
+
+		themeDisplay.setCompany(company);
+
+		_group = GroupTestUtil.addGroup();
+
+		Layout layout = LayoutTestUtil.addLayout(_group);
+
+		themeDisplay.setLayout(layout);
+		themeDisplay.setLayoutSet(layout.getLayoutSet());
+
+		themeDisplay.setLocale(LocaleUtil.US);
+		themeDisplay.setPlid(layout.getPlid());
+		themeDisplay.setPortalURL("http://localhost:8080");
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		return themeDisplay;
+	}
+
+	private MockPortletRequest _mockPortletRequest(String portletId)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = _getThemeDisplay();
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest() {
+				{
+					setAttribute(WebKeys.CTX, getServletContext());
+					setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
+				}
+			};
+
+		return new MockPortletRequest() {
+			{
+				setAttribute(WebKeys.PORTLET_ID, portletId);
+				setAttribute(
+					PortletServlet.PORTLET_SERVLET_REQUEST,
+					mockHttpServletRequest);
+				setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
+			}
+		};
+	}
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
 	@Inject
 	private File _file;
+
+	@DeleteAfterTestRun
+	private Group _group;
 
 	@Inject
 	private Portal _portal;
