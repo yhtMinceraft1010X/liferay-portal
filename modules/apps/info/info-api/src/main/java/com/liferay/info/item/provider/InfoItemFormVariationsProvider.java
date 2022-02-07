63c4fa6c541cb4280a2f65c1e160dfeabf6b3bc2
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-02-07 15:22:32 +0100

    LPS-146691 Provides a new method in order to get the formVariationKey for a specific info item.

--- modules/apps/info/info-api/src/main/java/com/liferay/info/item/provider/InfoItemFormVariationsProvider.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/item/provider/InfoItemFormVariationsProvider.java
@@ -44,6 +44,10 @@ public interface InfoItemFormVariationsProvider<T> {
 		return infoItemFormVariationOptional.orElse(null);
 	}
 
+	public default String getInfoItemFormVariationKey(T t) {
+		return null;
+	}
+
 	public Collection<InfoItemFormVariation> getInfoItemFormVariations(
 		long groupId);
 
