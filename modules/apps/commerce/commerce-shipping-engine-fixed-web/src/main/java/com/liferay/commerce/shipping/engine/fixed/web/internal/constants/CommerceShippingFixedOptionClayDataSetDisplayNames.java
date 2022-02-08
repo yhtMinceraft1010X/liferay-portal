d6026c295c91fe7909d068a56ae21ac157ececad
Alessio Rendina <asilcomando@gmail.com>
2022-02-08 18:31:13 +0100

    COMMERCE-8197 Added new constants

---
+++ modules/apps/commerce/commerce-shipping-engine-fixed-web/src/main/java/com/liferay/commerce/shipping/engine/fixed/web/internal/constants/CommerceShippingFixedOptionClayDataSetDisplayNames.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.shipping.engine.fixed.web.internal.constants;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceShippingFixedOptionClayDataSetDisplayNames {
+
+	public static final String COMMERCE_DATA_SET_KEY_COMMERCE_ORDER_TYPES =
+		"commerceShippingFixedOptionQualifierCommerceOrderTypes";
+
+	public static final String COMMERCE_DATA_SET_KEY_COMMERCE_TERM_ENTRIES =
+		"commerceShippingFixedOptionQualifierCommerceTermEntries";
+
+}
