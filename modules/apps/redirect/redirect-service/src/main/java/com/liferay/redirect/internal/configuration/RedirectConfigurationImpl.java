7ea11ea534e06ea63ed20410ad40ca1405c8f35e
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2022-02-08 14:52:55 +0100

    LPS-145214 Remove deprecation
    We need this interface, as now we need to expose the service
    configuration so that we can conditionally show/hide the 404 tab.

--- modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/internal/configuration/RedirectConfigurationImpl.java
+++ modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/internal/configuration/RedirectConfigurationImpl.java
@@ -14,19 +14,36 @@
 
 package com.liferay.redirect.internal.configuration;
 
-import com.liferay.redirect.configuration.RedirectConfiguration;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 
 /**
  * @author Alejandro Tardín
  */
-@Component(service = RedirectConfiguration.class)
-public class RedirectConfigurationImpl implements RedirectConfiguration {
+@Component(
+	configurationPid = "com.liferay.redirect.internal.configuration.RedirectConfiguration",
+	service = com.liferay.redirect.configuration.RedirectConfiguration.class
+)
+public class RedirectConfigurationImpl
+	implements com.liferay.redirect.configuration.RedirectConfiguration {
 
 	@Override
 	public boolean isEnabled() {
-		return true;
+		return _redirectConfiguration.enabled();
 	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_redirectConfiguration = ConfigurableUtil.createConfigurable(
+			RedirectConfiguration.class, properties);
+	}
+
+	private volatile RedirectConfiguration _redirectConfiguration;
 
 }
