e234679200a3a4200db584f0f3ef328925955ffb
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-02-08 20:18:19 +0100

    LPS-134690 Add unit testing for grouping and ordering vocabularies

--- modules/apps/content-dashboard/content-dashboard-web/test/js/components/SidebarPanelInfoView.test.js
+++ modules/apps/content-dashboard/content-dashboard-web/test/js/components/SidebarPanelInfoView.test.js
@@ -63,16 +63,53 @@ describe('SidebarPanelInfoView', () => {
 		const avatar = container.querySelector('.lexicon-icon-user');
 		expect(avatar).toBeTruthy();
 
+		expect(getByText('id')).toBeInTheDocument();
+		expect(getByText('38070')).toBeInTheDocument();
+
+		expect(getByText('categorization')).toBeInTheDocument();
+		expect(getByText('details')).toBeInTheDocument();
+	});
+
+	it('renders sidebar panel with proper dates for a basic web content', () => {
+		const {getByText} = render(_getSidebarComponent(mockedProps));
+
+		expect(getByText('display-date')).toBeInTheDocument();
+		expect(getByText('Jul 27, 2020, 10:53 AM')).toBeInTheDocument();
+
+		expect(getByText('creation-date')).toBeInTheDocument();
+		expect(getByText('Jul 27, 2020, 10:50 AM')).toBeInTheDocument();
+
+		expect(getByText('modified-date')).toBeInTheDocument();
+		expect(getByText('Jul 27, 2020, 10:50 AM')).toBeInTheDocument();
+
+		expect(getByText('expiration-date')).toBeInTheDocument();
+		expect(getByText('Jul 28, 2020, 10:00 AM')).toBeInTheDocument();
+
+		expect(getByText('review-date')).toBeInTheDocument();
+		expect(getByText('Jul 27, 2020, 2:14 PM')).toBeInTheDocument();
+	});
+
+	it('renders sidebar panel with proper languages for a basic web content', () => {
+		const {getByText} = render(_getSidebarComponent(mockedProps));
+
 		expect(getByText('languages-translated-into')).toBeInTheDocument();
 		expect(getByText('en-US')).toBeInTheDocument();
 		expect(getByText('default')).toBeInTheDocument();
 		expect(getByText('es-ES')).toBeInTheDocument();
 		expect(getByText('fr-FR')).toBeInTheDocument();
 		expect(getByText('pt-BR')).toBeInTheDocument();
+	});
+
+	it('renders sidebar panel with proper tags for a basic web content', () => {
+		const {getByText} = render(_getSidebarComponent(mockedProps));
 
 		expect(getByText('tags')).toBeInTheDocument();
 		expect(getByText('tag1')).toBeInTheDocument();
 		expect(getByText('tag2')).toBeInTheDocument();
+	});
+
+	it('renders sidebar panel with proper vocabularies and categories for a basic web content', () => {
+		const {getByText} = render(_getSidebarComponent(mockedProps));
 
 		expect(getByText('public-categories')).toBeInTheDocument();
 		expect(getByText('internal-categories')).toBeInTheDocument();
@@ -91,29 +128,86 @@ describe('SidebarPanelInfoView', () => {
 		expect(getByText('Internal 1')).toBeInTheDocument();
 		expect(getByText('Internal 6')).toBeInTheDocument();
 
-		expect(getByText('display-date')).toBeInTheDocument();
-		expect(getByText('Jul 27, 2020, 10:53 AM')).toBeInTheDocument();
+		expect(getByText('Foods (Global)')).toBeInTheDocument();
+		expect(getByText('Italian')).toBeInTheDocument();
+		expect(getByText('Mexican')).toBeInTheDocument();
 
-		expect(getByText('creation-date')).toBeInTheDocument();
-		expect(getByText('Jul 27, 2020, 10:50 AM')).toBeInTheDocument();
+		expect(getByText('ZZ Fake vocabulary (Liferay)')).toBeInTheDocument();
+		expect(getByText('Fake 1')).toBeInTheDocument();
+		expect(getByText('Fake 2')).toBeInTheDocument();
 
-		expect(getByText('modified-date')).toBeInTheDocument();
-		expect(getByText('Jul 27, 2020, 10:50 AM')).toBeInTheDocument();
+		expect(
+			getByText('AA Another Fake vocabulary (Liferay)')
+		).toBeInTheDocument();
+		expect(getByText('Another Fake 1')).toBeInTheDocument();
+		expect(getByText('Another Fake 2')).toBeInTheDocument();
 
-		expect(getByText('expiration-date')).toBeInTheDocument();
-		expect(getByText('Jul 28, 2020, 10:00 AM')).toBeInTheDocument();
+		expect(
+			getByText('ZZ Global Random Vocabulary (Global)')
+		).toBeInTheDocument();
+		expect(getByText('Global Random 1')).toBeInTheDocument();
+		expect(getByText('Global Random 2')).toBeInTheDocument();
 
-		expect(getByText('review-date')).toBeInTheDocument();
-		expect(getByText('Jul 27, 2020, 2:14 PM')).toBeInTheDocument();
+		expect(
+			getByText('AA Another Global Random Vocabulary (Global)')
+		).toBeInTheDocument();
+		expect(getByText('Another Global Random 1')).toBeInTheDocument();
+		expect(getByText('Another Global Random 2')).toBeInTheDocument();
 
-		expect(getByText('id')).toBeInTheDocument();
-		expect(getByText('38070')).toBeInTheDocument();
+		expect(getByText('Travel (Demo Site)')).toBeInTheDocument();
+		expect(getByText('Travel 1')).toBeInTheDocument();
+		expect(getByText('Travel 2')).toBeInTheDocument();
 
-		expect(getByText('categorization')).toBeInTheDocument();
-		expect(getByText('details')).toBeInTheDocument();
+		expect(getByText('Clothes (Demo Site)')).toBeInTheDocument();
+		expect(getByText('Clothe 1')).toBeInTheDocument();
+		expect(getByText('Clothe 2')).toBeInTheDocument();
 	});
 
-	it('renders sidebar panel with proper info for a basic web content', () => {
+	it('renders sidebar panel with vocabularies subtitles in the proper order for a basic web content', () => {
+		const {container} = render(_getSidebarComponent(mockedProps));
+
+		const subtitles = container.querySelectorAll(
+			'.item-vocabularies h6.sidebar-section-subtitle-sm'
+		);
+		expect(subtitles.length).toBe(2);
+		expect(subtitles[0].textContent).toBe('public-categories');
+		expect(subtitles[1].textContent).toBe('internal-categories');
+	});
+
+	it('renders sidebar panel with vocabularies names grouped and sorted for a basic web content', () => {
+		const {container} = render(_getSidebarComponent(mockedProps));
+
+		const vocabularies = container.querySelectorAll(
+			'.item-vocabularies h5.font-weight-semi-bold'
+		);
+		expect(vocabularies.length).toBe(
+			Object.keys(mockedProps.vocabularies).length
+		);
+
+		expect(vocabularies[0].textContent).toBe(
+			'AA Another Global Random Vocabulary (Global)'
+		);
+		expect(vocabularies[1].textContent).toBe('Foods (Global)');
+		expect(vocabularies[2].textContent).toBe('Topic (Global)');
+		expect(vocabularies[3].textContent).toBe(
+			'ZZ Global Random Vocabulary (Global)'
+		);
+		expect(vocabularies[4].textContent).toBe(
+			'AA Another Fake vocabulary (Liferay)'
+		);
+		expect(vocabularies[5].textContent).toBe('Clothes (Demo Site)');
+		expect(vocabularies[6].textContent).toBe('Developers (Liferay)');
+		expect(vocabularies[7].textContent).toBe('Travel (Demo Site)');
+		expect(vocabularies[8].textContent).toBe(
+			'ZZ Fake vocabulary (Liferay)'
+		);
+		expect(vocabularies[9].textContent).toBe(
+			'Internal categorization (Liferay)'
+		);
+		expect(vocabularies[10].textContent).toBe('Private Stuff (Demo Site)');
+	});
+
+	it('renders sidebar panel without the categorization for a basic web content', () => {
 		const {queryByText} = render(
 			_getSidebarComponent({...mockedProps, ...mockedNoTaxonomies})
 		);
