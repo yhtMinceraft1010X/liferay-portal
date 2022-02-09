d71ebc4316fb2c3f683b405cc58553bee5d1be80
Matija Petanjek <matija.petanjek@liferay.com>
2022-02-09 14:47:44 +0100

    COMMERCE-7574 batch-engine - change enum with Constants class

---
+++ modules/apps/batch-engine/batch-engine-api/src/main/java/com/liferay/batch/engine/constants/BatchEngineImportTaskConstants.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.batch.engine.constants;
+
+/**
+ * @author Matija Petanjek
+ */
+public class BatchEngineImportTaskConstants {
+
+	public static final String IMPORT_STRATEGY_NAME_ON_ERROR_CONTINUE =
+		"ON_ERROR_CONTINUE";
+
+	public static final String IMPORT_STRATEGY_NAME_ON_ERROR_FAIL =
+		"ON_ERROR_FAIL";
+
+	public static final int IMPORT_STRATEGY_ON_ERROR_CONTINUE = 1;
+
+	public static final int IMPORT_STRATEGY_ON_ERROR_FAIL = 2;
+
+	public static int getImportStrategy(String importStrategyName) {
+		if (importStrategyName.equals(IMPORT_STRATEGY_NAME_ON_ERROR_CONTINUE)) {
+			return IMPORT_STRATEGY_ON_ERROR_CONTINUE;
+		}
+		else if (importStrategyName.equals(
+					IMPORT_STRATEGY_NAME_ON_ERROR_FAIL)) {
+
+			return IMPORT_STRATEGY_ON_ERROR_FAIL;
+		}
+
+		throw new IllegalArgumentException(
+			"Invalid batch engine import task strategy " + importStrategyName);
+	}
+
+}
