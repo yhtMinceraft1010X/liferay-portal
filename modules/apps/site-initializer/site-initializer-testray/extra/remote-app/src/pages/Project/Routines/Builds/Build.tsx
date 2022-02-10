8a3caba8b2c819393419d250696520d211667cac
Keven Leone <keven.leone@liferay.com>
2022-02-10 19:35:13 -0300

    LPS-147214 Render Build Page with Objects

--- modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/pages/Project/Routines/Builds/Build.tsx
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/pages/Project/Routines/Builds/Build.tsx
@@ -12,122 +12,56 @@
  * details.
  */
 
-import {Outlet} from 'react-router-dom';
-
-import BarChartComponent from '../../../../components/Charts/BarChart';
-import PieChartComponent from '../../../../components/Charts/PieChart';
 import Container from '../../../../components/Layout/Container';
-import QATable from '../../../../components/Table/QATable';
-import {DATA_COLORS} from '../../../../util/constants';
-import {runs} from '../../../../util/mock';
+import ListView from '../../../../components/ListView/ListView';
+import {getTestrayCases} from '../../../../graphql/queries';
+import {Liferay} from '../../../../services/liferay/liferay';
 
 const Build = () => {
 	return (
-		<>
-			<Container title="Details">
-				<QATable
-					items={[
-						{title: 'product version', value: ''},
+		<Container className="mt-4" title="Tests">
+			<ListView
+				query={getTestrayCases}
+				tableProps={{
+					columns: [
 						{
-							title: 'description',
-							value:
-								'Portal hash: df78f2f11fbfd7ff41825673dd9a89ee65c8b5c4; Plugins hash: 60a8e64c1db57588b9f1efda4ba94b9d827b4335; Portal branch: 7.1.x; Bundle: latest;',
+							key: 'priority',
+							value: 'Priority',
 						},
-						{title: 'git hash', value: ''},
-						{title: 'create date', value: 'fev 3, 2022 8:38 PM'},
-						{title: 'created by', value: 'John Doe'},
-						{title: 'all issues found', value: '-'},
-					]}
-				/>
-
-				<div className="d-flex mt-4">
-					<dl>
-						<dd>0 minutes</dd>
-
-						<dd className="small-heading">TOTAL ESTIMATED TIME</dd>
-					</dl>
-
-					<dl className="ml-3">
-						<dd>0 minutes</dd>
-
-						<dd className="small-heading">REMAINING ESTIMATED</dd>
-					</dl>
-
-					<dl className="ml-3">
-						<dd>0 minutes</dd>
-
-						<dd className="small-heading">TIME 0 TOTAL ISSUES</dd>
-					</dl>
-				</div>
-			</Container>
-
-			<Container className="mt-4" title="Total Test Cases">
-				<div className="row">
-					<div className="col-3">
-						<PieChartComponent
-							data={[
-								{
-									color: DATA_COLORS['metrics.passed'],
-									name: 'passed',
-									value: 30529,
-								},
-								{
-									color: DATA_COLORS['metrics.failed'],
-									name: 'failed',
-									value: 5374,
-								},
-								{
-									color: DATA_COLORS['metrics.blocked'],
-									name: 'blocked',
-									value: 0,
-								},
-								{
-									color: DATA_COLORS['metrics.test-fix'],
-									name: 'test fix',
-									value: 0,
-								},
-								{
-									color: DATA_COLORS['metrics.incomplete'],
-									name: 'incomplete',
-									value: 21,
-								},
-							]}
-							pieProps={{dataKey: 'value'}}
-						/>
-					</div>
-
-					<div className="col-8 ml-6">
-						<BarChartComponent
-							bars={[
-								{
-									dataKey: 'failed',
-									fill: DATA_COLORS['metrics.failed'],
-								},
-								{
-									dataKey: 'incomplete',
-									fill: DATA_COLORS['metrics.incomplete'],
-								},
-								{
-									dataKey: 'test_fix',
-									fill: DATA_COLORS['metrics.test-fix'],
-								},
-								{
-									dataKey: 'blocked',
-									fill: DATA_COLORS['metrics.blocked'],
-								},
-								{
-									dataKey: 'passed',
-									fill: DATA_COLORS['metrics.passed'],
-								},
-							]}
-							data={runs}
-						/>
-					</div>
-				</div>
-			</Container>
-
-			<Outlet />
-		</>
+						{
+							key: 'component',
+							value: 'Component',
+						},
+						{
+							key: 'name',
+							value: 'Case',
+						},
+						{
+							key: 'run',
+							value: 'Run',
+						},
+						{
+							key: 'assignee',
+							value: 'Assignee',
+						},
+						{
+							key: 'status',
+							value: 'Status',
+						},
+						{
+							key: 'issues',
+							value: 'Issues',
+						},
+						{
+							key: 'error',
+							value: 'Errors',
+						},
+					],
+				}}
+				transformData={(data) => data?.c?.testrayCases}
+				variables={{scopeKey: Liferay.ThemeDisplay.getScopeGroupId()}}
+			/>
+		</Container>
 	);
 };
 
