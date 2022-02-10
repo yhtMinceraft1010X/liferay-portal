7ac999cb5d9843782c056d2f30ecf0b8666bcccc
Julien Castelain <julien.castelain@liferay.com>
2022-02-10 15:20:12 +0100

    LPS-144630 Add a feature flag mechanism for frontend only features
    With this change, we'll be able to define feature flags for "front end"
    related features, such as migrating an existing component to a new one,
    or similar use cases.
    
    To use it, you'll first need to define a name for your feature flag
    in the configuration file (FFFrontendJSComponentsConfiguration.java).
    Once it is defined, create a file named:
    
    com.liferay.frontend.js.components.web.internal.configuration.FFFrontendJSComponentsConfiguration.config
    
    in bundles/osgi/configs with the following format:
    
    FEATURE_FLAG_NAME=ENABLED_OR_DISABLED
    
    for example:
    enableClayTreeView="true"
    
    Once that's done, make sure the frontend-js-components-web module
    is deployed and you'll be able to access the feature flag via
    JavaScript in the Liferay.__FF__ object

---
+++ modules/apps/frontend-js/frontend-js-components-web/src/main/java/com/liferay/frontend/js/components/web/internal/servlet/taglib/FrontendComponentsTopHeadDynamicInclude.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.components.web.internal.servlet.taglib;
+
+import com.liferay.frontend.js.components.web.internal.configuration.FFFrontendJSComponentsConfiguration;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Julien Castelain
+ */
+@Component(
+	configurationPid = "com.liferay.frontend.js.components.web.internal.configuration.FFFrontendJSComponentsConfiguration",
+	immediate = true, service = DynamicInclude.class
+)
+public class FrontendComponentsTopHeadDynamicInclude
+	extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("<script>var Liferay = window.Liferay || {};");
+		sb.append("Liferay.__FF__ = Liferay.__FF__ || {};");
+		sb.append("Liferay.__FF__.enableClayTreeView = ");
+		sb.append(_ffFrontendJSComponentsConfiguration.enableClayTreeView());
+		sb.append(";</script>");
+
+		printWriter.println(sb.toString());
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register("/html/common/themes/top_head.jsp#pre");
+	}
+
+	@Activate
+	@Modified
+	protected void activate(
+			BundleContext bundleContext, ComponentContext componentContext,
+			Map<String, Object> properties)
+		throws Exception {
+
+		_ffFrontendJSComponentsConfiguration =
+			ConfigurableUtil.createConfigurable(
+				FFFrontendJSComponentsConfiguration.class, properties);
+	}
+
+	private volatile FFFrontendJSComponentsConfiguration
+		_ffFrontendJSComponentsConfiguration;
+
+	@Reference
+	private Portal _portal;
+
+}
