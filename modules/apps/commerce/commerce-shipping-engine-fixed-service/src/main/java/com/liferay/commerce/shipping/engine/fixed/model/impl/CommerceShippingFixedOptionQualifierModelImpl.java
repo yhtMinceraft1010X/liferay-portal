fe320689bae245fa35e10b176ab085964712e87e
Brian Chan <brian.chan@liferay.com>
2022-02-09 16:40:07 -0800

    LPS-145347 regen

--- modules/apps/commerce/commerce-shipping-engine-fixed-service/src/main/java/com/liferay/commerce/shipping/engine/fixed/model/impl/CommerceShippingFixedOptionQualifierModelImpl.java
+++ modules/apps/commerce/commerce-shipping-engine-fixed-service/src/main/java/com/liferay/commerce/shipping/engine/fixed/model/impl/CommerceShippingFixedOptionQualifierModelImpl.java
@@ -16,7 +16,6 @@ package com.liferay.commerce.shipping.engine.fixed.model.impl;
 
 import com.liferay.commerce.shipping.engine.fixed.model.CommerceShippingFixedOptionQualifier;
 import com.liferay.commerce.shipping.engine.fixed.model.CommerceShippingFixedOptionQualifierModel;
-import com.liferay.commerce.shipping.engine.fixed.model.CommerceShippingFixedOptionQualifierSoap;
 import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.expando.kernel.util.ExpandoBridgeFactoryUtil;
 import com.liferay.petra.string.StringBundler;
@@ -44,12 +43,10 @@ import java.lang.reflect.InvocationHandler;
 import java.sql.Blob;
 import java.sql.Types;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.BiConsumer;
@@ -163,66 +160,6 @@ public class CommerceShippingFixedOptionQualifierModelImpl
 	 */
 	@Deprecated
 	public static final long CREATEDATE_COLUMN_BITMASK = 8L;
-
-	/**
-	 * Converts the soap model instance into a normal model instance.
-	 *
-	 * @param soapModel the soap model instance to convert
-	 * @return the normal model instance
-	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
-	 */
-	@Deprecated
-	public static CommerceShippingFixedOptionQualifier toModel(
-		CommerceShippingFixedOptionQualifierSoap soapModel) {
-
-		if (soapModel == null) {
-			return null;
-		}
-
-		CommerceShippingFixedOptionQualifier model =
-			new CommerceShippingFixedOptionQualifierImpl();
-
-		model.setMvccVersion(soapModel.getMvccVersion());
-		model.setCommerceShippingFixedOptionQualifierId(
-			soapModel.getCommerceShippingFixedOptionQualifierId());
-		model.setCompanyId(soapModel.getCompanyId());
-		model.setUserId(soapModel.getUserId());
-		model.setUserName(soapModel.getUserName());
-		model.setCreateDate(soapModel.getCreateDate());
-		model.setModifiedDate(soapModel.getModifiedDate());
-		model.setClassNameId(soapModel.getClassNameId());
-		model.setClassPK(soapModel.getClassPK());
-		model.setCommerceShippingFixedOptionId(
-			soapModel.getCommerceShippingFixedOptionId());
-
-		return model;
-	}
-
-	/**
-	 * Converts the soap model instances into normal model instances.
-	 *
-	 * @param soapModels the soap model instances to convert
-	 * @return the normal model instances
-	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
-	 */
-	@Deprecated
-	public static List<CommerceShippingFixedOptionQualifier> toModels(
-		CommerceShippingFixedOptionQualifierSoap[] soapModels) {
-
-		if (soapModels == null) {
-			return null;
-		}
-
-		List<CommerceShippingFixedOptionQualifier> models =
-			new ArrayList<CommerceShippingFixedOptionQualifier>(
-				soapModels.length);
-
-		for (CommerceShippingFixedOptionQualifierSoap soapModel : soapModels) {
-			models.add(toModel(soapModel));
-		}
-
-		return models;
-	}
 
 	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(
 		com.liferay.commerce.shipping.engine.fixed.service.util.ServiceProps.
