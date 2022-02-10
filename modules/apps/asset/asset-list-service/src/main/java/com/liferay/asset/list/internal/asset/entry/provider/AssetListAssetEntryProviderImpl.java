ecad468be1488aa5339150856d977c7fe1c2c1a7
Victor Galan <victor.galan@liferay.com>
2022-02-10 14:27:53 +0100

    LPS-145480 Rename to proper segmentEntryId name when transforming to long array
    This reverts commit 7b9ea7f515844317684f99a700a9269b1d10c3db.

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
@@ -722,7 +722,7 @@ public class AssetListAssetEntryProviderImpl
 		).map(
 			AssetListEntrySegmentsEntryRelModel::getSegmentsEntryId
 		).mapToLong(
-			i -> i
+			segmentEntryId -> segmentEntryId
 		).toArray();
 	}
 
