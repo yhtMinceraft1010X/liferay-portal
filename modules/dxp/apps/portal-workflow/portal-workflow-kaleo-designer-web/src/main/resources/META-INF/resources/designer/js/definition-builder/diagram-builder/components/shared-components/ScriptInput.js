f376dc094719112d55f1b69c744a904a4ce51f74
MariMalta <maltamariana123@gmail.com>
2022-02-10 16:02:11 -0300

    LPS-146613 Create ScriptInput component

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/shared-components/ScriptInput.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import ClayForm, {ClayInput} from '@clayui/form';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const ScriptInput = ({inputValue, updateSelectedItem}) => (
+	<ClayForm.Group>
+		<label htmlFor="nodeScript">
+			{`${Liferay.Language.get('script')} (${Liferay.Language.get(
+				'groovy'
+			)})`}
+		</label>
+
+		<ClayInput
+			component="textarea"
+			id="nodeScript"
+			onChange={updateSelectedItem}
+			placeholder='returnValue = "Transition Name";'
+			type="text"
+			value={inputValue}
+		/>
+	</ClayForm.Group>
+);
+
+export default ScriptInput;
+
+ScriptInput.propTypes = {
+	inputValue: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
+		.isRequired,
+	updateSelectedItem: PropTypes.func,
+};
