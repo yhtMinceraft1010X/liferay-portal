2c0cfda0da3c3023fd9828cc297f72cd60e3f308
gustavolimav <gustavo.lima@liferay.com>
2022-02-10 14:25:08 -0300

    LPS-143238 search-experiences-rest-test: Delete data after test run (fixes testGetSXP*PageWithFilterDateTimeEquals)
    https://issues.liferay.com/browse/LPS-143238

--- modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/SXPBlueprintResourceTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/SXPBlueprintResourceTest.java
@@ -18,6 +18,7 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.search.experiences.rest.client.dto.v1_0.SXPBlueprint;
+import com.liferay.search.experiences.rest.client.pagination.Page;
 
 import java.util.Collections;
 
@@ -31,6 +32,16 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class SXPBlueprintResourceTest extends BaseSXPBlueprintResourceTestCase {
+
+	@Override
+	@Test
+	public void testGetSXPBlueprintsPageWithFilterDateTimeEquals()
+		throws Exception {
+
+		_deleteSXPBlueprints();
+
+		super.testGetSXPBlueprintsPageWithFilterDateTimeEquals();
+	}
 
 	@Ignore
 	@Override
@@ -80,6 +91,8 @@ public class SXPBlueprintResourceTest extends BaseSXPBlueprintResourceTestCase {
 	@Override
 	protected SXPBlueprint randomSXPBlueprint() throws Exception {
 		SXPBlueprint sxpBlueprint = super.randomSXPBlueprint();
+
+		sxpBlueprint.setTitle(_TITLE_PREFIX + sxpBlueprint.getTitle());
 
 		sxpBlueprint.setTitle_i18n(
 			Collections.singletonMap("en_US", sxpBlueprint.getTitle()));
@@ -147,5 +160,20 @@ public class SXPBlueprintResourceTest extends BaseSXPBlueprintResourceTestCase {
 
 		return sxpBlueprintResource.postSXPBlueprint(sxpBlueprint);
 	}
+
+	private void _deleteSXPBlueprints() throws Exception {
+		Page<SXPBlueprint> page = sxpBlueprintResource.getSXPBlueprintsPage(
+			null, null, null, null);
+
+		for (SXPBlueprint sxpBlueprint : page.getItems()) {
+			String title = sxpBlueprint.getTitle();
+
+			if (title.startsWith(_TITLE_PREFIX)) {
+				sxpBlueprintResource.deleteSXPBlueprint(sxpBlueprint.getId());
+			}
+		}
+	}
+
+	private static final String _TITLE_PREFIX = "SXPBRT";
 
 }
--- modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/SXPElementResourceTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/SXPElementResourceTest.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.search.experiences.rest.client.dto.v1_0.SXPElement;
+import com.liferay.search.experiences.rest.client.pagination.Page;
 
 import java.util.Collections;
 
@@ -32,6 +33,16 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class SXPElementResourceTest extends BaseSXPElementResourceTestCase {
+
+	@Override
+	@Test
+	public void testGetSXPElementsPageWithFilterDateTimeEquals()
+		throws Exception {
+
+		_deleteSXPElements();
+
+		super.testGetSXPElementsPageWithFilterDateTimeEquals();
+	}
 
 	@Ignore
 	@Override
@@ -93,6 +104,8 @@ public class SXPElementResourceTest extends BaseSXPElementResourceTestCase {
 	protected SXPElement randomSXPElement() throws Exception {
 		SXPElement sxpElement = super.randomSXPElement();
 
+		sxpElement.setTitle(_TITLE_PREFIX + sxpElement.getTitle());
+
 		sxpElement.setTitle_i18n(
 			Collections.singletonMap("en_US", sxpElement.getTitle()));
 
@@ -150,5 +163,20 @@ public class SXPElementResourceTest extends BaseSXPElementResourceTestCase {
 	private SXPElement _addSXPElement(SXPElement sxpElement) throws Exception {
 		return sxpElementResource.postSXPElement(sxpElement);
 	}
+
+	private void _deleteSXPElements() throws Exception {
+		Page<SXPElement> page = sxpElementResource.getSXPElementsPage(
+			null, null, null, null);
+
+		for (SXPElement sxpElement : page.getItems()) {
+			String title = sxpElement.getTitle();
+
+			if (title.startsWith(_TITLE_PREFIX)) {
+				sxpElementResource.deleteSXPElement(sxpElement.getId());
+			}
+		}
+	}
+
+	private static final String _TITLE_PREFIX = "SXPERT";
 
 }
