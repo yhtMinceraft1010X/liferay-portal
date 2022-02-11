30da04f0dfa89d8f6be04e9c43a1ee4fd46e399b
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-02-11 14:16:00 +0100

    LPS-148010 Creates a ModelDocumentContributor for JournalArticle.

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/spi/model/index/contributor/JournalArticleModelDocumentContributor.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.search.spi.model.index.contributor;
+
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.journal.model.JournalArticle",
+	service = ModelDocumentContributor.class
+)
+public class JournalArticleModelDocumentContributor
+	implements ModelDocumentContributor<JournalArticle> {
+
+	@Override
+	public void contribute(Document document, JournalArticle journalArticle) {
+	}
+
+}
