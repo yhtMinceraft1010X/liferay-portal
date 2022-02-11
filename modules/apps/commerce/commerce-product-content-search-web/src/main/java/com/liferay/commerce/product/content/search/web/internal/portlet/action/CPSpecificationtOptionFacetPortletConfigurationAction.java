3b0a4d9558cc84f3df3ec7d7114b98462a2620a5
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2022-02-11 16:13:29 +0100

    COMMERCE-1489 Added action class
    COMMERCE-1489 Added check on the maximum terms limit
    
    action

---
+++ modules/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/portlet/action/CPSpecificationtOptionFacetPortletConfigurationAction.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.content.search.web.internal.portlet.action;
+
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.commerce.product.content.search.web.internal.configuration.CPSpecificationOptionFacetPortletInstanceConfiguration;
+import com.liferay.commerce.product.content.search.web.internal.display.context.CPSpecificationOptionFacetsDisplayContext;
+import com.liferay.commerce.product.content.search.web.internal.portlet.CPSpecificationOptionFacetPortletPreferences;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropertiesParamUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Crescenzo Rega
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "javax.portlet.name=" + CPPortletKeys.CP_SPECIFICATION_OPTION_FACETS,
+	service = ConfigurationAction.class
+)
+public class CPSpecificationtOptionFacetPortletConfigurationAction
+	extends DefaultConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		try {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)httpServletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+			CPSpecificationOptionFacetPortletInstanceConfiguration
+				cpSpecificationOptionFacetPortletInstanceConfiguration =
+					portletDisplay.getPortletInstanceConfiguration(
+						CPSpecificationOptionFacetPortletInstanceConfiguration.
+							class);
+
+			CPSpecificationOptionFacetsDisplayContext
+				cpSpecificationOptionFacetsDisplayContext =
+					new CPSpecificationOptionFacetsDisplayContext(
+						cpSpecificationOptionFacetPortletInstanceConfiguration);
+
+			httpServletRequest.setAttribute(
+				WebKeys.PORTLET_DISPLAY_CONTEXT,
+				cpSpecificationOptionFacetsDisplayContext);
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+		}
+
+		return "/specification_option_facets/configuration.jsp";
+	}
+
+	@Override
+	public void processAction(
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse)
+		throws Exception {
+
+		UnicodeProperties unicodeProperties = PropertiesParamUtil.getProperties(
+			actionRequest, _PARAMETER_NAME_PREFIX);
+
+		String maxTerms = unicodeProperties.getProperty(
+			CPSpecificationOptionFacetPortletPreferences.
+				PREFERENCE_KEY_MAX_TERMS);
+
+		if (Validator.isNumber(maxTerms)) {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+			PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+			CPSpecificationOptionFacetPortletInstanceConfiguration
+				cpSpecificationOptionFacetPortletInstanceConfiguration =
+					portletDisplay.getPortletInstanceConfiguration(
+						CPSpecificationOptionFacetPortletInstanceConfiguration.
+							class);
+
+			if (GetterUtil.getInteger(maxTerms) >
+					cpSpecificationOptionFacetPortletInstanceConfiguration.
+						limitMaxTerms()) {
+
+				SessionErrors.add(actionRequest, "exceededMaxTermsLimit");
+			}
+		}
+
+		if (SessionErrors.isEmpty(actionRequest)) {
+			super.processAction(portletConfig, actionRequest, actionResponse);
+		}
+	}
+
+	private static final String _PARAMETER_NAME_PREFIX = "preferences--";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPSpecificationtOptionFacetPortletConfigurationAction.class);
+
+}
