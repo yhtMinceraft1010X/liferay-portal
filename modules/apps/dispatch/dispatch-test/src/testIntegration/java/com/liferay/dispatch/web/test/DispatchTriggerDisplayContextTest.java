da246b07d6835f98209beb1780201e507aceaf88
joemduffy <89780329+joemduffy@users.noreply.github.com>
2022-02-11 12:44:38 +0000

    COMMERCE-8276 dispatch-test java hidden executor and DispatchTriggerDisplayContextTest.java

---
+++ modules/apps/dispatch/dispatch-test/src/testIntegration/java/com/liferay/dispatch/web/test/DispatchTriggerDisplayContextTest.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dispatch.web.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dispatch.internal.messaging.test.UIInvisibleTestDispatchTaskExecutor;
+import com.liferay.dispatch.service.DispatchTriggerLocalService;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderRequest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderResponse;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Joe Duffy
+ */
+@DataGuard(scope = DataGuard.Scope.METHOD)
+@RunWith(Arquillian.class)
+@Sync
+public class DispatchTriggerDisplayContextTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE,
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Test
+	public void testDispatchTriggerDisplayContextCheckHidden()
+		throws Exception {
+
+		Group group = GroupTestUtil.addGroup();
+
+		Company company = _companyLocalService.getCompany(group.getCompanyId());
+
+		User user = UserTestUtil.addUser(company);
+
+		Object dispatchTriggerDisplayContext =
+			_getDispatchTriggerDisplayContext(
+				_getMockHttpServletRequest(
+					company, LayoutTestUtil.addLayout(group), user));
+
+		Assert.assertNotNull(dispatchTriggerDisplayContext);
+
+		try {
+			Set<String> executorTypes = ReflectionTestUtil.invoke(
+				dispatchTriggerDisplayContext, "getDispatchTaskExecutorTypes",
+				new Class<?>[0], null);
+
+			Assert.assertFalse(
+				"Executor not hidden!",
+				executorTypes.contains(
+					UIInvisibleTestDispatchTaskExecutor.
+						DISPATCH_TASK_EXECUTOR_TYPE_TEST_HIDDEN));
+		}
+		catch (Exception exception) {
+		}
+	}
+
+	private Object _getDispatchTriggerDisplayContext(
+			MockHttpServletRequest mockHttpServletRequest)
+		throws Exception {
+
+		MockLiferayPortletRenderRequest mockLiferayPortletRenderRequest =
+			new MockLiferayPortletRenderRequest(mockHttpServletRequest);
+
+		_mvcRenderCommand.render(
+			mockLiferayPortletRenderRequest,
+			new MockLiferayPortletRenderResponse());
+
+		return mockLiferayPortletRenderRequest.getAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT);
+	}
+
+	private MockHttpServletRequest _getMockHttpServletRequest(
+			Company company, Layout layout, User user)
+		throws Exception {
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay(company, layout, user));
+
+		return mockHttpServletRequest;
+	}
+
+	private ThemeDisplay _getThemeDisplay(
+			Company company, Layout layout, User user)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setCompany(company);
+		themeDisplay.setLayout(layout);
+		themeDisplay.setLocale(LocaleUtil.US);
+		themeDisplay.setPermissionChecker(
+			PermissionThreadLocal.getPermissionChecker());
+		themeDisplay.setScopeGroupId(layout.getGroupId());
+		themeDisplay.setUser(user);
+
+		return themeDisplay;
+	}
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@Inject
+	private DispatchTriggerLocalService _dispatchTriggerLocalService;
+
+	@Inject(
+		filter = "component.name=com.liferay.dispatch.web.internal.portlet.action.ViewDispatchTriggerMVCRenderCommand"
+	)
+	private MVCRenderCommand _mvcRenderCommand;
+
+}
