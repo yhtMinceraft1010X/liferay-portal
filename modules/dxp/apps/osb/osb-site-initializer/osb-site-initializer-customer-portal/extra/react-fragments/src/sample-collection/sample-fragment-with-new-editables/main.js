9b2cbc44c1295515b78b82ecc61dd84c9dd7d871
Jair Medeiros <jair.medeiros@liferay.com>
2022-02-14 20:44:05 -0300

    LPS-144867 Remove unused sample fragments

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/react-fragments/src/sample-collection/sample-fragment-with-new-editables/main.js
+++
@@ -1,4 +0,0 @@
-console.group('Sample fragment with new editables');
-console.log('fragmentElement', fragmentElement);
-console.log('configuration', configuration);
-console.groupEnd();
--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/react-fragments/src/sample-collection/sample-fragment/main.js
+++
@@ -1,4 +0,0 @@
-console.group('Sample fragment');
-console.log('fragmentElement', fragmentElement);
-console.log('configuration', configuration);
-console.groupEnd();
