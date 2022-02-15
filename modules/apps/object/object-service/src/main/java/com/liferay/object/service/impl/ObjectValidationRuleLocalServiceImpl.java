3d92facd6dad46eb75d76f1b75bfb29978a8542f
Brian Chan <brian.chan@liferay.com>
2022-02-15 13:07:39 -0300

    LPS-143916 Pass Locale

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleLocalServiceImpl.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
 
 import java.util.List;
 import java.util.Map;
@@ -103,7 +104,8 @@ public class ObjectValidationRuleLocalServiceImpl
 					hashMapWrapper.build(), objectValidationRule.getScript())) {
 
 				throw new ObjectValidationException(
-					objectValidationRule.getErrorLabel());
+					objectValidationRule.getErrorLabel(
+						LocaleUtil.getMostRelevantLocale()));
 			}
 		}
 	}
