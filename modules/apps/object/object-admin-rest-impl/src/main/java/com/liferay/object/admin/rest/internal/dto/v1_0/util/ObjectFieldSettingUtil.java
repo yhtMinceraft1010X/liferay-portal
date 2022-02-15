ea26115cba570d991eab0758e5b1fec1e3fb43de
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-02-14 22:13:15 -0300

    LPS-147113 Implement resource for ObjectFieldSetting

---
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectFieldSettingUtil.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.admin.rest.internal.dto.v1_0.util;
+
+import com.liferay.object.admin.rest.dto.v1_0.ObjectFieldSetting;
+import com.liferay.object.service.ObjectFieldSettingLocalService;
+
+/**
+ * @author Carolina Barbosa
+ */
+public class ObjectFieldSettingUtil {
+
+	public static com.liferay.object.model.ObjectFieldSetting
+		toObjectFieldSetting(
+			ObjectFieldSetting objectFieldSetting,
+			ObjectFieldSettingLocalService objectFieldSettingLocalService) {
+
+		com.liferay.object.model.ObjectFieldSetting
+			serviceBuilderObjectFieldSetting =
+				objectFieldSettingLocalService.createObjectFieldSetting(0L);
+
+		serviceBuilderObjectFieldSetting.setObjectFieldId(
+			objectFieldSetting.getObjectFieldId());
+		serviceBuilderObjectFieldSetting.setName(objectFieldSetting.getName());
+		serviceBuilderObjectFieldSetting.setRequired(
+			objectFieldSetting.getRequired());
+		serviceBuilderObjectFieldSetting.setValue(
+			objectFieldSetting.getValue());
+
+		return serviceBuilderObjectFieldSetting;
+	}
+
+	public static ObjectFieldSetting toObjectFieldSetting(
+		com.liferay.object.model.ObjectFieldSetting
+			serviceBuilderObjectFieldSetting) {
+
+		return new ObjectFieldSetting() {
+			{
+				id = serviceBuilderObjectFieldSetting.getObjectFieldSettingId();
+				name = serviceBuilderObjectFieldSetting.getName();
+				objectFieldId =
+					serviceBuilderObjectFieldSetting.getObjectFieldId();
+				required = serviceBuilderObjectFieldSetting.getRequired();
+				value = serviceBuilderObjectFieldSetting.getValue();
+			}
+		};
+	}
+
+}
