2a00a29168b6ca77c0d020f6afed15b9f839b609
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2022-02-15 23:01:38 +0100

    LRAC-10551 analytics-batch-export-import-api Analytics Batch export import helper

---
+++ modules/apps/analytics/analytics-batch-export-import-api/src/main/java/com/liferay/analytics/batch/exportimport/helper/AnalyticsBatchExportImportHelper.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.batch.exportimport.helper;
+
+import com.liferay.petra.function.UnsafeConsumer;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Riccardo Ferrari
+ */
+public interface AnalyticsBatchExportImportHelper {
+
+	public void exportToAnalyticsCloud(
+			String batchEngineExportTaskItemDelegateName, long companyId,
+			List<String> fieldList,
+			UnsafeConsumer<String, Exception> notificationHandler,
+			Date resourceLastModifiedDate, String resourceName, long userId)
+		throws Exception;
+
+	public void importFromAnalyticsCloud(
+			String batchEngineImportTaskItemDelegateName, long companyId,
+			Map<String, String> fieldMapping,
+			UnsafeConsumer<String, Exception> notificationHandler,
+			Date resourceLastModifiedDate, String resourceName, long userId)
+		throws Exception;
+
+}
