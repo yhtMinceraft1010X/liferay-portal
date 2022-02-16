11804166da27d57c71d05062668a9ceb6e594afb
Cheryl Tang <cheryl.tang@liferay.com>
2022-02-16 03:41:12 -0800

    LPS-124618 Delete the PortletPreferenceValue children of PortletPreferences that are deleted by the constraint resolver

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/spi/resolver/PortletPreferenceConstraintResolver.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/spi/resolver/PortletPreferenceConstraintResolver.java
@@ -16,10 +16,16 @@ package com.liferay.change.tracking.internal.spi.resolver;
 
 import com.liferay.change.tracking.spi.resolver.ConstraintResolver;
 import com.liferay.change.tracking.spi.resolver.context.ConstraintResolverContext;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.PortletPreferenceValueTable;
 import com.liferay.portal.kernel.model.PortletPreferences;
+import com.liferay.portal.kernel.service.PortletPreferenceValueLocalService;
 import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
+import com.liferay.portal.kernel.service.persistence.PortletPreferenceValuePersistence;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -61,14 +67,48 @@ public class PortletPreferenceConstraintResolver
 
 	@Override
 	public void resolveConflict(
-		ConstraintResolverContext<PortletPreferences>
-			constraintResolverContext) {
+			ConstraintResolverContext<PortletPreferences>
+				constraintResolverContext)
+		throws PortalException {
+
+		PortletPreferences sourcePortletPreferences =
+			constraintResolverContext.getSourceCTModel();
 
 		_portletPreferencesLocalService.deletePortletPreferences(
 			constraintResolverContext.getSourceCTModel());
+
+		List<Long> portletPreferenceValueIds =
+			_portletPreferenceValueLocalService.dslQuery(
+				DSLQueryFactoryUtil.select(
+					PortletPreferenceValueTable.INSTANCE.
+						portletPreferenceValueId
+				).from(
+					PortletPreferenceValueTable.INSTANCE
+				).where(
+					PortletPreferenceValueTable.INSTANCE.portletPreferencesId.
+						eq(
+							sourcePortletPreferences.getPortletPreferencesId()
+						).and(
+							PortletPreferenceValueTable.INSTANCE.ctCollectionId.
+								eq(sourcePortletPreferences.getCtCollectionId())
+						)
+				));
+
+		for (long portletPreferenceValueId : portletPreferenceValueIds) {
+			_portletPreferenceValueLocalService.deletePortletPreferenceValue(
+				portletPreferenceValueId);
+		}
 	}
 
 	@Reference
 	private PortletPreferencesLocalService _portletPreferencesLocalService;
+
+	@Reference
+	private PortletPreferenceValueLocalService
+		_portletPreferenceValueLocalService;
+
+	@Reference
+	private PortletPreferenceValuePersistence
+		_portletPreferenceValuePersistence;
 
 }
