037e699c8629490ff510eb48e835f2d367ff7c25
Wade Cao <wade.cao@liferay.com>
2022-02-16 10:14:14 -0800

    LPS-140441 search-experiences-test:add testIPStackConfiguration case back
    https://issues.liferay.com/browse/LPS-140441

--- modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/search/spi/searcher/test/SXPBlueprintSearchRequestContributorTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/search/spi/searcher/test/SXPBlueprintSearchRequestContributorTest.java
@@ -17,13 +17,22 @@ package com.liferay.search.experiences.internal.blueprint.search.spi.searcher.te
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.ConfigurationTemporarySwapper;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -32,18 +41,22 @@ import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
 import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.searcher.SearchResponse;
 import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.portal.search.spi.searcher.SearchRequestContributor;
 import com.liferay.portal.search.test.util.DocumentsAssert;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.util.HttpImpl;
 import com.liferay.search.experiences.model.SXPBlueprint;
 import com.liferay.search.experiences.service.SXPBlueprintLocalService;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.IOException;
 
+import java.util.Collections;
+
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -64,54 +77,26 @@ public class SXPBlueprintSearchRequestContributorTest {
 			new LiferayIntegrationTestRule(),
 			PermissionCheckerMethodTestRule.INSTANCE);
 
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		WorkflowThreadLocal.setEnabled(false);
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		WorkflowThreadLocal.setEnabled(true);
+	}
+
 	@Before
 	public void setUp() throws Exception {
-		WorkflowThreadLocal.setEnabled(false);
+		_group = GroupTestUtil.addGroup();
 
-		_sxpBlueprint = _addSXPBlueprint();
-	}
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group, TestPropsValues.getUserId());
 
-	@Test
-	public void testQueryConfiguration() throws Exception {
-		_addJournalArticles("alpha", "beta", "charlie");
-
-		_assertSearch("[beta]", "localized_title_en_US", "beta");
-	}
-
-	@Test
-	public void testSortConfiguration() throws Exception {
-		_addJournalArticles("alpha delta", "beta delta", "charlie delta");
-
-		_assertSearch(
-			"[charlie delta, beta delta, alpha delta]", "localized_title_en_US",
-			"delta");
-	}
-
-	@Rule
-	public TestName testName = new TestName();
-
-	private void _addJournalArticles(String... titles) throws Exception {
-		for (String title : titles) {
-			_journalArticles.add(
-				JournalTestUtil.addArticle(
-					TestPropsValues.getGroupId(), 0,
-					PortalUtil.getClassNameId(JournalArticle.class),
-					HashMapBuilder.put(
-						LocaleUtil.US, title
-					).build(),
-					null,
-					HashMapBuilder.put(
-						LocaleUtil.US, ""
-					).build(),
-					LocaleUtil.getSiteDefault(), false, true,
-					ServiceContextTestUtil.getServiceContext()));
-		}
-	}
-
-	private SXPBlueprint _addSXPBlueprint() throws Exception {
 		Class<?> clazz = getClass();
 
-		return _sxpBlueprintLocalService.addSXPBlueprint(
+		_sxpBlueprint = _sxpBlueprintLocalService.addSXPBlueprint(
 			TestPropsValues.getUserId(),
 			StringUtil.read(
 				clazz,
@@ -123,38 +108,166 @@ public class SXPBlueprintSearchRequestContributorTest {
 			"", "",
 			Collections.singletonMap(
 				LocaleUtil.US, RandomTestUtil.randomString()),
-			ServiceContextTestUtil.getServiceContext());
+			_serviceContext);
 	}
 
+	@Test
+	public void testIPStackConfiguration() throws Exception {
+		_test(
+			new String[] {"diamond bar city", "walnut city"},
+			() -> {
+				HttpUtil httpUtil = new HttpUtil();
+
+				try (ConfigurationTemporarySwapper
+						configurationTemporarySwapper =
+							_getConfigurationTemporarySwapper(
+								"2345", "34.94.32.240", "true")) {
+
+					httpUtil.setHttp(
+						_getHttp(
+							JSONUtil.put(
+								"city", "diamond bar"
+							).toJSONString()));
+
+					_assertSearch("[diamond bar city]", "34.94.32.240", "city");
+				}
+				finally {
+					httpUtil.setHttp(_http);
+				}
+
+				try (ConfigurationTemporarySwapper
+						configurationTemporarySwapper =
+							_getConfigurationTemporarySwapper(
+								"2345", "91.233.116.229", "true")) {
+
+					httpUtil.setHttp(
+						_getHttp(
+							JSONUtil.put(
+								"city", "walnut"
+							).toJSONString()));
+
+					_assertSearch("[walnut city]", "91.233.116.229", "city");
+				}
+				finally {
+					httpUtil.setHttp(_http);
+				}
+			});
+	}
+
+	@Test
+	public void testQueryConfiguration() throws Exception {
+		_test(
+			new String[] {"alpha", "beta", "charlie"},
+			() -> _assertSearch("[beta]", "", "beta"));
+	}
+
+	@Test
+	public void testSortConfiguration() throws Exception {
+		_test(
+			new String[] {"alpha delta", "beta delta", "charlie delta"},
+			() -> _assertSearch(
+				"[charlie delta, beta delta, alpha delta]", "", "delta"));
+	}
+
+	@Rule
+	public TestName testName = new TestName();
+
 	private void _assertSearch(
-			String expected, String fieldName, String keywords)
+			String expected, String ipAddress, String keywords)
 		throws Exception {
 
 		SearchResponse searchResponse = _searcher.search(
-			_searchRequestBuilderFactory.builder(
-			).companyId(
-				TestPropsValues.getCompanyId()
-			).queryString(
-				keywords
-			).withSearchContext(
-				searchContext -> searchContext.setAttribute(
-					"search.experiences.blueprint.id",
-					_sxpBlueprint.getSXPBlueprintId())
-			).build());
+			_searchRequestContributor.contribute(
+				_searchRequestBuilderFactory.builder(
+				).companyId(
+					TestPropsValues.getCompanyId()
+				).queryString(
+					keywords
+				).withSearchContext(
+					_searchContext -> {
+						_searchContext.setAttribute(
+							"search.experiences.blueprint.id",
+							_sxpBlueprint.getSXPBlueprintId());
+						_searchContext.setAttribute(
+							"search.experiences.ip.address", ipAddress);
+						_searchContext.setAttribute(
+							"search.experiences.scope.group.id",
+							_group.getGroupId());
+						_searchContext.setUserId(_serviceContext.getUserId());
+					}
+				).build()));
 
 		DocumentsAssert.assertValues(
 			searchResponse.getRequestString(),
-			searchResponse.getDocumentsStream(), fieldName, expected);
+			searchResponse.getDocumentsStream(), "localized_title_en_US",
+			expected);
+	}
+
+	private ConfigurationTemporarySwapper _getConfigurationTemporarySwapper(
+			String apiKey, String apiURL, String enabled)
+		throws Exception {
+
+		return new ConfigurationTemporarySwapper(
+			"com.liferay.search.experiences.internal.configuration." +
+				"IpstackConfiguration",
+			HashMapDictionaryBuilder.put(
+				"apiKey", (Object)apiKey
+			).put(
+				"apiURL", apiURL
+			).put(
+				"enabled", enabled
+			).build());
+	}
+
+	private Http _getHttp(String urlResponse) throws Exception {
+		return new HttpImpl() {
+
+			@Override
+			public String URLtoString(String url) throws IOException {
+				return urlResponse;
+			}
+
+		};
+	}
+
+	private void _test(
+			String[] titles, UnsafeRunnable<Exception> unsafeRunnable)
+		throws Exception {
+
+		for (String title : titles) {
+			JournalTestUtil.addArticle(
+				_group.getGroupId(), 0,
+				PortalUtil.getClassNameId(JournalArticle.class),
+				HashMapBuilder.put(
+					LocaleUtil.US, title
+				).build(),
+				null,
+				HashMapBuilder.put(
+					LocaleUtil.US, ""
+				).build(),
+				LocaleUtil.getSiteDefault(), false, true,
+				ServiceContextTestUtil.getServiceContext());
+		}
+
+		unsafeRunnable.run();
 	}
 
 	@DeleteAfterTestRun
-	private List<JournalArticle> _journalArticles = new ArrayList<>();
+	private Group _group;
+
+	@Inject
+	private Http _http;
 
 	@Inject
 	private Searcher _searcher;
 
 	@Inject
 	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+	@Inject
+	private SearchRequestContributor _searchRequestContributor;
+
+	private ServiceContext _serviceContext;
 
 	@DeleteAfterTestRun
 	private SXPBlueprint _sxpBlueprint;
