db26545868ffe7c977dfb0a0a883d983ba1fb98b
Kenji Heigel <kenji.heigel@liferay.com>
2022-02-15 19:30:23 -0800

    LRCI-2801 Use com.liferay.jenkins.results.parser:1.0.998 in testray gradle plugin

--- modules/sdk/gradle-plugins-testray/src/main/java/com/liferay/gradle/plugins/testray/TestrayExtension.java
+++ modules/sdk/gradle-plugins-testray/src/main/java/com/liferay/gradle/plugins/testray/TestrayExtension.java
@@ -47,7 +47,7 @@ public class TestrayExtension {
 		_testrayPropertiesFile = testrayPropertiesFile;
 	}
 
-	private Object _jenkinsResultsParserVersion = "1.0.990";
+	private Object _jenkinsResultsParserVersion = "1.0.998";
 	private final Project _project;
 	private Object _testrayPropertiesFile = "testray.properties";
 
