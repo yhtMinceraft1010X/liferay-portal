9742592d4b084a44a5d011db2a28ddade7334a39
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-02-16 12:01:37 +0100

    LPS-146974 Creates new interface to reuse the code collection pagination code

---
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/CollectionPaginationUtil.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.util.structure;
+
+import com.liferay.info.pagination.Pagination;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public interface CollectionPaginationUtil {
+
+	public Pagination getPagination(
+		int activePage, int count, boolean displayAllPages,
+		boolean displayAllItems, int numberOfItems, int numberOfItemsPerPage,
+		int numberOfPages, String paginationType, boolean showAllItems);
+
+	public int getTotalNumberOfItems(
+		int count, boolean displayAllPages, boolean displayAllItems,
+		int numberOfItems, int numberOfItemsPerPage, int numberOfPages,
+		String paginationType);
+
+	public boolean isPaginationEnabled(String paginationType);
+
+}
