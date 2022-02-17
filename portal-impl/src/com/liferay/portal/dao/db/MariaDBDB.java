e401ea5820e646fabfb9c620cc8b182676baef6f
Alberto Chaparro <alberto.chaparro@liferay.com>
2022-02-17 14:32:43 +0100

    LPS-134196 MariaDB fails when dropping a column which is part of a multi-column primary key or unique index. See https://jira.mariadb.org/browse/MDEV-13613 and https://mariadb.com/kb/en/alter-table/#drop-column. We can not recreate unique indexes because we do not know the new name but that's fine because they are recreated after every upgrade"

--- portal-impl/src/com/liferay/portal/dao/db/MariaDBDB.java
+++ portal-impl/src/com/liferay/portal/dao/db/MariaDBDB.java
@@ -15,6 +15,12 @@
 package com.liferay.portal.dao.db;
 
 import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+import java.sql.Connection;
+
+import java.util.List;
 
 /**
  * @author Preston Crary
@@ -23,6 +29,37 @@ public class MariaDBDB extends MySQLDB {
 
 	public MariaDBDB(int majorVersion, int minorVersion) {
 		super(DBType.MARIADB, majorVersion, minorVersion);
+	}
+
+	@Override
+	public void alterTableDropColumn(
+			Connection connection, String tableName, String columnName)
+		throws Exception {
+
+		String[] primaryKeyColumnNames = getPrimaryKeyColumnNames(
+			connection, tableName);
+
+		boolean primaryKey = ArrayUtil.contains(
+			primaryKeyColumnNames, columnName);
+
+		if (primaryKey && (primaryKeyColumnNames.length > 1)) {
+			removePrimaryKey(connection, tableName);
+
+			addPrimaryKey(
+				connection, tableName,
+				ArrayUtil.remove(primaryKeyColumnNames, columnName));
+		}
+
+		List<IndexMetadata> uniqueIndexes = getIndexes(
+			connection, tableName, columnName, true);
+
+		for (IndexMetadata uniqueIndex : uniqueIndexes) {
+			String[] columnNames = uniqueIndex.getColumnNames();
+
+			if (columnNames.length > 1) {
+				runSQL(uniqueIndex.getDropSQL());
+			}
+		}
 	}
 
 }
