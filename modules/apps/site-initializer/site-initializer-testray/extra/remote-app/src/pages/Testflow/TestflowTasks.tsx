2122e7384176aaf2a5d31ca9cf7db703e0bd0735
Wellington Barbosa <welitonpe@gmail.com>
2022-02-18 15:02:39 -0300

    LPS-147666 Created Tesflow Tasks Page.

--- modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/pages/Testflow/TestflowTasks.tsx
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/pages/Testflow/TestflowTasks.tsx
@@ -13,11 +13,12 @@
  */
 
 import {useEffect} from 'react';
-import {Link} from 'react-router-dom';
 
 import {Avatar, AvatarGroup} from '../../components/Avatar';
+import Code from '../../components/Code';
 import Container from '../../components/Layout/Container';
 import ProgressBar from '../../components/ProgressBar';
+import StatusBadge from '../../components/StatusBadge';
 import Table from '../../components/Table';
 import QATable from '../../components/Table/QATable';
 import useHeader from '../../hooks/useHeader';
@@ -52,15 +53,9 @@ const TestFlowTasks: React.FC = () => {
 								{
 									title: 'Status',
 									value: (
-										<Link
-											to={`/testflow/${subtask[1].status
-												.toLowerCase()
-												.replace(' ', '_')}`}
-										>
-											<span className="label label-inverse-secondary">
-												{subtask[1].status.toUpperCase()}
-											</span>
-										</Link>
+										<StatusBadge type="passed">
+											passed
+										</StatusBadge>
 									),
 								},
 								{
@@ -117,26 +112,34 @@ const TestFlowTasks: React.FC = () => {
 			<Container className="mt-3" title="Subtasks">
 				<Table
 					columns={[
-						{key: 'name', value: 'Name'},
 						{
+							clickable: true,
+							key: 'name',
+							value: 'Name',
+						},
+						{
+							clickable: true,
 							key: 'status',
-							render: (value: string) => (
-								<Link
-									to={`/testflow/${value
-										.toLowerCase()
-										.replace(' ', '_')}`}
-								>
-									<span className="label label-inverse-secondary">
-										{value.toUpperCase()}
-									</span>
-								</Link>
+							render: () => (
+								<StatusBadge type="blocked">
+									blocked
+								</StatusBadge>
 							),
+
 							value: 'Status',
 						},
-						{key: 'score', value: 'Score'},
-						{key: 'tests', value: 'Tests'},
-						{key: 'error', value: 'Errors'},
+						{clickable: true, key: 'score', value: 'Score'},
+						{clickable: true, key: 'tests', value: 'Tests'},
 						{
+							className: 'table-cell-expand-small',
+							clickable: true,
+							key: 'error',
+							render: (value) => <Code>{value}</Code>,
+							value: 'Errors',
+						},
+						{
+							className: 'table-cell-minw-150',
+							clickable: true,
 							key: 'assignee',
 							render: (assignee: any) => (
 								<Avatar
@@ -150,6 +153,7 @@ const TestFlowTasks: React.FC = () => {
 						},
 					]}
 					items={subtask}
+					navigateTo={() => `/testflow/subtasks`}
 				/>
 			</Container>
 		</>
