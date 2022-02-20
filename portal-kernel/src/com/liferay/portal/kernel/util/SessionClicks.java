e1c7d878ebacdf79b6271b54139d334de8e3abc2
Brian Chan <brian.chan@liferay.com>
2022-02-20 19:30:50 -0300

    LPS-144246 Wordsmith

--- portal-kernel/src/com/liferay/portal/kernel/util/SessionClicks.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/SessionClicks.java
@@ -158,8 +158,8 @@ public class SessionClicks {
 			_log.warn(
 				StringBundler.concat(
 					"Session clicks has attempted to exceed the maximum size ",
-					"allowed for keys or values with {key=", key, ", value=",
-					value, "}"));
+					"allowed for keys or values with key \"", key,
+					"\" and value \"", value, "\""));
 		}
 
 		return false;
@@ -174,8 +174,8 @@ public class SessionClicks {
 			_log.warn(
 				StringBundler.concat(
 					"Session clicks has attempted to exceed the maximum ",
-					"number of allowed values with {key=", key, ", value=",
-					value, "}"));
+					"number of allowed values with key \"", key,
+					"\" and value \"", value, "\""));
 		}
 
 		return false;
