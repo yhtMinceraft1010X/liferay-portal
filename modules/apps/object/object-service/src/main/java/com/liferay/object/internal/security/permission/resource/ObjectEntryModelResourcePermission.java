fb2f406285cabe17a1ac6a7a41ca01d95a8e0d14
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-02-21 09:55:43 -0300

    LPS-147155 Avoid owner permission checks if the user is a guest

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/security/permission/resource/ObjectEntryModelResourcePermission.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/security/permission/resource/ObjectEntryModelResourcePermission.java
@@ -17,6 +17,7 @@ package com.liferay.object.internal.security.permission.resource;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
@@ -80,6 +81,14 @@ public class ObjectEntryModelResourcePermission
 			PermissionChecker permissionChecker, ObjectEntry objectEntry,
 			String actionId)
 		throws PortalException {
+
+		User user = permissionChecker.getUser();
+
+		if (user.isDefaultUser()) {
+			return permissionChecker.hasPermission(
+				objectEntry.getGroupId(), _modelName,
+				objectEntry.getObjectEntryId(), actionId);
+		}
 
 		if (permissionChecker.hasOwnerPermission(
 				permissionChecker.getCompanyId(), _modelName,
