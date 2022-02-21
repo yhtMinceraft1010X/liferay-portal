6052ffd57f40df3715a6a68f4efe923b360207ac
Carlos Montenegro <carlos.montenegro@liferay.com>
2022-02-21 18:02:56 -0300

    LPS-146640 Add timer information panel content

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/timers/TimerInfo.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/timers/TimerInfo.js
@@ -9,15 +9,74 @@
  * distribution rights of the Software.
  */
 
+import {ClayButtonWithIcon} from '@clayui/button';
+import ClayForm, {ClayInput} from '@clayui/form';
+import ClayLayout from '@clayui/layout';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useState} from 'react';
 
 import SidebarPanel from '../../SidebarPanel';
 
-const TimerInfo = () => {
+const TimerInfo = ({
+	deleteTimer,
+	index,
+	sectionsLength,
+	selectedItem,
+	updateSelectedItem,
+}) => {
+	const [timerDescription, setTimerDescription] = useState(
+		selectedItem?.data.taskTimers?.description[index] || ''
+	);
+	const [timerName, setTimerName] = useState(
+		selectedItem?.data.taskTimers?.name[index] || ''
+	);
+
 	return (
 		<SidebarPanel panelTitle={Liferay.Language.get('information')}>
-			Information Placeholder
+			{sectionsLength > 1 && (
+				<ClayLayout.Row justify="end">
+					<ClayButtonWithIcon
+						className="delete-button text-secondary trash-button"
+						displayType="unstyled"
+						onClick={deleteTimer}
+						symbol="trash"
+					/>
+				</ClayLayout.Row>
+			)}
+
+			<ClayForm.Group>
+				<label htmlFor="timerName">
+					{Liferay.Language.get('name')}
+				</label>
+
+				<ClayInput
+					id="timerName"
+					onBlur={({target}) =>
+						updateSelectedItem({name: target.value})
+					}
+					onChange={({target}) => setTimerName(target.value)}
+					placeholder={Liferay.Language.get('my-task-timer')}
+					type="text"
+					value={timerName}
+				/>
+			</ClayForm.Group>
+
+			<ClayForm.Group>
+				<label htmlFor="timerDescription">
+					{Liferay.Language.get('description')}
+				</label>
+
+				<ClayInput
+					component="textarea"
+					id="timerDescription"
+					onBlur={({target}) =>
+						updateSelectedItem({description: target.value})
+					}
+					onChange={({target}) => setTimerDescription(target.value)}
+					type="text"
+					value={timerDescription}
+				/>
+			</ClayForm.Group>
 		</SidebarPanel>
 	);
 };
