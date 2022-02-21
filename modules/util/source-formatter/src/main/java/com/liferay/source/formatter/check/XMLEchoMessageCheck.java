e9ec63d052422115d24bc948749d705e4655fd87
Seiphon Wang <seiphon.wang@liferay.com>
2022-02-21 20:41:25 +0800

    LPS-147690 Add echo message check for Source Format

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/XMLEchoMessageCheck.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.check.util.SourceUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.Node;
+
+/**
+ * @author Seiphon Wang
+ */
+public class XMLEchoMessageCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (fileName.endsWith(".xml")) {
+			return _checkEchoMessageExist(content);
+		}
+
+		return content;
+	}
+
+	private String _checkEchoMessageExist(String content) {
+		Matcher matcher = _echoMessagePattern.matcher(content);
+
+		List<String> matchedTags = new ArrayList<>();
+
+		while (matcher.find()) {
+			matchedTags.add(matcher.group());
+		}
+
+		try {
+			Document document = SourceUtil.readXML(content);
+
+			List<Node> echoNodes = document.selectNodes("//*[name() = 'echo']");
+
+			for (Node node : echoNodes) {
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element)node;
+
+					Attribute attribute = element.attribute("message");
+
+					if (attribute != null) {
+						for (String matchedTag : matchedTags) {
+							Document documentElement = DocumentHelper.parseText(
+								matchedTag);
+
+							Element rootElement =
+								documentElement.getRootElement();
+
+							if (Objects.equals(
+									element.asXML(), rootElement.asXML())) {
+
+								element.setText(attribute.getText());
+
+								element.remove(attribute);
+
+								content = StringUtil.replace(
+									content, matchedTag, element.asXML());
+							}
+						}
+					}
+				}
+			}
+		}
+		catch (DocumentException documentException) {
+		}
+
+		return content;
+	}
+
+	private static final Pattern _echoMessagePattern = Pattern.compile(
+		"<echo .*message=\".*\".* />");
+
+}
