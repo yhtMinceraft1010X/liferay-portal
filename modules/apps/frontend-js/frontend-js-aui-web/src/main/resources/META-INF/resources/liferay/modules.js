399012a005189d1ce2912159f76d5de40cecbfd8
Roberto Díaz <roberto.diaz@liferay.com>
2022-02-23 15:51:42 +0100

    LPS-147968 Auto SF

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/modules.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/modules.js
@@ -46,7 +46,11 @@
 	};
 
 	window.YUI_config = {
-		base: Liferay.ThemeDisplay.getCDNBaseURL() + Liferay.ThemeDisplay.getPathContext() + PATH_JAVASCRIPT + '/aui/',
+		base:
+			Liferay.ThemeDisplay.getCDNBaseURL() +
+			Liferay.ThemeDisplay.getPathContext() +
+			PATH_JAVASCRIPT +
+			'/aui/',
 		combine: COMBINE,
 		comboBase: LiferayAUI.getComboPath(),
 		filter: process.env.NODE_ENV === 'development' ? 'raw' : 'min',
