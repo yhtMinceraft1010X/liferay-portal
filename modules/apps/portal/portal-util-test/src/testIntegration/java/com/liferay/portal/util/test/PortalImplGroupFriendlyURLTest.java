1b7edf578d1992981b0ed9e0e2b5670146cfac9d
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-02-23 07:21:00 +0100

    LPS-136518 Adds test for no default site configured for the default company host.

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplGroupFriendlyURLTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplGroupFriendlyURLTest.java
@@ -16,6 +16,7 @@ package com.liferay.portal.util.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
@@ -30,6 +31,7 @@ import com.liferay.portal.kernel.service.VirtualHostLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.PropsValuesTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
@@ -163,6 +165,63 @@ public class PortalImplGroupFriendlyURLTest {
 
 		_testGroupFriendlyURL(
 			hostName, StringPool.BLANK, group, nondefaultSiteLayout);
+	}
+
+	@Test
+	public void testGetGroupFriendlyURLFromPublicLayoutNoVirtualHostDefaultSite()
+		throws Exception {
+
+		try (SafeCloseable safeCloseable =
+				PropsValuesTestUtil.swapWithSafeCloseable(
+					"VIRTUAL_HOSTS_DEFAULT_SITE_NAME", StringPool.BLANK)) {
+
+			Layout defaultSiteLayout = _layoutLocalService.fetchDefaultLayout(
+				_group.getGroupId(), false);
+
+			_updateLayoutSetVirtualHostname(
+				defaultSiteLayout, StringPool.BLANK);
+
+			_testGroupFriendlyURL(
+				_company.getVirtualHostname(),
+				PropsValues.LAYOUT_FRIENDLY_URL_PUBLIC_SERVLET_MAPPING +
+					_group.getFriendlyURL(),
+				_group, defaultSiteLayout);
+
+			_updateLayoutSetVirtualHostname(
+				defaultSiteLayout, _PUBLIC_LAYOUT_HOSTNAME);
+
+			_testGroupFriendlyURL(
+				_company.getVirtualHostname(),
+				PropsValues.LAYOUT_FRIENDLY_URL_PUBLIC_SERVLET_MAPPING +
+					_group.getFriendlyURL(),
+				_group, defaultSiteLayout);
+
+			_testGroupFriendlyURL(
+				_PUBLIC_LAYOUT_HOSTNAME, StringPool.BLANK, _group,
+				defaultSiteLayout);
+
+			User user = UserTestUtil.getAdminUser(_company.getCompanyId());
+
+			Group group = GroupTestUtil.addGroup(
+				_company.getCompanyId(), user.getUserId(), 0);
+
+			Layout nondefaultSiteLayout = LayoutTestUtil.addLayout(group);
+
+			_testGroupFriendlyURL(
+				_company.getVirtualHostname(),
+				PropsValues.LAYOUT_FRIENDLY_URL_PUBLIC_SERVLET_MAPPING +
+					group.getFriendlyURL(),
+				group, nondefaultSiteLayout);
+
+			String hostName =
+				RandomTestUtil.randomString(6) + StringPool.PERIOD +
+					RandomTestUtil.randomString(3);
+
+			_updateLayoutSetVirtualHostname(nondefaultSiteLayout, hostName);
+
+			_testGroupFriendlyURL(
+				hostName, StringPool.BLANK, group, nondefaultSiteLayout);
+		}
 	}
 
 	private void _testGroupFriendlyURL(
