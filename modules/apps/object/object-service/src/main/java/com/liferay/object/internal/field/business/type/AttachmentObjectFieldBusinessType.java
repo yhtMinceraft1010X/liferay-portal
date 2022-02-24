72bbb2b33e33abf9860c9cd75400658598bd7c75
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-02-24 19:45:14 -0300

    LPS-147114 Merge AttachmentHelper into AttachmentObjectFieldBusinessType

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/AttachmentObjectFieldBusinessType.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/AttachmentObjectFieldBusinessType.java
@@ -14,16 +14,23 @@
 
 package com.liferay.object.internal.field.business.type;
 
+import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.dynamic.data.mapping.form.field.type.constants.ObjectDDMFormFieldTypeConstants;
 import com.liferay.object.field.business.type.ObjectFieldBusinessType;
 import com.liferay.object.field.render.ObjectFieldRenderingContext;
-import com.liferay.object.internal.field.business.type.attachment.AttachmentHelper;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.model.ObjectFieldSetting;
 import com.liferay.object.service.ObjectFieldSettingLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Repository;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepository;
 import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
@@ -32,6 +39,8 @@ import com.liferay.portal.kernel.util.Validator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -92,8 +101,7 @@ public class AttachmentObjectFieldBusinessType
 					return null;
 				}
 
-				Folder folder = _attachmentHelper.getFolder(
-					objectFieldRenderingContext);
+				Folder folder = _getFolder(objectFieldRenderingContext);
 
 				if (folder == null) {
 					return null;
@@ -121,10 +129,91 @@ public class AttachmentObjectFieldBusinessType
 		return properties;
 	}
 
-	@Reference
-	private AttachmentHelper _attachmentHelper;
+	private Folder _addFolder(
+		long userId, long repositoryId, HttpServletRequest httpServletRequest) {
+
+		try {
+			return _portletFileRepository.addPortletFolder(
+				userId, repositoryId,
+				DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+				String.valueOf(userId),
+				ServiceContextFactory.getInstance(httpServletRequest));
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
+			}
+
+			return null;
+		}
+	}
+
+	private Folder _getFolder(
+		ObjectFieldRenderingContext objectFieldRenderingContext) {
+
+		Repository repository = _getRepository(
+			objectFieldRenderingContext.getGroupId(),
+			objectFieldRenderingContext.getPortletId(),
+			objectFieldRenderingContext.getHttpServletRequest());
+
+		if (repository == null) {
+			return null;
+		}
+
+		try {
+			return _portletFileRepository.getPortletFolder(
+				repository.getRepositoryId(),
+				DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+				String.valueOf(objectFieldRenderingContext.getUserId()));
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
+			}
+
+			return _addFolder(
+				objectFieldRenderingContext.getUserId(),
+				repository.getRepositoryId(),
+				objectFieldRenderingContext.getHttpServletRequest());
+		}
+	}
+
+	private Repository _getRepository(
+		long groupId, String portletId, HttpServletRequest httpServletRequest) {
+
+		Repository repository = _portletFileRepository.fetchPortletRepository(
+			groupId, portletId);
+
+		if (repository != null) {
+			return repository;
+		}
+
+		try {
+			ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				httpServletRequest);
+
+			serviceContext.setAddGroupPermissions(true);
+			serviceContext.setAddGuestPermissions(true);
+
+			return _portletFileRepository.addPortletRepository(
+				groupId, portletId, serviceContext);
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
+			}
+
+			return null;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AttachmentObjectFieldBusinessType.class);
 
 	@Reference
 	private ObjectFieldSettingLocalService _objectFieldSettingLocalService;
+
+	@Reference
+	private PortletFileRepository _portletFileRepository;
 
 }
