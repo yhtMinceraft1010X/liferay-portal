c68bd84a7a53a51f43e2a5f600f1ba08dc0e365e
Jiefeng Wu <jeffwu0724@gmail.com>
2022-02-25 13:41:29 -0800

    LPS-148015 - auto generate for "portal-impl/src/com/liferay/portlet/social/service/impl/SocialActivityLocalServiceImpl.java"

--- portal-kernel/src/com/liferay/social/kernel/service/SocialActivityLocalServiceWrapper.java
+++ portal-kernel/src/com/liferay/social/kernel/service/SocialActivityLocalServiceWrapper.java
@@ -568,30 +568,6 @@ public class SocialActivityLocalServiceWrapper
 	}
 
 	/**
-	 * @param className the target asset's class name
-	 * @param start the lower bound of the range of results
-	 * @param end the upper bound of the range of results (not inclusive)
-	 * @return the range of matching activities
-	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
-	 #getActivities(long, String, int, int)}  Returns a range of
-	 all the activities done on assets identified by the class
-	 name.  <p> Useful when paginating results. Returns a maximum
-	 of <code>end - start</code> instances. <code>start</code> and
-	 <code>end</code> are not primary keys, they are indexes in
-	 the result set. Thus, <code>0</code> refers to the first
-	 result in the set. Setting both <code>start</code> and
-	 <code>end</code> to {@link QueryUtil#ALL_POS} will return the
-	 full result set.</p>
-	 */
-	@Deprecated
-	@Override
-	public java.util.List<SocialActivity> getActivities(
-		String className, int start, int end) {
-
-		return _socialActivityLocalService.getActivities(className, start, end);
-	}
-
-	/**
 	 * @param classNameId the target asset's class name ID
 	 * @return the number of matching activities
 	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
@@ -661,18 +637,6 @@ public class SocialActivityLocalServiceWrapper
 
 		return _socialActivityLocalService.getActivitiesCount(
 			mirrorActivityId, className, classPK);
-	}
-
-	/**
-	 * @param className the target asset's class name
-	 * @return the number of matching activities
-	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
-	 #getActivitiesCount(long, String)}
-	 */
-	@Deprecated
-	@Override
-	public int getActivitiesCount(String className) {
-		return _socialActivityLocalService.getActivitiesCount(className);
 	}
 
 	/**
