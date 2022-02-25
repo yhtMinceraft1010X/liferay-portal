7544c6663240940e4d0b2e3d75c5c1c368a37853
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2022-02-24 22:47:10 -0300

    LPS-146758 Remove userId checking from the ListTypeDefinition permission checker

--- modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/internal/security/permission/resource/ListTypeDefinitionModelResourcePermission.java
+++ modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/internal/security/permission/resource/ListTypeDefinitionModelResourcePermission.java
@@ -74,7 +74,6 @@ public class ListTypeDefinitionModelResourcePermission
 				ListTypeDefinition.class.getName(),
 				listTypeDefinition.getListTypeDefinitionId(),
 				listTypeDefinition.getUserId(), actionId) ||
-			(permissionChecker.getUserId() == listTypeDefinition.getUserId()) ||
 			permissionChecker.hasPermission(
 				null, ListTypeDefinition.class.getName(),
 				listTypeDefinition.getPrimaryKey(), actionId)) {
