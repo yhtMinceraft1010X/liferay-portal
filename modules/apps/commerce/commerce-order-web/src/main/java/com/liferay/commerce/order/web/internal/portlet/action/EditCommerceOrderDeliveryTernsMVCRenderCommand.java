b4f8c4ab5b8a38a39e691816d15116a6597ce673
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2022-02-28 10:07:02 +0100

    COMMERCE-8141 Added render command class for payment and delivery terms

---
+++ modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/action/EditCommerceOrderDeliveryTernsMVCRenderCommand.java
@@ -0,0 +1,153 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.web.internal.portlet.action;
+
+import com.liferay.commerce.configuration.CommerceOrderItemDecimalQuantityConfiguration;
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.exception.NoSuchOrderException;
+import com.liferay.commerce.notification.service.CommerceNotificationQueueEntryLocalService;
+import com.liferay.commerce.order.engine.CommerceOrderEngine;
+import com.liferay.commerce.order.status.CommerceOrderStatusRegistry;
+import com.liferay.commerce.order.web.internal.display.context.CommerceOrderEditDisplayContext;
+import com.liferay.commerce.payment.service.CommercePaymentMethodGroupRelLocalService;
+import com.liferay.commerce.product.service.CPMeasurementUnitService;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.commerce.service.CommerceOrderItemService;
+import com.liferay.commerce.service.CommerceOrderNoteService;
+import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.commerce.service.CommerceOrderTypeService;
+import com.liferay.commerce.service.CommerceShipmentService;
+import com.liferay.commerce.term.service.CommerceTermEntryService;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Map;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Crescenzo Rega
+ */
+@Component(
+	configurationPid = "com.liferay.commerce.configuration.CommerceOrderItemDecimalQuantityConfiguration",
+	enabled = false,
+	property = {
+		"javax.portlet.name=" + CommercePortletKeys.COMMERCE_ORDER,
+		"mvc.command.name=/commerce_order/edit_commerce_order_delivery_terms"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditCommerceOrderDeliveryTernsMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			CommerceOrderEditDisplayContext commerceOrderEditDisplayContext =
+				new CommerceOrderEditDisplayContext(
+					_commerceChannelLocalService,
+					_commerceNotificationQueueEntryLocalService,
+					_commerceOrderEngine,
+					_commerceOrderItemDecimalQuantityConfiguration,
+					_commerceOrderItemService, _commerceOrderNoteService,
+					_commerceOrderService, _commerceOrderStatusRegistry,
+					_commerceOrderTypeService,
+					_commercePaymentMethodGroupRelLocalService,
+					_commerceShipmentService, _cpMeasurementUnitService,
+					_commerceTermEntryService, renderRequest);
+
+			renderRequest.setAttribute(
+				WebKeys.PORTLET_DISPLAY_CONTEXT,
+				commerceOrderEditDisplayContext);
+		}
+		catch (Exception exception) {
+			if (exception instanceof NoSuchOrderException ||
+				exception instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, exception.getClass());
+
+				return "/error.jsp";
+			}
+
+			throw new PortletException(exception);
+		}
+
+		return "/commerce_order/delivery-terms.jsp";
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_commerceOrderItemDecimalQuantityConfiguration =
+			ConfigurableUtil.createConfigurable(
+				CommerceOrderItemDecimalQuantityConfiguration.class,
+				properties);
+	}
+
+	@Reference
+	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@Reference
+	private CommerceNotificationQueueEntryLocalService
+		_commerceNotificationQueueEntryLocalService;
+
+	@Reference
+	private CommerceOrderEngine _commerceOrderEngine;
+
+	private volatile CommerceOrderItemDecimalQuantityConfiguration
+		_commerceOrderItemDecimalQuantityConfiguration;
+
+	@Reference
+	private CommerceOrderItemService _commerceOrderItemService;
+
+	@Reference
+	private CommerceOrderNoteService _commerceOrderNoteService;
+
+	@Reference
+	private CommerceOrderService _commerceOrderService;
+
+	@Reference
+	private CommerceOrderStatusRegistry _commerceOrderStatusRegistry;
+
+	@Reference
+	private CommerceOrderTypeService _commerceOrderTypeService;
+
+	@Reference
+	private CommercePaymentMethodGroupRelLocalService
+		_commercePaymentMethodGroupRelLocalService;
+
+	@Reference
+	private CommerceShipmentService _commerceShipmentService;
+
+	@Reference
+	private CommerceTermEntryService _commerceTermEntryService;
+
+	@Reference
+	private CPMeasurementUnitService _cpMeasurementUnitService;
+
+}
