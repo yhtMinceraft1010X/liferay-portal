ea8f5d4191b25279251aa3cf478faf9035d06653
Carlos Correa <carlos.correa@liferay.com>
2022-03-02 13:42:46 +0100

    LPS-141833 Revert Add priority to BlogPostingDTOConverter
    This reverts commit 4ae8903e8badc78698ea5e76be675f9d61a350af.

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/BlogPostingDTOConverter.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/BlogPostingDTOConverter.java
@@ -14,7 +14,6 @@
 
 package com.liferay.headless.delivery.internal.dto.v1_0.converter;
 
-import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.asset.kernel.service.AssetEntryLocalService;
@@ -134,19 +133,6 @@ public class BlogPostingDTOConverter
 							StringUtil.shorten(
 								blogsEntry.getContent(),
 								PropsValues.BLOGS_PAGE_ABSTRACT_LENGTH));
-					});
-				setPriority(
-					() -> {
-						AssetEntry assetEntry =
-							_assetEntryLocalService.fetchEntry(
-								blogsEntry.getModelClassName(),
-								blogsEntry.getEntryId());
-
-						if (assetEntry != null) {
-							return assetEntry.getPriority();
-						}
-
-						return null;
 					});
 				setRenderedContents(
 					() -> DisplayPageRendererUtil.getRenderedContent(
