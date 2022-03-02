0d1e3d991c60b5c86d68bac2d6a9446b3ad3c5ef
Erick Monteiro <erickhenriquehmc@gmail.com>
2022-03-02 11:50:32 -0300

    LPS-148152 (users-admin-web): Uses javax.portlet.version=3.0 on UsersAdminPortlet

--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/UsersAdminPortlet.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/UsersAdminPortlet.java
@@ -43,7 +43,8 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + UsersAdminPortletKeys.USERS_ADMIN,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
