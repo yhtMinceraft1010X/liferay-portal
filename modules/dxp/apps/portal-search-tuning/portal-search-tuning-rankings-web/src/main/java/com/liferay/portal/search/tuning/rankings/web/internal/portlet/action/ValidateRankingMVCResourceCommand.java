0de64eb6649bc87190fe93e0ea01d194e31e065e
Alan Huang <alan.huang@liferay.com>
2022-03-03 22:08:49 +0800

    LPS-147450 SF, do not call printStackTrace()

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/portlet/action/ValidateRankingMVCResourceCommand.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/portlet/action/ValidateRankingMVCResourceCommand.java
@@ -20,6 +20,8 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -76,7 +78,7 @@ public class ValidateRankingMVCResourceCommand implements MVCResourceCommand {
 			return false;
 		}
 		catch (RuntimeException runtimeException) {
-			runtimeException.printStackTrace();
+			_log.error(runtimeException);
 
 			throw runtimeException;
 		}
@@ -206,6 +208,9 @@ public class ValidateRankingMVCResourceCommand implements MVCResourceCommand {
 	}
 
 	private static final String _UPDATE_SPECIAL = StringPool.GREATER_THAN;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ValidateRankingMVCResourceCommand.class);
 
 	private class ValidateRankingMVCResourceRequest {
 
