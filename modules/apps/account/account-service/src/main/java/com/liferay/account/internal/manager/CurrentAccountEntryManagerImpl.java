1fdbf155bcfd92ca8e8ded7d20bdde94612eae00
drewbrokke <drew.brokke@liferay.com>
2022-03-03 10:15:24 -0600

    COMMERCE-8602 account-service: retrieves the user's associated accounts and uses the list to validate the selected account

--- modules/apps/account/account-service/src/main/java/com/liferay/account/internal/manager/CurrentAccountEntryManagerImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/manager/CurrentAccountEntryManagerImpl.java
@@ -21,6 +21,7 @@ import com.liferay.account.manager.CurrentAccountEntryManager;
 import com.liferay.account.model.AccountEntry;
 import com.liferay.account.service.AccountEntryLocalService;
 import com.liferay.account.settings.AccountEntryGroupSettings;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -38,7 +39,6 @@ import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.List;
-import java.util.Objects;
 
 import javax.servlet.http.HttpSession;
 
@@ -71,25 +71,29 @@ public class CurrentAccountEntryManagerImpl
 			return guestAccountEntry;
 		}
 
+		List<AccountEntry> userAccountEntries =
+			_accountEntryLocalService.getUserAccountEntries(
+				userId, AccountConstants.PARENT_ACCOUNT_ENTRY_ID_DEFAULT, null,
+				_getAllowedTypes(groupId), WorkflowConstants.STATUS_APPROVED,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
 		AccountEntry accountEntry = _getAccountEntryFromHttpSession(groupId);
 
-		String[] allowedTypes = _getAllowedTypes(groupId);
-
-		if (_isValid(accountEntry, allowedTypes)) {
+		if (_isValid(accountEntry, userAccountEntries)) {
 			return accountEntry;
 		}
 
 		accountEntry = _getAccountEntryFromPortalPreferences(groupId, userId);
 
-		if (_isValid(accountEntry, allowedTypes)) {
+		if (_isValid(accountEntry, userAccountEntries)) {
 			_saveInHttpSession(accountEntry.getAccountEntryId(), groupId);
 
 			return accountEntry;
 		}
 
-		accountEntry = _getDefaultAccountEntry(allowedTypes, userId);
+		if (!userAccountEntries.isEmpty()) {
+			accountEntry = userAccountEntries.get(0);
 
-		if (_isValid(accountEntry, allowedTypes)) {
 			setCurrentAccountEntry(
 				accountEntry.getAccountEntryId(), groupId, userId);
 
@@ -164,22 +168,6 @@ public class CurrentAccountEntryManagerImpl
 		return _accountEntryGroupSettings.getAllowedTypes(groupId);
 	}
 
-	private AccountEntry _getDefaultAccountEntry(
-			String[] allowedTypes, long userId)
-		throws PortalException {
-
-		List<AccountEntry> accountEntries =
-			_accountEntryLocalService.getUserAccountEntries(
-				userId, AccountConstants.PARENT_ACCOUNT_ENTRY_ID_DEFAULT, null,
-				allowedTypes, 0, 1);
-
-		if (accountEntries.size() == 1) {
-			return accountEntries.get(0);
-		}
-
-		return null;
-	}
-
 	private String _getKey(long groupId) {
 		return AccountWebKeys.CURRENT_ACCOUNT_ENTRY_ID + groupId;
 	}
@@ -188,13 +176,13 @@ public class CurrentAccountEntryManagerImpl
 		return _portletPreferencesFactory.getPortalPreferences(userId, true);
 	}
 
-	private boolean _isValid(AccountEntry accountEntry, String[] allowedTypes) {
+	private boolean _isValid(
+		AccountEntry accountEntry, List<AccountEntry> userAccountEntries) {
+
 		if ((accountEntry != null) &&
-			Objects.equals(
-				WorkflowConstants.STATUS_APPROVED, accountEntry.getStatus()) &&
 			((accountEntry.getAccountEntryId() ==
 				AccountConstants.ACCOUNT_ENTRY_ID_GUEST) ||
-			 ArrayUtil.contains(allowedTypes, accountEntry.getType()))) {
+			 userAccountEntries.contains(accountEntry))) {
 
 			return true;
 		}
