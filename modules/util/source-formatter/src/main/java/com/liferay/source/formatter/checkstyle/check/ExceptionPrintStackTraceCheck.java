6bfd41c407bcdb28f0a792ac142fe17b7f98da9b
Alan Huang <alan.huang@liferay.com>
2022-03-03 09:53:40 +0800

    LPS-147450 Add ExceptionPrintStackTraceCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/ExceptionPrintStackTraceCheck.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.check;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+
+/**
+ * @author Alan Huang
+ */
+public class ExceptionPrintStackTraceCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.LITERAL_CATCH};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		String absolutePath = getAbsolutePath();
+
+		if (absolutePath.contains("/test/") ||
+			absolutePath.contains("/testIntegration/")) {
+
+			return;
+		}
+
+		if (detailAST.getType() == TokenTypes.LITERAL_CATCH) {
+			_checkPrintStackTraceCall(detailAST);
+		}
+	}
+
+	private void _checkPrintStackTraceCall(DetailAST detailAST) {
+		DetailAST parameterDefinitionDetailAST = detailAST.findFirstToken(
+			TokenTypes.PARAMETER_DEF);
+
+		String exceptionVariableName = getName(parameterDefinitionDetailAST);
+
+		String variableTypeName = getVariableTypeName(
+			detailAST, exceptionVariableName, false);
+
+		if ((variableTypeName == null) ||
+			!variableTypeName.endsWith("Exception")) {
+
+			return;
+		}
+
+		List<DetailAST> methodCallDetailASTList = getMethodCalls(
+			detailAST, exceptionVariableName, "printStackTrace");
+
+		if (methodCallDetailASTList.isEmpty()) {
+			return;
+		}
+
+		for (DetailAST methodCallDetailAST : methodCallDetailASTList) {
+			log(methodCallDetailAST, _MSG_AVOID_METHOD_CALL);
+		}
+	}
+
+	private static final String _MSG_AVOID_METHOD_CALL = "method.call.avoid";
+
+}
