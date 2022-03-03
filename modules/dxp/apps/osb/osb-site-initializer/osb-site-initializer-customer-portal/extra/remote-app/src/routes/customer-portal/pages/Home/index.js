91274967f24608f7ffe32f86c2bcd21da236cc6a
Clarice Brasil <clarice.brasil@liferay.com>
2022-03-03 12:29:47 -0300

    LPS-148471 SF

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Home/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Home/index.js
@@ -79,7 +79,8 @@ const Home = ({userAccount}) => {
 					accounts = dataAccounts?.accounts?.items;
 					accountKeysFilter = '';
 				}
-			} else if (userAccount?.accountBriefs?.length) {
+			}
+			else if (userAccount?.accountBriefs?.length) {
 				accounts = userAccount?.accountBriefs;
 				accountKeysFilter = getKoroneikiFilter(accounts);
 			}
