125199c2c3f17877761af6f6b51b586c4f6a7726
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-03-04 11:05:17 -0300

    LPS-148113 The hasObjectField function can also receive an array of strings as a parameter

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/HasObjectFieldFunction.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/HasObjectFieldFunction.java
@@ -16,21 +16,38 @@ package com.liferay.dynamic.data.mapping.form.evaluator.internal.function;
 
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 /**
  * @author Carolina Barbosa
  */
 public class HasObjectFieldFunction
-	implements DDMExpressionFunction.Function1<String, Boolean> {
+	implements DDMExpressionFunction.Function1<Object, Boolean> {
 
 	public static final String NAME = "hasObjectField";
 
 	@Override
-	public Boolean apply(String field) {
-		if (Validator.isNull(field) ||
+	public Boolean apply(Object object) {
+		if (object instanceof String) {
+			return _apply((String)object);
+		}
+		else if (object instanceof String[]) {
+			return _apply((String[])object);
+		}
+
+		return false;
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	private boolean _apply(String objectFieldName) {
+		if (Validator.isNull(objectFieldName) ||
 			Validator.isNull(
-				field.replaceAll("\\[|\\]|\"", StringPool.BLANK))) {
+				objectFieldName.replaceAll("\\[|\\]|\"", StringPool.BLANK))) {
 
 			return false;
 		}
@@ -38,9 +55,12 @@ public class HasObjectFieldFunction
 		return true;
 	}
 
-	@Override
-	public String getName() {
-		return NAME;
+	private boolean _apply(String[] objectFieldNames) {
+		if (ArrayUtil.isEmpty(objectFieldNames)) {
+			return false;
+		}
+
+		return true;
 	}
 
 }
