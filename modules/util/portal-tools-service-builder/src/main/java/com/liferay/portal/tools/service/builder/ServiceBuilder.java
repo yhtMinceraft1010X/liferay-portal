10865aa8e4057f6c018ef608bcc1b03f1c1327bd
Brian Chan <brian.chan@liferay.com>
2022-03-04 10:54:26 -0300

    LPS-143575 Sort

--- modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -6543,6 +6543,7 @@ public class ServiceBuilder {
 				"name", externalReferenceCodeUpperCase.charAt(0) + "_ERC");
 
 			finderElement.addAttribute("return-type", entityName);
+			finderElement.addAttribute("unique", "true");
 
 			Element finderColumnElement = finderElement.addElement(
 				"finder-column");
@@ -6553,8 +6554,6 @@ public class ServiceBuilder {
 			finderColumnElement = finderElement.addElement("finder-column");
 
 			finderColumnElement.addAttribute("name", "externalReferenceCode");
-
-			finderElement.addAttribute("unique", "true");
 
 			finderElements.add(finderElement);
 		}
