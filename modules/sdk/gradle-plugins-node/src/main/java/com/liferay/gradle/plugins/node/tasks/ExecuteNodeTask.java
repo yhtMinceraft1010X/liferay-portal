b02633c59d8879428168bd060952715806273320
Tay Yi Hsuen <69678785+yhtMinceraft1010X@users.noreply.github.com>
2022-03-06 15:22:48 +0800

    Add missing import

--- modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/ExecuteNodeTask.java
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/ExecuteNodeTask.java
@@ -29,6 +29,7 @@ import org.gradle.api.logging.Logger;
 import org.gradle.api.tasks.CacheableTask;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 
