93732d83581f1db1da80138d2157b31bf8287da1
Igor Beslic <igor.beslic@liferay.com>
2022-03-07 13:05:37 +0100

    COMMERCE-7570 batch-engine-web - portlet class - empty portlet to unlock Notification configuration

---
+++ modules/apps/batch-engine/batch-engine-web/src/main/java/com/liferay/batch/engine/web/internal/portlet/BatchEnginePortlet.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.batch.engine.web.internal.portlet;
+
+import com.liferay.batch.engine.constants.BatchEnginePortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Igor Beslic
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"javax.portlet.display-name=Batch Engine",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + BatchEnginePortletKeys.BATCH_ENGINE,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,power-user,user",
+		"javax.portlet.version=3.0"
+	},
+	service = Portlet.class
+)
+public class BatchEnginePortlet extends MVCPortlet {
+
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		super.render(renderRequest, renderResponse);
+	}
+
+}
