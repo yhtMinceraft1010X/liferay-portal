5207cc411ceda0a2c9c19faa2a5c8c31c34df70e
Brian Chan <brian.chan@liferay.com>
2022-03-09 19:55:00 +0100

    POSHI-306 rename to setup.sh

--- modules/test/poshi/poshi-standalone/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -o build.gradle https://raw.githubusercontent.com/liferay/liferay-portal/master/modules/test/poshi/poshi-standalone/build.gradle
-curl -o poshi.properties https://raw.githubusercontent.com/liferay/liferay-portal/master/modules/test/poshi/poshi-standalone/poshi.properties
-
-touch poshi-ext.properties
-touch settings.gradle
