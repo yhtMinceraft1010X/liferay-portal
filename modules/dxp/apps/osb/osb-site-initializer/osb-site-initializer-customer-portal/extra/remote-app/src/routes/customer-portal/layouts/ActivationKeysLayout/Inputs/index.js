6d9419c1d7abb2dbe62c6b98e6b9e2ffb96598b8
Joao Silva <joao.silva@liferay.com>
2022-03-10 11:09:51 -0300

    LPS-148951 Incrementing logic for message

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/layouts/ActivationKeysLayout/Inputs/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/layouts/ActivationKeysLayout/Inputs/index.js
@@ -11,7 +11,7 @@
 
 import {ClaySelect} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import client from '../../../../../apolloClient';
 import {Button} from '../../../../../common/components';
 import {useApplicationProvider} from '../../../../../common/context/AppPropertiesProvider';
@@ -20,8 +20,10 @@ import {
 	getAccountSubscriptionsTerms,
 } from '../../../../../common/services/liferay/graphql/queries';
 import {getCommonLicenseKey} from '../../../../../common/services/liferay/rest/raysource/LicenseKeys';
+import {ROLE_TYPES} from '../../../../../common/utils/constants';
 import downloadFromBlob from '../../../../../common/utils/downloadFromBlob';
 import getCurrentEndDate from '../../../../../common/utils/getCurrentEndDate';
+import {useCustomerPortal} from '../../../context';
 import {EXTENSION_FILE_TYPES, STATUS_CODE} from '../../../utils/constants';
 import {getYearlyTerms} from '../../../utils/getYearlyTerms';
 
@@ -31,6 +33,8 @@ const ActivationKeysInputs = ({
 	productTitle,
 	sessionId,
 }) => {
+	const [{project, userAccount}] = useCustomerPortal();
+
 	const {
 		createSupportRequest,
 		licenseKeyDownloadURL,
@@ -132,6 +136,55 @@ const ActivationKeysInputs = ({
 		setLicenseDownloadError(true);
 	};
 
+	const AccountBrief = userAccount.accountBriefs?.find(
+		(accountBrief) =>
+			accountBrief.externalReferenceCode === project?.accountKey
+	);
+
+	const errorDownloadMessage = useMemo(
+		() => ({
+			messageRequestersAdministrators: (
+				<p className="mt-3 text-neutral-7 text-paragraph">
+					{`The requested activation key is not yet available. For more
+                     information about the availability of your ${productTitle} 
+                     activation keys, please `}
+
+					<a
+						href={createSupportRequest}
+						rel="noreferrer"
+						target="_blank"
+					>
+						<u className="font-weight-bold text-neutral-9">
+							contact the Support team.
+						</u>
+					</a>
+				</p>
+			),
+			messageUsers: (
+				<p className="mt-3 text-neutral-7 text-paragraph">
+					{`The requested activation key is not yet available. If you need
+                 more information about the availability of your ${productTitle} activation keys, please ask one of your Administrator
+                 team members to update your permissions, so you can contact
+                 Liferay Support. Alternatively, team members with Administrator
+                 or Requester role can submit a support ticket on your behalf.`}
+				</p>
+			),
+		}),
+		[createSupportRequest, productTitle]
+	);
+
+	const currentEnterpriseMessage = useMemo(() => {
+		const isAdminOrRequester = AccountBrief.roleBriefs?.some(
+			({name}) =>
+				name === ROLE_TYPES.admin.key ||
+				name === ROLE_TYPES.requestor.key
+		);
+
+		return isAdminOrRequester
+			? errorDownloadMessage.messageRequestersAdministrators
+			: errorDownloadMessage.messageUsers;
+	}, [AccountBrief.roleBriefs, errorDownloadMessage]);
+
 	return (
 		<div className="mt-3">
 			<p className="text-paragraph">
@@ -222,23 +275,7 @@ const ActivationKeysInputs = ({
 				Download Key
 			</Button>
 
-			{hasLicenseDownloadError && (
-				<p className="mt-3 text-neutral-7 text-paragraph">
-					{`The requested activation key is not yet available. For more
-					information about the availability of your Enterprise Search
-					activation keys, please `}
-
-					<a
-						href={createSupportRequest}
-						rel="noreferrer"
-						target="_blank"
-					>
-						<u className="font-weight-bold text-neutral-9">
-							contact the Support team
-						</u>
-					</a>
-				</p>
-			)}
+			{hasLicenseDownloadError && currentEnterpriseMessage}
 		</div>
 	);
 };
