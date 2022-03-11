37b34ebcc4018d606f02f28c2e47cea04f8e9898
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-03-11 11:56:00 -0300

    LPS-148844 Cover the search on Rich Text field

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceSearchObjectEntriesTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceSearchObjectEntriesTest.java
@@ -16,6 +16,7 @@ package com.liferay.object.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.model.ObjectField;
@@ -24,6 +25,7 @@ import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.util.LocalizedMapUtil;
 import com.liferay.object.util.ObjectFieldUtil;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -45,6 +47,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Map;
+import java.util.Objects;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -177,6 +180,25 @@ public class ObjectEntryLocalServiceSearchObjectEntriesTest {
 		_assertKeywords("True", 1);
 		_assertKeywords("true", 1);
 		_assertKeywords("yes", 0);
+	}
+
+	@Test
+	public void testClob() throws Exception {
+		_testCharacterDataType(
+			false, ObjectFieldConstants.BUSINESS_TYPE_RICH_TEXT,
+			ObjectFieldConstants.DB_TYPE_CLOB, true, false);
+
+		_testCharacterDataType(
+			false, ObjectFieldConstants.BUSINESS_TYPE_RICH_TEXT,
+			ObjectFieldConstants.DB_TYPE_CLOB, false, false);
+
+		_testCharacterDataType(
+			false, ObjectFieldConstants.BUSINESS_TYPE_RICH_TEXT,
+			ObjectFieldConstants.DB_TYPE_CLOB, true, true);
+
+		_testCharacterDataType(
+			true, ObjectFieldConstants.BUSINESS_TYPE_RICH_TEXT,
+			ObjectFieldConstants.DB_TYPE_CLOB, true, false);
 	}
 
 	@Test
@@ -429,77 +451,21 @@ public class ObjectEntryLocalServiceSearchObjectEntriesTest {
 
 	@Test
 	public void testString() throws Exception {
-		_addObjectDefinition(
-			ObjectFieldUtil.createObjectField(
-				"Text", "String", true, false, null, "Alpha", "alpha", false));
+		_testCharacterDataType(
+			false, ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+			ObjectFieldConstants.DB_TYPE_STRING, true, false);
 
-		_addObjectEntry(
-			HashMapBuilder.<String, Serializable>put(
-				"alpha", "The quick brown fox trusted the lazy dog"
-			).build());
+		_testCharacterDataType(
+			false, ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+			ObjectFieldConstants.DB_TYPE_STRING, false, false);
 
-		_assertKeywords("fox", 1);
-		_assertKeywords("LAZY dog", 1);
-		_assertKeywords("lazy dog", 1);
-		_assertKeywords("trust", 0);
-		_assertKeywords("trusted", 1);
-	}
+		_testCharacterDataType(
+			false, ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+			ObjectFieldConstants.DB_TYPE_STRING, true, true);
 
-	@Test
-	public void testStringAnalyzed() throws Exception {
-		_addObjectDefinition(
-			ObjectFieldUtil.createObjectField(
-				"Text", "String", true, false, "en_US", "Alpha", "alpha",
-				false));
-
-		_addObjectEntry(
-			HashMapBuilder.<String, Serializable>put(
-				"alpha", "The english brown fox trusted the lazy dog"
-			).build());
-
-		_assertKeywords("fox", 1);
-		_assertKeywords("LAZY dog", 1);
-		_assertKeywords("lazy dog", 1);
-		_assertKeywords("trust", 1);
-		_assertKeywords("trusted", 1);
-	}
-
-	@Test
-	public void testStringKeyword() throws Exception {
-		_addObjectDefinition(
-			ObjectFieldUtil.createObjectField(
-				"Text", "String", true, true, null, "Alpha", "alpha", false));
-
-		_addObjectEntry(
-			HashMapBuilder.<String, Serializable>put(
-				"alpha", "test45"
-			).build());
-
-		_assertKeywords("45", 0);
-		_assertKeywords("te", 1);
-		_assertKeywords("test", 1);
-		_assertKeywords("test4", 1);
-		_assertKeywords("TEST45", 1);
-		_assertKeywords("Test45", 1);
-		_assertKeywords("test45", 1);
-		_assertKeywords("test456", 0);
-	}
-
-	@Test
-	public void testStringNotIndexed() throws Exception {
-		_addObjectDefinition(
-			ObjectFieldUtil.createObjectField(
-				"Text", "String", false, false, null, "Alpha", "alpha", false));
-
-		_addObjectEntry(
-			HashMapBuilder.<String, Serializable>put(
-				"alpha", "The unsearchable brown fox jumps over the lazy dog"
-			).build());
-
-		_assertKeywords("fox", 0);
-		_assertKeywords("The", 0);
-		_assertKeywords("The unsearchable", 0);
-		_assertKeywords("unsearchable", 0);
+		_testCharacterDataType(
+			true, ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+			ObjectFieldConstants.DB_TYPE_STRING, true, false);
 	}
 
 	private void _addObjectDefinition(ObjectField objectField)
@@ -549,6 +515,56 @@ public class ObjectEntryLocalServiceSearchObjectEntriesTest {
 			Collections.emptyList());
 
 		return objectField.getObjectFieldId();
+	}
+
+	private void _testCharacterDataType(
+			boolean analyzed, String businessType, String dbType,
+			boolean indexed, boolean indexedAsKeyword)
+		throws Exception {
+
+		if (_objectDefinition != null) {
+			_objectDefinitionLocalService.deleteObjectDefinition(
+				_objectDefinition);
+		}
+
+		_addObjectDefinition(
+			ObjectFieldUtil.createObjectField(
+				businessType, dbType, indexed, indexedAsKeyword,
+				analyzed ? "en_US" : null, "Alpha", "alpha", false));
+
+		String text = "The quick brown fox jumps over the lazy dog";
+
+		if (Objects.equals(
+				businessType, ObjectFieldConstants.BUSINESS_TYPE_RICH_TEXT)) {
+
+			text = StringBundler.concat("<strong>", text, "</strong>");
+		}
+
+		_addObjectEntry(
+			HashMapBuilder.<String, Serializable>put(
+				"alpha", text
+			).build());
+
+		if (indexedAsKeyword) {
+			_assertKeywords("quick", 0);
+			_assertKeywords("The quick", 1);
+			_assertKeywords("THE QUICK", 1);
+			_assertKeywords("the quick", 1);
+			_assertKeywords("They", 0);
+		}
+		else if (indexed) {
+			_assertKeywords("fox", 1);
+			_assertKeywords("jump", analyzed ? 1 : 0);
+			_assertKeywords("jumps", 1);
+			_assertKeywords("LAZY dog", 1);
+			_assertKeywords("lazy dog", 1);
+			_assertKeywords("strong", 0);
+		}
+		else {
+			_assertKeywords("fox", 0);
+			_assertKeywords("LAZY dog", 0);
+			_assertKeywords("lazy dog", 0);
+		}
 	}
 
 	@DeleteAfterTestRun
