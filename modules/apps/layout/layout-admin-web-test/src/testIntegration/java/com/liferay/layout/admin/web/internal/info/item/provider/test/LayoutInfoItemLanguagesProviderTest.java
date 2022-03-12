73d0e921f4621b26584ad0280bb9795f8f1b24c8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-03-12 10:19:04 +0100

    LPS-149104 No needed

--- modules/apps/layout/layout-admin-web-test/src/testIntegration/java/com/liferay/layout/admin/web/internal/info/item/provider/test/LayoutInfoItemLanguagesProviderTest.java
+++ modules/apps/layout/layout-admin-web-test/src/testIntegration/java/com/liferay/layout/admin/web/internal/info/item/provider/test/LayoutInfoItemLanguagesProviderTest.java
@@ -22,26 +22,16 @@ import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
-import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
-import com.liferay.portal.kernel.service.LayoutSetLocalService;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.util.PropsValues;
 import com.liferay.translation.info.item.provider.InfoItemLanguagesProvider;
 
 import java.util.HashSet;
@@ -50,16 +40,12 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Alicia García
@@ -77,13 +63,6 @@ public class LayoutInfoItemLanguagesProviderTest {
 		_group = GroupTestUtil.addGroup();
 
 		_layout = LayoutTestUtil.addTypePortletLayout(_group);
-
-		_serviceContext = ServiceContextTestUtil.getServiceContext(
-			_group.getGroupId());
-
-		_serviceContext.setRequest(_getHttpServletRequest());
-
-		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
 	}
 
 	@Test
@@ -146,49 +125,6 @@ public class LayoutInfoItemLanguagesProviderTest {
 		return availableLocales.toArray(new String[0]);
 	}
 
-	private HttpServletRequest _getHttpServletRequest() throws Exception {
-		MockHttpServletRequest mockHttpServletRequest =
-			new MockHttpServletRequest();
-
-		ThemeDisplay themeDisplay = _getThemeDisplay();
-
-		themeDisplay.setRequest(mockHttpServletRequest);
-
-		mockHttpServletRequest.setAttribute(
-			WebKeys.THEME_DISPLAY, themeDisplay);
-
-		mockHttpServletRequest.setRequestURI(
-			PropsValues.LAYOUT_FRIENDLY_URL_PUBLIC_SERVLET_MAPPING +
-				_group.getFriendlyURL() + _layout.getFriendlyURL());
-
-		return mockHttpServletRequest;
-	}
-
-	private ThemeDisplay _getThemeDisplay() throws Exception {
-		ThemeDisplay themeDisplay = new ThemeDisplay();
-
-		Company company = _companyLocalService.getCompany(
-			TestPropsValues.getCompanyId());
-
-		themeDisplay.setCompany(company);
-
-		themeDisplay.setLanguageId(_group.getDefaultLanguageId());
-		themeDisplay.setLayout(_layout);
-		themeDisplay.setLayoutSet(
-			_layoutSetLocalService.getLayoutSet(_group.getGroupId(), false));
-		themeDisplay.setLocale(
-			LocaleUtil.fromLanguageId(_group.getDefaultLanguageId()));
-		themeDisplay.setPortalDomain("localhost");
-		themeDisplay.setPortalURL(company.getPortalURL(_group.getGroupId()));
-		themeDisplay.setScopeGroupId(_group.getGroupId());
-		themeDisplay.setSecure(true);
-		themeDisplay.setServerName("localhost");
-		themeDisplay.setServerPort(8080);
-		themeDisplay.setSiteGroupId(_group.getGroupId());
-
-		return themeDisplay;
-	}
-
 	private Set<String> _getTranslatableFragmentLocales(
 		JSONObject jsonObject, String key, Set<Locale> siteAvailableLocales) {
 
@@ -242,9 +178,6 @@ public class LayoutInfoItemLanguagesProviderTest {
 	};
 
 	@Inject
-	private CompanyLocalService _companyLocalService;
-
-	@Inject
 	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
 
 	@DeleteAfterTestRun
@@ -260,10 +193,5 @@ public class LayoutInfoItemLanguagesProviderTest {
 
 	@Inject
 	private LayoutLocalService _layoutLocalService;
-
-	@Inject
-	private LayoutSetLocalService _layoutSetLocalService;
-
-	private ServiceContext _serviceContext;
 
 }
