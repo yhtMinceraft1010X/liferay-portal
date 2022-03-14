33c79f44f5fabc82952eb93ef1b1a1d9654275e9
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2022-03-14 15:06:40 +0100

    COMMERCE-8369 commerce-shipping-web added dynamic include for shipping options

---
+++ modules/apps/commerce/commerce-shipping-web/src/main/java/com/liferay/commerce/shipping/web/internal/servlet/taglib/DefaultCommerceShippingOptionsJSPDynamicInclude.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.shipping.web.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseJSPDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Sbarra
+ */
+@Component(enabled = false, immediate = true, service = DynamicInclude.class)
+public class DefaultCommerceShippingOptionsJSPDynamicInclude
+	extends BaseJSPDynamicInclude {
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"com.liferay.commerce.account.web#/account_entry" +
+				"/order_defaults.jsp#shipping_options");
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/dynamic_include/default_commerce_shipping_options.jsp";
+	}
+
+	@Override
+	protected Log getLog() {
+		return _log;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.shipping.web)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DefaultCommerceShippingOptionsJSPDynamicInclude.class);
+
+}
