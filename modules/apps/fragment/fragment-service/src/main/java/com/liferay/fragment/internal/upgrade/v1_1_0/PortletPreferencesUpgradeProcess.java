6f94d203f5a194a64055e1e0ba0224d26ec54e47
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-03-15 10:38:11 +0100

    LPS-148918 Use prepared statements to avoid 2nd order SQL injection in PortletPreferencesUpgradeProcess

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/upgrade/v1_1_0/PortletPreferencesUpgradeProcess.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/upgrade/v1_1_0/PortletPreferencesUpgradeProcess.java
@@ -94,19 +94,21 @@ public class PortletPreferencesUpgradeProcess extends UpgradeProcess {
 
 		Map<Long, Long> portletPreferencesMap = new HashMap<>();
 
-		long companyControlPanelPlid = _companyControlPanelPlids.get(companyId);
-
 		try (PreparedStatement preparedStatement = connection.prepareStatement(
 				StringBundler.concat(
 					"select PortletPreferences.portletPreferencesId, ",
 					"PortletPreferences.plid from PortletPreferences inner ",
 					"join Layout on PortletPreferences.plid = Layout.plid ",
 					"where PortletPreferences.portletId like ",
-					"CONCAT('%_INSTANCE_', '", namespace,
-					"') and (Layout.groupId = ", groupId,
-					" or PortletPreferences.plid = ", companyControlPanelPlid,
-					")"));
-			ResultSet resultSet = preparedStatement.executeQuery()) {
+					"CONCAT('%_INSTANCE_', '?') and (Layout.groupId = ? or ",
+					"PortletPreferences.plid = ?)"))) {
+
+			preparedStatement.setString(1, namespace);
+			preparedStatement.setLong(2, groupId);
+			preparedStatement.setLong(
+				3, _companyControlPanelPlids.get(companyId));
+
+			ResultSet resultSet = preparedStatement.executeQuery();
 
 			while (resultSet.next()) {
 				long portletPreferencesId = resultSet.getLong(
