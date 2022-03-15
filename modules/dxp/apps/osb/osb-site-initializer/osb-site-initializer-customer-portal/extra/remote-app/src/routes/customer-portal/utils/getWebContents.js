d7a951c88b9a8968c9756ca5d04e33788d133089
iagochaves <iagovieirachaves@gmail.com>
2022-03-15 16:01:56 -0300

    LPS-144952 Refactored getWebContent logic & added new validation

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/utils/getWebContents.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/utils/getWebContents.js
@@ -9,6 +9,7 @@
  * distribution rights of the Software.
  */
 
+import {SLA_TYPES} from '../../../common/utils/constants/slaTypes';
 import {
 	PRODUCT_TYPES,
 	SLA_NAMES,
@@ -17,44 +18,46 @@ import {
 
 export function getWebContents({dxpVersion, slaCurrent, subscriptionGroups}) {
 	const webContents = [];
+	const hasProjectSLA = Object.values(SLA_TYPES).some((slaType) =>
+		slaCurrent?.includes(slaType)
+	);
 
-	if (
-		subscriptionGroups.some(
-			({name}) =>
-				name === PRODUCT_TYPES.dxp ||
-				name === PRODUCT_TYPES.portal ||
-				name === PRODUCT_TYPES.commerce
-		) ||
-		!subscriptionGroups.some(
-			({name}) =>
-				name === PRODUCT_TYPES.partnership ||
-				name === PRODUCT_TYPES.dxpCloud
-		)
-	) {
+	const initialSubscriptions = Object.keys(
+		PRODUCT_TYPES
+	).map((productKey) => [productKey, false]);
+
+	const hasSubscriptionGroup = subscriptionGroups?.reduce(
+		(subscriptionGroupsAccumulator, subscriptionGroup) => {
+			const [currentProduct] = Object.entries(PRODUCT_TYPES).find(
+				([, productName]) => productName === subscriptionGroup?.name
+			);
+
+			if (currentProduct) {
+				subscriptionGroupsAccumulator[currentProduct] = true;
+			}
+
+			return subscriptionGroupsAccumulator;
+		},
+		Object.fromEntries(initialSubscriptions)
+	);
+
+	if (hasProjectSLA) {
 		webContents.push('WEB-CONTENT-ACTION-01');
 	}
 	if (
-		!subscriptionGroups.some(
-			({name}) => name === PRODUCT_TYPES.partnership
-		) &&
+		!hasSubscriptionGroup.partnership &&
 		slaCurrent !== SLA_NAMES.limitedSubscription
 	) {
 		webContents.push('WEB-CONTENT-ACTION-02');
 	}
 	if (
-		subscriptionGroups.some(
-			({name}) =>
-				name === PRODUCT_TYPES.dxp || name === PRODUCT_TYPES.dxpCloud
-		)
+		hasSubscriptionGroup.partnership ||
+		hasSubscriptionGroup.dxpCloud ||
+		hasSubscriptionGroup.dxp
 	) {
 		webContents.push('WEB-CONTENT-ACTION-03');
 	}
-	if (
-		subscriptionGroups.some(
-			({name}) =>
-				name === PRODUCT_TYPES.dxp || name === PRODUCT_TYPES.dxpCloud
-		)
-	) {
+	if (hasSubscriptionGroup.dxp || hasSubscriptionGroup.dxpCloud) {
 		webContents.push(
 			dxpVersion
 				? WEB_CONTENT_DXP_VERSION_TYPES[dxpVersion]
@@ -62,20 +65,21 @@ export function getWebContents({dxpVersion, slaCurrent, subscriptionGroups}) {
 		);
 	}
 	if (
-		!subscriptionGroups.some(
-			({name}) => name === PRODUCT_TYPES.analytics_cloud
-		) &&
-		(!subscriptionGroups.some(({name}) => name === PRODUCT_TYPES.portal) ||
-			(subscriptionGroups.some(
-				({name}) => name === PRODUCT_TYPES.portal
-			) &&
-				subscriptionGroups.some(
-					({name}) =>
-						name === PRODUCT_TYPES.dxp ||
-						name === PRODUCT_TYPES.dxpCloud
-				)))
+		!hasSubscriptionGroup.partnership &&
+		!hasSubscriptionGroup.analyticsCloud &&
+		(!hasSubscriptionGroup.portal ||
+			(hasSubscriptionGroup.portal &&
+				(hasSubscriptionGroup.dxp || hasSubscriptionGroup.dxpCloud)))
 	) {
 		webContents.push('WEB-CONTENT-ACTION-09');
+	}
+	if (
+		hasSubscriptionGroup.dxp ||
+		hasSubscriptionGroup.portal ||
+		hasSubscriptionGroup.commerce ||
+		!(hasSubscriptionGroup.partnership || hasSubscriptionGroup.dxpCloud)
+	) {
+		webContents.push('WEB-CONTENT-ACTION-10');
 	}
 
 	return webContents;
