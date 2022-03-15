e7b4b900fde6b88e39762ff6d988574fae2dfff2
Lino Alves <lino.alves@liferay.com>
2022-03-15 16:46:57 -0300

    LPS-149340 also checking if group is staged

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/exportimport/portlet/preferences/processor/DDMFormExportImportPortletPreferencesProcessor.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/exportimport/portlet/preferences/processor/DDMFormExportImportPortletPreferencesProcessor.java
@@ -132,7 +132,9 @@ public class DDMFormExportImportPortletPreferencesProcessor
 				return portletPreferences;
 			}
 
-			if (!group.isCompanyStagingGroup() && !group.isStagingGroup()) {
+			if (!group.isCompanyStagingGroup() && !group.isStagingGroup() &&
+				!group.isStaged()) {
+
 				return portletPreferences;
 			}
 		}
