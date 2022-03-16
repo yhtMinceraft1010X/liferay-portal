55e8b4d35b80b7e74e74709d20597563e4612549
BarbaraCabrera <barbara.cabrera@liferay.com>
2022-03-16 13:24:46 +0100

    LPS-146857 Update places where small empty space should appear (Page Editor: Fragments and Widgets search)

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/fragments-widgets/components/SearchResultsPanel.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/fragments-widgets/components/SearchResultsPanel.js
@@ -12,7 +12,7 @@
  * details.
  */
 
-import ClayAlert from '@clayui/alert';
+import ClayEmptyState from '@clayui/empty-state';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -38,11 +38,14 @@ export default function SearchResultsPanel({filteredTabs}) {
 			</div>
 		))
 	) : (
-		<ClayAlert displayType="info" title={Liferay.Language.get('info')}>
-			{Liferay.Language.get(
-				'there-are-no-fragments-or-widgets-on-this-page'
+		<ClayEmptyState
+			description={Liferay.Language.get(
+				'try-again-with-a-different-search'
 			)}
-		</ClayAlert>
+			imgSrc={`${themeDisplay.getPathThemeImages()}/states/search_state.gif`}
+			small
+			title={Liferay.Language.get('no-results-found')}
+		/>
 	);
 }
 
