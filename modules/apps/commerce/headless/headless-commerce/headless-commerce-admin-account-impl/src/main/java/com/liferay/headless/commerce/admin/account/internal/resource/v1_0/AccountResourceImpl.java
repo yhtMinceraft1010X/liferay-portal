b55891607b2418833d08aa7b60c61201f621ab21
Brian Chan <brian.chan@liferay.com>
2022-03-16 17:43:39 -0300

    Revert "COMMERCE-8766 remove null items"
    This reverts commit 18ae275a56fe569ddee2e22302105b7b1e449b73.

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/resource/v1_0/AccountResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/resource/v1_0/AccountResourceImpl.java
@@ -74,7 +74,6 @@ import com.liferay.portal.vulcan.util.SearchUtil;
 
 import java.io.IOException;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -205,7 +204,7 @@ public class AccountResourceImpl
 			String search, Filter filter, Pagination pagination, Sort[] sorts)
 		throws Exception {
 
-		Page<Account> page = SearchUtil.search(
+		return SearchUtil.search(
 			Collections.emptyMap(),
 			booleanQuery -> booleanQuery.getPreBooleanFilter(), filter,
 			AccountEntry.class.getName(), search, pagination,
@@ -224,12 +223,6 @@ public class AccountResourceImpl
 			document -> _toAccount(
 				_commerceAccountService.fetchCommerceAccount(
 					GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK)))));
-
-		Collection<Account> items = page.getItems();
-
-		items.removeIf(item -> item == null);
-
-		return Page.of(items);
 	}
 
 	@Override
