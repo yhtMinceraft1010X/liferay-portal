abcb338961f1f3b898e9947a09c7a1d02f83b9ec
Janis Zhang <janis.zhang@liferay.com>
2022-03-17 21:15:12 +0800

    LPS-150848 Use Mockito only in DestinationStatisticsManagerTest

--- modules/apps/portal/portal-messaging/src/test/java/com/liferay/portal/messaging/internal/jmx/DestinationStatisticsManagerTest.java
+++ modules/apps/portal/portal-messaging/src/test/java/com/liferay/portal/messaging/internal/jmx/DestinationStatisticsManagerTest.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.messaging.internal.jmx;
 
 import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.lang.management.ManagementFactory;
 
@@ -23,21 +24,21 @@ import javax.management.ObjectName;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import org.mockito.Mock;
-
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.Mockito;
 
 /**
  * @author Michael C. Han
  */
-@PowerMockIgnore("javax.management.*")
-@RunWith(PowerMockRunner.class)
 public class DestinationStatisticsManagerTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
 
 	@Before
 	public void setUp() throws Exception {
@@ -46,7 +47,9 @@ public class DestinationStatisticsManagerTest {
 
 	@Test
 	public void testRegisterMBean() throws Exception {
-		PowerMockito.when(
+		_destination = Mockito.mock(Destination.class);
+
+		Mockito.when(
 			_destination.getName()
 		).thenReturn(
 			"test"
@@ -63,9 +66,7 @@ public class DestinationStatisticsManagerTest {
 		Assert.assertTrue(_mBeanServer.isRegistered(objectName));
 	}
 
-	@Mock
 	private Destination _destination;
-
 	private MBeanServer _mBeanServer;
 
 }
