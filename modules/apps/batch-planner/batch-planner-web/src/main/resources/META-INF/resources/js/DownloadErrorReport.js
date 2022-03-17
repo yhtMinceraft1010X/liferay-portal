472bd3c2d53f1c8ad64c0007ea2fa387462067cd
Matija Petanjek <matija.petanjek@liferay.com>
2022-03-17 17:31:56 +0100

    COMMERCE-7556 batch-planner-web - fetch report from headless endpoint

---
+++ modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/DownloadErrorReport.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {fetch} from 'frontend-js-web';
+
+export default function ({batchEngineImportTaskId, namespace}) {
+	document
+		.getElementById(
+			`${namespace}downloadErrorReport${batchEngineImportTaskId}`
+		)
+		.addEventListener('click', (event) => {
+			event.preventDefault();
+			fetch(
+				`/o/headless-batch-engine/v1.0/import-task/${batchEngineImportTaskId}/failed-items/report`
+			).then((response) => {
+				response.blob().then((blob) => {
+					const LinkElement = document.createElement('a');
+
+					LinkElement.href = URL.createObjectURL(blob);
+
+					const fileName = response.headers
+						.get('Content-Disposition')
+						.match(/filename=(.*)/)[1];
+
+					LinkElement.download = fileName;
+
+					document.body.appendChild(LinkElement);
+
+					LinkElement.click();
+
+					LinkElement.remove();
+				});
+			});
+		});
+}
