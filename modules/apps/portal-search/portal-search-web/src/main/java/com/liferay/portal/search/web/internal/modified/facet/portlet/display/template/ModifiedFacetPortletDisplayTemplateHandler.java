3cb6dbd9cb8d4c81876dc8a4451166076bce4a69
Alan Huang <alan.huang@liferay.com>
2022-03-18 16:55:36 +0800

    LPS-148877 Move package to portlet.display.template (modified/facet in portal-search-web)

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/modified/facet/portlet/display/template/ModifiedFacetPortletDisplayTemplateHandler.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/modified/facet/portlet/display/template/ModifiedFacetPortletDisplayTemplateHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.search.web.internal.modified.facet.template;
+package com.liferay.portal.search.web.internal.modified.facet.portlet.display.template;
 
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
