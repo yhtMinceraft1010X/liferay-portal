6e971fd547ad579bcc73643d9e7f4f8deeff64ff
Janis Zhang <janis.zhang@liferay.com>
2022-03-21 20:07:15 +0800

    LPS-149473 Use Mockito only in LocationVariableResolverTest

--- portal-kernel/test/unit/com/liferay/portal/kernel/settings/LocationVariableResolverTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/settings/LocationVariableResolverTest.java
@@ -25,19 +25,19 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.powermock.api.mockito.PowerMockito;
+import org.mockito.Mockito;
 
 /**
  * @author Iván Zaera
  */
-public class LocationVariableResolverTest extends PowerMockito {
+public class LocationVariableResolverTest {
 
 	@Before
 	public void setUp() throws Exception {
 		_mockResourceManager = new MockResourceManager(
 			"En un lugar de la Mancha...");
 
-		_mockSettingsLocatorHelper = mock(SettingsLocatorHelper.class);
+		_mockSettingsLocatorHelper = Mockito.mock(SettingsLocatorHelper.class);
 
 		_locationVariableResolver = new LocationVariableResolver(
 			_mockResourceManager, _mockSettingsLocatorHelper);
@@ -124,7 +124,7 @@ public class LocationVariableResolverTest extends PowerMockito {
 
 		memorySettings.setValue("admin.email.from.address", expectedValue);
 
-		when(
+		Mockito.when(
 			_mockSettingsLocatorHelper.getServerSettings("com.liferay.portal")
 		).thenReturn(
 			memorySettings
