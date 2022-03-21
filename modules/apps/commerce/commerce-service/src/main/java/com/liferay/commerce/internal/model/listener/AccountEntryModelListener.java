1438f269692889464e15c3583a7b9ef0868c4562
Alessio Rendina <asilcomando@gmail.com>
2022-03-21 12:01:38 +0100

    COMMERCE-8731 Refactor account entry and commerce channel model listeners

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/model/listener/AccountEntryModelListener.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/model/listener/AccountEntryModelListener.java
@@ -17,6 +17,7 @@ package com.liferay.commerce.internal.model.listener;
 import com.liferay.account.model.AccountEntry;
 import com.liferay.commerce.account.exception.CommerceAccountOrdersException;
 import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.commerce.service.CommerceShippingOptionAccountEntryRelLocalService;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 
@@ -25,6 +26,7 @@ import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alec Sloan
+ * @author Alessio Antonio Rendina
  */
 @Component(enabled = false, immediate = true, service = ModelListener.class)
 public class AccountEntryModelListener extends BaseModelListener<AccountEntry> {
@@ -39,9 +41,17 @@ public class AccountEntryModelListener extends BaseModelListener<AccountEntry> {
 		if (accountOrders > 0) {
 			throw new CommerceAccountOrdersException();
 		}
+
+		_commerceShippingOptionAccountEntryRelLocalService.
+			deleteCommerceShippingOptionAccountEntryRelsByAccountEntryId(
+				accountEntry.getAccountEntryId());
 	}
 
 	@Reference
 	private CommerceOrderLocalService _commerceOrderLocalService;
+
+	@Reference
+	private CommerceShippingOptionAccountEntryRelLocalService
+		_commerceShippingOptionAccountEntryRelLocalService;
 
 }
