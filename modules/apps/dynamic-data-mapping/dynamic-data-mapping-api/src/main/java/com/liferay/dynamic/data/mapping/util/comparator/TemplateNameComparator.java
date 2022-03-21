c732890bcada50238e1ebb790cd7018774a63bb0
Attila Bakay <attila.bakay@liferay.com>
2022-03-21 16:28:19 +0100

    LPS-149701 Add a new name based comparator for DDMTemplates

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/comparator/TemplateNameComparator.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.util.comparator;
+
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.portal.kernel.util.CollatorUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.text.Collator;
+
+import java.util.Locale;
+
+/**
+ * @author Attila Bakay
+ */
+public class TemplateNameComparator extends OrderByComparator<DDMTemplate> {
+
+	public static final String ORDER_BY_ASC = "DDMTemplate.name ASC";
+
+	public static final String ORDER_BY_DESC = "DDMTemplate.name DESC";
+
+	public static final String[] ORDER_BY_FIELDS = {"name"};
+
+	public TemplateNameComparator() {
+		this(false);
+	}
+
+	public TemplateNameComparator(boolean ascending) {
+		this(ascending, LocaleUtil.getDefault());
+	}
+
+	public TemplateNameComparator(boolean ascending, Locale locale) {
+		_ascending = ascending;
+		_locale = locale;
+
+		_collator = CollatorUtil.getInstance(locale);
+	}
+
+	@Override
+	public int compare(DDMTemplate template1, DDMTemplate template2) {
+		String name1 = StringUtil.toLowerCase(template1.getName(_locale));
+		String name2 = StringUtil.toLowerCase(template2.getName(_locale));
+
+		int value = _collator.compare(name1, name2);
+
+		if (_ascending) {
+			return value;
+		}
+
+		return -value;
+	}
+
+	@Override
+	public String getOrderBy() {
+		if (_ascending) {
+			return ORDER_BY_ASC;
+		}
+
+		return ORDER_BY_DESC;
+	}
+
+	@Override
+	public String[] getOrderByFields() {
+		return ORDER_BY_FIELDS;
+	}
+
+	@Override
+	public boolean isAscending() {
+		return _ascending;
+	}
+
+	private final boolean _ascending;
+	private final Collator _collator;
+	private final Locale _locale;
+
+}
