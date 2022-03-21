20dc496658b717be7df268bb4f7636ac569c97ca
Alan Huang <alan.huang@liferay.com>
2022-03-21 17:09:46 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/portal-security/portal-security-service-access-policy-web/src/main/java/com/liferay/portal/security/service/access/policy/web/internal/portlet/SAPPortlet.java
+++ modules/apps/portal-security/portal-security-service-access-policy-web/src/main/java/com/liferay/portal/security/service/access/policy/web/internal/portlet/SAPPortlet.java
@@ -74,7 +74,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + SAPPortletKeys.SERVICE_ACCESS_POLICY,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
