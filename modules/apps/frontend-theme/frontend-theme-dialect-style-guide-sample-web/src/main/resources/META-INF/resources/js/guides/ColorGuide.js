5ae32abb3f3f3a8928a7d424be888d92a1c748e2
Ilza Medeiros <ilza.medeiros@liferay.com>
2022-03-21 14:47:05 -0300

    LPS-149717 - Include background and text color variations

--- modules/apps/frontend-theme/frontend-theme-dialect-style-guide-sample-web/src/main/resources/META-INF/resources/js/guides/ColorGuide.js
+++ modules/apps/frontend-theme/frontend-theme-dialect-style-guide-sample-web/src/main/resources/META-INF/resources/js/guides/ColorGuide.js
@@ -61,7 +61,37 @@ const NEUTRAL_COLORS = [
 	'neutral-10',
 ];
 
-const STATE_COLORS = ['success', 'info', 'warning', 'danger'];
+const STATE_COLORS_SUCCESS = [
+	'success-darken-2',
+	'success-darken-1',
+	'success',
+	'success-lighten-1',
+	'success-lighten-2',
+];
+
+const STATE_COLORS_INFO = [
+	'info-darken-2',
+	'info-darken-1',
+	'info',
+	'info-lighten-1',
+	'info-lighten-2',
+];
+
+const STATE_COLORS_WARNING = [
+	'warning-darken-2',
+	'warning-darken-1',
+	'warning',
+	'warning-lighten-1',
+	'warning-lighten-2',
+];
+
+const STATE_COLORS_DANGER = [
+	'danger-darken-2',
+	'danger-darken-1',
+	'danger',
+	'danger-lighten-1',
+	'danger-lighten-2',
+];
 
 const ACCENT_COLORS = [
 	'accent-1',
@@ -141,9 +171,51 @@ const ColorGuide = () => {
 
 			<TokenGroup
 				group="colors"
-				title={Liferay.Language.get('background-state')}
+				title={Liferay.Language.get('background-state-success')}
 			>
-				{STATE_COLORS.map((item) => (
+				{STATE_COLORS_SUCCESS.map((item) => (
+					<TokenItem
+						border={true}
+						className={`bg-${item}`}
+						key={item}
+						label={`bg-${item}`}
+					/>
+				))}
+			</TokenGroup>
+
+			<TokenGroup
+				group="colors"
+				title={Liferay.Language.get('background-state-info')}
+			>
+				{STATE_COLORS_INFO.map((item) => (
+					<TokenItem
+						border={true}
+						className={`bg-${item}`}
+						key={item}
+						label={`bg-${item}`}
+					/>
+				))}
+			</TokenGroup>
+
+			<TokenGroup
+				group="colors"
+				title={Liferay.Language.get('background-state-warning')}
+			>
+				{STATE_COLORS_WARNING.map((item) => (
+					<TokenItem
+						border={true}
+						className={`bg-${item}`}
+						key={item}
+						label={`bg-${item}`}
+					/>
+				))}
+			</TokenGroup>
+
+			<TokenGroup
+				group="colors"
+				title={Liferay.Language.get('background-state-danger')}
+			>
+				{STATE_COLORS_DANGER.map((item) => (
 					<TokenItem
 						border={true}
 						className={`bg-${item}`}
@@ -219,9 +291,57 @@ const ColorGuide = () => {
 
 			<TokenGroup
 				group="texts"
-				title={Liferay.Language.get('text-state')}
+				title={Liferay.Language.get('text-state-success')}
 			>
-				{STATE_COLORS.map((item) => (
+				{STATE_COLORS_SUCCESS.map((item) => (
+					<TokenItem
+						border={true}
+						className={`text-${item}`}
+						key={item}
+						label={`text-${item}`}
+					>
+						{SAMPLE_TEXT}
+					</TokenItem>
+				))}
+			</TokenGroup>
+
+			<TokenGroup
+				group="texts"
+				title={Liferay.Language.get('text-state-info')}
+			>
+				{STATE_COLORS_INFO.map((item) => (
+					<TokenItem
+						border={true}
+						className={`text-${item}`}
+						key={item}
+						label={`text-${item}`}
+					>
+						{SAMPLE_TEXT}
+					</TokenItem>
+				))}
+			</TokenGroup>
+
+			<TokenGroup
+				group="texts"
+				title={Liferay.Language.get('text-state-warning')}
+			>
+				{STATE_COLORS_WARNING.map((item) => (
+					<TokenItem
+						border={true}
+						className={`text-${item}`}
+						key={item}
+						label={`text-${item}`}
+					>
+						{SAMPLE_TEXT}
+					</TokenItem>
+				))}
+			</TokenGroup>
+
+			<TokenGroup
+				group="texts"
+				title={Liferay.Language.get('text-state-danger')}
+			>
+				{STATE_COLORS_DANGER.map((item) => (
 					<TokenItem
 						border={true}
 						className={`text-${item}`}
