4a2893676d9788a07cb6115aa27a2b1a579ec2f8
Alan Huang <alan.huang@liferay.com>
2022-03-21 17:09:53 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/portal/portal-cluster-multiple-sample-web/src/main/java/com/liferay/portal/cluster/multiple/sample/web/internal/portlet/ClusterSampleSessionReplicationPortlet.java
+++ modules/apps/portal/portal-cluster-multiple-sample-web/src/main/java/com/liferay/portal/cluster/multiple/sample/web/internal/portlet/ClusterSampleSessionReplicationPortlet.java
@@ -34,7 +34,8 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=com_liferay_portal_cluster_multiple_sample_web_internal_portlet_ClusterSampleSessionReplicationPortlet",
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
