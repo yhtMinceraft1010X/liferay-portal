521134f2f161fb4a180ecb48782fd5c764d059b1
Keven Leone <keven.leone@liferay.com>
2022-03-21 14:02:45 -0300

    LPS-149641 SF

--- modules/apps/site-initializer/site-initializer-testray/src/main/resources/site-initializer/fragments/testray/fragments/login/index.js
+++ modules/apps/site-initializer/site-initializer-testray/src/main/resources/site-initializer/fragments/testray/fragments/login/index.js
@@ -11,3 +11,49 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
+function main() {
+	const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+	const EMAIL_INPUT_ID = '_com_liferay_login_web_portlet_LoginPortlet_login';
+	const PASSWORD_INPUT_ID =
+		'_com_liferay_login_web_portlet_LoginPortlet_password';
+
+	const emailInput = document.getElementById(EMAIL_INPUT_ID);
+	const passwordInput = document.getElementById(PASSWORD_INPUT_ID);
+	const [emailLabel, passwordLabel] = document.querySelectorAll(
+		'div.form-group label'
+	);
+
+	const TEXT_VARIANT = {
+		danger: 'text-danger',
+		success: 'text-success',
+	};
+
+	const isEmailValid = (email) => {
+		const regex = new RegExp(EMAIL_REGEX);
+
+		return regex.test(email);
+	};
+
+	function onInputChange(element, validation) {
+		if (validation) {
+			element.classList.remove(TEXT_VARIANT.danger);
+			element.classList.add(TEXT_VARIANT.success);
+
+			return;
+		}
+
+		element.classList.remove(TEXT_VARIANT.success);
+		element.classList.add(TEXT_VARIANT.danger);
+	}
+
+	emailInput.addEventListener('change', () =>
+		onInputChange(emailLabel, isEmailValid(emailInput.value))
+	);
+
+	passwordInput.addEventListener('change', () =>
+		onInputChange(passwordLabel, passwordInput.value !== '')
+	);
+}
+
+main();
