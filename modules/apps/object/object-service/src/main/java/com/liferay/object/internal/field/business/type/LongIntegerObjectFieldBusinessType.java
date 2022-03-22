69d78f3aff1ba59411b77a945bbb9c9851824301
Mateus Santana <mateus.santana@liferay.com>
2022-03-22 16:40:41 -0300

    LPS-137866 Change Long Integer Business Field Type description

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/LongIntegerObjectFieldBusinessType.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/LongIntegerObjectFieldBusinessType.java
@@ -57,7 +57,7 @@ public class LongIntegerObjectFieldBusinessType
 		return LanguageUtil.get(
 			ResourceBundleUtil.getModuleAndPortalResourceBundle(
 				locale, getClass()),
-			"add-a-long-integer-up-to-19-digits");
+			"add-a-long-integer-up-to-16-digits");
 	}
 
 	@Override
