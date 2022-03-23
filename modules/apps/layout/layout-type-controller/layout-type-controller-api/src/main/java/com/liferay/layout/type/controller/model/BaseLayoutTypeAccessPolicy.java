5166f78fe43e7a8c4d32df0469f333cac2207169
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-03-23 09:15:40 +0100

    LPS-149978 Prevents using master layout to check the permissions if the portlet is not installed in the master layout.

--- modules/apps/layout/layout-type-controller/layout-type-controller-api/src/main/java/com/liferay/layout/type/controller/model/BaseLayoutTypeAccessPolicy.java
+++ modules/apps/layout/layout-type-controller/layout-type-controller-api/src/main/java/com/liferay/layout/type/controller/model/BaseLayoutTypeAccessPolicy.java
@@ -17,9 +17,13 @@ package com.liferay.layout.type.controller.model;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.PortletPreferences;
+import com.liferay.portal.kernel.model.PortletPreferencesIds;
 import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.model.impl.DefaultLayoutTypeAccessPolicyImpl;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactory;
 import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.permission.PortletPermissionUtil;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -47,18 +51,30 @@ public abstract class BaseLayoutTypeAccessPolicy
 				layout.getMasterLayoutPlid());
 
 			if (masterLayout != null) {
-				String resourcePrimKey = PortletPermissionUtil.getPrimaryKey(
-					masterLayout.getPlid(), portlet.getPortletId());
+				PortletPreferencesIds portletPreferencesIds =
+					portletPreferencesFactory.getPortletPreferencesIds(
+						httpServletRequest, masterLayout,
+						portlet.getPortletId());
 
-				List<ResourcePermission> resourcePermissions =
-					resourcePermissionLocalService.
-						getResourceResourcePermissions(
-							masterLayout.getCompanyId(),
-							masterLayout.getGroupId(), portlet.getPortletName(),
-							resourcePrimKey);
+				javax.portlet.PortletPreferences jxPortletPreferences =
+					portletPreferencesLocalService.fetchPreferences(
+						portletPreferencesIds);
 
-				if (ListUtil.isNotEmpty(resourcePermissions)) {
-					layout = masterLayout;
+				if (jxPortletPreferences != null) {
+					String resourcePrimKey =
+						PortletPermissionUtil.getPrimaryKey(
+							masterLayout.getPlid(), portlet.getPortletId());
+
+					List<ResourcePermission> resourcePermissions =
+						resourcePermissionLocalService.
+							getResourceResourcePermissions(
+								masterLayout.getCompanyId(),
+								masterLayout.getGroupId(),
+								portlet.getPortletName(), resourcePrimKey);
+
+					if (ListUtil.isNotEmpty(resourcePermissions)) {
+						layout = masterLayout;
+					}
 				}
 			}
 		}
@@ -68,6 +84,12 @@ public abstract class BaseLayoutTypeAccessPolicy
 
 	@Reference
 	protected LayoutLocalService layoutLocalService;
+
+	@Reference
+	protected PortletPreferencesFactory portletPreferencesFactory;
+
+	@Reference
+	protected PortletPreferencesLocalService portletPreferencesLocalService;
 
 	@Reference
 	protected ResourcePermissionLocalService resourcePermissionLocalService;
