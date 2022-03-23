a40bdfea8b7ba4aa18c7ffaef4c2774c39e875fd
Marcos Martins <marcos.martins@liferay.com>
2022-03-23 17:00:40 -0300

    LRAC-10637 create a dispatch task executor for each entity

---
+++ modules/apps/analytics/analytics-dxp-entity-impl/src/main/java/com/liferay/analytics/dxp/entity/internal/dispatch/executor/UploadOrganizationAnalyticsDXPEntityDispatchTaskExecutor.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.dxp.entity.internal.dispatch.executor;
+
+import com.liferay.analytics.dxp.entity.internal.helper.UploadAnalyticsDXPEntityDispatchTaskExecutorHelper;
+import com.liferay.dispatch.executor.BaseDispatchTaskExecutor;
+import com.liferay.dispatch.executor.DispatchTaskExecutor;
+import com.liferay.dispatch.executor.DispatchTaskExecutorOutput;
+import com.liferay.dispatch.model.DispatchTrigger;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.io.IOException;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcos Martins
+ */
+@Component(
+	immediate = true,
+	property = {
+		"dispatch.task.executor.name=" + UploadOrganizationAnalyticsDXPEntityDispatchTaskExecutor.KEY,
+		"dispatch.task.executor.type=" + UploadOrganizationAnalyticsDXPEntityDispatchTaskExecutor.KEY
+	},
+	service = DispatchTaskExecutor.class
+)
+public class UploadOrganizationAnalyticsDXPEntityDispatchTaskExecutor
+	extends BaseDispatchTaskExecutor {
+
+	public static final String KEY =
+		"upload-organization-analytics-dxp-entities";
+
+	@Override
+	public void doExecute(
+			DispatchTrigger dispatchTrigger,
+			DispatchTaskExecutorOutput dispatchTaskExecutorOutput)
+		throws IOException, PortalException {
+
+		_uploadAnalyticsDXPEntityDispatchTaskExecutorHelper.doExecute(
+			"organization-analytics-dxp-entities", dispatchTrigger,
+			dispatchTaskExecutorOutput);
+	}
+
+	@Override
+	public String getName() {
+		return KEY;
+	}
+
+	@Reference
+	private UploadAnalyticsDXPEntityDispatchTaskExecutorHelper
+		_uploadAnalyticsDXPEntityDispatchTaskExecutorHelper;
+
+}
