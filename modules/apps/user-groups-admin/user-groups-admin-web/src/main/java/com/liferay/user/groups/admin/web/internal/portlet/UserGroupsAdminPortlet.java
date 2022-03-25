89607514060c5cd61bae444d1c39e6c5be7d79a6
drewbrokke <drew.brokke@liferay.com>
2022-03-25 15:16:08 -0500

    LPS-148152 user-groups-admin-web: opt-in to portlet 3.0

--- modules/apps/user-groups-admin/user-groups-admin-web/src/main/java/com/liferay/user/groups/admin/web/internal/portlet/UserGroupsAdminPortlet.java
+++ modules/apps/user-groups-admin/user-groups-admin-web/src/main/java/com/liferay/user/groups/admin/web/internal/portlet/UserGroupsAdminPortlet.java
@@ -67,7 +67,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + UserGroupsAdminPortletKeys.USER_GROUPS_ADMIN,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
