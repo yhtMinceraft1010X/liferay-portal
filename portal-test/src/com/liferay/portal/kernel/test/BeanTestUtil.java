d9df99c3300416d96781221c95bd21f35a81db79
Guilherme Camacho <guilherme.camacho@liferay.com>
2022-03-28 11:50:52 -0300

    LPS-149082 New util class using pure reflection to deal with copy/set properties using JavaBeans

---
+++ portal-test/src/com/liferay/portal/kernel/test/BeanTestUtil.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.test;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import java.util.Objects;
+
+/**
+ * @author Guilherme Camacho
+ */
+public class BeanTestUtil {
+
+	public static void copyProperties(Object source, Object target)
+		throws Exception {
+
+		Class<?> sourceClass = source.getClass();
+
+		if (_hasSuperClass(sourceClass)) {
+			sourceClass = sourceClass.getSuperclass();
+		}
+
+		Class<?> targetClass = target.getClass();
+
+		for (Field field : sourceClass.getDeclaredFields()) {
+			Method getMethod = _getMethod(sourceClass, field.getName(), "get");
+
+			Method setMethod = _getMethod(
+				targetClass, field.getName(), "set", getMethod.getReturnType());
+
+			setMethod.invoke(target, getMethod.invoke(source, null));
+		}
+	}
+
+	public static void setProperty(Object bean, String name, Object value)
+		throws Exception {
+
+		Class<?> clazz = bean.getClass();
+
+		Method getMethod = _getMethod(clazz, name, "get");
+
+		Method setMethod = _getMethod(
+			clazz, name, "set", getMethod.getReturnType());
+
+		setMethod.invoke(
+			bean, _translateValue(getMethod.getReturnType(), value));
+	}
+
+	private static Method _getMethod(
+			Class<?> clazz, String fieldName, String prefix,
+			Class<?>... parameterTypes)
+		throws Exception {
+
+		return clazz.getMethod(
+			prefix + StringUtil.upperCaseFirstLetter(fieldName),
+			parameterTypes);
+	}
+
+	private static boolean _hasSuperClass(Class<?> clazz) {
+		Class<?> superClass = clazz.getSuperclass();
+
+		if ((superClass != null) &&
+			!Objects.equals(superClass.getName(), "java.lang.Object")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private static Object _translateValue(
+		Class<?> parameterType, Object value) {
+
+		if (parameterType.equals(Long.class)) {
+			return GetterUtil.getLong(value);
+		}
+
+		return value;
+	}
+
+}
