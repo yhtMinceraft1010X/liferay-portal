ac3bad2b6db57ddd16a94126a6fc379c3c3c8e27
Igor Beslic <igor.beslic@liferay.com>
2022-03-28 18:15:58 +0200

    COMMERCE-8906 batch-planner-web - SF

--- modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/DownloadHelper.js
+++ modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/DownloadHelper.js
@@ -17,11 +17,11 @@ import {fetch} from 'frontend-js-web';
 const getEndpoint = (type, taskId) => {
 	const endpoints = {
 		errorReport: `/o/headless-batch-engine/v1.0/import-task/${taskId}/failed-items/report`,
-		importFile: `/o/headless-batch-engine/v1.0/import-task/${taskId}/content`
-	} 
+		importFile: `/o/headless-batch-engine/v1.0/import-task/${taskId}/content`,
+	};
 
-	return endpoints[type]
-}
+	return endpoints[type];
+};
 
 export default function ({batchEngineImportTaskId, namespace, type}) {
 	document
@@ -31,26 +31,26 @@ export default function ({batchEngineImportTaskId, namespace, type}) {
 		.addEventListener('click', (event) => {
 			event.preventDefault();
 
-			fetch(
-				getEndpoint(type, batchEngineImportTaskId)
-			).then((response) => {
-				response.blob().then((blob) => {
-					const LinkElement = document.createElement('a');
+			fetch(getEndpoint(type, batchEngineImportTaskId)).then(
+				(response) => {
+					response.blob().then((blob) => {
+						const LinkElement = document.createElement('a');
 
-					LinkElement.href = URL.createObjectURL(blob);
+						LinkElement.href = URL.createObjectURL(blob);
 
-					const fileName = response.headers
-						.get('Content-Disposition')
-						.match(/filename=(.*)/)[1];
+						const fileName = response.headers
+							.get('Content-Disposition')
+							.match(/filename=(.*)/)[1];
 
-					LinkElement.download = fileName;
+						LinkElement.download = fileName;
 
-					document.body.appendChild(LinkElement);
+						document.body.appendChild(LinkElement);
 
-					LinkElement.click();
+						LinkElement.click();
 
-					LinkElement.remove();
-				});
-			});
+						LinkElement.remove();
+					});
+				}
+			);
 		});
 }
