3a205deef6ae7134fbc4a56724a6e9a7372fe346
Michael Bowerman <michael.bowerman@liferay.com>
2022-03-28 11:00:44 -0700

    LPS-150425 document-library-service: Remove usages of setInterval from model-specific index writer classes; they are no longer necessary since we are calling setInterval from the top level now
    https://issues.liferay.com/browse/LPS-150425

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/index/contributor/DLFileEntryModelIndexerWriterContributor.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/index/contributor/DLFileEntryModelIndexerWriterContributor.java
@@ -22,16 +22,12 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.Props;
-import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.search.batch.BatchIndexingActionable;
 import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
 import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
 import com.liferay.portal.search.spi.model.index.contributor.helper.IndexerWriterMode;
 import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -50,7 +46,6 @@ public class DLFileEntryModelIndexerWriterContributor
 		BatchIndexingActionable batchIndexingActionable,
 		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
 
-		batchIndexingActionable.setInterval(_dlFileIndexingInterval);
 		batchIndexingActionable.setPerformActionMethod(
 			(DLFileEntry dlFileEntry) -> batchIndexingActionable.addDocuments(
 				modelIndexerWriterDocumentHelper.getDocument(dlFileEntry)));
@@ -96,12 +91,6 @@ public class DLFileEntryModelIndexerWriterContributor
 		return IndexerWriterMode.UPDATE;
 	}
 
-	@Activate
-	protected void activate() {
-		_dlFileIndexingInterval = GetterUtil.getInteger(
-			props.get(PropsKeys.DL_FILE_INDEXING_INTERVAL), 500);
-	}
-
 	@Reference
 	protected DLFileEntryLocalService dlFileEntryLocalService;
 
@@ -109,12 +98,7 @@ public class DLFileEntryModelIndexerWriterContributor
 	protected DynamicQueryBatchIndexingActionableFactory
 		dynamicQueryBatchIndexingActionableFactory;
 
-	@Reference
-	protected Props props;
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		DLFileEntryModelIndexerWriterContributor.class);
-
-	private int _dlFileIndexingInterval;
 
 }
