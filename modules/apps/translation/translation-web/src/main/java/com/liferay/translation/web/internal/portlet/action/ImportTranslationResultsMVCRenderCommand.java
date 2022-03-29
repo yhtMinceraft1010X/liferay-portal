084e3235563c34681ef4f272d37ec2efa68d6373
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2022-03-29 13:31:01 +0200

    LPS-150271 Redirect if no display context set

--- modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/portlet/action/ImportTranslationResultsMVCRenderCommand.java
+++ modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/portlet/action/ImportTranslationResultsMVCRenderCommand.java
@@ -15,14 +15,20 @@
 package com.liferay.translation.web.internal.portlet.action;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.constants.MVCRenderConstants;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.translation.constants.TranslationPortletKeys;
 import com.liferay.translation.web.internal.display.context.ImportTranslationResultsDisplayContext;
 
+import java.io.IOException;
+
+import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.osgi.service.component.annotations.Component;
@@ -43,7 +49,8 @@ public class ImportTranslationResultsMVCRenderCommand
 
 	@Override
 	public String render(
-		RenderRequest renderRequest, RenderResponse renderResponse) {
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
 
 		HttpServletRequest httpServletRequest =
 			_portal.getOriginalServletRequest(
@@ -51,15 +58,42 @@ public class ImportTranslationResultsMVCRenderCommand
 
 		HttpSession httpSession = httpServletRequest.getSession();
 
+		ImportTranslationResultsDisplayContext
+			importTranslationResultsDisplayContext =
+				(ImportTranslationResultsDisplayContext)
+					httpSession.getAttribute(
+						ImportTranslationResultsDisplayContext.class.getName());
+
+		if (importTranslationResultsDisplayContext == null) {
+			_sendRedirect(renderRequest, renderResponse);
+
+			return MVCRenderConstants.MVC_PATH_VALUE_SKIP_DISPATCH;
+		}
+
 		renderRequest.setAttribute(
 			ImportTranslationResultsDisplayContext.class.getName(),
-			httpSession.getAttribute(
-				ImportTranslationResultsDisplayContext.class.getName()));
+			importTranslationResultsDisplayContext);
 
 		httpSession.removeAttribute(
 			ImportTranslationResultsDisplayContext.class.getName());
 
 		return "/import_translation_results.jsp";
+	}
+
+	private void _sendRedirect(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			HttpServletResponse httpServletResponse =
+				_portal.getHttpServletResponse(renderResponse);
+
+			httpServletResponse.sendRedirect(
+				ParamUtil.getString(renderRequest, "redirect"));
+		}
+		catch (IOException ioException) {
+			throw new PortletException(ioException);
+		}
 	}
 
 	@Reference
