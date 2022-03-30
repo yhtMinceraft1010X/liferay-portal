48dcd734d7e2938a292fe2b7f7b5db3bff4912b7
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-03-30 14:30:20 +0200

    LPS-142518 We should set this to true, otherwise the portlet topper won't be shown and we'll never be able to remove this from a widget page

--- modules/apps/cookies/cookies-banner-web/src/main/java/com/liferay/cookies/banner/web/internal/portlet/CookiesBannerConfigurationPortlet.java
+++ modules/apps/cookies/cookies-banner-web/src/main/java/com/liferay/cookies/banner/web/internal/portlet/CookiesBannerConfigurationPortlet.java
@@ -43,7 +43,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.render-weight=50",
 		"com.liferay.portlet.show-portlet-access-denied=false",
 		"com.liferay.portlet.show-portlet-inactive=false",
-		"com.liferay.portlet.use-default-template=false",
+		"com.liferay.portlet.use-default-template=true",
 		"javax.portlet.display-name=Cookies Banner Configuration",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/META-INF/resources/",
