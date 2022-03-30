fd67995b867c19c6f7f4febee3f85942efe0f81d
Brian Chan <brian.chan@liferay.com>
2022-03-30 06:32:46 -0300

    Revert "LPS-147681 Add javax.portlet.version=3.0"
    This reverts commit 18811d2ee2b9ebbfbb15c88c10a076686bb7178f.

--- modules/apps/portlet-configuration/portlet-configuration-css-web/src/main/java/com/liferay/portlet/configuration/css/web/internal/portlet/PortletConfigurationCSSPortlet.java
+++ modules/apps/portlet-configuration/portlet-configuration-css-web/src/main/java/com/liferay/portlet/configuration/css/web/internal/portlet/PortletConfigurationCSSPortlet.java
@@ -68,8 +68,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.template-path=/META-INF/resources/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + PortletConfigurationCSSPortletKeys.PORTLET_CONFIGURATION_CSS,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.version=3.0"
+		"javax.portlet.resource-bundle=content.Language"
 	},
 	service = Portlet.class
 )
