efdcba34031ed28728dba4a1e80f0a58ca8e177e
Marcela Cunha <marcela.cunha@liferay.com>
2022-03-30 22:07:14 -0300

    LPS-148738 Add upgrade process

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/ObjectServiceUpgrade.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/ObjectServiceUpgrade.java
@@ -24,6 +24,7 @@ import com.liferay.object.internal.upgrade.v2_3_0.util.ObjectFieldSettingTable;
 import com.liferay.object.internal.upgrade.v2_4_0.util.ObjectViewSortColumnTable;
 import com.liferay.object.internal.upgrade.v2_5_0.util.ObjectViewColumnUpgradeProcess;
 import com.liferay.object.internal.upgrade.v3_0_0.ObjectFieldSettingUpgradeProcess;
+import com.liferay.object.internal.upgrade.v3_2_0.ObjectValidationRuleUpgradeProcess;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
@@ -64,6 +65,9 @@ public class ObjectServiceUpgrade implements UpgradeStepRegistrator {
 			"3.0.0", "3.1.0",
 			new com.liferay.object.internal.upgrade.v3_1_0.
 				ObjectFieldBusinessTypeUpgradeProcess());
+
+		registry.register(
+			"3.1.0", "3.2.0", new ObjectValidationRuleUpgradeProcess());
 	}
 
 }
