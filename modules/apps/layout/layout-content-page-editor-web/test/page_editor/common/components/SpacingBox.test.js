b6c6b536537b187dc86c26a190669b9ee97499a0
Pablo Molina <contact@pablomolina.me>
2022-04-01 09:16:57 +0200

    LPS-150275 Add tests for SpacingBox

---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/common/components/SpacingBox.test.js
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, fireEvent, render, screen} from '@testing-library/react';
+import React from 'react';
+
+import SpacingBox from '../../../../src/main/resources/META-INF/resources/page_editor/common/components/SpacingBox';
+
+const SpacingBoxTest = ({onChange = () => {}, value = {}}) => (
+	<SpacingBox
+		defaultValue="0"
+		onChange={onChange}
+		options={[
+			{label: '0', value: '0'},
+			{label: '10', value: '10'},
+		]}
+		value={value}
+	/>
+);
+
+describe('SpacingBox', () => {
+	afterEach(() => {
+		cleanup();
+	});
+
+	it('renders given spacing values', async () => {
+		render(<SpacingBoxTest value={{marginTop: 10}} />);
+		expect(screen.getByLabelText('Padding Left')).toHaveTextContent('0');
+		expect(screen.getByLabelText('Margin Top')).toHaveTextContent('10');
+	});
+
+	it('can be navigated with keyboard', () => {
+		render(<SpacingBoxTest />);
+
+		const grid = screen.getByRole('grid');
+
+		screen.getByLabelText('Margin Top').focus();
+
+		fireEvent.keyDown(grid, {key: 'ArrowDown'});
+		fireEvent.keyDown(grid, {key: 'ArrowRight'});
+		fireEvent.keyDown(grid, {key: 'ArrowRight'});
+		fireEvent.keyDown(grid, {key: 'ArrowDown'});
+		fireEvent.keyDown(grid, {key: 'ArrowLeft'});
+		fireEvent.keyDown(grid, {key: 'ArrowRight'});
+
+		expect(screen.getByLabelText('Padding Left')).toHaveFocus();
+	});
+
+	it('can be used to update spacing', () => {
+		window.Liferay.Util.sub = (key, args) => {
+			return args
+				.reduce((key, arg) => key.replace('x', arg), key)
+				.replaceAll('-', ' ');
+		};
+
+		const onChange = jest.fn();
+		render(<SpacingBoxTest onChange={onChange} />);
+
+		fireEvent.click(screen.getByLabelText('Padding Left'));
+		fireEvent.click(screen.getByLabelText('set Padding Left to 10'));
+
+		expect(onChange).toHaveBeenCalledWith('paddingLeft', '10');
+	});
+});
