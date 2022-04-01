679ed2dd8f52ffbe64fd49fb60c3390c042b8540
Victor Galan <victor.galan@liferay.com>
2022-04-01 09:58:55 +0200

    LPS-150573 Create component that injects and modify a style tag for the common styles
    There is different style sheet for master page, since it is more unusual to change the master layout than to change styles. This way we don't calculate master styles more than needed

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/topper/CommonStylesManager.js
@@ -0,0 +1,187 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {usePrevious} from '@liferay/frontend-js-react-web';
+import {useEffect, useRef} from 'react';
+
+import {CONTAINER_WIDTH_TYPES} from '../../config/constants/containerWidthTypes';
+import {LAYOUT_DATA_ITEM_TYPES} from '../../config/constants/layoutDataItemTypes';
+import {VIEWPORT_SIZES} from '../../config/constants/viewportSizes';
+import {config} from '../../config/index';
+import {useGlobalContext} from '../../contexts/GlobalContext';
+import {useSelector} from '../../contexts/StoreContext';
+import {deepEqual} from '../../utils/checkDeepEqual';
+import generateStyleSheet from '../../utils/generateStyleSheet';
+import {getResponsiveConfig} from '../../utils/getResponsiveConfig';
+
+const LAYOUT_DATA_ITEMS_WITH_COMMON_STYLES = [
+	LAYOUT_DATA_ITEM_TYPES.collection,
+	LAYOUT_DATA_ITEM_TYPES.container,
+	LAYOUT_DATA_ITEM_TYPES.row,
+	LAYOUT_DATA_ITEM_TYPES.fragment,
+];
+
+export default function CommonStylesManager() {
+	const layoutData = useSelector((state) => state.layoutData);
+	const masterLayoutData = useSelector(
+		(state) => state.masterLayout?.masterLayoutData
+	);
+	const selectedViewportSize = useSelector(
+		(state) => state.selectedViewportSize
+	);
+
+	const previousMasterLayoutData = usePrevious(masterLayoutData);
+
+	const globalContext = useGlobalContext();
+
+	const layoutDataItemsConfigRef = useRef(null);
+	const masterLayoutDataItemsConfigRef = useRef(null);
+
+	useEffect(() => {
+		const items = Object.values(layoutData.items);
+
+		const nextStyles = {};
+
+		items.forEach((item) => {
+			if (hasCommonStyles(item)) {
+				const styles = getResponsiveConfig(
+					item.config,
+					selectedViewportSize
+				)?.styles;
+
+				nextStyles[item.itemId] = filterStyles({
+					item,
+					selectedViewportSize,
+					styles,
+				});
+			}
+		});
+
+		if (
+			!layoutDataItemsConfigRef.current ||
+			!deepEqual(nextStyles, layoutDataItemsConfigRef.current)
+		) {
+			layoutDataItemsConfigRef.current = nextStyles;
+
+			const styleSheet = generateStyleSheet(nextStyles);
+
+			createOrUpdateStyleTag({
+				globalContext,
+				id: 'layout-common-styles',
+				styleSheet,
+			});
+		}
+	}, [layoutData.items, selectedViewportSize, globalContext]);
+
+	useEffect(() => {
+		if (previousMasterLayoutData === masterLayoutData) {
+			return;
+		}
+
+		const items = Object.values(masterLayoutData.items);
+
+		const nextStyles = {};
+
+		items.forEach((item) => {
+			if (hasCommonStyles(item)) {
+				const styles = getResponsiveConfig(
+					item.config,
+					selectedViewportSize
+				)?.styles;
+
+				nextStyles[item.itemId] = filterStyles({
+					item,
+					selectedViewportSize,
+					styles,
+				});
+			}
+		});
+
+		if (
+			!masterLayoutDataItemsConfigRef.current ||
+			!deepEqual(nextStyles, masterLayoutDataItemsConfigRef.current)
+		) {
+			masterLayoutDataItemsConfigRef.current = nextStyles;
+
+			const styleSheet = generateStyleSheet(nextStyles, {
+				hasTopper: false,
+			});
+
+			createOrUpdateStyleTag({
+				globalContext,
+				id: 'layout-master-common-styles',
+				styleSheet,
+			});
+		}
+	}, [
+		masterLayoutData,
+		selectedViewportSize,
+		globalContext,
+		previousMasterLayoutData,
+	]);
+
+	return null;
+}
+
+function createOrUpdateStyleTag({globalContext, id, styleSheet}) {
+	let styleTag = globalContext.document.getElementById(id);
+
+	if (!styleTag) {
+		styleTag = globalContext.document.createElement('style');
+		styleTag.id = id;
+		styleTag.type = 'text/css';
+		styleTag.dataset.sennaTrack = 'temporary';
+
+		globalContext.document.head.appendChild(styleTag);
+	}
+
+	styleTag.innerHTML = styleSheet;
+
+	return styleTag;
+}
+
+function hasCommonStyles(item) {
+	return LAYOUT_DATA_ITEMS_WITH_COMMON_STYLES.includes(item.type);
+}
+
+/**
+ * Filter the styles that we don't need to include in the CSS.
+ *
+ * The values that are rejected are:
+ *  - Empty style values
+ *  - Values equals to the default value (in desktop).
+ *  - Margin left and margin right in a fixed Container (fixed container already have defined margins).
+ */
+function filterStyles({item, selectedViewportSize, styles}) {
+	const filteredStyles = {};
+
+	Object.entries(styles).forEach(([styleName, styleValue]) => {
+		const {defaultValue} = config.commonStylesFields[styleName];
+
+		const isContainerFixed =
+			item.config?.widthType === CONTAINER_WIDTH_TYPES.fixed;
+
+		if (
+			styleValue &&
+			defaultValue !== styleValue &&
+			selectedViewportSize === VIEWPORT_SIZES.desktop &&
+			(!isContainerFixed ||
+				(styleName !== 'marginRight' && styleName !== 'marginLeft'))
+		) {
+			filteredStyles[styleName] = styleValue;
+		}
+	});
+
+	return filteredStyles;
+}
