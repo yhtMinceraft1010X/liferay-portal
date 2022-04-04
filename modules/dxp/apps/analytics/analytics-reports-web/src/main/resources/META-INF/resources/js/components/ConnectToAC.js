17e66098e0746700c4654127a8626eeb58082bb3
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-04-04 16:19:48 +0200

    LPS-148315 Add ConnectToAC component

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/ConnectToAC.js
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/ConnectToAC.js
@@ -9,10 +9,104 @@
  * distribution rights of the Software.
  */
 
+import ClayIcon from '@clayui/icon';
+import ClayLink from '@clayui/link';
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function ConnectToAC() {
-	return <div className="p-3 pt-4 text-center">TODO</div>;
+export default function ConnectToAC({
+	analyticsCloudTrialURL,
+	analyticsURL,
+	hideAnalyticsReportsPanelURL,
+	isAnalyticsConnected,
+	pathToAssets,
+}) {
+	return (
+		<div className="p-3 pt-4 text-center">
+			<img
+				alt={Liferay.Language.get('connect-to-liferay-analytics-cloud')}
+				src={`${pathToAssets}assets/ac-icon.svg`}
+			></img>
+
+			{isAnalyticsConnected ? (
+				<>
+					<h4 className="font-weight-semi-bold h5 mt-3">
+						{Liferay.Language.get('sync-to-analytics-cloud')}
+					</h4>
+
+					<p className="text-secondary">
+						{Liferay.Language.get(
+							'sync-your-liferay-dxp-instance-with-analytics-cloud-to-view-content-performance-metrics-and-build-a-successful-content-strategy'
+						)}
+					</p>
+
+					<ClayLink
+						button={true}
+						displayType="primary"
+						href={analyticsURL}
+						target="_blank"
+					>
+						{Liferay.Language.get('open-analytics-cloud')}
+
+						<span className="inline-item inline-item-after">
+							<ClayIcon symbol="shortcut" />
+						</span>
+					</ClayLink>
+				</>
+			) : (
+				<>
+					<h4 className="font-weight-semi-bold h5 mt-3">
+						{Liferay.Language.get(
+							'connect-to-liferay-analytics-cloud'
+						)}
+					</h4>
+
+					<p className="text-secondary">
+						{Liferay.Language.get(
+							'liferay-dxp-instance-has-to-be-connected-with-analytics-cloud-to-view-content-performance-metrics-and-build-a-successful-content-strategy'
+						)}
+					</p>
+
+					<ClayLink
+						button={true}
+						displayType="secondary"
+						href={analyticsCloudTrialURL}
+						target="_blank"
+					>
+						{Liferay.Language.get('start-free-trial')}
+
+						<span className="inline-item inline-item-after">
+							<ClayIcon symbol="shortcut" />
+						</span>
+					</ClayLink>
+
+					<ClayLink
+						className="d-block font-weight-bold mb-2 mt-5"
+						href={hideAnalyticsReportsPanelURL}
+					>
+						{Liferay.Language.get('do-not-show-me-this-again')}
+					</ClayLink>
+
+					<p className="text-secondary">
+						{Liferay.Language.get('do-not-show-me-this-again-help')}
+					</p>
+				</>
+			)}
+		</div>
+	);
 }
 
-ConnectToAC.propTypes = {};
+ConnectToAC.propTypes = {
+	analyticsCloudTrialURL: PropTypes.string,
+	hideAnalyticsReportsPanelURL: PropTypes.string,
+	isAnalyticsConnected: PropTypes.bool,
+	pathToAssets: PropTypes.string,
+};
+
+ConnectToAC.defaultProps = {
+	analyticsCloudTrialURL:
+		'https://www.liferay.com/products/analytics-cloud/get-started',
+	hideAnalyticsReportsPanelURL: '',
+	isAnalyticsConnected: false,
+	pathToAssets: '/o/analytics-reports-web/',
+};
