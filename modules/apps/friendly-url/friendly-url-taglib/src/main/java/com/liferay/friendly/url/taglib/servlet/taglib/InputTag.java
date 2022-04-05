377c57d6c19a29650137f88a2b095d60afa57671
Alicia García <alicia.garcia@liferay.com>
2022-04-05 10:54:36 +0200

    LPS-150652 the input tag url is double encoded, so we need to escape the url to let the user see the xml and the proper value of the url title

--- modules/apps/friendly-url/friendly-url-taglib/src/main/java/com/liferay/friendly/url/taglib/servlet/taglib/InputTag.java
+++ modules/apps/friendly-url/friendly-url-taglib/src/main/java/com/liferay/friendly/url/taglib/servlet/taglib/InputTag.java
@@ -29,6 +29,7 @@ import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.PersistedModelLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalServiceRegistryUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -226,10 +227,11 @@ public class InputTag extends IncludeTag {
 					getClassPK());
 
 			if (isLocalizable()) {
-				return mainFriendlyURLEntry.getUrlTitleMapAsXML();
+				return HtmlUtil.escapeURL(
+					mainFriendlyURLEntry.getUrlTitleMapAsXML());
 			}
 
-			return mainFriendlyURLEntry.getUrlTitle();
+			return HtmlUtil.escapeURL(mainFriendlyURLEntry.getUrlTitle());
 		}
 		catch (NoSuchFriendlyURLEntryMappingException
 					noSuchFriendlyURLEntryMappingException) {
