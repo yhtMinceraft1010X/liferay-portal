5fcb3636d1203baa87946e3c14debb72b1359070
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-04-05 12:47:01 +0200

    LPS-149255 Disable paid and organic traffic sources in the front-end part

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/TrafficSources.js
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/TrafficSources.js
@@ -173,8 +173,9 @@ export default function TrafficSources({dataProvider, onTrafficSourceClick}) {
 						<tbody>
 							{trafficSources?.map((entry) => {
 								const hasDetails =
-									entry?.value > 0 && entry.name !== 'direct';
-
+									entry?.value > 0 &&
+									entry?.endpointURL;	
+																		
 								return (
 									<tr key={entry.name}>
 										<td
