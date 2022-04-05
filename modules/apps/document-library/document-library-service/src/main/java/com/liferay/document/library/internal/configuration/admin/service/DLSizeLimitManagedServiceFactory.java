ed64d713b800d6cdd8d8a221426b13d3c9d8b302
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2022-04-05 16:29:18 +0200

    LPS-114787 Add methods to work with the group configuration

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/configuration/admin/service/DLSizeLimitManagedServiceFactory.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/configuration/admin/service/DLSizeLimitManagedServiceFactory.java
@@ -20,6 +20,9 @@ import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -35,6 +38,7 @@ import org.osgi.service.cm.ManagedServiceFactory;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Adolfo Pérez
@@ -80,6 +84,32 @@ public class DLSizeLimitManagedServiceFactory implements ManagedServiceFactory {
 		return map.getOrDefault(String.format("%s/*", parts.get(0)), 0L);
 	}
 
+	public long getGroupFileMaxSize(long groupId) {
+		DLSizeLimitConfiguration dlSizeLimitConfiguration =
+			_getGroupDLSizeLimitConfiguration(groupId);
+
+		return dlSizeLimitConfiguration.fileMaxSize();
+	}
+
+	public long getGroupMimeTypeSizeLimit(long groupId, String mimeType) {
+		if (Validator.isNull(mimeType)) {
+			return 0;
+		}
+
+		Map<String, Long> map = _groupMimeTypeSizeLimitsMap.computeIfAbsent(
+			groupId, this::_computeGroupMimeTypeSizeLimit);
+
+		long sizeLimit = map.getOrDefault(mimeType, 0L);
+
+		if (sizeLimit != 0) {
+			return sizeLimit;
+		}
+
+		List<String> parts = StringUtil.split(mimeType, CharPool.SLASH);
+
+		return map.getOrDefault(String.format("%s/*", parts.get(0)), 0L);
+	}
+
 	@Override
 	public String getName() {
 		return "com.liferay.document.library.internal.configuration." +
@@ -98,12 +128,20 @@ public class DLSizeLimitManagedServiceFactory implements ManagedServiceFactory {
 		if (companyId != CompanyConstants.SYSTEM) {
 			_updateCompanyConfiguration(companyId, pid, dictionary);
 		}
+
+		long groupId = GetterUtil.getLong(
+			dictionary.get("groupId"), GroupConstants.DEFAULT_PARENT_GROUP_ID);
+
+		if (groupId != GroupConstants.DEFAULT_PARENT_GROUP_ID) {
+			_updateGroupConfiguration(groupId, pid, dictionary);
+		}
 	}
 
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
 		_companyMimeTypeSizeLimitsMap = new ConcurrentHashMap<>();
+		_groupMimeTypeSizeLimitsMap = new ConcurrentHashMap<>();
 		_systemDLSizeLimitConfiguration = ConfigurableUtil.createConfigurable(
 			DLSizeLimitConfiguration.class, properties);
 	}
@@ -111,6 +149,22 @@ public class DLSizeLimitManagedServiceFactory implements ManagedServiceFactory {
 	private Map<String, Long> _computeCompanyMimeTypeSizeLimit(long companyId) {
 		DLSizeLimitConfiguration dlSizeLimitConfiguration =
 			_getCompanyDLSizeLimitConfiguration(companyId);
+
+		Map<String, Long> mimeTypeSizeLimits = new HashMap<>();
+
+		for (String mimeTypeSizeLimit :
+				dlSizeLimitConfiguration.mimeTypeSizeLimit()) {
+
+			MimeTypeSizeLimitUtil.parseMimeTypeSizeLimit(
+				mimeTypeSizeLimit, mimeTypeSizeLimits::put);
+		}
+
+		return mimeTypeSizeLimits;
+	}
+
+	private Map<String, Long> _computeGroupMimeTypeSizeLimit(long groupId) {
+		DLSizeLimitConfiguration dlSizeLimitConfiguration =
+			_getGroupDLSizeLimitConfiguration(groupId);
 
 		Map<String, Long> mimeTypeSizeLimits = new HashMap<>();
 
@@ -134,12 +188,31 @@ public class DLSizeLimitManagedServiceFactory implements ManagedServiceFactory {
 		return _systemDLSizeLimitConfiguration;
 	}
 
+	private DLSizeLimitConfiguration _getGroupDLSizeLimitConfiguration(
+		long groupId) {
+
+		if (_groupConfigurationBeans.containsKey(groupId)) {
+			return _groupConfigurationBeans.get(groupId);
+		}
+
+		Group group = _groupLocalService.fetchGroup(groupId);
+
+		return _getCompanyDLSizeLimitConfiguration(group.getCompanyId());
+	}
+
 	private void _unmapPid(String pid) {
 		if (_companyIds.containsKey(pid)) {
 			long companyId = _companyIds.remove(pid);
 
 			_companyConfigurationBeans.remove(companyId);
 			_companyMimeTypeSizeLimitsMap.remove(companyId);
+		}
+
+		if (_groupIds.containsKey(pid)) {
+			long groupId = _groupIds.remove(pid);
+
+			_groupConfigurationBeans.remove(groupId);
+			_groupMimeTypeSizeLimitsMap.remove(groupId);
 		}
 	}
 
@@ -154,10 +227,29 @@ public class DLSizeLimitManagedServiceFactory implements ManagedServiceFactory {
 		_companyMimeTypeSizeLimitsMap.remove(companyId);
 	}
 
+	private void _updateGroupConfiguration(
+		long groupId, String pid, Dictionary<String, ?> dictionary) {
+
+		_groupConfigurationBeans.put(
+			groupId,
+			ConfigurableUtil.createConfigurable(
+				DLSizeLimitConfiguration.class, dictionary));
+		_groupIds.put(pid, groupId);
+		_groupMimeTypeSizeLimitsMap.remove(groupId);
+	}
+
 	private final Map<Long, DLSizeLimitConfiguration>
 		_companyConfigurationBeans = new ConcurrentHashMap<>();
 	private final Map<String, Long> _companyIds = new ConcurrentHashMap<>();
 	private volatile Map<Long, Map<String, Long>> _companyMimeTypeSizeLimitsMap;
+	private final Map<Long, DLSizeLimitConfiguration> _groupConfigurationBeans =
+		new ConcurrentHashMap<>();
+	private final Map<String, Long> _groupIds = new ConcurrentHashMap<>();
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	private volatile Map<Long, Map<String, Long>> _groupMimeTypeSizeLimitsMap;
 	private volatile DLSizeLimitConfiguration _systemDLSizeLimitConfiguration;
 
 }
