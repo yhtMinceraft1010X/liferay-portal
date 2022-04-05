6a416b8733bddf55152198438c059414587d881b
Matija Petanjek <matija.petanjek@liferay.com>
2022-04-05 18:26:57 +0200

    COMMERCE-9023 batch-planner-service - remove Log indexes

--- modules/apps/batch-planner/batch-planner-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/batch-planner/batch-planner-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,8 +1,3 @@
-create unique index IX_3758C647 on BatchPlannerLog (batchEngineExportTaskERC[$COLUMN_LENGTH:75$]);
-create unique index IX_EA3EC576 on BatchPlannerLog (batchEngineImportTaskERC[$COLUMN_LENGTH:75$]);
-create unique index IX_8806EF8E on BatchPlannerLog (batchPlannerPlanId, dispatchTriggerERC[$COLUMN_LENGTH:75$]);
-create index IX_ECB4A956 on BatchPlannerLog (companyId);
-
 create unique index IX_E025DC1A on BatchPlannerMapping (batchPlannerPlanId, externalFieldName[$COLUMN_LENGTH:75$], internalFieldName[$COLUMN_LENGTH:75$]);
 
 create index IX_18CD7477 on BatchPlannerPlan (companyId, export, template);
