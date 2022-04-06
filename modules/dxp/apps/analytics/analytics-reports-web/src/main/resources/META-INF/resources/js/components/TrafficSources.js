f3eecc8dff55f94587e11d791ea9cfab87d69166
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-04-06 17:46:24 +0200

    LPS-149255 SF

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/TrafficSources.js
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/TrafficSources.js
@@ -173,9 +173,8 @@ export default function TrafficSources({dataProvider, onTrafficSourceClick}) {
 						<tbody>
 							{trafficSources?.map((entry) => {
 								const hasDetails =
-									entry?.value > 0 &&
-									entry?.endpointURL;	
-																		
+									entry?.value > 0 && entry?.endpointURL;
+
 								return (
 									<tr key={entry.name}>
 										<td
