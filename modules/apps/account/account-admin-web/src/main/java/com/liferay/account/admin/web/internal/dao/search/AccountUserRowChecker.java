fb3a4cdd931f17cc7a48d7b2d076c96721691f37
pei-jung <pei-jung.lan@liferay.com>
2022-04-05 20:33:48 -0700

    LPS-149125 Disable checkboxes for users with invalid email domain

--- modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/dao/search/AccountUserRowChecker.java
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/dao/search/AccountUserRowChecker.java
@@ -15,8 +15,20 @@
 package com.liferay.account.admin.web.internal.dao.search;
 
 import com.liferay.account.admin.web.internal.display.AccountUserDisplay;
+import com.liferay.account.configuration.AccountEntryEmailDomainsConfiguration;
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.service.AccountEntryLocalServiceUtil;
 import com.liferay.account.service.AccountEntryUserRelLocalServiceUtil;
+import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProviderUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import javax.portlet.PortletResponse;
 
@@ -43,8 +55,60 @@ public class AccountUserRowChecker extends EmptyOnClickRowChecker {
 
 	@Override
 	public boolean isDisabled(Object object) {
-		return isChecked(object);
+		if (isChecked(object)) {
+			return true;
+		}
+
+		AccountUserDisplay accountUserDisplay = (AccountUserDisplay)object;
+
+		User user = UserLocalServiceUtil.fetchUser(
+			accountUserDisplay.getUserId());
+
+		try {
+			AccountEntryEmailDomainsConfiguration
+				accountEntryEmailDomainsConfiguration =
+					ConfigurationProviderUtil.getCompanyConfiguration(
+						AccountEntryEmailDomainsConfiguration.class,
+						user.getCompanyId());
+
+			if (!accountEntryEmailDomainsConfiguration.
+					enableEmailDomainValidation()) {
+
+				return false;
+			}
+
+			AccountEntry accountEntry =
+				AccountEntryLocalServiceUtil.fetchAccountEntry(_accountEntryId);
+
+			if ((accountEntry == null) ||
+				ArrayUtil.isEmpty(accountEntry.getDomainsArray())) {
+
+				return false;
+			}
+
+			String emailAddress = accountUserDisplay.getEmailAddress();
+
+			emailAddress = StringUtil.toLowerCase(emailAddress);
+
+			int index = emailAddress.indexOf(CharPool.AT);
+
+			String domain = emailAddress.substring(index + 1);
+
+			if (!ArrayUtil.contains(accountEntry.getDomainsArray(), domain)) {
+				return true;
+			}
+		}
+		catch (ConfigurationException configurationException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(configurationException);
+			}
+		}
+
+		return false;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AccountUserRowChecker.class);
 
 	private final long _accountEntryId;
 
