b2a87f82175ea1cdb4fbb06d0e505fe22d19409a
Sandro Chinea <sandrochinea@gmail.com>
2022-04-06 11:54:15 +0100

    LPS-150921 Add feedback message when a parent is hidden from search, allow users to select parent fragment

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment-configuration-fields/HideFromSearchField.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment-configuration-fields/HideFromSearchField.js
@@ -12,60 +12,85 @@
  * details.
  */
 
+import ClayButton from '@clayui/button';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import {useSelectItem} from '../../contexts/ControlsContext';
 import {useDispatch, useSelector} from '../../contexts/StoreContext';
 import selectSegmentsExperienceId from '../../selectors/selectSegmentsExperienceId';
 import updateItemConfig from '../../thunks/updateItemConfig';
 import {CheckboxField} from './CheckboxField';
 
-function hasHiddenAncestor(layoutData, item) {
+function getHiddenAncestorId(layoutData, item) {
 	const parent = layoutData.items[item.parentId];
 
 	if (!parent) {
-		return false;
+		return null;
 	}
 
-	return (
-		parent.config.indexed === false || hasHiddenAncestor(layoutData, parent)
-	);
+	return parent.config.indexed === false
+		? parent.itemId
+		: getHiddenAncestorId(layoutData, parent);
 }
 
 export function HideFromSearchField({item}) {
 	const dispatch = useDispatch();
 	const segmentsExperienceId = useSelector(selectSegmentsExperienceId);
 	const layoutData = useSelector((state) => state.layoutData);
+	const selectItem = useSelectItem();
 
-	const hiddenAncestor = hasHiddenAncestor(layoutData, item);
+	const hiddenAncestorId = getHiddenAncestorId(layoutData, item);
 
 	return (
-		<CheckboxField
-			disabled={hiddenAncestor}
-			field={{
-				label: Liferay.Language.get('hide-from-site-search-results'),
-				name: 'indexed',
-			}}
-			onValueSelect={(name, value) => {
-				const itemConfig = {[name]: !value};
+		<>
+			<CheckboxField
+				disabled={Boolean(hiddenAncestorId)}
+				field={{
+					label: Liferay.Language.get(
+						'hide-from-site-search-results'
+					),
+					name: 'indexed',
+				}}
+				onValueSelect={(name, value) => {
+					const itemConfig = {[name]: !value};
 
-				dispatch(
-					updateItemConfig({
-						itemConfig,
-						itemId: item.itemId,
-						segmentsExperienceId,
-					})
-				);
-			}}
-			title={
-				hiddenAncestor
-					? Liferay.Language.get(
-							'configuration-inherited-from-parent-fragment'
-					  )
-					: null
-			}
-			value={hiddenAncestor || item.config.indexed === false}
-		/>
+					dispatch(
+						updateItemConfig({
+							itemConfig,
+							itemId: item.itemId,
+							segmentsExperienceId,
+						})
+					);
+				}}
+				title={
+					hiddenAncestorId
+						? Liferay.Language.get(
+								'configuration-inherited-from-parent-fragment'
+						  )
+						: null
+				}
+				value={hiddenAncestorId || item.config.indexed === false}
+			/>
+
+			{hiddenAncestorId && (
+				<>
+					<p className="m-0 small text-secondary">
+						{Liferay.Language.get(
+							'this-configuration-is-inherited'
+						)}
+					</p>
+
+					<ClayButton
+						className="p-0 page-editor__hide-feedback-button text-left"
+						displayType="link"
+						onClick={() => selectItem(hiddenAncestorId)}
+					>
+						{Liferay.Language.get('go-to-parent-fragment-to-edit')}
+					</ClayButton>
+				</>
+			)}
+		</>
 	);
 }
 
