b00cb8f9fd0afbda8036ea3ef66d00623bd743ad
Brian Chan <brian.chan@liferay.com>
2022-04-07 20:44:09 -0300

    Revert "LPS-149222 minimize get"
    This reverts commit 15dd58f9442b6444f019e6269a9b395e6413d2b6.

--- portal-impl/src/com/liferay/mail/service/impl/MailServiceImpl.java
+++ portal-impl/src/com/liferay/mail/service/impl/MailServiceImpl.java
@@ -148,13 +148,11 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 	public Session getSession() {
 		long companyId = CompanyThreadLocal.getCompanyId();
 
-		Session session = _sessions.get(companyId);
-
-		if (session != null) {
-			return session;
+		if (_sessions.get(companyId) != null) {
+			return _sessions.get(companyId);
 		}
 
-		session = InfrastructureUtil.getMailSession();
+		Session session = InfrastructureUtil.getMailSession();
 
 		if (!PrefsPropsUtil.getBoolean(
 				companyId, PropsKeys.MAIL_SESSION_MAIL,
