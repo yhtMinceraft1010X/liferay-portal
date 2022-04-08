dc0539e1075a238eea85784d5f1077571364e512
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-04-08 13:28:28 +0200

    LPS-148919 Add an Integration Test to ensure that published content pages appear on the sitemap.

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/layout/admin/util/test/LayoutSitemapURLProviderTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/layout/admin/util/test/LayoutSitemapURLProviderTest.java
@@ -18,6 +18,7 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.admin.kernel.util.SitemapURLProvider;
 import com.liferay.layout.admin.kernel.util.SitemapURLProviderRegistryUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
@@ -35,6 +36,7 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.SAXReader;
@@ -42,6 +44,11 @@ import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.theme.ThemeDisplayFactory;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -110,6 +117,68 @@ public class LayoutSitemapURLProviderTest {
 			_themeDisplay);
 
 		Assert.assertFalse(rootElement.hasContent());
+	}
+
+	@Test
+	public void testLayoutSitemapURLProviderContentLayoutTypePublished()
+		throws Exception {
+
+		SitemapURLProvider layoutSitemapURLProvider =
+			SitemapURLProviderRegistryUtil.getSitemapURLProvider(
+				Layout.class.getName());
+
+		Document document = _saxReader.createDocument();
+
+		document.setXMLEncoding("UTF-8");
+
+		Element rootElement = document.addElement(
+			"urlset", "http://www.sitemaps.org/schemas/sitemap/0.9");
+
+		rootElement.addAttribute(
+			"xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+		rootElement.addAttribute(
+			"xsi:schemaLocation",
+			"http://www.w3.org/1999/xhtml " +
+				"http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd");
+		rootElement.addAttribute("xmlns:xhtml", "http://www.w3.org/1999/xhtml");
+
+		Layout layout = LayoutTestUtil.addTypeContentLayout(_group);
+
+		Layout draftLayout = layout.fetchDraftLayout();
+
+		Assert.assertNotNull(draftLayout);
+
+		_layoutLocalService.updateStatus(
+			TestPropsValues.getUserId(), draftLayout.getPlid(),
+			WorkflowConstants.STATUS_APPROVED, _serviceContext);
+
+		layoutSitemapURLProvider.visitLayout(
+			rootElement, layout.getUuid(), _layoutSet, _themeDisplay);
+
+		Assert.assertTrue(rootElement.hasContent());
+
+		List<Element> elements = rootElement.elements();
+
+		Set<Locale> availableLocales = LanguageUtil.getAvailableLocales(
+			_group.getLiveGroupId());
+
+		Assert.assertEquals(
+			elements.toString(), availableLocales.size(), elements.size());
+
+		String canonicalURL = _portal.getCanonicalURL(
+			_portal.getLayoutFullURL(layout, _themeDisplay), _themeDisplay,
+			layout);
+
+		Map<Locale, String> alternateURLsMap = _portal.getAlternateURLs(
+			canonicalURL, _themeDisplay, layout);
+
+		for (Element element : elements) {
+			String layoutLocalizedURL = element.elementText("loc");
+
+			Assert.assertTrue(
+				layoutLocalizedURL,
+				alternateURLsMap.containsValue(layoutLocalizedURL));
+		}
 	}
 
 	private void _initThemeDisplay() throws Exception {
