fa6ff495112233e553d8dd66d77988fc7ed69df4
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-04-08 13:27:30 +0200

    LPS-149471 Add tests for Active Test dropdown menu icons

--- modules/dxp/apps/segments/segments-experiment-web/test/js/components/SegmentsExperimentsSidebar.es.js
+++ modules/dxp/apps/segments/segments-experiment-web/test/js/components/SegmentsExperimentsSidebar.es.js
@@ -74,7 +74,18 @@ describe('SegmentsExperimentsSidebar', () => {
 		getByText(segmentsExperiment.goal.label);
 
 		getByText('edit');
+		expect(
+			document.querySelectorAll(
+				'.dropdown-item .lexicon-icon.lexicon-icon-pencil'
+			).length
+		).toBe(1);
+
 		getByText('delete');
+		expect(
+			document.querySelectorAll(
+				'.dropdown-item .lexicon-icon.lexicon-icon-trash'
+			).length
+		).toBe(1);
 
 		getByText('review-and-run-test');
 		getByText('view-data-in-analytics-cloud');
