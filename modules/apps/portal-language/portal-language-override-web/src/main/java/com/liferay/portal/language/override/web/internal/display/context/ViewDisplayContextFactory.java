45ac50411a4b4285e3436c4673e8be40410f6ae0
Tina Tian <tina.tian@liferay.com>
2022-04-11 10:41:51 -0700

    LPS-150185 Apply HttpHelperUtil in modules/apps/portal-language

--- modules/apps/portal-language/portal-language-override-web/src/main/java/com/liferay/portal/language/override/web/internal/display/context/ViewDisplayContextFactory.java
+++ modules/apps/portal-language/portal-language-override-web/src/main/java/com/liferay/portal/language/override/web/internal/display/context/ViewDisplayContextFactory.java
@@ -28,7 +28,7 @@ import com.liferay.portal.kernel.portlet.PortletURLUtil;
 import com.liferay.portal.kernel.portlet.SearchOrderByUtil;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
 import com.liferay.portal.kernel.service.permission.PortalPermissionUtil;
-import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.HttpHelperUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -356,7 +356,7 @@ public class ViewDisplayContextFactory {
 					dropdownItem.setActive(
 						Objects.equals(selectedLanguageId, languageId));
 					dropdownItem.setHref(
-						HttpUtil.setParameter(
+						HttpHelperUtil.setParameter(
 							currentURL, namespace + "selectedLanguageId",
 							languageId));
 					dropdownItem.setIcon(icon);
