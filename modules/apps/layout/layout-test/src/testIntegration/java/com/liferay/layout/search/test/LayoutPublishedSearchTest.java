a570259aca62337fa04f927462cfb704be473c1d
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-04-11 18:01:24 +0200

    LPS-151303 Adds a method to update the content layout with inline content.

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/search/test/LayoutPublishedSearchTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/search/test/LayoutPublishedSearchTest.java
@@ -15,7 +15,19 @@
 package com.liferay.layout.search.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkService;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.layout.util.structure.LayoutStructureItem;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
@@ -34,6 +46,7 @@ import com.liferay.portal.search.test.util.IndexerFixture;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
 
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -119,14 +132,93 @@ public class LayoutPublishedSearchTest {
 		_layoutIndexerFixture = new IndexerFixture<>(Layout.class);
 	}
 
+	private void _updateDraftLayout(Layout layout, String value)
+		throws Exception {
+
+		FragmentEntry contributedFragmentEntry =
+			_fragmentCollectionContributorTracker.getFragmentEntry(
+				"BASIC_COMPONENT-heading");
+
+		long defaultSegmentsExperienceId =
+			_segmentsExperienceLocalService.fetchDefaultSegmentsExperienceId(
+				layout.getPlid());
+
+		Layout draftLayout = layout.fetchDraftLayout();
+
+		JSONObject inlineValueJSONObject = JSONUtil.put(
+			"com.liferay.fragment.entry.processor.editable." +
+				"EditableFragmentEntryProcessor",
+			JSONUtil.put(
+				"element-text",
+				JSONUtil.put(
+					"config", JSONFactoryUtil.createJSONObject()
+				).put(
+					"defaultValue", "default value"
+				).put(
+					layout.getDefaultLanguageId(), value
+				)));
+
+		FragmentEntryLink inlineFragmentEntryLink =
+			_fragmentEntryLinkService.addFragmentEntryLink(
+				_group.getGroupId(), 0,
+				contributedFragmentEntry.getFragmentEntryId(),
+				defaultSegmentsExperienceId, draftLayout.getPlid(),
+				contributedFragmentEntry.getCss(),
+				contributedFragmentEntry.getHtml(),
+				contributedFragmentEntry.getJs(),
+				contributedFragmentEntry.getConfiguration(),
+				inlineValueJSONObject.toString(), StringPool.BLANK, 0,
+				contributedFragmentEntry.getFragmentEntryKey(),
+				ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				fetchLayoutPageTemplateStructure(
+					_group.getGroupId(), draftLayout.getPlid());
+
+		LayoutStructure layoutStructure = LayoutStructure.of(
+			layoutPageTemplateStructure.getDefaultSegmentsExperienceData());
+
+		LayoutStructureItem rowStyledLayoutStructureItem =
+			layoutStructure.addRowStyledLayoutStructureItem(
+				layoutStructure.getMainItemId(), 0, 1);
+
+		LayoutStructureItem columnLayoutStructureItem =
+			layoutStructure.addColumnLayoutStructureItem(
+				rowStyledLayoutStructureItem.getItemId(), 0);
+
+		layoutStructure.addFragmentStyledLayoutStructureItem(
+			inlineFragmentEntryLink.getFragmentEntryLinkId(),
+			columnLayoutStructureItem.getItemId(), 0);
+
+		_layoutPageTemplateStructureLocalService.
+			updateLayoutPageTemplateStructureData(
+				_group.getGroupId(), draftLayout.getPlid(),
+				defaultSegmentsExperienceId, layoutStructure.toString());
+	}
+
+	@Inject
+	private FragmentCollectionContributorTracker
+		_fragmentCollectionContributorTracker;
+
+	@Inject
+	private FragmentEntryLinkService _fragmentEntryLinkService;
+
 	@DeleteAfterTestRun
 	private Group _group;
 
 	private IndexerFixture<Layout> _layoutIndexerFixture;
 
+	@Inject
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
 	@Inject(
 		filter = "mvc.command.name=/layout_content_page_editor/publish_layout"
 	)
 	private MVCActionCommand _mvcActionCommand;
+
+	@Inject
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
 
 }
