47be524dffde105c43d7d36dd081feb69760ddbc
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-04-12 12:31:41 +0100

    LPS-151347 Match with RatingsOrderingNavigatorEntry

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/frontend/taglib/form/navigator/ScopeFormNavigatorEntry.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/frontend/taglib/form/navigator/ScopeFormNavigatorEntry.java
@@ -17,12 +17,15 @@ package com.liferay.asset.publisher.web.internal.frontend.taglib.form.navigator;
 import com.liferay.asset.publisher.constants.AssetPublisherConstants;
 import com.liferay.asset.publisher.constants.AssetPublisherPortletKeys;
 import com.liferay.frontend.taglib.form.navigator.FormNavigatorEntry;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+
+import java.util.Objects;
 
 import javax.servlet.ServletContext;
 
@@ -66,10 +69,13 @@ public class ScopeFormNavigatorEntry
 
 		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
 
-		String rootPortletId = PortletIdCodec.decodePortletName(
-			portletDisplay.getPortletName());
+		Portlet portlet = _portletLocalService.getPortletById(
+			themeDisplay.getCompanyId(), portletDisplay.getPortletResource());
 
-		if (rootPortletId.equals(AssetPublisherPortletKeys.RELATED_ASSETS)) {
+		if (Objects.equals(
+				portlet.getRootPortletId(),
+				AssetPublisherPortletKeys.RELATED_ASSETS)) {
+
 			return false;
 		}
 
@@ -89,5 +95,8 @@ public class ScopeFormNavigatorEntry
 	protected String getJspPath() {
 		return "/configuration/scope.jsp";
 	}
+
+	@Reference
+	private PortletLocalService _portletLocalService;
 
 }
