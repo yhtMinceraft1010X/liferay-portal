fca99dc9fbf8a792c078e0b0cfa601cb7fa6874e
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2022-04-12 12:20:30 -0300

    LPS-151410 Create Account system object

---
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/object/system/AccountEntrySystemObjectDefinitionMetadata.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.internal.object.system;
+
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.model.AccountEntryTable;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.system.BaseSystemObjectDefinitionMetadata;
+import com.liferay.object.system.SystemObjectDefinitionMetadata;
+import com.liferay.petra.sql.dsl.Column;
+import com.liferay.petra.sql.dsl.Table;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+@Component(immediate = true, service = SystemObjectDefinitionMetadata.class)
+public class AccountEntrySystemObjectDefinitionMetadata
+	extends BaseSystemObjectDefinitionMetadata {
+
+	@Override
+	public Map<Locale, String> getLabelMap() {
+		return createLabelMap("account");
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return AccountEntry.class;
+	}
+
+	@Override
+	public List<ObjectField> getObjectFields() {
+		return Arrays.asList(
+			createObjectField("Boolean", "Boolean", "active", "active", true),
+			createObjectField("Text", "String", "name", "name", true),
+			createObjectField("Boolean", "Boolean", "root", "root", true),
+			createObjectField("Text", "String", "tax-id", "taxId", false));
+	}
+
+	@Override
+	public Map<Locale, String> getPluralLabelMap() {
+		return createLabelMap("accounts");
+	}
+
+	@Override
+	public Column<?, Long> getPrimaryKeyColumn() {
+		return AccountEntryTable.INSTANCE.accountEntryId;
+	}
+
+	@Override
+	public String getRESTContextPath() {
+		return "headless-commerce-admin-account/v1.0/accounts";
+	}
+
+	@Override
+	public String getScope() {
+		return ObjectDefinitionConstants.SCOPE_COMPANY;
+	}
+
+	@Override
+	public Table getTable() {
+		return AccountEntryTable.INSTANCE;
+	}
+
+	@Override
+	public int getVersion() {
+		return 1;
+	}
+
+}
