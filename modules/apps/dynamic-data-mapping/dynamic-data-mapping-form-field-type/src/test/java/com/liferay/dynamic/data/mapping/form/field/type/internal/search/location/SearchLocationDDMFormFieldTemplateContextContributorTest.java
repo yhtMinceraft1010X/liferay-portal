cd083961a27d957543d07afa7be010f10b803328
Janis Zhang <janis.zhang@liferay.com>
2022-04-14 10:06:21 +0800

    LPS-151445 Use mockito only in SearchLocationDDMFormFieldTemplateContextContributorTe

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/search/location/SearchLocationDDMFormFieldTemplateContextContributorTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/search/location/SearchLocationDDMFormFieldTemplateContextContributorTest.java
@@ -18,35 +18,42 @@ import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
 import com.liferay.dynamic.data.mapping.test.util.DDMFormTestUtil;
 import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
-import com.liferay.google.places.util.GooglePlacesUtil;
 import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoader;
+import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoaderUtil;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PrefsProps;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+import javax.portlet.PortletPreferences;
+
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import org.mockito.Matchers;
-
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.api.support.membermodification.MemberMatcher;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.Mockito;
 
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -56,14 +63,15 @@ import org.springframework.mock.web.MockHttpServletRequest;
 /**
  * @author Rodrigo Paulino
  */
-@PrepareForTest(
-	{GooglePlacesUtil.class, LanguageUtil.class, ResourceBundleUtil.class}
-)
-@RunWith(PowerMockRunner.class)
 public class SearchLocationDDMFormFieldTemplateContextContributorTest {
 
-	@Before
-	public void setUp() throws Exception {
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@BeforeClass
+	public static void setUpClass() {
 		_setUpGooglePlacesUtil();
 		_setUpJSONFactory();
 		_setUpJSONFactoryUtil();
@@ -72,7 +80,7 @@ public class SearchLocationDDMFormFieldTemplateContextContributorTest {
 	}
 
 	@Test
-	public void testGetGooglePlacesAPIKey() throws Exception {
+	public void testGetGooglePlacesAPIKey() {
 		DDMFormField ddmFormField =
 			DDMFormTestUtil.createSearchLocationDDMFormField(
 				DDMFormValuesTestUtil.createLocalizedValue(
@@ -127,6 +135,93 @@ public class SearchLocationDDMFormFieldTemplateContextContributorTest {
 			parameters.get("visibleFields"));
 	}
 
+	private static void _mockGet(String key, String message) {
+		Mockito.when(
+			_language.get(Matchers.any(ResourceBundle.class), Matchers.eq(key))
+		).thenReturn(
+			message
+		);
+
+		_languageUtil.setLanguage(_language);
+	}
+
+	private static void _setUpGooglePlacesUtil() {
+		PortletPreferences portletPreferences = Mockito.mock(
+			PortletPreferences.class);
+		GroupLocalService groupLocalService = Mockito.mock(
+			GroupLocalService.class);
+
+		Mockito.when(
+			portletPreferences.getValue(
+				Mockito.anyString(), Mockito.anyString())
+		).thenReturn(
+			"googlePlacesAPIKey"
+		);
+
+		PrefsPropsUtil prefsPropsUtil = new PrefsPropsUtil();
+
+		PrefsProps prefsProps = Mockito.mock(PrefsProps.class);
+
+		ReflectionTestUtil.setFieldValue(
+			_searchLocationDDMFormFieldTemplateContextContributor,
+			"_groupLocalService", groupLocalService);
+
+		Mockito.when(
+			prefsProps.getPreferences(Mockito.anyLong())
+		).thenReturn(
+			portletPreferences
+		);
+
+		prefsPropsUtil.setPrefsProps(prefsProps);
+	}
+
+	private static void _setUpJSONFactory() {
+		ReflectionTestUtil.setFieldValue(
+			_searchLocationDDMFormFieldTemplateContextContributor,
+			"_jsonFactory", new JSONFactoryImpl());
+	}
+
+	private static void _setUpJSONFactoryUtil() {
+		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
+
+		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
+	}
+
+	private static void _setUpLanguageUtil() {
+		_language = Mockito.mock(Language.class);
+
+		_mockGet("address", "Address");
+		_mockGet("city", "City");
+		_mockGet("country", "Country");
+		_mockGet("postal-code", "Postal Code");
+		_mockGet("state", "State");
+	}
+
+	private static void _setUpResourceBundleUtil() {
+		PortalUtil portalUtil = new PortalUtil();
+
+		Portal portal = Mockito.mock(Portal.class);
+
+		Mockito.when(
+			portal.getResourceBundle(Mockito.any(Locale.class))
+		).thenReturn(
+			ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE
+		);
+		portalUtil.setPortal(portal);
+
+		ResourceBundleLoader resourceBundleLoader = Mockito.mock(
+			ResourceBundleLoader.class);
+
+		ResourceBundleLoaderUtil.setPortalResourceBundleLoader(
+			resourceBundleLoader);
+
+		Mockito.when(
+			resourceBundleLoader.loadResourceBundle(LocaleUtil.BRAZIL)
+		).thenReturn(
+			ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE
+		);
+	}
+
 	private DDMFormFieldRenderingContext _createDDMFormFieldRenderingContext() {
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext =
 			new DDMFormFieldRenderingContext();
@@ -135,7 +230,7 @@ public class SearchLocationDDMFormFieldTemplateContextContributorTest {
 			new MockHttpServletRequest();
 
 		mockHttpServletRequest.setAttribute(
-			WebKeys.THEME_DISPLAY, PowerMockito.mock(ThemeDisplay.class));
+			WebKeys.THEME_DISPLAY, Mockito.mock(ThemeDisplay.class));
 
 		ddmFormFieldRenderingContext.setHttpServletRequest(
 			mockHttpServletRequest);
@@ -146,67 +241,11 @@ public class SearchLocationDDMFormFieldTemplateContextContributorTest {
 		return ddmFormFieldRenderingContext;
 	}
 
-	private void _mockGet(String key, String message) {
-		PowerMockito.when(
-			LanguageUtil.get(
-				Matchers.any(ResourceBundle.class), Matchers.eq(key))
-		).thenReturn(
-			message
-		);
-	}
-
-	private void _setUpGooglePlacesUtil() {
-		PowerMockito.mockStatic(GooglePlacesUtil.class);
-
-		PowerMockito.when(
-			GooglePlacesUtil.getGooglePlacesAPIKey(
-				Matchers.anyLong(), Matchers.eq(_GROUP_ID),
-				Matchers.any(GroupLocalService.class))
-		).thenReturn(
-			"googlePlacesAPIKey"
-		);
-	}
-
-	private void _setUpJSONFactory() throws Exception {
-		MemberMatcher.field(
-			SearchLocationDDMFormFieldTemplateContextContributor.class,
-			"_jsonFactory"
-		).set(
-			_searchLocationDDMFormFieldTemplateContextContributor,
-			new JSONFactoryImpl()
-		);
-	}
-
-	private void _setUpJSONFactoryUtil() {
-		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
-
-		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
-	}
-
-	private void _setUpLanguageUtil() {
-		PowerMockito.mockStatic(LanguageUtil.class);
-
-		_mockGet("address", "Address");
-		_mockGet("city", "City");
-		_mockGet("country", "Country");
-		_mockGet("postal-code", "Postal Code");
-		_mockGet("state", "State");
-	}
-
-	private void _setUpResourceBundleUtil() {
-		PowerMockito.mockStatic(ResourceBundleUtil.class);
-
-		PowerMockito.when(
-			ResourceBundleUtil.getModuleAndPortalResourceBundle(
-				Matchers.any(Locale.class), Matchers.any())
-		).thenReturn(
-			PowerMockito.mock(ResourceBundle.class)
-		);
-	}
-
 	private static final long _GROUP_ID = RandomTestUtil.randomLong();
 
-	private final SearchLocationDDMFormFieldTemplateContextContributor
+	private static Language _language;
+	private static final LanguageUtil _languageUtil = new LanguageUtil();
+	private static final SearchLocationDDMFormFieldTemplateContextContributor
 		_searchLocationDDMFormFieldTemplateContextContributor =
 			new SearchLocationDDMFormFieldTemplateContextContributor();
 
