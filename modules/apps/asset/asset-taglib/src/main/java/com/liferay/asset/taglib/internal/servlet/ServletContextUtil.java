7f2de43c99d918a5a791c8742af71785c64df406
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-04-14 09:21:47 +0100

    LPS-147983 Removes deprecated AssetEntryUsage service

--- modules/apps/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/internal/servlet/ServletContextUtil.java
+++ modules/apps/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/internal/servlet/ServletContextUtil.java
@@ -14,35 +14,20 @@
 
 package com.liferay.asset.taglib.internal.servlet;
 
-import com.liferay.asset.util.AssetEntryUsageRecorder;
 import com.liferay.asset.util.AssetHelper;
 import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
 import com.liferay.fragment.renderer.FragmentRendererTracker;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.Validator;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.ServletContext;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Michael Bradford
  */
 @Component(immediate = true, service = {})
 public class ServletContextUtil {
-
-	public static Map<String, AssetEntryUsageRecorder>
-		getAssetEntryUsageRecorders() {
-
-		return _assetEntryUsageRecorders;
-	}
 
 	public static AssetHelper getAssetHelper() {
 		return _assetHelper;
@@ -60,39 +45,6 @@ public class ServletContextUtil {
 
 	public static ServletContext getServletContext() {
 		return _servletContext;
-	}
-
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void addAssetEntryUsageRecorder(
-		AssetEntryUsageRecorder assetEntryUsageRecorder,
-		Map<String, Object> properties) {
-
-		String modelClassName = GetterUtil.getString(
-			properties.get("model.class.name"));
-
-		if (Validator.isNull(modelClassName)) {
-			return;
-		}
-
-		_assetEntryUsageRecorders.put(modelClassName, assetEntryUsageRecorder);
-	}
-
-	protected void removeAssetEntryUsageRecorder(
-		AssetEntryUsageRecorder assetEntryUsageRecorder,
-		Map<String, Object> properties) {
-
-		String modelClassName = GetterUtil.getString(
-			properties.get("model.class.name"));
-
-		if (Validator.isNull(modelClassName)) {
-			return;
-		}
-
-		_assetEntryUsageRecorders.remove(modelClassName);
 	}
 
 	@Reference(unbind = "-")
@@ -124,8 +76,6 @@ public class ServletContextUtil {
 		_servletContext = servletContext;
 	}
 
-	private static final Map<String, AssetEntryUsageRecorder>
-		_assetEntryUsageRecorders = new ConcurrentHashMap<>();
 	private static AssetHelper _assetHelper;
 	private static FragmentCollectionContributorTracker
 		_fragmentCollectionContributorTracker;
