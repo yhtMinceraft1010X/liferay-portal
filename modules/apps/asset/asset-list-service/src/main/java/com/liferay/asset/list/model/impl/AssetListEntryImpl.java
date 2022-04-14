dfd7021b7ffe1bf1ccbf97117143765b918b26b1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-04-14 09:21:47 +0100

    LPS-152027 Removes deprecated methods and classes (7.2.x and 7.3.x) from asset modules

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/model/impl/AssetListEntryImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/model/impl/AssetListEntryImpl.java
@@ -14,9 +14,6 @@
 
 package com.liferay.asset.list.model.impl;
 
-import com.liferay.asset.kernel.model.AssetEntry;
-import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
-import com.liferay.asset.list.asset.entry.provider.AssetListAssetEntryProvider;
 import com.liferay.asset.list.constants.AssetListEntryTypeConstants;
 import com.liferay.asset.list.model.AssetListEntrySegmentsEntryRel;
 import com.liferay.asset.list.service.AssetListEntrySegmentsEntryRelLocalServiceUtil;
@@ -25,141 +22,12 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
-import java.util.List;
 import java.util.Locale;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @author Pavel Savinov
  */
 public class AssetListEntryImpl extends AssetListEntryBaseImpl {
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), replaced by {@link
-	 *             AssetListAssetEntryProvider#getAssetEntries(AssetListEntry,
-	 *             long)}
-	 */
-	@Deprecated
-	@Override
-	public List<AssetEntry> getAssetEntries(long segmentsEntryId) {
-		AssetListAssetEntryProvider assetListAssetEntryProvider =
-			_getAssetListAssetEntryProvider();
-
-		return assetListAssetEntryProvider.getAssetEntries(
-			this, segmentsEntryId);
-	}
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), replaced by {@link
-	 *             AssetListAssetEntryProvider#getAssetEntries(AssetListEntry,
-	 *             long, int, int)}
-	 */
-	@Deprecated
-	@Override
-	public List<AssetEntry> getAssetEntries(
-		long segmentsEntryId, int start, int end) {
-
-		AssetListAssetEntryProvider assetListAssetEntryProvider =
-			_getAssetListAssetEntryProvider();
-
-		return assetListAssetEntryProvider.getAssetEntries(
-			this, segmentsEntryId, start, end);
-	}
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), replaced by {@link
-	 *             AssetListAssetEntryProvider#getAssetEntries(AssetListEntry,
-	 *             long[])}
-	 */
-	@Deprecated
-	@Override
-	public List<AssetEntry> getAssetEntries(long[] segmentsEntryIds) {
-		AssetListAssetEntryProvider assetListAssetEntryProvider =
-			_getAssetListAssetEntryProvider();
-
-		return assetListAssetEntryProvider.getAssetEntries(
-			this, segmentsEntryIds);
-	}
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), replaced by {@link
-	 *             AssetListAssetEntryProvider#getAssetEntries(AssetListEntry,
-	 *             long[], int, int)}
-	 */
-	@Deprecated
-	@Override
-	public List<AssetEntry> getAssetEntries(
-		long[] segmentsEntryIds, int start, int end) {
-
-		AssetListAssetEntryProvider assetListAssetEntryProvider =
-			_getAssetListAssetEntryProvider();
-
-		return assetListAssetEntryProvider.getAssetEntries(
-			this, segmentsEntryIds, start, end);
-	}
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), replaced by {@link
-	 *             AssetListAssetEntryProvider#getAssetEntriesCount(
-	 *             AssetListEntry, long)}
-	 */
-	@Deprecated
-	@Override
-	public int getAssetEntriesCount(long segmentsEntryId) {
-		AssetListAssetEntryProvider assetListAssetEntryProvider =
-			_getAssetListAssetEntryProvider();
-
-		return assetListAssetEntryProvider.getAssetEntriesCount(
-			this, segmentsEntryId);
-	}
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), replaced by {@link
-	 *             AssetListAssetEntryProvider#getAssetEntriesCount(
-	 *             AssetListEntry, long[])}
-	 */
-	@Deprecated
-	@Override
-	public int getAssetEntriesCount(long[] segmentsEntryIds) {
-		AssetListAssetEntryProvider assetListAssetEntryProvider =
-			_getAssetListAssetEntryProvider();
-
-		return assetListAssetEntryProvider.getAssetEntriesCount(
-			this, segmentsEntryIds);
-	}
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), replaced by {@link
-	 *             AssetListAssetEntryProvider#getAssetEntryQuery(
-	 *             AssetListEntry, long)}
-	 */
-	@Deprecated
-	@Override
-	public AssetEntryQuery getAssetEntryQuery(long segmentsEntryId) {
-		AssetListAssetEntryProvider assetListAssetEntryProvider =
-			_getAssetListAssetEntryProvider();
-
-		return assetListAssetEntryProvider.getAssetEntryQuery(
-			this, segmentsEntryId);
-	}
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), replaced by {@link
-	 *             AssetListAssetEntryProvider#getAssetEntryQuery(
-	 *             AssetListEntry, long[])}
-	 */
-	@Deprecated
-	@Override
-	public AssetEntryQuery getAssetEntryQuery(long[] segmentsEntryIds) {
-		AssetListAssetEntryProvider assetListAssetEntryProvider =
-			_getAssetListAssetEntryProvider();
-
-		return assetListAssetEntryProvider.getAssetEntryQuery(
-			this, segmentsEntryIds);
-	}
 
 	@Override
 	public String getTypeLabel() {
@@ -186,27 +54,6 @@ public class AssetListEntryImpl extends AssetListEntryBaseImpl {
 
 		return StringUtil.appendParentheticalSuffix(
 			getTitle(), group.getName(locale));
-	}
-
-	private AssetListAssetEntryProvider _getAssetListAssetEntryProvider() {
-		return _serviceTracker.getService();
-	}
-
-	private static final ServiceTracker
-		<AssetListAssetEntryProvider, AssetListAssetEntryProvider>
-			_serviceTracker;
-
-	static {
-		Bundle bundle = FrameworkUtil.getBundle(AssetListEntryImpl.class);
-
-		ServiceTracker<AssetListAssetEntryProvider, AssetListAssetEntryProvider>
-			serviceTracker = new ServiceTracker<>(
-				bundle.getBundleContext(), AssetListAssetEntryProvider.class,
-				null);
-
-		serviceTracker.open();
-
-		_serviceTracker = serviceTracker;
 	}
 
 }
