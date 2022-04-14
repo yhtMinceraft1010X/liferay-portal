0a68e5e07ca014a5ab8799405f3f79df3d0995e3
Clarice Brasil <clarice.brasil@liferay.com>
2022-04-14 18:31:29 -0300

    LPS-151330 create new header component for deactivate keys

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/DeactivateKeysTable/components/Header/index.js
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import ClayAlert from '@clayui/alert';
+import {useCallback, useMemo, useState} from 'react';
+import {ALERT_DOWNLOAD_TYPE} from '../../../../utils/constants/alertDownloadType';
+import BadgeFilter from '../../../ActivationKeysTable/components/BadgeFilter';
+import DeactivateButton from '../../../ActivationKeysTable/components/Deactivate';
+import DownloadAlert from '../../../ActivationKeysTable/components/DownloadAlert';
+import Filter from '../../../ActivationKeysTable/components/Filter';
+import {ALERT_ACTIVATION_AGGREGATED_KEYS_DOWNLOAD_TEXT} from '../../../ActivationKeysTable/utils/constants/alertAggregateKeysDownloadText';
+import {DOWNLOADABLE_LICENSE_KEYS} from '../../../ActivationKeysTable/utils/constants/downlodableLicenseKeys';
+
+const DeactivationKeysTableHeader = ({
+	activationKeysByStatusPaginatedChecked,
+	activationKeysState,
+	sessionId,
+	loading,
+	filterState: [filters, setFilters],
+}) => {
+	const [activationKeys, setActivationKeys] = activationKeysState;
+
+	const [status, setStatus] = useState({
+		deactivate: '',
+		download: '',
+	});
+
+	const filterCheckedActivationKeys = useMemo(
+		() =>
+			activationKeysByStatusPaginatedChecked.reduce(
+				(
+					filterCheckedActivationKeysAccumulator,
+					activationKeyChecked,
+					index
+				) =>
+					`${filterCheckedActivationKeysAccumulator}${
+						index > 0 ? '&' : ''
+					}licenseKeyIds=${activationKeyChecked.id}`,
+				''
+			),
+		[activationKeysByStatusPaginatedChecked]
+	);
+
+	const isAbleToDownloadAggregateKeys = useMemo(() => {
+		const [
+			firstActivationKeyChecked,
+			...restActivationKeysChecked
+		] = activationKeysByStatusPaginatedChecked;
+
+		return restActivationKeysChecked.every(
+			(activationKeyChecked) =>
+				DOWNLOADABLE_LICENSE_KEYS.above71DXPVersion(
+					firstActivationKeyChecked,
+					activationKeyChecked
+				) ||
+				DOWNLOADABLE_LICENSE_KEYS.below71DXPVersion(
+					firstActivationKeyChecked,
+					activationKeyChecked
+				)
+		);
+	}, [activationKeysByStatusPaginatedChecked]);
+
+	const handleDeactivate = useCallback(
+		() =>
+			setActivationKeys((previousActivationKeys) =>
+				previousActivationKeys.filter(
+					(activationKey) =>
+						!activationKeysByStatusPaginatedChecked.find(
+							({id}) => activationKey.id === id
+						)
+				)
+			),
+		[activationKeysByStatusPaginatedChecked, setActivationKeys]
+	);
+
+	return (
+		<>
+			<div className="bg-neutral-1 d-flex flex-column pb-1 pt-3 px-3 rounded">
+				<div className="d-flex">
+					<Filter
+						activationKeys={activationKeys}
+						filtersState={[filters, setFilters]}
+					/>
+
+					<div className="align-items-center d-flex ml-auto">
+						<>
+							<p className="font-weight-semi-bold m-0 ml-auto text-neutral-10">
+								{`${activationKeysByStatusPaginatedChecked.length} Keys Selected`}
+							</p>
+
+							<DeactivateButton
+								deactivateKeysStatus={status.deactivate}
+								filterCheckedActivationKeys={
+									filterCheckedActivationKeys
+								}
+								handleDeactivate={handleDeactivate}
+								sessionId={sessionId}
+								setDeactivateKeysStatus={(value) =>
+									setStatus((previousStatus) => ({
+										...previousStatus,
+										deactivate: value,
+									}))
+								}
+							/>
+						</>
+					</div>
+				</div>
+
+				<BadgeFilter
+					activationKeysLength={activationKeys?.length}
+					filtersState={[filters, setFilters]}
+					loading={loading}
+				/>
+			</div>
+
+			{status.download && (
+				<DownloadAlert
+					downloadStatus={status.download}
+					message={
+						ALERT_ACTIVATION_AGGREGATED_KEYS_DOWNLOAD_TEXT[
+							status.download
+						]
+					}
+					setDownloadStatus={(value) =>
+						setStatus((previousStatus) => ({
+							...previousStatus,
+							download: value,
+						}))
+					}
+				/>
+			)}
+
+			{status.deactivate === ALERT_DOWNLOAD_TYPE.success && (
+				<DownloadAlert
+					downloadStatus="success"
+					message="Activation Key(s) were deactivated successfully."
+					setDownloadStatus={(value) =>
+						setStatus((previousStatus) => ({
+							...previousStatus,
+							deactivate: value,
+						}))
+					}
+				/>
+			)}
+
+			{!isAbleToDownloadAggregateKeys && (
+				<ClayAlert className="my-2" displayType="info">
+					To download an aggregate key, select keys with identical
+					<b> Type, Start Date, End Date,</b>
+					and
+					<b> Instance Size</b>
+				</ClayAlert>
+			)}
+		</>
+	);
+};
+
+export default DeactivationKeysTableHeader;
