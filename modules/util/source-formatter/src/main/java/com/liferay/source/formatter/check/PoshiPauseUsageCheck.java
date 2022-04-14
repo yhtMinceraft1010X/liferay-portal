af5dc6638b2a42773b0b4a31b55378eda3ce5fb4
Alan Huang <alan.huang@liferay.com>
2022-04-14 16:14:30 +0800

    LPS-151512 Add PoshiPauseUsageCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/PoshiPauseUsageCheck.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.IOException;
+
+/**
+ * @author Alan Huang
+ */
+public class PoshiPauseUsageCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws IOException {
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			int lineNumber = 0;
+
+			String line = StringPool.BLANK;
+			String previousLine = StringPool.BLANK;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				lineNumber++;
+
+				if (Validator.isNull(line)) {
+					continue;
+				}
+
+				String trimmedLine = StringUtil.trimLeading(line);
+
+				if (trimmedLine.startsWith("Pause(") &&
+					!previousLine.startsWith("//")) {
+
+					addMessage(
+						fileName,
+						"Add a comment with explanation before using 'Pause'",
+						lineNumber);
+				}
+
+				previousLine = trimmedLine;
+			}
+		}
+
+		return content;
+	}
+
+}
