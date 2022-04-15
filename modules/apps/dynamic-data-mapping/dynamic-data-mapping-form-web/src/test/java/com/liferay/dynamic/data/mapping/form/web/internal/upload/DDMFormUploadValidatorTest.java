946cdd745f52598e93b1556da2b90f0fcf006a86
Janis Zhang <janis.zhang@liferay.com>
2022-04-15 09:22:51 +0800

    LPS-151616 Use Mockito only in DDMFormUploadValidatorTest

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/test/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/DDMFormUploadValidatorTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/test/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/DDMFormUploadValidatorTest.java
@@ -20,19 +20,17 @@ import com.liferay.document.library.kernel.exception.InvalidFileException;
 import com.liferay.dynamic.data.mapping.form.web.internal.configuration.DDMFormWebConfiguration;
 import com.liferay.dynamic.data.mapping.form.web.internal.configuration.activator.DDMFormWebConfigurationActivator;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.io.File;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import org.mockito.Mockito;
-
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.api.support.membermodification.MemberMatcher;
 
 /**
  * @author Carolina Barbosa
@@ -43,8 +41,8 @@ public class DDMFormUploadValidatorTest {
 	public static LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUpClass() {
 		_setUpDDMFormWebConfigurationActivator();
 	}
 
@@ -73,10 +71,27 @@ public class DDMFormUploadValidatorTest {
 		_ddmFormUploadValidator.validateFileSize(_mockFile(24), "test.jpg");
 	}
 
+	private static void _setUpDDMFormWebConfigurationActivator() {
+		DDMFormWebConfigurationActivator ddmFormWebConfigurationActivator =
+			new DDMFormWebConfigurationActivator();
+
+		DDMFormWebConfiguration ddmFormWebConfiguration =
+			ConfigurableUtil.createConfigurable(
+				DDMFormWebConfiguration.class, new HashMapDictionary<>());
+
+		ReflectionTestUtil.setFieldValue(
+			ddmFormWebConfigurationActivator, "_formWebConfiguration",
+			ddmFormWebConfiguration);
+
+		ReflectionTestUtil.setFieldValue(
+			_ddmFormUploadValidator, "_ddmFormWebConfigurationActivator",
+			ddmFormWebConfigurationActivator);
+	}
+
 	private File _mockFile(long length) {
 		File file = Mockito.mock(File.class);
 
-		PowerMockito.when(
+		Mockito.when(
 			file.length()
 		).thenReturn(
 			length * _FILE_LENGTH_MB
@@ -85,30 +100,9 @@ public class DDMFormUploadValidatorTest {
 		return file;
 	}
 
-	private void _setUpDDMFormWebConfigurationActivator() throws Exception {
-		DDMFormWebConfigurationActivator ddmFormWebConfigurationActivator =
-			Mockito.mock(DDMFormWebConfigurationActivator.class);
-
-		MemberMatcher.field(
-			DDMFormUploadValidator.class, "_ddmFormWebConfigurationActivator"
-		).set(
-			_ddmFormUploadValidator, ddmFormWebConfigurationActivator
-		);
-
-		DDMFormWebConfiguration ddmFormWebConfiguration =
-			ConfigurableUtil.createConfigurable(
-				DDMFormWebConfiguration.class, new HashMapDictionary<>());
-
-		PowerMockito.when(
-			ddmFormWebConfigurationActivator.getDDMFormWebConfiguration()
-		).thenReturn(
-			ddmFormWebConfiguration
-		);
-	}
-
 	private static final long _FILE_LENGTH_MB = 1024 * 1024;
 
-	private final DDMFormUploadValidator _ddmFormUploadValidator =
+	private static final DDMFormUploadValidator _ddmFormUploadValidator =
 		new DDMFormUploadValidator();
 
 }
