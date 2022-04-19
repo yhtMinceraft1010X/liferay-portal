f12dc2559e914e4425df88c019812ca1660e6ef7
shuyangzhou <shuyang.zhou@liferay.com>
2022-04-19 16:21:51 -0700

    LPS-152198 Move test setup to test.

--- modules/apps/static/portal-osgi-web/portal-osgi-web-wab-generator-impl/src/main/java/com/liferay/portal/osgi/web/wab/generator/internal/connection/WabURLConnection.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-wab-generator-impl/src/main/java/com/liferay/portal/osgi/web/wab/generator/internal/connection/WabURLConnection.java
@@ -16,16 +16,11 @@ package com.liferay.portal.osgi.web.wab.generator.internal.connection;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
-import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.osgi.web.wab.generator.WabGenerator;
-import com.liferay.portal.util.FastDateFormatFactoryImpl;
-import com.liferay.portal.util.FileImpl;
-import com.liferay.portal.util.HttpImpl;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -52,8 +47,6 @@ public class WabURLConnection extends URLConnection {
 
 		_classLoader = classLoader;
 		_wabGenerator = wabGenerator;
-
-		_wireSpringUtils();
 	}
 
 	@Override
@@ -129,27 +122,6 @@ public class WabURLConnection extends URLConnection {
 		StreamUtil.transfer(url.openStream(), new FileOutputStream(file));
 
 		return file;
-	}
-
-	private void _wireSpringUtils() {
-		if (FastDateFormatFactoryUtil.getFastDateFormatFactory() == null) {
-			FastDateFormatFactoryUtil instance =
-				new FastDateFormatFactoryUtil();
-
-			instance.setFastDateFormatFactory(new FastDateFormatFactoryImpl());
-		}
-
-		if (FileUtil.getFile() == null) {
-			FileUtil instance = new FileUtil();
-
-			instance.setFile(new FileImpl());
-		}
-
-		if (HttpUtil.getHttp() == null) {
-			HttpUtil instance = new HttpUtil();
-
-			instance.setHttp(new HttpImpl());
-		}
 	}
 
 	private final ClassLoader _classLoader;
