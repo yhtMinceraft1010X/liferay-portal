ee2cc44fa1f94654db6c7244ff90263ba319acfe
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-04-19 16:45:44 +0200

    LPS-150990 Not used

--- modules/apps/cookies/cookies-banner-web/src/main/java/com/liferay/cookies/banner/web/internal/portlet/CookiesBannerConfigurationPortlet.java
+++ modules/apps/cookies/cookies-banner-web/src/main/java/com/liferay/cookies/banner/web/internal/portlet/CookiesBannerConfigurationPortlet.java
@@ -30,7 +30,6 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-cookies-banner-configuration",
 		"com.liferay.portlet.display-category=category.tools",
-		"com.liferay.portlet.header-portlet-css=/cookies_banner_configuration/css/main.css",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
