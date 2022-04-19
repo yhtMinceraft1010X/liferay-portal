ef52aafadcf47e903abb02f2c75df00d53bf5882
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-04-19 16:04:23 +0100

    LPS-149984 Allow hide form elements when we are indexing

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/mapper/FormLayoutStructureItemMapper.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/mapper/FormLayoutStructureItemMapper.java
@@ -57,6 +57,7 @@ public class FormLayoutStructureItemMapper
 									formStyledLayoutStructureItem);
 							}
 						};
+						indexed = formStyledLayoutStructureItem.isIndexed();
 
 						setFragmentStyle(
 							() -> {
