6005bc35c76b9e44f28153581ba11e41a56aaa64
Matija Petanjek <matija.petanjek@liferay.com>
2022-04-20 00:06:43 +0200

    COMMERCE-9073 batch-planner-test - update tests

--- modules/apps/batch-planner/batch-planner-test/src/testIntegration/java/com/liferay/batch/planner/service/test/BatchPlannerPlanServiceTest.java
+++ modules/apps/batch-planner/batch-planner-test/src/testIntegration/java/com/liferay/batch/planner/service/test/BatchPlannerPlanServiceTest.java
@@ -19,16 +19,13 @@ import com.liferay.batch.planner.constants.BatchPlannerPlanConstants;
 import com.liferay.batch.planner.exception.BatchPlannerPlanExternalTypeException;
 import com.liferay.batch.planner.exception.BatchPlannerPlanInternalClassNameException;
 import com.liferay.batch.planner.exception.BatchPlannerPlanNameException;
-import com.liferay.batch.planner.exception.DuplicateBatchPlannerPlanException;
 import com.liferay.batch.planner.exception.RequiredBatchPlannerPlanException;
 import com.liferay.batch.planner.model.BatchPlannerPlan;
 import com.liferay.batch.planner.service.BatchPlannerPlanService;
-import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -111,8 +108,7 @@ public class BatchPlannerPlanServiceTest {
 		}
 		catch (BatchPlannerPlanNameException batchPlannerPlanNameException) {
 			Assert.assertEquals(
-				"Batch planner plan name is null for company " +
-					TestPropsValues.getCompanyId(),
+				"Batch planner plan name is null",
 				batchPlannerPlanNameException.getMessage());
 		}
 
@@ -131,25 +127,6 @@ public class BatchPlannerPlanServiceTest {
 			Assert.assertEquals(
 				"Batch planner plan name must not be longer than " + maxLength,
 				batchPlannerPlanNameException.getMessage());
-		}
-
-		try {
-			_batchPlannerPlanService.addBatchPlannerPlan(
-				true, BatchPlannerPlanConstants.EXTERNAL_TYPE_CSV,
-				"/" + RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), name, null, false);
-
-			Assert.fail();
-		}
-		catch (DuplicateBatchPlannerPlanException
-					duplicateBatchPlannerPlanException) {
-
-			Assert.assertEquals(
-				StringBundler.concat(
-					"Batch planner plan name \"", name,
-					"\" already exists for company ",
-					TestPropsValues.getCompanyId()),
-				duplicateBatchPlannerPlanException.getMessage());
 		}
 
 		_batchPlannerPlanService.deleteBatchPlannerPlan(
