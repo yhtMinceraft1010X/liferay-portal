a5627290c582ef4ce5eaea9b6ffadc04261abd9c
Matija Petanjek <matija.petanjek@liferay.com>
2022-04-19 19:42:31 +0200

    COMMERCE-9073 batch-planner-service - remove unique index

--- modules/apps/batch-planner/batch-planner-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/batch-planner/batch-planner-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,7 +1,6 @@
 create unique index IX_E025DC1A on BatchPlannerMapping (batchPlannerPlanId, externalFieldName[$COLUMN_LENGTH:75$], internalFieldName[$COLUMN_LENGTH:75$]);
 
 create index IX_18CD7477 on BatchPlannerPlan (companyId, export, template);
-create unique index IX_221A54A0 on BatchPlannerPlan (companyId, name[$COLUMN_LENGTH:75$]);
 create index IX_F2F05D4F on BatchPlannerPlan (companyId, template);
 create index IX_874FA8DB on BatchPlannerPlan (companyId, userId);
 
