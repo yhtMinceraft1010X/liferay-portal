3971173eefe8e4cb6c5be3e0cfa7a9e0f4bb94ba
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-04-21 17:23:24 +0200

    LPS-151747 Adds needed criteria

--- modules/apps/asset/asset-vocabulary-item-selector-api/src/main/java/com/liferay/asset/vocabulary/item/selector/criterion/AssetVocabularyItemSelectorCriterion.java
+++ modules/apps/asset/asset-vocabulary-item-selector-api/src/main/java/com/liferay/asset/vocabulary/item/selector/criterion/AssetVocabularyItemSelectorCriterion.java
@@ -19,6 +19,53 @@ import com.liferay.item.selector.BaseItemSelectorCriterion;
 /**
  * @author Lourdes Fernández Besada
  */
-public class AssetVocabularyItemSelectorCriterion extends
-	BaseItemSelectorCriterion {
+public class AssetVocabularyItemSelectorCriterion
+	extends BaseItemSelectorCriterion {
+
+	public AssetVocabularyItemSelectorCriterion() {
+		_includeInternalVocabularies = true;
+	}
+
+	public long getGroupId() {
+		return _groupId;
+	}
+
+	public boolean isIncludeAncestorSiteAndDepotGroupIds() {
+		return _includeAncestorSiteAndDepotGroupIds;
+	}
+
+	public boolean isIncludeInternalVocabularies() {
+		return _includeInternalVocabularies;
+	}
+
+	public boolean isMultiSelection() {
+		return _multiSelection;
+	}
+
+	public void setGroupId(long groupId) {
+		_groupId = groupId;
+	}
+
+	public void setIncludeAncestorSiteAndDepotGroupIds(
+		boolean includeAncestorSiteAndDepotGroupIds) {
+
+		_includeAncestorSiteAndDepotGroupIds =
+			includeAncestorSiteAndDepotGroupIds;
+	}
+
+	public void setIncludeInternalVocabularies(
+		boolean includeInternalVocabularies) {
+
+		_includeInternalVocabularies = includeInternalVocabularies;
+	}
+
+	public void setMultiSelection(boolean multiSelection) {
+		_multiSelection = multiSelection;
+	}
+
+	private long _groupId;
+	private boolean _includeAncestorSiteAndDepotGroupIds;
+	private boolean _includeInternalVocabularies;
+	private boolean _multiSelection;
+
 }
