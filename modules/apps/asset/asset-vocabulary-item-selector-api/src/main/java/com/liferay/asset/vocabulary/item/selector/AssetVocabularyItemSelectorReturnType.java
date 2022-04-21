0aa2317dc20dc953bfea98e868247a7c1a96f6af
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-04-21 17:23:34 +0200

    LPS-151747 Adds javadoc for easy use

--- modules/apps/asset/asset-vocabulary-item-selector-api/src/main/java/com/liferay/asset/vocabulary/item/selector/AssetVocabularyItemSelectorReturnType.java
+++ modules/apps/asset/asset-vocabulary-item-selector-api/src/main/java/com/liferay/asset/vocabulary/item/selector/AssetVocabularyItemSelectorReturnType.java
@@ -17,6 +17,24 @@ package com.liferay.asset.vocabulary.item.selector;
 import com.liferay.item.selector.ItemSelectorReturnType;
 
 /**
+ * This return type should return the following information of a vocabulary as a
+ * JSON object:
+ *
+ * <ul>
+ * <li>
+ * <code>groupId</code>: The vocabularyId of the selected vocabulary
+ * </li>
+ * <li>
+ * <code>title</code>: The localized title of the selected vocabulary
+ * </li>
+ * <li>
+ * <code>uuid</code>: The uuid of the selected vocabulary
+ * </li>
+ * <li>
+ * <code>vocabularyId</code>: The vocabularyId of the selected vocabulary
+ * </li>
+ * </ul>
+ *
  * @author Lourdes Fernández Besada
  */
 public class AssetVocabularyItemSelectorReturnType
