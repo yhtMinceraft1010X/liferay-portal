436797818431c7a890afffd6a495706b1e68391b
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-04-21 17:23:27 +0200

    LPS-151747 Create AssetVocabularyItemSelectorView

---
+++ modules/apps/asset/asset-vocabulary-item-selector-web/src/main/java/com/liferay/asset/vocabulary/item/selector/web/internal/AssetVocabularyItemSelectorView.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.vocabulary.item.selector.web.internal;
+
+import com.liferay.asset.vocabulary.item.selector.AssetVocabularyItemSelectorReturnType;
+import com.liferay.asset.vocabulary.item.selector.criterion.AssetVocabularyItemSelectorCriterion;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = ItemSelectorView.class)
+public class AssetVocabularyItemSelectorView
+	implements ItemSelectorView<AssetVocabularyItemSelectorCriterion> {
+
+	@Override
+	public Class<AssetVocabularyItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return AssetVocabularyItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return LanguageUtil.get(locale, "vocabularies");
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			AssetVocabularyItemSelectorCriterion
+				assetVocabularyItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher("/select_asset_vocabulary.jsp");
+
+		requestDispatcher.include(servletRequest, servletResponse);
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.singletonList(
+			new AssetVocabularyItemSelectorReturnType());
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.asset.vocabulary.item.selector.web)"
+	)
+	private ServletContext _servletContext;
+
+}
