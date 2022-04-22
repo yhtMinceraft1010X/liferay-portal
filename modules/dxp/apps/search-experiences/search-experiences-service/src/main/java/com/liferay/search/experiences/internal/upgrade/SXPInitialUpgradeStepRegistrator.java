aff76619e245607d7c124ce89692d08b2b67c7c2
Petteri Karttunen <petteri.karttunen@liferay.com>
2022-04-22 09:45:34 +0300

    LPS-151266 search-experiences-service: Split the UpgradeStepRegistrator in two

---
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/upgrade/SXPInitialUpgradeStepRegistrator.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.internal.upgrade;
+
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.search.experiences.internal.model.listener.CompanyModelListener;
+import com.liferay.search.experiences.internal.search.SXPElementSearchRegistrar;
+import com.liferay.search.experiences.internal.upgrade.v1_0_0.SXPElementUpgradeProcess;
+import com.liferay.search.experiences.service.SXPElementLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Shuyang Zhou
+ */
+@Component(
+	enabled = true, immediate = true, service = UpgradeStepRegistrator.class
+)
+public class SXPInitialUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.registerInitialUpgradeSteps(
+			new SXPElementUpgradeProcess(
+				_companyLocalService, _companyModelListener,
+				_sxpElementLocalService));
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private CompanyModelListener _companyModelListener;
+
+	@Reference
+	private SXPElementLocalService _sxpElementLocalService;
+
+	@Reference
+	private SXPElementSearchRegistrar _sxpElementSearchRegistrar;
+
+}
--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/upgrade/SXPUpgradeStepRegistrator.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/upgrade/SXPUpgradeStepRegistrator.java
@@ -16,14 +16,12 @@ package com.liferay.search.experiences.internal.upgrade;
 
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.search.experiences.internal.upgrade.v1_1_0.SXPBlueprintUpgradeProcess;
+import com.liferay.search.experiences.internal.upgrade.v1_1_0.SXPElementUpgradeProcess;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
- * @author Shuyang Zhou
+ * @author Petteri Karttunen
  */
 @Component(
 	enabled = true, immediate = true, service = UpgradeStepRegistrator.class
@@ -32,22 +30,9 @@ public class SXPUpgradeStepRegistrator implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
-		if (_sxUpgradeStepRegistratorHelper != null) {
-			_sxUpgradeStepRegistratorHelper.registerInitialUpgradeSteps(
-				registry);
-		}
-
 		registry.register(
-			"1.0.0", "1.1.0",
-			new com.liferay.search.experiences.internal.upgrade.v1_1_0.
-				SXPElementUpgradeProcess(),
+			"1.0.0", "1.1.0", new SXPElementUpgradeProcess(),
 			new SXPBlueprintUpgradeProcess());
 	}
-
-	@Reference(
-		cardinality = ReferenceCardinality.OPTIONAL,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	private SXUpgradeStepRegistratorHelper _sxUpgradeStepRegistratorHelper;
 
 }
