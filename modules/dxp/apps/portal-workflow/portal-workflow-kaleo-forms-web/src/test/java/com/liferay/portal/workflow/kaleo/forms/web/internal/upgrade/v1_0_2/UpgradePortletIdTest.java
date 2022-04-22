e4073c9e905398b437db00be1074baa42eed81e4
Janis Zhang <janis.zhang@liferay.com>
2022-04-22 09:54:42 +0800

    LPS-151993 Avoid mock in UpgradePortletIdTest

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-forms-web/src/test/java/com/liferay/portal/workflow/kaleo/forms/web/internal/upgrade/v1_0_2/UpgradePortletIdTest.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-forms-web/src/test/java/com/liferay/portal/workflow/kaleo/forms/web/internal/upgrade/v1_0_2/UpgradePortletIdTest.java
@@ -18,21 +18,24 @@ import com.liferay.portal.kernel.test.util.PropsTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Inácio Nery
  */
-@RunWith(PowerMockRunner.class)
-public class UpgradePortletIdTest extends PowerMockito {
+public class UpgradePortletIdTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
 
 	@BeforeClass
 	public static void setUpClass() {
