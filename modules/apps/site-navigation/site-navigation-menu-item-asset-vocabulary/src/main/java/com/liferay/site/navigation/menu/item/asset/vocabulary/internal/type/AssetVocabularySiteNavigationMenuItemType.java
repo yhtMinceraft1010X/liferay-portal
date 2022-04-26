84d094db4e9c9aafd72305028f6ec2a5dad28c18
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-04-26 14:23:33 +0200

    LPS-146816 Handle export-import

--- modules/apps/site-navigation/site-navigation-menu-item-asset-vocabulary/src/main/java/com/liferay/site/navigation/menu/item/asset/vocabulary/internal/type/AssetVocabularySiteNavigationMenuItemType.java
+++ modules/apps/site-navigation/site-navigation-menu-item-asset-vocabulary/src/main/java/com/liferay/site/navigation/menu/item/asset/vocabulary/internal/type/AssetVocabularySiteNavigationMenuItemType.java
@@ -14,14 +14,24 @@
 
 package com.liferay.site.navigation.menu.item.asset.vocabulary.internal.type;
 
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
+import com.liferay.portal.kernel.xml.Element;
 import com.liferay.site.navigation.menu.item.layout.constants.SiteNavigationMenuItemTypeConstants;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
 import com.liferay.site.navigation.type.SiteNavigationMenuItemType;
 import com.liferay.site.navigation.type.SiteNavigationMenuItemTypeContext;
 
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
@@ -43,6 +53,38 @@ public class AssetVocabularySiteNavigationMenuItemType
 	implements SiteNavigationMenuItemType {
 
 	@Override
+	public boolean exportData(
+		PortletDataContext portletDataContext,
+		Element siteNavigationMenuItemElement,
+		SiteNavigationMenuItem siteNavigationMenuItem) {
+
+		UnicodeProperties typeSettingsUnicodeProperties =
+			UnicodePropertiesBuilder.fastLoad(
+				siteNavigationMenuItem.getTypeSettings()
+			).build();
+
+		long vocabularyId = GetterUtil.getLong(
+			typeSettingsUnicodeProperties.get("classPK"));
+
+		AssetVocabulary assetVocabulary =
+			_assetVocabularyLocalService.fetchAssetVocabulary(vocabularyId);
+
+		if (assetVocabulary == null) {
+			return false;
+		}
+
+		siteNavigationMenuItemElement.addAttribute(
+			"vocabulary-id", String.valueOf(vocabularyId));
+
+		portletDataContext.addReferenceElement(
+			siteNavigationMenuItem, siteNavigationMenuItemElement,
+			assetVocabulary, PortletDataContext.REFERENCE_TYPE_DEPENDENCY,
+			false);
+
+		return true;
+	}
+
+	@Override
 	public String getLabel(Locale locale) {
 		return LanguageUtil.get(locale, "vocabulary");
 	}
@@ -50,6 +92,50 @@ public class AssetVocabularySiteNavigationMenuItemType
 	@Override
 	public String getType() {
 		return SiteNavigationMenuItemTypeConstants.ASSET_VOCABULARY;
+	}
+
+	@Override
+	public boolean importData(
+		PortletDataContext portletDataContext,
+		SiteNavigationMenuItem siteNavigationMenuItem,
+		SiteNavigationMenuItem importedSiteNavigationMenuItem) {
+
+		Element element = portletDataContext.getImportDataElement(
+			siteNavigationMenuItem);
+
+		long classPK = GetterUtil.getLong(
+			element.attributeValue("vocabulary-id"));
+
+		if (classPK <= 0) {
+			return false;
+		}
+
+		long newClassPK = MapUtil.getLong(
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				AssetVocabulary.class.getName()),
+			classPK, classPK);
+
+		AssetVocabulary assetVocabulary =
+			_assetVocabularyLocalService.fetchAssetVocabulary(newClassPK);
+
+		if (assetVocabulary == null) {
+			return false;
+		}
+
+		importedSiteNavigationMenuItem.setTypeSettings(
+			UnicodePropertiesBuilder.fastLoad(
+				siteNavigationMenuItem.getTypeSettings()
+			).put(
+				"classPK", String.valueOf(newClassPK)
+			).put(
+				"groupId", String.valueOf(assetVocabulary.getGroupId())
+			).put(
+				"title", assetVocabulary.getTitle(LocaleUtil.getSiteDefault())
+			).put(
+				"type", "vocabulary"
+			).buildString());
+
+		return true;
 	}
 
 	@Override
@@ -62,6 +148,9 @@ public class AssetVocabularySiteNavigationMenuItemType
 
 		return false;
 	}
+
+	@Reference
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
 
 	@Reference(
 		target = "(osgi.web.symbolicname=com.liferay.site.navigation.menu.item.asset.vocabulary)",
