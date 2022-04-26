05bb24dcf5cb196a59e3dc91cfbc3c8a8b86f143
MariMalta <maltamariana123@gmail.com>
2022-04-26 14:50:28 -0300

    LPS-151713/LPS-151714 Pass Elements array to tabs component
    Co-authored-by: Carlos Montenegro <cgmonte@gmail.com>

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/EditObjectValidation.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/EditObjectValidation.tsx
@@ -56,6 +56,7 @@ function closeSidePanel() {
 
 export default function EditObjectValidation({
 	objectValidationRule: initialValues,
+	objectValidationRuleElements,
 	readOnly,
 }: IProps) {
 	const [activeIndex, setActiveIndex] = useState<number>(0);
@@ -125,6 +126,9 @@ export default function EditObjectValidation({
 									errors={errors}
 									handleChange={handleChange}
 									locales={locales}
+									objectValidationRuleElements={
+										objectValidationRuleElements
+									}
 									setValues={setValues}
 									values={values}
 								/>
@@ -154,5 +158,6 @@ export default function EditObjectValidation({
 
 interface IProps {
 	objectValidationRule: ObjectValidation;
+	objectValidationRuleElements: ObjectValidationRuleElement[];
 	readOnly: boolean;
 }
