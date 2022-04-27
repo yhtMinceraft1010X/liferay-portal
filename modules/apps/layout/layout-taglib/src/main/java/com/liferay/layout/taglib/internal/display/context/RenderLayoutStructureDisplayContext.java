4c05461b4639ef3a906051c5a93c7dee1be86a02
Rubén Pulido <ruben.pulido@liferay.com>
2022-04-27 16:42:53 +0200

    LPS-152245 Store collectionStyledLayoutStructureItemIds in request instead of in an instance variable.

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderLayoutStructureDisplayContext.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderLayoutStructureDisplayContext.java
@@ -99,7 +99,19 @@ public class RenderLayoutStructureDisplayContext {
 	}
 
 	public List<String> getCollectionStyledLayoutStructureItemIds() {
-		return _collectionStyledLayoutStructureItemIds;
+		List<String> collectionStyledLayoutStructureItemIds =
+			(List<String>)_httpServletRequest.getAttribute(
+				_COLLECTION_STYLED_LAYOUT_STRUCTURE_ITEM_IDS);
+
+		if (collectionStyledLayoutStructureItemIds == null) {
+			collectionStyledLayoutStructureItemIds = new ArrayList<>();
+
+			_httpServletRequest.setAttribute(
+				_COLLECTION_STYLED_LAYOUT_STRUCTURE_ITEM_IDS,
+				collectionStyledLayoutStructureItemIds);
+		}
+
+		return collectionStyledLayoutStructureItemIds;
 	}
 
 	public String getContainerLinkHref(
@@ -1167,11 +1179,12 @@ public class RenderLayoutStructureDisplayContext {
 		return _segmentsEntryIds;
 	}
 
+	private static final String _COLLECTION_STYLED_LAYOUT_STRUCTURE_ITEM_IDS =
+		"COLLECTION_STYLED_LAYOUT_STRUCTURE_ITEM_IDS";
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		RenderLayoutStructureDisplayContext.class);
 
-	private final List<String> _collectionStyledLayoutStructureItemIds =
-		new ArrayList<>();
 	private JSONObject _frontendTokensJSONObject;
 	private final HttpServletRequest _httpServletRequest;
 	private final LayoutStructure _layoutStructure;
