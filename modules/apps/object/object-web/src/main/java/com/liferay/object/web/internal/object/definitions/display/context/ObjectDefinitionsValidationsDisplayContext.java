26dd50a2843fad6ae734b84e34fa90d5930a24d2
Brian Chan <brian.chan@liferay.com>
2022-04-27 16:23:04 -0300

    Revert "LPS-151713 Make it private"
    This reverts commit 70791867bc23596fca262697e037ea0dfc7aa1f8.

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ObjectDefinitionsValidationsDisplayContext.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ObjectDefinitionsValidationsDisplayContext.java
@@ -139,7 +139,7 @@ public class ObjectDefinitionsValidationsDisplayContext
 		).build();
 	}
 
-	private enum DDMExpressionFunction {
+	public enum DDMExpressionFunction {
 
 		CONCAT("concat(parameters)", "concat"),
 		CONTAINS("contains(field_name, parameter)", "contains"),
@@ -195,7 +195,7 @@ public class ObjectDefinitionsValidationsDisplayContext
 
 	}
 
-	private enum DDMExpressionOperator {
+	public enum DDMExpressionOperator {
 
 		AND("AND", "and"), DIVIDED_BY("field_name / field_name2", "divided-by"),
 		MINUS("field_name - field_name2", "minus"), OR("OR", "or"),
