1ebf5c0c7b3b4c988109c2ce9c96b753184e05ee
Tina Tian <tina.tian@liferay.com>
2022-04-28 13:15:00 -0700

    LPS-152249 Remove dependecy on ClientExtensionEntryLocalService, prepare for next

--- modules/apps/client-extension/client-extension-web/src/main/java/com/liferay/client/extension/web/internal/upgrade/ClientExtensionWebUpgrade.java
+++ modules/apps/client-extension/client-extension-web/src/main/java/com/liferay/client/extension/web/internal/upgrade/ClientExtensionWebUpgrade.java
@@ -14,14 +14,19 @@
 
 package com.liferay.client.extension.web.internal.upgrade;
 
-import com.liferay.client.extension.service.ClientExtensionEntryLocalService;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.BasePortletIdUpgradeProcess;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
-import com.liferay.portal.vulcan.util.TransformUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Iván Zaera Avellón
@@ -39,7 +44,7 @@ public class ClientExtensionWebUpgrade implements UpgradeStepRegistrator {
 				@Override
 				protected String[][] getRenamePortletIdsArray() {
 					return _getRenamePortletIdsArray(
-						"remote_app_",
+						connection, "remote_app_",
 						"com_liferay_remote_app_web_internal_portlet_" +
 							"RemoteAppEntryPortlet_");
 				}
@@ -71,6 +76,7 @@ public class ClientExtensionWebUpgrade implements UpgradeStepRegistrator {
 				@Override
 				protected String[][] getRenamePortletIdsArray() {
 					return _getRenamePortletIdsArray(
+						connection,
 						"com_liferay_remote_app_web_internal_portlet_" +
 							"RemoteAppEntryPortlet_",
 						"com_liferay_client_extension_web_internal_portlet_" +
@@ -81,21 +87,34 @@ public class ClientExtensionWebUpgrade implements UpgradeStepRegistrator {
 	}
 
 	private String[][] _getRenamePortletIdsArray(
-		String oldPortletIdPrefix, String newPortletIdPrefix) {
+		Connection connection, String oldPortletIdPrefix,
+		String newPortletIdPrefix) {
 
-		return TransformUtil.transformToArray(
-			_clientExtensionEntryLocalService.getClientExtensionEntries(
-				QueryUtil.ALL_POS, QueryUtil.ALL_POS),
-			clientExtensionEntry -> new String[] {
-				oldPortletIdPrefix +
-					clientExtensionEntry.getClientExtensionEntryId(),
-				newPortletIdPrefix +
-					clientExtensionEntry.getClientExtensionEntryId()
-			},
-			String[].class);
+		List<String[]> portletIds = new ArrayList<>();
+
+		try (Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(
+				"select clientExtensionEntryId FROM ClientExtensionEntry ")) {
+
+			while (resultSet.next()) {
+				long clientExtensionEntryId = resultSet.getLong(
+					"clientExtensionEntryId");
+
+				portletIds.add(
+					new String[] {
+						oldPortletIdPrefix + clientExtensionEntryId,
+						newPortletIdPrefix + clientExtensionEntryId
+					});
+			}
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+		}
+
+		return portletIds.toArray(new String[0][0]);
 	}
 
-	@Reference
-	private ClientExtensionEntryLocalService _clientExtensionEntryLocalService;
+	private static final Log _log = LogFactoryUtil.getLog(
+		ClientExtensionWebUpgrade.class);
 
 }
