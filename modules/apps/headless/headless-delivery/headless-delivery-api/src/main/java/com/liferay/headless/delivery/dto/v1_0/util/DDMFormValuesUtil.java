1afc6157af8a0f95218c10abef0d85e129e661bd
Carlos Correa <carlos.correa@liferay.com>
2022-04-28 18:36:02 +0200

    LPS-151945 Fix available locales and default locale of DDMFormValues

--- modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/DDMFormValuesUtil.java
+++ modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/DDMFormValuesUtil.java
@@ -30,18 +30,22 @@ import com.liferay.headless.delivery.dto.v1_0.ContentField;
 import com.liferay.journal.service.JournalArticleService;
 import com.liferay.petra.function.UnsafeFunction;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.vulcan.util.TransformUtil;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -76,8 +80,8 @@ public class DDMFormValuesUtil {
 	}
 
 	public static DDMFormValues toDDMFormValues(
-		ContentField[] contentFields, DDMForm ddmForm,
-		DLAppService dlAppService, long groupId,
+		Set<Locale> availableLocales, ContentField[] contentFields,
+		DDMForm ddmForm, DLAppService dlAppService, long groupId,
 		JournalArticleService journalArticleService,
 		LayoutLocalService layoutLocalService, Locale locale,
 		List<DDMFormField> rootDDMFormFields) {
@@ -92,7 +96,8 @@ public class DDMFormValuesUtil {
 
 		return new DDMFormValues(ddmForm) {
 			{
-				setAvailableLocales(ddmForm.getAvailableLocales());
+				setAvailableLocales(
+					_getAvailableLocales(availableLocales, ddmForm, groupId));
 				setDDMFormFieldValues(
 					_flattenDDMFormFieldValues(
 						rootDDMFormFields,
@@ -102,7 +107,7 @@ public class DDMFormValuesUtil {
 							ddmFormField, dlAppService, groupId,
 							journalArticleService, layoutLocalService,
 							locale)));
-				setDefaultLocale(ddmForm.getDefaultLocale());
+				setDefaultLocale(_getDefaultLocale(ddmForm, locale));
 			}
 		};
 	}
@@ -135,6 +140,36 @@ public class DDMFormValuesUtil {
 		).collect(
 			Collectors.toList()
 		);
+	}
+
+	private static Set<Locale> _getAvailableLocales(
+		Set<Locale> availableLocales, DDMForm ddmForm, Long groupId) {
+
+		if (SetUtil.isEmpty(availableLocales)) {
+			return ddmForm.getAvailableLocales();
+		}
+
+		Set<Locale> siteAvailableLocales = LanguageUtil.getAvailableLocales(
+			groupId);
+		Set<Locale> locales = new HashSet<>();
+
+		for (Locale availableLocale : availableLocales) {
+			if (siteAvailableLocales.contains(availableLocale)) {
+				locales.add(availableLocale);
+			}
+		}
+
+		return locales;
+	}
+
+	private static Locale _getDefaultLocale(
+		DDMForm ddmForm, Locale defaultLocale) {
+
+		if (defaultLocale == null) {
+			return ddmForm.getDefaultLocale();
+		}
+
+		return defaultLocale;
 	}
 
 	private static Map<String, List<ContentField>> _toContentFieldsMap(
