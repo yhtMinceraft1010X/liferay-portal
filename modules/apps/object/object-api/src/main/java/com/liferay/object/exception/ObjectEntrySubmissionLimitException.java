76f9139b8c0ad57d681818b492f4a7ca28a4578b
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2022-04-28 10:58:30 -0300

    LPS-151134 Auto SF

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectEntrySubmissionLimitException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectEntrySubmissionLimitException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.object.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -27,7 +28,9 @@ public class ObjectEntrySubmissionLimitException extends PortalException {
 		super(msg);
 	}
 
-	public ObjectEntrySubmissionLimitException(String msg, Throwable throwable) {
+	public ObjectEntrySubmissionLimitException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
