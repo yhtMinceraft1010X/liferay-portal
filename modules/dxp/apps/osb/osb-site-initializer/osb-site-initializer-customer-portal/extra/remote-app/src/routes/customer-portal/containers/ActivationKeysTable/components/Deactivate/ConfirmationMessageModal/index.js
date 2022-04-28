53a02b1a062d7abf581928c34470182f4701065e
Igor Farias <igor.farias@liferay.com>
2022-04-28 15:19:39 -0300

    LPS-152461 Creating confirmation deactivate modal

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/ActivationKeysTable/components/Deactivate/ConfirmationMessageModal/index.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import ClayModal from '@clayui/modal';
+import Button from '../../../../../../../common/components/Button';
+
+const ConfirmationMessageModal = ({confirmKeyNoLongerVisible, observer}) => {
+	return (
+		<ClayModal center observer={observer}>
+			<div className="pt-4 px-4">
+				<div className="flex-row mb-1">
+					<div className="d-flex justify-content-between">
+						<h2 className="text-neutral-10">
+							Deactivated Key(s) Request
+						</h2>
+
+						<Button
+							appendIcon="times"
+							aria-label="close"
+							className="align-self-start"
+							displayType="unstyled"
+							onClick={confirmKeyNoLongerVisible}
+						/>
+					</div>
+
+					<p className="mb-6 mt-5 text-neutral-10">
+						A request was just sent to deactivate the selected
+						activation keys. From now on. they will be hidden and no
+						longer be visible.
+					</p>
+				</div>
+
+				<div className="d-flex justify-content-end my-4">
+					<Button
+						className="bg-danger d-flex ml-2"
+						onClick={confirmKeyNoLongerVisible}
+					>
+						Continue
+					</Button>
+				</div>
+			</div>
+		</ClayModal>
+	);
+};
+
+export default ConfirmationMessageModal;
