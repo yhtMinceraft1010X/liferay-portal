39b21f4e3df80140ae31f9a8deaa0bc37dcf2431
rolandpakai <roland.pakai@liferay.com>
2022-04-29 15:54:38 +0200

    LPS-152357 Revert LPS-103591 Fix invisible page tree hitarea icons

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutsTreeImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutsTreeImpl.java
@@ -45,6 +45,7 @@ import com.liferay.portal.kernel.service.permission.LayoutPermission;
 import com.liferay.portal.kernel.service.permission.LayoutPermissionUtil;
 import com.liferay.portal.kernel.servlet.BrowserSniffer;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.SessionClicks;
@@ -54,6 +55,7 @@ import com.liferay.portlet.layoutsadmin.util.LayoutsTree;
 import com.liferay.sites.kernel.util.Sites;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
@@ -248,6 +250,58 @@ public class LayoutsTreeImpl implements LayoutsTree {
 			httpServletRequest, groupId, layoutTreeNodes, layoutSetBranch);
 	}
 
+	private Layout _fetchCurrentLayout(HttpServletRequest httpServletRequest) {
+		long selPlid = ParamUtil.getLong(httpServletRequest, "selPlid");
+
+		if (selPlid > 0) {
+			return _layoutLocalService.fetchLayout(selPlid);
+		}
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (!layout.isTypeControlPanel()) {
+			return layout;
+		}
+
+		return null;
+	}
+
+	private List<Layout> _getAncestorLayouts(
+			HttpServletRequest httpServletRequest)
+		throws Exception {
+
+		Layout layout = _fetchCurrentLayout(httpServletRequest);
+
+		if (layout == null) {
+			return Collections.emptyList();
+		}
+
+		List<Layout> ancestorLayouts = _layoutService.getAncestorLayouts(
+			layout.getPlid());
+
+		if (_log.isDebugEnabled()) {
+			StringBundler sb = new StringBundler(7);
+
+			sb.append("_getAncestorLayouts(plid=");
+			sb.append(layout.getPlid());
+			sb.append(", ancestorLayouts=");
+			sb.append(ancestorLayouts);
+			sb.append(", layout=");
+			sb.append(layout);
+			sb.append(StringPool.CLOSE_PARENTHESIS);
+
+			_log.debug(sb.toString());
+		}
+
+		ancestorLayouts.add(layout);
+
+		return ancestorLayouts;
+	}
+
 	private Layout _getDraftLayout(Layout layout) {
 		if (!layout.isTypeContent()) {
 			return null;
@@ -284,6 +338,8 @@ public class LayoutsTreeImpl implements LayoutsTree {
 
 		List<LayoutTreeNode> layoutTreeNodes = new ArrayList<>();
 
+		List<Layout> ancestorLayouts = _getAncestorLayouts(httpServletRequest);
+
 		int count = _layoutService.getLayoutsCount(
 			groupId, privateLayout, parentLayoutId);
 
@@ -299,20 +355,32 @@ public class LayoutsTreeImpl implements LayoutsTree {
 
 			LayoutTreeNodes childLayoutTreeNodes = null;
 
-			if (layout instanceof VirtualLayout) {
-				VirtualLayout virtualLayout = (VirtualLayout)layout;
+			if (_isExpandableLayout(
+					httpServletRequest, ancestorLayouts, expandedLayoutIds,
+					layout)) {
 
-				childLayoutTreeNodes = _getLayoutTreeNodes(
-					httpServletRequest, virtualLayout.getSourceGroupId(),
-					virtualLayout.isPrivateLayout(),
-					virtualLayout.getLayoutId(), incomplete, expandedLayoutIds,
-					treeId, true);
+				if (layout instanceof VirtualLayout) {
+					VirtualLayout virtualLayout = (VirtualLayout)layout;
+
+					childLayoutTreeNodes = _getLayoutTreeNodes(
+						httpServletRequest, virtualLayout.getSourceGroupId(),
+						virtualLayout.isPrivateLayout(),
+						virtualLayout.getLayoutId(), incomplete,
+						expandedLayoutIds, treeId, true);
+				}
+				else {
+					childLayoutTreeNodes = _getLayoutTreeNodes(
+						httpServletRequest, groupId, layout.isPrivateLayout(),
+						layout.getLayoutId(), incomplete, expandedLayoutIds,
+						treeId, true);
+				}
 			}
 			else {
-				childLayoutTreeNodes = _getLayoutTreeNodes(
-					httpServletRequest, groupId, layout.isPrivateLayout(),
-					layout.getLayoutId(), incomplete, expandedLayoutIds, treeId,
-					true);
+				int childLayoutsCount = _layoutService.getLayoutsCount(
+					groupId, privateLayout, layout.getLayoutId());
+
+				childLayoutTreeNodes = new LayoutTreeNodes(
+					new ArrayList<LayoutTreeNode>(), childLayoutsCount);
 			}
 
 			layoutTreeNode.setChildLayoutTreeNodes(childLayoutTreeNodes);
@@ -447,6 +515,22 @@ public class LayoutsTreeImpl implements LayoutsTree {
 		}
 
 		return true;
+	}
+
+	private boolean _isExpandableLayout(
+		HttpServletRequest httpServletRequest, List<Layout> ancestorLayouts,
+		long[] expandedLayoutIds, Layout layout) {
+
+		boolean expandParentLayouts = ParamUtil.getBoolean(
+			httpServletRequest, "expandParentLayouts");
+
+		if (expandParentLayouts || ancestorLayouts.contains(layout) ||
+			ArrayUtil.contains(expandedLayoutIds, layout.getLayoutId())) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	private boolean _isPaginationEnabled(
