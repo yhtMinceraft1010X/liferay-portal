110a61415af230669cb37a85b6bff69a93ba8dae
shuyangzhou <shuyang.zhou@liferay.com>
2022-04-29 17:33:19 -0700

    LPS-152617 Remove leftover index from 4ef40867eeab4c60da4431d84b214dee3f0c249e

--- modules/apps/client-extension/client-extension-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/client-extension/client-extension-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,3 +1,2 @@
 create index IX_CA514799 on ClientExtensionEntry (companyId, externalReferenceCode[$COLUMN_LENGTH:75$]);
-create unique index IX_7CDD4FB0 on ClientExtensionEntry (companyId, iFrameURL[$COLUMN_LENGTH:4000$]);
 create index IX_F8DF9578 on ClientExtensionEntry (uuid_[$COLUMN_LENGTH:75$], companyId);
