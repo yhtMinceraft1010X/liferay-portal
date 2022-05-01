da7d42d99dbf57c1c208e58e2a86d521410e7876
Alan Huang <alan.huang@liferay.com>
2022-05-01 23:25:07 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/layout/layout-portlets-web/src/main/java/com/liferay/layout/portlets/web/internal/portlet/LayoutPortletsPortlet.java
+++ modules/apps/layout/layout-portlets-web/src/main/java/com/liferay/layout/portlets/web/internal/portlet/LayoutPortletsPortlet.java
@@ -48,7 +48,8 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + LayoutsPortletsPortletKeys.LAYOUT_PORTLETS,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
