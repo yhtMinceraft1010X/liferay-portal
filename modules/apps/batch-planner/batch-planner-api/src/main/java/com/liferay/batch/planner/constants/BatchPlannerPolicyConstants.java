56c36fa9a2838b806544f60f096f5fa576d913bc
Igor Beslic <igor.beslic@liferay.com>
2022-05-02 14:35:38 +0200

    COMMERCE-7541 batch-planner-api - consolidate batch plan policy keys in constants

---
+++ modules/apps/batch-planner/batch-planner-api/src/main/java/com/liferay/batch/planner/constants/BatchPlannerPolicyConstants.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.batch.planner.constants;
+
+import com.liferay.portal.kernel.util.HashMapBuilder;
+
+import java.util.Map;
+
+/**
+ * @author Igor Beslic
+ */
+public class BatchPlannerPolicyConstants {
+
+	public static final Map<String, String> policyUITypes = HashMapBuilder.put(
+		"allowUpdate", "checkbox"
+	).put(
+		"containsHeaders", "checkbox"
+	).put(
+		"delimiter", "text"
+	).put(
+		"enclosingCharacter", "text"
+	).put(
+		"headlessEndpoint", "text"
+	).put(
+		"onErrorFail", "checkbox"
+	).put(
+		"onUpdateDoPatch", "checkbox"
+	).build();
+
+}
