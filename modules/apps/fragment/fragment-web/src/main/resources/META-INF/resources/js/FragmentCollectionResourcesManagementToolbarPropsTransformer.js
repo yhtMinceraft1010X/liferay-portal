c5b32deab774e38cc58a501d84b6cf92a5e5e840
Victor Galan <victor.galan@liferay.com>
2022-05-03 16:45:42 +0200

    LPS-147705 Show modal when trying to remove a fragment collection resource

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/FragmentCollectionResourcesManagementToolbarPropsTransformer.js
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/FragmentCollectionResourcesManagementToolbarPropsTransformer.js
@@ -14,6 +14,8 @@
 
 import {openSelectionModal} from 'frontend-js-web';
 
+import openDeleteFragmentCollectionResourceModal from './openDeleteFragmentCollectionResourceModal';
+
 export default function propsTransformer({portletNamespace, ...otherProps}) {
 	return {
 		...otherProps,
@@ -21,24 +23,21 @@ export default function propsTransformer({portletNamespace, ...otherProps}) {
 			const data = item?.data;
 
 			if (data?.action === 'deleteSelectedFragmentCollectionResources') {
-				if (
-					confirm(
-						Liferay.Language.get(
-							'are-you-sure-you-want-to-delete-this'
-						)
-					)
-				) {
-					const form = document.getElementById(
-						`${portletNamespace}fm`
-					);
-
-					if (form) {
-						submitForm(
-							form,
-							data?.deleteFragmentCollectionResourcesURL
+				openDeleteFragmentCollectionResourceModal({
+					multiple: true,
+					onDelete: () => {
+						const form = document.getElementById(
+							`${portletNamespace}fm`
 						);
-					}
-				}
+
+						if (form) {
+							submitForm(
+								form,
+								data?.deleteFragmentCollectionResourcesURL
+							);
+						}
+					},
+				});
 			}
 		},
 		onCreateButtonClick(event, {item}) {
