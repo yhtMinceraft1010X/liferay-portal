06ea5c44d5da08604ead5454fec0f2862f83fe57
pei-jung <pei-jung.lan@liferay.com>
2022-05-03 16:24:41 -0700

    LPS-151916 (headless-admin-address-impl) - implement getRegionsPage method

--- modules/apps/headless/headless-admin-address/headless-admin-address-impl/src/main/java/com/liferay/headless/admin/address/internal/resource/v1_0/RegionResourceImpl.java
+++ modules/apps/headless/headless-admin-address/headless-admin-address-impl/src/main/java/com/liferay/headless/admin/address/internal/resource/v1_0/RegionResourceImpl.java
@@ -14,9 +14,23 @@
 
 package com.liferay.headless.admin.address.internal.resource.v1_0;
 
+import com.liferay.headless.admin.address.dto.v1_0.Region;
 import com.liferay.headless.admin.address.resource.v1_0.RegionResource;
+import com.liferay.portal.kernel.model.RegionTable;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.service.RegionService;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +41,59 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = RegionResource.class
 )
 public class RegionResourceImpl extends BaseRegionResourceImpl {
+
+	@Override
+	public Page<Region> getRegionsPage(
+			Boolean active, String search, Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		BaseModelSearchResult<com.liferay.portal.kernel.model.Region>
+			baseModelSearchResult = _regionService.searchRegions(
+				contextCompany.getCompanyId(), active, search,
+				pagination.getStartPosition(), pagination.getEndPosition(),
+				_toOrderByComparator(sorts));
+
+		return Page.of(
+			transform(baseModelSearchResult.getBaseModels(), this::_toRegion),
+			pagination, baseModelSearchResult.getLength());
+	}
+
+	private OrderByComparator<com.liferay.portal.kernel.model.Region>
+		_toOrderByComparator(Sort[] sorts) {
+
+		if (ArrayUtil.isEmpty(sorts)) {
+			return null;
+		}
+
+		List<Object> objects = new ArrayList<>();
+
+		for (Sort sort : sorts) {
+			objects.add(sort.getFieldName());
+			objects.add(!sort.isReverse());
+		}
+
+		return OrderByComparatorFactoryUtil.create(
+			RegionTable.INSTANCE.getTableName(),
+			objects.toArray(new Object[0]));
+	}
+
+	private Region _toRegion(
+			com.liferay.portal.kernel.model.Region serviceBuilderRegion)
+		throws Exception {
+
+		return new Region() {
+			{
+				active = serviceBuilderRegion.getActive();
+				countryId = serviceBuilderRegion.getCountryId();
+				id = serviceBuilderRegion.getRegionId();
+				name = serviceBuilderRegion.getName();
+				regionCode = serviceBuilderRegion.getRegionCode();
+				title_i18n = serviceBuilderRegion.getLanguageIdToTitleMap();
+			}
+		};
+	}
+
+	@Reference
+	private RegionService _regionService;
+
 }
