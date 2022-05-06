7dc607a599d8566f669f41c3659ebf2d1101a5f8
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-05-06 09:05:46 -0300

    LPS-152195 gw buildUpgradeTable

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v3_3_0/util/ObjectViewFilterColumnTable.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.internal.upgrade.v3_3_0.util;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ * @see com.liferay.portal.tools.upgrade.table.builder.UpgradeTableBuilder
+ */
+public class ObjectViewFilterColumnTable {
+
+	public static UpgradeProcess create() {
+		return new UpgradeProcess() {
+
+			@Override
+			protected void doUpgrade() throws Exception {
+				if (!hasTable(_TABLE_NAME)) {
+					runSQL(_TABLE_SQL_CREATE);
+				}
+			}
+
+		};
+	}
+
+	private static final String _TABLE_NAME = "ObjectViewFilterColumn";
+
+	private static final String _TABLE_SQL_CREATE =
+		"create table ObjectViewFilterColumn (mvccVersion LONG default 0 not null,uuid_ VARCHAR(75) null,objectViewFilterColumnId LONG not null primary key,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,objectViewId LONG,filterType VARCHAR(75) null,json VARCHAR(75) null,objectFieldName VARCHAR(75) null)";
+
+}
