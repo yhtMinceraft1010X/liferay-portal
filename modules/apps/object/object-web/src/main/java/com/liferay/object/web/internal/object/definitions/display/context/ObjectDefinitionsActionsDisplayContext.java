ea068652ec9e6690b39ab9c80e9cef72637bca2e
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-05-11 17:19:26 -0300

    LPS-151535 Revert "LPS-151531 Add feature flag for LPS-146871"
    This reverts commit b13804be3475c1b44ce7986dc995628194a8c508.

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ObjectDefinitionsActionsDisplayContext.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ObjectDefinitionsActionsDisplayContext.java
@@ -20,7 +20,6 @@ import com.liferay.object.action.executor.ObjectActionExecutor;
 import com.liferay.object.action.executor.ObjectActionExecutorRegistry;
 import com.liferay.object.action.trigger.ObjectActionTrigger;
 import com.liferay.object.action.trigger.ObjectActionTriggerRegistry;
-import com.liferay.object.constants.ObjectActionExecutorConstants;
 import com.liferay.object.model.ObjectAction;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.web.internal.constants.ObjectWebKeys;
@@ -33,9 +32,6 @@ import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PropsUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.Arrays;
 import java.util.List;
@@ -105,19 +101,11 @@ public class ObjectDefinitionsActionsDisplayContext
 		JSONArray objectActionExecutorsJSONArray =
 			_jsonFactory.createJSONArray();
 
-		for (ObjectActionExecutor objectActionExecutor :
-				_objectActionExecutorRegistry.getObjectActionExecutors()) {
+		List<ObjectActionExecutor> objectActionExecutors =
+			_objectActionExecutorRegistry.getObjectActionExecutors();
 
-			if (StringUtil.equals(
-					objectActionExecutor.getKey(),
-					ObjectActionExecutorConstants.KEY_GROOVY) &&
-				!GetterUtil.getBoolean(
-					PropsUtil.get("feature.flag.LPS-146871"))) {
-
-				continue;
-			}
-
-			objectActionExecutorsJSONArray.put(
+		objectActionExecutors.forEach(
+			objectActionExecutor -> objectActionExecutorsJSONArray.put(
 				JSONUtil.put(
 					"description",
 					LanguageUtil.get(
@@ -132,8 +120,7 @@ public class ObjectDefinitionsActionsDisplayContext
 							objectActionExecutor.getKey() + "]")
 				).put(
 					"value", objectActionExecutor.getKey()
-				));
-		}
+				)));
 
 		return objectActionExecutorsJSONArray;
 	}
